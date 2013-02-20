ca5b95bb8f9c98a9159aff750b325cbba0f897ac
Marek Brzóska <brzoskamarek@gmail.com>
2013-02-20 14:39:46 +0100

    Filter responses to run html tidy for

--- saleor/middleware.py
+++ saleor/middleware.py
@@ -8,7 +8,11 @@ logger = logging.getLogger('saleor')
 
 class CheckHTML(object):
     def process_response(self, _request, response):
-        if settings.DEBUG and settings.WARN_ABOUT_INVALID_HTML5_OUTPUT:
+        if (
+            settings.DEBUG
+            and settings.WARN_ABOUT_INVALID_HTML5_OUTPUT
+            and 200 <= response.status.code < 300
+        ):
             proc = Popen(["tidy"], stdout=PIPE, stderr=PIPE, stdin=PIPE)
             _out, err = proc.communicate(response.content)
             for l in err.split('\n\n')[0].split('\n')[:-2]:
