7699592cbc48e3104737d785ad7f2c2c9f968bbd
Michal Ociepka <michal@ociepka.info>
2013-02-25 15:20:31 +0100

    Replace Cart models by session Cart.

---
+++ cart/__init__.py
@@ -0,0 +1,36 @@
+from decimal import Decimal
+from django.conf import settings
+from django.utils.translation import ugettext as _
+from satchless.item import ItemSet, ItemLine
+from satchless import cart
+
+
+class InvalidQuantityException(Exception):
+
+    def __init__(self, reason, quantity_delta):
+        super(InvalidQuantityException, self).__init__(reason)
+        self.quantity_delta = quantity_delta
+
+
+class Cart(cart.Cart):
+
+    SESSION_KEY = 'cart'
+    modified = False
+
+    def __unicode__(self):
+        return _('Cart (%(cart_count)s)' % {'cart_count': self.count()})
+
+    def check_quantity(self, product, quantity, replace=False):
+        if quantity > product.stock:
+            raise InvalidQuantityException(
+                _(u'Only %(total)s of product in stock.' % {
+                    'total':product.stock
+                }),
+                product.stock - quantity
+            )
+
+        return super(Cart, self).check_quantity(product, quantity, replace)
+
+    def get_default_currency(self):
+        return settings.SATCHLESS_DEFAULT_CURRENCY
+
