6d6070caf4d2f1f5fd07ef22152eb070345ace72
Michal Ociepka <michal@ociepka.info>
2013-03-21 13:25:38 +0100

    Steps refactoring

--- order/views.py
+++ order/views.py
@@ -1,28 +1,9 @@
-from .steps import (BillingAddressStep, ShippingStep, DigitalDeliveryStep,
-                    SummaryStep, SuccessStep)
 from cart import (get_cart_from_request, remove_cart_from_request,
                   CartPartitioner)
 from django.http.response import Http404
 from django.shortcuts import redirect, get_object_or_404
-from order.models import Order, DigitalDeliveryGroup
-from satchless.process import ProcessManager
-
-
-class CheckoutProcessManager(ProcessManager):
-
-    def __init__(self, order, request):
-        self.steps = [BillingAddressStep(order, request)]
-        for delivery_group in order.groups.all():
-            delivery_step_class = ShippingStep
-            if isinstance(delivery_group, DigitalDeliveryGroup):
-                delivery_step_class = DigitalDeliveryStep
-            self.steps.append(delivery_step_class(order, request,
-                                                  delivery_group))
-        self.steps.append(SummaryStep(order, request))
-        self.steps.append(SuccessStep(order, request))
-
-    def __iter__(self):
-        return iter(self.steps)
+from order.models import Order
+from order.steps import CheckoutProcessManager
 
 
 def index(request, token=None):
