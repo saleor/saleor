d9e8d89565005a79bfdb3aaf5e398434a163e5e5
Michal Ociepka <michal@ociepka.info>
2013-03-21 18:05:38 +0100

    Add new groups

--- cart/__init__.py
+++ cart/__init__.py
@@ -1,3 +1,4 @@
+from delivery import DummyShipping, DigitalDelivery
 from django.conf import settings
 from django.utils.translation import ugettext
 from itertools import groupby
@@ -10,13 +11,74 @@ from userprofile.forms import AddressForm
 import datetime
 
 
+class Group(ItemSet):
+
+    _state = None
+
+    def __getstate__(self):
+        return self._state
+
+    def __setstate__(self, state):
+        self._state = state
+
+    def __str__(self):
+        raise NotImplementedError()
+
+    def add_to_order(self, order):
+        raise NotImplementedError()
+
+    def get_delivery_methods(self, **kwargs):
+        raise NotImplementedError()
+
+
+class ShippedGroup(Group):
+
+    def __init__(self, items, pk=None, address=None, delivery=None):
+        super(Group, self).__init__(items)
+        self._state = {}
+        self._state['pk'] = pk
+        self._state['address'] = address
+        self._state['delivery'] = delivery
+
+    def __str__(self):
+        if self._state['pk']:
+            return 'delivery-' % (self._state['pk'], )
+        return 'delivery'
+
+    def add_to_order(self, order):
+        raise NotImplementedError()
+
+    def get_delivery_methods(self, **kwargs):
+        yield DummyShipping(self)
+
+
+class DigitalGroup(Group):
+
+    def __init__(self, items, pk=None, email=None):
+        super(Group, self).__init__(items)
+        self._state = {}
+        self._state['pk'] = pk
+        self._state['email'] = email
+
+    def __str__(self):
+        if self._state['pk']:
+            return 'digital-delivery-' % (self._state['pk'], )
+        return 'digital-delivery'
+
+    def add_to_order(self, order):
+        raise NotImplementedError()
+
+    def get_delivery_methods(self, **kwargs):
+        yield DigitalDelivery(self)
+
+
 class CartPartitioner(Partitioner):
 
     def __iter__(self):
         for product_class, items in groupby(
                 self.subject,
                 lambda cart_item: cart_item.product.__class__):
-            delivery_class = ShippedDeliveryGroup
+            delivery_class = ShippedGroup
             if issubclass(product_class, DigitalShip):
                 delivery_class = DigitalDeliveryGroup
             delivery = delivery_class()
@@ -50,6 +112,7 @@ class Cart(cart.Cart):
 
     SESSION_KEY = 'cart'
     timestamp = None
+    billing_address = None
 
     def __init__(self, *args, **kwargs):
         super(Cart, self).__init__(self, *args, **kwargs)
