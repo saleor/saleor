0dfca8c861374ca0ca1d8f5f3c6ff5182af8dfe4
Michal Ociepka <michal@ociepka.info>
2013-03-29 14:12:01 +0100

    Pep8

--- cart/middleware.py
+++ cart/middleware.py
@@ -1,5 +1,6 @@
 from cart import Cart
 
+
 class CartMiddleware(object):
     '''
     Saves the cart instance into the django session.
@@ -10,12 +11,10 @@ class CartMiddleware(object):
         except KeyError:
             cart = Cart()
             request.session[Cart.SESSION_KEY] = cart
-
         setattr(request, 'cart', cart)
 
     def process_response(self, request, response):
         if hasattr(request, 'cart') and request.cart.modified:
             request.cart.modified = False
             request.session[Cart.SESSION_KEY] = request.cart
-
         return response
