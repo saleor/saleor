ca35d64e919f413386f2ba1865319163f5d6c11f
Michal Ociepka <michal@ociepka.info>
2013-04-23 17:55:52 +0200

    Add confirmation emails sending in order & payment

--- payment/models.py
+++ payment/models.py
@@ -1,7 +1,9 @@
+from communication.mail import send_email
 from django.core.urlresolvers import reverse
 from django.db import models
 from order.models import Order
 from payments.models import BasePayment
+from saleor.utils import build_absolute_uri
 
 
 class Payment(BasePayment):
@@ -14,3 +16,10 @@ class Payment(BasePayment):
 
     def get_success_url(self):
         return reverse('order:success', kwargs={'token': self.order.token})
+
+    def send_confirmation_email(self):
+        email = self.order.get_user_email()
+        order_url = build_absolute_uri(
+            reverse('order:details', kwargs={'token': self.order.token}))
+        context = {'order_url': order_url}
+        send_email(email, 'payment/emails/confirm_email.txt', context)
