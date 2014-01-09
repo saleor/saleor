f6a974a1dc5337e482fe6fcac402597735892567
Michał Ociepka <michal@ociepka.info>
2014-01-09 12:28:05 +0100

    Use the delivery classes as proxy for items groups

--- saleor/delivery/__init__.py
+++ saleor/delivery/__init__.py
@@ -1,25 +1,51 @@
 from __future__ import unicode_literals
+from re import sub
 
 from django.conf import settings
 from prices import Price
-from satchless.item import Item
+from satchless.item import ItemSet
+
+from ..cart import ShippedGroup
 
 
-class BaseDelivery(Item):
+class BaseDelivery(ItemSet):
+
+    group = None
 
     def __init__(self, delivery_group):
         self.group = delivery_group
 
-    def get_price_per_item(self, **kwargs):
+    def __iter__(self):
+        return iter(self.group)
+
+    def get_delivery_total(self, **kwargs):
         return Price(0, currency=settings.SATCHLESS_DEFAULT_CURRENCY)
+
+    def get_total_with_delivery(self):
+        return self.group.get_total() + self.get_delivery_total()
+
+    @property
+    def name(self):
+        '''
+        Returns undescored version of class name
+        '''
+        name = type(self).__name__
+        name = sub('(((?<=[a-z])[A-Z])|([A-Z](?![A-Z]|$)))', '_\\1', name)
+        return name.lower().strip('_')
 
 
 class DummyShipping(BaseDelivery):
 
+    address = None
+
+    def __init__(self, delivery_group, address):
+        self.address = address
+        super(DummyShipping, self).__init__(delivery_group)
+
     def __unicode__(self):
         return 'Dummy shipping'
 
-    def get_price_per_item(self, **kwargs):
+    def get_delivery_total(self, **kwargs):
         weight = sum(line.product.weight for line in self.group)
         qty = sum(line.quantity for line in self.group)
         return Price(qty * weight,
@@ -28,5 +54,18 @@ class DummyShipping(BaseDelivery):
 
 class DigitalDelivery(BaseDelivery):
 
+    email = None
+
+    def __init__(self, delivery_group, email):
+        self.email = email
+        super(DigitalDelivery, self).__init__(delivery_group)
+
     def __unicode__(self):
         return 'Digital delivery'
+
+
+def get_delivery_methods_for_group(group, **kwargs):
+    if isinstance(group, ShippedGroup):
+        yield DummyShipping(group, kwargs['address'])
+    else:
+        yield DigitalDelivery(group, kwargs['email'])
