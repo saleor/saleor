5f02516f01150d638f44b33439d98e964257a750
Michał Ociepka <michal@ociepka.info>
2014-01-20 16:02:06 +0100

    Add custom forms for each product type

--- saleor/product/forms.py
+++ saleor/product/forms.py
@@ -1,6 +1,22 @@
+from django import forms
+
 from ..cart.forms import AddToCartForm
+from .models import Bag, Shirt
 
 
-class ProductForm(AddToCartForm):
+class BagForm(AddToCartForm):
 
-    pass
+    color = forms.CharField()
+
+
+class ShirtForm(AddToCartForm):
+
+    color = forms.CharField()
+
+
+def get_form_class_for_product(product):
+    if type(product) is Shirt:
+        return ShirtForm
+    if type(product) is Bag:
+        return BagForm
+    raise NotImplemented()
