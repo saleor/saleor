7ca7d38415e8651b4f23b6382c977bd0b5b473c6
Artur Smęt <artur.smet@mirumee.com>
2014-02-17 19:07:03 +0100

    ColorVariants connected to product forms, PriceRange errors fixed in template

--- saleor/product/forms.py
+++ saleor/product/forms.py
@@ -1,22 +1,17 @@
 from django import forms
 
 from ..cart.forms import AddToCartForm
-from .models import Bag, BagVariant, Shirt, ShirtVariant
+from .models import Bag, BagVariant, Shirt, ShirtVariant, Color
 
 
 class BagForm(AddToCartForm):
 
-    color = forms.ChoiceField(choices=BagVariant.COLOR_CHOICES)
+    color = forms.ModelChoiceField(queryset=None)
 
     def __init__(self, *args, **kwargs):
         super(BagForm, self).__init__(*args, **kwargs)
         color_field = self.fields['color']
-        color_values = list(
-            self.product.variants.values_list('color', flat=True))
-        color_choices = color_field.choices
-        new_choices = [
-            choice for choice in color_choices if choice[0] in color_values]
-        color_field.choices = new_choices
+        color_field.queryset = self.product.get_available_colors()
 
     def get_variant(self, clean_data):
         color = clean_data.get('color')
@@ -25,13 +20,18 @@ class BagForm(AddToCartForm):
 
 class ShirtForm(AddToCartForm):
 
-    color = forms.ChoiceField(choices=ShirtVariant.COLOR_CHOICES)
+    color = forms.ModelChoiceField(queryset=None)
     size = forms.ChoiceField(choices=ShirtVariant.SIZE_CHOICES)
 
     def get_variant(self, clean_data):
         color = clean_data.get('color')
         size = clean_data.get('size')
-        return ShirtVariant.objects.get(color=color, size=size)
+        return ShirtVariant.objects.get(size=size, color=color)
+
+    def __init__(self, *args, **kwargs):
+        super(ShirtForm, self).__init__(*args, **kwargs)
+        color_field = self.fields['color']
+        color_field.queryset = self.product.get_available_colors()
 
 
 def get_form_class_for_product(product):
