7a28edcac44758a6c1779963041a9dadec1145f7
Artur Smęt <artur.smet@mirumee.com>
2014-02-21 12:35:37 +0100

    Bugfix, select variant only from selected product variants, not whole database

--- saleor/product/forms.py
+++ saleor/product/forms.py
@@ -1,18 +1,17 @@
 from django import forms
 
 from ..cart.forms import AddToCartForm
-from .models import Bag, BagVariant, Shirt, ShirtVariant
+from .models import Bag, Shirt, ShirtVariant
 
 
 class BagForm(AddToCartForm):
     quantity = forms.IntegerField(initial=1)
 
     def get_variant(self, clean_data):
-        return BagVariant.objects.get(product__color=self.product.color)
+        return self.product.variants.get(product__color=self.product.color)
 
 
 class ShirtForm(AddToCartForm):
-
     size = forms.ChoiceField(choices=ShirtVariant.SIZE_CHOICES,
                              widget=forms.RadioSelect())
     quantity = forms.IntegerField(initial=1)
@@ -26,8 +25,8 @@ class ShirtForm(AddToCartForm):
 
     def get_variant(self, clean_data):
         size = clean_data.get('size')
-        return ShirtVariant.objects.get(size=size,
-                                        product__color=self.product.color)
+        return self.product.variants.get(size=size,
+                                         product__color=self.product.color)
 
 
 def get_form_class_for_product(product):
