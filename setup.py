6a7d08ed84848fb9535a0efc167dbb1f3777d48e
Artur Smęt <artur.smet@mirumee.com>
2014-02-27 12:55:13 +0100

    Moved fake-factory to project requirements

--- setup.py
+++ setup.py
@@ -43,14 +43,15 @@ setup(
         'South>=0.7.6',
         'requests>=1.2.0',
         'unidecode',
-        'django-selectable==0.8.0'
+        'django-selectable==0.8.0',
+        'fake-factory>=0.3.2'
     ],
     entry_points={
         'console_scripts': ['saleor = saleor:manage']},
     tests_require=[
         'mock==1.0.1',
         'purl>=0.4.1',
-        'fake-factory>=0.3.2'],
+    ],
     test_suite='saleor.tests.suite',
     cmdclass={
         'populatedb': PopulateDatabase
