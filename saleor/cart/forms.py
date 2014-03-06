7225ef1f92d6127a6e5a70a450f59f37bd30fff0
Artur Smęt <artur.smet@mirumee.com>
2014-03-06 16:44:00 +0100

    Set max_value of QuantityField to 999

--- saleor/cart/forms.py
+++ saleor/cart/forms.py
@@ -11,7 +11,8 @@ from satchless.item import InsufficientStock
 class QuantityField(forms.IntegerField):
 
     def __init__(self, *args, **kwargs):
-        super(QuantityField, self).__init__(min_value=0, initial=1)
+        super(QuantityField, self).__init__(min_value=0, max_value=999,
+                                            initial=1)
 
 
 class AddToCartForm(forms.Form):
