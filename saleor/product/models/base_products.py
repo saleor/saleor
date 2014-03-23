aa4dc9bc2f3b7fb009f5f12673ed8e6f45aab4ed
George Lubaretsi <gio@transparency.ge>
2014-03-24 02:58:13 +0400

    Remove redundant filters

--- saleor/product/models/base_products.py
+++ saleor/product/models/base_products.py
@@ -66,9 +66,7 @@ class Product(Subtyped, ItemRange):
 
     def get_products_from_collection(self):
         return Product.objects.filter(
-            collection=self.collection,
-            collection__isnull=False
-        ).exclude(collection__iexact='') if self.collection else None
+            collection=self.collection) if self.collection else None
 
     def __iter__(self):
         return iter(self.variants.all())
