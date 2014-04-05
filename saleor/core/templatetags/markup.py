4e7073a825cbc7cd57d783c7e275a083156d66e3
zauddelig <zauddelig@gmail.com>
2014-04-05 18:15:16 +0200

    Update markup.py

--- saleor/core/templatetags/markup.py
+++ saleor/core/templatetags/markup.py
@@ -1,8 +1,10 @@
 from django import template
+import markdown as markd
+from django.utils.safestring import mark_safe
 register = template.Library()
 
 @register.filter
 def markdown(text):
-    #pretty simple
-    return markdown.markdown(text, safe_mode="remove", output_format="html5")
+    #should use bleach here instad of safe_mode="remove"?
+    return mark_safe(markd.markdown(text, safe_mode="remove", output_format="html5"))
     pass
