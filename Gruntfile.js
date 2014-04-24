7f207184c2260baaea4ae6bab1268aeb930993df
Paweł Kowalewski <pawel@mirumee.com>
2014-04-24 14:13:46 +0200

    Add Gruntfile.js for grunt

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      production: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          cleancss: true,
+          optimization: 2
+        },
+        files: {
+          "saleor/static/css/style.css": "saleor/static/less/style.less"
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.registerTask('default', ['less']);
+};
