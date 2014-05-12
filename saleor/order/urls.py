2762599917362bc621e84a17ae922588ad4296ae
Michał Ociepka <michal@ociepka.info>
2014-05-12 15:12:07 +0200

    Add all words and "-" to payment url

--- saleor/order/urls.py
+++ saleor/order/urls.py
@@ -9,7 +9,7 @@ TOKEN_PATTERN = ('(?P<token>[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}'
 urlpatterns = patterns(
     '',
     url(r'^%s/$' % TOKEN_PATTERN, views.details, name='details'),
-    url(r'^%s/payment/(?P<variant>[a-z-]+)/$' % TOKEN_PATTERN,
+    url(r'^%s/payment/(?P<variant>[-\w]+)/$' % TOKEN_PATTERN,
         views.start_payment, name='payment'),
     url(r'^%s/cancel-payment/$' % TOKEN_PATTERN, views.cancel_payment,
         name='cancel-payment'))
