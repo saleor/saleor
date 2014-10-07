6638cf8a98c7e976e32232689ce83cb582e42eeb
Marcin Gębala <maarcin.gebala@gmail.com>
2014-10-07 12:06:27 +0200

    Remove payment actions from payment details view

--- saleor/dashboard/payments/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class CaptureForm(forms.Form):
-    amount = forms.DecimalField()
--- saleor/dashboard/payments/views.py
+++ saleor/dashboard/payments/views.py
@@ -1,13 +1,8 @@
-from django.contrib import messages
-from django.utils.translation import ugettext_lazy as _
 from django.views.generic import ListView, DetailView
-from django.shortcuts import redirect
-from payments import PaymentError
 from payments.models import PAYMENT_STATUS_CHOICES
 
 from ..views import FilterByStatusMixin
 from ...order.models import Payment
-from .forms import CaptureForm
 
 
 class PaymentList(FilterByStatusMixin, ListView):
@@ -28,50 +23,3 @@ class PaymentDetails(DetailView):
     model = Payment
     template_name = 'dashboard/payments/detail.html'
     context_object_name = 'payment'
-    form_class = CaptureForm
-
-    def get_context_data(self, **kwargs):
-        ctx = super(PaymentDetails, self).get_context_data(**kwargs)
-        ctx['capture_form'] = self.get_capture_form()
-        ctx['refund_available'] = self.object.status == 'confirmed'
-        ctx['release_available'] = self.object.status == 'preauth'
-        return ctx
-
-    def get_capture_form(self):
-        if self.object.status == 'preauth':
-            return self.form_class(initial={'amount': self.object.total})
-        else:
-            return None
-
-    def post(self, *args, **kwargs):
-        form = self.form_class(self.request.POST)
-        payment = self.get_object()
-        error_msg = None
-
-        if form.is_valid():
-            try:
-                payment.capture(amount=form.cleaned_data['amount'])
-            except PaymentError as e:
-                error_msg = _('Payment gateway error: ') + e.message
-            else:
-                messages.success(self.request, _('Funds captured'))
-
-        if 'refund' in self.request.POST and payment.status == 'confirmed':
-            try:
-                payment.refund(amount=payment.captured_amount)
-            except PaymentError as e:
-                error_msg = _('Payment gateway error: ') + e.message
-            else:
-                messages.success(self.request, _('Refund successful'))
-
-        if 'release' in self.request.POST and payment.status == 'preauth':
-            try:
-                payment.release()
-            except PaymentError as e:
-                error_msg = _('Payment gateway error: ') + e.message
-            else:
-                messages.success(self.request, _('Release successful'))
-
-        if error_msg:
-            messages.error(self.request, error_msg)
-        return redirect('dashboard:payment-details', pk=payment.pk)
