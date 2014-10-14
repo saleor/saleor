d37dddd3ada20434d889ff9dcfb162e62c95f3d7
Marcin Gębala <maarcin.gebala@gmail.com>
2014-10-14 12:55:12 +0200

    Use prices module to display prices in payments views

--- saleor/dashboard/payments/views.py
+++ saleor/dashboard/payments/views.py
@@ -1,6 +1,7 @@
 import json
 from django.views.generic import ListView, DetailView
 from payments.models import PAYMENT_STATUS_CHOICES
+from prices import Price
 
 from ..views import FilterByStatusMixin
 from ...order.models import Payment
@@ -15,6 +16,14 @@ class PaymentList(FilterByStatusMixin, ListView):
     status_order = ['waiting', 'input', 'preauth', 'confirmed', 'refunded',
                     'rejected', 'error']
 
+    def get_context_data(self, **kwargs):
+        ctx = super(PaymentList, self).get_context_data(**kwargs)
+        for payment in ctx['payments']:
+            payment.total = Price(payment.total, currency=payment.currency)
+            payment.captured_amount = Price(payment.captured_amount,
+                                            currency=payment.currency)
+        return ctx
+
     def get_queryset(self):
         qs = super(PaymentList, self).get_queryset()
         return qs.order_by('-created')
@@ -24,6 +33,13 @@ class PaymentDetails(DetailView):
     model = Payment
     template_name = 'dashboard/payments/detail.html'
     context_object_name = 'payment'
+
+    def get_object(self, queryset=None):
+        obj = super(PaymentDetails, self).get_object(queryset)
+        currency = obj.currency
+        obj.total = Price(obj.total, currency=currency)
+        obj.captured_amount = Price(obj.captured_amount, currency=currency)
+        return obj
 
     def get_context_data(self, **kwargs):
         ctx = super(PaymentDetails, self).get_context_data(**kwargs)
