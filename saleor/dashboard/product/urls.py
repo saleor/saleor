0d2afc354f0a4dcd6759df237ff6783ec74b7577
Marcin Gębala <maarcin.gebala@gmail.com>
2014-10-16 12:10:24 +0200

    More refactoring of product views

--- saleor/dashboard/product/urls.py
+++ saleor/dashboard/product/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = patterns(
     '',
     url(r'^$',
-        views.ProductListView.as_view(), name='products'),
+        views.product_list, name='products'),
     url(r'^(?P<pk>[0-9]+)/update/$',
         views.product_details, name='product-update'),
     url(r'^add/(?P<category>[-\w]+)$',
--- saleor/dashboard/product/views.py
+++ saleor/dashboard/product/views.py
@@ -1,43 +1,33 @@
-from django.core.urlresolvers import reverse, reverse_lazy
+from django.core.urlresolvers import reverse_lazy
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
-from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
-from django.views.generic import ListView, DeleteView
-from django.views.generic.edit import FormMixin
+from django.views.generic import DeleteView
 from django.utils.translation import ugettext_lazy as _
-from ..views import StaffMemberOnlyMixin
 from ...product.models import Product
+from ..views import StaffMemberOnlyMixin
 from .forms import (ProductImageFormSet, ProductCategoryForm, get_product_form,
                     get_variant_formset)
 
 
-class ProductListView(StaffMemberOnlyMixin, ListView, FormMixin):
-    model = Product
-    paginate_by = 30
-    template_name = 'dashboard/product/list.html'
-    context_object_name = 'products'
-    form_class = ProductCategoryForm
-
-    def get_queryset(self):
-        qs = super(ProductListView, self).get_queryset()
-        qs = qs.prefetch_related('images').select_related('brand')
-        qs = qs.select_subclasses()
-        return qs
-
-    def get_context_data(self, **kwargs):
-        ctx = super(ProductListView, self).get_context_data(**kwargs)
-        ctx['form'] = self.form_class()
-        return ctx
-
-    def post(self, request, **kwargs):
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            return HttpResponseRedirect(reverse(
-                'dashboard:product-add',
-                kwargs={'category': form.cleaned_data['category']}))
-        return self.get(request, **kwargs)
+@staff_member_required
+def product_list(request):
+    products = Product.objects.prefetch_related('images').select_subclasses()
+    paginator = Paginator(products, 30)
+    try:
+        products = paginator.page(request.GET.get('page'))
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+    form = ProductCategoryForm(request.POST or None)
+    if form.is_valid():
+        category = form.cleaned_data['category']
+        return redirect('dashboard:product-add', category=category)
+    ctx = {'products': products, 'form': form, 'paginator': paginator}
+    return TemplateResponse(request, 'dashboard/product/list.html', ctx)
 
 
 @staff_member_required
@@ -46,32 +36,30 @@ def product_details(request, pk=None, category=None):
     form_cls = get_product_form(product, category)
     variant_formset_cls = get_variant_formset(product, category)
     form = form_cls(request.POST or None, instance=product)
-    if request.method == 'POST':
-        if form.is_valid():
-            product = form.save()
-            variant_formset = variant_formset_cls(request.POST,
-                                                  instance=product)
-            image_formset = ProductImageFormSet(request.POST, request.FILES,
-                                                instance=product)
-            if variant_formset.is_valid() and image_formset.is_valid():
-                variant_formset.save()
-                image_formset.save()
-                if pk:
-                    messages.success(request, _('Product %s updated' % product))
-                    return redirect('dashboard:product-update', pk=product.pk)
-                else:
-                    messages.success(request, _('Product %s added' % product))
-                    return redirect('dashboard:products')
+    variant_formset = variant_formset_cls(instance=product)
+    image_formset = ProductImageFormSet(instance=product)
+    err_msg = _('Your submitted data was not valid - please correct the errors'
+                ' below')
+    if form.is_valid():
+        product = form.save()
+        variant_formset = variant_formset_cls(request.POST,
+                                              instance=product)
+        image_formset = ProductImageFormSet(request.POST, request.FILES,
+                                            instance=product)
+        if variant_formset.is_valid() and image_formset.is_valid():
+            variant_formset.save()
+            image_formset.save()
+            if pk:
+                msg = _('Product %s updated' % product)
             else:
-                error = True
+                msg = _('Product %s added' % product)
+            messages.success(request, msg)
+            return redirect('dashboard:products')
         else:
-            error = True
-        if error:
-            messages.error(request, _('Your submitted data was not valid'
-                                      ' - please correct the errors below'))
+            messages.error(request, err_msg)
     else:
-        variant_formset = variant_formset_cls(instance=product)
-        image_formset = ProductImageFormSet(instance=product)
+        if form.errors:
+            messages.error(request, err_msg)
     title = product.name if product else _('Add new product')
     ctx = {'title': title, 'product': product, 'form': form,
            'variant_formset': variant_formset, 'image_formset': image_formset}
