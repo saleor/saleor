c4e9017983fc70b522d9aea8693e9a2afda87e24
Marcin Gębala <maarcin.gebala@gmail.com>
2014-10-22 11:53:16 +0200

    Reverse logic of initializing form object

--- saleor/order/views.py
+++ saleor/order/views.py
@@ -32,9 +32,9 @@ def details(request, token):
             None, order=order, initial={'payment_id': waiting_payment.id})
     if order.is_fully_paid():
         form_data = None
-    if order.is_pre_authorized():
-        payment_form = None
-    else:
+
+    payment_form = None
+    if not order.is_pre_authorized():
         payment_form = PaymentMethodsForm(form_data)
 
         if payment_form.is_valid():
