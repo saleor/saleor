640a71ffd4d3e43131ceedf922e38f86b5e61f76
Patryk Zawadzki <patrys@room-303.com>
2015-05-18 15:46:14 +0200

    Convert the price field to bootstrap

--- saleor/dashboard/product/forms.py
+++ saleor/dashboard/product/forms.py
@@ -4,14 +4,13 @@ from django import forms
 from django.forms.models import inlineformset_factory
 from django.forms.widgets import ClearableFileInput
 
+from ...core.widgets import PriceInput
 from ...product.models import (ProductImage, Product, ShirtVariant, BagVariant,
                                Shirt, Bag)
 
-
 PRODUCT_CLASSES = {
     'shirt': Shirt,
-    'bag': Bag
-}
+    'bag': Bag}
 
 
 class ProductClassForm(forms.Form):
@@ -29,12 +28,14 @@ class ShirtForm(ProductForm):
     class Meta:
         model = Shirt
         exclude = []
+        widgets = {'price': PriceInput}
 
 
 class BagForm(ProductForm):
     class Meta:
         model = Bag
         exclude = []
+        widgets = {'price': PriceInput}
 
 
 class ImageInputWidget(ClearableFileInput):
