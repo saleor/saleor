0b55fce3894ab2919d9b0fbbf15429b17140a333
Marcin Gębala <maarcin.gebala@gmail.com>
2015-06-01 18:04:06 +0200

    Initial work for category management

--- saleor/dashboard/product/urls.py
+++ saleor/dashboard/product/urls.py
@@ -12,4 +12,11 @@ urlpatterns = patterns(
     url(r'^add/(?P<cls_name>[-\w]+)/$',
         views.product_details, name='product-add'),
     url(r'^(?P<pk>[0-9]+)/delete/$',
-        views.ProductDeleteView.as_view(), name='product-delete'),)
+        views.ProductDeleteView.as_view(), name='product-delete'),
+
+    url(r'^categories/$',
+        views.category_list, name='category-list'),
+    url(r'^categories/(?P<pk>[0-9]+)/update/$',
+        views.category_details, name='category-update'),
+    url(r'^categories/add/$',
+        views.category_details, name='category-add'))
