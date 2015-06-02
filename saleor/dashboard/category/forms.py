e1c57cb41c59c118648602ff9837418e5d4baad4
Marcin Gębala <maarcin.gebala@gmail.com>
2015-06-02 14:38:24 +0200

    Add validation on category parent field

--- saleor/dashboard/category/forms.py
+++ saleor/dashboard/category/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from ...product.models import Category
 
@@ -7,3 +8,9 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         exclude = []
+
+    def clean_parent(self):
+        parent = self.cleaned_data['parent']
+        if parent == self.instance:
+            raise forms.ValidationError(_('A category may not be made a child of itself'))
+        return parent
