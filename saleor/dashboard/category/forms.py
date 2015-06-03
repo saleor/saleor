9fab51577d29ec21c1322fe550727c041f51fdf4
Marcin Gębala <maarcin.gebala@gmail.com>
2015-06-03 13:03:34 +0200

    Use TreeNodeChoiceField for rendering 'parent' field in CategoryForm

--- saleor/dashboard/category/forms.py
+++ saleor/dashboard/category/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from mptt.forms import TreeNodeChoiceField
 
 from ...product.models import Category
 
@@ -8,6 +9,10 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         exclude = []
+
+    def __init__(self, *args, **kwargs):
+        super(CategoryForm, self).__init__(*args, **kwargs)
+        self.fields['parent'] = TreeNodeChoiceField(queryset=Category.objects.all())
 
     def clean_parent(self):
         parent = self.cleaned_data['parent']
