48179191e1a54f3224600f7b2ed573d2f7ea6f87
Marcin Gębala <maarcin.gebala@gmail.com>
2015-07-09 13:49:39 +0200

    Provide initial value for datepicker

--- saleor/dashboard/product/forms.py
+++ saleor/dashboard/product/forms.py
@@ -51,6 +51,9 @@ class ProductForm(forms.ModelForm):
             'data-placeholder'] = pgettext_lazy('Product form labels', 'Search')
         self.fields['attributes'].widget.attrs[
             'data-placeholder'] = pgettext_lazy('Product form labels', 'Search')
+        if self.instance.available_on:
+            self.fields['available_on'].widget.attrs[
+                'datavalue'] = self.instance.available_on.strftime('%Y/%m/%d')
 
     def clean(self):
         data = super(ProductForm, self).clean()
