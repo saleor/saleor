44875553d5dee5d8b2c60661163390b3f7202d8f
Adam Bogdał <adam@bogdal.pl>
2015-07-22 15:36:58 +0200

    Change appearance of default product image

--- saleor/product/management/commands/populatedb.py
+++ saleor/product/management/commands/populatedb.py
@@ -1,17 +1,16 @@
-from os.path import exists, join
+from os.path import exists
 
 from django.core.management.base import BaseCommand, CommandError
-from django.core.management import call_command
 
 from utils.create_random_data import create_items, create_users
 
 
 class Command(BaseCommand):
     help = 'Populate database with test objects'
-    BASE_DIR = r'saleor/static/placeholders/'
+    placeholders_dir = r'saleor/static/placeholders/'
 
     def handle(self, *args, **options):
-        for msg in create_items(self.BASE_DIR, 10):
+        for msg in create_items(self.placeholders_dir, 10):
             self.stdout.write(msg)
         for msg in create_users(10):
             self.stdout.write(msg)
