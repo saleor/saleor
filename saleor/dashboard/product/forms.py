8f6dca1c5bfb4476f00d42b734d086073f71886f
Adam Bogdał <adam@bogdal.pl>
2015-07-23 12:22:27 +0200

    Improve readability of code

--- saleor/dashboard/product/forms.py
+++ saleor/dashboard/product/forms.py
@@ -20,8 +20,8 @@ class ProductClassForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(ProductClassForm, self).__init__(*args, **kwargs)
-        product_classes = list(PRODUCT_CLASSES.keys())
-        self.fields['product_cls'].initial = product_classes[0].__name__
+        product_class = next(iter((PRODUCT_CLASSES)))
+        self.fields['product_cls'].initial = product_class.__name__
 
 
 class StockForm(forms.ModelForm):
