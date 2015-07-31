b489bd41b7b109c30afe839fb4842df37a577d5f
Paweł Kowalewski <pawel@mirumee.com>
2015-07-31 13:46:36 +0200

    Limit product count on homepage

--- saleor/core/views.py
+++ saleor/core/views.py
@@ -3,7 +3,7 @@ from saleor.product.models import Product
 
 
 def home(request):
-    products = Product.objects.get_available_products()
+    products = Product.objects.get_available_products()[:12]
     products = products.prefetch_related('categories', 'images',
                                          'variants__stock')
     return TemplateResponse(
