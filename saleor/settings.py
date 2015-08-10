3daa9173a4f27958249f490fcad3f8cfb6a8ba9e
Adam Bogdał <adam@bogdal.pl>
2015-08-10 08:34:35 +0200

    Use literal_eval for the bool variable

--- saleor/settings.py
+++ saleor/settings.py
@@ -39,7 +39,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_USE_TLS = bool(os.environ.get('EMAIL_USE_TLS', False))
+EMAIL_USE_TLS = ast.literal_eval(os.environ.get('EMAIL_USE_TLS', False))
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 
