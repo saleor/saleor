5a5496e083c897d5ab60d5df709c37ecd114764f
Adam Bogdał <adam@bogdal.pl>
2015-10-14 00:25:33 +0200

    Use relative imports

--- saleor/dashboard/api.py
+++ saleor/dashboard/api.py
@@ -1,8 +1,8 @@
 from rest_framework import routers
 
-from customer.api import CustomerViewSet
-from order.api import OrderViewSet
-from saleor.dashboard.payments.api import PaymentViewSet
+from .customer.api import CustomerViewSet
+from .order.api import OrderViewSet
+from .payments.api import PaymentViewSet
 
 router = routers.DefaultRouter()
 router.register(r'customers', CustomerViewSet)
