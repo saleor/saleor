65235c2a2547c3ec4291f33cf1013f5c4be480f3
Artur Smęt <artur.smet@mirumee.com>
2015-10-19 13:20:52 +0200

    Create Wombat user on demand

--- saleor/wombat/authentication.py
+++ saleor/wombat/authentication.py
@@ -14,11 +14,7 @@ class WombatAuthentication(BaseAuthentication):
 
         if (store_id == settings.WOMBAT_STORE_ID and
                 access_token in settings.WOMBAT_ALLOWED_INTEGRATION_TOKENS):
-            try:
-                wombat = User.objects.get(email=wombat_username,
-                                          is_active=True)
-            except User.DoesNotExist:
-                return None
-            else:
+            wombat = User.objects.get_or_create(email=wombat_username)
+            if wombat.is_active:
                 return (wombat, None)
         return None
