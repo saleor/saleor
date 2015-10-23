ec661d944efb21012ca1274fcdad32eda814ef35
Artur Smęt <artur.smet@mirumee.com>
2015-10-23 16:18:39 +0200

    Add shipping method field in delivery group

---
+++ saleor/order/migrations/0006_deliverygroup_shipping_method.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('order', '0005_deliverygroup_last_updated'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='deliverygroup',
+            name='shipping_method',
+            field=models.CharField(default='', max_length=255, db_index=True),
+        ),
+    ]
--- saleor/order/models.py
+++ saleor/order/models.py
@@ -183,6 +183,8 @@ class DeliveryGroup(models.Model, ItemSet):
         decimal_places=4,
         default=0,
         editable=False)
+    shipping_method = models.CharField(max_length=255, default='',
+                                       db_index=True)
     last_updated = models.DateTimeField(null=True, auto_now=True)
 
     objects = DeliveryGroupManager()
