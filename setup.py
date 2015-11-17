78cccc0372f5ed5c5beb6344f70ac485446f25c4
Patryk Zawadzki <patrys@room-303.com>
2015-11-17 11:36:48 +0100

    Avoid installing Django 1.8.6 as it's broken
    https://github.com/mirumee/saleor/issues/386

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     install_requires=[
         'Babel>=1.3,<1.4a0',
         'BabelDjango>=0.2,<0.3a0',
-        'Django>=1.8',
+        'Django>=1.8,!=1.8.6',
         'dj_database_url>=0.3.0',
         'django-emailit>=0.2.2',
         'django-materializecss-form>=0.1,<0.2a0',
