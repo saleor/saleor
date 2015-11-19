1f5b6deaac370e748446926d352d18ba1cf8c5d3
Patryk Zawadzki <patrys@room-303.com>
2015-11-19 13:28:54 +0100

    Clean up settings

--- saleor/settings.py
+++ saleor/settings.py
@@ -86,7 +86,7 @@ TEMPLATES = [{
         'debug': DEBUG,
         'context_processors': context_processors,
         'loaders': loaders,
-        'string_if_invalid': '<< MISSING VARIABLE >>'}}]
+        'string_if_invalid': '<< MISSING VARIABLE "%s" >>' if DEBUG else ''}}]
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
@@ -118,7 +118,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.webdesign',
 
     # Local apps
     'saleor.userprofile',
