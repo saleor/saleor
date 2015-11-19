98807dd05472fec28e1b496c1d3ce34ac1af1949
Adam Bogdał <adam@bogdal.pl>
2015-11-19 17:06:38 +0100

    Prepare static files inside the image file

--- compose/entrypoint.sh
+++ compose/entrypoint.sh
@@ -2,17 +2,15 @@
 set -e
 
 USER=saleor
+APP_UID=`stat -c "%u" /app`
 
 export DATABASE_URL=postgres://$DB_ENV_DB_USER:$DB_ENV_DB_PASS@db/$DB_ENV_DB_NAME
 
-if [ $(cat /etc/passwd | grep $USER | wc -l) == "0" ]; then
-  useradd -u `stat -c "%u" /app` -m $USER
+if [ $APP_UID != "0" ]; then
+    if [ $APP_UID != $(id -u $USER) ]; then
+        usermod -u $APP_UID $USER
+    fi
+    su $USER -c "PATH=$PATH:/app/node_modules/.bin $*"
 fi
 
-chown $USER.$USER /node -R
-if [[ ( $1 == "/app/compose/start.sh" ) && ( ! -d "/app/node_modules" ) ]]; then
-  echo Copying /node/node_modules to /app/node_modules..
-  su saleor -c "cp -r /node/node_modules /app"
-fi
-
-su saleor -c "PATH=$PATH:/app/node_modules/.bin $*"
+exec "$@"
