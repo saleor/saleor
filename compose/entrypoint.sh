c4759ccddacf9632f3e2cfa4278182907869f544
Adam Bogdał <adam@bogdal.pl>
2015-11-19 17:17:33 +0100

    Add redis container

--- compose/entrypoint.sh
+++ compose/entrypoint.sh
@@ -5,6 +5,7 @@ USER=saleor
 APP_UID=`stat -c "%u" /app`
 
 export DATABASE_URL=postgres://$DB_ENV_DB_USER:$DB_ENV_DB_PASS@db/$DB_ENV_DB_NAME
+export REDIS_URL=redis://redis:6379/0
 
 if [ $APP_UID != "0" ]; then
     if [ $APP_UID != $(id -u $USER) ]; then
