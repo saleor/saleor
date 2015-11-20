119c7bcf1b2a4f27bb28336ca348d0f636a0da9d
Adam Bogdał <adam@bogdal.pl>
2015-11-20 11:37:25 +0100

    Improve script syntax

--- compose/entrypoint.sh
+++ compose/entrypoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 USER=saleor
-APP_UID=`stat -c "%u" /app`
+APP_UID=$(stat -c "%u" /app)
 
 export DATABASE_URL=postgres://$DB_ENV_DB_USER:$DB_ENV_DB_PASS@db/$DB_ENV_DB_NAME
 export REDIS_URL=redis://redis:6379/0
 
-if [ $APP_UID != "0" ]; then
-    if [ $APP_UID != $(id -u $USER) ]; then
-        usermod -u $APP_UID $USER
+if [ "$APP_UID" != "0" ]; then
+    if [ "$APP_UID" != "$(id -u $USER)" ]; then
+        usermod -u "$APP_UID" $USER
     fi
     su $USER -c "PATH=$PATH:/app/node_modules/.bin $*"
 fi
