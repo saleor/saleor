756f72c3897586a21616a43f245ed118d1f62517
Paweł Burniak <pawelb@mirumee.com>
2015-11-23 17:32:50 +0100

    Change test - initial commit

--- saleor/checkout/test_checkout.py
+++ saleor/checkout/test_checkout.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import AnonymousUser
 from mock import MagicMock
+import pytest
 
 from .core import STORAGE_SESSION_KEY
-from ..userprofile.models import Address
+from ..userprofile.models import Address, User
 from .steps import ShippingAddressStep, ShippingMethodStep, SummaryStep
+
 
 NEW_ADDRESS = {
     'first_name': 'Test',
@@ -17,19 +19,94 @@ NEW_ADDRESS = {
     'country_area': '',
     'company_name': 'Test'}
 
+SHIPPING_PREFIX = 'shipping-address'
+SUMMARY_PREFIX = 'summary'
+USER_EMAIL = 'user@example.com'
 
-def test_shipping_step_save_without_address(rf):
-    data = dict(NEW_ADDRESS, method='dummy_shipping')
+
+class TestUser(User):
+
+    class QuerysetMock(list):
+        model = MagicMock()
+
+        def get(self, id):
+            pytest.set_trace()
+            print "get ********()", id
+            print "get ********()", id
+            print "get ********()", id
+            for address in self._addresses:
+                if address.id == id:
+                    print "tutaj"
+                    return address
+
+    _addresses = QuerysetMock()
+
+    @property
+    def addresses(self):
+        addresses_mock = MagicMock()
+        addresses_mock.all = MagicMock()
+        addresses_mock.all.return_value = self._addresses
+        return addresses_mock
+
+    @addresses.setter
+    def addresses(self, addresses_list):
+        for address in addresses_list:
+            self._addresses.append(address)
+
+
+def get_address(address, prefix=''):
+    if prefix:
+        prefix = '%s-' % (prefix,)
+
+    return {prefix + k: v for k, v in address.items()}
+
+
+def test_shipping_step_save_address_anonymous_user(rf):
+    address = get_address(NEW_ADDRESS, SHIPPING_PREFIX)
+    data = dict(address, email=USER_EMAIL)
     request = rf.post('/checkout/', data)
     request.user = AnonymousUser()
     request.session = {STORAGE_SESSION_KEY: {}}
-    group = MagicMock()
-    group.address = None
-    storage = {'address': NEW_ADDRESS.copy()}
-    step = ShippingAddressStep(request, storage, group)
+    storage = {}
+    step = ShippingAddressStep(request, storage, checkout=MagicMock())
     assert step.forms_are_valid()
     step.save()
     assert isinstance(storage['address'], dict)
+    assert storage['email'] == USER_EMAIL
+
+
+def test_shipping_step_save_address_authenticated_user(rf):
+    address = get_address(NEW_ADDRESS, SHIPPING_PREFIX)
+    user = TestUser()
+    user.addresses = []
+    request = rf.post('/checkout/', address)
+    request.user = user
+    request.session = {STORAGE_SESSION_KEY: {}}
+    storage = {}
+    step = ShippingAddressStep(request, storage, checkout=MagicMock())
+    assert step.forms_are_valid()
+    step.save()
+    assert isinstance(storage['address'], dict)
+
+
+def test_shipping_step_choose_address(rf):
+    # address_dict = get_address(NEW_ADDRESS, SHIPPING_PREFIX)
+    user = TestUser()
+    address = Address(**NEW_ADDRESS)
+    address.id = 42
+    user.addresses = [address]
+    request = rf.post('/checkout/', {'shipping-address-address': '42'})
+    request.user = user
+    request.session = {STORAGE_SESSION_KEY: {}}
+    storage = {}
+    step = ShippingAddressStep(request, storage, checkout=MagicMock())
+    print repr(step.forms['addresses_form'].is_valid())
+    print repr(step.forms['addresses_form'])
+    print repr(step.forms['addresses_form'].errors)
+    assert step.forms_are_valid()
+    step.save()
+    assert isinstance(storage['address'. dict])
+    assert storage['address_id'] == 42
 
 
 def test_shipping_step_save_with_address_in_group(rf):
@@ -37,10 +114,10 @@ def test_shipping_step_save_with_address_in_group(rf):
     request = rf.post('/checkout/', data)
     request.user = AnonymousUser()
     request.session = {}
-    group = MagicMock()
-    group.address = NEW_ADDRESS.copy()
+    # group = MagicMock()
+    # group.address = NEW_ADDRESS.copy()
     storage = {'address': NEW_ADDRESS.copy()}
-    step = ShippingAddressStep(request, storage, group)
+    step = ShippingAddressStep(request, storage)
     assert step.forms_are_valid()
     step.save()
     assert storage['address'] == NEW_ADDRESS
@@ -51,14 +128,14 @@ def test_shipping_step_save_with_address_in_checkout(rf):
     request = rf.post('/checkout/', data)
     request.user = AnonymousUser()
     request.session = {}
-    group = MagicMock()
-    group.address = None
+    # group = MagicMock()
+    # group.address = None
     storage = {
         'address': {
             'first_name': 'Change',
             'last_name': 'Me',
             'id': 10}}
-    step = ShippingAddressStep(request, storage, group)
+    step = ShippingAddressStep(request, storage)
     assert step.forms_are_valid()
     step.save()
     assert storage['address'] == NEW_ADDRESS
@@ -91,8 +168,8 @@ def test_shipping_step_save_with_address_other_than_billing(rf):
             'first_name': 'Billing Address',
             'last_name': 'Test',
             'id': 10}}
-    billing_address = Address(**address_data)
-    step = ShippingAddressStep(request, storage, group, billing_address)
+    # billing_address = Address(**address_data)
+    step = ShippingAddressStep(request, storage)
     assert step.forms_are_valid()
     step.save()
     assert storage['address'] == address_data
