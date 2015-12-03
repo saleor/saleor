567b22a3a47e30635fda5fdb40483662296119b6
Patryk Zawadzki <patrys@room-303.com>
2015-12-03 17:57:22 +0100

    Add react address form

--- saleor/static/js_src/storefront.jsx
+++ saleor/static/js_src/storefront.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import $ from 'jquery';
+import {AddressForm} from './components/addressform';
 import {CartItemAmount, CartItemSubtotal, CartTotal} from './components/cart';
 import CartStore from './stores/cart-store';
 require('jquery.cookie');
@@ -87,6 +88,50 @@ $(function() {
     let $img = $(this).find('.modal-body img');
     let dataSrc = $img.attr('data-src');
     $img.attr('src', dataSrc);
+  });
+});
+
+$(function () {
+  let $address = $('.i18n-address');
+  let addressUrl = $address.data('address-url');
+  $.ajax({
+    url: addressUrl,
+    dataType: 'json',
+    cache: false,
+    success: function (data) {
+      let countries = [
+        'AC', 'AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AO', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AW', 'AX', 'AZ',
+        'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BL', 'BM', 'BN', 'BO', 'BQ', 'BR', 'BS', 'BT', 'BV', 'BW', 'BY', 'BZ',
+        'CA', 'CC', 'CD', 'CF', 'CG', 'CH', 'CI', 'CK', 'CL', 'CM', 'CN', 'CO', 'CR', 'CV', 'CW', 'CX', 'CY', 'CZ',
+        'DE', 'DJ', 'DK', 'DM', 'DO', 'DZ',
+        'EC', 'EE', 'EG', 'EH', 'ER', 'ES', 'ET',
+        'FI', 'FJ', 'FK', 'FM', 'FO', 'FR',
+        'GA', 'GB', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GM', 'GN', 'GP', 'GQ', 'GR', 'GS', 'GT', 'GU', 'GW', 'GY',
+        'HK', 'HM', 'HN', 'HR', 'HT', 'HU',
+        'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IR', 'IS',
+        'IT', 'JE', 'JM', 'JO', 'JP', 'KE',
+        'KG', 'KH', 'KI', 'KM', 'KN', 'KR', 'KW', 'KY', 'KZ',
+        'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY',
+        'MA', 'MC', 'MD', 'ME', 'MF', 'MG', 'MH', 'MK', 'ML', 'MM', 'MN', 'MO', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MU', 'MV', 'MW', 'MX', 'MY', 'MZ',
+        'NA', 'NC', 'NE', 'NF', 'NG', 'NI', 'NL', 'NO', 'NP', 'NR', 'NU', 'NZ',
+        'OM',
+        'PA', 'PE', 'PF', 'PG', 'PH', 'PK', 'PL', 'PM', 'PN', 'PR', 'PS', 'PT', 'PW', 'PY',
+        'QA',
+        'RE', 'RO', 'RS', 'RU', 'RW', 'SA',
+        'SB', 'SC', 'SE', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS', 'ST', 'SV', 'SX', 'SZ',
+        'TA', 'TC', 'TD', 'TF', 'TG', 'TH', 'TJ', 'TK', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TW', 'TZ',
+        'UA', 'UG', 'UM', 'US', 'UY', 'UZ',
+        'VA', 'VC', 'VE', 'VG', 'VI', 'VN', 'VU',
+        'WF', 'WS',
+        'XK',
+        'YE', 'YT',
+        'ZA', 'ZM', 'ZW'
+      ];
+      render(<AddressForm lang="it" countries={countries} country="CN" data={data} />, $address[0]);
+    }.bind(this),
+    error: function (xhr, status, err) {
+      console.error(status, err.toString());
+    }.bind(this)
   });
 });
 
