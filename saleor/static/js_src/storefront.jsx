29b320185fda8e3f165bf43205ed64e2a7b40ff4
Patryk Zawadzki <patrys@room-303.com>
2015-12-04 12:27:19 +0100

    Do not hardode the list of countries

--- saleor/static/js_src/storefront.jsx
+++ saleor/static/js_src/storefront.jsx
@@ -102,42 +102,16 @@ $(function () {
     let message = {type: messageType, [messageField]: value};
     AddressStore.dispatch(message);
   });
+  let $countryField = $address.find(`[name=${prefixName('country', prefix)}]`);
+  let countries = $countryField.find('option').map((option, item) => item.value).get();
   $.ajax({
     url: addressUrl,
     dataType: 'json',
     cache: false,
     success: (data) => {
-      let countries = [
-        'AC', 'AD', 'AE', 'AF', 'AG', 'AI', 'AL', 'AM', 'AO', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AW', 'AX', 'AZ',
-        'BA', 'BB', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BL', 'BM', 'BN', 'BO', 'BQ', 'BR', 'BS', 'BT', 'BV', 'BW', 'BY', 'BZ',
-        'CA', 'CC', 'CD', 'CF', 'CG', 'CH', 'CI', 'CK', 'CL', 'CM', 'CN', 'CO', 'CR', 'CV', 'CW', 'CX', 'CY', 'CZ',
-        'DE', 'DJ', 'DK', 'DM', 'DO', 'DZ',
-        'EC', 'EE', 'EG', 'EH', 'ER', 'ES', 'ET',
-        'FI', 'FJ', 'FK', 'FM', 'FO', 'FR',
-        'GA', 'GB', 'GD', 'GE', 'GF', 'GG', 'GH', 'GI', 'GL', 'GM', 'GN', 'GP', 'GQ', 'GR', 'GS', 'GT', 'GU', 'GW', 'GY',
-        'HK', 'HM', 'HN', 'HR', 'HT', 'HU',
-        'ID', 'IE', 'IL', 'IM', 'IN', 'IO', 'IQ', 'IR', 'IS',
-        'IT', 'JE', 'JM', 'JO', 'JP', 'KE',
-        'KG', 'KH', 'KI', 'KM', 'KN', 'KR', 'KW', 'KY', 'KZ',
-        'LA', 'LB', 'LC', 'LI', 'LK', 'LR', 'LS', 'LT', 'LU', 'LV', 'LY',
-        'MA', 'MC', 'MD', 'ME', 'MF', 'MG', 'MH', 'MK', 'ML', 'MM', 'MN', 'MO', 'MP', 'MQ', 'MR', 'MS', 'MT', 'MU', 'MV', 'MW', 'MX', 'MY', 'MZ',
-        'NA', 'NC', 'NE', 'NF', 'NG', 'NI', 'NL', 'NO', 'NP', 'NR', 'NU', 'NZ',
-        'OM',
-        'PA', 'PE', 'PF', 'PG', 'PH', 'PK', 'PL', 'PM', 'PN', 'PR', 'PS', 'PT', 'PW', 'PY',
-        'QA',
-        'RE', 'RO', 'RS', 'RU', 'RW', 'SA',
-        'SB', 'SC', 'SE', 'SG', 'SH', 'SI', 'SJ', 'SK', 'SL', 'SM', 'SN', 'SO', 'SR', 'SS', 'ST', 'SV', 'SX', 'SZ',
-        'TA', 'TC', 'TD', 'TF', 'TG', 'TH', 'TJ', 'TK', 'TL', 'TM', 'TN', 'TO', 'TR', 'TT', 'TV', 'TW', 'TZ',
-        'UA', 'UG', 'UM', 'US', 'UY', 'UZ',
-        'VA', 'VC', 'VE', 'VG', 'VI', 'VN', 'VU',
-        'WF', 'WS',
-        'XK',
-        'YE', 'YT',
-        'ZA', 'ZM', 'ZW'
-      ];
       render(
         <Provider store={AddressStore}>
-          <AddressForm lang={lang} countries={countries} country="CN" data={data} prefix={prefix} />
+          <AddressForm lang={lang} countries={countries} data={data} prefix={prefix} />
         </Provider>,
         $address[0]
       );
