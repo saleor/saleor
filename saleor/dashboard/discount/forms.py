72caac2aa29881dfc1615615d8980d3f604c4d7e
Artur Smęt <artur.smet@mirumee.com>
2016-01-11 13:14:26 +0100

    Add discounts to the dashboard

---
+++ saleor/dashboard/discount/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from ...product.models import FixedProductDiscount
+
+
+class FixedProductDiscountForm(forms.ModelForm):
+    class Meta:
+        model = FixedProductDiscount
+        fields = ['name', 'products', 'discount']
