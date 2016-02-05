3b1b91fb56a52fbf3f1e2f1a3ab05a3c809698e6
Patryk Zawadzki <patrys@room-303.com>
2016-02-05 14:59:50 +0100

    Test both ways

--- saleor/userprofile/test_userprofile.py
+++ saleor/userprofile/test_userprofile.py
@@ -15,11 +15,21 @@ def test_address_form_for_country(country):
     required = i18naddress.validate_areas(country)[0]
     if 'street_address' in required:
         assert 'street_address_1' in errors
+    else:
+        assert 'street_address_1' not in errors
     if 'city' in required:
         assert 'city' in errors
+    else:
+        assert 'city' not in errors
     if 'city_area' in required:
         assert 'city_area' in errors
+    else:
+        assert 'city_area' not in errors
     if 'country_area' in required:
         assert 'country_area' in errors
+    else:
+        assert 'country_area' not in errors
     if 'postal_code' in required:
         assert 'postal_code' in errors
+    else:
+        assert 'postal_code' not in errors
