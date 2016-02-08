201d3d0dcb3727445166a0b870ec7d03eaa857f4
Artur Smęt <artur.smet@mirumee.com>
2016-02-08 20:51:39 +0100

    Add VariantImage model

--- saleor/product/models/__init__.py
+++ saleor/product/models/__init__.py
@@ -1,6 +1,6 @@
 from .base import (AttributeChoiceValue, Category, Product, ProductAttribute,
                    ProductVariant, Stock)
-from .images import ProductImage
+from .images import ProductImage, VariantImage
 
 __all__ = ['AttributeChoiceValue', 'Category', 'Product', 'ProductAttribute',
-           'ProductImage', 'ProductVariant', 'Stock']
+           'ProductImage', 'ProductVariant', 'Stock', 'VariantImage']
