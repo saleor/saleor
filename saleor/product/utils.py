dd43297740365b8d19a00fb7afb3b0a9777e829c
Artur Smęt <artur.smet@mirumee.com>
2016-02-08 18:08:10 +0100

    Don't iterate over all values trying to find one

--- saleor/product/utils.py
+++ saleor/product/utils.py
@@ -3,12 +3,10 @@ def get_attributes_display_map(variant, attributes):
     for attribute in attributes:
         value = variant.get_attribute(attribute.pk)
         if value:
-            choices = attribute.values.all()
-            if choices:
-                for choice in attribute.values.all():
-                    if choice.pk == value:
-                        display[attribute.pk] = choice
-                        break
+            choices = {a.pk: a for a in attribute.values.all()}
+            attr = choices.get(value)
+            if attr:
+                display[attribute.pk] = attr
             else:
                 display[attribute.pk] = value
     return display
