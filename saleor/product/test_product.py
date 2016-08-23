4db28430c0e2c050cd0e2b588df773e79aeec61d
Patryk Zawadzki <patrys@room-303.com>
2016-08-23 22:33:18 +0200

    Add test for #547

--- saleor/product/test_product.py
+++ saleor/product/test_product.py
@@ -20,8 +20,16 @@ def product_in_stock(db):
     return product
 
 
-@pytest.mark.django_db
-def test_stock_slector(product_in_stock):
+def test_stock_selector(product_in_stock):
     variant = product_in_stock.variants.get()
     preferred_stock = variant.select_stockrecord(5)
     assert preferred_stock.quantity_available >= 5
+
+
+def test_stock_allocator(product_in_stock):
+    variant = product_in_stock.variants.get()
+    stock = variant.select_stockrecord(5)
+    assert stock.quantity_allocated == 0
+    models.Stock.objects.allocate_stock(stock, 1)
+    stock = models.Stock.objects.get(pk=stock.pk)
+    assert stock.quantity_allocated == 1
