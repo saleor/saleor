9bb2f9df19faffe4a6cb2f2204b7725b8d9df34a
Patryk Zawadzki <patrys@room-303.com>
2016-10-14 23:42:41 +0200

    Add pylint config

--- saleor/settings.py
+++ saleor/settings.py
@@ -172,7 +172,7 @@ LOGGING = {
     'formatters': {
         'verbose': {
             'format': '%(levelname)s %(asctime)s %(module)s '
-            '%(process)d %(thread)d %(message)s'
+                      '%(process)d %(thread)d %(message)s'
         },
         'simple': {
             'format': '%(levelname)s %(message)s'
