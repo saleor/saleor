7a2528d73bad44451af730fccb9dd3c6d885b3e0
Artur Smęt <artur.smet@mirumee.com>
2016-10-25 16:05:24 +0200

    Improve address formatting

--- saleor/userprofile/templatetags/i18n_address_tags.py
+++ saleor/userprofile/templatetags/i18n_address_tags.py
@@ -9,7 +9,10 @@ register = template.Library()
 @register.simple_tag
 def format_address(address, latin=False):
     address_data = Address.objects.as_data(address)
+    address_data['name'] = '%(first_name)s %(last_name)s' % address_data
     address_data['country_code'] = address_data['country']
-    address_data['street_address'] = '%s\n%s' % (
-        address_data['street_address_1'], address_data['street_address_2'])
-    return mark_safe(i18naddress.format_address(address_data, latin))
+    address_data['street_address'] = ('%(street_address_1)s\n'
+                                     '%(street_address_2)s' % address_data)
+    formatted_address = i18naddress.format_address(address_data, latin)
+    formatted_address = formatted_address.replace('\n', '<br>')
+    return mark_safe(formatted_address)
