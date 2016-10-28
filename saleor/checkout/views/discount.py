96b62d7d6df06637e471014ff1bdb70b910d4ed0
Artur Smęt <artur.smet@mirumee.com>
2016-10-28 11:44:22 +0200

    Add voucher validation layer

--- saleor/checkout/views/discount.py
+++ saleor/checkout/views/discount.py
@@ -1,8 +1,11 @@
 from functools import wraps
 
+from django.contrib import messages
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
+from django.utils.translation import pgettext
 
+from ...discount.models import Voucher
 from ...discount.forms import CheckoutDiscountForm
 
 
@@ -32,6 +35,24 @@ def add_voucher_form(view):
             response.context_data['voucher_form'] = voucher_form
             response.context_data['voucher'] = voucher
         return response
+    return func
+
+
+def validate_voucher(view):
+    @wraps(view)
+    def func(request, checkout, cart):
+        if checkout.voucher_code:
+            try:
+                Voucher.objects.active().get(code=checkout.voucher_code)
+            except Voucher.DoesNotExist:
+                del checkout.voucher_code
+                checkout.recalculate_discount()
+                msg = pgettext(
+                    'checkout warning',
+                    'This voucher has expired. Please review your checkout.')
+                messages.warning(request, msg)
+                return redirect('checkout:summary')
+        return view(request, checkout, cart)
     return func
 
 
