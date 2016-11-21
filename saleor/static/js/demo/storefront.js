54adb5240ddcbaa54d356ba2c6e8b0b27bf3de45
weronka <v.terpilovska@gmail.com>
2016-11-21 15:26:02 +0100

    Add side navigation for mobile

--- saleor/static/js/demo/storefront.js
+++ saleor/static/js/demo/storefront.js
@@ -1,7 +1,25 @@
-/* @flow */
-
-import 'bootstrap-sass'
-import $ from 'jquery'
-import 'jquery.cookie'
-
 import '../../scss/demo/storefront.scss'
+
+// Mobile menu
+
+var $toogleIcon = $('.navbar__brand__menu-toggle');
+var $mobileNav = $('nav');
+
+if ($(window).width() < 768) {
+  $mobileNav.append('<ul class="nav navbar-nav navbar__menu__login"></ul>');
+  $('.navbar__login a').appendTo('.navbar__menu__login')
+                       .wrap( '<li class="nav-item login-item"></li>')
+                       .addClass('nav-link');
+}
+
+$toogleIcon.click((e) => {
+  $mobileNav.toggleClass('open');
+});
+
+$(document).click((e) => {
+  $mobileNav.removeClass('open');
+});
+
+$toogleIcon.click((e) => {
+   event.stopPropagation();
+});
