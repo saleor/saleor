6c3a9ff3f0f7623fae364a4a3ecd60f8d0584496
Artur Smęt <artur.smet@mirumee.com>
2016-11-24 16:37:49 +0100

    Use more verbose helper names

--- saleor/checkout/core.py
+++ saleor/checkout/core.py
@@ -13,7 +13,7 @@ from ..discount.models import Voucher, NotApplicable
 from ..order.models import Order
 from ..shipping.models import ShippingMethodCountry, ANY_COUNTRY
 from ..userprofile.models import Address, User
-from ..search.utils import update_order, update_user
+from ..search.utils import update_order_index, update_user_index
 
 STORAGE_SESSION_KEY = 'checkout_storage'
 
@@ -267,9 +267,9 @@ class Checkout(object):
 
         if voucher is not None:
             Voucher.objects.increase_usage(voucher)
-        update_order(order)
+        update_order_index(order)
         if order.user:
-            update_user(order.user)
+            update_user_index(order.user)
         return order
 
     def _get_voucher(self, vouchers=None):
--- saleor/search/utils.py
+++ saleor/search/utils.py
@@ -1,13 +1,13 @@
 from . import search_indexes
 
 
-def update_product(instance):
+def update_product_index(instance):
     search_indexes.ProductIndex().update_object(instance)
 
 
-def update_order(instance):
+def update_order_index(instance):
     search_indexes.OrderIndex().update_object(instance)
 
 
-def update_user(instance):
+def update_user_index(instance):
     search_indexes.UserIndex().update_object(instance)
