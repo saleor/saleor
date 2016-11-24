da1854fb1204cafdb20e08d1544c4136e3bff567
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2016-11-24 18:55:54 +0100

    Add category formatter test

--- tests/test_integrations.py
+++ tests/test_integrations.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from saleor.product.models import Category
 from saleor.integrations.feeds import SaleorFeed
 
 
@@ -11,3 +12,14 @@ def test_saleor_feed_items(product_in_stock):
     attributes = feed.item_attributes(items[0])
     assert attributes.get('mpn') == valid_variant.sku
     assert attributes.get('availability') == 'in stock'
+
+
+def test_category_formatter(db):
+    feed = SaleorFeed()
+    main_category = Category(name='Main', slug='main')
+    main_category.save()
+    sub_category = Category(name='Sub', slug='sub', parent=main_category)
+    sub_category.save()
+    assert feed.get_full_category_name_path(main_category) == 'Main'
+    assert feed.get_full_category_name_path(sub_category) == 'Main > Sub'
+
