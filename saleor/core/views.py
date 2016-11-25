2f26629a47dd6d3fcd6520b638b25fa8ecd7736a
weronka <v.terpilovska@gmail.com>
2016-11-25 16:13:05 +0100

    Add featured products to the home page

--- saleor/core/views.py
+++ saleor/core/views.py
@@ -4,9 +4,9 @@ from ..product.utils import products_with_availability, products_with_details
 
 
 def home(request):
-    products = products_with_details(request.user)[:12]
-    products = products_with_availability(
-        products, discounts=request.discounts, local_currency=request.currency)
+    products = Product.objects.get_available_products()[:6]
+    products = products.prefetch_related('categories', 'images',
+                                         'variants__stock')
     return TemplateResponse(
         request, 'home.html',
         {'products': products, 'parent': None})
