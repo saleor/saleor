7419c514116266360330899c71fa7c03ed60f1e1
Marcin Gębala <maarcin.gebala@gmail.com>
2016-11-30 15:20:25 +0100

    Use more meaningful names in attributes helper

--- saleor/product/models/utils.py
+++ saleor/product/models/utils.py
@@ -1,12 +1,13 @@
 def get_attributes_display_map(variant, attributes):
-    display = {}
+    display_map = {}
     for attribute in attributes:
-        value = variant.get_attribute(attribute.pk)
-        if value:
+        value_pk = variant.get_attribute(attribute.pk)
+        if value_pk:
+            value_pk = int(value_pk)
             choices = {a.pk: a for a in attribute.values.all()}
-            attr = choices.get(value)
-            if attr:
-                display[attribute.pk] = attr
+            choice_obj = choices.get(value_pk)
+            if choice_obj:
+                display_map[attribute.pk] = choice_obj
             else:
-                display[attribute.pk] = value
-    return display
+                display_map[attribute.pk] = value
+    return display_map
