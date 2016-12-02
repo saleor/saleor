2d55cf766baeb6c9f3ad0c1925b049464680cf7e
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2016-12-02 13:42:44 +0100

    Fix compressed feeds in python3

--- saleor/integrations/utils.py
+++ saleor/integrations/utils.py
@@ -1,3 +1,5 @@
+from __future__ import unicode_literals
+
 import gzip
 import csv
 
@@ -5,14 +7,17 @@ from django.core.files.storage import default_storage
 
 
 def update_feed(feed):
-    with default_storage.open(feed.file_path, 'w') as output_file:
+    with default_storage.open(feed.file_path, 'wb') as output_file:
         if feed.compression:
-            output = gzip.GzipFile(fileobj=output_file)
+            try:
+                output = gzip.open(output_file, 'wt')
+            except TypeError:
+                output = gzip.GzipFile(fileobj=output_file, mode='w')
         else:
             output = output_file
 
-        writer = csv.DictWriter(output,feed.attributes,
-                                delimiter=str("\t"))
+        writer = csv.DictWriter(output, feed.attributes,
+                                dialect=csv.excel_tab)
         writer.writeheader()
         for item in feed.items():
             writer.writerow(feed.item_attributes(item))
