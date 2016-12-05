d23845e45f0c829707f93d9112bfd295ff4730d5
Artur Smęt <artur.smet@mirumee.com>
2016-12-05 16:19:30 +0100

    Update search index after successful order placement

--- saleor/checkout/views/summary.py
+++ saleor/checkout/views/summary.py
@@ -5,6 +5,7 @@ from django.template.response import TemplateResponse
 from ..forms import (
     AnonymousUserBillingForm, BillingAddressesForm,
     BillingWithoutShippingAddressForm)
+from ...search.utils import update_order_index, update_user_index
 from ...userprofile.forms import get_address_form
 from ...userprofile.models import Address
 
@@ -17,6 +18,9 @@ def create_order(checkout):
     checkout.cart.clear()
     order.create_history_entry()
     order.send_confirmation_email()
+    update_order_index(order)
+    if order.user:
+        update_user_index(order.user)
     return order, redirect('order:payment', token=order.token)
 
 
