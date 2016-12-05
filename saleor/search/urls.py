e12ecdc8e7704934382dcede8048ccd5e6d1ddce
Artur Smęt <artur.smet@mirumee.com>
2016-12-05 16:20:04 +0100

    Improve code style

--- saleor/search/urls.py
+++ saleor/search/urls.py
@@ -1,8 +1,10 @@
+from __future__ import unicode_literals
+
 from django.conf.urls import url
 
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.search, name='search'),
+    url(r'^$', views.search, name='search')
 ]
