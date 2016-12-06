5dae79d8b880660dc4029147397ced7a8352249b
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2016-12-06 19:21:29 +0100

    Added docs

--- saleor/data_feeds/urls.py
+++ saleor/data_feeds/urls.py
@@ -4,5 +4,5 @@ from django.views.generic.base import RedirectView
 from .google_merchant import FILE_URL
 
 urlpatterns = [
-    url(r'saleor-feed/$',
-        RedirectView.as_view(url=FILE_URL), name='saleor-feed')]
+    url(r'google/$',
+        RedirectView.as_view(url=FILE_URL), name='google-feed')]
