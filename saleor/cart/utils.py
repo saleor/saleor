72f060f0463d205f803dad93b51a4c34610644d6
Mira Szewczyk <szewczyk.mira@gmail.com>
2016-12-13 14:25:56 +0100

    Remove redundant code

--- saleor/cart/utils.py
+++ saleor/cart/utils.py
@@ -4,8 +4,6 @@ from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 from satchless.item import InsufficientStock
 
-from ..product.forms import get_form_class_for_product
-
 
 def contains_unavailable_variants(cart):
     try:
@@ -53,12 +51,3 @@ def check_product_availability_and_warn(request, cart):
                 'Quantity was set to maximum available for now.')
         messages.warning(request, msg)
         remove_unavailable_variants(cart)
-
-
-def process_cart_form(request, cart, product):
-    form_class = get_form_class_for_product(product)
-    form = form_class(cart=cart, product=product,
-                      data=request.POST or None, discounts=request.discounts)
-    if form.is_valid():
-        form.save()
-    return form
