6db7b6a294cf5cdb09c274686a266afee27c9c1f
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2016-12-19 09:15:26 +0100

    Refactor cart decorators

--- saleor/cart/decorators.py
+++ saleor/cart/decorators.py
@@ -7,23 +7,23 @@ from .core import set_cart_cookie
 from .models import Cart
 
 
-def find_and_assign_cart(request):
+def find_and_assign_cart(request, queryset=Cart.objects.all()):
     """Assign cart from cookie to request user
     :type request: django.http.HttpRequest
     """
-    cookie = request.get_signed_cookie(Cart.COOKIE_NAME, default=None)
-    if not cookie:
+    token = request.get_signed_cookie(Cart.COOKIE_NAME, default=None)
+    if not token:
         return
-    cart = Cart.objects.open().filter(token=cookie, user=None).first()
+    cart = get_anonymous_cart_from_token(token=token, cart_queryset=queryset)
     if cart is None:
         return
     cart.change_user(request.user)
     carts_to_close = Cart.objects.open().filter(user=request.user)
-    carts_to_close = carts_to_close.exclude(token=cookie)
+    carts_to_close = carts_to_close.exclude(token=token)
     carts_to_close.update(status=Cart.CANCELED, last_status_change=now())
 
 
-def get_or_create_anonymous_cart_from_token(cart_queryset, token):
+def get_or_create_anonymous_cart_from_token(token, cart_queryset=Cart.objects.all()):
     """Returns open anonymous cart with given token or creates new.
     :type cart_queryset: django.db.models.manager.ManagerFromCartQueryset
     :type token: string
@@ -32,7 +32,7 @@ def get_or_create_anonymous_cart_from_token(cart_queryset, token):
     return cart_queryset.open().filter(token=token).get_or_create(user=None)[0]
 
 
-def get_or_create_user_cart(cart_queryset, user):
+def get_or_create_user_cart(user, cart_queryset=Cart.objects.all()):
     """Returns open cart for given user or creates one.
     :type cart_queryset: django.db.models.manager.ManagerFromCartQueryset
     :type user: User
@@ -41,15 +41,15 @@ def get_or_create_user_cart(cart_queryset, user):
     return cart_queryset.open().get_or_create(user=user)[0]
 
 
-def get_anonymous_cart_from_token(cart_queryset, token):
+def get_anonymous_cart_from_token(token, cart_queryset=Cart.objects.all()):
     """Returns open anonymous cart with given token or None if not found.
     :rtype: saleor.cart.models.Cart | None
     """
-    return cart_queryset.open().filter(token=token).filter(user=None).first()
+    return cart_queryset.open().filter(token=token, user=None).first()
 
 
-def get_user_cart(cart_queryset, user):
-    """Returns open cart for with given token or None if not found.
+def get_user_cart(user, cart_queryset=Cart.objects.all()):
+    """Returns open cart for given user or None if not found.
     :type cart_queryset: django.db.models.manager.ManagerFromCartQueryset
     :type user: User
     :rtype: saleor.cart.models.Cart | None
@@ -57,36 +57,37 @@ def get_user_cart(cart_queryset, user):
     return cart_queryset.open().filter(user=user).first()
 
 
-def get_or_create_cart(cart_queryset, request):
+def get_or_create_cart_from_request(request, cart_queryset=Cart.objects.all()):
     """Get cart from database or create new Cart if not found
     :type cart_queryset: django.db.models.manager.ManagerFromCartQueryset
     :type request: django.http.HttpRequest
     :rtype: saleor.cart.models.Cart
     """
     if request.user.is_authenticated():
-        return get_or_create_user_cart(cart_queryset, request.user)
+        return get_or_create_user_cart(request.user, cart_queryset)
     else:
         token = request.get_signed_cookie(Cart.COOKIE_NAME,
                                                  default=None)
-        return get_or_create_anonymous_cart_from_token(cart_queryset,
-                                                       token)
+        return get_or_create_anonymous_cart_from_token(token, cart_queryset)
 
 
-def get_cart(cart_queryset, request):
+def get_cart_from_request(request, cart_queryset=Cart.objects.all()):
     """Get cart from database or return unsaved Cart
     :type cart_queryset: django.db.models.manager.ManagerFromCartQueryset
     :type request: django.http.HttpRequest
     :rtype: saleor.cart.models.Cart
     """
     if request.user.is_authenticated():
-        cart = get_user_cart(cart_queryset, request.user)
+        cart = get_user_cart(request.user, cart_queryset)
+        user = request.user
     else:
         token = request.get_signed_cookie(Cart.COOKIE_NAME, default=None)
-        cart = get_anonymous_cart_from_token(cart_queryset, token)
+        cart = get_anonymous_cart_from_token(token, cart_queryset)
+        user = None
     if cart is not None:
         return cart
     else:
-        return Cart()
+        return Cart(user=user)
 
 
 def get_or_create_db_cart(cart_queryset=Cart.objects.all()):
@@ -96,7 +97,7 @@ def get_or_create_db_cart(cart_queryset=Cart.objects.all()):
     def get_cart(view):
         @wraps(view)
         def func(request, *args, **kwargs):
-            cart = get_or_create_cart(cart_queryset, request)
+            cart = get_or_create_cart_from_request(request, cart_queryset)
             response = view(request, cart, *args, **kwargs)
             if not request.user.is_authenticated():
                 set_cart_cookie(cart, response)
@@ -113,7 +114,7 @@ def get_or_empty_db_cart(cart_queryset=Cart.objects.all()):
     def get_cart(view):
         @wraps(view)
         def func(request, *args, **kwargs):
-            cart = get_cart(cart_queryset, request)
+            cart = get_cart_from_request(request, cart_queryset)
             return view(request, cart, *args, **kwargs)
         return func
     return get_cart
