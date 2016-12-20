d3ce7fad4cc2491f8a07fd7e212f07a7fcd47c80
Mira Szewczyk <szewczyk.mira@gmail.com>
2016-12-20 16:01:20 +0100

    Fetch values instead of object to json

--- saleor/shipping/utils.py
+++ saleor/shipping/utils.py
@@ -5,4 +5,5 @@ from .models import ShippingMethodCountry
 def get_shipment_options(country_code):
     return ShippingMethodCountry.objects.select_related(
         'shipping_method').filter(
-        Q(country_code=country_code) | Q(country_code='')).order_by('price')
+        Q(country_code=country_code) | Q(country_code='')).order_by(
+        'price').values('price', 'shipping_method__name')
