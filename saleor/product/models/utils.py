861da6561f10dc2a3acfde50083f6b3faef7145a
Marcin Gębala <maarcin.gebala@gmail.com>
2016-12-20 16:02:31 +0100

    Use smart_text

--- saleor/product/models/utils.py
+++ saleor/product/models/utils.py
@@ -4,7 +4,7 @@ from django.utils.encoding import smart_text
 def get_attributes_display_map(variant, attributes):
     display = {}
     for attribute in attributes:
-        value = variant.attributes.get(str(attribute.pk))
+        value = variant.attributes.get(smart_text(attribute.pk))
         if value:
             choices = {smart_text(a.pk): a for a in attribute.values.all()}
             attr = choices.get(value)
