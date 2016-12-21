ad87cf107368788a015139673164d430e74b71cb
lampyjon <git@jonathan-lawrence.co.uk>
2016-12-21 19:29:06 +0000

    Fix failing attribute lookups
    Lookups were via pk, but inserts were smart_text().

--- saleor/product/models/utils.py
+++ saleor/product/models/utils.py
@@ -1,9 +1,12 @@
+from django.utils.encoding import smart_text
+
 def get_attributes_display_map(variant, attributes):
+    print "in get_attributes_display_map with " + str(variant) + " and " + str(attributes)
     display = {}
     for attribute in attributes:
         value = variant.get_attribute(attribute.pk)
         if value:
-            choices = {a.pk: a for a in attribute.values.all()}
+            choices = {smart_text(a.pk): a for a in attribute.values.all()}
             attr = choices.get(value)
             if attr:
                 display[attribute.pk] = attr
