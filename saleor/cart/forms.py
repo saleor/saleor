346d1d90fcc317963c35e34fde90a26ac66a9b53
Mira Szewczyk <szewczyk.mira@gmail.com>
2016-12-21 13:37:42 +0100

    Sort displayed list of countries

--- saleor/cart/forms.py
+++ saleor/cart/forms.py
@@ -118,7 +118,8 @@ class ReplaceCartLineForm(AddToCartForm):
 
 class CountryForm(forms.Form):
 
-    country = forms.ChoiceField(choices=COUNTRY_CODE_CHOICES, label='')
+    country = forms.ChoiceField(
+        choices=sorted(COUNTRY_CODE_CHOICES, key=lambda t: t[1]), label='')
 
     def get_shipment_options(self):
         code = self.cleaned_data['country']
