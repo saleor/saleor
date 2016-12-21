2860e4a36dba8f9e3d0d10fd7774369371e1ead2
Krzysztof Białogłowicz <krzysztof@mirumee.com>
2016-12-21 13:31:35 +0100

    Fix sorting styles

--- saleor/static/js/components/SortBy.js
+++ saleor/static/js/components/SortBy.js
@@ -26,40 +26,58 @@ export default class sortBy extends Component {
   }
 
   render() {
-    const { sortedValue } = this.props;
+    const { sortedValue }  = this.props
     const { visibility } = this.state;
     return (
       <div className="sort-by">
         <button className="btn btn-link" onClick={this.changeVisibility}>
-          <span>Sort by: <strong>{sortedValue}</strong></span>
+          {sortedValue ? (
+            sortedValue.search('-') ? (
+              <div>
+                <span>Sort by: <strong>{sortedValue}</strong></span>
+                <i className="fa fa-arrow-up" aria-hidden="true"></i>
+              </div>
+            ) : (
+               <div>
+                <span>Sort by: <strong>{sortedValue.replace('-','')}</strong></span>
+                <i className="fa fa-arrow-down" aria-hidden="true"></i>
+              </div>
+            )
+          ) : (
+            <span>Sort by: <strong>default</strong></span>
+          )}
         </button>
         {visibility ? (
         <ul className="sort-list">
           <li className="name">
             <div className="row">
-              <div className="col-md-6">Name:</div>
+              <div className="col-md-6">Sort by <strong>Name</strong></div>
               <div className="col-md-6">
-                <span className="name" onClick={this.setSorting}>ascending</span>
+                  <span className="name" onClick={this.setSorting}>ascending</span>
+                  <i className="fa fa-arrow-up pull-right" aria-hidden="true"></i>
               </div>
             </div>
             <div className="row">
               <div className="col-md-6"></div>
               <div className="col-md-6">
-                <span className="-name" onClick={this.setSorting}>descending</span>
+                  <span className="-name" onClick={this.setSorting}>descending</span>
+                  <i className="fa fa-arrow-down pull-right" aria-hidden="true"></i>
               </div>
             </div>
           </li>
           <li className="price">
             <div className="row">
-              <div className="col-md-6">Price:</div>
+              <div className="col-md-6">Sort by <strong>Price</strong></div>
               <div className="col-md-6">
-                <span className="price" onClick={this.setSorting}>ascending</span>
+                  <span className="price" onClick={this.setSorting}>ascending</span>
+                  <i className="fa fa-arrow-up pull-right" aria-hidden="true"></i>
               </div>
             </div>
             <div className="row">
               <div className="col-md-6"></div>
               <div className="col-md-6">
-                <span className="-price" onClick={this.setSorting}>descending</span>
+                  <span className="-price" onClick={this.setSorting}>descending</span>
+                  <i className="fa fa-arrow-down pull-right" aria-hidden="true"></i>
               </div>
             </div>    
           </li>
