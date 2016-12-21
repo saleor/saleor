d81a26b8fa0c69948c2aee4deb3d9ff309b4cfd8
Mira Szewczyk <szewczyk.mira@gmail.com>
2016-12-21 12:41:50 +0100

    Change logic of filtering shipping methods

--- saleor/shipping/utils.py
+++ saleor/shipping/utils.py
@@ -3,7 +3,9 @@ from .models import ShippingMethodCountry
 
 
 def get_shipment_options(country_code):
-    return ShippingMethodCountry.objects.select_related(
-        'shipping_method').filter(
-        Q(country_code=country_code) | Q(country_code='')).order_by(
-        'price').values('price', 'shipping_method__name')
+    shipping_methods_qs = ShippingMethodCountry.objects.select_related(
+        'shipping_method').values('price', 'shipping_method__name')
+    shipping_methods = shipping_methods_qs.filter(country_code=country_code)
+    if not shipping_methods.exists():
+        shipping_methods = shipping_methods_qs.filter(country_code='')
+    return shipping_methods
