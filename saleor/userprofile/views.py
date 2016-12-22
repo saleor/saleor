a03a00f3ffbc9816539feb4913fbd6ae426e76d6
Artur Smęt <artur.smet@mirumee.com>
2016-12-22 11:22:37 +0100

    Add password change form to user profile page

--- saleor/userprofile/views.py
+++ saleor/userprofile/views.py
@@ -1,20 +1,36 @@
+from allauth.account.adapter import get_adapter
+from allauth.account.forms import ChangePasswordForm
+from allauth.account.utils import logout_on_password_change
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, Http404
-from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
-from django.views.decorators.http import require_POST
 from django.utils.translation import ugettext as _
-
-from .forms import get_address_form
 
 
 @login_required
 def details(request):
+    password_form = get_or_process_password_form(request)
     ctx = {'addresses': request.user.addresses.all(),
-           'orders': request.user.orders.prefetch_related('groups__items')}
+           'orders': request.user.orders.prefetch_related('groups__items'),
+           'change_password_form': password_form}
+
     return TemplateResponse(request, 'userprofile/details.html', ctx)
+
+
+def get_or_process_password_form(request):
+    form = ChangePasswordForm(data=request.POST or None, user=request.user)
+    if form.is_valid():
+        form.save()
+        logout_on_password_change(request, form.user)
+        get_adapter(request).add_message(
+            request,
+            messages.SUCCESS,
+            'account/messages/password_changed.txt')
+    return form
+
 
 @login_required
 def address_delete(request, pk):
