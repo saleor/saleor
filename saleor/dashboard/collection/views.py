e19b6d212130122f0510cca913ac8a83a87d2b38
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-09 10:24:32 +0100

    Add collection view

--- saleor/dashboard/collection/views.py
+++ saleor/dashboard/collection/views.py
@@ -1,8 +1,12 @@
 from django.template.response import TemplateResponse
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.utils.translation import ugettext_lazy as _
 
 from ...product.models import Collection
 from ...core.utils import get_paginator_items
 from ..views import staff_member_required
+from .forms import CollectionForm
 
 
 @staff_member_required
@@ -12,3 +16,15 @@ def collection_list(request):
     ctx = {'colections': collections}
     return TemplateResponse(request,
                             'dashboard/collection/list.html', ctx)
+
+
+@staff_member_required
+def collection_create(request):
+    collection = Collection()
+    form = CollectionForm(request.POST or None)
+    if form.is_valid():
+        collection = form.save()
+        messages.success(request, _('Added collection %s') % collection)
+        return redirect('dashboard:collection-list')
+    ctx = {'collection': collection, 'form': form}
+    return TemplateResponse(request, 'dashboard/collection/detail.html', ctx)
