8ace79782e5c0787028a7c1ad7e2c132d44b3fbe
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-09 11:06:44 +0100

    Display and edit collection

--- saleor/dashboard/collection/urls.py
+++ saleor/dashboard/collection/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.collection_list, name='collection-list'),
     url(r'^add$', views.collection_create, name='collection-add'),
+    url(r'^(?P<collection_pk>[0-9]+)/edit/$',
+        views.collection_update, name='collection-update'),
 ]
--- saleor/dashboard/collection/views.py
+++ saleor/dashboard/collection/views.py
@@ -1,6 +1,6 @@
 from django.template.response import TemplateResponse
 from django.contrib import messages
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.utils.translation import ugettext_lazy as _
 
 from ...product.models import Collection
@@ -26,5 +26,16 @@ def collection_create(request):
         collection = form.save()
         messages.success(request, _('Added collection %s') % collection)
         return redirect('dashboard:collection-list')
+    ctx = {'collection': collection, 'form': form}
+    return TemplateResponse(request, 'dashboard/collection/detail.html', ctx)
+
+
+@staff_member_required
+def collection_update(request, collection_pk=None):
+    collection = get_object_or_404(Collection, pk=collection_pk)
+    form = CollectionForm(request.POST or None, instance=collection)
+    if form.is_valid():
+        collection = form.save()
+        messages.success(request, _('Updated collection %s') % collection)
     ctx = {'collection': collection, 'form': form}
     return TemplateResponse(request, 'dashboard/collection/detail.html', ctx)
