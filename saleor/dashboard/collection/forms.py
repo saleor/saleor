552b4a8802c308183adc2dc21bc4a7a5b6d5cd42
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-10 10:54:47 +0100

    Update CollectionForm to handle slug field

--- saleor/dashboard/collection/forms.py
+++ saleor/dashboard/collection/forms.py
@@ -1,8 +1,17 @@
+from unidecode import unidecode
+
 from django import forms
+from django.utils.text import slugify
+
 from ...product.models import Collection
 
 
 class CollectionForm(forms.ModelForm):
     class Meta:
         model = Collection
-        exclude = []
+        exclude = ['slug']
+
+    def save(self, commit=True):
+        self.instance.slug = slugify(unidecode(self.instance.name))
+        super(CollectionForm, self).save(commit=commit)
+        return self.instance
