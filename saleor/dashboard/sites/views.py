0648fe7165a414d0d411bd96018f252e3739d021
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-13 10:42:00 +0100

    Add staff_member_required decorator to sites dashboard views

--- saleor/dashboard/sites/views.py
+++ saleor/dashboard/sites/views.py
@@ -4,15 +4,18 @@ from django.template.response import TemplateResponse
 from django.utils.translation import ugettext_lazy as _
 
 from ...site.models import Setting
+from ..views import staff_member_required
 from .forms import SettingForm
 
 
+@staff_member_required
 def index(request):
     settings = Setting.objects.all()
     ctx = {'settings': settings}
     return TemplateResponse(request, 'dashboard/sites/index.html', ctx)
 
 
+@staff_member_required
 def create(request):
     form = SettingForm(request.POST or None)
     if form.is_valid():
