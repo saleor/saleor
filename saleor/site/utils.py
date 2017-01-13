99f2c47c4a3e18b6c0773afc2550354d86cc0bd4
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-01-13 16:41:16 +0100

    Get site settings and attach to request

---
+++ saleor/site/utils.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+
+from .models import SiteSetting
+
+
+def get_site_settings(request):
+    settings_id = getattr(settings, 'SITE_SETTINGS_ID', None)
+    try:
+        site_settings = SiteSetting.objects.get(id=settings_id)
+    except SiteSetting.DoesNotExist:
+        site_settings = None
+    request.site_settings = site_settings
+    return site_settings
