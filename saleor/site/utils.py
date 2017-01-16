9ee155bf8ed58e38be78a24dc35713c9beab6081
Mira <szewczyk.mira@gmail.com>
2017-01-16 14:21:33 +0100

    Rewrite get_site_settings_function

--- saleor/site/utils.py
+++ saleor/site/utils.py
@@ -3,13 +3,16 @@ from django.conf import settings
 from .models import SiteSetting
 
 
-def get_site_settings_and_add_to_request(request):
-    settings_id = getattr(settings, 'SITE_SETTINGS_ID', None)
-    try:
-        site_settings = SiteSetting.objects.get(id=settings_id)
-    except SiteSetting.DoesNotExist:
-        site_settings = None
-    request.site_settings = site_settings
+def get_site_settings(request):
+    if hasattr(request, 'site_settings'):
+        site_settings = request.site_settings
+    else:
+        site_settings_id = getattr(settings, 'SITE_SETTINGS_ID', None)
+        try:
+            site_settings = get_site_settings_uncached(site_settings_id)
+        except SiteSetting.DoesNotExist:
+            site_settings = None
+        request.site_settings = site_settings
     return site_settings
 
 
