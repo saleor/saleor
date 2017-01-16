7845aa9b892a8a65f8847b66211c81a04cb4d990
Mira <szewczyk.mira@gmail.com>
2017-01-16 09:49:24 +0100

    Test for creating and displaying site settings

---
+++ tests/test_site_settings.py
@@ -0,0 +1,56 @@
+import pytest
+
+from django.core.urlresolvers import reverse
+from django.utils.encoding import smart_text
+
+from saleor.site.models import SiteSetting
+from saleor.site import utils
+from saleor.dashboard.sites.forms import SiteSettingForm
+from .utils import get_redirect_location
+
+
+@pytest.fixture
+def site_settings(db):
+    return SiteSetting.objects.create(name="mirumee.com", domain="mirumee.com")
+
+
+def test_get_site_settings_uncached(site_settings):
+    result = utils.get_site_settings_uncached(site_settings.id)
+    assert result == site_settings
+
+
+def test_index_view(admin_client, site_settings):
+    response = admin_client.get(reverse('dashboard:site-index'))
+    assert response.status_code == 200
+
+    context = response.context
+    assert list(context['sites']) == [site_settings]
+
+
+@pytest.mark.django_db
+def test_form():
+    data = {'name': 'mirumee', 'domain': 'mirumee.com'}
+    form = SiteSettingForm(data)
+    assert form.is_valid()
+
+    site = form.save()
+    assert smart_text(site) == 'mirumee'
+
+    form = SiteSettingForm({})
+    assert not form.is_valid()
+
+
+def test_create_view(admin_client):
+    url = reverse('dashboard:site-create')
+    response = admin_client.get(url)
+    assert response.status_code == 200
+
+    site_count = SiteSetting.objects.count()
+    data = {'name': 'mirumee', 'domain': 'mirumee.com'}
+    response_post = admin_client.post(url, data)
+    assert response_post.status_code == 302
+
+    redirect_location = get_redirect_location(response_post)
+    assert redirect_location == reverse('dashboard:site-index')
+
+    assert SiteSetting.objects.count() == site_count + 1
