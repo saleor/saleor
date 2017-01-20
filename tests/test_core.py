41c6a71e2a9e013966df06e3b5f458aa9a902bc8
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2017-01-20 11:48:56 +0100

    Add populatedb admin creation test

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,7 +2,8 @@ import pytest
 from mock import Mock
 
 from saleor.core.utils import (
-    Country, get_country_by_ip, get_currency_for_country)
+    Country, get_country_by_ip, get_currency_for_country, create_superuser)
+from saleor.userprofile.models import User
 
 
 @pytest.mark.parametrize('ip_data, expected_country', [
@@ -26,3 +27,22 @@ def test_get_country_by_ip(ip_data, expected_country, monkeypatch):
 def test_get_currency_for_country(country, expected_currency, monkeypatch):
     currency = get_currency_for_country(country)
     assert currency == expected_currency
+
+
+def test_create_superuser(db, client):
+    credentials = {'email': 'admin@example.com', 'password': 'admin'}
+    # Test admin creation
+    assert User.objects.all().count() == 0
+    create_superuser(credentials)
+    assert User.objects.all().count() == 1
+    admin = User.objects.all().first()
+    assert admin.is_superuser
+    # Test duplicating
+    create_superuser(credentials)
+    assert User.objects.all().count() == 1
+    # Test logging in
+    response = client.post('/account/login/',
+                           {'login': credentials['email'],
+                            'password': credentials['password']},
+                           follow=True)
+    assert response.context['request'].user == admin
