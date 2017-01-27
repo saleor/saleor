e04acb4062dcda1ed9fc001abc17b3c25d5754a3
Artur Smęt <artur.smet@mirumee.com>
2017-01-27 13:04:58 +0100

    Remove list and delete feature from site settings

--- saleor/dashboard/sites/urls.py
+++ saleor/dashboard/sites/urls.py
@@ -4,7 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='site-index'),
-    url(r'^add/$', views.create, name='site-create'),
     url(r'^(?P<site_id>[0-9]+)/edit/$', views.update, name='site-update'),
-    url(r'^(?P<site_id>[0-9]+)/delete/$', views.delete, name='site-delete'),
 ]
--- saleor/dashboard/sites/views.py
+++ saleor/dashboard/sites/views.py
@@ -1,54 +1,26 @@
 from django.contrib import messages
-from django.core.urlresolvers import reverse
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.utils.translation import ugettext_lazy as _
 
-from ...site.models import SiteSetting
-from ..views import staff_member_required
 from .forms import SiteSettingForm
+from ..views import staff_member_required
+from ...site.models import SiteSettings
+from ...site.utils import get_site_settings
 
 
 @staff_member_required
 def index(request):
-    sites = SiteSetting.objects.all()
-    ctx = {'sites': sites}
-    return TemplateResponse(request, 'dashboard/sites/index.html', ctx)
-
-
-@staff_member_required
-def create(request):
-    form = SiteSettingForm(request.POST or None)
-    if form.is_valid():
-        site = form.save()
-        messages.success(request, _('Added site %s') % site)
-        return redirect('dashboard:site-index')
-    ctx = {'form': form}
-    return TemplateResponse(request, 'dashboard/sites/detail.html', ctx)
+    settings = get_site_settings(request)
+    return redirect('dashboard:site-update', site_id=settings.pk)
 
 
 @staff_member_required
 def update(request, site_id=None):
-    site = get_object_or_404(SiteSetting, pk=site_id)
+    site = get_object_or_404(SiteSettings, pk=site_id)
     form = SiteSettingForm(request.POST or None, instance=site)
     if form.is_valid():
         site = form.save()
         messages.success(request, _('Updated site %s') % site)
     ctx = {'site': site, 'form': form}
     return TemplateResponse(request, 'dashboard/sites/detail.html', ctx)
-
-
-@staff_member_required
-def delete(request, site_id=None):
-    site = get_object_or_404(SiteSetting, pk=site_id)
-    if request.method == 'POST':
-        site.delete()
-        messages.success(request, _('Delete site %s') % site)
-        if request.is_ajax():
-            response = {'redirectUrl': reverse(
-                'dashboard:site-index')}
-            return JsonResponse(response)
-        return redirect('dashboard:site-index')
-    ctx = {'site': site}
-    return TemplateResponse(request, 'dashboard/sites/delete_modal.html', ctx)
