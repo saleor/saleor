6ae1faad36d327a60ba2595f57c8b301a73639cb
Marcin Gębala <maarcin.gebala@gmail.com>
2017-02-01 12:10:20 +0100

    Fix urls categories in homepage

--- saleor/core/views.py
+++ saleor/core/views.py
@@ -1,12 +1,15 @@
 from django.template.response import TemplateResponse
 
-from ..product.utils import products_with_availability, products_for_homepage
+from ..product.utils import (categories_for_homepage,
+                             products_with_availability, products_for_homepage)
 
 
 def home(request):
     products = products_for_homepage()[:8]
     products = products_with_availability(
         products, discounts=request.discounts, local_currency=request.currency)
+    featured_categories = categories_for_homepage(3)
     return TemplateResponse(
         request, 'home.html',
-        {'products': products, 'parent': None})
+        {'products': products, 'parent': None,
+         'featured_categories': featured_categories})
