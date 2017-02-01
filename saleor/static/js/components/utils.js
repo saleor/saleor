3ce0db29636323a7af74764528e3d3f1d25994f1
Krzysztof Białogłowicz <krzysztof@mirumee.com>
2017-02-01 15:09:44 +0100

    Fix commented issue

--- saleor/static/js/components/utils.js
+++ saleor/static/js/components/utils.js
@@ -4,17 +4,9 @@ export const mdBreakpoint = 992;
 export const lgBreakpoint = 1200;
 
 export const isMobile = () => {
-  	if(window.innerWidth < smBreakpoint) {
-  		return true;
-  	} else {
-  		return false;
-  	}
+  	return window.innerWidth < smBreakpoint;
 }
 
 export const isTablet = () => {
-  	if(window.innerWidth >= smBreakpoint && window.innerWidth <= mdBreakpoint) {
-  		return true;
-  	} else {
-  		return false;
-  	}
+  	return window.innerWidth >= smBreakpoint && window.innerWidth <= mdBreakpoint;
 }
