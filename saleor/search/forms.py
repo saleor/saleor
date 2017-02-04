442b083e9d1618569aa96a653ed2c0e4dfc27e59
Marco Badan <info@marcobadan.com>
2017-02-04 15:01:05 +0100

    Add contextual marker for search app

--- saleor/search/forms.py
+++ saleor/search/forms.py
@@ -1,10 +1,11 @@
 from django import forms
+from django.utils.translation import pgettext
 
 from .backends import get_search_backend
 
 
 class SearchForm(forms.Form):
-    q = forms.CharField(label='Query', required=True)
+    q = forms.CharField(label=pgettext('Search form label', 'Query'), required=True)
 
     def search(self, model_or_queryset):
         backend = get_search_backend('default')
