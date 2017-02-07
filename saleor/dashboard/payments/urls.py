8da0fa5bdcfacc96875d15080fdfe7613c9feec7
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-02-07 11:58:08 +0100

    Convert payment list in dashboard to function based views

--- saleor/dashboard/payments/urls.py
+++ saleor/dashboard/payments/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.PaymentList.as_view(), name='payments'),
+    url(r'^$', views.payment_list, name='payments'),
     url(r'^(?P<pk>[0-9]+)/$', views.payment_details, name='payment-details')
 ]
--- saleor/dashboard/payments/views.py
+++ saleor/dashboard/payments/views.py
@@ -1,27 +1,36 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
-from django.views.generic import ListView
-from payments.models import PAYMENT_STATUS_CHOICES
 
-from ..views import (FilterByStatusMixin, StaffMemberOnlyMixin,
-                     staff_member_required)
+from ..views import staff_member_required
 from ...order.models import Payment
 from ..order.forms import PaymentFilterForm
 
 
-class PaymentList(StaffMemberOnlyMixin, FilterByStatusMixin, ListView):
-    model = Payment
-    template_name = 'dashboard/payments/list.html'
-    context_object_name = 'payments'
-    paginate_by = 30
-    form_class = PaymentFilterForm
-    status_choices = PAYMENT_STATUS_CHOICES
-    status_order = ['waiting', 'input', 'preauth', 'confirmed', 'refunded',
-                    'rejected', 'error']
+@staff_member_required
+def payment_list(request):
+    payments = Payment.objects.select_related('order').all().order_by(
+        '-created')
 
-    def get_queryset(self):
-        qs = super(PaymentList, self).get_queryset()
-        return qs.order_by('-created').select_related('order')
+    active_status = request.GET.get('status')
+    if active_status:
+        payments = payments.filter(status=active_status)
+
+    paginator = Paginator(payments, 30)
+    page_number = request.GET.get('page')
+    try:
+        page = paginator.page(page_number)
+    except PageNotAnInteger:
+        page = paginator.page(1)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+
+    form = PaymentFilterForm(request.GET or None)
+
+    ctx = {'payments': page.object_list, 'paginator': paginator,
+           'page_obj': page, 'is_paginated': page.has_other_pages(),
+           'form': form}
+    return TemplateResponse(request, 'dashboard/payments/list.html', ctx)
 
 
 @staff_member_required
