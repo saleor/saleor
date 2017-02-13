734ee002b4aee78fbb54b4255fed0cc09ec876c7
Mira Szewczyk <szewczyk.mira@gmail.com>
2017-02-13 10:38:41 +0100

    Remove collection from context processors

--- saleor/settings.py
+++ saleor/settings.py
@@ -100,7 +100,6 @@ context_processors = [
     'saleor.core.context_processors.default_currency',
     'saleor.core.context_processors.categories',
     'saleor.cart.context_processors.cart_counter',
-    'saleor.core.context_processors.collections',
     'saleor.core.context_processors.search_enabled',
     'saleor.site.context_processors.site',
     'social_django.context_processors.backends',
