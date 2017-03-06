9627bbb832490c0f360b1b72a284fff9b3bc673d
Till Kolter <till.kolter@gmail.com>
2017-03-06 03:41:18 +0100

    Add dbrouting for Oye legacy models

---
+++ saleor/dbrouters.py
@@ -0,0 +1,41 @@
+__author__ = 'tkolter'
+
+
+class OyeRouter(object):
+    """
+    A router to control all database operations on models in the
+    auth application.
+    """
+
+    def db_for_read(self, model, **hints):
+        """
+        Attempts to read auth models go to auth_db.
+        """
+        if model._meta.app_label == 'saleor_oye':
+            return 'oye_legacy'
+        return None
+
+    def db_for_write(self, model, **hints):
+        """
+        Attempts to write auth models go to auth_db.
+        """
+        if model._meta.app_label == 'saleor_oye':
+            return 'oye_legacy'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Allow relations if a model in the auth app is involved.
+        """
+        if obj1._meta.app_label == 'saleor_oye' or obj2._meta.app_label == 'saleor_oye':
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        """
+        Make sure the auth app only appears in the 'auth_db'
+        database.
+        """
+        if app_label == 'saleor_oye':
+            return db == 'oye_legacy'
+        return None
