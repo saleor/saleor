c6757f3f36343cc35ea4faf696574a6aab9241d1
weronka <v.terpilovska@gmail.com>
2017-03-14 16:16:07 +0100

    Replace country_form select with html markup

--- saleor/core/views.py
+++ saleor/core/views.py
@@ -1,9 +1,5 @@
 from django.template.response import TemplateResponse
-
-from ..core.utils import get_user_shipping_country
 from ..product.utils import products_with_availability, products_for_homepage
-from ..cart.forms import CountryForm
-
 
 def home(request):
     products = products_for_homepage()[:8]
@@ -14,8 +10,5 @@ def home(request):
         {'products': products, 'parent': None})
 
 def styleguide(request):
-    default_country = get_user_shipping_country(request)
-    country_form = CountryForm(initial={'country': default_country})
     return TemplateResponse(
-        request, 'styleguide.html',
-        {'country_form': country_form})
+        request, 'styleguide.html')
