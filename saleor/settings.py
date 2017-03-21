a9f2f43eb76aae80aa9a5a648332400b920940f5
weronka <v.terpilovska@gmail.com>
2017-03-21 17:33:32 +0100

    Fix styles

--- saleor/settings.py
+++ saleor/settings.py
@@ -283,7 +283,7 @@ BOOTSTRAP3 = {
 
 TEST_RUNNER = ''
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split()
+ALLOWED_HOSTS = ['127.0.0.1']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
