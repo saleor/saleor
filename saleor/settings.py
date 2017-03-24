ea3e6bbc4d7fe7ba854f9711eb0300d8548c5660
Michal Kalinowski <michal.kalinowski@amadeus.com>
2017-03-24 10:57:13 +0100

    exposes aws s3 region

--- saleor/settings.py
+++ saleor/settings.py
@@ -294,6 +294,7 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_MEDIA_BUCKET_NAME = os.environ.get('AWS_MEDIA_BUCKET_NAME')
 AWS_QUERYSTRING_AUTH = ast.literal_eval(
     os.environ.get('AWS_QUERYSTRING_AUTH', 'False'))
+AWS_DEFAULT_REGION = os.environ.get('AWS_DEFAULT_REGION')
 
 if AWS_STORAGE_BUCKET_NAME:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
