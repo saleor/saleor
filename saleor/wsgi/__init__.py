a2ca39fb6aa8ae7410d18f526159552f74cdb215
Till Kolter <till.kolter@gmail.com>
2017-04-04 17:27:33 +0200

    load .env file in wsgi.__init__

--- saleor/wsgi/__init__.py
+++ saleor/wsgi/__init__.py
@@ -14,7 +14,34 @@ framework.
 
 """
 import os
+import re
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saleor.settings')
+
+def read_env():
+    """Pulled from Honcho code with minor updates, reads local default
+    environment variables from a .env file located in the project root
+    directory.
+    """
+    try:
+        with open('../../.env') as f:
+            content = f.read()
+    except IOError:
+        content = ''
+
+    for line in content.splitlines():
+        m1 = re.match(r'\A([A-Za-z_0-9]+)=(.*)\Z', line)
+        if m1:
+            key, val = m1.group(1), m1.group(2)
+            m2 = re.match(r"\A'(.*)'\Z", val)
+            if m2:
+                val = m2.group(1)
+            m3 = re.match(r'\A"(.*)"\Z', val)
+            if m3:
+                val = re.sub(r'\\(.)', r'\1', m3.group(1))
+            os.environ.setdefault(key, val)
+
+
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
