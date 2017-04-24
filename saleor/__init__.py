e12fdcfce54dac2766bb993fd62ceb25824fea9b
Till Kolter <till.kolter@gmail.com>
2017-04-24 02:02:38 +0200

    enable celery

--- saleor/__init__.py
+++ saleor/__init__.py
@@ -1,1 +1,10 @@
+from __future__ import absolute_import, unicode_literals
+
 __version__ = 'dev'
+
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
+
+__all__ = ['celery_app']
