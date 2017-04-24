3d3b7c1291842e3ca92658bccfc32ed77f9aa0b8
Till Kolter <till.kolter@gmail.com>
2017-04-24 03:54:19 +0200

    set CELERY namespace

--- saleor/celery.py
+++ saleor/celery.py
@@ -13,7 +13,7 @@ app = Celery('saleor')
 
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
