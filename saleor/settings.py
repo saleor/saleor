a9bc917ee0cc38a76fba303aa6ec75a5ae3f76db
Till Kolter <till.kolter@gmail.com>
2017-06-10 15:49:15 +0500

    commented fallback authentications

--- saleor/settings.py
+++ saleor/settings.py
@@ -416,8 +416,8 @@ SITE_SETTINGS_ID = 1
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 25,
