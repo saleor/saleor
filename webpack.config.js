8da097a529de0de537850df61c3563713b200eb7
thodnev <thodnev@mail.ru>
2017-07-13 07:17:04 +0300

    Changed assets root to relative in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
   output = {
     path: resolve('saleor/static/assets/'),
     filename: '[name].[chunkhash].js',
-    publicPath: 'https://saleor-demo.s3.amazonaws.com/assets/'
+    publicPath: '/static/assets/'
   };
   fileLoaderPath = 'file?name=[name].[hash].[ext]';
   extractTextPlugin = new ExtractTextPlugin('[name].[contenthash].css');
