54fa25d41d400d5b1fc838738877068f79597aca
dominik-zeglen <flesz3@o2.pl>
2017-09-27 16:07:35 +0200

    Switch from relay to apollo WIP

---
+++ saleor/static/js/components/categoryPage/ProductList.js
@@ -0,0 +1,71 @@
+import React, { Component, PropTypes } from 'react';
+import {gql} from 'react-apollo';
+
+import ProductItem from './ProductItem';
+import NoResults from './NoResults';
+
+class ProductList extends Component {
+
+  static propTypes = {
+    onLoadMore: PropTypes.func.isRequired,
+    products: PropTypes.object,
+    setSorting: PropTypes.object,
+    updating: PropTypes.object
+  };
+
+  onLoadMore = () => this.props.onLoadMore();
+  setSorting = (event) => this.props.setSorting(event);
+
+  static fragments = {
+    products: gql`
+      fragment ProductListFragmentQuery on ProductTypeConnection {
+        edges {
+          node {
+            ...ProductFragmentQuery
+          }
+        }
+        pageInfo {
+          hasNextPage
+        }
+      }
+      ${ProductItem.fragments.product}
+    `
+  };
+
+  render() {
+    const { edges, pageInfo: { hasNextPage } } = this.props.products;
+    return (
+      <div className={this.props.updating && 'category-list--loading'}>
+        <div className="row">
+          {edges.length > 0 ? (edges.map((edge, i) => (
+            <ProductItem key={i} product={edge.node} />
+          ))) : (<NoResults />)}
+        </div>
+        <div className="load-more">
+          {hasNextPage && (
+            <button className="btn gray" onClick={this.onLoadMore}>{pgettext('Load more products on category view', 'Load more')}</button>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+// export default Relay.createContainer(ProductList, {
+//   fragments: {
+//     products: () => Relay.QL`
+//       fragment on ProductTypeConnection {
+//         edges {
+//           node {
+//             ${ProductItem.getFragment('product')}
+//           }
+//         }
+//         pageInfo {
+//           hasNextPage
+//         }
+//       }
+//     `
+//   }
+// });
+
+export default ProductList;
