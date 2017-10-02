cc8610b7145484f099ccc7aad719f661b6fc56bd
dominik-zeglen <flesz3@o2.pl>
2017-10-02 17:05:00 +0200

    Fix sorter

--- saleor/static/js/storefront.js
+++ saleor/static/js/storefront.js
@@ -119,6 +119,26 @@ $(function() {
   });
 });
 
+// Sorter dropdown
+
+$(document).ready((e) => {
+  $('.sort-by button').on('click', (e) => {
+    const t = $(e.currentTarget).parent();
+    const l = t.find('.sort-list');
+    if(l.hasClass('d-none')) {
+      l.removeClass('d-none');
+      t.find('.click-area').removeClass('d-none');
+    } else {
+      l.addClass('d-none');
+      t.find('.click-area').addClass('d-none');
+    }
+  });
+  $('.sort-by .click-area').on('click', (e) => {
+    $('.sort-by .sort-list').addClass('d-none');
+    $(e.currentTarget).addClass('d-none');
+  });
+});
+
 // Input Passwords
 
 let $inputPassword = $('input[type=password]');
