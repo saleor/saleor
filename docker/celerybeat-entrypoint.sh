813f45a3d887ba6c135b6d40dba0f61c0abbb417
Till Kolter <till.kolter@gmail.com>
2017-10-04 22:35:12 +0200

    Use Django scheduler for celerybeat

--- docker/celerybeat-entrypoint.sh
+++ docker/celerybeat-entrypoint.sh
@@ -3,4 +3,4 @@
 # Wait for rabbitmq to be available
 ./wait-for-it.sh 127.0.0.1:5672 --timeout=600 -- echo "RabbitMQ is up!"
 
-celery -A saleor beat --loglevel INFO
+celery -A saleor beat --loglevel DEBUG -S django
