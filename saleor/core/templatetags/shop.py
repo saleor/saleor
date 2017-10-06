0ba195f2efdacb674c8ff7affbea353abbe2a936
michal <michal.sipa@mirumee.com>
2017-10-06 14:22:50 +0200

    Further refactoring of sort_by and sort_menu
    Combine sort_by and sort_menu to one template and templatetag.
    Move templatetags to shop.py.
    Delete sort_menu.py and _sort_menu.html.
    Update index.html with new templatetags.

--- saleor/core/templatetags/shop.py
+++ saleor/core/templatetags/shop.py
@@ -7,6 +7,8 @@ except ImportError:
 from django.template import Library
 from django.utils.http import urlencode
 
+from ...product.filters import DEFAULT_SORT
+
 register = Library()
 
 
@@ -18,8 +20,23 @@ def slice(items, group_size=1):
 
 
 @register.simple_tag(takes_context=True)
-def get_sort_by_url(context, field):
+def get_sort_by_url(context, field, descending=False):
     request = context['request']
     request_get = request.GET.dict()
-    request_get['sort_by'] = field
+    if descending:
+        request_get['sort_by'] = '-' + field
+    else:
+        request_get['sort_by'] = field
     return '%s?%s' % (request.path, urlencode(request_get))
+
+
+@register.inclusion_tag('category/_sort_by.html', takes_context=True)
+def sort_by(context, attributes):
+    ctx = {
+        'request': context['request'],
+        'sort_by': (context['request'].GET.get('sort_by', DEFAULT_SORT)
+                    .strip('-')),
+        'sort_by_choices': attributes,
+        'arrow_down': (context['request'].GET.get('sort_by', DEFAULT_SORT)
+                       .startswith('-'))}
+    return ctx
