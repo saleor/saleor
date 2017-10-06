85f866918098e9f0c88af4163c3c3dfa536ec0be
michal <michal.sipa@mirumee.com>
2017-10-06 09:28:58 +0200

    Refactor sort_menu.py
    Refactor sort_menu.py.

--- saleor/product/filters.py
+++ saleor/product/filters.py
@@ -10,8 +10,6 @@ from django_prices.models import PriceField
 
 from .models import Product, ProductAttribute
 
-# SORT_BY_FIELDS = (('name', pgettext_lazy('Sort by filter', 'name')),
-#                   ('price', pgettext_lazy()))
 
 SORT_BY_FIELDS = [{'value': 'name',
                    'label': pgettext_lazy('Sort by filter', 'name')},
--- saleor/product/templatetags/sort_menu.py
+++ saleor/product/templatetags/sort_menu.py
@@ -5,14 +5,9 @@ register = template.Library()
 
 @register.inclusion_tag('category/_sort_menu.html', takes_context=True)
 def sort_menu(context, attributes):
-    ordering_options = []
-    for attr in [a['value'] for a in attributes]:
-        ordering_options.append(attr)
-        ordering_options.append('-' + str(attr))
-    choices = [a['label'] for a in attributes]
+    context['sort_by'] = \
+        context['request'].GET.get('sort_by', 'name').strip('-')
     context['sort_by_choices'] = attributes
-    get_sort_by = context['request'].GET.get('sort_by', 'name')
-    context['choice'] = choices[int(ordering_options.index(get_sort_by) / 2)]
-    context['arrow_up'] = \
-        (context['request'].GET.get('sort_by', 'name')[0] != '-')
+    context['arrow_down'] = \
+        (context['request'].GET.get('sort_by', 'name').startswith('-'))
     return context
