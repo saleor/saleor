1f8febd827ea1d6d80cfec77ae26481c530eb79b
Jakub Draganek <jakub.draganek@gmail.com>
2017-10-11 19:08:47 +0200

    Fill email context with proper data

--- saleor/order/emails.py
+++ saleor/order/emails.py
@@ -1,9 +1,14 @@
 from templated_email import send_templated_mail
+from ..site.utils import get_site_name
+from django.conf import settings
 
 
-CONFIRMATION_TEMPLATE = 'foo'
+CONFIRMATION_TEMPLATE = 'order/confirm_order'
 
 
-def send_confirmation(address):
-    send_templated_mail(from_email=address, recipient_list=[], context={},
+def send_confirmation(address, url):
+    send_templated_mail(from_email=settings.ORDER_FROM_EMAIL,
+                        recipient_list=[address],
+                        context={'site_name': get_site_name(),
+                                 'payment_url': url},
                         template_name=CONFIRMATION_TEMPLATE)
--- tests/test_emails.py
+++ tests/test_emails.py
@@ -1,17 +1,23 @@
 from __future__ import unicode_literals
+from django.conf import settings
 
 import saleor.order.emails as emails
 import pytest
 import mock
 
 
-EMAIL_DATA = {'from_email': 'foo@bar.com',
+EMAIL_DATA = {'from_email': settings.ORDER_FROM_EMAIL,
               'template_name': emails.CONFIRMATION_TEMPLATE}
 
 
+@mock.patch('saleor.order.emails.get_site_name', return_value='WOOBALOOBA')
 @mock.patch('saleor.order.emails.send_templated_mail')
-def test_send_confirmation_using_templated_email(mocked_templated_email):
-    emails.send_confirmation(EMAIL_DATA['from_email'])
-    mocked_templated_email.assert_called_once_with(recipient_list=[],
-                                                   context={},
-                                                   **EMAIL_DATA)
+def test_send_confirmation_using_templated_email(mocked_templated_email,
+                                                 mocked_get_site_name):
+    email_address = 'foo@bar.com'
+    url = 'wooba/looba'
+    emails.send_confirmation(email_address, url)
+    context = {'site_name': mocked_get_site_name(),
+               'payment_url': url}
+    mocked_templated_email.assert_called_once_with(
+        recipient_list=[email_address], context=context, **EMAIL_DATA)
