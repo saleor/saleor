c81b771a572c570d1dc0c7050d53d77d38fabf1d
dominik-zeglen <flesz3@o2.pl>
2017-10-11 17:23:27 +0200

    Make codeclimate happy

--- saleor/static/js/components/mobile/filters-menu.js
+++ saleor/static/js/components/mobile/filters-menu.js
@@ -1,10 +1,10 @@
 export default $(document).ready((e) => {
   $('.filters-menu').on('click', (e) => {
-    const menu_container = $('.filters-menu__body');
-    if (menu_container.hasClass('d-none')) {
-      menu_container.removeClass('d-none');
+    const menuContainer = $('.filters-menu__body');
+    if (menuContainer.hasClass('d-none')) {
+      menuContainer.removeClass('d-none');
     } else {
-      menu_container.addClass('d-none');
+      menuContainer.addClass('d-none');
     }
   });
 });
--- saleor/static/js/components/sorter.js
+++ saleor/static/js/components/sorter.js
@@ -1,14 +1,14 @@
 export default $(document).ready((e) => {
   $(document).ready((e) => {
     $('.sort-by button').on('click', (e) => {
-      const parent_container = $(e.currentTarget).parent();
-      const list = parent_container.find('.sort-list');
+      const parentContainer = $(e.currentTarget).parent();
+      const list = parentContainer.find('.sort-list');
       if (list.hasClass('d-none')) {
         list.removeClass('d-none');
-        parent_container.find('.click-area').removeClass('d-none');
+        parentContainer.find('.click-area').removeClass('d-none');
       } else {
         list.addClass('d-none');
-        parent_container.find('.click-area').addClass('d-none');
+        parentContainer.find('.click-area').addClass('d-none');
       }
     });
     $('.sort-by .click-area').on('click', (e) => {
