5223cd1fee02bcb5d54933f6f69286a681d9d8be
michal <michal.sipa@mirumee.com>
2017-10-13 15:26:30 +0200

    Add initial value to StaffForm
    Add initial value for field 'is_staff' to True. When we are creating new staff member we want to have it already checked.

--- saleor/dashboard/staff/forms.py
+++ saleor/dashboard/staff/forms.py
@@ -6,6 +6,7 @@ from ...userprofile.models import User
 class StaffForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.admin = kwargs.pop('admin', None)
+        kwargs.update(initial={'is_staff': True})
         super(StaffForm, self).__init__(*args, **kwargs)
         if self.admin == self.instance:
             self.fields['is_superuser'].widget.attrs['disabled'] = True
