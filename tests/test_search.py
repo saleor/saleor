22708cbaa094e1a21f04c1ec51b0ff1a988d781e
Jakub Draganek <jakub.draganek@gmail.com>
2017-10-16 17:24:23 +0200

    Record search query with no results

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,9 +1,18 @@
 from django.core.management import call_command
+from django.core.urlresolvers import reverse
 import pytest
 
 
 @pytest.mark.integration
 @pytest.mark.vcr(record_mode='once')
 def test_index_products(product_list):
-    call_command('update_index')
+    options = {'backend_name': 'default'}
+    call_command('update_index', **options)
 
+
+@pytest.mark.integration
+@pytest.mark.vcr(
+    record_mode='once', match_on=['url', 'method', 'body'])
+def test_search_with_empty_result(db, client):
+    WORD = 'foo'
+    client.get(reverse('search:search'), {'q': WORD})
