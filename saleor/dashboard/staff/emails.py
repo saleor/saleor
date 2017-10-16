b31193289e363b459fc97130517d6c63150ad891
michal <michal.sipa@mirumee.com>
2017-10-16 09:16:43 +0200

    Update email sending to use celery
    Change send_set_password_email in staff/emails to use Celery while sending emails.

--- saleor/dashboard/staff/emails.py
+++ saleor/dashboard/staff/emails.py
@@ -3,14 +3,17 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
 from templated_email import send_templated_mail
+from celery import shared_task
 
 from ...settings import DEFAULT_FROM_EMAIL, get_host
+from ...site.utils import get_site_name
 
 
+@shared_task
 def send_set_password_email(staff):
     ctx = {'protocol': 'http',
            'domain': get_host(),
-           'site_name': 'saleor',
+           'site_name': get_site_name(),
            'uid': urlsafe_base64_encode(force_bytes(staff.pk)),
            'token': default_token_generator.make_token(staff)}
     send_templated_mail(
