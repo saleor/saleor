c0309caeb490d598f916124b89e5d21e60d4d580
Adam Bogdał <adam@bogdal.pl>
2017-10-16 22:53:56 +0200

    Use domain from SiteSettings model

--- saleor/core/sitemaps.py
+++ saleor/core/sitemaps.py
@@ -1,9 +1,13 @@
 from django.contrib.sitemaps import Sitemap
 
 from ..product.models import Product
+from ..site.utils import get_domain
 
 
 class ProductSitemap(Sitemap):
+    def _urls(self, page, protocol, domain):
+        domain = get_domain()
+        return super(ProductSitemap, self)._urls(page, protocol, domain)
 
     def items(self):
         return Product.objects.only('id', 'name').order_by('-id')
