14d7abee483bc77f5f931c5863f0c255c948c1f4
Jakub Draganek <jakub.draganek@gmail.com>
2017-10-17 14:26:23 +0200

    Add new integration test for search backend
    - record new cassettes with external commumnication of search backend.
    - tune request matching to be deterministic
    - check both parsing of search response and ORM part

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,5 +1,7 @@
+from saleor.product.models import Product
 from django.core.management import call_command
 from django.core.urlresolvers import reverse
+from decimal import Decimal
 import pytest
 
 
@@ -10,9 +12,48 @@ def test_index_products(product_list):
     call_command('update_index', **options)
 
 
+MATCH_SEARCH_REQUEST = ['method', 'host', 'port', 'path', 'body']
+
+
 @pytest.mark.integration
-@pytest.mark.vcr(
-    record_mode='once', match_on=['url', 'method', 'body'])
+@pytest.mark.vcr(record_mode='once', match_on=MATCH_SEARCH_REQUEST)
 def test_search_with_empty_result(db, client):
     WORD = 'foo'
-    client.get(reverse('search:search'), {'q': WORD})
+    response = client.get(reverse('search:search'), {'q': WORD})
+    assert 0 == len(response.context['results'].object_list)
+    assert WORD == response.context['query']
+
+
+@pytest.fixture
+def indexed_products(product_class, default_category):
+    ''' Products to be found by search backend
+
+    We need existing objects with primary keys same as search service
+    returned in response. Otherwise search view won't find anything.
+    Purpose of this fixture is for integration with search service only!
+    pks must be in response in appropiate recorded cassette.
+    '''
+    first_product = Product.objects.create(
+        pk=59,
+        name='Test product',
+        price=Decimal(10.0),
+        product_class=product_class)
+    first_product.categories.add(default_category)
+    second_product = Product.objects.create(
+        pk=41,
+        name='Test product',
+        price=Decimal(10.0),
+        product_class=product_class)
+    second_product.categories.add(default_category)
+    return [first_product, second_product]
+
+
+@pytest.mark.integration
+@pytest.mark.vcr(record_mode='once', match_on=MATCH_SEARCH_REQUEST)
+def test_search_with_result(db, product_in_stock, indexed_products, client):
+    EXISTING_PHRASE = 'Group'
+    response = client.get(reverse('search:search'), {'q': EXISTING_PHRASE})
+    print(response.context['results'].object_list)
+    assert len(indexed_products) == len(
+        response.context['results'].object_list)
+    assert EXISTING_PHRASE == response.context['query']
