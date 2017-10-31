cf0f451c1a4073d41329c65d7b92c31b6c247f89
Jakub Draganek <jakub.draganek@gmail.com>
2017-10-31 13:34:49 +0100

    Make product search query more specific
    - Use storefront index, hardcoded for now
    - Refactor code

--- saleor/search/backends/test_newelastic.py
+++ saleor/search/backends/test_newelastic.py
@@ -10,20 +10,21 @@ def search_phrase():
 @pytest.fixture
 def es_search_query(search_phrase):
     query = {
-        '_source': ['pk'],
         'query': {
             'multi_match': {
-                'fields': ['name', 'description'],
-                'query': 'How fortunate man with none'
+                'query': 'How fortunate man with none',
+                'index': 'storefront__product_product',
+                'fields': ['name', 'description']
             }
-        }
+        },
+        '_source': ['pk']
     }
     return query
 
 
 def test_no_result_search(mocker, es_search_query, search_phrase):
     mocker.patch.object(newelastic.CLIENT, 'search', returnvalue=[])
-    products = newelastic.search(search_phrase)
+    products = newelastic.search_products(search_phrase)
     newelastic.CLIENT.search.assert_called_once_with(
         body=es_search_query, doc_type=[], index=None)
     assert not products
