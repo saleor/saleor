ece1082e939b1339e92fd7a93edbbeebb45900f1
dominik-zeglen <flesz3@o2.pl>
2017-11-08 10:56:18 +0100

    Fix new image upload widget display

--- saleor/dashboard/product/widgets.py
+++ saleor/dashboard/product/widgets.py
@@ -5,18 +5,6 @@ from versatileimagefield.widgets import (ClearableFileInputWithImagePreview,
 
 class ImagePreviewFileInput(ClearableFileInputWithImagePreview):
     template_name = 'dashboard/product/product_image/versatile_image.html'
-    template_with_initial_and_imagepreview = """
-    <div class="sizedimage-mod preview">
-        <div class="image-wrap outer">
-            <div class="point-stage" id="%(point_stage_id)s"
-                 data-image_preview_id="%(image_preview_id)s">
-                <div class="ppoi-point" id="%(ppoi_id)s"></div>
-            </div>
-            <div class="image-wrap inner">
-                %(image_preview)s
-            </div>
-        </div>
-    </div>"""
 
     def get_context(self, name, value, attrs):
         context = super(ImagePreviewFileInput, self).get_context(
