e34a023fdaa353bb1921d67403050a3eb1e3b685
michal <michal.sipa@mirumee.com>
2017-11-09 11:36:46 +0100

    Refactor get_purchase_cost_and_gross_margins

--- saleor/product/utils.py
+++ saleor/product/utils.py
@@ -316,35 +316,44 @@ def get_variant_availability_status(variant):
         return VariantAvailabilityStatus.AVAILABLE
 
 
-def get_purchase_cost_and_gross_margins(product, **kwargs):
-    zero_price = PriceRange(Price(0, 0, currency=DEFAULT_CURRENCY),
-                            Price(0, 0, currency=DEFAULT_CURRENCY))
-    purchase_costs_range = gross_margin = zero_price
+def get_purchase_cost_and_gross_margins(product):
+    zero_price = Price(0, 0, currency=DEFAULT_CURRENCY)
+    zero_price_range = PriceRange(zero_price, zero_price)
+    purchase_costs_range = gross_margin = zero_price_range
     gross_margin_percent = (0, 0)
-    costs = []
-    margins = []
-    percents = []
 
-    if product.variants.exists():
-        variants = product.variants.all()
-        for variant in variants:
-            for stock in variant.stock.all():
-                if stock.cost_price:
-                    cost = stock.cost_price
-                    costs.append(cost)
-                    price = product.get_price_per_item(variant, **kwargs)
-                    margin = price - cost
-                    margins.append(margin)
-                    percent = round((margin.gross / cost.gross) * 100, 0)
-                    percents.append(percent)
-        costs = sorted(costs, key=lambda x: x.gross)
-        margins = sorted(margins, key=lambda x: x.gross)
-        percents = sorted(percents)
+    if not product.variants.exists():
+        return purchase_costs_range, gross_margin, gross_margin_percent
+
+    variants = product.variants.all()
+    costs, margins, percents = get_cost_data_from_variants(variants)
 
     if costs:
         purchase_costs_range = PriceRange(min(costs), max(costs))
-    if margins:
         gross_margin = PriceRange(min(margins), max(margins))
-    if percents:
         gross_margin_percent = (percents[0], percents[-1])
     return purchase_costs_range, gross_margin, gross_margin_percent
+
+
+def sort_cost_data(costs, margins, percents):
+    costs = sorted(costs, key=lambda x: x.gross)
+    margins = sorted(margins, key=lambda x: x.gross)
+    percents = sorted(percents)
+    return costs, margins, percents
+
+
+def get_cost_data_from_variants(variants):
+    costs = []
+    margins = []
+    percents = []
+    for variant in variants:
+        for stock in variant.stock.all():
+            if stock.cost_price:
+                cost = stock.cost_price
+                costs.append(cost)
+                price = variant.get_price_per_item()
+                margin = price - cost
+                margins.append(margin)
+                percent = round((margin.gross / cost.gross) * 100, 0)
+                percents.append(percent)
+    return sort_cost_data(costs, margins, percents)
