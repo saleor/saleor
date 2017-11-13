3da878264e9cad05473acc39ff17179c03637a3a
Jakub Draganek <jakub.draganek@gmail.com>
2017-11-13 12:38:22 +0100

    Decouple es query generation and execution with testability in mind

--- saleor/search/backends/test_newelastic.py
+++ saleor/search/backends/test_newelastic.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
 from . import newelastic
-import pytest
 
 
 PHRASE = 'How fortunate man with none'
@@ -28,16 +27,6 @@ QUERY = {
 }
 
 
-@pytest.fixture
-def elasticsearch_client(mocker):
-    SETTINGS = {'URLS': ['http://localhost']}
-    nes = newelastic.SearchBackend(SETTINGS)
-    mocker.patch.object(
-        newelastic.SearchBackend.client, 'search', returnvalue=[])
-    return nes
-
-
-def test_storefront_product_search_query_syntax(elasticsearch_client):
-    products = newelastic.search_products(PHRASE)
-    newelastic.SearchBackend.client.search.assert_called_once_with(
-        body=QUERY, doc_type=[DOC_TYPE], index=[INDEX])
+def test_storefront_product_search_query_syntax():
+    products = newelastic.get_search_query(PHRASE)
+    assert products.to_dict() == QUERY
