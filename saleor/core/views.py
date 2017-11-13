7b77c102c3670012300ee8520d925b542273370e
dominik-zeglen <flesz3@o2.pl>
2017-11-13 10:46:50 +0100

    Change settings importing to match project's convention

--- saleor/core/views.py
+++ saleor/core/views.py
@@ -1,10 +1,10 @@
 from django.template.response import TemplateResponse
 from django.contrib import messages
+from django.conf import settings
 from django.utils.translation import pgettext_lazy
 
 from ..dashboard.views import staff_member_required
 from ..product.utils import products_with_availability, products_for_homepage
-from ..settings import IMPERSONATE_GET_PARAM
 
 
 def home(request):
@@ -12,7 +12,7 @@ def home(request):
     products = products_with_availability(
         products, discounts=request.discounts, local_currency=request.currency)
     # TODO: Get rid of url parameters
-    if request.GET.get('action') == IMPERSONATE_GET_PARAM:
+    if request.GET.get('action') == settings.IMPERSONATE_GET_PARAM:
         msg = pgettext_lazy('Impersonation message',
                             'You are now logged as %s') % request.user.email
         messages.success(request, msg)
