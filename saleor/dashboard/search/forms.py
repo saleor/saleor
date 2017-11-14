0f6db4f28ff3d610922d798293f21e06f9a63a8f
Jakub Draganek <jakub.draganek@gmail.com>
2017-11-14 15:43:07 +0100

    Use new elasticsearch backend in dashboard search

--- saleor/dashboard/search/forms.py
+++ saleor/dashboard/search/forms.py
@@ -1,10 +1,8 @@
 from ...search.forms import SearchForm
-from ...search.backends import newelastic
+from ...search.backends import elasticsearch_dashboard
 
 
 class DashboardSearchForm(SearchForm):
-    def search(self, queryset_map=None):
+    def search(self):
         query = self.cleaned_data['q']
-        results = newelastic.SearchBackend.search(
-            query, queryset_map=queryset_map)
-        return results
+        return elasticsearch_dashboard.search(query)
--- saleor/search/backends/elasticsearch_dashboard.py
+++ saleor/search/backends/elasticsearch_dashboard.py
@@ -1,5 +1,22 @@
-from . import newelastic
+from __future__ import unicode_literals
+
+from ..documents import ProductDocument, OrderDocument, UserDocument
+from elasticsearch_dsl.query import MultiMatch
 
 
-class SearchBackend(newelastic.SearchBackend):
-    pass
+def get_search_query(phrase):
+    ''' Execute external search for all objects matching phrase  '''
+    prod_query = MultiMatch(fields=['name', 'description'], query=phrase)
+    products = ProductDocument.search().query(prod_query).source(False)
+
+    users = UserDocument.search().query('match', email=phrase).source(False)
+
+    order_query = MultiMatch(
+        fields=['user_email', 'status', 'discount_name'], query=phrase)
+    orders = OrderDocument.search().query(order_query).source(False)
+
+    return products, users, orders
+
+
+def search(phrase):
+    return [s.to_queryset() for s in get_search_query(phrase)]
