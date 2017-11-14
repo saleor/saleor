492b2af15a6be89f3a94d2ebc8ff9582f9a54ad1
michal <michal.sipa@mirumee.com>
2017-11-14 10:00:30 +0100

    Add filters for product list in dashboard
    Add filters.py to dashboard/product.
    Update product_list view.
    Update dashboard/product/list.html.

---
+++ saleor/dashboard/product/filters.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+
+from django_filters import (FilterSet, MultipleChoiceFilter, RangeFilter,
+                            OrderingFilter)
+from django.utils.translation import pgettext_lazy
+from django_prices.models import PriceField
+
+from ...product.models import Product, ProductAttribute
+
+
+SORT_BY_FIELDS = {'name': pgettext_lazy('Product list sorting option', 'name'),
+                  'price': pgettext_lazy(
+                      'Product list sorting option', 'price')}
+
+
+class ProductFilter(FilterSet):
+    sort_by = OrderingFilter(
+        label=pgettext_lazy('Product list sorting form', 'Sort by'),
+        fields=SORT_BY_FIELDS.keys(),
+        field_labels=SORT_BY_FIELDS
+    )
+
+    class Meta:
+        model = Product
+        fields = ['categories', 'is_published', 'is_featured', 'price']
+        filter_overrides = {
+            PriceField: {
+                'filter_class': RangeFilter
+            }
+        }
