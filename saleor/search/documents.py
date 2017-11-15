b7c7c073595668c8ac7604d3fe0ee712f9b77321
Jakub Draganek <jakub.draganek@gmail.com>
2017-11-15 13:08:51 +0100

    Match whole email address in seach

--- saleor/search/documents.py
+++ saleor/search/documents.py
@@ -1,4 +1,5 @@
-from django_elasticsearch_dsl import DocType, Index
+from django_elasticsearch_dsl import DocType, Index, fields
+from elasticsearch_dsl import analyzer
 from ..product.models import Product
 from ..userprofile.models import User
 from ..order.models import Order
@@ -24,8 +25,16 @@ users = Index('users')
 users.settings(number_of_shards=1, number_of_replicas=0)
 
 
+email_analyzer = analyzer('email_analyzer', tokenizer='uax_url_email')
+
+
 @users.doc_type
 class UserDocument(DocType):
+    user = fields.StringField(analyzer=email_analyzer)
+
+    def prepare_user(self, instance):
+        return instance.email
+
     class Meta:
         model = User
         fields = ['email']
@@ -37,6 +46,11 @@ orders.settings(number_of_shards=1, number_of_replicas=0)
 
 @orders.doc_type
 class OrderDocument(DocType):
+    user = fields.StringField(analyzer=email_analyzer)
+
+    def prepare_user(self, instance):
+        return instance.user_email
+
     class Meta:
         model = Order
         fields = ['status', 'user_email', 'discount_name']
