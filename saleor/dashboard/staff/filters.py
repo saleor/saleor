35678262256af733306e4471cf8dd62aac4dc751
michal <michal.sipa@mirumee.com>
2017-11-15 12:24:53 +0100

    Add filters for staff members
    Move include _filters before check if QuerySet exists.

--- saleor/dashboard/staff/filters.py
+++ saleor/dashboard/staff/filters.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 
-from django_filters import (FilterSet, OrderingFilter)
+from django_filters import (FilterSet, RangeFilter, OrderingFilter)
 from django.utils.translation import pgettext_lazy
+from django_prices.models import PriceField
 
 from ...userprofile.models import User
 
@@ -12,20 +13,21 @@ SORT_BY_FIELDS = {
     'default_billing_address__first_name': pgettext_lazy(
         'Customer list sorting option', 'name'),
     'default_billing_address__city': pgettext_lazy(
-        'Customer list sorting option', 'location'),
-    'num_orders': pgettext_lazy(
-        'Customer list sorting option', 'number of orders'),
-    'last_order': pgettext_lazy(
-        'Customer list sorting option', 'last order')}
+        'Customer list sorting option', 'location')}
 
 
-class CustomerFilter(FilterSet):
+class StaffFilter(FilterSet):
     sort_by = OrderingFilter(
-        label=pgettext_lazy('Product list sorting form', 'Sort by'),
+        label=pgettext_lazy('Sale list sorting form', 'Sort by'),
         fields=SORT_BY_FIELDS.keys(),
         field_labels=SORT_BY_FIELDS
     )
 
     class Meta:
         model = User
-        fields = ['email', 'is_active', 'is_staff']
+        fields = ['email', 'is_active', 'groups']
+        filter_overrides = {
+            PriceField: {
+                'filter_class': RangeFilter
+            }
+        }
