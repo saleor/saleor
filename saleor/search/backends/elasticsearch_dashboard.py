761e45cecb557929ec1a5ea21c54a7a87f7bef2d
Jakub Draganek <jakub.draganek@gmail.com>
2017-11-15 11:09:32 +0100

    Refactor dashboard search tests

--- saleor/search/backends/elasticsearch_dashboard.py
+++ saleor/search/backends/elasticsearch_dashboard.py
@@ -4,20 +4,30 @@ from ..documents import ProductDocument, OrderDocument, UserDocument
 from elasticsearch_dsl.query import MultiMatch
 
 
-def get_search_query(phrase):
-    ''' Execute external search for all objects matching phrase  '''
+def _search_products(phrase):
     prod_query = MultiMatch(fields=['name', 'description'], query=phrase)
-    products = ProductDocument.search().query(prod_query).source(False)
+    return ProductDocument.search().query(prod_query).source(False)
 
-    users = UserDocument.search().query('match', email=phrase).source(False)
 
+def _search_users(phrase):
+    return UserDocument.search().query('match', email=phrase).source(False)
+
+
+def _search_orders(phrase):
     order_query = MultiMatch(
         fields=['user_email', 'status', 'discount_name'], query=phrase)
-    orders = OrderDocument.search().query(order_query).source(False)
+    return OrderDocument.search().query(order_query).source(False)
 
-    return {'products': products, 'users': users, 'orders': orders}
+
+def get_search_queries(phrase):
+    ''' Execute external search for all objects matching phrase  '''
+    return {
+        'products': _search_products(phrase),
+        'users': _search_users(phrase),
+        'orders': _search_orders(phrase)
+    }
 
 
 def search(phrase):
     ''' Provide queryset for every search result '''
-    return {k: s.to_queryset() for k, s in get_search_query(phrase).items()}
+    return {k: s.to_queryset() for k, s in get_search_queries(phrase).items()}
