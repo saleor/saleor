19c65ab790f4628427953ae0ef5288435ca046db
Jakub Draganek <jakub.draganek@gmail.com>
2017-11-15 10:37:40 +0100

    Provide named arguments for search result template

--- saleor/dashboard/search/views.py
+++ saleor/dashboard/search/views.py
@@ -1,10 +1,5 @@
-from django.conf import settings
 from django.shortcuts import render
 
-from ...order.models import Order
-from ...product.models import Product
-from ...search.views import paginate_results
-from ...userprofile.models import User
 from ..views import staff_member_required
 from .forms import DashboardSearchForm
 
@@ -13,19 +8,20 @@ from .forms import DashboardSearchForm
 def search(request):
     form = DashboardSearchForm(data=request.GET or None)
     query = ''
-    queryset_map = {
-        Product: Product.objects.prefetch_related('images'),
-        Order: Order.objects.prefetch_related('user'),
-        User: User.objects.all()}
+    users = []
+    products = []
+    orders = []
     if form.is_valid():
         results = form.search()
-        #page = paginate_results(results, request.GET, settings.PAGINATE_BY)
+        users = results['users']
+        products = results['products']
+        orders = results['orders']
         query = form.cleaned_data['q']
-    else:
-        results = []
     ctx = {
         'form': form,
         'query': query,
-        'results': results,
+        'products': products,
+        'orders': orders,
+        'users': users,
         'query_string': '?q=%s' % query}
     return render(request, 'dashboard/search/results.html', ctx)
