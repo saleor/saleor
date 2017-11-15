6dea9f4605969d9017f5ed0b27cb0e7fa99df944
Anna Siwik <anna.siwik@mirumee.com>
2017-11-15 16:33:49 +0100

    Describe AjaxSelect2ChoiceField in docstring

--- saleor/core/forms.py
+++ saleor/core/forms.py
@@ -2,6 +2,10 @@ from django import forms
 
 
 class AjaxSelect2ChoiceField(forms.ChoiceField):
+    """ This field enables usage of select2 with ajax fetched data.
+
+    fetch_data_url - specifies url, from which select2 will fetch data
+    """
 
     def __init__(self, fetch_data_url='', initial=None, *args, **kwargs):
         self.queryset = kwargs.pop('queryset')
@@ -25,9 +29,7 @@ class AjaxSelect2ChoiceField(forms.ChoiceField):
         forms.Field.validate(self, value)
         return True
 
-    def set_fetch_data_url(self, url):
-        self.widget.attrs['data-url'] = url
-
     def set_initial(self, obj):
+        """ Sets initially selected object on field's widget. """
         self.widget.attrs['data-initial-display'] = str(obj)
         self.widget.attrs['data-initial-value'] = obj.pk
