ff9c0aa867034907f082f4f2adb046bdd9db5748
akjanik <adam.janik@mirumee.com>
2017-11-16 12:11:24 +0100

    Use reverse function in tests

--- tests/test_impersonation.py
+++ tests/test_impersonation.py
@@ -10,8 +10,8 @@ def test_staff_with_permission_can_impersonate(
     staff_group.permissions.add(permission_impersonate_user)
     staff_user.groups.add(staff_group)
     staff_user = User.objects.get(pk=staff_user.pk)
-    response = staff_client.get('/impersonate/{}'.format(customer_user.pk),
-                                follow=True)
+    response = staff_client.get(reverse('impersonate-start',
+                                args=[customer_user.pk]), follow=True)
     assert response.context['user'] == customer_user
     assert response.context['user'].is_impersonate
     assert response.context['request'].impersonator == staff_user
