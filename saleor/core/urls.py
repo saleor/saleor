5b4b7e9aa53d59ff6f9dc74a6230619d48f7472e
akjanik <adam.janik@mirumee.com>
2017-11-16 15:46:01 +0100

    Use custom impersonate view to display additional message

--- saleor/core/urls.py
+++ saleor/core/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^style-guide/', views.styleguide, name='styleguide'),
+    url(r'^impersonate-msg/(?P<uid>\d+)/', views.impersonate_with_msg,
+        name='impersonate-msg'),
 ]
