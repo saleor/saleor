f895772f96972c1a4476f396c8e35c818545afe6
akjanik <adam.janik@mirumee.com>
2017-11-16 15:46:01 +0100

    Use custom impersonate view to display additional message

--- saleor/core/views.py
+++ saleor/core/views.py
@@ -2,20 +2,17 @@ from django.template.response import TemplateResponse
 from django.contrib import messages
 from django.conf import settings
 from django.utils.translation import pgettext_lazy
+from impersonate.views import impersonate
 
 from ..dashboard.views import staff_member_required
 from ..product.utils import products_with_availability, products_for_homepage
+from ..userprofile.models import User
 
 
 def home(request):
     products = products_for_homepage()[:8]
     products = products_with_availability(
         products, discounts=request.discounts, local_currency=request.currency)
-    # TODO: Get rid of url parameters
-    if request.GET.get('action') == settings.IMPERSONATE_GET_PARAM:
-        msg = pgettext_lazy('Impersonation message',
-                            'You are now logged as %s') % request.user.email
-        messages.success(request, msg)
     return TemplateResponse(
         request, 'home.html',
         {'products': products, 'parent': None})
@@ -24,3 +21,13 @@ def home(request):
 @staff_member_required
 def styleguide(request):
     return TemplateResponse(request, 'styleguide.html')
+
+
+def impersonate_with_msg(request, uid):
+    response = impersonate(request, uid)
+    if request.session.modified:
+        msg = pgettext_lazy('Impersonation message',
+                            'You are now logged as {}'.format(
+                                User.objects.get(pk=uid)))
+        messages.success(request, msg)
+    return response
