60d7e062a4b4f625a0f8c68bef60cc39a57c59b0
akjanik <adam.janik@mirumee.com>
2017-11-16 11:39:00 +0100

    * Allow staff user with permission to perform impersonation
    * Move functions related to impersonation to separate file
    
    * Add tests

--- saleor/settings.py
+++ saleor/settings.py
@@ -419,8 +419,8 @@ CELERY_RESULT_BACKEND = 'django-db'
 
 # IMPERSONATE SETTINGS
 IMPERSONATE_URI_EXCLUSIONS = [r'^dashboard/']
-IMPERSONATE_CUSTOM_USER_QUERYSET = 'saleor.userprofile.utils.get_customers'
+IMPERSONATE_CUSTOM_USER_QUERYSET = 'saleor.userprofile.impersonate.get_impersonatable_users'
 IMPERSONATE_GET_PARAM = 'impersonate'
 IMPERSONATE_REDIRECT_URL = '/?action=' + IMPERSONATE_GET_PARAM
 IMPERSONATE_USE_HTTP_REFERER = True
-IMPERSONATE_CUSTOM_ALLOW = 'saleor.userprofile.utils.can_impersonate'
+IMPERSONATE_CUSTOM_ALLOW = 'saleor.userprofile.impersonate.can_impersonate'
