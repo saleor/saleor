bd6dcc6de5f9a9f2dad793d4dd43a5a1f6dfecbe
dominik-zeglen <flesz3@o2.pl>
2017-11-17 15:09:13 +0100

    Make codeclimate happy

--- saleor/static/dashboard/js/components/product-filter.js
+++ saleor/static/dashboard/js/components/product-filter.js
@@ -1,15 +1,15 @@
-import {screenSizes} from "./misc";
+import {screenSizes} from './misc';
 
 let prevWindowWidth = null;
 let initialized = false;
 
 const moveFilters = () => {
-  if(prevWindowWidth !== window.innerWidth) {
+  if (prevWindowWidth !== window.innerWidth) {
     const $collapsibleCard = $('.collapse');
     prevWindowWidth = window.innerWidth;
-    if(window.innerWidth < screenSizes.md) {
+    if (window.innerWidth < screenSizes.md) {
       $('#product-list').before($('#product-filters'));
-      if(!initialized) {
+      if (!initialized) {
         $collapsibleCard.addClass('collapsed');
         initialized = true;
       }
@@ -21,17 +21,17 @@ const moveFilters = () => {
 };
 
 export default $(document).ready(() => {
-  if($('.body-products-list').length) {
+  if ($('.body-products-list').length) {
     moveFilters();
     $(window).on('resize', moveFilters);
   }
 
   $('.collapse-activate').on('click', (e) => {
     const $collapsibleCard = $('.collapse');
-    if($collapsibleCard.hasClass('collapsed')) {
+    if ($collapsibleCard.hasClass('collapsed')) {
       $collapsibleCard.removeClass('collapsed');
     } else {
       $collapsibleCard.addClass('collapsed');
     }
-  })
+  });
 });
