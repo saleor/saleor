0ae7b0a1063c11a05362517fdda047c889e43487
michal <michal.sipa@mirumee.com>
2017-11-21 09:47:57 +0100

    Small refactor of get_filter_chips
    Refactor get_filter_chips to be filter function.

--- saleor/dashboard/product/filters.py
+++ saleor/dashboard/product/filters.py
@@ -4,7 +4,7 @@ from django_filters import (FilterSet, RangeFilter, OrderingFilter)
 from django.utils.translation import pgettext_lazy
 from django_prices.models import PriceField
 
-from ...product.models import Product
+from ...product.models import Category, Product
 
 
 SORT_BY_FIELDS = {'name': pgettext_lazy('Product list sorting option', 'name'),
@@ -26,3 +26,27 @@ class ProductFilter(FilterSet):
                 'filter_class': RangeFilter
             }
         }
+
+    def get_chips(self):
+        categories = Category.objects.all()
+        chips = []
+        for iterator, item in enumerate(self.form):
+            if item.name is not 'sort_by' and item.value():
+                label = item.label
+                if isinstance(item.value(), (list, tuple)):
+                    if item.name == 'price':
+                        if any(item.value()):
+                            chips.append(label + ': ' + ' - '.join(item.value()))
+                    elif item.name == 'categories':
+                        for value in item.value():
+                            chips.append(
+                                label + ': ' + categories.get(pk=value).name)
+                    else:
+                        for value in item.value():
+                            chips.append(label + ': ' + str(value))
+                else:
+                    value = item.value()
+                    chips.append(label + ': ' + str(value))
+            else:
+                continue
+        return chips
