3297133f8f36f0da24db25c2bd571cb5c3fffb17
michal <michal.sipa@mirumee.com>
2017-11-24 09:41:38 +0100

    Add sort_by name in group_list
    Add filters.py to dashboard/group.
    Update group_list view.
    Update dashboard/group/list.html template.

--- saleor/dashboard/group/filters.py
+++ saleor/dashboard/group/filters.py
@@ -1,20 +1,19 @@
 from __future__ import unicode_literals
 
-from django_filters import (FilterSet, OrderingFilter)
+from django.contrib.auth.models import Group
+from django_filters import FilterSet, OrderingFilter
 from django.utils.translation import pgettext_lazy
 
-from ...product.models import Category
+
+SORT_BY_FIELDS = {'name': pgettext_lazy('Group list sorting option', 'name')}
 
 
-SORT_BY_FIELDS = {'name': pgettext_lazy('Product list sorting option', 'name')}
-
-
-class CategoryFilter(FilterSet):
+class GroupFilter(FilterSet):
     sort_by = OrderingFilter(
         label=pgettext_lazy('Product list sorting filter label', 'Sort by'),
         fields=SORT_BY_FIELDS.keys(),
         field_labels=SORT_BY_FIELDS)
 
     class Meta:
-        model = Category
+        model = Group
         fields = []
