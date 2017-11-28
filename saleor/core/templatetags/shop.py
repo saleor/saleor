0d81468f3ebc6cc3fd3e4e4e8eaefa740e080f10
michal <michal.sipa@mirumee.com>
2017-11-28 10:55:40 +0100

    Refactor get_sort_by_toggle to be one function

--- saleor/core/templatetags/shop.py
+++ saleor/core/templatetags/shop.py
@@ -32,6 +32,9 @@ def get_sort_by_url(context, field, descending=False):
 
 @register.assignment_tag(takes_context=True)
 def get_sort_by_toggle(context, field):
+    '''
+    This templatetag returns data needed for sorting querysets by links.
+    '''
     request = context['request']
     request_get = request.GET.copy()
     sort_by = request_get.get('sort_by')
@@ -39,39 +42,24 @@ def get_sort_by_toggle(context, field):
     active = ''
     if sort_by:
         # toggle existing sort_by
-        new_sort_by, arrow_src, active = toggle(field, sort_by)
+        if field == sort_by:  # descending sort
+            new_sort_by = u'-%s' % field
+            arrow_src = static('/images/arrow_up_icon.svg')
+            active = 'active'
+        else:  # ascending sort
+            new_sort_by = field
+            if field == sort_by[1:]:
+                arrow_src = static('/images/arrow_down_icon.svg')
+                active = 'active'
+            else:
+                arrow_src = ''
+                active = ''
     else:
         # first click on link
-        new_sort_by = u'-%s' % field  # descending sort
+        new_sort_by = field  # descending sort
     request_get['sort_by'] = new_sort_by
 
     return {
         'url': '%s?%s' % (request.path, request_get.urlencode()),
         'is_active': active,
         'arrow_src': arrow_src}
-
-
-def toggle(field, sort_by):
-    if field == sort_by:
-        active, arrow_src, new_sort_by = set_ascending(field)
-    else:
-        active, arrow_src, new_sort_by = set_descending(field, sort_by)
-    return new_sort_by, arrow_src, active
-
-
-def set_ascending(field):
-    new_sort_by = u'-%s' % field  # descending sort
-    arrow_src = static('/images/arrow_up_icon.svg')
-    active = 'active'
-    return active, arrow_src, new_sort_by
-
-
-def set_descending(field, sort_by):
-    new_sort_by = field  # ascending sort
-    if field == sort_by[1:]:
-        arrow_src = static('/images/arrow_down_icon.svg')
-        active = 'active'
-    else:
-        arrow_src = ''
-        active = ''
-    return active, arrow_src, new_sort_by
