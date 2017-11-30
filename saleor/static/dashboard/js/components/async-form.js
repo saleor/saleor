2e958c6241162773b35efefe126c4772e19f4d0d
dominik-zeglen <flesz3@o2.pl>
2017-11-30 11:59:39 +0100

    Refactor async forms' code

--- saleor/static/dashboard/js/components/async-form.js
+++ saleor/static/dashboard/js/components/async-form.js
@@ -1,28 +1,42 @@
-import {initSelects} from './utils';
+import { initSelects } from './utils';
 
-export default $(document).on('submit', '.form-async', function (e) {
-  let that = this;
-  $.ajax({
-    url: $(that).attr('action'),
-    method: 'post',
-    data: $(that).serialize(),
-    complete: function (response) {
-      if (response.status === 400) {
-        $(that).parent().html(response.responseText);
-        initSelects();
-      } else {
-        $('.modal-close').click();
-      }
-    },
-    success: function (response) {
-      if (response.redirectUrl) {
-        window.location.href = response.redirectUrl;
-      } else {
-        location.reload();
-      }
-    }
-  });
+const onAsyncFormSubmit = (e) => {
   e.preventDefault();
-}).on('click', '.modal-close', function () {
-  $('.modal').modal('close');
-});
+  const $target = $(e.currentTarget);
+  fetch($target.attr('action'), {
+    method: 'POST',
+    body: new FormData($target[0]),
+    credentials: 'same-origin',
+    headers: {
+      'X-CSRFToken': $.cookie('csrftoken'),
+    },
+  }).then((res) => {
+    let output;
+    if (res.status !== 200) {
+      output = new Error(res.statusText);
+    } else {
+      output = res.text();
+    }
+    return output;
+  }).then((data) => {
+    if (data.redirectUrl) {
+      window.location.href = data.redirectUrl;
+    } else {
+      window.location.reload();
+    }
+  }).catch((err) => {
+    $target.parent().html(err);
+    initSelects();
+  });
+};
+
+const onModalClose = () => $('.modal').modal('close');
+
+function init() {
+  return $(document).on('submit', '.form-async', onAsyncFormSubmit)
+    .on('click', '.modal-close', onModalClose);
+}
+
+export {
+  init as default,
+};
