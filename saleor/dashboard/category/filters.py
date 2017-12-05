397cacf4ba6269db1559c07f68bf4ec0efc00b6a
michal <michal.sipa@mirumee.com>
2017-12-05 12:33:11 +0100

    Add 'is_hidden'
    Rename Category field 'hidden' to 'is_hidden'.
    Add proper migration.
    Update project code with new field name.
    Add order_by('name') for querysets in category_list and category_detail view.
    Add filters for 'name' and 'is_hidden' for current level of Category tree structure.
    Update templates.

--- saleor/dashboard/category/filters.py
+++ saleor/dashboard/category/filters.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 
+from django import forms
 from django.utils.translation import pgettext_lazy
-from django_filters import OrderingFilter
+from django_filters import CharFilter, ChoiceFilter, OrderingFilter
 
 from ...core.filters import SortedFilterSet
 from ...product.models import Category
@@ -9,10 +10,23 @@ from ...product.models import Category
 SORT_BY_FIELDS = {
     'name': pgettext_lazy('Category list sorting option', 'name'),
     'description': pgettext_lazy(
-        'Category list sorting option', 'description')}
+        'Category list sorting option', 'description'),
+    'is_hidden': pgettext_lazy('Category list sorting option', 'is hidden')}
+
+IS_HIDDEN_CHOICES = (
+    ('1', pgettext_lazy('Is hidden filter choice', 'Hidden')),
+    ('0', pgettext_lazy('Is hidden filter choice', 'Not hidden')))
 
 
 class CategoryFilter(SortedFilterSet):
+    name = CharFilter(
+        label=pgettext_lazy('Product list filter label', 'Name'),
+        lookup_expr='icontains')
+    is_hidden = ChoiceFilter(
+        label=pgettext_lazy('Product list filter label', 'Is published'),
+        choices=IS_HIDDEN_CHOICES,
+        empty_label=pgettext_lazy('Filter empty choice label', 'All'),
+        widget=forms.Select)
     sort_by = OrderingFilter(
         label=pgettext_lazy('Product list sorting filter label', 'Sort by'),
         fields=SORT_BY_FIELDS.keys(),
