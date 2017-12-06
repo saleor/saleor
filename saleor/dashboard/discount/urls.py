b3c2443686a05c5422ab973e8ad7bfd2e0df4470
Anna Siwik <anna.siwik@mirumee.com>
2017-12-06 17:05:33 +0100

    Fix too long line issue

--- saleor/dashboard/discount/urls.py
+++ saleor/dashboard/discount/urls.py
@@ -8,10 +8,13 @@ urlpatterns = [
     url(r'sale/$', views.sale_list, name='sale-list'),
     url(r'sale/(?P<pk>[0-9]+)/$', views.sale_edit, name='sale-update'),
     url(r'sale/add/$', views.sale_edit, name='sale-add'),
-    url(r'sale/(?P<pk>[0-9]+)/delete/$', views.sale_delete, name='sale-delete'),
+    url(r'sale/(?P<pk>[0-9]+)/delete/$', views.sale_delete,
+        name='sale-delete'),
 
     url(r'voucher/$', views.voucher_list, name='voucher-list'),
-    url(r'voucher/(?P<pk>[0-9]+)/$', views.voucher_edit, name='voucher-update'),
+    url(r'voucher/(?P<pk>[0-9]+)/$', views.voucher_edit,
+        name='voucher-update'),
     url(r'voucher/add/$', views.voucher_edit, name='voucher-add'),
-    url(r'voucher/(?P<pk>[0-9]+)/delete/$', views.voucher_delete, name='voucher-delete'),
+    url(r'voucher/(?P<pk>[0-9]+)/delete/$', views.voucher_delete,
+        name='voucher-delete'),
 ]
