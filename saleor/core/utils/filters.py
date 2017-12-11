f1264675301e26cebb62aeb8bbd7aac4a4d76876
dominik-zeglen <flesz3@o2.pl>
2017-12-11 10:48:00 +0100

    Turn off sorting indicator translation before blocktrans

--- saleor/core/utils/filters.py
+++ saleor/core/utils/filters.py
@@ -9,12 +9,12 @@ def get_sort_by_choices(filter_set):
             filter_set.filters['sort_by'].field.choices[1::2]]
 
 
-def get_now_sorted_by(filter_set, fields, default_sort='name'):
+def get_now_sorted_by(filter_set, default_sort='name'):
     sort_by = filter_set.form.cleaned_data.get('sort_by')
     if sort_by:
-        sort_by = fields[sort_by[0].strip('-')]
+        sort_by = sort_by[0].strip('-')
     else:
-        sort_by = fields[default_sort]
+        sort_by = default_sort
     return sort_by
 
 
