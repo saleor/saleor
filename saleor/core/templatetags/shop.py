2e30f3dcce0b40b3ff26fba3fed8d2b134b460d9
Pacu2 <filip.m.kucharczyk@gmail.com>
2017-12-11 10:08:03 +0100

    Remove legacy slice templatetag

--- saleor/core/templatetags/shop.py
+++ saleor/core/templatetags/shop.py
@@ -7,13 +7,6 @@ from django.utils.http import urlencode
 register = Library()
 
 
-@register.filter
-def slice(items, group_size=1):
-    args = [iter(items)] * group_size
-    return (filter(None, group)
-            for group in zip_longest(*args, fillvalue=None))
-
-
 @register.simple_tag(takes_context=True)
 def get_sort_by_url(context, field, descending=False):
     request = context['request']
