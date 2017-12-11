22ae8fd45ab4c4274ce0e0e59d4c7503daeb4e49
Pacu2 <filip.m.kucharczyk@gmail.com>
2017-12-11 10:28:51 +0100

    Move build_absolute_url to templatetag

---
+++ saleor/core/templatetags/urls.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def build_absolute_uri(request, location):
+    return request.build_absolute_uri(location)
+
