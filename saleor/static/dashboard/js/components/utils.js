496e8506350caef9ca9772ea52943814db7b7e0a
dominik-zeglen <flesz3@o2.pl>
2017-12-11 19:04:25 +0100

    Refactor code

--- saleor/static/dashboard/js/components/utils.js
+++ saleor/static/dashboard/js/components/utils.js
@@ -11,43 +11,35 @@ try {
 } catch (e) {
 }
 
-export function onScroll(func) {
+function appendOption ($select, option) {
+  $select.append($('<option></option>')
+    .attr('value', option.id).text(option.text));
+}
+
+export function onScroll (func) {
   window.addEventListener('scroll', func, supportsPassive ? {passive: true} : false);
 }
 
-export function initSelects() {
+export function initSelects () {
   $('select:not(.browser-default):not(.enable-ajax-select2):not([multiple])').material_select();
   $('select[multiple]:not(.browser-default):not(.enable-ajax-select2)').select2({width: '100%'});
 
-  let $ajaxSelect2Elements = $('select.enable-ajax-select2');
-  $ajaxSelect2Elements.each(function() {
-    let $select = $(this);
+  $('select.enable-ajax-select2').each((i, select) => {
+    const $select = $(select);
+    const initial = $select.data('initial');
 
-    if ($select.attr('data-initial')) {
-      function appendOption(select, option) {
-        $select.append($('<option></option>').attr(
-          'value', option.id).text(option.text));
-      }
+    if (initial) {
+      const initialData = initial instanceof Array ? initial : [initial];
+      const selected = initialData.map((item) => {
+        appendOption($select, item);
+        return (item.id);
+      });
+      $select.val(selected);
+    }
 
-      let initial = $select.data('initial');
-      if (initial instanceof Array) {
-        let selected = []
-        initial.forEach(function(item) {
-          appendOption($select, item);
-          selected.push(item.id);
-        });
-        $select.val(selected);
-      }
-      else {
-        appendOption($select, initial);
-        $select.val(initial.id);
-      }
-    };
-
-    let url = $select.data('url');
     $select.select2({
       ajax: {
-        url: url,
+        url: $select.data('url'),
         delay: 250
       },
       width: '100%',
