88d14ff56ce9665943264a487cf3d4e249ad106e
dominik-zeglen <flesz3@o2.pl>
2017-12-12 14:29:11 +0100

    Refactor code

--- saleor/static/dashboard/js/components/search.js
+++ saleor/static/dashboard/js/components/search.js
@@ -1,16 +1,27 @@
+function submit (e) {
+  e.preventDefault();
+  $(e.currentTarget).parent().submit();
+}
+
+function openSearchBar (e) {
+  e.preventDefault();
+  const $target = $(e.currentTarget);
+  $('.search').toggleClass('expanded', !$target.hasClass('expanded'));
+}
+
+function closeSearchBar (e) {
+  const $target = $(e.currentTarget);
+  const isExpanded = $target.hasClass('expanded');
+  $('.search').toggleClass('expanded', isExpanded);
+  $target.toggleClass('active', !isExpanded);
+}
+
 const init = $(() => {
-  $('#btn-search').on('click', (e) => {
-    e.preventDefault();
-    const $target = $(e.currentTarget);
-    $('.search').toggleClass('expanded', !$target.hasClass('expanded'));
-  });
-  $('#btn-search-close').on('click', (e) => {
-    const $target = $(e.currentTarget);
-    const isExpanded = $target.hasClass('expanded');
-    $('.search').toggleClass('expanded', isExpanded);
-    $target.toggleClass('active', !isExpanded);
-  });
+  $('#btn-search').on('click', openSearchBar);
+  $('#btn-search-close').on('click', closeSearchBar);
+  $('#btn-search-submit').on('click', submit);
 });
+
 export {
   init as default
 };
