eb9d13bb0c8da879e81cc0f03be338bc096238d2
dominik-zeglen <flesz3@o2.pl>
2017-12-13 15:39:49 +0100

    Add GitHub widget

--- themes/saleor/js/app.js
+++ themes/saleor/js/app.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
-     $("#dashboard-screens").carousel();
+$(document).ready(function () {
+  $('#dashboard-screens').carousel();
+  $.ajax({
+    url: 'https://api.github.com/repos/mirumee/saleor',
+    contentType: 'application/json',
+    success: function(res) {
+      $('#gh-stars').text(res.stargazers_count);
+    },
+    error: function(res) {
+      $('#gh-stars').text('error')
+    }
+  })
 });
