0d8471bc09d706c3e9845c22803f42d0107a2367
akjanik <adam.janik@mirumee.com>
2017-12-15 15:10:08 +0100

    * WIP: Add separate view for modal
    * Use instance of base_modal

--- saleor/dashboard/product/views.py
+++ saleor/dashboard/product/views.py
@@ -111,7 +111,6 @@ def product_list(request):
     products = Product.objects.prefetch_related('images')
     products = products.order_by('name')
     product_classes = ProductClass.objects.all()
-
     form = forms.ProductClassSelectorForm(
         request.POST or None, product_classes=product_classes)
     if form.is_valid():
@@ -127,6 +126,23 @@ def product_list(request):
         'filter': product_filter,
     }
     return TemplateResponse(request, 'dashboard/product/list.html', ctx)
+
+
+@staff_member_required
+@permission_required('product.edit_product')
+def product_select_classes(request):
+    product_classes = ProductClass.objects.all()
+    form = forms.ProductClassSelectorForm(
+        request.POST or None, product_classes=product_classes)
+    status = 200
+    if form.is_valid():
+        return redirect(
+            'dashboard:product-add', class_pk=form.cleaned_data['product_cls'])
+    elif form.errors:
+        status = 400
+    ctx = {'form': form, 'product_classes': product_classes}
+    template = 'product/modal/add_product.html'
+    return TemplateResponse(request, template, ctx, status=status)
 
 
 @staff_member_required
