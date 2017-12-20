b27b9acad4b9f58b146032feff89e8a059d99f7b
Marcin Gębala <maarcin.gebala@gmail.com>
2017-12-20 16:10:29 +0100

    Use custom field to allow distinct filtering

---
+++ saleor/graphql/fields.py
@@ -0,0 +1,14 @@
+
+from graphene_django.fields import DjangoConnectionField
+
+
+class DistinctConnectionField(DjangoConnectionField):
+    """Connection that allows combining two querysets, by making both of them
+    distinct, if at least one of them is distinct."""
+
+    @classmethod
+    def merge_querysets(cls, default_queryset, queryset):
+        if queryset.query.distinct or default_queryset.query.distinct:
+            queryset = queryset.distinct()
+            default_queryset = default_queryset.distinct()
+        return queryset & default_queryset
