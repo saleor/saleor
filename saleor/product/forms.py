e006c1f583493899812e30392288ee9e2716a999
dominik-zeglen <flesz3@o2.pl>
2018-01-05 15:17:33 +0100

    Simplify code

--- saleor/product/forms.py
+++ saleor/product/forms.py
@@ -23,19 +23,19 @@ class VariantChoiceField(forms.ModelChoiceField):
                 'price': gross(obj.get_price(discounts=self.discounts))}
         return label
 
-    def update_field_data(self, product, cart):
+    def update_field_data(self, variants, cart):
         """ Function initializing fields custom data """
-        self.queryset = product.variants
+        self.queryset = variants
         self.discounts = cart.discounts
         self.empty_label = None
         images_map = {variant.pk: [vi.image.image.url
                                    for vi in variant.variant_images.all()]
-                      for variant in product.variants.all()}
+                      for variant in variants.all()}
         self.widget.attrs['data-images'] = json.dumps(images_map)
         # Don't display select input if there are less than two variants
         if self.queryset.count() < 2:
             self.widget = forms.HiddenInput(
-                {'value': product.variants.all()[0].pk})
+                {'value': variants.all()[0].pk})
 
 
 class ProductForm(AddToCartForm):
@@ -44,7 +44,7 @@ class ProductForm(AddToCartForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         variant_field = self.fields['variant']
-        variant_field.update_field_data(self.product, self.cart)
+        variant_field.update_field_data(self.product.variants, self.cart)
 
     def get_variant(self, cleaned_data):
         return cleaned_data.get('variant')
