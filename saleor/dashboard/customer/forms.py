3c3c998e90ee6d4b174d3a96b38c1274f6136c7d
michal <michal.sipa@mirumee.com>
2018-01-09 11:17:10 +0100

    Split customer_edit to customer_edit and customer_create
    Add comment to CustomerForm __init__.

--- saleor/dashboard/customer/forms.py
+++ saleor/dashboard/customer/forms.py
@@ -7,6 +7,7 @@ class CustomerForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
+        # disable 'is_active' checkbox if user edits his own account
         if self.user == self.instance:
             self.fields['is_active'].disabled = True
 
