2895335b79586e40a22a785252b68ce4bebb4b36
Anna Siwik <anna.siwik@mirumee.com>
2018-01-11 09:41:19 +0100

    Rename success payment page to checkout_success_anonymous

--- saleor/order/urls.py
+++ saleor/order/urls.py
@@ -12,8 +12,8 @@ urlpatterns = [
         views.start_payment, name='payment'),
     url(r'^%s/cancel-payment/$' % (TOKEN_PATTERN,), views.cancel_payment,
         name='cancel-payment'),
-    url(r'^%s/create-password/$' % (TOKEN_PATTERN,),
-        views.create_password, name='create-password'),
+    url(r'^%s/checkout-success-anonymous/$' % (TOKEN_PATTERN,),
+        views.checkout_success_anonymous, name='checkout-success-anonymous'),
     url(r'^%s/attach/$' % (TOKEN_PATTERN,),
         views.connect_order_with_user, name='connect-order-with-user'),
 ]
