16b1fccc849e0e5606169f16f7830c580fb2ae4e
primal100 <paul>
2018-01-14 01:57:55 +0000

    Made country comparison client on country area change event

--- saleor/static/js/components/address-form.js
+++ saleor/static/js/components/address-form.js
@@ -1,5 +1,4 @@
-export default $(document).ready((e) => {
-  $(function () {
+export default $(document).ready((e) => {  $(function () {
     const $i18nAddresses = $('.i18n-address');
     $i18nAddresses.each(function () {
       const $form = $(this).closest('form');
@@ -9,6 +8,39 @@ export default $(document).ready((e) => {
         $previewField.val('on');
         $form.submit();
       });
+      const $countryAreaList = $form.find('#country_area_list');
+      if ($countryAreaList){
+          const $countryAreaField = $form.find('input[name=country_area]');
+          const countryAreaOptions = $countryAreaList.find('option').toArray().reduce((options, option) => {
+              option = $(option);
+              let value = option.val();
+              let text = option.text();
+              options.push(value);
+              if (option != text) options.push(text);
+              return options;
+        }, []);
+        let countryAreaTimeout = null;
+        $countryAreaField.on('change', () => {
+            clearTimeout(countryAreaTimeout);
+            countryAreaTimeout = setTimeout(() => {
+                let lowerCaseValue = $countryAreaField.val().toLowerCase();
+                console.log($countryField.val());
+                if (lowerCaseValue){
+                    let value = countryAreaOptions.find(val => val.toLowerCase() == lowerCaseValue);
+                    if (value) $countryAreaField.val(value);
+                    else {
+                        let value = countryAreaOptions.find(val => val.toLowerCase().startsWith(lowerCaseValue));
+                        if (value) $countryAreaField.val(value);
+                        else {
+                            let value = countryAreaOptions.find(val => val.toLowerCase().includes(lowerCaseValue));
+                            if (value) $countryAreaField.val(value);
+                        }
+                    }
+                }
+            }, 500)
+        })
+        $countryAreaField.trigger('change');
+      }
     });
   });
 
