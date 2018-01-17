c26169a7ba542be290cc48f61820872e7bc5f061
Anna Siwik <anna.siwik@mirumee.com>
2018-01-17 10:09:48 +0100

    Improve adding category migration

--- saleor/product/migrations/0046_product_category.py
+++ saleor/product/migrations/0046_product_category.py
@@ -8,11 +8,16 @@ import django.db.models.deletion
 
 def assign_category_to_products(apps, schema_editor):
     Product = apps.get_model('product', 'Product')
-    Category = apps.get_model('product', 'Category')
-    default_category = Category.objects.first()
     for product in Product.objects.all():
-        product.category = product.categories.first() or default_category
+        product.category = product.categories.first()
         product.save()
+
+
+def assign_categories_to_products(apps, schema_editor):
+    Product = apps.get_model('product', 'Product')
+    for product in Product.objects.all():
+        if product.category:
+            product.categories.add(product.category)
 
 
 class Migration(migrations.Migration):
@@ -27,5 +32,5 @@ class Migration(migrations.Migration):
             name='category',
             field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='product.Category'),
         ),
-        migrations.RunPython(assign_category_to_products, migrations.RunPython.noop),
+        migrations.RunPython(assign_category_to_products, assign_categories_to_products),
     ]
