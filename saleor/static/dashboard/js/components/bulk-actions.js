c694d81ad193e4d02e0cfbbc3c30a62598c98d4f
dominik-zeglen <flesz3@o2.pl>
2018-01-18 18:55:50 +0100

    Overhaul visual side of bulk actions bar

--- saleor/static/dashboard/js/components/bulk-actions.js
+++ saleor/static/dashboard/js/components/bulk-actions.js
@@ -1,28 +1,34 @@
-function updateSelectedItemsText () {
+function moveSelectAllCheckbox(bulk, selectAllState) {
+  const $selectAll = $('#select-all-products').parent()[0].innerHTML;
+  const $headerContainer = $('thead .bulk-checkbox');
+  const $actionBarContainer = $('.data-table-bulk-actions__select-all');
+  if (bulk) {
+    $headerContainer.html('');
+    $actionBarContainer[0].innerHTML = $selectAll;
+  } else {
+    $actionBarContainer.html('');
+    $headerContainer[0].innerHTML = $selectAll;
+  }
+  $actionBarContainer.find('#select-all-products').prop('checked', selectAllState);
+  $('.select-all').on('change', onSelectAll);
+}
+
+function updateSelectedItemsText (selectAllState) {
+  if ([true, false].indexOf(selectAllState) === -1) {
+    selectAllState = false;
+  }
   const count = $('.select-item:checked').length - $('#select-all-products:checked').length;
-  const $counterTextNode = $('.data-table-header-action-selected-items');
-  const $header = $('.data-table-header-alternative');
+  const $counterTextNode = $('.data-table-bulk-actions__selected-items');
+  const $header = $('.data-table-bulk-actions');
   const counterText = ngettext('item selected', 'items selected', count);
 
   if (!count) {
     $header.addClass('single').removeClass('bulk');
+    moveSelectAllCheckbox(false, selectAllState);
   } else {
     $counterTextNode.html(`${count} ${counterText}`);
     $header.addClass('bulk').removeClass('single');
-  }
-}
-
-function onSwitchActions (e) {
-  const $target = $(e.currentTarget);
-  const $targetForm = $target.parents('form');
-  const $btnChecked = $targetForm.find('.btn-show-when-checked');
-  const $btnUnchecked = $targetForm.find('.btn-show-when-unchecked');
-  if ($targetForm.find('.switch-actions:checked').length) {
-    $btnChecked.show();
-    $btnUnchecked.hide();
-  } else {
-    $btnUnchecked.show();
-    $btnChecked.hide();
+    moveSelectAllCheckbox(true, selectAllState);
   }
 }
 
@@ -31,7 +37,9 @@ function onSelectAll (e) {
   const $targetForm = $target.parents('form');
   const $items = $targetForm.find('.switch-actions');
   $items.prop('checked', $target[0].checked);
-  updateSelectedItemsText();
+  console.log($target[0].checked);
+  $target.off('change');
+  updateSelectedItemsText($target[0].checked);
 }
 
 function onSubmit (e) {
@@ -44,7 +52,6 @@ function onSubmit (e) {
 // -----
 
 updateSelectedItemsText();
-$('.select-all').on('change', onSelectAll);
-$('.switch-actions').on('change', onSwitchActions);
+$('.select-all').on('click', onSelectAll);
 $('.select-item').on('change', updateSelectedItemsText);
 $('.bulk-actions a').on('click', onSubmit);
