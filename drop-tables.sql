cbe56f182a7f9e1cb016ab548a7c622eaeb2a04c
Till Kolter <till.kolter@gmail.com>
2018-01-24 00:29:39 +0100

    Add script that drops all relevant tables. DANGER!!!!!

---
+++ drop-tables.sql
@@ -0,0 +1,20 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+# Drop all relevant tables
+DROP TABLE lief_art;
+DROP TABLE artikel;
+DROP TABLE depot;
+DROP TABLE saleor_oye_webshoprelease;
+DROP TABLE verkauft;
+DROP TABLE bestellungen;
+DROP TABLE orders;
+
+
+### Insert DB
+# ...
+# ...
+
+### Reactivate
+SET FOREIGN_KEY_CHECKS = 1;
+
+
