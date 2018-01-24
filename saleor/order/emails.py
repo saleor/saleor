bd8897c92a54167ce9f3425794f321e4805b284e
Anna Siwik <anna.siwik@mirumee.com>
2018-01-24 16:18:07 +0100

    Add HTML template for order confirmation e-mail

--- saleor/order/emails.py
+++ saleor/order/emails.py
@@ -8,19 +8,22 @@ CONFIRM_PAYMENT_TEMPLATE = 'order/payment/confirm_payment'
 CONFIRM_NOTE_TEMPLATE = 'order/note/confirm_note'
 
 
-def _send_confirmation(email, url, template):
+def _send_confirmation(email, url, template, ctx=None):
     site = Site.objects.get_current()
+    context = {'site_name': site.name, 'url': url}
+    if ctx:
+        context.update(ctx)
     send_templated_mail(
         from_email=settings.ORDER_FROM_EMAIL,
         recipient_list=[email],
-        context={'site_name': site.name,
-                 'url': url},
+        context=context,
         template_name=template)
 
 
 @shared_task
-def send_order_confirmation(email, url):
-    _send_confirmation(email, url, CONFIRM_ORDER_TEMPLATE)
+def send_order_confirmation(email, url, order):
+    ctx = {'order': order}
+    _send_confirmation(email, url, CONFIRM_ORDER_TEMPLATE, ctx)
 
 
 @shared_task
