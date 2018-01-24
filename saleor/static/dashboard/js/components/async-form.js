58d6847ecec4a00b124867d2e423d5d007ed0ebb
Rafał Pitoń <rafal@mirumee.com>
2018-01-24 15:03:13 +0100

    Use message and SVG from WSGI middleware

--- saleor/static/dashboard/js/components/async-form.js
+++ saleor/static/dashboard/js/components/async-form.js
@@ -1,8 +1,21 @@
 import { initSelects } from './selects';
 
 export default $(document).on('submit', '.form-async', function (e) {
+  let that = this;
+  $.ajax({
+    url: $(that).attr('action'),
+    method: 'post',
+    data: $(that).serialize(),
+    success: function (data) {
+      let $message = $(data).find('div');
+      $message.find('a').remove();
+      $(that).find('.row').last().html($message);
+      $(that).find('.row').last().css('text-align', 'center');
+      $(that).find('svg').height('160px');
+      $(that).find('button').remove();
+    }
+  });
   e.preventDefault();
-  alert("Be aware admin pirate! Dashboard runs in read only mode!");
 }).on('click', '.modal-close', function () {
   $('.modal').modal('close');
 });
