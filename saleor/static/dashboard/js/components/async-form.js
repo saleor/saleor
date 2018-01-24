8772630bd05294080a98419875536534961b24f3
Rafał Pitoń <rafal@mirumee.com>
2018-01-24 11:18:28 +0100

    Block modal submissions in dashboard

--- saleor/static/dashboard/js/components/async-form.js
+++ saleor/static/dashboard/js/components/async-form.js
@@ -1,35 +1,8 @@
 import { initSelects } from './selects';
 
-const onAsyncFormSubmit = (e) => {
-  const $target = $(e.currentTarget);
-  $.ajax({
-    url: $target.attr('action'),
-    method: 'POST',
-    data: $target.serialize(),
-    complete: (response) => {
-      // Write HTML if got 400 response, otherwise pretend nothing happened
-      if (response.status === 400) {
-        $target.parent().html(response.responseText);
-        initSelects();
-      } else {
-        $('.modal-close').click();
-      }
-    },
-    success: (response) => {
-      if (response.redirectUrl) {
-        window.location.href = response.redirectUrl;
-      } else {
-        location.reload();
-      }
-    }
-  });
+export default $(document).on('submit', '.form-async', function (e) {
   e.preventDefault();
-};
-
-const onModalClose = () => $('.modal').modal('close');
-
-// -----
-
-$(document)
-  .on('submit', '.form-async', onAsyncFormSubmit)
-  .on('click', '.modal-close', onModalClose);
+  alert("Be aware admin pirate! Dashboard runs in read only mode!");
+}).on('click', '.modal-close', function () {
+  $('.modal').modal('close');
+});
