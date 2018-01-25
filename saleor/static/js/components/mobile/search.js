c67a9ab4450771a6d88c3ad51a2fa009694707b2
dominik-zeglen <flesz3@o2.pl>
2018-01-25 17:34:17 +0100

    Fix search bar

--- saleor/static/js/components/mobile/search.js
+++
@@ -1,8 +0,0 @@
-const $searchIcon = $('.mobile-search-icon');
-const $closeSearchIcon = $('.mobile-close-search');
-const $searchForm = $('.search-form');
-
-// -----
-
-$searchIcon.on('click', () => $searchForm.removeClass('search-form--hidden'));
-$closeSearchIcon.on('click', () => $searchForm.addClass('search-form--hidden'));
--- saleor/static/js/components/navbar.js
+++ saleor/static/js/components/navbar.js
@@ -1,7 +1,10 @@
 const $toogleIcon = $('.navbar__brand__menu-toggle');
 const $mobileNav = $('nav');
+const $searchIcon = $('.mobile-search-icon');
+const $closeSearchIcon = $('.mobile-close-search');
+const $searchForm = $('.search-form');
 
-const renderLoginBar = () => {
+const renderNavbar = () => {
   const $desktopLinkBar = $('.navbar__login');
   const $mobileLinkBar = $('.navbar__menu__login');
   const windowWidth = window.innerWidth;
@@ -9,6 +12,8 @@ const renderLoginBar = () => {
   if (windowWidth < 768) {
     const $desktopLinks = $desktopLinkBar.find('a');
     if ($desktopLinks.length) {
+      $searchForm.addClass('search-form--hidden');
+      console.log($searchForm.hasClass('search-form--hidden'));
       $mobileNav.append('<ul class="nav navbar-nav navbar__menu__login"></ul>');
       $desktopLinks
         .appendTo('.navbar__menu__login')
@@ -21,6 +26,8 @@ const renderLoginBar = () => {
   } else {
     const $mobileLinks = $mobileLinkBar.find('a');
     if ($mobileLinks.length) {
+      $searchForm.removeClass('search-form--hidden');
+      console.log($searchForm.hasClass('search-form--hidden'));
       $mobileLinks
         .appendTo('.navbar__login ul')
         .wrap('<li></li>')
@@ -32,7 +39,7 @@ const renderLoginBar = () => {
 
 // -----
 
-renderLoginBar();
+renderNavbar();
 $toogleIcon
   .on('click', (e) => {
     $mobileNav.toggleClass('open');
@@ -41,4 +48,8 @@ $toogleIcon
 $(document)
   .on('click', () => $mobileNav.removeClass('open'));
 $(window)
-  .on('resize', renderLoginBar);
+  .on('resize', renderNavbar);
+$searchIcon
+  .on('click', () => $searchForm.removeClass('search-form--hidden'));
+$closeSearchIcon
+  .on('click', () => $searchForm.addClass('search-form--hidden'));
