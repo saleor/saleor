7072a6ad07df0f555f916737137a9a1ca2ef547b
dominik-zeglen <flesz3@o2.pl>
2018-01-30 14:20:22 +0100

    Add service worker

--- saleor/static/js/components/misc.js
+++ saleor/static/js/components/misc.js
@@ -26,4 +26,15 @@ export default $(document).ready((e) => {
 
   let hash = window.location.hash;
   $('.nav-tabs a[href="' + hash + '"]').tab('show');
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/serviceworker.js')
+      .then((registration) => {
+        console.log('Registration successful, scope is:', registration.scope);
+      })
+      .catch((error) => {
+        console.log('Service worker registration failed, error:', error);
+      });
+  }
 });
