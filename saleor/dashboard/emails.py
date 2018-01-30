ebb5fb43910a96f5cebe0533bb23587f9701148d
Anna Siwik <anna.siwik@mirumee.com>
2018-01-30 15:27:04 +0100

    Use MJML for promote customer to staff confirmation e-mail

--- saleor/dashboard/emails.py
+++ saleor/dashboard/emails.py
@@ -10,11 +10,12 @@ from templated_email import send_templated_mail
 @shared_task
 def send_set_password_email(staff):
     site = Site.objects.get_current()
-    ctx = {'protocol': 'http',
-           'domain': site.domain,
-           'site_name': site.name,
-           'uid': urlsafe_base64_encode(force_bytes(staff.pk)).decode(),
-           'token': default_token_generator.make_token(staff)}
+    ctx = {
+        'protocol': 'http',
+        'domain': site.domain,
+        'site_name': site.name,
+        'uid': urlsafe_base64_encode(force_bytes(staff.pk)).decode(),
+        'token': default_token_generator.make_token(staff)}
     send_templated_mail(
         template_name='dashboard/staff/set_password',
         from_email=settings.DEFAULT_FROM_EMAIL,
@@ -25,7 +26,10 @@ def send_set_password_email(staff):
 @shared_task
 def send_promote_customer_to_staff_email(staff):
     site = Site.objects.get_current()
-    ctx = {'domain': site.domain, 'site_name': site.name}
+    ctx = {
+        'protocol': 'http',
+        'domain': site.domain,
+        'site_name': site.name}
     send_templated_mail(
         template_name='dashboard/staff/promote_customer',
         from_email=settings.DEFAULT_FROM_EMAIL,
