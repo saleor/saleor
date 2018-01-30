1dd0725344e6f44568a9a58a7ae8a58c2609433e
Marcin Gębala <maarcin.gebala@gmail.com>
2018-01-30 13:01:55 +0100

    Remove unnused function

--- saleor/graphql/utils.py
+++ saleor/graphql/utils.py
@@ -26,17 +26,3 @@ class DjangoPkInterface(graphene.Interface):
 
     def resolve_pk(self, info):
         return self.pk
-
-
-def get_object_or_none(klass, *args, **kwargs):
-    queryset = _get_queryset(klass)
-    try:
-        return queryset.get(*args, **kwargs)
-    except AttributeError:
-        klass__name = klass.__name__ if isinstance(klass, type) else klass.__class__.__name__
-        raise ValueError(
-            "First argument to get_object_or_none() must be a Model, Manager, "
-            "or QuerySet, not '%s'." % klass__name
-        )
-    except queryset.model.DoesNotExist:
-        return None
