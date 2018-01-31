17ea52fc760febde1484cbe2cc0da5c6012aaea7
dominik-zeglen <flesz3@o2.pl>
2018-01-31 12:47:57 +0100

    Add placeholder templatetag

--- saleor/core/urls.py
+++ saleor/core/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
         name='impersonate-start'),
     url(r'^impersonate/stop/$', views.stop_impersonate,
         name='impersonate-stop'),
-    url(r'^404', views.handle_404, name='handle-404')
+    url(r'^404', views.handle_404, name='handle-404'),
     url(r'^offline$', views.offline, name='offline'),
     url(r'^manifest\.json$', TemplateView.as_view(
         template_name='manifest.json', content_type='application/json')),
