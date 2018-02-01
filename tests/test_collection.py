b97bcdccd9bebc79ee083fa3ca90c7ee394c5313
akjanik <adam.janik@mirumee.com>
2018-02-01 13:26:29 +0100

    Add test

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -3,14 +3,7 @@ import pytest
 from django.urls import reverse
 
 from .utils import get_redirect_location
-from saleor.product.models import Collection
 from saleor.dashboard.collection.forms import CollectionForm
-
-
-@pytest.fixture
-def collection(db):
-    collection = Collection.objects.create(name="Collection", slug='collection')
-    return collection
 
 
 def test_list_view(admin_client, collection):
@@ -87,3 +80,11 @@ def test_collection_delete_view(admin_client, collection):
     response = admin_client.post(url)
     assert response.status_code == 302
     assert Collection.objects.count() == (collections_count - 1)
+
+from unittest.mock import Mock, MagicMock
+def test_collection_index_in_storefront(admin_client, collection):
+    collection.products = MagicMock()
+    response = admin_client.get(
+        reverse('product:collection',
+                kwargs={'pk': collection.id, 'slug': collection.slug}))
+    assert response.status_code == 200
