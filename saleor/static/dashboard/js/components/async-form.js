d6c64c63c47a992a2df4adb1bc3bfd18128dd06b
Rafał Pitoń <rafal@mirumee.com>
2018-02-01 15:44:00 +0100

    Improv js

--- saleor/static/dashboard/js/components/async-form.js
+++ saleor/static/dashboard/js/components/async-form.js
@@ -1,18 +1,17 @@
 import { initSelects } from './selects';
 
 export default $(document).on('submit', '.form-async', function (e) {
-  let that = this;
   $.ajax({
-    url: $(that).attr('action'),
+    url: $(e.currentTarget).attr('action'),
     method: 'post',
-    data: $(that).serialize(),
+    data: $(e.currentTarget).serialize(),
     success: function (data) {
-      let $message = $(data).find('div');
+      const $message = $(data).find('div');
       $message.find('a').remove();
-      $(that).find('.row').last().html($message);
-      $(that).find('.row').last().css('text-align', 'center');
-      $(that).find('svg').height('160px');
-      $(that).find('button').remove();
+      $(e.currentTarget).find('.row').last().html($message);
+      $(e.currentTarget).find('.row').last().css('text-align', 'center');
+      $(e.currentTarget).find('svg').height('160px');
+      $(e.currentTarget).find('button').remove();
     }
   });
   e.preventDefault();
