f751cbe63b4bf1f78d3f11c64837efe143b42f47
dominik-zeglen <flesz3@o2.pl>
2018-02-06 14:38:09 +0100

    Refactor style injection

--- saleor/static/dashboard/js/components/components/table.js
+++ saleor/static/dashboard/js/components/components/table.js
@@ -1,34 +1,27 @@
 import React from 'react';
-import { TableCell as MuiTableCell } from 'material-ui/Table';
+import { TableCell } from 'material-ui/Table';
 import { withStyles } from 'material-ui/styles';
 
 
-const styleFragments = {
-  tableCell: {
-    fontSize: '12px',
-  },
-};
 const styles = {
-  tableCell: {
-    root: {
-      ...styleFragments.tableCell,
-    },
-  },
   wideTableCell: {
-    root: {
-      ...styleFragments.tableCell,
-      width: '99%',
-    },
+    width: '99%',
   },
 };
-const TableCell = withStyles(styles.tableCell)((props) => {
-  return <MuiTableCell {...props}/>
-});
-const WideTableCell = withStyles(styles.wideTableCell)((props) => {
-  return <MuiTableCell {...props}/>
-});
 
-export {
-  TableCell,
-  WideTableCell,
-};
+export default withStyles(styles)(
+  (props) => {
+    let wideComponent = false;
+    if (props.wide) {
+      wideComponent = true;
+      delete props.wide;
+    }
+    const {classes, ...componentProps} = props;
+    return (
+      <TableCell
+        classes={wideComponent ? {root: classes.wideTableCell} : {}}
+        {...componentProps}
+      />
+    );
+  }
+);
