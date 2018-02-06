b9a6a77784e43c582435a4b4fdd3511af57ca7b4
dominik-zeglen <flesz3@o2.pl>
2018-02-06 17:06:33 +0100

    Add proptypes

--- saleor/static/dashboard/js/components/app/category/details/index.js
+++ saleor/static/dashboard/js/components/app/category/details/index.js
@@ -1,22 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import Description from './description';
 import Subcategories from './subcategoryList';
 
-class CategoryDetails extends Component {
-  render() {
-    return (
-      <div>
-        {this.props.category && (
-          <Description category={this.props.category} />
-        )}
-        <Subcategories
-          category={this.props.category}
-          categoryChildren={this.props.categoryChildren}
-        />
-      </div>
-    );
-  }
-}
+const CategoryDetails = (props) => (
+  <div>
+    {props.category && (
+      <Description category={props.category} />
+    )}
+    <Subcategories
+      category={props.category}
+      categoryChildren={props.categoryChildren}
+    />
+  </div>
+);
+CategoryDetails.propTypes = {
+  data: PropTypes.shape({
+    category: PropTypes.shape({
+      pk: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      parent: PropTypes.shape({
+        pk: PropTypes.number
+      })
+    }),
+    categoryChildren: PropTypes.arrayOf(PropTypes.shape({
+      pk: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string
+    }))
+  })
+};
 
 export default CategoryDetails;
