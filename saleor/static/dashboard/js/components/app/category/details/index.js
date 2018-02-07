0a226b98620aeaecd50d70cf2879994b15fb5aa9
dominik-zeglen <flesz3@o2.pl>
2018-02-07 14:42:44 +0100

    Refactor code

--- saleor/static/dashboard/js/components/app/category/details/index.js
+++ saleor/static/dashboard/js/components/app/category/details/index.js
@@ -7,35 +7,13 @@ import Subcategories from './subcategoryList';
 const CategoryDetails = (props) => (
   <div>
     {props.pk && (
-      <Description
-        pk={props.pk}
-        setLoadingStatus={props.setLoadingStatus}
-      />
+      <Description pk={props.pk} />
     )}
-    <Subcategories
-      pk={props.pk}
-      setLoadingStatus={props.setLoadingStatus}
-    />
+    <Subcategories pk={props.pk} />
   </div>
 );
 CategoryDetails.propTypes = {
-  pk: PropTypes.int,
-  setLoadingStatus: PropTypes.func.isRequired
+  pk: PropTypes.int
 };
-// CategoryDetails.propTypes = {
-//   category: PropTypes.shape({
-//     pk: PropTypes.number,
-//     name: PropTypes.string,
-//     description: PropTypes.string,
-//     parent: PropTypes.shape({
-//       pk: PropTypes.number
-//     })
-//   }),
-//   categoryChildren: PropTypes.arrayOf(PropTypes.shape({
-//     pk: PropTypes.number,
-//     name: PropTypes.string,
-//     description: PropTypes.string
-//   }))
-// };
 
 export default CategoryDetails;
--- saleor/static/dashboard/js/components/app/category/edit.js
+++ saleor/static/dashboard/js/components/app/category/edit.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { withStyles } from 'material-ui/styles';
 import Card, { CardContent, CardActions } from 'material-ui/Card';
@@ -26,42 +27,70 @@ function handleBack(history) {
   return () => history.goBack();
 }
 
+const Component = (props) => (
+  <Card>
+    <CardContent>
+      <div className={props.classes.inputContainer}>
+        <TextField name={'name'}
+                   label={'Name'}
+                   defaultValue={props.category ? props.category.name : ''}
+                   className={props.classes.largeTextInput}
+                   required
+        />
+      </div>
+      <div className={props.classes.inputContainer}>
+        <TextField name={'description'}
+                   label={'Description (optional)'}
+                   defaultValue={props.category ? props.category.description : ''}
+                   multiline
+        />
+      </div>
+    </CardContent>
+    <CardActions classes={{ root: props.classes.cardActions }}>
+      <Button color="secondary"
+              onClick={handleBack(props.history)}
+      >
+        Anuluj
+      </Button>
+      <Button
+        variant="raised"
+        color="secondary"
+        onClick={handleBack(props.history)}
+      >
+        Aktualizuj
+      </Button>
+    </CardActions>
+  </Card>
+);
+Component.propTypes = {
+  pk: PropTypes.number.isRequired,
+  data: PropTypes.shape({
+    category: PropTypes.shape({
+      pk: PropTypes.number,
+      name: PropTypes.string,
+      description: PropTypes.string,
+      parent: PropTypes.shape({
+        pk: PropTypes.number
+      })
+    }),
+    loading: PropTypes.bool
+  })
+}
+const query = gql`
+  query CategoryDetails($pk: Int!) {
+    category(pk: $pk) {
+      pk
+      name
+      description
+      parent {
+        pk
+      }
+    }
+  }
+`;
+
 export default withStyles(styles)(
   withRouter(
-    (props) => (
-      <Card>
-        <CardContent>
-          <div className={props.classes.inputContainer}>
-            <TextField name={'name'}
-              label={'Name'}
-              defaultValue={props.category ? props.category.name : ''}
-              className={props.classes.largeTextInput}
-              required
-            />
-          </div>
-          <div className={props.classes.inputContainer}>
-            <TextField name={'description'}
-              label={'Description (optional)'}
-              defaultValue={props.category ? props.category.description : ''}
-              multiline
-            />
-          </div>
-        </CardContent>
-        <CardActions classes={{ root: props.classes.cardActions }}>
-          <Button color="secondary"
-            onClick={handleBack(props.history)}
-          >
-            Anuluj
-          </Button>
-          <Button
-            variant="raised"
-            color="secondary"
-            onClick={handleBack(props.history)}
-          >
-            Aktualizuj
-          </Button>
-        </CardActions>
-      </Card>
-    )
+    Component
   )
 );
