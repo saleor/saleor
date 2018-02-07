40736b19cc5f6c14e5364ee78678d0dfe31940a5
dominik-zeglen <flesz3@o2.pl>
2018-02-07 14:42:44 +0100

    Refactor code

--- saleor/static/dashboard/js/components/app/category/index.js
+++ saleor/static/dashboard/js/components/app/category/index.js
@@ -1,70 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Switch } from 'react-router-dom';
 
 import CategoryEdit from './edit';
 import CategoryDetails from './details/index';
 
-class CategorySection extends Component {
-  static propTypes = {
-    match: PropTypes.object
-  };
+const Component = () => {
+  const CategoryEditComponent = () => (
+    <CategoryEdit pk={this.props.match.params.pk} />
+  );
+  const CategoryDetailsComponent = () => (
+    <CategoryDetails pk={this.props.match.params.pk} />
+  );
 
-  constructor(props) {
-    super(props);
-    this.state = { loading: true };
-  }
+  return (
+    <div>
+      <Switch>
+        <Route
+          exact
+          path={'/categories/:pk/edit'}
+          component={CategoryEditComponent}
+        />
+        <Route
+          exact
+          path={'/categories/:pk/add'}
+          component={CategoryEdit}
+        />
+        <Route
+          exact
+          path={'/categories/add'}
+          component={CategoryEdit}
+        />
+        <Route
+          exact
+          path={'/categories/:pk'}
+          component={CategoryDetailsComponent}
+        />
+        <Route
+          exact
+          path={'/categories'}
+          component={CategoryDetailsComponent}
+        />
+      </Switch>
+    </div>
+  );
+};
+Component.propTypes = {
+  match: PropTypes.object,
+};
 
-  setLoadingStatus(status) {
-    this.setState({ loading: status });
-  }
-
-  render() {
-    const CategoryEditComponent = () => (
-      <CategoryEdit
-        pk={this.props.match.params.pk}
-        setLoadingStatus={this.setLoadingStatus}
-      />
-    );
-    const CategoryDetailsComponent = () => (
-      <CategoryDetails
-        pk={this.props.match.params.pk}
-        setLoadingStatus={this.setLoadingStatus}
-      />
-    );
-
-    return (
-      <div>
-        <Switch>
-          <Route
-            exact
-            path={'/categories/:pk/edit'}
-            component={CategoryEditComponent}
-          />
-          <Route
-            exact
-            path={'/categories/:pk/add'}
-            component={CategoryEdit}
-          />
-          <Route
-            exact
-            path={'/categories/add'}
-            component={CategoryEdit}
-          />
-          <Route
-            exact
-            path={'/categories/:pk'}
-            component={CategoryDetailsComponent}
-          />
-          <Route
-            exact
-            path={'/categories'}
-            component={CategoryDetailsComponent}
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
-
-export default CategorySection;
+export default Component;
