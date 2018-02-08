ee9174487f08f71a1127f2d1109d45d53a5fc4a9
dominik-zeglen <flesz3@o2.pl>
2018-02-08 18:40:28 +0100

    Add category removing

--- saleor/static/dashboard/js/components/app/category/details/description.js
+++ saleor/static/dashboard/js/components/app/category/details/description.js
@@ -1,46 +1,90 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import Card, { CardContent, CardActions } from 'material-ui/Card';
 import Button from 'material-ui/Button';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 import { CircularProgress } from 'material-ui/Progress';
 
 import { CardTitle, CardSubtitle } from '../../../components/cards';
+import { ConfirmRemoval } from '../../../components/modals';
 
-const Component = (props) => (
-  <div>
-    {props.data.loading && (
-      <CircularProgress
-        size={80}
-        thickness={5}
-        style={{ margin: 'auto' }}
-      />
-    )}
-    {!props.data.loading && (
-      <Card>
-        <CardContent>
-          <CardTitle>
-            {props.data.category.name}
-          </CardTitle>
-          <CardSubtitle>
-            Opis
-          </CardSubtitle>
-          {props.data.category.description}
-          <CardActions>
-            <Link to={`/categories/${props.data.category.pk}/edit/`}>
-              <Button color={'secondary'}>Edytuj</Button>
-            </Link>
-            <Link to={`/categories/${props.data.category.parent ? props.data.category.parent.pk : ''}`}>
-              <Button color={'secondary'}>Usuń</Button>
-            </Link>
-          </CardActions>
-        </CardContent>
-      </Card>
-    )}
-  </div>
-);
+class CategoryDescription extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { opened: false };
+    this.handleModalOpen = this.handleModalOpen.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
+    this.handleRemoval = this.handleRemoval.bind(this);
+  }
+
+  handleModalOpen() {
+    this.setState({ opened: true });
+  }
+
+  handleModalClose() {
+    this.setState({ opened: false });
+  }
+
+  handleRemoval() {
+    const parentPk = this.props.data.category.parent.pk;
+    this.props.mutate({
+      pk: this.props.data.category.pk
+    }).then(this.handleModalClose)
+      .then(() => this.props.history.push(`/categories/${parentPk}/`))
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.data.loading && (
+          <CircularProgress
+            size={80}
+            thickness={5}
+            style={{ margin: 'auto' }}
+          />
+        )}
+        {!this.props.data.loading && (
+          <div>
+            <Card>
+              <CardContent>
+                <CardTitle>
+                  {this.props.data.category.name}
+                </CardTitle>
+                <CardSubtitle>
+                  Opis
+                </CardSubtitle>
+                {this.props.data.category.description}
+                <CardActions>
+                  <Link to={`/categories/${this.props.data.category.pk}/edit/`}>
+                    <Button color={'secondary'}>Edytuj</Button>
+                  </Link>
+                  {/*<Link to={`/categories/${props.data.category.parent ? props.data.category.parent.pk : ''}`}>*/}
+                  {/*<Button color={'secondary'}>Usuń</Button>*/}
+                  {/*</Link>*/}
+                  <Button
+                    color={'secondary'}
+                    onClick={this.handleModalOpen}
+                  >
+                    Usuń
+                  </Button>
+                </CardActions>
+              </CardContent>
+            </Card>
+            <ConfirmRemoval
+              open={this.state.opened}
+              onClose={this.handleModalClose}
+              onConfirm={this.handleRemoval}
+              content={`Are you sure you want to delete category ${this.props.data.category.name}?`}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
 const query = gql`
   query CategoryDetails($pk: Int!) {
     category(pk: $pk) {
@@ -51,6 +95,13 @@ const query = gql`
       parent {
         pk
       }
+    }
+  }
+`;
+const mutation = gql`
+  mutation CategoryDelete($pk: Int!) {
+    categoryDelete(pk: $pk) {
+      errors
     }
   }
 `;
@@ -70,8 +121,19 @@ Component.propTypes = {
   })
 };
 
-export default graphql(query, {
-  options: (props) => ({
-    pk: props.pk
+export default compose(
+  withRouter,
+  graphql(query, {
+    options: (props) => ({
+      pk: props.pk
+    })
+  }),
+  graphql(mutation, {
+    options: {
+      refetchQueries: [
+        'CategoryPage',
+        'CategoryDetails',
+      ]
+    }
   })
-})(Component);
+)(CategoryDescription);
