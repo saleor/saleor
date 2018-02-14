b8e80f6e180dfc3ddf4e373fc84777a825840876
dominik-zeglen <flesz3@o2.pl>
2018-02-14 13:50:43 +0100

    Improve contenerization

--- saleor/static/dashboard/js/components/app/category/queries.js
+++ saleor/static/dashboard/js/components/app/category/queries.js
@@ -1,9 +1,10 @@
 import gql from 'graphql-tag';
 
 const categoryChildren = gql`
-  query CategoryChildren ($pk: Int) {
-    categories(parent: $pk) {
+  query CategoryChildren ($pk: Int, $first: Int, $after: String, $last: Int, $before: String) {
+    categories(parent: $pk, first: $first, after: $after, last: $last, before: $before) {
       edges {
+        cursor
         node {
           id
           pk
