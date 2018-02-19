73eb6ee8252d4e788a9405ac0bb90ee478efc033
Patryk Zawadzki <patrys@room-303.com>
2018-02-19 13:55:58 +0100

    Make GraphQL connections countable
    Have all ORM-backed connection fields support `totalCount`.

--- saleor/graphql/core/types.py
+++ saleor/graphql/core/types.py
@@ -1,5 +1,31 @@
 import graphene
 from django_prices.templatetags import prices_i18n
+from graphene_django import DjangoObjectType
+
+
+class CountableConnection(graphene.relay.Connection):
+    class Meta:
+        abstract = True
+
+    total_count = graphene.Int()
+
+    @staticmethod
+    def resolve_total_count(root, info, *args, **kwargs):
+        return root.length
+
+
+class CountableDjangoObjectType(DjangoObjectType):
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def __init_subclass_with_meta__(cls, *args, **kwargs):
+        # Force it to use the countable connection
+        countable_conn = CountableConnection.create_type(
+            "{}CountableConnection".format(cls.__name__),
+            node=cls)
+        super().__init_subclass_with_meta__(
+            *args, connection=countable_conn, **kwargs)
 
 
 class Price(graphene.ObjectType):
