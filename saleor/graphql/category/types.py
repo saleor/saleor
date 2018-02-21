d27fe68d891d23a53e32efca8fc5c6ad2001c47b
Marcin Gębala <maarcin.gebala@gmail.com>
2018-02-21 14:45:31 +0100

    Remove unused code

--- saleor/graphql/category/types.py
+++
@@ -1,8 +0,0 @@
-from ...product.models import Category
-
-
-def resolve_categories(parent, info):
-    if parent == -1:
-        parent = None
-    categories = Category.objects.filter(parent=parent).distinct()
-    return categories
