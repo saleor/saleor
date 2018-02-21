29eae20a063fb976de40ca977289db51846d88c8
akjanik <adam.janik@mirumee.com>
2018-02-21 17:23:17 +0100

    Implement visibility and availability date

--- saleor/page/models.py
+++ saleor/page/models.py
@@ -1,13 +1,17 @@
+import datetime
+
 from django.db import models
+from django.db.models import Q
 from django.urls import reverse
 from django.core.validators import validate_slug
-
-from . import PageStatus
 
 
 class PageManager(models.QuerySet):
     def public(self):
-        return self.filter(status=PageStatus.PUBLIC)
+        today = datetime.date.today()
+        return self.filter(
+            Q(visible=True),
+            Q(available_on__lte=today) | Q(available_on__isnull=True))
 
     def get_available(self, allow_draft=False):
         if not allow_draft:
@@ -21,8 +25,8 @@ class Page(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(
-        max_length=255, choices=PageStatus.CHOICES, default=PageStatus.DRAFT)
+    visible = models.BooleanField(default=False)
+    available_on = models.DateField(blank=True, null=True)
 
     objects = PageManager.as_manager()
 
@@ -34,9 +38,6 @@ class Page(models.Model):
 
     def get_absolute_url(self):
         return reverse('page:details', kwargs={'url': self.url})
-
-    def is_public(self):
-        return self.status == PageStatus.PUBLIC
 
     def save(self, *args, **kwargs):
         """
