39dd211582ffdf8ababa716c19e1bbf6fc320d1f
dominik-zeglen <flesz3@o2.pl>
2018-02-22 13:36:08 +0100

    Enable React to handle routes on it's own

--- saleor/dashboard/category/urls.py
+++ saleor/dashboard/category/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',
+    url(r'^',
         views.category_list, name='category-list'),
     url(r'^(?P<pk>[0-9]+)/$',
         views.category_detail, name='category-detail'),
