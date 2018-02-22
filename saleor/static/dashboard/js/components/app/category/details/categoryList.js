fd446f60b22b7500e65cfc77c4e83147e765aaf8
dominik-zeglen <flesz3@o2.pl>
2018-02-22 16:57:46 +0100

    Refactor code

--- saleor/static/dashboard/js/components/app/category/details/categoryList.js
+++ saleor/static/dashboard/js/components/app/category/details/categoryList.js
@@ -20,76 +20,7 @@ const tableHeaders = [
 ];
 const PAGINATE_BY = 5;
 
-function handleAddAction() {
-  this.props.history.push('add');
-}
-function handleChangePage(firstCursor, lastCursor) {
-  return (event, currentPage) => {
-    const prevPage = parseQs(this.props.location.search.substr(1)).currentPage || 0;
-    const action = (parseInt(prevPage) < parseInt(currentPage)) ? 'next' : 'prev';
-    this.props.history.push({
-      search: createQueryString(this.props.location.search, {
-        firstCursor,
-        lastCursor,
-        currentPage,
-        action
-      })
-    });
-  };
-}
-function handleChangeRowsPerPage(event) {
-  this.props.history.push({
-    search: createQueryString(this.props.location.search, {
-      rowsPerPage: event.target.value,
-      firstCursor: null,
-      lastCursor: null,
-      currentPage: 0,
-      action: 'next'
-    })
-  });
-}
-
-class CategoryListComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChangePage = handleChangePage.bind(this);
-    this.handleChangeRowsPerPage = handleChangeRowsPerPage.bind(this);
-    this.handleAddAction = handleAddAction.bind(this);
-  }
-
-  render() {
-    const { data } = this.props;
-    const categories = data.category ? data.category.children : [];
-    const qs = parseQs(this.props.location.search.substr(1));
-    const firstCursor = (!data.loading && categories.edges.length) ? categories.edges[0].cursor : '';
-    const lastCursor = (!data.loading && categories.edges.length) ? categories.edges.slice(-1)[0].cursor : '';
-    return (
-      <div>
-        {data.loading ? (
-          <span>loading</span>
-        ) : (
-          <ListCard
-            displayLabel={true}
-            label={pgettext('Category list card title', 'Subcategories')}
-            addActionLabel={pgettext('Category list add category action', 'Add category')}
-            headers={tableHeaders}
-            list={categories.edges.map(edge => edge.node)}
-            handleAddAction={this.handleAddAction}
-            handleChangePage={this.handleChangePage}
-            handleChangeRowsPerPage={this.handleChangeRowsPerPage}
-            page={parseInt(qs.currentPage) || 0}
-            rowsPerPage={qs.rowsPerPage || PAGINATE_BY}
-            count={categories.totalCount}
-            firstCursor={firstCursor}
-            lastCursor={lastCursor}
-            noDataLabel={pgettext('Category list no categories found', 'No categories found')}
-          />
-        )}
-      </div>
-    );
-  }
-}
-const CategoryList = withRouter(graphql(categoryChildren, {
+const feederOptions = {
   options: (props) => {
     const qs = parseQs(props.location.search.substr(1));
     const options = {
@@ -128,16 +59,80 @@ const CategoryList = withRouter(graphql(categoryChildren, {
     options.variables = Object.assign({}, options.variables, variables);
     return options;
   }
-})(CategoryListComponent));
+};
+const categoryListFeeder = graphql(categoryChildren, feederOptions);
+const rootCategoryListFeeder = graphql(rootCategoryChildren, feederOptions);
 
-class RootCategoryListComponent extends Component {
-  constructor(props) {
-    super(props);
-    this.handleChangePage = handleChangePage.bind(this);
-    this.handleChangeRowsPerPage = handleChangeRowsPerPage.bind(this);
-    this.handleAddAction = handleAddAction.bind(this);
+class BaseCategoryList extends Component {
+  handleAddAction = () => {
+    this.props.history.push('add');
+  };
+  handleChangePage = (firstCursor, lastCursor) => {
+    return (event, currentPage) => {
+      const prevPage = parseQs(this.props.location.search.substr(1)).currentPage || 0;
+      const action = (parseInt(prevPage) < parseInt(currentPage)) ? 'next' : 'prev';
+      this.props.history.push({
+        search: createQueryString(this.props.location.search, {
+          firstCursor,
+          lastCursor,
+          currentPage,
+          action
+        })
+      });
+    };
+  };
+  handleChangeRowsPerPage = (event) => {
+    this.props.history.push({
+      search: createQueryString(this.props.location.search, {
+        rowsPerPage: event.target.value,
+        firstCursor: null,
+        lastCursor: null,
+        currentPage: 0,
+        action: 'next'
+      })
+    });
+  };
+}
+
+@withRouter
+@categoryListFeeder
+class CategoryList extends BaseCategoryList {
+  render() {
+    const { data } = this.props;
+    const categories = data.category ? data.category.children : [];
+    const qs = parseQs(this.props.location.search.substr(1));
+    const firstCursor = (!data.loading && categories.edges.length) ? categories.edges[0].cursor : '';
+    const lastCursor = (!data.loading && categories.edges.length) ? categories.edges.slice(-1)[0].cursor : '';
+    return (
+      <div>
+        {data.loading ? (
+          <span>loading</span>
+        ) : (
+          <ListCard
+            displayLabel={true}
+            label={pgettext('Category list card title', 'Subcategories')}
+            addActionLabel={pgettext('Category list add category action', 'Add category')}
+            headers={tableHeaders}
+            list={categories.edges.map(edge => edge.node)}
+            handleAddAction={this.handleAddAction}
+            handleChangePage={this.handleChangePage}
+            handleChangeRowsPerPage={this.handleChangeRowsPerPage}
+            page={parseInt(qs.currentPage) || 0}
+            rowsPerPage={parseInt(qs.rowsPerPage) || PAGINATE_BY}
+            count={categories.totalCount}
+            firstCursor={firstCursor}
+            lastCursor={lastCursor}
+            noDataLabel={pgettext('Category list no categories found', 'No categories found')}
+          />
+        )}
+      </div>
+    );
   }
+}
 
+@withRouter
+@rootCategoryListFeeder
+class RootCategoryList extends BaseCategoryList {
   render() {
     const { data } = this.props;
     const qs = parseQs(this.props.location.search.substr(1));
@@ -155,7 +150,7 @@ class RootCategoryListComponent extends Component {
             handleChangePage={this.handleChangePage}
             handleChangeRowsPerPage={this.handleChangeRowsPerPage}
             page={parseInt(qs.currentPage) || 0}
-            rowsPerPage={qs.rowsPerPage || PAGINATE_BY}
+            rowsPerPage={parseInt(qs.rowsPerPage) || PAGINATE_BY}
             count={data.categories.totalCount}
             firstCursor={firstCursor}
             lastCursor={lastCursor}
@@ -166,44 +161,6 @@ class RootCategoryListComponent extends Component {
     );
   }
 }
-const RootCategoryList = withRouter(graphql(rootCategoryChildren, {
-  options: (props) => {
-    const qs = parseQs(props.location.search.substr(1));
-    const options = {
-      variables: {},
-      fetchPolicy: 'network-only'
-    };
-    let variables;
-    switch (qs.action) {
-      case 'prev':
-        variables = {
-          last: parseInt(qs.rowsPerPage) || PAGINATE_BY,
-          before: qs.firstCursor,
-          first: null,
-          after: null
-        };
-        break;
-      case 'next':
-        variables = {
-          first: parseInt(qs.rowsPerPage) || PAGINATE_BY,
-          after: qs.lastCursor,
-          last: null,
-          before: null
-        };
-        break;
-      default:
-        variables = {
-          first: parseInt(qs.rowsPerPage) || PAGINATE_BY,
-          after: qs.lastCursor,
-          last: null,
-          before: null
-        };
-        break;
-    }
-    options.variables = Object.assign({}, options.variables, variables);
-    return options;
-  }
-})(RootCategoryListComponent));
 
 export {
   CategoryList,
