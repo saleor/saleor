cf207719ff3a867aaa0fb9239628f36ee5f50e25
Adam Janik <adam.janik@mirumee.com>
2018-02-26 17:32:50 +0100

    Refactor urls

--- saleor/page/utils.py
+++ saleor/page/utils.py
@@ -4,4 +4,4 @@ from .models import Page
 def pages_visible_to_user(user):
     if user.is_authenticated and user.is_active and user.is_staff:
         return Page.objects.all()
-    return
+    return Page.objects.public()
