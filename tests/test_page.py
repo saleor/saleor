91ea643d26a74a10e009fdbd3bf812bfa5387f96
Adam Janik <adam.janik@mirumee.com>
2018-02-26 12:34:43 +0100

    Adjust tests

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,19 +1,19 @@
 from saleor.page.models import Page
 
 
-def test_draft_page_detail(client, admin_client):
-    url = '/page_draft'
-    Page.objects.get_or_create(url=url, title='Draft', status=Page.DRAFT)
-    response = client.get(url)
+def test_draft_page_detail(client, admin_client, page):
+    page_url = page.get_absolute_url()
+    response = client.get(page_url)
     assert response.status_code == 404
-    response = admin_client.get(url)
+    response = admin_client.get(page_url)
     assert response.status_code == 200
 
 
-def test_public_page_detail(client, admin_client):
-    url = '/page'
-    Page.objects.get_or_create(url=url, title='Public', status=Page.PUBLIC)
-    response = client.get(url)
+def test_public_page_detail(client, admin_client, page):
+    page.visible = True
+    page.save()
+    page_url = page.get_absolute_url()
+    response = client.get(page_url)
     assert response.status_code == 200
-    response = admin_client.get(url)
+    response = admin_client.get(page_url)
     assert response.status_code == 200
