4fb81a86a8838a256a06db688a3e2a06dfaabe65
Patryk Zawadzki <patrys@room-303.com>
2018-03-01 16:46:51 +0100

    Apply prettier

--- saleor/static/dashboard-next/js/category/form/categoryUpdateForm.js
+++ saleor/static/dashboard-next/js/category/form/categoryUpdateForm.js
@@ -1,25 +1,27 @@
-import React, { Component, Fragment } from 'react';
-import Grid from 'material-ui/Grid';
-import PropTypes from 'prop-types';
-import { graphql } from 'react-apollo';
-import { withRouter } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import Grid from "material-ui/Grid";
+import PropTypes from "prop-types";
+import { graphql } from "react-apollo";
+import { withRouter } from "react-router-dom";
 
-import BaseCategoryForm from './base';
-import { categoryDetails } from '../queries';
-import { categoryUpdate } from '../mutations';
+import BaseCategoryForm from "./base";
+import { categoryDetails } from "../queries";
+import { categoryUpdate } from "../mutations";
 
 @withRouter
 @graphql(categoryUpdate)
-@graphql(categoryDetails, { options: props => ({ variables: { id: props.match.params.id } }) })
+@graphql(categoryDetails, {
+  options: props => ({ variables: { id: props.match.params.id } })
+})
 class CategoryUpdateForm extends Component {
   static propTypes = {
     data: PropTypes.shape({
       category: PropTypes.object,
-      loading: PropTypes.bool,
+      loading: PropTypes.bool
     }),
     history: PropTypes.object,
     match: PropTypes.object,
-    mutate: PropTypes.func,
+    mutate: PropTypes.func
   };
 
   constructor(props) {
@@ -29,13 +31,18 @@ class CategoryUpdateForm extends Component {
 
   handleConfirm(formData) {
     return () => {
-      this.props.mutate({
-        variables: {
-          ...formData,
-          id: this.props.match.params.id,
-        },
-      })
-        .then(({ data }) => this.props.history.push(`/categories/${data.categoryUpdate.category.id}/`))
+      this.props
+        .mutate({
+          variables: {
+            ...formData,
+            id: this.props.match.params.id
+          }
+        })
+        .then(({ data }) =>
+          this.props.history.push(
+            `/categories/${data.categoryUpdate.category.id}/`
+          )
+        )
         .catch(error => console.error(error));
     };
   }
@@ -45,18 +52,24 @@ class CategoryUpdateForm extends Component {
     return (
       <Fragment>
         {loading || (
-        <Grid container spacing={16}>
-          <Grid item xs={12} md={9}>
-            <BaseCategoryForm
-              title={pgettext('Edit category form card title', 'Edit category')}
-              name={category.name}
-              description={category.description}
-              handleConfirm={this.handleConfirm}
-              confirmButtonLabel={pgettext('Dashboard update action', 'Update')}
-            />
+          <Grid container spacing={16}>
+            <Grid item xs={12} md={9}>
+              <BaseCategoryForm
+                title={pgettext(
+                  "Edit category form card title",
+                  "Edit category"
+                )}
+                name={category.name}
+                description={category.description}
+                handleConfirm={this.handleConfirm}
+                confirmButtonLabel={pgettext(
+                  "Dashboard update action",
+                  "Update"
+                )}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-      )}
+        )}
       </Fragment>
     );
   }
--- saleor/static/dashboard-next/js/theme.js
+++ saleor/static/dashboard-next/js/theme.js
@@ -1,109 +1,109 @@
-import { createMuiTheme } from 'material-ui/styles/index';
-import grey from 'material-ui/colors/grey';
+import { createMuiTheme } from "material-ui/styles/index";
+import grey from "material-ui/colors/grey";
 
-const transition = '200ms';
+const transition = "200ms";
 export default createMuiTheme({
   palette: {
     primary: {
-      main: '#2bb673',
+      main: "#2bb673"
     },
     secondary: {
-      main: '#26A5D4',
-    },
+      main: "#26A5D4"
+    }
   },
   typography: {
     htmlFontSize: 16,
-    fontFamily: 'Roboto, sans-serif',
+    fontFamily: "Roboto, sans-serif"
   },
   overrides: {
     MuiButton: {
       root: {
         fontWeight: 400,
-        fontSize: '0.875rem',
+        fontSize: "0.875rem"
       },
       raised: {
-        color: '#ffffff !important',
-      },
+        color: "#ffffff !important"
+      }
     },
     MuiTableCell: {
       root: {
-        paddingRight: '1.5rem !important',
-        '&:first-child': {
-          whiteSpace: 'nowrap',
-        },
+        paddingRight: "1.5rem !important",
+        "&:first-child": {
+          whiteSpace: "nowrap"
+        }
       },
       typeHead: {
-        fontWeight: 400,
-      },
+        fontWeight: 400
+      }
     },
     MuiTextField: {
       inkbar: {
-        '&:after': {
-          backgroundColor: '#26A5D4',
-        },
-      },
+        "&:after": {
+          backgroundColor: "#26A5D4"
+        }
+      }
     },
     MuiInput: {
       root: {
-        fontSize: '0.875rem',
-      },
+        fontSize: "0.875rem"
+      }
     },
     MuiFormLabel: {
       root: {
-        fontSize: '0.875rem',
-      },
+        fontSize: "0.875rem"
+      }
     },
     MuiPaper: {
       root: {
-        marginTop: '1rem',
-      },
+        marginTop: "1rem"
+      }
     },
     MuiCardActions: {
       root: {
         borderTop: `1px solid ${grey[300]}`,
-        margin: '24px -16px -24px',
-        padding: '0 8px',
-        '@media (max-width: 480px)': {
-          padding: '32px 8px',
-        },
-      },
+        margin: "24px -16px -24px",
+        padding: "0 8px",
+        "@media (max-width: 480px)": {
+          padding: "32px 8px"
+        }
+      }
     },
     MuiTableRow: {
       root: {
         transition,
-        '&:hover': {
-          backgroundColor: grey[100],
-        },
+        "&:hover": {
+          backgroundColor: grey[100]
+        }
       },
       typeHead: {
-        '&:hover': {
-          backgroundColor: 'inherit',
-        },
+        "&:hover": {
+          backgroundColor: "inherit"
+        }
       },
       typeFooter: {
-        '&:hover': {
-          backgroundColor: 'inherit',
-        },
-      },
+        "&:hover": {
+          backgroundColor: "inherit"
+        }
+      }
     },
     MuiCircularProgress: {
       root: {
-        display: 'block',
-        margin: '0 auto',
-      },
+        display: "block",
+        margin: "0 auto"
+      }
     },
     MuiTypography: {
       display1: {
-        fontSize: '1.5rem',
-        marginBottom: '1rem',
+        fontSize: "1.5rem",
+        marginBottom: "1rem",
         fontWeight: 300,
-        color: '#000000',
+        color: "#000000"
       },
       title: {
-        fontSize: '1rem',
+        fontSize: "1rem",
         fontWeight: 400,
-        marginBottom: '.75rem',
-      },
-    },
-  },
+        marginBottom: ".75rem"
+      }
+    }
+  }
 });
