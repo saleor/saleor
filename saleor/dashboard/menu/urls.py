982cca97e41b6b6339f96f24c47c1b550197f6cb
Anna Siwik <anna.siwik@mirumee.com>
2018-03-02 19:25:14 +0100

    Create menu item action

--- saleor/dashboard/menu/urls.py
+++ saleor/dashboard/menu/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/delete/$',
         views.menu_delete, name='menu-delete'),
     url(r'^(?P<pk>[0-9]+)/$',
-        views.menu_detail, name='menu-detail')]
+        views.menu_detail, name='menu-detail'),
+
+    url(r'^(?P<menu_pk>[0-9]+)/add/$',
+        views.menu_item_create, name='menu-item-add'),
+    url(r'^(?P<menu_pk>[0-9]+)/item/(?P<root_pk>[0-9]+)/add/$',
+        views.menu_item_create, name='menu-item-add')]
