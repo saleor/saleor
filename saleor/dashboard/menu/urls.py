5a4300d5192be4bd6e41cc280bf93e10ae1c5f6f
Anna Siwik <anna.siwik@mirumee.com>
2018-03-02 17:31:13 +0100

    Menu edit action

--- saleor/dashboard/menu/urls.py
+++ saleor/dashboard/menu/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
         views.menu_list, name='menu-list'),
     url(r'^add/$',
         views.menu_create, name='menu-add'),
+    url(r'^(?P<pk>[0-9]+)/edit/$',
+        views.menu_edit, name='menu-edit'),
     url(r'^(?P<pk>[0-9]+)/$',
         views.menu_detail, name='menu-detail')]
