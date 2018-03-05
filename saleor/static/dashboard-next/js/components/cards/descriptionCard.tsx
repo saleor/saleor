96d29f3962b7d2db86342b275fbbbdfd74f88cb2
Patryk Zawadzki <patrys@room-303.com>
2018-03-05 10:03:56 +0100

    Add new table pagination

--- saleor/static/dashboard-next/js/components/cards/descriptionCard.tsx
+++ saleor/static/dashboard-next/js/components/cards/descriptionCard.tsx
@@ -15,46 +15,40 @@ interface DescriptionCardProps {
   title: string;
 }
 
-const defaultProps = {
-  description: undefined,
+export const DescriptionCard: React.StatelessComponent<
+  DescriptionCardProps
+> = ({
+  description,
+  descriptionTextLabel,
+  editButtonLabel,
+  handleEditButtonClick,
+  handleRemoveButtonClick,
+  removeButtonLabel,
+  title
+}) => (
+  <div>
+    <Card>
+      <CardContent>
+        <Typography variant="display1">{title}</Typography>
+        <Typography variant="title">{descriptionTextLabel}</Typography>
+        <Typography>{description}</Typography>
+        <CardActions>
+          <Button color="secondary" onClick={handleEditButtonClick}>
+            {editButtonLabel}
+          </Button>
+          <Button color="secondary" onClick={handleRemoveButtonClick}>
+            {removeButtonLabel}
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
+  </div>
+);
+DescriptionCard.defaultProps = {
   descriptionTextLabel: pgettext(
     "Description card widget description text label",
     "Description"
   ),
   editButtonLabel: pgettext("Category edit action", "Edit"),
-  removeButtonLabel: pgettext("Category list action link", "Remove"),
-  title: undefined
-};
-
-export const DescriptionCard: React.StatelessComponent<DescriptionCardProps> = (
-  props = defaultProps
-) => {
-  const {
-    description,
-    descriptionTextLabel,
-    editButtonLabel,
-    handleEditButtonClick,
-    handleRemoveButtonClick,
-    removeButtonLabel,
-    title
-  } = props;
-  return (
-    <div>
-      <Card>
-        <CardContent>
-          <Typography variant="display1">{title}</Typography>
-          <Typography variant="title">{descriptionTextLabel}</Typography>
-          <Typography>{description}</Typography>
-          <CardActions>
-            <Button color="secondary" onClick={handleEditButtonClick}>
-              {editButtonLabel}
-            </Button>
-            <Button color="secondary" onClick={handleRemoveButtonClick}>
-              {removeButtonLabel}
-            </Button>
-          </CardActions>
-        </CardContent>
-      </Card>
-    </div>
-  );
+  removeButtonLabel: pgettext("Category list action link", "Remove")
 };
