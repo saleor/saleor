e7b1450039cc4a7dcf5cd2d6e914757d9df9a09d
dominik-zeglen <flesz3@o2.pl>
2018-03-06 20:15:08 +0100

    Add ErrorMessageCard component

---
+++ saleor/static/dashboard-next/js/components/cards/ErrorMessageCard.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Card, { CardContent } from "material-ui/Card";
+import Typography from "material-ui/Typography";
+
+import { pgettext } from "../../i18n";
+
+interface ErrorMessageCardProps {
+  message: string;
+}
+export const ErrorMessageCard: React.StatelessComponent<
+  ErrorMessageCardProps
+> = ({ message }) => (
+  <Card>
+    <CardContent>
+      <Typography variant={"display1"}>
+        {pgettext("Dashboard error message card title", "Error")}
+      </Typography>
+      <Typography variant={"body1"}>{message}</Typography>
+    </CardContent>
+  </Card>
+);
