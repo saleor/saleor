f44041f664cd050ac8f9bf765132b592db8b4661
dominik-zeglen <flesz3@o2.pl>
2018-03-06 20:15:08 +0100

    Add ErrorMessageCard component

--- saleor/static/dashboard-next/js/components/cards/index.ts
+++ saleor/static/dashboard-next/js/components/cards/index.ts
@@ -1,3 +1,4 @@
 export { DescriptionCard } from "./DescriptionCard";
+export { ErrorMessageCard } from "./ErrorMessageCard";
 export { FilterCard } from "./FilterCard";
 export { ListCard } from "./ListCard";
