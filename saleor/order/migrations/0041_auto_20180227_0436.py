da0e02c568f226e56598fc8731f4ecc1166d6014
Anna Siwik <anna.siwik@mirumee.com>
2018-03-06 11:56:05 +0100

    Fix migrations after rebase

--- saleor/order/migrations/0041_auto_20180227_0436.py
+++ saleor/order/migrations/0041_auto_20180227_0436.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('order', '0035_auto_20180222_0458'),
+        ('order', '0040_auto_20180222_0458'),
     ]
 
     operations = [
