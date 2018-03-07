70f8155cc1616f5f6a7339c9e6c2e7291df5723d
Patryk Zawadzki <patrys@room-303.com>
2018-03-07 15:18:09 +0100

    Clean UI

--- saleor/static/dashboard-next/js/components/cards/listCard.tsx
+++ saleor/static/dashboard-next/js/components/cards/listCard.tsx
@@ -1,69 +1,49 @@
 import * as React from "react";
-import Button from "material-ui/Button";
-import Card, { CardContent } from "material-ui/Card";
 import Typography from "material-ui/Typography";
 import { withStyles } from "material-ui/styles";
 
 import { Table } from "../table";
 import { gettext } from "../../i18n";
+import { Button, Toolbar, Paper } from "material-ui";
 
 const decorate = withStyles(theme => ({
-  listCard: {
-    paddingBottom: 0
+  spacer: {
+    flex: "1 1 100%"
   },
-  listCardActions: {
-    paddingBottom: 0
+  actions: {
+    whiteSpace: "nowrap",
+    minWidth: "auto"
   },
-  listCardAddActionButton: {
-    margin: "1rem 0"
+  title: {
+    flex: "0 0 auto"
   }
 }));
 
 interface ListCardProps {
   addActionLabel: string;
-  displayLabel: boolean;
   handleAddAction();
   label: string;
 }
 
 export const ListCard = decorate<ListCardProps>(
-  ({
-    addActionLabel,
-    children,
-    classes,
-    displayLabel,
-    handleAddAction,
-    label
-  }) => (
-    <Card className={classes.listCard}>
-      <div>
-        {displayLabel && (
-          <CardContent className={classes.listCardActions}>
-            <Typography variant="title">{label}</Typography>
-            <Button
-              color="secondary"
-              onClick={handleAddAction}
-              className={classes.listCardAddActionButton}
-            >
-              {addActionLabel}
-            </Button>
-          </CardContent>
-        )}
-        <CardContent
-          style={{
-            borderTop: "none",
-            padding: 0
-          }}
+  ({ addActionLabel, children, classes, handleAddAction, label }) => (
+    <Paper>
+      <Toolbar>
+        <div className={classes.title}>
+          <Typography variant="title">{label}</Typography>
+        </div>
+        <div className={classes.spacer} />
+        <Button
+          className={classes.actions}
+          color="primary"
+          onClick={handleAddAction}
         >
-          {children}
-        </CardContent>
-      </div>
-    </Card>
+          {addActionLabel}
+        </Button>
+      </Toolbar>
+      {children}
+    </Paper>
   )
 );
-ListCard.defaultProps = {
-  addActionLabel: gettext("Add"),
-  displayLabel: false
-};
 
 export default ListCard;
