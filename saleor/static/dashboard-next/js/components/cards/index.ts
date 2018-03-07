8803ee7cb98aa9d00ca7eda943451bfca1e7c0a8
Patryk Zawadzki <patrys@room-303.com>
2018-03-07 18:05:16 +0100

    Rename more files

--- saleor/static/dashboard-next/js/components/cards/index.ts
+++ saleor/static/dashboard-next/js/components/cards/index.ts
@@ -1,4 +1,2 @@
-export { DescriptionCard } from "./DescriptionCard";
 export { ErrorMessageCard } from "./ErrorMessageCard";
 export { FilterCard } from "./FilterCard";
-export { ListCard } from "./ListCard";
