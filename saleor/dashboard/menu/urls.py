1273a08ed0dbc6258d71d281283a9be82d6cd902
Anna Siwik <anna.siwik@mirumee.com>
2018-03-08 13:44:53 +0100

    Test menu and menu item views

--- saleor/dashboard/menu/urls.py
+++ saleor/dashboard/menu/urls.py
@@ -14,8 +14,6 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$',
         views.menu_detail, name='menu-detail'),
 
-    url(r'^(?P<menu_pk>[0-9]+)/item/(?P<item_pk>[0-9]+)/$',
-        views.menu_item_detail, name='menu-item-detail'),
     url(r'^(?P<menu_pk>[0-9]+)/add/$',
         views.menu_item_create, name='menu-item-add'),
     url(r'^(?P<menu_pk>[0-9]+)/item/(?P<root_pk>[0-9]+)/add/$',
@@ -24,7 +22,9 @@ urlpatterns = [
         views.menu_item_edit, name='menu-item-edit'),
     url(r'^(?P<menu_pk>[0-9]+)/item/(?P<item_pk>[0-9]+)/delete/$',
         views.menu_item_delete, name='menu-item-delete'),
-    url(r'^(?P<menu_pk>[0-9]+)/item/(?P<root_pk>[0-9]+)/items/reorder/$',
-        views.ajax_reorder_menu_items, name='menu-items-reorder'),
+    url(r'^(?P<menu_pk>[0-9]+)/item/(?P<item_pk>[0-9]+)/$',
+        views.menu_item_detail, name='menu-item-detail'),
     url(r'^(?P<menu_pk>[0-9]+)/items/reorder/$',
+        views.ajax_reorder_menu_items, name='menu-items-reorder'),
+    url(r'^(?P<menu_pk>[0-9]+)/item/(?P<root_pk>[0-9]+)/items/reorder/$',
         views.ajax_reorder_menu_items, name='menu-items-reorder')]
