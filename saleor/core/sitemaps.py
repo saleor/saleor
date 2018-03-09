75840e85345ec73bfd2f055c2f51cab1465a1cac
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-03-09 12:23:31 +0100

    Sitemap internalization

--- saleor/core/sitemaps.py
+++ saleor/core/sitemaps.py
@@ -1,30 +1,36 @@
+from django.conf import settings
 from django.contrib.sitemaps import Sitemap
 
 from ..page.models import Page
 from ..product.models import Category, Collection, Product
 
 
-class ProductSitemap(Sitemap):
+class i18nSitemap(Sitemap):
+    protocol = 'https' if settings.ENABLE_SSL else 'http'
+    i18n = True
+
+
+class ProductSitemap(i18nSitemap):
 
     def items(self):
         return Product.objects.only('id', 'name').order_by('-id')
 
 
-class CategorySitemap(Sitemap):
+class CategorySitemap(i18nSitemap):
 
     def items(self):
         categories = Category.objects.all().order_by('id')
         return categories.only('id', 'name', 'slug')
 
 
-class CollectionSitemap(Sitemap):
+class CollectionSitemap(i18nSitemap):
 
     def items(self):
         collections = Collection.objects.all().order_by('id')
         return collections.only('id', 'name', 'slug')
 
 
-class PageSitemap(Sitemap):
+class PageSitemap(i18nSitemap):
 
     def items(self):
         posts = Page.objects.public()
