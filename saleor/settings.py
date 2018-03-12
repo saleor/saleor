536a25358b4efc6f6b6018def2475112d2af5248
Artur Smęt <artur@mirumee.com>
2018-03-12 21:10:12 +0100

    Respect REDIS_URL variable from cloud providers

--- saleor/settings.py
+++ saleor/settings.py
@@ -28,6 +28,10 @@ MANAGERS = ADMINS
 
 INTERNAL_IPS = get_list(os.environ.get('INTERNAL_IPS', '127.0.0.1'))
 
+# Some cloud providers like Heroku export REDIS_URL variable instead of CACHE_URL
+REDIS_URL = os.environ.get('REDIS_URL')
+if REDIS_URL:
+    CACHE_URL = os.environ.setdefault('CACHE_URL', REDIS_URL)
 CACHES = {'default': django_cache_url.config()}
 
 DATABASES = {
