21f0d1f69ffbf16e4387a1938aed30f79fd7d253
Adam Janik <adam.janik@mirumee.com>
2018-03-12 18:13:09 +0100

    WIP: Abstract model delete class for Page

--- saleor/dashboard/graphql/api.py
+++ saleor/dashboard/graphql/api.py
@@ -3,8 +3,10 @@ from graphene_django.debug import DjangoDebug
 from graphene_django.filter import DjangoFilterConnectionField
 
 from ...graphql.core.filters import DistinctFilterSet
+from ...graphql.page.types import Page, resolve_pages
 from ...graphql.product.types import Category, resolve_categories
 from ...graphql.utils import get_node
+from .page.mutations import PageCreate, PageDelete, PageUpdate
 from .product.mutations import (
     CategoryCreateMutation, CategoryDelete, CategoryUpdateMutation)
 
@@ -16,6 +18,9 @@ class Query(graphene.ObjectType):
     category = graphene.Field(Category, id=graphene.Argument(graphene.ID))
     node = graphene.Node.Field()
     debug = graphene.Field(DjangoDebug, name='__debug')
+    page = graphene.Field(Page, id=graphene.Argument(graphene.ID))
+    pages = DjangoFilterConnectionField(
+        Page, filterset_class=DistinctFilterSet)
 
     def resolve_category(self, info, id):
         return get_node(info, id, only_type=Category)
@@ -23,11 +28,19 @@ class Query(graphene.ObjectType):
     def resolve_categories(self, info, level=None, **kwargs):
         return resolve_categories(info, level)
 
+    def resolve_pages(self, info):
+        return resolve_pages(info)
+
 
 class Mutations(graphene.ObjectType):
     category_create = CategoryCreateMutation.Field()
     category_delete = CategoryDelete.Field()
     category_update = CategoryUpdateMutation.Field()
 
+    page_create = PageCreate.Field()
+    page_delete = PageDelete.Field()
+    page_update = PageUpdate.Field()
+
 
 schema = graphene.Schema(Query, Mutations)
+
