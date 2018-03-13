20bba8892b68aeb9533fb4206653536aee7ea5fc
dominik-zeglen <flesz3@o2.pl>
2018-03-13 15:30:50 +0100

    Add component tests

---
+++ saleor/static/dashboard-next/js/__tests__/components/FilterCard.test.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { MemoryRouter } from "react-router-dom";
+import * as renderer from "react-test-renderer";
+
+import { FilterCard } from "../../components/cards/FilterCard";
+
+describe("<CategoryDetails />", () => {
+  it("renders properly", () => {
+    const component = renderer.create(
+      <FilterCard handleClear={jest.fn()} handleSubmit={jest.fn()} />
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
---
+++ saleor/static/dashboard-next/js/__tests__/components/fixtures/category.ts
@@ -0,0 +1,8 @@
+export default {
+  node: {
+    id: "Q2F0ZWdvcnk6NA==",
+    name: "Books",
+    description:
+      "Girl remain increase quite. More opportunity special special life foot employee. Wall employee all relate control cause need."
+  }
+};
