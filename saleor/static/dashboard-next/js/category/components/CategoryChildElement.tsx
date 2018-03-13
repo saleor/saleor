55555676832a6c4fe6ad89fb0584637285d9980b
Patryk Zawadzki <patrys@room-303.com>
2018-03-13 11:23:41 +0100

    Fix layout

--- saleor/static/dashboard-next/js/category/components/CategoryChildElement.tsx
+++ saleor/static/dashboard-next/js/category/components/CategoryChildElement.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
-import Grid from "material-ui/Grid";
-import Card, { CardContent } from "material-ui/Card";
+import ButtonBase from "material-ui/ButtonBase";
+import Card, { CardHeader } from "material-ui/Card";
+import Paper from "material-ui/Paper";
+import { withStyles } from "material-ui/styles";
 import Typography from "material-ui/Typography";
 import { Link } from "react-router-dom";
+import Folder from "material-ui-icons/Folder";
+
 import { Skeleton } from "../../components/Skeleton";
-import { withStyles } from "material-ui/styles";
 
 interface CategoryChildElementProps {
   label: string;
@@ -13,27 +16,49 @@ interface CategoryChildElementProps {
 }
 
 const decorate = withStyles(theme => ({
-  link: {
-    textDecoration: "none",
-    color: theme.typography.body1.color
+  button: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[2],
+    color: theme.palette.getContrastText(theme.palette.background.paper),
+    justifyContent: "flex-start" as "flex-start",
+    transition: theme.transitions.create(["background-color", "box-shadow"], {
+      duration: theme.transitions.duration.short
+    }),
+    width: "100%",
+    "&$keyboardFocused": {
+      boxShadow: theme.shadows[6]
+    },
+    "&:active": {
+      boxShadow: theme.shadows[8]
+    },
+    "&$disabled": {
+      boxShadow: theme.shadows[0],
+      backgroundColor: theme.palette.action.disabledBackground
+    },
+    "&:hover": {
+      backgroundColor: theme.palette.grey.A100,
+      // Reset on mouse devices
+      "@media (hover: none)": {
+        backgroundColor: theme.palette.grey[300]
+      },
+      "&$disabled": {
+        backgroundColor: theme.palette.action.disabledBackground
+      }
+    }
   }
 }));
+
 export const CategoryChildElement = decorate<CategoryChildElementProps>(
   ({ classes, label, url, loading }) => (
-    <Grid item xs={6} md={4} lg={3}>
-      <Card>
-        <Link to={url} className={classes.link}>
-          <CardContent>
-            {loading ? (
-              <Typography variant={"button"}>
-                <Skeleton style={{ width: "80%" }} />
-              </Typography>
-            ) : (
-              <Typography variant={"button"}>{label}</Typography>
-            )}
-          </CardContent>
-        </Link>
-      </Card>
-    </Grid>
+    <ButtonBase
+      focusRipple
+      className={classes.button}
+      component={props => <Link to={url} {...props} />}
+    >
+      <CardHeader
+        avatar={<Folder />}
+        title={loading ? <Skeleton style={{ width: "80%" }} /> : label}
+      />
+    </ButtonBase>
   )
 );
