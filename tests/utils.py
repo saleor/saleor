e1f340dd7e6098c1d27a6690ee2c56ddb669e00e
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-03-14 15:49:48 +0100

    WIP

--- tests/utils.py
+++ tests/utils.py
@@ -1,14 +1,18 @@
 import json
+from urllib.parse import urlparse
 
-from django.conf import settings
 from django.db.models import Q
-from django.urls import translate_url
 from django.utils.encoding import smart_text
 
 
+def get_url_path(url):
+    parsed_url = urlparse(url)
+    return parsed_url.path
+
+
 def get_redirect_location(response):
-    path = response['Location']
-    return translate_url(path, lang_code=settings.LANGUAGE_CODE)
+    # Due to Django 1.8 compatibility, we have to handle both cases
+    return get_url_path(response['Location'])
 
 
 def filter_products_by_attribute(queryset, attribute_id, value):
