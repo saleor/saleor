5ced31cfee0998267f00e40d7bf2f5a82fe8f36c
Rafał Pitoń <rafal@mirumee.com>
2018-03-16 18:09:53 +0100

    Fix showstopper on store's homepage

--- saleor/core/views.py
+++ saleor/core/views.py
@@ -15,7 +15,8 @@ from ..seo.schema.webpage import get_webpage_schema
 def home(request):
     products = products_for_homepage()[:8]
     products = products_with_availability(
-        products, discounts=request.discounts, local_currency=request.currency)
+        products, discounts=request.discounts, local_currency=request.currency,
+        taxes=request.taxes)
     webpage_schema = get_webpage_schema(request)
     return TemplateResponse(
         request, 'home.html', {
