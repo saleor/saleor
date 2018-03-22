6e560ede12a61b46706493398275be8d4d33bebf
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-03-22 12:50:17 +0100

    Page tests

--- tests/dashboard/test_page.py
+++ tests/dashboard/test_page.py
@@ -1,5 +1,8 @@
 from django.urls import reverse
 
+from saleor.dashboard.page.forms import PageForm
+from django.forms.models import model_to_dict
+
 
 def test_page_list(admin_client):
     url = reverse('dashboard:page-list')
@@ -47,3 +50,39 @@ def test_page_delete(admin_client, page):
 
     response = admin_client.post(url, data={'a': 'b'})
     assert response.status_code == 302
+
+
+def test_sanitize_page_content(page, default_category):
+    data = model_to_dict(page)
+    data['description'] = (
+        '<b>bold</b><p><i>italic</i></p><h2>Header</h2><h3>subheader</h3>'
+        '<blockquote>quote</blockquote>'
+        '<p><a href="www.mirumee.com">link</a></p>'
+        '<p>an <script>evil()</script>example</p>')
+    form = PageForm(data, instance=page)
+    assert form.is_valid()
+    form.save()
+    assert page.content == (
+        '<b>bold</b><p><i>italic</i></p><h2>Header</h2><h3>subheader</h3>'
+        '<blockquote>quote</blockquote>'
+        '<p><a href="www.mirumee.com">link</a></p>'
+        '<p>an &lt;script&gt;evil()&lt;/script&gt;example</p>')
+
+    assert page.seo_description == (
+        'bolditalicHeadersubheaderquotelinkan evil()example')
+
+
+def test_set_product_seo_description(unavailable_product):
+    seo_description = (
+        'This is a dummy product. '
+        'HTML <b>shouldn\'t be removed</b> since it\'s a simple text field.')
+    data = model_to_dict(unavailable_product)
+    data['price'] = 20
+    data['description'] = 'a description'
+    data['seo_description'] = seo_description
+
+    form = PageForm(data, instance=unavailable_product)
+
+    assert form.is_valid()
+    form.save()
+    assert unavailable_product.seo_description == seo_description
