3b8628ae1baf1272c2f2e82452da4fc59a52e733
Dominik Żegleń <flesz3@o2.pl>
2018-03-26 17:04:19 +0200

    Rewrite page forms (#1980)
    * Rewrite page forms
    
    * Refactor a bit

--- saleor/static/dashboard-next/page/components/PageBaseForm.tsx
+++ saleor/static/dashboard-next/page/components/PageBaseForm.tsx
@@ -12,26 +12,16 @@ import FormSpacer from "../../components/FormSpacer";
 import RichTextEditor from "../../components/RichTextEditor";
 import i18n from "../../i18n";
 
-interface PageUpdateFormComponentState {
+// FIXME: Change name
+interface PageBaseFormComponentProps {
   availableOn: string;
   content: string;
+  created?: string;
+  errors?: Array<{ field: string; message: string }>;
   isVisible: boolean;
   slug: string;
   title: string;
-}
-
-interface PageUpdateFormComponentProps {
-  created?: string;
-  errors?: Array<{ field: string; message: string }>;
-  formInitialValues?: PageUpdateFormComponentState;
-  handleSubmit(data: any);
-}
-
-interface CombinedErrorsType {
-  availableOn: string;
-  content: string;
-  slug: string;
-  title: string;
+  onChange?(event: React.ChangeEvent<any>);
 }
 
 const decorate = withStyles(theme => ({
@@ -45,136 +35,101 @@ const decorate = withStyles(theme => ({
   }
 }));
 
-export const PageUpdateFormComponent = decorate(
-  class PageUpdateFormUnstyledComponent extends React.Component<
-    PageUpdateFormComponentProps &
-      WithStyles<"cardActions" | "addHelperTextPadding">,
-    PageUpdateFormComponentState
-  > {
-    static defaultState = {
-      availableOn: "",
-      content: "",
-      isVisible: false,
-      slug: "",
-      title: ""
-    };
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        ...PageUpdateFormUnstyledComponent.defaultState,
-        ...props.formInitialValues
-      };
-    }
-
-    handleChange = event => {
-      this.setState({ [event.target.name]: event.target.value });
-    };
-
-    render() {
-      const { classes, created, errors, handleSubmit } = this.props;
-      const combinedErrors = errors
-        ? (errors.reduce((prev, curr) => {
-            prev[curr.field] = curr.message;
-            return prev;
-          }, {}) as CombinedErrorsType)
-        : undefined;
-      return (
-        <>
-          <CardContent>
-            <Grid container spacing={16}>
-              <Grid item xs={12} md={9}>
-                <TextField
-                  name="title"
-                  label={i18n.t("Title", { context: "object" })}
-                  defaultValue={this.state.title}
-                  onChange={this.handleChange}
-                  className={
-                    (errors ? !combinedErrors.title : true)
-                      ? classes.addHelperTextPadding
-                      : ""
-                  }
-                  helperText={
-                    combinedErrors && combinedErrors.title
-                      ? combinedErrors.title
-                      : ""
-                  }
-                  fullWidth
-                />
-                <FormSpacer />
-                <RichTextEditor
-                  defaultValue={this.state.content}
-                  name="content"
-                  label={i18n.t("Content", { context: "object" })}
-                  helperText={
-                    combinedErrors && combinedErrors.content
-                      ? combinedErrors.content
-                      : i18n.t("Select text to enable text-formatting tools.", {
-                          context: "object"
-                        })
-                  }
-                  onChange={this.handleChange}
-                  fullWidth
-                />
-              </Grid>
-              <Grid item xs={12} md={3}>
-                <TextField
-                  name="slug"
-                  label={i18n.t("Slug", { context: "object" })}
-                  defaultValue={this.state.slug}
-                  helperText={
-                    combinedErrors && combinedErrors.slug
-                      ? combinedErrors.slug
-                      : i18n.t("Slug is being used to create page URL", {
-                          context: "object"
-                        })
-                  }
-                  onChange={this.handleChange}
-                  fullWidth
-                />
-                <FormSpacer />
-                <TextField
-                  name="availableOn"
-                  label={i18n.t("Available on", { context: "label" })}
-                  type="date"
-                  defaultValue={this.state.availableOn}
-                  onChange={this.handleChange}
-                  InputLabelProps={{
-                    shrink: true
-                  }}
-                  helperText={
-                    combinedErrors && combinedErrors.availableOn
-                      ? combinedErrors.availableOn
-                      : ""
-                  }
-                />
-                <FormSpacer />
-                {created && (
-                  <Typography variant="body1">Created at: {created}</Typography>
-                )}
-                <FormSpacer />
-                <ControlledCheckbox
-                  checked={this.state.isVisible}
-                  label={i18n.t("Published", { context: "label" })}
-                  name="isVisible"
-                  onChange={this.handleChange}
-                />
-              </Grid>
-            </Grid>
-          </CardContent>
-          <Toolbar className={classes.cardActions}>
-            <Button
-              variant="raised"
-              color="primary"
-              onClick={() => handleSubmit(this.state)}
-            >
-              {i18n.t("Submit", { context: "label" })}
-            </Button>
-          </Toolbar>
-        </>
-      );
-    }
+export const PageBaseForm = decorate<PageBaseFormComponentProps>(
+  ({
+    classes,
+    created,
+    errors,
+    title,
+    content,
+    slug,
+    availableOn,
+    isVisible,
+    onChange
+  }) => {
+    const errorList: { [key: string]: string } = errors.reduce((acc, curr) => {
+      acc[curr.field] = curr.message;
+      return acc;
+    }, {});
+    return (
+      <CardContent>
+        <Grid container spacing={16}>
+          <Grid item xs={12} md={9}>
+            <TextField
+              autoFocus
+              name="title"
+              label={i18n.t("Title", { context: "object" })}
+              value={title}
+              onChange={onChange}
+              className={
+                (errors ? !errorList.title : true)
+                  ? classes.addHelperTextPadding
+                  : ""
+              }
+              helperText={errorList && errorList.title ? errorList.title : ""}
+              fullWidth
+            />
+            <FormSpacer />
+            <RichTextEditor
+              value={content}
+              name="content"
+              label={i18n.t("Content", { context: "object" })}
+              helperText={
+                errorList && errorList.content
+                  ? errorList.content
+                  : i18n.t("Select text to enable text-formatting tools.", {
+                      context: "object"
+                    })
+              }
+              onChange={onChange}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} md={3}>
+            <TextField
+              name="slug"
+              label={i18n.t("Slug", { context: "object" })}
+              value={slug}
+              helperText={
+                errorList && errorList.slug
+                  ? errorList.slug
+                  : i18n.t("Slug is being used to create page URL", {
+                      context: "object"
+                    })
+              }
+              onChange={onChange}
+              fullWidth
+            />
+            <FormSpacer />
+            <TextField
+              name="availableOn"
+              label={i18n.t("Available on", { context: "label" })}
+              type="date"
+              value={availableOn}
+              onChange={onChange}
+              InputLabelProps={{
+                shrink: true
+              }}
+              helperText={
+                errorList && errorList.availableOn ? errorList.availableOn : ""
+              }
+            />
+            <FormSpacer />
+            {created && (
+              <Typography variant="body1">Created at: {created}</Typography>
+            )}
+            <FormSpacer />
+            <ControlledCheckbox
+              checked={isVisible}
+              label={i18n.t("Published", { context: "label" })}
+              name="isVisible"
+              onChange={onChange}
+            />
+          </Grid>
+        </Grid>
+      </CardContent>
+    );
   }
 );
 
-export default PageUpdateFormComponent;
+export default PageBaseForm;
--- saleor/static/dashboard-next/page/views/PageCreateForm.tsx
+++ saleor/static/dashboard-next/page/views/PageCreateForm.tsx
@@ -2,11 +2,18 @@ import Card from "material-ui/Card";
 import * as React from "react";
 import { Redirect } from "react-router";
 
+import { pageListUrl } from "..";
+import Form, { FormActions, FormProps } from "../../components/Form";
 import { NavigatorLink } from "../../components/Navigator";
 import PageHeader from "../../components/PageHeader";
+import { PageCreateMutationVariables } from "../../gql-types";
 import i18n from "../../i18n";
 import PageBaseForm from "../components/PageBaseForm";
 import { pageCreateMutation, TypedPageCreateMutation } from "../mutations";
+
+const PageForm: React.ComponentType<
+  FormProps<PageCreateMutationVariables>
+> = Form;
 
 interface PageCreateFormProps {
   id: string;
@@ -16,29 +23,56 @@ export const PageCreateForm: React.StatelessComponent<PageCreateFormProps> = ({
   id
 }) => (
   <TypedPageCreateMutation mutation={pageCreateMutation}>
-    {(createPage, { called, data, error, loading }) => {
-      if (called && !loading && !data.pageCreate.errors.length) {
-        if (error) {
-          console.error(error);
-          return;
-        }
-        return <Redirect to="/pages/" />;
+    {(createPage, { called, data: createResult, error, loading }) => {
+      if (called && !loading && !createResult.pageCreate.errors.length) {
+        return <Redirect to={pageListUrl} push={false} />;
       }
       return (
-        <NavigatorLink to={"/pages/"}>
+        <NavigatorLink to={pageListUrl}>
           {handleCancel => (
-            <Card>
-              <PageHeader
-                onBack={handleCancel}
-                title={i18n.t("Add page", { context: "title" })}
-              />
-              <PageBaseForm
-                errors={called && data ? data.pageCreate.errors : undefined}
-                handleSubmit={data =>
-                  createPage({ variables: { id, ...data } })
-                }
-              />
-            </Card>
+            <PageForm
+              initial={{
+                availableOn: "",
+                content: "",
+                isVisible: false,
+                slug: "",
+                title: ""
+              }}
+              onSubmit={data =>
+                createPage({
+                  variables: data
+                })
+              }
+            >
+              {({ change, data, submit: handleSubmit }) => (
+                <Card>
+                  <PageHeader
+                    onBack={handleCancel}
+                    title={i18n.t("Add page", { context: "title" })}
+                  />
+                  <PageBaseForm
+                    errors={
+                      called && createResult
+                        ? createResult.pageCreate.errors
+                        : []
+                    }
+                    title={data.title}
+                    content={data.content}
+                    slug={data.slug}
+                    availableOn={data.availableOn}
+                    isVisible={data.isVisible}
+                    onChange={change}
+                  />
+                  <FormActions
+                    onCancel={handleCancel}
+                    onSubmit={handleSubmit}
+                    submitLabel={i18n.t("Save", {
+                      context: "button"
+                    })}
+                  />
+                </Card>
+              )}
+            </PageForm>
           )}
         </NavigatorLink>
       );
