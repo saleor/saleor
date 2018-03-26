4119afa155299864f020e4ff8b3e87c714ec46d4
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-03-26 15:15:43 +0200

    Tests for SEO helpers

--- tests/test_page.py
+++ tests/test_page.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_draft_page_detail(client, admin_client, page):
     page_url = page.get_absolute_url()
     response = client.get(page_url)
@@ -14,3 +17,30 @@ def test_public_page_detail(client, admin_client, page):
     assert response.status_code == 200
     response = admin_client.get(page_url)
     assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    'title, seo_title, expected_result',
+    (
+        ('Title', 'Seo Title', 'Seo Title'),
+        ('Title', None, 'Title')))
+def test_page_get_seo_title(
+        admin_client, page, title, seo_title, expected_result):
+    page.title = title
+    page.seo_title = seo_title
+    page.save()
+    result = page.get_seo_title()
+    expected_result == result
+
+
+@pytest.mark.parametrize(
+    'seo_description, expected_result',
+    (
+        ('Seo', 'Seo'),
+        (None, '')))
+def test_page_get_seo_description(
+        admin_client, page, seo_description, expected_result):
+    page.seo_description = seo_description
+    page.save()
+    result = page.get_seo_description()
+    expected_result == result
