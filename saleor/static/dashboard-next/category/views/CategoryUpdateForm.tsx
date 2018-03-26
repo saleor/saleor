31fe8f1354e6460e1be5e2cc4043af9a5043dd27
Patryk Zawadzki <patrys@room-303.com>
2018-03-26 13:41:06 +0200

    Move form logic to a typed Form component (#1978)

--- saleor/static/dashboard-next/category/views/CategoryUpdateForm.tsx
+++ saleor/static/dashboard-next/category/views/CategoryUpdateForm.tsx
@@ -1,11 +1,14 @@
 import Card from "material-ui/Card";
 import { CircularProgress } from "material-ui/Progress";
+import { withStyles } from "material-ui/styles";
 import * as React from "react";
 import { Redirect } from "react-router-dom";
 
 import ErrorMessageCard from "../../components/ErrorMessageCard";
+import Form, { FormActions, FormProps } from "../../components/Form";
 import { NavigatorLink } from "../../components/Navigator";
 import PageHeader from "../../components/PageHeader";
+import { CategoryUpdateMutationVariables } from "../../gql-types";
 import i18n from "../../i18n";
 import BaseCategoryForm from "../components/BaseForm";
 import { categoryShowUrl } from "../index";
@@ -15,68 +18,108 @@ import {
 } from "../mutations";
 import { categoryDetailsQuery, TypedCategoryDetailsQuery } from "../queries";
 
+const CategoryForm: React.ComponentType<
+  FormProps<CategoryUpdateMutationVariables>
+> = Form;
+
+const decorate = withStyles(theme => ({
+  root: {
+    marginBottom: theme.spacing.unit * 2,
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: "auto",
+      marginRight: "auto",
+      maxWidth: theme.breakpoints.width("sm")
+    }
+  }
+}));
+
 interface CategoryUpdateFormProps {
   id: string;
 }
 
-export const CategoryUpdateForm: React.StatelessComponent<
-  CategoryUpdateFormProps
-> = ({ id }) => (
-  <TypedCategoryDetailsQuery query={categoryDetailsQuery} variables={{ id }}>
-    {({ data, loading, error }) => {
-      if (error) {
-        console.error(error.message);
-        return <ErrorMessageCard message={error.message} />;
-      }
-      const { category } = data;
+export const CategoryUpdateForm = decorate<CategoryUpdateFormProps>(
+  ({ classes, id }) => (
+    <TypedCategoryDetailsQuery query={categoryDetailsQuery} variables={{ id }}>
+      {({ data, loading, error }) => {
+        if (error) {
+          console.error(error.message);
+          return <ErrorMessageCard message={error.message} />;
+        }
+        const { category } = data;
 
-      return (
-        <TypedCategoryUpdateMutation mutation={categoryUpdateMutation}>
-          {(mutate, { called, data, loading }) => {
-            if (called && !loading && data.categoryUpdate.errors.length === 0) {
+        return (
+          <TypedCategoryUpdateMutation mutation={categoryUpdateMutation}>
+            {(
+              mutate,
+              { called, data: updateResult, loading: updateInProgress }
+            ) => {
+              if (
+                called &&
+                !updateInProgress &&
+                updateResult.categoryUpdate.errors.length === 0
+              ) {
+                return (
+                  <Redirect
+                    to={categoryShowUrl(
+                      updateResult.categoryUpdate.category.id
+                    )}
+                  />
+                );
+              }
+              const errors =
+                called && !updateInProgress
+                  ? updateResult.categoryUpdate.errors
+                  : [];
               return (
-                <Redirect
-                  to={categoryShowUrl(data.categoryUpdate.category.id)}
-                />
-              );
-            }
-            return (
-              <NavigatorLink to={categoryShowUrl(id)}>
-                {handleCancel => (
-                  <Card style={{ maxWidth: 750 }}>
-                    <PageHeader
-                      onCancel={handleCancel}
-                      title={i18n.t("Edit category", { context: "title" })}
-                    />
-                    {loading ? (
+                <NavigatorLink to={categoryShowUrl(id)}>
+                  {handleCancel =>
+                    loading ? (
                       <CircularProgress />
                     ) : (
-                      <BaseCategoryForm
-                        name={category.name}
-                        description={category.description}
-                        handleConfirm={formData =>
+                      <CategoryForm
+                        initial={{
+                          description: category.description,
+                          id: category.id,
+                          name: category.name
+                        }}
+                        onSubmit={data =>
                           mutate({
-                            variables: {
-                              ...formData,
-                              id
-                            }
+                            variables: data
                           })
                         }
-                        confirmButtonLabel={i18n.t("Save", {
-                          context: "button"
-                        })}
-                        errors={
-                          called && !loading ? data.categoryUpdate.errors : []
-                        }
-                      />
-                    )}
-                  </Card>
-                )}
-              </NavigatorLink>
-            );
-          }}
-        </TypedCategoryUpdateMutation>
-      );
-    }}
-  </TypedCategoryDetailsQuery>
+                      >
+                        {({ change, data, submit: handleSubmit }) => (
+                          <Card className={classes.root}>
+                            <PageHeader
+                              onBack={handleCancel}
+                              title={i18n.t("Category details", {
+                                context: "title"
+                              })}
+                            />
+                            <BaseCategoryForm
+                              description={data.description}
+                              errors={errors}
+                              name={data.name}
+                              onChange={change}
+                            />
+                            <FormActions
+                              onCancel={handleCancel}
+                              onSubmit={handleSubmit}
+                              submitLabel={i18n.t("Save", {
+                                context: "button"
+                              })}
+                            />
+                          </Card>
+                        )}
+                      </CategoryForm>
+                    )
+                  }
+                </NavigatorLink>
+              );
+            }}
+          </TypedCategoryUpdateMutation>
+        );
+      }}
+    </TypedCategoryDetailsQuery>
+  )
 );
