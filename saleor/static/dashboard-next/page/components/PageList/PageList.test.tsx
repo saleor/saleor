4a498e07ebc6e5e36d56f1d5d710ec8459cad340
Dominik Żegleń <flesz3@o2.pl>
2018-03-27 13:43:06 +0200

    Add page tests (#1983)
    * Add pages tests
    
    * Fix naming convention

---
+++ saleor/static/dashboard-next/page/components/PageList/PageList.test.tsx
@@ -0,0 +1,110 @@
+import * as React from "react";
+import { MemoryRouter } from "react-router-dom";
+import * as renderer from "react-test-renderer";
+
+import PageList from "./";
+
+const pageList = [
+  {
+    cursor: "12345678",
+    node: {
+      id: "abcd",
+      isVisible: true,
+      slug: "lorem-ipsum-dolor",
+      title: "Lorem ipsum"
+    }
+  },
+  {
+    cursor: "12345678",
+    node: {
+      id: "abcde",
+      isVisible: true,
+      slug: "lorem-ipsum-consectetur",
+      title: "Pellentesque habitant morbi tristique senectus"
+    }
+  },
+  {
+    cursor: "12345678",
+    node: {
+      id: "abcdef",
+      isVisible: true,
+      slug: "lorem-ipsum-amet",
+      title: "Pellentesque metus turpis"
+    }
+  },
+  {
+    cursor: "12345678",
+    node: {
+      id: "abcdefg",
+      isVisible: true,
+      slug: "lorem-ipsum-sit",
+      title: "Nulla blandit ut lectus at placerat"
+    }
+  }
+];
+
+describe("<PageList />", () => {
+  it("renders while data is loading", () => {
+    const component = renderer.create(
+      <MemoryRouter>
+        <PageList
+          loading={true}
+          handlePreviousPage={jest.fn()}
+          handleNextPage={jest.fn()}
+          pages={[]}
+          editPageUrl={jest.fn()}
+          showPageUrl={jest.fn()}
+          pageInfo={{
+            endCursor: "",
+            hasNextPage: false,
+            hasPreviousPage: false,
+            startCursor: ""
+          }}
+        />
+      </MemoryRouter>
+    );
+    expect(component).toMatchSnapshot();
+  });
+  it("renders when data is fully loaded", () => {
+    const component = renderer.create(
+      <MemoryRouter>
+        <PageList
+          loading={true}
+          handlePreviousPage={jest.fn()}
+          handleNextPage={jest.fn()}
+          pages={pageList}
+          editPageUrl={jest.fn()}
+          showPageUrl={jest.fn()}
+          pageInfo={{
+            endCursor: "",
+            hasNextPage: false,
+            hasPreviousPage: false,
+            startCursor: ""
+          }}
+        />
+      </MemoryRouter>
+    );
+    expect(component).toMatchSnapshot();
+  });
+  it("renders when category list is empty", () => {
+    const component = renderer.create(
+      <MemoryRouter>
+        <PageList
+          loading={true}
+          handlePreviousPage={jest.fn()}
+          handleNextPage={jest.fn()}
+          pages={[]}
+          editPageUrl={jest.fn()}
+          showPageUrl={jest.fn()}
+          pageInfo={{
+            endCursor: "",
+            hasNextPage: false,
+            hasPreviousPage: false,
+            startCursor: ""
+          }}
+        />
+      </MemoryRouter>
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
