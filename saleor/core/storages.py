612df4fec617831148c1faac50b73dc67d8d48a7
NyanKiyoshi <NyanKiyoshi@users.noreply.github.com>
2018-04-02 23:06:05 +0200

    Allow user to set a custom domain to the S3 buckets

--- saleor/core/storages.py
+++ saleor/core/storages.py
@@ -5,4 +5,7 @@ from storages.backends.s3boto3 import S3Boto3Storage
 class S3MediaStorage(S3Boto3Storage):
     def __init__(self, *args, **kwargs):
         self.bucket_name = settings.AWS_MEDIA_BUCKET_NAME
+
+        if settings.AWS_MEDIA_CUSTOM_DOMAIN:
+            self.custom_domain = settings.AWS_MEDIA_CUSTOM_DOMAIN
         super().__init__(*args, **kwargs)
