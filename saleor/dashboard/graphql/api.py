2a3d3709fe167b6a2a40ef9895749109267e02c0
akjanik <adam.janik@mirumee.com>
2018-04-04 20:08:38 +0200

    Remove attributes from dashboard api

--- saleor/dashboard/graphql/api.py
+++ saleor/dashboard/graphql/api.py
@@ -6,17 +6,12 @@ from graphql_jwt.decorators import staff_member_required
 
 from ...graphql.core.filters import DistinctFilterSet
 from ...graphql.page.types import Page
-from ...graphql.product.types import ProductAttribute
 from ...graphql.utils import get_node
 from .page.mutations import PageCreate, PageDelete, PageUpdate
 from .page.types import resolve_all_pages
-from .product.types import resolve_attributes
 
 
 class Query(graphene.ObjectType):
-    attributes = DjangoFilterConnectionField(
-        ProductAttribute, filterset_class=DistinctFilterSet,
-        description='List of the shop\'s product attributes.')
     page = graphene.Field(
         Page, id=graphene.Argument(graphene.ID),
         description='Lookup a page by ID.')
@@ -27,17 +22,12 @@ class Query(graphene.ObjectType):
     debug = graphene.Field(DjangoDebug, name='__debug')
 
     @staff_member_required
-    def resolve_attributes(self, info):
-        return resolve_attributes()
-
-    @staff_member_required
     def resolve_page(self, info, id):
         return get_node(info, id, only_type=Page)
 
     @staff_member_required
     def resolve_pages(self, info):
         return resolve_all_pages()
-
 
 
 class Mutations(graphene.ObjectType):
