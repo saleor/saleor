abee82b5b14bdc984d96d1be4020a98a948898ea
Anna Siwik <anna.siwik@mirumee.com>
2018-04-05 10:35:17 +0200

    Fix reverse url in tests

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -511,7 +511,7 @@ def test_view_cart(client, sale, product, request_cart):
 def test_view_cart_with_taxes(client, sale, product, request_cart, vatlayer):
     variant = product.variants.get()
     request_cart.add(variant, 1)
-    response = client.get('/cart/')
+    response = client.get(reverse('cart:index'))
     response_cart_line = response.context[0]['cart_lines'][0]
     cart_line = request_cart.lines.first()
     assert response_cart_line['get_total'].tax.amount
@@ -536,9 +536,8 @@ def test_view_update_cart_quantity_with_taxes(
     variant = product.variants.get()
     request_cart.add(variant, 1)
     response = client.post(
-        '/cart/update/%s/' % (variant.pk,),
-        {'quantity': 3},
-        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        reverse('cart:update-line', kwargs={'variant_id': variant.id}),
+        {'quantity': 3}, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
     assert response.status_code == 200
     assert request_cart.quantity == 3
 
