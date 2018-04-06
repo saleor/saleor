b5bf179f499c796fadfdcf5fe191d81e4673bf17
Adam Janik <adam.janik@mirumee.com>
2018-04-06 15:43:13 +0200

    Set up permissions for mutations

--- saleor/graphql/core/decorators.py
+++ saleor/graphql/core/decorators.py
@@ -3,15 +3,15 @@ from functools import wraps
 from django.core.exceptions import PermissionDenied
 
 
-def permission_required(*permissions):
+def permission_required(permissions):
     def decorator(func):
         @wraps(func)
-        def wrapped_resolver(obj, info, *args, **kwargs):
-            user_permissions = info.context.user.get_all_permissions()
-            for permission in permissions:
-                if permission not in user_permissions:
-                    return PermissionDenied(
-                        'You have no permission to view %s' % info.field_name)
-            return func(obj, info, *args, **kwargs)
-        return wrapped_resolver
+        def wrapper(*args, **kwargs):
+            info = args[1]
+            user = info.context.user
+            if not user.has_perm(permissions):
+                raise PermissionDenied(
+                    'You have no permission to use %s' % info.field_name)
+            return func(*args, **kwargs)
+        return wrapper
     return decorator
--- saleor/graphql/page/mutations.py
+++ saleor/graphql/page/mutations.py
@@ -2,24 +2,27 @@ from ...dashboard.page.forms import PageForm
 from ...page import models
 from ..core.mutations import (
     ModelDeleteMutation, ModelFormMutation, ModelFormUpdateMutation,
-    StaffMemberRequiredMutation)
+    StaffMemberRequiredMixin)
 # TODO: This dummy import allows application to start. Find out why
 from .types import Page
 
 
-class PageCreate(StaffMemberRequiredMutation, ModelFormMutation):
+class PageCreate(StaffMemberRequiredMixin, ModelFormMutation):
+    permissions = 'page.edit_page'
     class Meta:
         description = 'Creates a new page.'
         form_class = PageForm
 
 
-class PageUpdate(StaffMemberRequiredMutation, ModelFormUpdateMutation):
+class PageUpdate(StaffMemberRequiredMixin, ModelFormUpdateMutation):
+    permissions = 'page.edit_page'
     class Meta:
         description = 'Updates an existing page.'
         form_class = PageForm
 
 
-class PageDelete(StaffMemberRequiredMutation, ModelDeleteMutation):
+class PageDelete(StaffMemberRequiredMixin, ModelDeleteMutation):
+    permissions = 'page.edit_page'
     class Meta:
         description = 'Deletes a page.'
         model = models.Page
