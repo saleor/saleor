5c181eb895a695110b3e220c5602d12cff94bf02
Marcin Gębala <maarcin.gebala@gmail.com>
2018-04-10 14:39:03 +0200

    Fix generating path to placeholder

--- saleor/core/templatetags/placeholder.py
+++ saleor/core/templatetags/placeholder.py
@@ -1,10 +1,9 @@
-from django.conf import settings
 from django.template import Library
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 register = Library()
 
 
 @register.simple_tag
 def placeholder(size):
-    return '{static_url}images/placeholder{size}x{size}.png'.format(
-        static_url=settings.STATIC_URL, size=size)
+    return static('/images/placeholder{size}x{size}.png'.format(size=size))
