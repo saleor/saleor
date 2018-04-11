552fc37588a934ef70f49f94f1d5f360566dd575
Anna Siwik <anna.siwik@mirumee.com>
2018-04-11 14:03:55 +0200

    Handle setting tax rate type for products

--- saleor/product/migrations/0060_product_taxes.py
+++ saleor/product/migrations/0060_product_taxes.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('product', '0058_auto_20180329_0142'),
+        ('product', '0059_generate_variant_name_from_attrs'),
     ]
 
     operations = [
