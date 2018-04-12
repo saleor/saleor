8a88fd66a65b739b7cbe5d9be362820c49613ed0
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-04-12 13:47:49 +0200

    Assign menu to the site in migrations

---
+++ saleor/site/migrations/0013_assign_default_menus.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+
+def assign_default_menus(apps, schema_editor):
+    Menu = apps.get_model('menu', 'Menu')
+    top_menu = Menu.objects.get_or_create(name='navbar')[0]
+    bottom_menu = Menu.objects.get_or_create(name='footer')[0]
+    Site = apps.get_model('sites', 'Site')
+    site = Site.objects.get_current()
+    site_settings = site.settings
+    site_settings.top_menu = top_menu
+    site_settings.bottom_menu = bottom_menu
+    site_settings.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('menu', '0003_auto_20180405_0854'),
+        ('site', '0012_auto_20180405_0757'),
+    ]
+
+    operations = [
+        migrations.RunPython(assign_default_menus, migrations.RunPython.noop)
+    ]
