f80da87198b4a0d0b78a7261cf62580cc7186015
Adam Janik <adam.janik@mirumee.com>
2018-04-13 17:24:47 +0200

    WIP: draft

--- saleor/graphql/api.py
+++ saleor/graphql/api.py
@@ -14,7 +14,7 @@ from .product.mutations import (
     CategoryCreateMutation, CategoryDelete, CategoryUpdateMutation,
     ProductCreateMutation, ProductDeleteMutation, ProductUpdateMutation,
     ProductTypeCreateMutation, ProductTypeDeleteMutation,
-    ProductTypeUpdateMutation)
+    ProductTypeUpdateMutation, ProductVariantCreateMutation)
 from .product.resolvers import (
     resolve_attributes, resolve_categories, resolve_products,
     resolve_product_types)
@@ -107,6 +107,8 @@ class Mutations(graphene.ObjectType):
     product_type_create = ProductTypeCreateMutation.Field()
     product_type_update = ProductTypeUpdateMutation.Field()
     product_type_delete = ProductTypeDeleteMutation.Field()
+
+    product_variant_create = ProductVariantCreateMutation.Field()
 
 
 schema = graphene.Schema(Query, Mutations)
--- saleor/graphql/product/forms.py
+++ saleor/graphql/product/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from ...product.models import Product
+from ...product.models import Product, ProductVariant
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         exclude = ['updated_at']
+
+
+class ProductVariantForm(forms.ModelForm):
+    class Meta:
+        model = ProductVariant
+        exclude = ['images' ,'name', 'product', 'quantity_allocated']
