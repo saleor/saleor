6d0124c2589babb8a0910bd7254d8694f768318b
Adam Janik <adam.janik@mirumee.com>
2018-04-17 14:25:11 +0200

    Split API tests into different files

---
+++ tests/api/test_category.py
@@ -0,0 +1,176 @@
+import json
+
+import graphene
+import pytest
+from django.shortcuts import reverse
+from tests.utils import get_graphql_content
+
+from saleor.product.models import Category
+
+
+def test_category_query(client, product):
+    category = Category.objects.first()
+    query = '''
+    query {
+        category(id: "%(category_pk)s") {
+            id
+            name
+            ancestors {
+                edges {
+                    node {
+                        name
+                    }
+                }
+            }
+            children {
+                edges {
+                    node {
+                        name
+                    }
+                }
+            }
+        }
+    }
+    ''' % {'category_pk': graphene.Node.to_global_id('Category', category.pk)}
+    response = client.post(reverse('api'), {'query': query})
+    content = get_graphql_content(response)
+    assert 'errors' not in content
+    category_data = content['data']['category']
+    assert category_data is not None
+    assert category_data['name'] == category.name
+    assert (
+        len(category_data['ancestors']['edges']) ==
+        category.get_ancestors().count())
+    assert (
+        len(category_data['children']['edges']) ==
+        category.get_children().count())
+
+
+def test_category_create_mutation(admin_client):
+    query = """
+        mutation($name: String!, $description: String, $parentId: ID) {
+            categoryCreate(
+                name: $name
+                description: $description
+                parentId: $parentId
+            ) {
+                category {
+                    id
+                    name
+                    slug
+                    description
+                    parent {
+                        name
+                        id
+                    }
+                }
+                errors {
+                    field
+                    message
+                }
+            }
+        }
+    """
+
+    category_name = 'Test category'
+    category_description = 'Test description'
+
+    # test creating root category
+    variables = json.dumps({
+        'name': category_name, 'description': category_description})
+    response = admin_client.post(
+        reverse('api'), {'query': query, 'variables': variables})
+    content = get_graphql_content(response)
+    assert 'errors' not in content
+    data = content['data']['categoryCreate']
+    assert data['errors'] == []
+    assert data['category']['name'] == category_name
+    assert data['category']['description'] == category_description
+    assert not data['category']['parent']
+
+    # test creating subcategory
+    parent_id = data['category']['id']
+    variables = json.dumps({
+        'name': category_name, 'description': category_description,
+        'parentId': parent_id})
+    response = admin_client.post(
+        reverse('api'), {'query': query, 'variables': variables})
+    content = get_graphql_content(response)
+    assert 'errors' not in content
+    data = content['data']['categoryCreate']
+    assert data['errors'] == []
+    assert data['category']['parent']['id'] == parent_id
+
+
+def test_category_update_mutation(admin_client, default_category):
+    query = """
+        mutation($id: ID, $name: String!, $description: String) {
+            categoryUpdate(
+                id: $id
+                name: $name
+                description: $description
+            ) {
+                category {
+                    id
+                    name
+                    description
+                    parent {
+                        id
+                    }
+                }
+                errors {
+                    field
+                    message
+                }
+            }
+        }
+    """
+    # create child category and test that the update mutation won't change
+    # it's parent
+    child_category = default_category.children.create(name='child')
+
+    category_name = 'Updated name'
+    category_description = 'Updated description'
+
+    category_id = graphene.Node.to_global_id('Category', child_category.pk)
+    variables = json.dumps({
+        'name': category_name, 'description': category_description,
+        'id': category_id})
+    response = admin_client.post(
+        reverse('api'), {'query': query, 'variables': variables})
+    content = get_graphql_content(response)
+    assert 'errors' not in content
+    data = content['data']['categoryUpdate']
+    assert data['errors'] == []
+    assert data['category']['id'] == category_id
+    assert data['category']['name'] == category_name
+    assert data['category']['description'] == category_description
+
+    parent_id = graphene.Node.to_global_id('Category', default_category.pk)
+    assert data['category']['parent']['id'] == parent_id
+
+
+def test_category_delete_mutation(admin_client, default_category):
+    query = """
+        mutation($id: ID!) {
+            categoryDelete(id: $id) {
+                category {
+                    name
+                }
+                errors {
+                    field
+                    message
+                }
+            }
+        }
+    """
+    variables = json.dumps({
+        'id': graphene.Node.to_global_id('Category', default_category.id)})
+    response = admin_client.post(
+        reverse('api'), {'query': query, 'variables': variables})
+    content = get_graphql_content(response)
+    assert 'errors' not in content
+    data = content['data']['categoryDelete']
+    assert data['category']['name'] == default_category.name
+    with pytest.raises(default_category._meta.model.DoesNotExist):
+        default_category.refresh_from_db()
