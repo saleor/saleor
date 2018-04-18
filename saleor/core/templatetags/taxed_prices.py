e54994d24c9f25d41f8c1866c70454055f2011d1
Anna Siwik <anna.siwik@mirumee.com>
2018-04-18 20:57:08 +0200

    Move taxed prices templatetags to core

--- saleor/core/templatetags/taxed_prices.py
+++ saleor/core/templatetags/taxed_prices.py
@@ -13,6 +13,16 @@ def price_range(context, price_range):
         'display_gross_prices': display_gross_prices}
 
 
+@register.simple_tag
+def shipping_price_range(shipping_method, taxes):
+    """Return shipping price to display (gross or net) depending on settings.
+
+    Extra with_taxes param forces return of gross (if true)
+    or net (if false) price.
+    """
+    return shipping_method.get_price_range(taxes)
+
+
 @register.simple_tag(takes_context=True)
 def price_to_display(context, price, with_taxes=None):
     """Return price to display (gross or net) depending on settings.
@@ -27,11 +37,11 @@ def price_to_display(context, price, with_taxes=None):
     return price.gross if display_gross_prices else price.net
 
 
-@register.simple_tag()
-def tax_value(taxes, tax_rate):
+@register.simple_tag
+def tax_rate(taxes, rate_name):
     """Return tax rate value for given tax rate type in current country."""
     if not taxes:
         return 0
 
-    tax = taxes.get(tax_rate) or taxes.get(DEFAULT_TAX_RATE_NAME)
+    tax = taxes.get(rate_name) or taxes.get(DEFAULT_TAX_RATE_NAME)
     return tax['value']
