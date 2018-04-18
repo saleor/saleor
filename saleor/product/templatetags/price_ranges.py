9fdfaa19a04cd808d88639c530d87293b6ee7d47
Anna Siwik <anna.siwik@mirumee.com>
2018-04-18 16:22:10 +0200

    Add debug data to all prices

--- saleor/product/templatetags/price_ranges.py
+++ saleor/product/templatetags/price_ranges.py
@@ -1,5 +1,7 @@
 from django import template
 
+from ...core.utils import DEFAULT_TAX_RATE_NAME
+
 register = template.Library()
 
 
@@ -23,3 +25,13 @@ def price_to_display(context, price, with_taxes=None):
     else:
         display_gross_prices = with_taxes
     return price.gross if display_gross_prices else price.net
+
+
+@register.simple_tag()
+def tax_value(taxes, tax_rate):
+    """Return tax rate value for given tax rate type in current country."""
+    if not taxes:
+        return 0
+
+    tax = taxes.get(tax_rate) or taxes.get(DEFAULT_TAX_RATE_NAME)
+    return tax['value']
