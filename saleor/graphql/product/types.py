c95d5aab88d38c6336bc91b2b941441bd3762abd
Patryk Zawadzki <patrys@room-303.com>
2018-04-18 18:46:57 +0200

    Fix attribute resolver

--- saleor/graphql/product/types.py
+++ saleor/graphql/product/types.py
@@ -125,6 +125,9 @@ class Product(CountableDjangoObjectType):
             self, context.discounts, context.currency)
         return ProductAvailability(**availability._asdict())
 
+    def resolve_attributes(self, info):
+        return resolve_attribute_list(self.attributes)
+
     def resolve_product_type(self, info):
         return self.product_type
 
