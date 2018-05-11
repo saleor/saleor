e0309a1dc20943b97dd72e898f9f8f833745328f
Adam Janik <adam.janik@mirumee.com>
2018-05-11 13:42:01 +0200

    Add queries for collection(s)

--- saleor/graphql/api.py
+++ saleor/graphql/api.py
@@ -20,9 +20,10 @@ from .product.mutations import (
     ProductVariantDeleteMutation, ProductVariantUpdateMutation,
     ProductImageCreateMutation)
 from .product.resolvers import (
-    resolve_attributes, resolve_categories, resolve_products,
-    resolve_product_types)
-from .product.types import Category, Product, ProductAttribute, ProductType
+    resolve_attributes, resolve_categories, resolve_collections,
+    resolve_products, resolve_product_types)
+from .product.types import (
+    Category, Collection, Product, ProductAttribute, ProductType)
 from .utils import get_node
 
 
@@ -38,6 +39,11 @@ class Query(graphene.ObjectType):
     category = graphene.Field(
         Category, id=graphene.Argument(graphene.ID),
         description='Lookup a category by ID.')
+    collection = graphene.Field(
+        Collection, id=graphene.Argument(graphene.ID), slug=graphene.String(),
+        description='Lookup a collection by ID.')
+    collections = DjangoFilterConnectionField(
+        Collection, description='List of the shop\'s collections.')
     order = graphene.Field(
         Order, description='Lookup an order by ID.',
         id=graphene.Argument(graphene.ID))
@@ -73,6 +79,12 @@ class Query(graphene.ObjectType):
 
     def resolve_categories(self, info, level=None, **kwargs):
         return resolve_categories(info, level)
+
+    def resolve_collection(self, info, id):
+        return get_node(info, id, only_type=Collection)
+
+    def resolve_collections(self, info, **kwargs):
+        resolve_collections(info)
 
     def resolve_page(self, info, id=None, slug=None):
         if slug is not None:
