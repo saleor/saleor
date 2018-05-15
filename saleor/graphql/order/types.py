a6e53d6c59daa7ee2d677099caf5b61fb6a4701f
Adam Janik <adam.janik@mirumee.com>
2018-05-15 12:52:07 +0200

    Initial order query setup

---
+++ saleor/graphql/order/types.py
@@ -0,0 +1,13 @@
+from ..core.types import CountableDjangoObjectType
+from graphene import relay
+
+from ...order import models
+
+class Order(CountableDjangoObjectType):
+
+    class Meta:
+        description = """Represents a version of a product such as different
+        size or color."""
+        interfaces = [relay.Node]
+        model = models.Order
+        filter_fields = ['user_email']
