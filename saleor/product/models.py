cc47bcbcb713599177321577d2223053df5c25a6
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-05-17 12:23:28 +0200

    Fix Price filtering label

--- saleor/product/models.py
+++ saleor/product/models.py
@@ -103,8 +103,7 @@ class Product(SeoModel):
         Category, related_name='products', on_delete=models.CASCADE)
     price = MoneyField(
         currency=settings.DEFAULT_CURRENCY, max_digits=12,
-        decimal_places=settings.DEFAULT_DECIMAL_PLACES,
-        verbose_name=pgettext_lazy('Currency amount', 'Price'))
+        decimal_places=settings.DEFAULT_DECIMAL_PLACES)
     available_on = models.DateField(blank=True, null=True)
     is_published = models.BooleanField(default=True)
     attributes = HStoreField(default={}, blank=True)
