57954a8f6ad6c494913283112e52deb245c499b1
Adam Janik <adam.janik@mirumee.com>
2018-05-17 15:59:48 +0200

    Add OrdeLine and OrderNote types

--- saleor/graphql/order/types.py
+++ saleor/graphql/order/types.py
@@ -1,9 +1,14 @@
-from ..core.types import CountableDjangoObjectType
+import graphene
 from graphene import relay
+from graphene_django import DjangoObjectType
 
 from ...order import models
+from ..core.types import CountableDjangoObjectType
+
 
 class Order(CountableDjangoObjectType):
+    order_id = graphene.Int(
+        description='Order number.')
 
     class Meta:
         description = """Represents a version of a product such as different
@@ -11,3 +16,16 @@ class Order(CountableDjangoObjectType):
         interfaces = [relay.Node]
         model = models.Order
         exclude_fields = []
+
+    def resolve_order_id(self, info):
+        return self.pk
+
+
+class OrderLine(DjangoObjectType):
+    class Meta:
+        model = models.OrderLine
+
+
+class OrderNote(DjangoObjectType):
+    class Meta:
+        model = models.OrderNote
