f733ad2213e589432a43721981212926ab990b85
akjanik <adam.janik@mirumee.com>
2018-05-21 00:08:29 +0200

    Return order based on requesting user

--- saleor/graphql/order/resolvers.py
+++ saleor/graphql/order/resolvers.py
@@ -1,5 +1,24 @@
+from django.core.exceptions import PermissionDenied
+
 from ...order import models
+from ..utils import get_node
+from .types import Order
 
 
-def resolve_orders():
-    return models.Order.objects.all().distinct()
+def resolve_orders(info):
+    user = info.context.user
+    if user.is_anonymous:
+        raise PermissionDenied('You have no permission to see this')
+    if user.get_all_permissions() & {'order.view_order', 'order.edit_order'}:
+        return models.Order.objects.all().distinct()
+    return models.Order.objects.filter(user=user).distinct()
+
+
+def resolve_order(info, id):
+    order = get_node(info, id, only_type=Order)
+    user = info.context.user
+    if (order.user == user
+        or user.is_superuser
+        or user.get_all_permissions() & {
+            'order.view_order', 'order.edit_order'}):
+        return order
