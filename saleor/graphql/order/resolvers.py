30523a200b52a98c26ba6627c8e162d23e8465af
Adam Janik <adam.janik@mirumee.com>
2018-05-22 10:51:47 +0200

    Improve order quering

--- saleor/graphql/order/resolvers.py
+++ saleor/graphql/order/resolvers.py
@@ -10,15 +10,14 @@ def resolve_orders(info):
     if user.is_anonymous:
         raise PermissionDenied('You have no permission to see this')
     if user.get_all_permissions() & {'order.view_order', 'order.edit_order'}:
-        return models.Order.objects.all().distinct()
-    return models.Order.objects.filter(user=user).distinct()
+        return models.Order.objects.all().distinct().prefetch_related('lines')
+    return user.orders.confirmed().distinct().prefetch_related('lines')
 
 
 def resolve_order(info, id):
     """Return order only for user assigned to it or proper staff user."""
     order = get_node(info, id, only_type=Order)
     user = info.context.user
-    if (order.user == user
-        or user.get_all_permissions() & {
+    if (order.user == user or user.get_all_permissions() & {
             'order.view_order', 'order.edit_order'}):
         return order
