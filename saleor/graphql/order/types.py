f7b5928924059a7f96211c08a0c8843b8f1184e8
Adam Janik <adam.janik@mirumee.com>
2018-05-22 16:04:42 +0200

    Make order more complete

--- saleor/graphql/order/types.py
+++ saleor/graphql/order/types.py
@@ -7,8 +7,12 @@ from ..core.types import CountableDjangoObjectType
 
 
 class Order(CountableDjangoObjectType):
+    is_paid = graphene.Boolean(description='Information if order is paid.')
     order_id = graphene.Int(
         description='Order number.')
+    payment_status = graphene.String(description='Information about payment')
+    payment_status_display = graphene.String(
+        description='Translated payment status')
     status_display = graphene.String(description='Translated status display.')
 
     class Meta:
@@ -19,11 +23,27 @@ class Order(CountableDjangoObjectType):
             'shipping_price_gross', 'shipping_price_net', 'total_gross',
             'total_net']
 
+    def resolve_is_paid(self, info):
+        return self.is_fully_paid()
+
     def resolve_order_id(self, info):
         return self.pk
 
+    def resolve_payment_status(self, info):
+        return self.get_last_payment_status()
+
+    def resolve_payment_status_display(self, info):
+        return self.get_last_payment_status_display()
+
     def resolve_status_display(self, info):
         return self.get_status_display()
+
+
+class OrderHistoryEntry(DjangoObjectType):
+    class Meta:
+        description = 'History log of the order.'
+        model = models.OrderHistoryEntry
+        exclude_fields = ['order']
 
 
 class OrderLine(DjangoObjectType):
@@ -37,3 +57,15 @@ class OrderNote(DjangoObjectType):
     class Meta:
         description = 'Note from customer or staff user.'
         model = models.OrderNote
+
+
+class Fulfillment(DjangoObjectType):
+    class Meta:
+        description = 'Represents order fulfillment.'
+        model = models.Fulfillment
+
+
+class FulfillmentLine(DjangoObjectType):
+    class Meta:
+        description = 'Represents line of the fulfillment.'
+        model = models.FulfillmentLine
