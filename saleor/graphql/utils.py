baaa3e432bbd8403ffcf8a2dac9d2241b1c897b7
akjanik <adam.janik@mirumee.com>
2018-05-23 14:31:28 +0200

    Return queryset instead of arbitrary list type

--- saleor/graphql/utils.py
+++ saleor/graphql/utils.py
@@ -16,14 +16,14 @@ def get_node(info, id, only_type=None):
 
 
 def get_nodes(ids, graphene_type):
-    """Return list of nodes of proper type."""
+    """Return queryset of nodes of proper type."""
     pks = []
     for graphql_id in ids:
         _type, _id = from_global_id(graphql_id)
         assert str(graphene_type) == _type, (
             'Must receive an {} id.').format(graphene_type._meta.name)
         pks.append(_id)
-    return list(graphene_type._meta.model.objects.filter(pk__in=pks))
+    return graphene_type._meta.model.objects.filter(pk__in=pks)
 
 
 def get_attributes_dict_from_list(attributes, attr_slug_id):
