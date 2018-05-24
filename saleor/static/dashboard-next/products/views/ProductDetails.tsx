589259cf99f5e1c077b9d4b1c921df18418fd9fa
dominik-zeglen <flesz3@o2.pl>
2018-05-24 16:06:56 +0200

    Enable dashboard 2.0 product detail viewing

--- saleor/static/dashboard-next/products/views/ProductDetails.tsx
+++ saleor/static/dashboard-next/products/views/ProductDetails.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 
 import { productEditUrl, productImageEditUrl, productShowUrl } from "..";
 import { productStorefrontUrl } from "../";
+import ErrorMessageCard from "../../components/ErrorMessageCard";
 import Navigator from "../../components/Navigator";
 import ProductCollections from "../components/ProductCollections";
 import ProductDescription from "../components/ProductDescription";
@@ -11,6 +12,10 @@ import ProductDetailsPage from "../components/ProductDetailsPage";
 import ProductImages from "../components/ProductImages";
 import ProductPriceAndAvailability from "../components/ProductPriceAndAvailability";
 import ProductVariants from "../components/ProductVariants";
+import {
+  productImageCreateMutation,
+  TypedProductImageCreateMutation
+} from "../mutations";
 import { productDetailsQuery, TypedProductDetailsQuery } from "../queries";
 
 interface ProductDetailsProps {
@@ -37,23 +42,46 @@ export const ProductDetails = decorate<ProductDetailsProps>(
           query={productDetailsQuery}
           variables={{ id }}
         >
-          {({ data, error, loading }) => (
-            <ProductDetailsPage
-              onBack={() => window.history.back()}
-              onCollectionShow={() => () => navigate("#")}
-              onDelete={() => {}}
-              onEdit={id => () => navigate(productEditUrl(id))}
-              onImageEdit={id => () => navigate(productImageEditUrl(id))}
-              onImageUpload={() => {}}
-              onImageReorder={() => {}}
-              onProductPublish={() => {}}
-              onProductShow={id => () => navigate(productShowUrl(id))}
-              onVariantShow={() => {}}
-              // TODO: replace with something nicers
-              placeholderImage={"/static/images/placeholder255x255.png"}
-              product={loading ? undefined : data && data.product}
-            />
-          )}
+          {({ data, error, loading }) => {
+            if (error) {
+              return <ErrorMessageCard message={error.message} />;
+            }
+            return (
+              <TypedProductImageCreateMutation
+                mutation={productImageCreateMutation}
+              >
+                {(uploadImage, { data: imageUploadData, error }) => {
+                  if (error) {
+                    return <ErrorMessageCard message={error.message} />;
+                  }
+
+                  const handleImageUpload = (event: React.ChangeEvent<any>) => {
+                    uploadImage({
+                      variables: { id, file: event.target.files[0] }
+                    });
+                  };
+                  return (
+                    <ProductDetailsPage
+                      onBack={() => window.history.back()}
+                      onCollectionShow={() => () => navigate("#")}
+                      onDelete={() => {}}
+                      onEdit={id => () => navigate(productEditUrl(id))}
+                      onImageEdit={id => () =>
+                        navigate(productImageEditUrl(id))}
+                      onImageUpload={handleImageUpload}
+                      onImageReorder={() => {}}
+                      onProductPublish={() => {}}
+                      onProductShow={id => () => navigate(productShowUrl(id))}
+                      onVariantShow={() => {}}
+                      // TODO: replace with something nicers
+                      placeholderImage={"/static/images/placeholder255x255.png"}
+                      product={data && data.product ? data.product : undefined}
+                    />
+                  );
+                }}
+              </TypedProductImageCreateMutation>
+            );
+          }}
         </TypedProductDetailsQuery>
       )}
     </Navigator>
