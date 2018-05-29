3697121a67078adeeb1d682db206fe551461a066
Adam Janik <adam.janik@mirumee.com>
2018-05-29 12:04:17 +0200

    Add query param to other endpoints

--- saleor/graphql/page/resolvers.py
+++ saleor/graphql/page/resolvers.py
@@ -1,7 +1,14 @@
 from ...page import models
+from ..utils import filter_by_query_param
+
+PAGE_SEARCH_FIELDS = ('content', 'slug', 'title')
 
 
-def resolve_pages(user):
+def resolve_pages(user, query):
     if user.is_authenticated and user.is_active and user.is_staff:
-        return models.Page.objects.all().distinct()
-    return models.Page.objects.public().distinct()
+        queryset = models.Page.objects.all().distinct()
+        queryset = filter_by_query_param(queryset, query, PAGE_SEARCH_FIELDS)
+        return queryset
+    queryset = models.Page.objects.public().distinct()
+    queryset = filter_by_query_param(queryset, query, PAGE_SEARCH_FIELDS)
+    return queryset
