ad91288bcf59a5898e6fc24db5e7d0b324a759c6
Adam Janik <adam.janik@mirumee.com>
2018-05-29 15:31:44 +0200

    Add generated description to query arguments

---
+++ saleor/graphql/descriptions.py
@@ -0,0 +1,16 @@
+from .order.resolvers import ORDER_SEARCH_FIELDS
+from .page.resolvers import PAGE_SEARCH_FIELDS
+from .product.resolvers import (
+    ATTRIBUTES_SEARCH_FIELDS, CATEGORY_SEARCH_FIELDS, COLLECTION_SEARCH_FIELDS,
+    PRODUCT_SEARCH_FIELDS)
+from .utils import generate_query_argument_description
+
+DESCRIPTIONS = {
+    'attributes': generate_query_argument_description(
+        ATTRIBUTES_SEARCH_FIELDS),
+    'category': generate_query_argument_description(CATEGORY_SEARCH_FIELDS),
+    'collection': generate_query_argument_description(
+        COLLECTION_SEARCH_FIELDS),
+    'order': generate_query_argument_description(ORDER_SEARCH_FIELDS),
+    'page': generate_query_argument_description(PAGE_SEARCH_FIELDS),
+    'product': generate_query_argument_description(PRODUCT_SEARCH_FIELDS)}
