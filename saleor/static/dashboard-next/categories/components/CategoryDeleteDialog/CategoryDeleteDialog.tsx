a1ec4817ca042229e977aaf59db598d75160b19d
Dominik Żegleń <flesz3@o2.pl>
2018-06-05 16:09:33 +0200

    Show category pages in storybook (#2250)
    * wip
    
    * Rewrite categories section
    
    * Fix few minor bugs
    
    * Update snapshots
    
    * Update stories

---
+++ saleor/static/dashboard-next/categories/components/CategoryDeleteDialog/CategoryDeleteDialog.tsx
@@ -0,0 +1,86 @@
+import Button from "material-ui/Button";
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogProps,
+  DialogTitle
+} from "material-ui/Dialog";
+import { withStyles } from "material-ui/styles";
+import * as React from "react";
+
+import i18n from "../../../i18n";
+
+const decorate = withStyles(theme => ({
+  deleteButton: {
+    "&:hover": {
+      backgroundColor: theme.palette.error.main
+    },
+    backgroundColor: theme.palette.error.main,
+    color: theme.palette.error.contrastText
+  }
+}));
+
+export interface CategoryDeleteDialogProps {
+  open: boolean;
+  name: string;
+  productCount?: number;
+  onClose?();
+  onConfirm?();
+}
+
+const CategoryDeleteDialog = decorate<CategoryDeleteDialogProps>(props => {
+  const {
+    children,
+    classes,
+    name,
+    open,
+    onConfirm,
+    onClose,
+    productCount,
+    ...dialogProps
+  } = props;
+  return (
+    <Dialog open={open}>
+      <DialogTitle>
+        {i18n.t("Delete category", { context: "title" })}
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText
+          dangerouslySetInnerHTML={{
+            __html: i18n.t(
+              "Are you sure you want to remove <strong>{{name}}</strong>?",
+              { name }
+            )
+          }}
+        />
+        {productCount && productCount > 0 ? (
+          <DialogContentText>
+            {i18n.t(
+              "There are {{count}} product(s) in this category that will also be removed.",
+              {
+                count: productCount
+              }
+            )}
+          </DialogContentText>
+        ) : (
+          undefined
+        )}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>
+          {i18n.t("Cancel", { context: "button" })}
+        </Button>
+        <Button
+          className={classes.deleteButton}
+          variant="raised"
+          onClick={onConfirm}
+        >
+          {i18n.t("Delete category", { context: "button" })}
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+});
+
+export default CategoryDeleteDialog;
---
+++ saleor/static/dashboard-next/categories/views/CategoryCreate.tsx
@@ -0,0 +1,56 @@
+import Card from "material-ui/Card";
+import * as React from "react";
+import { Redirect } from "react-router-dom";
+
+import { NavigatorLink } from "../../components/Navigator";
+import PageHeader from "../../components/PageHeader";
+import CategoryEditPage from "../components/CategoryEditPage";
+import { categoryShowUrl } from "../index";
+import {
+  categoryCreateMutation,
+  TypedCategoryCreateMutation
+} from "../mutations";
+
+interface CategoryCreateFormProps {
+  parentId: string;
+}
+
+export const CategoryCreateForm: React.StatelessComponent<
+  CategoryCreateFormProps
+> = ({ parentId }) => (
+  <TypedCategoryCreateMutation mutation={categoryCreateMutation}>
+    {(mutate, { called, data: createResult, loading: createInProgress }) => {
+      if (
+        called &&
+        !createInProgress &&
+        createResult &&
+        createResult.categoryCreate.errors.length === 0
+      ) {
+        return (
+          <Redirect
+            to={categoryShowUrl(createResult.categoryCreate.category.id)}
+            push={false}
+          />
+        );
+      }
+      const errors =
+        called && !createInProgress && createResult
+          ? createResult.categoryCreate.errors
+          : [];
+      return (
+        <NavigatorLink to={categoryShowUrl(parentId)}>
+          {handleCancel => (
+            <CategoryEditPage
+              category={{ description: "", name: "" }}
+              errors={errors}
+              disabled={createInProgress}
+              variant="add"
+              onBack={handleCancel}
+              onSubmit={data => mutate({ variables: { ...data, parentId } })}
+            />
+          )}
+        </NavigatorLink>
+      );
+    }}
+  </TypedCategoryCreateMutation>
+);
