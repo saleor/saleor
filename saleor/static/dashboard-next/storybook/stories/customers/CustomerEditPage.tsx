e3a01bfa2bd58c299dfb0277081c6499df946cac
dominik-zeglen <flesz3@o2.pl>
2018-06-06 16:03:18 +0200

    Add story

--- saleor/static/dashboard-next/storybook/stories/customers/CustomerEditPage.tsx
+++ saleor/static/dashboard-next/storybook/stories/customers/CustomerEditPage.tsx
@@ -5,6 +5,10 @@ import CustomerEditPage from "../../../customers/components/CustomerEditPage";
 import { customer } from "../../../customers/fixtures";
 import Decorator from "../../Decorator";
 
+const errors = [
+  { field: "email", message: "This field cannot be empty" },
+  { field: "note", message: "this field cannot be empty" }
+];
 const callbacks = {
   onBack: () => {},
   onSubmit: () => {}
@@ -14,6 +18,14 @@ storiesOf("Views / Customers / Edit customer", module)
   .addDecorator(Decorator)
   .add("default", () => (
     <CustomerEditPage customer={customer} variant="edit" {...callbacks} />
+  ))
+  .add("with errors", () => (
+    <CustomerEditPage
+      customer={customer}
+      variant="edit"
+      errors={errors}
+      {...callbacks}
+    />
   ))
   .add("when adding new customer", () => (
     <CustomerEditPage variant="add" {...callbacks} />
