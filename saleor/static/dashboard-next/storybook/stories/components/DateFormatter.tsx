32990ce1bc594036e2d2412dba89d5a06c3a31dd
dominik-zeglen <flesz3@o2.pl>
2018-06-06 14:34:15 +0200

    Fix stories

--- saleor/static/dashboard-next/storybook/stories/components/DateFormatter.tsx
+++ saleor/static/dashboard-next/storybook/stories/components/DateFormatter.tsx
@@ -6,13 +6,13 @@ import Decorator from "../../Decorator";
 
 storiesOf("Generics / DateFormatter", module)
   .addDecorator(Decorator)
-  .add("default", () => <DateFormatter date="2018-04-07T10:44:44+00:00" />)
+  .add("default", () => <DateFormatter date="2018-04-07T10:44:44" />)
   .add("humanized", () => (
-    <DateFormatter date={"2018-05-27T22:44:44+00:00"} dateNow={1527521275866} />
+    <DateFormatter date={"2018-05-27T22:44:44"} dateNow={1527521275866} />
   ))
   .add("humanized without tooltip", () => (
     <DateFormatter
-      date={"2018-05-27T22:44:44+00:00"}
+      date={"2018-05-27T22:44:44"}
       dateNow={1527521275866}
       showTooltip={false}
     />
