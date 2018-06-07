db865d7b41d42ccb687ed7f1e644b69776b8624b
Adam Janik <adam.janik@mirumee.com>
2018-06-07 17:25:50 +0200

    Add languages to the shop display

--- saleor/graphql/core/resolvers.py
+++ saleor/graphql/core/resolvers.py
@@ -1,10 +1,13 @@
 from .types import Shop
 from ...core.permissions import get_permissions
-from .types import PermissionDisplay
+from .types import LanguageDisplay
+from django.conf import settings
+from ..utils import format_permissions_for_display
 
 def resolve_shop(root, info):
     permissions = get_permissions()
-    permissions = [PermissionDisplay(
-            code='.'.join([permission.content_type.app_label, permission.codename]),
-            name=permission.name) for permission in permissions]
-    return Shop(permissions=permissions)
+    permissions = format_permissions_for_display(permissions)
+    languages = settings.LANGUAGES
+    languages = [LanguageDisplay(
+        code=language[0], language=language[1]) for language in languages]
+    return Shop(permissions=permissions, languages=languages)
