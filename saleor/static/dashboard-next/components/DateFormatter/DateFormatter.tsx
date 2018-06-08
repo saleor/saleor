ba17b0fa3f37e75b1abf5551031d3c0149306964
Dominik Żegleń <flesz3@o2.pl>
2018-06-08 13:22:16 +0200

    Minor fixes (#2257)
    * Fix dateformatter
    
    * Fix missing `key` prop
    
    * Fix stories
    
    * Disable DateFormatter

--- saleor/static/dashboard-next/components/DateFormatter/DateFormatter.tsx
+++ saleor/static/dashboard-next/components/DateFormatter/DateFormatter.tsx
@@ -28,36 +28,7 @@ const DateFormatter = decorate<DateFormatterProps>(
     locale,
     typography
   }) => {
-    const momentDate = inputFormat ? moment(date, inputFormat) : moment(date);
-    if (!dateNow || moment(dateNow).diff(momentDate, "days") > 1) {
-      return (
-        <Typography
-          className={classes.root}
-          variant={(typography as any) || "body1"}
-        >
-          {momentDate.format(outputFormat || "DD.MM.YYYY HH:mm")}
-        </Typography>
-      );
-    }
-    if (showTooltip) {
-      return (
-        <Tooltip
-          title={momentDate.format(outputFormat || "DD.MM.YYYY HH:mm")}
-          placement="bottom"
-        >
-          <Typography className={classes.root}>
-            {locale
-              ? momentDate.locale(locale).fromNow()
-              : momentDate.fromNow()}
-          </Typography>
-        </Tooltip>
-      );
-    }
-    return (
-      <Typography className={classes.root}>
-        {locale ? momentDate.locale(locale).fromNow() : momentDate.fromNow()}
-      </Typography>
-    );
+    return <span>{date}</span>;
   }
 );
 export default DateFormatter;
--- saleor/static/dashboard-next/storybook/stories/components/DateFormatter.tsx
+++ saleor/static/dashboard-next/storybook/stories/components/DateFormatter.tsx
@@ -6,22 +6,4 @@ import Decorator from "../../Decorator";
 
 storiesOf("Generics / DateFormatter", module)
   .addDecorator(Decorator)
-  .add("default", () => <DateFormatter date="2018-04-07T10:44:44+00:00" />)
-  .add("humanized", () => (
-    <DateFormatter date={"2018-05-27T22:44:44+00:00"} dateNow={1527521275866} />
-  ))
-  .add("humanized without tooltip", () => (
-    <DateFormatter
-      date={"2018-05-27T22:44:44+00:00"}
-      dateNow={1527521275866}
-      showTooltip={false}
-    />
-  ))
-  .add("with custom format", () => (
-    <DateFormatter
-      date="01.04.1999 14:30"
-      showTooltip={false}
-      inputFormat="DD.MM.YYYY HH:mm"
-      outputFormat="MM/DD/YYYY"
-    />
-  ));
+  .add("default", () => <DateFormatter date="2018-04-07T10:44:44" />);
