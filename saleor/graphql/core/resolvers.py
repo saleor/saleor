e539696e981c295377abcafa75688edd5d0ef364
Adam Janik <adam.janik@mirumee.com>
2018-06-08 13:25:48 +0200

    Add phone prefixes

--- saleor/graphql/core/resolvers.py
+++ saleor/graphql/core/resolvers.py
@@ -1,8 +1,10 @@
-from .types import Shop
-from ...core.permissions import get_permissions
-from .types import LanguageDisplay
 from django.conf import settings
+from phonenumbers import COUNTRY_CODE_TO_REGION_CODE
+
+from ...core.permissions import get_permissions
 from ..utils import format_permissions_for_display
+from .types import LanguageDisplay, Shop
+
 
 def resolve_shop(root, info):
     permissions = get_permissions()
@@ -10,4 +12,8 @@ def resolve_shop(root, info):
     languages = settings.LANGUAGES
     languages = [LanguageDisplay(
         code=language[0], language=language[1]) for language in languages]
-    return Shop(permissions=permissions, languages=languages)
+    phone_prefixes = list(COUNTRY_CODE_TO_REGION_CODE.keys())
+    return Shop(
+        permissions=permissions,
+        languages=languages,
+        phone_prefixes=phone_prefixes)
