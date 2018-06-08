352b5dd739b96182d3513e33500d42566f63b4ab
Adam Janik <adam.janik@mirumee.com>
2018-06-08 13:55:54 +0200

    Start adding tests

---
+++ tests/api/test_core.py
@@ -0,0 +1,35 @@
+import json
+from tests.utils import get_graphql_content
+from django.shortcuts import reverse
+from phonenumbers import COUNTRY_CODE_TO_REGION_CODE
+from saleor.core.permissions import MODELS_PERMISSIONS
+
+
+def test_shop_endpoint(settings, admin_api_client, admin_user):
+    query = """
+    query shop {
+        shop {
+            permissions {
+                code
+                name
+            }
+            languages {
+                code
+                language
+            }
+            phonePrefixes
+        }
+    }
+    """
+    languages = settings.LANGUAGES
+    response = admin_api_client.post(reverse('api'), {'query': query})
+    content = get_graphql_content(response)
+
+    assert 'errors' not in content
+    data = content['data']['shop']
+    permissions = data['permissions']
+    permissions_codes = {permission.get('code') for permission in permissions}
+    assert len(permissions_codes) == len(MODELS_PERMISSIONS)
+    for code in permissions_codes:
+        assert code in MODELS_PERMISSIONS
+
