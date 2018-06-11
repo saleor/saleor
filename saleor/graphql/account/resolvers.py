1d2c91aa8df07a3eec1724c4c9c0d9cacb5c8289
Adam Janik <adam.janik@mirumee.com>
2018-06-11 18:07:45 +0200

    Add query parameters

--- saleor/graphql/account/resolvers.py
+++ saleor/graphql/account/resolvers.py
@@ -1,15 +1,21 @@
 from graphql_jwt.decorators import login_required
 
 from ...account import models
-from ..utils import get_node
+from ..utils import filter_by_query_param, get_node
 from .types import User
 
+USER_SEARCH_FIELDS = (
+    'email', 'default_shipping_address__first_name',
+    'default_shipping_address__last_name', 'default_shipping_address__city',
+    'default_shipping_address__country')
 
 @login_required
-def resolve_users(info):
+def resolve_users(info, query):
     user = info.context.user
     if user.get_all_permissions() & {'account.view_user', 'account.edit_user'}:
-        qs =  models.User.objects.all().prefetch_related('addresses')
+        qs = models.User.objects.all().prefetch_related('addresses')
+        qs = filter_by_query_param(
+            queryset=qs, query=query, search_fields=USER_SEARCH_FIELDS)
         return qs
     return []
 
