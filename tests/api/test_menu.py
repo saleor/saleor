6317fa9408c3c08cc0cf0e896c19ba4ddf3caa75
akjanik <adam.janik@mirumee.com>
2018-06-13 16:42:51 +0200

    Add tests for menu mutations

--- tests/api/test_menu.py
+++ tests/api/test_menu.py
@@ -1,6 +1,7 @@
 import json
 
 import graphene
+import pytest
 from django.shortcuts import reverse
 from tests.utils import get_graphql_content
 
@@ -70,3 +71,61 @@ def test_menu_items_query(user_api_client, menu_item, collection):
     assert data['name'] == menu_item.name
     assert data['collection']['name'] == menu_item.collection.name
     assert data['children']['totalCount'] == menu_item.children.count()
+
+
+def test_create_menu(admin_api_client):
+    query = """
+    mutation mc($menu_name: String!){
+        menuCreate(input: {name: $menu_name}) {
+            menu {
+                name
+            }
+        }
+    }
+    """
+    variables = json.dumps({'menu_name': 'test-menu'})
+    response = admin_api_client.post(
+        reverse('api'), {'query': query, 'variables': variables})
+    content = get_graphql_content(response)
+    assert 'errors' not in content
+    assert content['data']['menuCreate']['menu']['name'] == 'test-menu'
+
+
+def test_update_menu(admin_api_client, menu):
+    query = """
+    mutation updatemenu($id: ID!, $name: String!) {
+        menuUpdate(id: $id, input: {name: $name}) {
+            menu {
+                name
+            }
+        }
+    }
+    """
+    menu_id = graphene.Node.to_global_id('Menu', menu.pk)
+    name = 'Blue oyster menu'
+    variables = json.dumps({'id': menu_id, 'name': name})
+    response = admin_api_client.post(
+        reverse('api'), {'query': query, 'variables': variables})
+    content = get_graphql_content(response)
+    assert 'errors' not in content
+    assert content['data']['menuUpdate']['menu']['name'] == name
+
+def test_delete_menu(admin_api_client, menu):
+    query = """
+        mutation deletemenu($id: ID!) {
+            menuDelete(id: $id) {
+                menu {
+                    name
+                }
+            }
+        }
+        """
+    menu_id = graphene.Node.to_global_id('Menu', menu.pk)
+    variables = json.dumps({'id': menu_id})
+    response = admin_api_client.post(
+        reverse('api'), {'query': query, 'variables': variables})
+    content = get_graphql_content(response)
+    assert 'errors' not in content
+    assert content['data']['menuDelete']['menu']['name'] == menu.name
+    with pytest.raises(menu._meta.model.DoesNotExist):
+        menu.refresh_from_db()
