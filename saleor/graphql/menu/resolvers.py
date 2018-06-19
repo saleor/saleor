e1a626c08dee67728a50b5f0a558735c31adeda0
Adam Janik <adam.janik@mirumee.com>
2018-06-19 12:56:11 +0200

    Rely on query instead of django-filters

--- saleor/graphql/menu/resolvers.py
+++ saleor/graphql/menu/resolvers.py
@@ -1,13 +1,16 @@
 from ...menu import models
 from ..utils import filter_by_query_param
 
-MENU_SEARCH_FIELDS = ATTRIBUTES_SEARCH_FIELDS = ('name')
+MENU_SEARCH_FIELDS = ('name',)
+MENU_ITEM_SEARCH_FIELDS = ('name',)
 
-def resolve_menus(info):
-    return models.Menu.objects.all()
+def resolve_menus(info, query):
+    queryset = models.Menu.objects.all()
+    queryset = filter_by_query_param(queryset, query, MENU_SEARCH_FIELDS)
+    return queryset
 
 
 def resolve_menu_items(info, query):
     queryset = models.MenuItem.objects.all()
-    queryset = filter_by_query_param(queryset, query, ATTRIBUTES_SEARCH_FIELDS)
+    queryset = filter_by_query_param(queryset, query, MENU_ITEM_SEARCH_FIELDS)
     return queryset
