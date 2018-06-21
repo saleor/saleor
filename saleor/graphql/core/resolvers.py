b80baaffc123bfe298cd18a548075aa841d75eb4
akjanik <adam.janik@mirumee.com>
2018-06-21 14:00:35 +0200

    Add countries field to shop query

--- saleor/graphql/core/resolvers.py
+++ saleor/graphql/core/resolvers.py
@@ -1,9 +1,10 @@
 from django.conf import settings
+from django_countries import countries
 from phonenumbers import COUNTRY_CODE_TO_REGION_CODE
 
 from ...core.permissions import get_permissions
 from ..utils import format_permissions_for_display
-from .types import LanguageDisplay, Shop
+from .types import CountryDisplay, LanguageDisplay, Shop
 
 
 def resolve_shop(root, info):
@@ -13,7 +14,11 @@ def resolve_shop(root, info):
     languages = [LanguageDisplay(
         code=language[0], language=language[1]) for language in languages]
     phone_prefixes = list(COUNTRY_CODE_TO_REGION_CODE.keys())
+    countries_map = [
+        CountryDisplay(
+            code=country[0], country=country[1]) for country in countries]
     return Shop(
         permissions=permissions,
         languages=languages,
-        phone_prefixes=phone_prefixes)
+        phone_prefixes=phone_prefixes,
+        countries=countries_map)
