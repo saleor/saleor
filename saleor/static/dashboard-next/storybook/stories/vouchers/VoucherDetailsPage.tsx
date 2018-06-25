a207d57085cbd429cc070a97bfb83453571d5157
Dominik Żegleń <flesz3@o2.pl>
2018-06-25 15:56:08 +0200

    Add `vouchers` section (#2309)
    * Add voucher list page
    
    * Add voucher details page
    
    * Update snapshots

---
+++ saleor/static/dashboard-next/storybook/stories/vouchers/VoucherDetailsPage.tsx
@@ -0,0 +1,23 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import { categories } from "../../../categories/fixtures";
+import { countries } from "../../../orders/fixtures";
+import { products } from "../../../products/fixtures";
+import VoucherDetailsPage from "../../../vouchers/components/VoucherDetailsPage";
+import { vouchers } from "../../../vouchers/fixtures";
+import Decorator from "../../Decorator";
+
+storiesOf("Views / Vouchers / Voucher details", module)
+  .addDecorator(Decorator)
+  .add("default", () => (
+    <VoucherDetailsPage
+      currency="USD"
+      voucher={vouchers[0]}
+      onVoucherDelete={() => {}}
+      shippingSearchResults={countries}
+      categorySearchResults={categories}
+      productSearchResults={products("")}
+    />
+  ))
+  .add("when loading", () => <VoucherDetailsPage disabled />);
