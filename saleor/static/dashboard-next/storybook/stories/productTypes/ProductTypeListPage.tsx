04f9b8468409fde9183574e019529e514020277f
dominik-zeglen <flesz3@o2.pl>
2018-06-27 18:06:10 +0200

    Add product types list

--- saleor/static/dashboard-next/storybook/stories/productTypes/ProductTypeListPage.tsx
+++ saleor/static/dashboard-next/storybook/stories/productTypes/ProductTypeListPage.tsx
@@ -1,10 +1,17 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
+import { pageListProps } from "../../../fixtures";
 import ProductTypeListPage from "../../../productTypes/components/ProductTypeListPage";
+import { productTypes } from "../../../productTypes/fixtures";
 import Decorator from "../../Decorator";
 
-storiesOf("ProductTypes / ProductTypeListPage", module)
+storiesOf("Views / Product types / Product types list", module)
   .addDecorator(Decorator)
-  .add("default", () => <ProductTypeListPage />)
-  .add("other", () => <ProductTypeListPage />);
+  .add("default", () => (
+    <ProductTypeListPage
+      productTypes={productTypes}
+      {...pageListProps.default}
+    />
+  ))
+  .add("loading", () => <ProductTypeListPage {...pageListProps.loading} />);
