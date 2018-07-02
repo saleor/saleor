faa546e537a49aafe19de99808d828fc38e7437f
dominik-zeglen <flesz3@o2.pl>
2018-07-02 14:05:52 +0200

    Add missing decorators

--- saleor/static/dashboard-next/storybook/stories/pages/PageDeleteDialog.tsx
+++ saleor/static/dashboard-next/storybook/stories/pages/PageDeleteDialog.tsx
@@ -2,12 +2,15 @@ import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
 import PageDeleteDialog from "../../../pages/components/PageDeleteDialog";
+import Decorator from "../../Decorator";
 
-storiesOf("Pages / PageDeleteDialog", module).add("default", () => (
-  <PageDeleteDialog
-    opened={true}
-    onConfirm={() => {}}
-    onClose={() => {}}
-    title="Lorem Ipsum"
-  />
-));
+storiesOf("Pages / PageDeleteDialog", module)
+  .addDecorator(Decorator)
+  .add("default", () => (
+    <PageDeleteDialog
+      opened={true}
+      onConfirm={() => {}}
+      onClose={() => {}}
+      title="Lorem Ipsum"
+    />
+  ));
