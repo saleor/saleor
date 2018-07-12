641649d9b5031cd366142ed71800a8921d0eabcd
Adam Janik <adam.janik@mirumee.com>
2018-07-12 17:47:53 +0200

    Fix grammar

--- saleor/graphql/core/utils.py
+++ saleor/graphql/core/utils.py
@@ -9,6 +9,6 @@ def clean_seo_fields(data):
 def snake_to_camel_case(name):
     """Convert snake_case variable name to camelCase."""
     if isinstance(name, str):
-        splitted = name.split('_')
-        return splitted[0] + "".join(map(str.capitalize, splitted[1:]))
+        split_name = name.split('_')
+        return split_name[0] + "".join(map(str.capitalize, split_name[1:]))
     return name
