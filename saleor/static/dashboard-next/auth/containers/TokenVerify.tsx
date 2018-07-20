9bf498600871633d481a3aee6fe13e18bf39399f
Marcin Gębala <maarcin.gebala@gmail.com>
2018-07-20 17:15:33 +0200

    Add wrappers for token mutations

---
+++ saleor/static/dashboard-next/auth/containers/TokenVerify.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+
+import {
+  PartialMutationProviderProps,
+  PartialMutationProviderRenderProps
+} from "../..";
+import {
+  VerifyTokenMutation,
+  VerifyTokenMutationVariables
+} from "../../gql-types";
+import { tokenVerifyMutation, TypedVerifyTokenMutation } from "../mutations";
+
+interface TokenVerifyProviderProps
+  extends PartialMutationProviderProps<VerifyTokenMutation> {
+  children: PartialMutationProviderRenderProps<
+    VerifyTokenMutation,
+    VerifyTokenMutationVariables
+  >;
+}
+
+const TokenVerifyProvider: React.StatelessComponent<
+  TokenVerifyProviderProps
+> = ({ children, onError, onSuccess }) => (
+  <TypedVerifyTokenMutation
+    mutation={tokenVerifyMutation}
+    onCompleted={onSuccess}
+    onError={onError}
+  >
+    {(mutate, { called, data, error, loading }) =>
+      children({ called, data, error, loading, mutate })
+    }
+  </TypedVerifyTokenMutation>
+);
+
+export default TokenVerifyProvider;
