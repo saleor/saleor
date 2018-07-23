6e370cb3446187cc0336d2470f190bd9544c9123
oldPadavan <evggoncharov@gmail.com>
2018-07-23 14:56:15 +0300

    Added missing variables to email template context

--- saleor/graphql/account/mutations.py
+++ saleor/graphql/account/mutations.py
@@ -1,5 +1,6 @@
 import graphene
 from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.models import Site
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
@@ -11,11 +12,15 @@ from ..core.mutations import BaseMutation, ModelMutation
 from ..core.types import Error
 
 
-def send_user_password_reset_email(user):
+def send_user_password_reset_email(user, use_https=False):
+    site = Site.objects.get_current()
     context = {
         'email': user.email,
         'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
-        'token': default_token_generator.make_token(user)
+        'token': default_token_generator.make_token(user),
+        'site_name': site.name,
+        'domain': site.domain,
+        'protocol': 'https' if use_https else 'http'
     }
     emails.send_password_reset_email.delay(context, user.email)
 
