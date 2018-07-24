f5ab2506e309751d361e932b6dcbd4b772d16f04
Marcin Gębala <maarcin.gebala@gmail.com>
2018-07-24 17:07:17 +0200

    Extend shop endpoint with tax rates, domain data and currencies

--- saleor/graphql/core/resolvers.py
+++ saleor/graphql/core/resolvers.py
@@ -4,21 +4,45 @@ from phonenumbers import COUNTRY_CODE_TO_REGION_CODE
 
 from ...core.permissions import get_permissions
 from ..utils import format_permissions_for_display
-from .types import CountryDisplay, LanguageDisplay, Shop
+from .types import CountryDisplay, LanguageDisplay, Shop, Domain
 
 
-def resolve_shop(root, info):
-    permissions = get_permissions()
-    permissions = format_permissions_for_display(permissions)
-    languages = settings.LANGUAGES
-    languages = [LanguageDisplay(
-        code=language[0], language=language[1]) for language in languages]
-    phone_prefixes = list(COUNTRY_CODE_TO_REGION_CODE.keys())
-    countries_map = [
-        CountryDisplay(
-            code=country[0], country=country[1]) for country in countries]
+def resolve_shop(root, info, *args, **kwargs):
+    site = info.context.site
     return Shop(
-        permissions=permissions,
-        languages=languages,
-        phone_prefixes=phone_prefixes,
-        countries=countries_map)
+        countries=resolve_countries(info),
+        currencies=settings.AVAILABLE_CURRENCIES,
+        domain=resolve_domain(info, site),
+        default_currency=settings.DEFAULT_CURRENCY,
+        languages=resolve_languages(info),
+        name=site.name,
+        permissions=resolve_permissions(info),
+        phone_prefixes=resolve_phone_prefixes(info))
+
+
+def resolve_domain(info, site):
+    return Domain(
+        host=site.domain,
+        ssl_enabled=settings.ENABLE_SSL,
+        url=info.context.build_absolute_uri('/'))
+
+
+def resolve_phone_prefixes(info):
+    return list(COUNTRY_CODE_TO_REGION_CODE.keys())
+
+
+def resolve_countries(info):
+    return [
+        CountryDisplay(code=country[0], country=country[1])
+        for country in countries]
+
+
+def resolve_languages(info):
+    return [
+        LanguageDisplay(code=language[0], language=language[1])
+        for language in settings.LANGUAGES]
+
+
+def resolve_permissions(info):
+    permissions = get_permissions()
+    return format_permissions_for_display(permissions)
