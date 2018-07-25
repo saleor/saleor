23c995f9ce031c37d1d49e63feebe4a2d807f158
Marcin Gębala <maarcin.gebala@gmail.com>
2018-07-25 16:56:26 +0200

    Fix rebase issues

--- saleor/graphql/account/resolvers.py
+++ saleor/graphql/account/resolvers.py
@@ -6,19 +6,17 @@ from ..utils import filter_by_query_param, get_node
 from .types import User
 
 
-CUSTOMER_SEARCH_FIELDS = (
+USER_SEARCH_FIELDS = (
     'email', 'default_shipping_address__first_name',
     'default_shipping_address__last_name', 'default_shipping_address__city',
     'default_shipping_address__country')
-
-GROUP_SEARCH_FIELDS = ('name', )
 
 
 @permission_required(['account.manage_users'])
 def resolve_users(info, query):
     qs = models.User.objects.all().prefetch_related('addresses')
     return filter_by_query_param(
-        queryset=qs, query=query, search_fields=CUSTOMER_SEARCH_FIELDS)
+        queryset=qs, query=query, search_fields=USER_SEARCH_FIELDS)
 
 
 @permission_required(['account.manage_users'])
--- saleor/graphql/descriptions.py
+++ saleor/graphql/descriptions.py
@@ -1,5 +1,4 @@
-from .account.resolvers import (
-    CUSTOMER_SEARCH_FIELDS, GROUP_SEARCH_FIELDS, STAFF_SEARCH_FIELDS)
+from .account.resolvers import USER_SEARCH_FIELDS
 from .discount.resolvers import SALE_SEARCH_FIELDS, VOUCHER_SEARCH_FIELDS
 from .menu.resolvers import MENU_ITEM_SEARCH_FIELDS, MENU_SEARCH_FIELDS
 from .order.resolvers import ORDER_SEARCH_FIELDS
@@ -16,8 +15,6 @@ DESCRIPTIONS = {
     'category': generate_query_argument_description(CATEGORY_SEARCH_FIELDS),
     'collection': generate_query_argument_description(
         COLLECTION_SEARCH_FIELDS),
-    'customer': generate_query_argument_description(CUSTOMER_SEARCH_FIELDS),
-    'group': generate_query_argument_description(GROUP_SEARCH_FIELDS),
     'menu': generate_query_argument_description(MENU_SEARCH_FIELDS),
     'menu_item': generate_query_argument_description(MENU_ITEM_SEARCH_FIELDS),
     'order': generate_query_argument_description(ORDER_SEARCH_FIELDS),
@@ -25,4 +22,4 @@ DESCRIPTIONS = {
     'product': generate_query_argument_description(PRODUCT_SEARCH_FIELDS),
     'voucher': generate_query_argument_description(VOUCHER_SEARCH_FIELDS),
     'sale': generate_query_argument_description(SALE_SEARCH_FIELDS),
-    'staff': generate_query_argument_description(STAFF_SEARCH_FIELDS)}
+    'user': generate_query_argument_description(USER_SEARCH_FIELDS)}
