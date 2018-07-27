5a09a0da21f168a59ea4dbf5b0655aa3a56363c8
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2018-07-27 19:36:39 +0200

    Add basic grid style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { GitHubLink } from '..';
+import Logo from '../../images/saleor-logo.svg';
 
 const Header = () => (
     <header>
-        <nav>
-            <ul>
-                <li><Link to="/">Saleor</Link></li>
-                <li><Link to="/features">Features</Link></li>
-                <li><a href="https://saleor.readthedocs.io/en/latest/">Docs</a></li>
-                <li><a href="https://medium.com/saleor">Blog</a></li>
-                <GitHubLink owner="mirumee" name="saleor" />
-            </ul>
-        </nav>
+        <div className="container">
+            <div class="grid">
+                <div className="logo column-4">
+                    <Link to="/"><img src={Logo} /></Link>
+                </div>
+                <nav className="menu column-6">
+                    <ul>
+                        <li><Link to="/">Saleor</Link></li>
+                        <li><Link to="/features">Features</Link></li>
+                        <li><a href="https://saleor.readthedocs.io/en/latest/">Docs</a></li>
+                        <li><a href="https://medium.com/saleor">Blog</a></li>
+                        <GitHubLink owner="mirumee" name="saleor" />
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </header>
 )
 
