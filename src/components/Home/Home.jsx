949a896f0bd6fa672d8cf6a90ecd46664ab85b46
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2018-07-30 13:52:00 +0200

    Add basic hero section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 
+import css from './home.css';
+
 class Home extends Component {
   constructor(props) {
     super(props);
   }
 
   render() {
-    console.log(this.props);
     return (
-      <div className="home">
-        Start
+      <div id="home">
+        <section className="hero">
+          <div className="plane">
+            <h1>A graphql-first ecommerce <span class="primaryColor">platform for perfectionists</span></h1>
+            <a href="#" className="btn btn-secondary">See demo</a>
+            <a href="#" className="btn btn-primary">Brief us</a>
+          </div>
+        </section>
       </div>
     );
   }
