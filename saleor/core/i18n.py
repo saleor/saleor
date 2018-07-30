fa40acc4ad7bc225ca084eeae7af5321b0bc154c
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-07-30 11:58:25 +0200

    Change shipping discount voucher choices

--- saleor/core/i18n.py
+++ saleor/core/i18n.py
@@ -2,7 +2,7 @@ from django.utils.translation import pgettext_lazy
 from django_countries import countries
 
 ANY_COUNTRY = ''
-ANY_COUNTRY_DISPLAY = pgettext_lazy('Country choice', 'Rest of World')
+ANY_COUNTRY_DISPLAY = pgettext_lazy('Country choice', 'Any Country')
 COUNTRY_CODE_CHOICES = [(ANY_COUNTRY, ANY_COUNTRY_DISPLAY)] + list(countries)
 
 VAT_RATE_TYPE_TRANSLATIONS = {
