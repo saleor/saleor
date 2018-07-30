474f3e9a8773e10571347e11ddc333e0b6c9cbe6
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2018-07-30 13:11:54 +0200

    Fix indent to 2 spaces

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,26 +7,26 @@ import { GitHubLink } from '..';
 import css from './header.css';
 
 const Header = () => (
-    <header>
-        <div className="container">
-            <div class="grid">
-                <div className="logo column-3">
-                    <Link to="/"><ReactSVG className="star-icon" path="images/saleor-logo.svg" /></Link>
-                </div>
-                <nav className="menu column-7">
-                    <ul>
-                        <li><Link to="/features">Features</Link></li>
-                        <li><Link to="/">Roadmap</Link></li>
-                        <li><a href="https://saleor.readthedocs.io/en/latest/">Docs</a></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><a href="https://medium.com/saleor">Blog</a></li>
-                        <li><GitHubLink owner="mirumee" name="saleor" /></li>
-                        <li><a class="btn btn-primary" href="#">Contact Us</a></li>
-                    </ul>
-                </nav>
-            </div>
+  <header>
+    <div className="container">
+      <div class="grid">
+        <div className="logo column-3">
+          <Link to="/"><ReactSVG className="star-icon" path="images/saleor-logo.svg" /></Link>
         </div>
-    </header>
+        <nav className="menu column-7">
+          <ul>
+            <li><Link to="/features">Features</Link></li>
+            <li><Link to="/">Roadmap</Link></li>
+            <li><a href="https://saleor.readthedocs.io/en/latest/">Docs</a></li>
+            <li><Link to="/about">About</Link></li>
+            <li><a href="https://medium.com/saleor">Blog</a></li>
+            <li><GitHubLink owner="mirumee" name="saleor" /></li>
+            <li><a class="btn btn-primary" href="#">Contact Us</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 )
 
 export default Header;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,7 +9,7 @@ class Home extends Component {
     console.log(this.props);
     return (
       <div className="home">
-          Start
+        Start
       </div>
     );
   }
