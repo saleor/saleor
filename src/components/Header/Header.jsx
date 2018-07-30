4d5497898d0c547d3db53f00d20dddf7d4bf73d4
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2018-07-30 13:04:31 +0200

    Add style to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ReactSVG from 'react-svg';
 
 import { GitHubLink } from '..';
-import Logo from '../../images/saleor-logo.svg';
+
+import css from './header.css';
 
 const Header = () => (
     <header>
         <div className="container">
             <div class="grid">
-                <div className="logo column-4">
-                    <Link to="/"><img src={Logo} /></Link>
+                <div className="logo column-3">
+                    <Link to="/"><ReactSVG className="star-icon" path="images/saleor-logo.svg" /></Link>
                 </div>
-                <nav className="menu column-6">
+                <nav className="menu column-7">
                     <ul>
-                        <li><Link to="/">Saleor</Link></li>
                         <li><Link to="/features">Features</Link></li>
+                        <li><Link to="/">Roadmap</Link></li>
                         <li><a href="https://saleor.readthedocs.io/en/latest/">Docs</a></li>
+                        <li><Link to="/about">About</Link></li>
                         <li><a href="https://medium.com/saleor">Blog</a></li>
-                        <GitHubLink owner="mirumee" name="saleor" />
+                        <li><GitHubLink owner="mirumee" name="saleor" /></li>
+                        <li><a class="btn btn-primary" href="#">Contact Us</a></li>
                     </ul>
                 </nav>
             </div>
