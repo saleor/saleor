e04b7c3985b624fb54f8b3701d4fb5e16f70ce5c
oldPadavan <evggoncharov@gmail.com>
2018-08-02 12:38:46 +0300

    Fix templates and tests

--- tests/api/test_graphql.py
+++ tests/api/test_graphql.py
@@ -120,8 +120,8 @@ def test_real_query(user_api_client, product):
             ...ProductPriceFragmentQuery
             __typename
         }
-        thumbnailUrl1x: thumbnailUrl(size: "255x255")
-        thumbnailUrl2x: thumbnailUrl(size: "510x510")
+        thumbnailUrl1x: thumbnailUrl(size: 255)
+        thumbnailUrl2x: thumbnailUrl(size: 510)
         url
         __typename
     }
