88004e4888b2fe5b351db9e7e02053bee2ab6ce9
dominik-zeglen <flesz3@o2.pl>
2018-08-06 15:01:54 +0200

    Paginate page list

--- saleor/static/dashboard-next/pages/views/PageList.tsx
+++ saleor/static/dashboard-next/pages/views/PageList.tsx
@@ -1,14 +1,20 @@
+import { stringify } from "querystring";
 import * as React from "react";
 
 import ErrorMessageCard from "../../components/ErrorMessageCard";
 import Navigator from "../../components/Navigator";
 import i18n from "../../i18n";
 import PageListPage from "../components/PageListPage/PageListPage";
-import { pageAddUrl, pageEditUrl, pageStorefrontUrl } from "../index";
+import {
+  pageAddUrl,
+  pageEditUrl,
+  pageListUrl,
+  pageStorefrontUrl
+} from "../index";
 import { pageListQuery, TypedPageListQuery } from "../queries";
 
 interface PageListProps {
-  filters: any;
+  params: any;
 }
 
 interface PageListState {
@@ -23,100 +29,107 @@ export class PageList extends React.Component<PageListProps, PageListState> {
     }));
   };
   render() {
+    const { params } = this.props;
+    const PAGINATE_BY = 20;
+    const paginationState =
+      params && (params.before || params.after)
+        ? params.after
+          ? {
+              after: params.after,
+              first: PAGINATE_BY
+            }
+          : {
+              before: params.before,
+              last: PAGINATE_BY
+            }
+        : {
+            first: PAGINATE_BY
+          };
     return (
-      <TypedPageListQuery
-        query={pageListQuery}
-        variables={{ first: 4 }}
-        fetchPolicy="network-only"
-      >
-        {({ data, loading, error, fetchMore }) => {
-          if (error) {
-            return (
-              <ErrorMessageCard
-                message={i18n.t("Something went terribly wrong.")}
-              />
-            );
-          }
-
-          const loadNextPage = () => {
-            if (loading) {
-              return;
-            }
-            return fetchMore({
-              updateQuery: (previousResult, { fetchMoreResult }) => {
-                return {
-                  ...fetchMoreResult,
-                  pages: {
-                    ...fetchMoreResult.pages,
-                    pageInfo: {
-                      ...fetchMoreResult.pages.pageInfo,
-                      hasPreviousPage: true
-                    }
-                  }
-                };
-              },
-              variables: {
-                after: data.pages.pageInfo.endCursor,
-                first: 12
-              }
-            });
-          };
-          const loadPreviousPage = () => {
-            if (loading) {
-              return;
-            }
-            return fetchMore({
-              updateQuery: (previousResult, { fetchMoreResult, variables }) => {
-                return {
-                  ...fetchMoreResult,
-                  pages: {
-                    ...fetchMoreResult.pages,
-                    pageInfo: {
-                      ...fetchMoreResult.pages.pageInfo,
-                      hasNextPage: true
-                    }
-                  }
-                };
-              },
-              variables: {
-                before: data.pages.pageInfo.startCursor,
-                first: undefined,
-                last: 12
-              }
-            });
-          };
-
-          return (
-            <Navigator>
-              {navigate => {
-                const handleEditClick = (id: string) => () =>
-                  navigate(pageEditUrl(id));
-                const handleShowPageClick = (slug: string) => () =>
-                  window.open(pageStorefrontUrl(slug));
+      <Navigator>
+        {navigate => (
+          <TypedPageListQuery
+            query={pageListQuery}
+            variables={paginationState}
+            fetchPolicy="network-only"
+          >
+            {({ data, loading, error }) => {
+              if (error) {
                 return (
-                  <PageListPage
-                    disabled={loading}
-                    pages={
-                      data && data.pages
-                        ? data.pages.edges.map(edge => edge.node)
-                        : undefined
-                    }
-                    pageInfo={
-                      data && data.pages && data.pages.pageInfo
-                        ? data.pages.pageInfo
-                        : undefined
-                    }
-                    onAdd={() => navigate(pageAddUrl)}
-                    onRowClick={handleEditClick}
-                    onNextPage={loadNextPage}
-                    onPreviousPage={loadPreviousPage}
+                  <ErrorMessageCard
+                    message={i18n.t("Something went terribly wrong.")}
                   />
                 );
-              }}
-            </Navigator>
-          );
-        }}
-      </TypedPageListQuery>
+              }
+
+              const loadNextPage = () => {
+                if (loading) {
+                  return;
+                }
+                return navigate(
+                  pageListUrl +
+                    "?" +
+                    stringify({
+                      after: data.pages.pageInfo.endCursor
+                    }),
+                  true
+                );
+              };
+              const loadPreviousPage = () => {
+                if (loading) {
+                  return;
+                }
+                return navigate(
+                  pageListUrl +
+                    "?" +
+                    stringify({
+                      before: data.pages.pageInfo.startCursor
+                    }),
+                  true
+                );
+              };
+              const pageInfo =
+                data && data.pages && data.pages.pageInfo
+                  ? {
+                      ...data.pages.pageInfo,
+                      hasNextPage:
+                        !!paginationState.before ||
+                        data.pages.pageInfo.hasNextPage,
+                      hasPreviousPage:
+                        !!paginationState.after ||
+                        data.pages.pageInfo.hasPreviousPage
+                    }
+                  : undefined;
+
+              return (
+                <Navigator>
+                  {navigate => {
+                    const handleEditClick = (id: string) => () =>
+                      navigate(pageEditUrl(id));
+                    const handleShowPageClick = (slug: string) => () =>
+                      window.open(pageStorefrontUrl(slug));
+                    return (
+                      <PageListPage
+                        disabled={loading}
+                        pages={
+                          data && data.pages
+                            ? data.pages.edges.map(edge => edge.node)
+                            : undefined
+                        }
+                        pageInfo={pageInfo}
+                        onAdd={() => navigate(pageAddUrl)}
+                        onRowClick={handleEditClick}
+                        onNextPage={loadNextPage}
+                        onPreviousPage={loadPreviousPage}
+                      />
+                    );
+                  }}
+                </Navigator>
+              );
+            }}
+          </TypedPageListQuery>
+        )}
+      </Navigator>
     );
   }
 }
