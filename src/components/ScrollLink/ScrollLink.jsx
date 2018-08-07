f0f5ac202c56b888001fb8a94fe0364c7872e657
Marcin Skorek <marcin.skorek@mirumee.com>
2018-08-07 13:22:32 +0200

    Add scroll link component

---
+++ src/components/ScrollLink/ScrollLink.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import arrowDownIcon from '../../images/arrow-down-icon.png';
+
+const ScrollButton = (props) => (
+	<div className="scroll-link">
+    <a href={props.to}>
+		  <span>{props.children}</span>
+      <img src={arrowDownIcon} alt="Learn more"/>
+    </a>
+  </div>
+);
+
+export default ScrollButton;
