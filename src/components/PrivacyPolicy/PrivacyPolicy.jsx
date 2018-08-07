68415c9f5c4057310c8895817197cce3e27f7ce7
Marcin Skorek <marcin.skorek@mirumee.com>
2018-08-07 10:06:03 +0200

    Add Privacy Policy component and stylesheet

---
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import css from './privacypolicy.css';
+
+const PrivacyPolicy = () => (
+	<div id="privacy-policy">
+		<section className="hero">
+			<h1>Privacy Policy</h1>
+		</section>
+	</div>
+)
+
+export default PrivacyPolicy;
