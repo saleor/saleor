ec443cbebe3d5db2d592f6eae05c1ee192708db9
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2018-08-08 17:15:58 +0200

    Add basic mobile menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ReactSVG from 'react-svg';
 
@@ -6,27 +6,54 @@ import { GitHubLink } from '..';
 
 import css from './header.css';
 
-const Header = () => (
-  <header>
-    <div className="container">
-      <div className="grid">
-        <div className="logo col-sm-3">
-          <Link to="/"><ReactSVG className="star-icon" path="images/saleor-logo.svg" /></Link>
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleMenu= this.toggleMenu.bind(this);
+    this.state = { mobileMenu: false };
+  }
+
+  toggleMenu() {
+    const currentState = this.state.mobileMenu;
+    this.setState({ mobileMenu: !currentState });
+  };
+
+  render() {
+    return (
+      <header>
+        <div className="container">
+          <div className="grid">
+            <div className={this.state.mobileMenu ? 'logo open col-xs-3 col-sm-3' : 'logo col-xs-3 col-sm-3'}>
+              <Link to="/"><ReactSVG className="star-icon" path="images/saleor-logo.svg" /></Link>
+            </div>
+            <nav className="menu col-xs-9 col-sm-9">
+              <ul className={this.state.mobileMenu ? 'menu-mobile' : null}>
+                {this.state.mobileMenu &&
+                  <li><span className="count">01. </span><Link to="/">Home</Link></li>
+                }
+                <li><span className="count">02. </span><Link to="/features">Features</Link></li>
+                <li><span className="count">03. </span><Link to="/">Roadmap</Link></li>
+                <li><span className="count">04. </span><a href="https://saleor.readthedocs.io/en/latest/">Docs</a></li>
+                <li><span className="count">05. </span><Link to="/about">About</Link></li>
+                <li><span className="count">06. </span><a href="https://medium.com/saleor">Blog</a></li>
+                <li className="github-link"><GitHubLink owner="mirumee" name="saleor" /></li>
+                <li><span className="count">07. </span><a className={this.state.mobileMenu ? null : 'btn btn-primary'} href="#">Contact Us</a></li>
+              </ul>
+              <ul className="mobile-btn">
+                <li className={this.state.mobileMenu ? 'github-link open' : 'github-link'} onClick={this.toggleMenu}><GitHubLink owner="mirumee" name="saleor" /></li>
+                <li className={this.state.mobileMenu ? 'menu-icon open' : 'menu-icon'} onClick={this.toggleMenu}>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-        <nav className="menu col-sm-9">
-          <ul>
-            <li><Link to="/features">Features</Link></li>
-            <li><Link to="/">Roadmap</Link></li>
-            <li><a href="https://saleor.readthedocs.io/en/latest/">Docs</a></li>
-            <li><Link to="/about">About</Link></li>
-            <li><a href="https://medium.com/saleor">Blog</a></li>
-            <li><GitHubLink owner="mirumee" name="saleor" /></li>
-            <li><a className="btn btn-primary" href="#">Contact Us</a></li>
-          </ul>
-        </nav>
-      </div>
-    </div>
-  </header>
-)
+      </header>
+    );
+  }
+}
 
 export default Header;
