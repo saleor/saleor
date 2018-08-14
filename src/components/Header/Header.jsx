0ca157512e69bb10756b7b70008b5a24df7f7026
Marcin Skorek <marcin.skorek@mirumee.com>
2018-08-14 09:26:41 +0200

    Fix toggling active class on menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { withRouter } from 'react-router';
 import ReactSVG from 'react-svg';
 
 import { GitHubLink } from '..';
@@ -19,20 +20,21 @@ class Header extends Component {
   };
 
   render() {
+    const { location } = this.props
     return (
       <header>
         <div className="container">
           <div className="grid">
             <div className={this.state.mobileMenu ? 'logo open col-xs-3 col-sm-3' : 'logo col-xs-3 col-sm-3'}>
-              <Link to="/"><ReactSVG className="logo-svg" path="images/saleor-logo.svg" /></Link>
+              <NavLink to="/"><ReactSVG className="logo-svg" path="images/saleor-logo.svg" /></NavLink>
             </div>
             <nav className="menu col-xs-9 col-sm-9">
               <ul className={this.state.mobileMenu ? 'menu-mobile hovered' : null}>
-                <li className="home"><span className="count">01. </span><Link to="/">Home</Link></li>
-                <li><span className="count">02. </span><Link to="/features">Features</Link></li>
-                <li><span className="count">03. </span><Link to="/roadmap">Roadmap</Link></li>
+                <li><span className="count">01. </span><NavLink exact to="/">Home</NavLink></li>
+                <li><span className="count">02. </span><NavLink to="/features">Features</NavLink></li>
+                <li><span className="count">03. </span><NavLink to="/roadmap">Roadmap</NavLink></li>
                 <li><span className="count">04. </span><a href="https://saleor.readthedocs.io/en/latest/">Docs</a></li>
-                <li><span className="count">05. </span><Link to="/about">About</Link></li>
+                <li><span className="count">05. </span><NavLink to="/about">About</NavLink></li>
                 <li><span className="count">06. </span><a href="https://medium.com/saleor">Blog</a></li>
                 <li className="github-link"><GitHubLink owner="mirumee" name="saleor" /></li>
                 <li><span className="count">07. </span><a className={this.state.mobileMenu ? null : 'btn btn-primary'} href="https://mirumee.com/hire-us/">Contact Us</a></li>
@@ -54,4 +56,4 @@ class Header extends Component {
   }
 }
 
-export default Header;
+export default withRouter(Header);
