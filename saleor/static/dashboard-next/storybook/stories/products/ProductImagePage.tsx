786c0c92875cb7cfd5ae3f7e9d7a8a78c3597ac8
dominik-zeglen <flesz3@o2.pl>
2018-08-14 16:30:00 +0200

    Update stories

--- saleor/static/dashboard-next/storybook/stories/products/ProductImagePage.tsx
+++ saleor/static/dashboard-next/storybook/stories/products/ProductImagePage.tsx
@@ -5,6 +5,7 @@ import * as placeholder from "../../../../images/placeholder1080x1080.png";
 import ProductImagePage from "../../../products/components/ProductImagePage";
 import Decorator from "../../Decorator";
 
+const image = { id: "", url: placeholder };
 const images = (Array(8) as any)
   .fill({ id: "img", url: placeholder })
   .map((image, imageIndex) => ({ ...image, id: image.id + imageIndex }));
@@ -13,18 +14,22 @@ storiesOf("Views / Products / Product image details", module)
   .addDecorator(Decorator)
   .add("when loaded data", () => (
     <ProductImagePage
-      onSubmit={() => {}}
-      image={{ id: "img1", url: placeholder }}
+      description={"Lorem ipsum"}
+      disabled={false}
+      image={image}
       images={images}
       onBack={() => {}}
       onDelete={() => {}}
+      onRowClick={() => () => {}}
+      onSubmit={() => {}}
     />
   ))
   .add("when loading data", () => (
     <ProductImagePage
-      onDelete={() => {}}
       disabled={true}
-      onSubmit={() => {}}
       onBack={() => {}}
+      onDelete={() => {}}
+      onRowClick={() => () => {}}
+      onSubmit={() => {}}
     />
   ));
