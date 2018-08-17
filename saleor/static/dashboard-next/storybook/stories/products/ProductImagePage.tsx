ebabeffa819fd5445c8d6123f08479e0e3c07e52
dominik-zeglen <flesz3@o2.pl>
2018-08-17 13:24:48 +0200

    Fix stories and update snapshots

--- saleor/static/dashboard-next/storybook/stories/products/ProductImagePage.tsx
+++ saleor/static/dashboard-next/storybook/stories/products/ProductImagePage.tsx
@@ -5,7 +5,7 @@ import * as placeholder from "../../../../images/placeholder1080x1080.png";
 import ProductImagePage from "../../../products/components/ProductImagePage";
 import Decorator from "../../Decorator";
 
-const image = { id: "", url: placeholder };
+const image = { id: "", url: placeholder, alt: "Lorem ipsum" };
 const images = (Array(8) as any)
   .fill({ id: "img", url: placeholder })
   .map((image, imageIndex) => ({ ...image, id: image.id + imageIndex }));
@@ -14,7 +14,6 @@ storiesOf("Views / Products / Product image details", module)
   .addDecorator(Decorator)
   .add("when loaded data", () => (
     <ProductImagePage
-      description={"Lorem ipsum"}
       disabled={false}
       image={image}
       images={images}
