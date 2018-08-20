5ce8b920025f109d5acf2fcb5f5f20d139c1501b
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-08-20 11:55:29 +0300

    add dry-atoll url

--- saleor/settings.py
+++ saleor/settings.py
@@ -370,7 +370,7 @@ bootstrap4 = {
 TEST_RUNNER = ''
 
 ALLOWED_HOSTS = get_list(
-    os.environ.get('localhost,127.0.0.1','obscure-cliffs-51663.herokuapp.com'))
+    os.environ.get('localhost,127.0.0.1','dry-atoll-53464.herokuapp.com'))
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
