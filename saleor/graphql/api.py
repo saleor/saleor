b6d8af14ea01b1792a25a0a174c0ec2134d2f783
Artur Smęt <artur@mirumee.com>
2018-08-20 17:22:41 +0200

    Add more checkout mutations

--- saleor/graphql/api.py
+++ saleor/graphql/api.py
@@ -52,7 +52,10 @@ from .shipping.mutations import (
     ShippingPriceCreate, ShippingPriceDelete, ShippingPriceUpdate)
 from .utils import get_node
 from .checkout.types import CheckoutLine, Checkout
-from .checkout.mutations import CheckoutCreate, CheckoutLinesAdd
+from .checkout.mutations import (
+    CheckoutCreate, CheckoutLinesAdd, CheckoutLinesUpdate, CheckoutLineDelete,
+    CheckoutCustomerAttach, CheckoutCustomerDetach,
+    CheckoutShippingAddressUpdate, CheckoutEmailUpdate)
 from .checkout.resolvers import resolve_checkouts
 from .shop.mutations import (
     AuthorizationKeyAdd, AuthorizationKeyDelete, HomepageCollectionUpdate,
@@ -273,6 +276,12 @@ class Mutations(ProductMutations):
 
     checkout_create = CheckoutCreate.Field()
     checkout_lines_add = CheckoutLinesAdd.Field()
+    checkout_lines_update = CheckoutLinesUpdate.Field()
+    checkout_line_delete = CheckoutLineDelete.Field()
+    checkout_customer_attach = CheckoutCustomerAttach.Field()
+    checkout_customer_detach = CheckoutCustomerDetach.Field()
+    checkout_shipping_address_update = CheckoutShippingAddressUpdate.Field()
+    checkout_email_update = CheckoutEmailUpdate.Field()
 
     menu_create = MenuCreate.Field()
     menu_delete = MenuDelete.Field()
--- saleor/graphql/checkout/mutations.py
+++ saleor/graphql/checkout/mutations.py
@@ -1,12 +1,16 @@
 import graphene
-from ..core.mutations import ModelMutation, ModelDeleteMutation, BaseMutation
-from ...checkout import models
-from ..account.types import AddressInput
+from django.db import transaction
+
 from ...account.models import Address
-from ..product.types import ProductVariant
-from ..order.mutations.draft_orders import check_lines_quantity
+from ...checkout import models
 from ...checkout.utils import add_variant_to_cart
-from .types import Checkout
+from ..account.types import AddressInput, User
+from ..core.mutations import BaseMutation, ModelMutation
+from ..order.mutations.draft_orders import check_lines_quantity
+from ..product.types import ProductVariant
+from ..utils import get_node, get_nodes
+from .types import Checkout, CheckoutLine
+
 
 class CheckoutLineInput(graphene.InputObjectType):
     quantity = graphene.Int(description='quantity')
@@ -37,9 +41,7 @@ class CheckoutCreate(ModelMutation):
         lines = input.pop('lines', None)
         if lines:
             variant_ids = [line.get('variant_id') for line in lines]
-            variants = cls.get_nodes_or_error(
-                ids=variant_ids, only_type=ProductVariant, errors=errors,
-                field='variants')
+            variants = get_nodes(variant_ids, ProductVariant)
             quantities = [line.get('quantity') for line in lines]
             line_errors = check_lines_quantity(variants, quantities)
             if line_errors:
@@ -62,7 +64,7 @@ class CheckoutCreate(ModelMutation):
             shipping_address.save()
             instance.shipping_address = shipping_address
         super().save(info, instance, cleaned_input)
-        instance.save(update_fields=['billing_address', 'shipping_address'])
+        instance.save(update_fields=['shipping_address'])
         variants = cleaned_input.get('variants')
         quantities = cleaned_input.get('quantities')
         if variants and quantities:
@@ -83,70 +85,134 @@ class CheckoutLinesAdd(BaseMutation):
         description = 'Adds a checkout line to existing checkout'
 
     @classmethod
-    def mutate(cls, root, info, checkout_id, lines):
+    def mutate(cls, root, info, checkout_id, lines, replace=False):
+        checkout = get_node(info, checkout_id, only_type=Checkout)
+        variants, quantities = None, None
         errors = []
-        checkout = cls.get_node_or_error(
-            id=checkout_id, only_type=Checkout, errors=errors,
-            field='checkout_id', info=info)
-        if errors:
-            return cls(errors=errors)
-        cleaned_data = {}
         if lines:
             variant_ids = [line.get('variant_id') for line in lines]
             variants = cls.get_nodes_or_error(
-                ids=variant_ids, only_type=ProductVariant, errors=errors,
-                field='variants')
+                ids=variant_ids, errors=errors, field='variant_id',
+                only_type=ProductVariant)
             quantities = [line.get('quantity') for line in lines]
             line_errors = check_lines_quantity(variants, quantities)
             if line_errors:
                 for err in line_errors:
                     cls.add_error(errors, field=err[0], message=err[1])
-            else:
-                cleaned_data['variants'] = variants
-                cleaned_data['quantities'] = quantities
 
         if variants and quantities:
             for variant, quantity in zip(variants, quantities):
-                add_variant_to_cart(checkout, variant, quantity)
-        return CheckoutLinesAdd(checkout=checkout)
-
-# class CheckoutLineUpdate(ModelMutation):
-#     class Arguments:
-#         checkout_id = graphene.ID(description='Checkout ID', required=True)
-#         lines = graphene.List(
-#             CheckouLineInput, description='Checkout lines', required=True)
-
-#     checkout = Checkout()
-
-#     class Meta:
-#         description = 'Adds a checkout line to existing checkout'
+                add_variant_to_cart(checkout, variant, quantity, replace=replace)
+        return CheckoutLinesAdd(checkout=checkout, errors=errors)
 
 
-# class CheckoutLineDelete(ModelDeleteMutation):
-#     class Arguments:
-#         id = graphene.ID(description='Checkout line ID')
+class CheckoutLinesUpdate(CheckoutLinesAdd):
+    checkout = graphene.Field(Checkout, description='An updated checkout')
 
-#     class Meta:
-#         description = 'Deletes a checkout line'
-#         model = models.CartLine
+    class Meta:
+        description = 'Updates checkout line in existing checkout'
 
-
-# class CheckoutCustomerAttach(ModelMutation):
-#     class Arguments:
-#         checkout = graphene.ID(description='Checkout ID')
-#         customer_id = graphene.ID(description='Customer ID')
-
-# class CheckoutCustomerDetach(ModelMutation):
-#     class Arguments:
-#         checkout = graphene.ID(description='Checkout ID')
-
-# class CheckoutShippingAddressUpdate(ModelMutation):
-#     class Arguments:
-#         checkout = graphene.ID(description='Checkout ID')
-#         shipping_address = AddressInput(description='Shipping address')
+    @classmethod
+    def mutate(cls, root, info, checkout_id, lines):
+        return super().mutate(root, info, checkout_id, lines, replace=True)
 
 
-# class CheckoutEmailUpdate(ModelMutation):
-#     class Arguments:
-#         checkout = graphene.ID(description='Checkout ID')
-#         email = graphene.String(required=True, description='email')
+class CheckoutLineDelete(BaseMutation):
+    class Arguments:
+        checkout_id = graphene.ID(description='Checkout ID', required=True)
+        line_id = graphene.ID(description='Checkout line ID')
+
+    checkout = graphene.Field(Checkout, description='An updated checkout')
+
+    class Meta:
+        description = 'Deletes a checkout line'
+
+    @classmethod
+    def mutate(cls, root, info, checkout_id, line_id):
+        errors = []
+        checkout = cls.get_node_or_error(
+            info, checkout_id, errors, 'checkout_id', only_type=Checkout)
+        line = cls.get_node_or_error(
+            info, line_id, errors, 'line_id', only_type=CheckoutLine)
+        line.delete()
+        return CheckoutLinesAdd(checkout=checkout, errors=errors)
+
+
+
+class CheckoutCustomerAttach(BaseMutation):
+    class Arguments:
+        checkout_id = graphene.ID(description='Checkout ID', required=True)
+        customer_id = graphene.ID(description='Customer ID', required=True)
+
+    checkout = graphene.Field(Checkout, description='An updated checkout')
+
+    @classmethod
+    def mutate(cls, root, info, checkout_id, customer_id):
+        errors = []
+        checkout = cls.get_node_or_error(
+            info, checkout_id, errors, 'checkout_id', only_type=Checkout)
+        customer = cls.get_node_or_error(
+            info, customer_id, errors, 'customer_id', only_type=User)
+        if checkout and customer:
+            checkout.user = customer
+            checkout.save(update_fields=['user'])
+        return CheckoutCustomerAttach(checkout=checkout, errors=errors)
+
+class CheckoutCustomerDetach(BaseMutation):
+    class Arguments:
+        checkout_id = graphene.ID(description='Checkout ID', required=True)
+
+
+    checkout = graphene.Field(Checkout, description='An updated checkout')
+
+    @classmethod
+    def mutate(cls, root, info, checkout_id):
+        checkout = get_node(info, checkout_id, only_type=Checkout)
+        if checkout:
+            checkout.user = None
+            checkout.save(update_fields=['user'])
+        return CheckoutCustomerDetach(checkout=checkout)
+
+class CheckoutShippingAddressUpdate(BaseMutation):
+    class Arguments:
+        checkout_id = graphene.ID(description='Checkout ID')
+        shipping_address = AddressInput(description='Shipping address')
+
+    checkout = graphene.Field(Checkout, description='An updated checkout')
+
+    class Meta:
+        description = 'Update shipping address in existing Checkout object'
+
+    @classmethod
+    def mutate(cls, root, info, checkout_id, shipping_address):
+        errors = {}
+        checkout = cls.get_node_or_error(
+            info, checkout_id, errors, 'checkout_id', only_type=Checkout)
+        shipping_address = Address(**shipping_address)
+        if checkout and shipping_address:
+            with transaction.atomic():
+                shipping_address.save()
+                checkout.shipping_address = shipping_address
+                checkout.save(update_fields=['shipping_address'])
+        return CheckoutShippingAddressUpdate(checkout=checkout, errors=errors)
+
+class CheckoutEmailUpdate(BaseMutation):
+    class Arguments:
+        checkout_id = graphene.ID(description='Checkout ID')
+        email = graphene.String(required=True, description='email')
+
+    checkout = graphene.Field(Checkout, description='An updated checkout')
+
+    class Meta:
+        description = 'Update email address in existing Checkout object'
+
+    @classmethod
+    def mutate(cls, root, info, checkout_id, email):
+        errors = []
+        checkout = cls.get_node_or_error(
+            info, checkout_id, errors, 'checkout_id', only_type=Checkout)
+        if checkout:
+            checkout.email = email
+            checkout.save(update_fields=['email'])
+
+        return CheckoutEmailUpdate(checkout=checkout, errors=errors)
