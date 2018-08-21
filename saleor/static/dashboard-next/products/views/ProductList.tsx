c022016f8a64df884ddb3edc0525ac4df09577ce
dominik-zeglen <flesz3@o2.pl>
2018-08-21 13:38:29 +0200

    Change card alignment

--- saleor/static/dashboard-next/products/views/ProductList.tsx
+++ saleor/static/dashboard-next/products/views/ProductList.tsx
@@ -15,70 +15,54 @@ interface ProductListProps {
   };
 }
 
-const decorate = withStyles(theme => ({
-  root: {
-    display: "grid",
-    gridGap: theme.spacing.unit + "px",
-    gridTemplateColumns: "100%",
-    [theme.breakpoints.up("md")]: {
-      gridGap: theme.spacing.unit * 2 + "px",
-      gridTemplateColumns: "9fr 4fr"
-    }
-  }
-}));
-
 const PAGINATE_BY = 20;
 
-export const ProductList = decorate<ProductListProps>(({ classes, params }) => (
-  <div className={classes.root}>
-    <Navigator>
-      {navigate => {
-        const paginationState = createPaginationState(PAGINATE_BY, params);
-        return (
-          <TypedProductListQuery
-            query={productListQuery}
-            variables={paginationState}
-            fetchPolicy="network-only"
-          >
-            {({ data, loading, error }) => {
-              if (error) {
-                return <ErrorMessageCard message="Something went wrong" />;
-              }
-              const {
-                loadNextPage,
-                loadPreviousPage,
-                pageInfo
-              } = createPaginationData(
-                navigate,
-                paginationState,
-                productListUrl,
-                data && data.products ? data.products.pageInfo : undefined,
-                loading
-              );
-              return (
-                <div>
-                  <ProductListCard
-                    onAdd={() => navigate(productAddUrl)}
-                    disabled={loading}
-                    products={
-                      data &&
-                      data.products !== undefined &&
-                      data.products !== null
-                        ? data.products.edges.map(p => p.node)
-                        : undefined
-                    }
-                    onNextPage={loadNextPage}
-                    onPreviousPage={loadPreviousPage}
-                    pageInfo={pageInfo}
-                    onRowClick={id => () => navigate(productUrl(id))}
-                  />
-                </div>
-              );
-            }}
-          </TypedProductListQuery>
-        );
-      }}
-    </Navigator>
-  </div>
-));
+export const ProductList: React.StatelessComponent<ProductListProps> = ({
+  params
+}) => (
+  <Navigator>
+    {navigate => {
+      const paginationState = createPaginationState(PAGINATE_BY, params);
+      return (
+        <TypedProductListQuery
+          query={productListQuery}
+          variables={paginationState}
+          fetchPolicy="network-only"
+        >
+          {({ data, loading, error }) => {
+            if (error) {
+              return <ErrorMessageCard message="Something went wrong" />;
+            }
+            const {
+              loadNextPage,
+              loadPreviousPage,
+              pageInfo
+            } = createPaginationData(
+              navigate,
+              paginationState,
+              productListUrl,
+              data && data.products ? data.products.pageInfo : undefined,
+              loading
+            );
+            return (
+              <ProductListCard
+                onAdd={() => navigate(productAddUrl)}
+                disabled={loading}
+                products={
+                  data && data.products !== undefined && data.products !== null
+                    ? data.products.edges.map(p => p.node)
+                    : undefined
+                }
+                onNextPage={loadNextPage}
+                onPreviousPage={loadPreviousPage}
+                pageInfo={pageInfo}
+                onRowClick={id => () => navigate(productUrl(id))}
+              />
+            );
+          }}
+        </TypedProductListQuery>
+      );
+    }}
+  </Navigator>
+);
 export default ProductList;
