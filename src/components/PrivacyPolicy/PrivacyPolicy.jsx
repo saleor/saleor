1b69204a8baf7471e6f39770628636d04085761a
Marcin Skorek <marcin.skorek@mirumee.com>
2018-08-21 17:07:43 +0200

    Fix Privacy Policy font weights

--- src/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -8,16 +8,16 @@ const PrivacyPolicy = () => (
 			<div className="bg-container"></div>
 			<div className="plane">
 				<h1 className="title">Privacy Policy</h1>
-				<p className="text-large">We respect your privacy and transparency is something we highly value. Below you can learn everything about information we collect.</p>
+				<p className="text-large text-light">We respect your privacy and transparency is something we highly value. Below you can learn everything about information we collect.</p>
 			</div>
 			<ScrollLink to="#general-information"> Learn more </ScrollLink>
-      <div className="decoration">
-        <img src="../../images/decoration08.png" />
-      </div>
+			<div className="decoration">
+				<img src="../../images/decoration08.png" />
+			</div>
 		</section>
 		<section id="general-information" className="general-information">
 			<h3 className="title">General Information</h3>
-			<p className="description">The Saleor website is operated by Mirumee Software sp. z.o.o. sp. k. operating at the following address:</p>
+			<p className="description text-light">The Saleor website is operated by Mirumee Software sp. z.o.o. sp. k. operating at the following address:</p>
 			<div className="border-left bold">
 				<h5>Mirumee Software sp. z.o.o. sp. k.</h5>
 				<h5>ul. Tęczowa 7</h5>
@@ -27,20 +27,20 @@ const PrivacyPolicy = () => (
 		</section>
 		<section className="analytics">
 			<h3 className="title">Analytics</h3>
-			<p className="description">This website includes Google Analytics software that collects anonymized information about visitors to help us provide better services. <a href="">See how Google uses your data.</a></p>
+			<p className="description text-light">This website includes Google Analytics software that collects anonymized information about visitors to help us provide better services. <a href="">See how Google uses your data.</a></p>
 		</section>
 		<section className="your-rights">
 			<h3 className="title">Your Rights</h3>
-			<p className="description">GDPR guarantees you a number of rights including but not limited to:</p>
-			<ul className="list">
+			<p className="description text-light">GDPR guarantees you a number of rights including but not limited to:</p>
+			<ul className="list text-light">
 				<li>the right to know what data concerning you is held by any particular company and how it is processed;</li>
 				<li>the right to rectify any inaccurate personal data;</li>
 				<li>the right to be forgotten;</li>
 				<li>the right to restriction of processing;</li>
 				<li>the right to object to processing.</li>
 			</ul>
-      <p className="description">Please consult the <a href="">GDPR text in your language</a> to better understand your rights.</p>
-      <p className="description">We ask that you direct all questions and requests related to your personal data to <a href="">privacy@mirumee.com</a> or that you send them to the postal address of the company listed above </p>
+      <p className="description text-light">Please consult the <a href="">GDPR text in your language</a> to better understand your rights.</p>
+      <p className="description text-light">We ask that you direct all questions and requests related to your personal data to <a href="">privacy@mirumee.com</a> or that you send them to the postal address of the company listed above </p>
 		</section>
 		<GitHubBanner />
 	</div>
