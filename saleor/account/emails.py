80dd2b8a845c0c5f3ed2a2894248bcd3ed21a72a
TOMEK <tomekrajzer@gmail.com>
2018-08-30 21:51:56 +0200

    Generate whole url on the backend

--- saleor/account/emails.py
+++ saleor/account/emails.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.templatetags.static import static
 from django.urls import reverse
 from templated_email import send_templated_mail
 
@@ -12,8 +13,13 @@ def send_password_reset_email(context, recipient):
     reset_url = build_absolute_uri(
         reverse(
             'account:reset-password-confirm',
-            kwargs={'uidb64': context['uid'], 'token': context['token']}))
+            kwargs={
+                'uidb64': context['uid'],
+                'token': context['token']}))
+    logo_url = build_absolute_uri(
+        location=None) + static('images/logo-document.svg')
     context['reset_url'] = reset_url
+    context['logo_url'] = logo_url
     send_templated_mail(
         template_name='account/password_reset',
         from_email=settings.DEFAULT_FROM_EMAIL,
@@ -24,12 +30,14 @@ def send_password_reset_email(context, recipient):
 @shared_task
 def send_account_delete_confirmation_email(token, recipient_email):
     site = Site.objects.get_current()
-    absolute_url = build_absolute_uri(location=None)
+    logo_url = build_absolute_uri(
+        location=None) + static('images/logo-document.svg')
     delete_url = build_absolute_uri(
         reverse('account:delete-confirm', kwargs={'token': token}))
     ctx = {
         'site_name': site.name,
-        'absolute_url': absolute_url,
+        'domain': site.domain,
+        'logo_url': logo_url,
         'url': delete_url}
     send_templated_mail(
         template_name='account/account_delete',
