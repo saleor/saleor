7b83ef77388b16a53eaf9ccd3c89333a9ff74c00
Artur Smęt <artur@mirumee.com>
2018-08-31 16:56:06 +0200

    Use choices for tax rate type

--- saleor/dashboard/taxes/views.py
+++ saleor/dashboard/taxes/views.py
@@ -9,7 +9,7 @@ from django.utils.translation import pgettext_lazy
 from django_countries.fields import Country
 from django_prices_vatlayer.models import VAT
 
-from ...core.i18n import VAT_RATE_TYPE_TRANSLATIONS
+from ...core import TaxRateType
 from ...core.utils import get_paginator_items
 from ...core.utils.taxes import get_taxes_for_country
 from ...dashboard.taxes.filters import TaxFilter
@@ -33,8 +33,9 @@ def tax_list(request):
 def tax_details(request, country_code):
     tax = get_object_or_404(VAT, country_code=country_code)
     tax_rates = get_taxes_for_country(Country(country_code))
+    translations = dict(TaxRateType.CHOICES)
     tax_rates = [
-        (VAT_RATE_TYPE_TRANSLATIONS.get(rate_name, rate_name), tax['value'])
+        (translations.get(rate_name, rate_name), tax['value'])
         for rate_name, tax in tax_rates.items()]
     ctx = {'tax': tax, 'tax_rates': sorted(tax_rates)}
     return TemplateResponse(request, 'dashboard/taxes/details.html', ctx)
