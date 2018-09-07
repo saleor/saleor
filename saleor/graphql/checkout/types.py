ecb73fb490a7ecafed50636f8d183224b049d6db
Artur Smęt <artur@mirumee.com>
2018-09-07 11:09:31 +0200

    Expose totals in chekcout API

--- saleor/graphql/checkout/types.py
+++ saleor/graphql/checkout/types.py
@@ -1,18 +1,48 @@
-from ..core.types.common import CountableDjangoObjectType
+import graphene
+
 from ...checkout import models
-from ...product.models import Product
-from graphene import relay
+from ..core.types.common import CountableDjangoObjectType
+from ..core.types.money import TaxedMoney
+from ...core.utils.taxes import get_taxes_for_address
+
 
 class CheckoutLine(CountableDjangoObjectType):
+    total_price = graphene.Field(TaxedMoney, description='Total price')
+    requires_shipping = graphene.Boolean(description='Line requires shipping')
+
     class Meta:
         exclude_fields = ['cart', 'data']
         description = 'Represents user address data.'
-        interfaces = [relay.Node]
+        interfaces = [graphene.relay.Node]
         model = models.CartLine
+
+    def resolve_total_price(self, info):
+        taxes = get_taxes_for_address(self.cart.shipping_address)
+        return self.get_total(taxes=taxes)
+
+    def resolve_requires_shipping(self, info):
+        return self.is_shipping_required()
 
 
 class Checkout(CountableDjangoObjectType):
+    total_price = graphene.Field(TaxedMoney, description='Total price')
+    subtotal_price = graphene.Field(
+        TaxedMoney, description='Total without shipping')
+    shipping_price = graphene.Field(TaxedMoney, description='Shipping price')
+
     class Meta:
         description = 'Checkout object'
         model = models.Cart
-        interfaces = [relay.Node]
+        interfaces = [graphene.relay.Node]
+
+    def resolve_total_price(self, info):
+        taxes = get_taxes_for_address(self.shipping_address)
+        return self.get_total(taxes=taxes)
+
+    def resolve_subtotal_price(self, info):
+        taxes = get_taxes_for_address(self.shipping_address)
+        return self.get_subtotal(taxes=taxes)
+
+    def resolve_shipping_price(self, info):
+        taxes = get_taxes_for_address(self.shipping_address)
+        return self.get_shipping_price(taxes=taxes)
