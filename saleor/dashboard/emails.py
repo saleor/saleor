01675198424f7203e7d5b4b37eccafeb9e629a41
TOMEK <tomekrajzer@gmail.com>
2018-09-17 23:14:19 +0200

    Sort imports

--- saleor/dashboard/emails.py
+++ saleor/dashboard/emails.py
@@ -6,8 +6,8 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from templated_email import send_templated_mail
 
-from ..core.utils import build_absolute_uri
 from ..core.emails import get_email_base_context
+from ..core.utils import build_absolute_uri
 
 
 @shared_task
