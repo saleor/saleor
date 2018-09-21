cdb726d30ef67d41dbe293127c3a9a0a386b4143
Marcin Gębala <maarcin.gebala@gmail.com>
2018-09-21 13:27:26 +0200

    WIP Optimize products connection

--- saleor/graphql/core/fields.py
+++ saleor/graphql/core/fields.py
@@ -4,6 +4,9 @@ from django_prices.models import MoneyField, TaxedMoneyField
 from graphene_django.converter import convert_django_field
 from graphene_django.fields import DjangoConnectionField
 
+from graphene.relay import PageInfo
+from graphql_relay.connection.arrayconnection import connection_from_list_slice
+
 from .types.common import Weight
 from .types.money import Money, TaxedMoney
 
@@ -26,5 +29,20 @@ def convert_field_measurements(field, registry=None):
 class PrefetchingConnectionField(DjangoConnectionField):
 
     @classmethod
-    def merge_querysets(cls, default_queryset, queryset):
-        return queryset
+    def resolve_connection(cls, connection, default_manager, args, iterable):
+        if iterable is None:
+            iterable = default_manager
+        _len = iterable.count()
+        connection = connection_from_list_slice(
+            iterable,
+            args,
+            slice_start=0,
+            list_length=_len,
+            list_slice_length=_len,
+            connection_type=connection,
+            edge_type=connection.Edge,
+            pageinfo_type=PageInfo,
+        )
+        connection.iterable = iterable
+        connection.length = _len
+        return connection
