b2c950c97412a6261b6c48a4d28413e2eaf3afd7
dominik-zeglen <flesz3@o2.pl>
2018-09-21 18:41:38 +0200

    Enable user permission management

--- saleor/static/dashboard-next/staff/views/StaffDetails.tsx
+++ saleor/static/dashboard-next/staff/views/StaffDetails.tsx
@@ -3,9 +3,12 @@ import * as React from "react";
 import { staffListUrl } from "..";
 import Messages from "../../components/messages";
 import Navigator from "../../components/Navigator";
+import i18n from "../../i18n";
 import { maybe } from "../../misc";
 import StaffDetailsPage from "../components/StaffDetailsPage/StaffDetailsPage";
+import { TypedStaffMemberUpdateMutation } from "../mutations";
 import { TypedStaffMemberDetailsQuery } from "../queries";
+import { StaffMemberUpdate } from "../types/StaffMemberUpdate";
 
 interface OrderListProps {
   id: string;
@@ -17,18 +20,40 @@ export const StaffDetails: React.StatelessComponent<OrderListProps> = ({
   <Navigator>
     {navigate => (
       <Messages>
-        {_ => (
+        {pushMessage => (
           <TypedStaffMemberDetailsQuery variables={{ id }}>
             {({ data, loading }) => {
+              const handleStaffMemberUpdate = (data: StaffMemberUpdate) => {
+                if (!maybe(() => data.staffUpdate.errors.length !== 0)) {
+                  pushMessage({
+                    text: i18n.t("Succesfully updated staff member account")
+                  });
+                }
+              };
               return (
-                <StaffDetailsPage
-                  disabled={loading}
-                  onBack={() => navigate(staffListUrl)}
-                  onDelete={() => undefined}
-                  onSubmit={() => undefined}
-                  permissions={maybe(() => data.shop.permissions)}
-                  staffMember={maybe(() => data.user)}
-                />
+                <TypedStaffMemberUpdateMutation
+                  onCompleted={handleStaffMemberUpdate}
+                >
+                  {updateStaffMember => (
+                    <StaffDetailsPage
+                      disabled={loading}
+                      onBack={() => navigate(staffListUrl)}
+                      onDelete={() => undefined}
+                      onSubmit={variables =>
+                        updateStaffMember({
+                          variables: {
+                            id,
+                            input: {
+                              permissions: variables.permissions
+                            }
+                          }
+                        })
+                      }
+                      permissions={maybe(() => data.shop.permissions)}
+                      staffMember={maybe(() => data.user)}
+                    />
+                  )}
+                </TypedStaffMemberUpdateMutation>
               );
             }}
           </TypedStaffMemberDetailsQuery>
