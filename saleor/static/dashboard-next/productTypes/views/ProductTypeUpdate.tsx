79c61195940da68230b097188d9acf8ce4f2c494
dominik-zeglen <flesz3@o2.pl>
2018-09-27 16:55:45 +0200

    Remove charge_taxes flag

--- saleor/static/dashboard-next/productTypes/views/ProductTypeUpdate.tsx
+++ saleor/static/dashboard-next/productTypes/views/ProductTypeUpdate.tsx
@@ -9,7 +9,6 @@ import { maybe } from "../../misc";
 import ProductTypeDetailsPage, {
   ProductTypeForm
 } from "../components/ProductTypeDetailsPage";
-import { AttributeSearchProvider } from "../containers/AttributeSearch";
 import ProductTypeOperations from "../containers/ProductTypeOperations";
 import { TypedProductTypeDetailsQuery } from "../queries";
 import { ProductTypeDelete } from "../types/ProductTypeDelete";
@@ -35,97 +34,79 @@ export const ProductTypeUpdate: React.StatelessComponent<
                   />
                 );
               }
+              const handleDeleteSuccess = (deleteData: ProductTypeDelete) => {
+                if (deleteData.productTypeDelete.errors.length === 0) {
+                  pushMessage({
+                    text: i18n.t("Successfully deleted product type")
+                  });
+                  navigate(productTypeListUrl);
+                }
+              };
+              const handleUpdateSuccess = (
+                updateData: ProductTypeUpdateMutation
+              ) => {
+                if (updateData.productTypeUpdate.errors.length === 0) {
+                  pushMessage({
+                    text: i18n.t("Successfully updated product type")
+                  });
+                }
+              };
+
               return (
-                <AttributeSearchProvider>
-                  {(searchAttribute, searchState) => {
-                    const handleDeleteSuccess = (
-                      deleteData: ProductTypeDelete
-                    ) => {
-                      if (deleteData.productTypeDelete.errors.length === 0) {
-                        pushMessage({
-                          text: i18n.t("Successfully deleted product type")
-                        });
-                        navigate(productTypeListUrl);
-                      }
+                <ProductTypeOperations
+                  id={id}
+                  onDelete={handleDeleteSuccess}
+                  onUpdate={handleUpdateSuccess}
+                >
+                  {({
+                    deleteProductType,
+                    errors,
+                    loading: mutationLoading,
+                    updateProductType
+                  }) => {
+                    const handleDelete = () => deleteProductType.mutate({ id });
+                    const handleUpdate = (formData: ProductTypeForm) => {
+                      updateProductType.mutate({
+                        id,
+                        input: {
+                          hasVariants: formData.hasVariants,
+                          isShippingRequired: formData.isShippingRequired,
+                          name: formData.name,
+                          productAttributes: formData.productAttributes.map(
+                            choice => choice.value
+                          ),
+                          taxRate: formData.taxRate,
+                          variantAttributes: formData.variantAttributes.map(
+                            choice => choice.value
+                          ),
+                          weight: formData.weight
+                        }
+                      });
                     };
-                    const handleUpdateSuccess = (
-                      updateData: ProductTypeUpdateMutation
-                    ) => {
-                      if (updateData.productTypeUpdate.errors.length === 0) {
-                        pushMessage({
-                          text: i18n.t("Successfully updated product type")
-                        });
-                      }
-                    };
+                    const loading = mutationLoading || dataLoading;
                     return (
-                      <ProductTypeOperations
-                        id={id}
-                        onDelete={handleDeleteSuccess}
-                        onUpdate={handleUpdateSuccess}
-                      >
-                        {({
-                          deleteProductType,
-                          errors,
-                          loading: mutationLoading,
-                          updateProductType
-                        }) => {
-                          const handleDelete = () =>
-                            deleteProductType.mutate({ id });
-                          const handleUpdate = (formData: ProductTypeForm) => {
-                            updateProductType.mutate({
-                              id,
-                              input: {
-                                hasVariants: formData.hasVariants,
-                                isShippingRequired: formData.isShippingRequired,
-                                name: formData.name,
-                                productAttributes: formData.productAttributes.map(
-                                  choice => choice.value
-                                ),
-                                taxRate: formData.taxRate,
-                                variantAttributes: formData.variantAttributes.map(
-                                  choice => choice.value
-                                ),
-                                weight: formData.weight
-                              }
-                            });
-                          };
-                          const loading = mutationLoading || dataLoading;
-                          return (
-                            <ProductTypeDetailsPage
-                              defaultWeightUnit={maybe(
-                                () => data.shop.defaultWeightUnit
-                              )}
-                              disabled={loading}
-                              errors={errors}
-                              pageTitle={
-                                data && data.productType
-                                  ? data.productType.name
-                                  : undefined
-                              }
-                              productType={maybe(() => data.productType)}
-                              saveButtonBarState={
-                                loading ? "loading" : "default"
-                              }
-                              searchLoading={maybe(() => searchState.loading)}
-                              searchResults={maybe(
-                                () =>
-                                  searchState.data.attributes.edges.map(
-                                    edge => edge.node
-                                  ),
-                                []
-                              )}
-                              onAttributeSearch={searchAttribute}
-                              onBack={() => navigate(productTypeListUrl)}
-                              onDelete={handleDelete}
-                              onSubmit={handleUpdate}
-                              key={searchState ? undefined : "search"}
-                            />
-                          );
-                        }}
-                      </ProductTypeOperations>
+                      <ProductTypeDetailsPage
+                        defaultWeightUnit={maybe(
+                          () => data.shop.defaultWeightUnit
+                        )}
+                        disabled={loading}
+                        errors={errors}
+                        pageTitle={
+                          data && data.productType
+                            ? data.productType.name
+                            : undefined
+                        }
+                        productType={maybe(() => data.productType)}
+                        saveButtonBarState={loading ? "loading" : "default"}
+                        onAttributeAdd={() => undefined}
+                        onAttributeUpdate={() => undefined}
+                        onBack={() => navigate(productTypeListUrl)}
+                        onDelete={handleDelete}
+                        onSubmit={handleUpdate}
+                      />
                     );
                   }}
-                </AttributeSearchProvider>
+                </ProductTypeOperations>
               );
             }}
           </TypedProductTypeDetailsQuery>
