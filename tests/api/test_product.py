acd5737b9d5e4793bdee761188006f56b22dfe99
axcel <axcel.github@gmail.com>
2018-09-29 17:16:03 +0500

    tests and migrations

--- tests/api/test_product.py
+++ tests/api/test_product.py
@@ -395,7 +395,7 @@ def test_create_product(
     assert data['product']['description'] == product_description
     assert data['product']['isPublished'] == product_isPublished
     assert data['product']['chargeTaxes'] == product_chargeTaxes
-    assert data['product']['taxRate'] == product_taxRate.lower()
+    assert data['product']['taxRate'] == product_taxRate
     assert data['product']['productType']['name'] == product_type.name
     assert data['product']['category']['name'] == category.name
     values = (
@@ -491,7 +491,7 @@ def test_update_product(
     assert data['product']['description'] == product_description
     assert data['product']['isPublished'] == product_isPublished
     assert data['product']['chargeTaxes'] == product_chargeTaxes
-    assert data['product']['taxRate'] == product_taxRate.lower()
+    assert data['product']['taxRate'] == product_taxRate
     assert not data['product']['category']['name'] == category.name
 
 
