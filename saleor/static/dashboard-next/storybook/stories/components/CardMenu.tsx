1e1fd94fd909373c2c14b3efc266f2e88d78690a
dominik-zeglen <flesz3@o2.pl>
2018-10-03 19:06:02 +0200

    Add card menu component

---
+++ saleor/static/dashboard-next/storybook/stories/components/CardMenu.tsx
@@ -0,0 +1,15 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import CardMenu, { CardMenuItem } from "../../../components/CardMenu";
+import Decorator from "../../Decorator";
+
+const menuItems: CardMenuItem[] = [
+  { label: "Do this", onSelect: () => undefined },
+  { label: "Or do this", onSelect: () => undefined },
+  { label: "Or maybe this?", onSelect: () => undefined }
+];
+
+storiesOf("Generics / Card menu", module)
+  .addDecorator(Decorator)
+  .add("default", () => <CardMenu menuItems={menuItems} />);
