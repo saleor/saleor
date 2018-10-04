889a1c4d898ac89fcc1546b36883c8d60a964656
oldPadavan <evggoncharov@gmail.com>
2018-10-04 16:43:32 +0300

    Turn Menu.items into list

--- saleor/graphql/menu/types.py
+++ saleor/graphql/menu/types.py
@@ -5,20 +5,6 @@ from ...menu import models
 from ..core.types.common import CountableDjangoObjectType
 
 
-class Menu(CountableDjangoObjectType):
-    class Meta:
-        description = """Represents a single menu - an object that is used
-        to help navigate through the store."""
-        interfaces = [relay.Node]
-        exclude_fields = ['json_content']
-        filter_fields = {}
-        model = models.Menu
-
-    def resolve_items(self, info, **kwargs):
-        return self.items.filter(level=0).select_related(
-            'category', 'collection', 'page')
-
-
 class MenuItem(CountableDjangoObjectType):
     url = graphene.String(description='URL to the menu item.')
 
@@ -29,3 +15,21 @@ class MenuItem(CountableDjangoObjectType):
         exclude_fields = ['sort_order', 'lft', 'rght', 'tree_id']
         filter_fields = {}
         model = models.MenuItem
+
+
+class Menu(CountableDjangoObjectType):
+    items = graphene.List(
+        MenuItem, required=True,
+        description='List of menu items of the menu')
+
+    class Meta:
+        description = """Represents a single menu - an object that is used
+        to help navigate through the store."""
+        interfaces = [relay.Node]
+        exclude_fields = ['json_content']
+        filter_fields = {}
+        model = models.Menu
+
+    def resolve_items(self, info, **kwargs):
+        return self.items.filter(level=0).select_related(
+            'category', 'collection', 'page').all()
