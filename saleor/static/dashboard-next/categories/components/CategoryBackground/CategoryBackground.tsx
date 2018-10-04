7aafa42085886b6dd3da50ffdf1493620d27e76b
Łukasz Szewczyk <lukaszszewczyk@ukaszs-mac-mini.lan>
2018-10-04 16:43:21 +0200

    add basic designe for background and subcategories

---
+++ saleor/static/dashboard-next/categories/components/CategoryBackground/CategoryBackground.tsx
@@ -0,0 +1,77 @@
+import { withStyles } from "@material-ui/core/styles";
+import * as React from "react";
+
+import Card from "@material-ui/core/Card";
+import Button from "@material-ui/core/Button";
+import CardContent from "@material-ui/core/CardContent";
+import AddPhotoIcon from "@material-ui/icons/AddAPhoto";
+import Typography from "@material-ui/core/Typography";
+
+import CardTitle from "../../../components/CardTitle";
+
+import i18n from "../../../i18n";
+
+interface CategoryBackgroundProps {
+  onImageUpload?(event: React.ChangeEvent<any>);
+  disabled: boolean;
+}
+
+const decorate = withStyles(theme => ({
+  noPhotosIcon: {
+    height: theme.spacing.unit * 8,
+    margin: "0 auto",
+    width: theme.spacing.unit * 8
+  },
+  noPhotosIconContainer: {
+    margin: `${theme.spacing.unit * 5}px 0`,
+    textAlign: "center" as "center"
+  },
+  noPhotosIconText: {
+    fontSize: "1rem",
+    fontWeight: 600 as 600,
+    marginTop: theme.spacing.unit
+  },
+  fileField: {
+    display: "none"
+  }
+}));
+
+export const CategoryBackground = decorate<CategoryBackgroundProps>(
+  ({ classes, onImageUpload, disabled }) => {
+    return (
+      <Card>
+        <CardTitle
+          title={i18n.t("Background image (optional)")}
+          toolbar={
+            <>
+              <Button
+                variant="flat"
+                color="secondary"
+                disabled={disabled}
+                onClick={() => this.upload.click()}
+              >
+                {i18n.t("Upload image")}
+              </Button>
+              <input
+                className={classes.fileField}
+                id="fileUpload"
+                onChange={onImageUpload}
+                type="file"
+                ref={ref => (this.upload = ref)}
+              />
+            </>
+          }
+        />
+        <CardContent>
+          <div className={classes.noPhotosIconContainer}>
+            <AddPhotoIcon className={classes.noPhotosIcon} />
+            <Typography className={classes.noPhotosIconText}>
+              {i18n.t("Drop images to upload")}
+            </Typography>
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
+);
+export default CategoryBackground;
