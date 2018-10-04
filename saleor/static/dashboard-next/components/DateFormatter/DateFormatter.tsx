b6898d29c26ba7565ca7af5c652db3661ce61343
Patryk Zawadzki <patrys@room-303.com>
2018-10-04 13:17:29 +0200

    Reintroduce moment as the date formatter

--- saleor/static/dashboard-next/components/DateFormatter/DateFormatter.tsx
+++ saleor/static/dashboard-next/components/DateFormatter/DateFormatter.tsx
@@ -1,16 +1,37 @@
 import Tooltip from "@material-ui/core/Tooltip";
+import * as moment from "moment";
 import * as React from "react";
+import ReactMoment from "react-moment";
+
+import { LocaleConsumer } from "../Locale";
+import { Consumer } from "./DateContext";
 
 interface DateFormatterProps {
   date: string;
-  locale?: string;
 }
 
 const DateFormatter: React.StatelessComponent<DateFormatterProps> = ({
   date
-}) => (
-  <Tooltip title={date}>
-    <span>{date}</span>
-  </Tooltip>
-);
+}) => {
+  return (
+    <LocaleConsumer>
+      {locale => (
+        <Consumer>
+          {currentDate => (
+            <Tooltip
+              title={moment(date)
+                .locale(locale)
+                .toLocaleString()}
+            >
+              <ReactMoment from={currentDate} locale={locale}>
+                {date}
+              </ReactMoment>
+            </Tooltip>
+          )}
+        </Consumer>
+      )}
+    </LocaleConsumer>
+  );
+};
+
 export default DateFormatter;
