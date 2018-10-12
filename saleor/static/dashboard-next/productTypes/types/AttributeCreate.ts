f771503b87c1c3497d7abbf3b1e0ed0fc9e02943
dominik-zeglen <flesz3@o2.pl>
2018-10-12 11:35:27 +0200

    Refresh cache after mutations

--- saleor/static/dashboard-next/productTypes/types/AttributeCreate.ts
+++ saleor/static/dashboard-next/productTypes/types/AttributeCreate.ts
@@ -1,7 +1,7 @@
 /* tslint:disable */
 // This file was automatically generated and should not be edited.
 
-import { AttributeCreateInput, AttributeTypeEnum } from "./../../types/globalTypes";
+import { AttributeCreateInput, AttributeTypeEnum, TaxRateType } from "./../../types/globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: AttributeCreate
@@ -13,9 +13,58 @@ export interface AttributeCreate_attributeCreate_errors {
   message: string | null;
 }
 
+export interface AttributeCreate_attributeCreate_productType_productAttributes_values {
+  __typename: "AttributeValue";
+  id: string;
+  name: string | null;
+  slug: string | null;
+}
+
+export interface AttributeCreate_attributeCreate_productType_productAttributes {
+  __typename: "Attribute";
+  id: string;
+  name: string | null;
+  slug: string | null;
+  values: (AttributeCreate_attributeCreate_productType_productAttributes_values | null)[] | null;
+}
+
+export interface AttributeCreate_attributeCreate_productType_variantAttributes_values {
+  __typename: "AttributeValue";
+  id: string;
+  name: string | null;
+  slug: string | null;
+}
+
+export interface AttributeCreate_attributeCreate_productType_variantAttributes {
+  __typename: "Attribute";
+  id: string;
+  name: string | null;
+  slug: string | null;
+  values: (AttributeCreate_attributeCreate_productType_variantAttributes_values | null)[] | null;
+}
+
+export interface AttributeCreate_attributeCreate_productType_weight {
+  __typename: "Weight";
+  unit: string;
+  value: number;
+}
+
+export interface AttributeCreate_attributeCreate_productType {
+  __typename: "ProductType";
+  id: string;
+  name: string;
+  hasVariants: boolean;
+  isShippingRequired: boolean;
+  taxRate: TaxRateType | null;
+  productAttributes: (AttributeCreate_attributeCreate_productType_productAttributes | null)[] | null;
+  variantAttributes: (AttributeCreate_attributeCreate_productType_variantAttributes | null)[] | null;
+  weight: AttributeCreate_attributeCreate_productType_weight | null;
+}
+
 export interface AttributeCreate_attributeCreate {
   __typename: "AttributeCreate";
   errors: (AttributeCreate_attributeCreate_errors | null)[] | null;
+  productType: AttributeCreate_attributeCreate_productType | null;
 }
 
 export interface AttributeCreate {
