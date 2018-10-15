4f35cc5288e0d2df8fce564708cb98a6a53ca056
dominik-zeglen <flesz3@o2.pl>
2018-10-15 16:30:21 +0200

    Update stories

--- saleor/static/dashboard-next/storybook/stories/categories/CategoryDeleteDialog.tsx
+++ saleor/static/dashboard-next/storybook/stories/categories/CategoryDeleteDialog.tsx
@@ -2,10 +2,8 @@ import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
 import CategoryDeleteDialog from "../../../categories/components/CategoryDeleteDialog";
-import { category as categoryFixture } from "../../../categories/fixtures";
+import { category } from "../../../categories/fixtures";
 import Decorator from "../../Decorator";
-
-const category = categoryFixture("");
 
 storiesOf("Views / Categories / Category Delete Dialog", module)
   .addDecorator(Decorator)
