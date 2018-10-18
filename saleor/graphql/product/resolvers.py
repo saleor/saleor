32ac8d5c286c002121f63f9734cbc54bc4e5ca9a
Marcin Gębala <maarcin.gebala@gmail.com>
2018-10-18 17:47:38 +0200

    Add query to get report of product sales

--- saleor/graphql/product/resolvers.py
+++ saleor/graphql/product/resolvers.py
@@ -1,10 +1,13 @@
+import datetime
 import graphene
 from django.db.models import Q
+from django.db.models.aggregates import Sum
 
+from ...order import OrderStatus
 from ...product import models
 from ...product.utils import products_with_details
 from ..utils import filter_by_query_param, get_database_id
-from .types import Category, ProductVariant
+from .types import ReportingPeriod, Category, ProductVariant
 
 PRODUCT_SEARCH_FIELDS = ('name', 'description', 'category__name')
 CATEGORY_SEARCH_FIELDS = ('name', 'slug', 'description', 'parent__name')
@@ -75,3 +78,29 @@ def resolve_product_variants(info, ids=None):
             for node_id in ids]
         queryset = queryset.filter(pk__in=db_ids)
     return queryset
+
+
+def resolve_report_product_sales(info, period):
+    qs = models.ProductVariant.objects.prefetch_related(
+        'product', 'product__images', 'order_lines__order').all()
+
+    # exclude draft and canceled orders
+    exclude_status = [OrderStatus.DRAFT, OrderStatus.CANCELED]
+    qs = qs.exclude(order_lines__order__status__in=exclude_status)
+
+    # filter by period
+    if (period == ReportingPeriod.DAY):
+        start_date = datetime.datetime.now().replace(
+            hour=0, minute=0, second=0, microsecond=0)
+    elif (period == ReportingPeriod.MONTH):
+        start_date = datetime.datetime.now().replace(
+            day=1, hour=0, minute=0, second=0, microsecond=0)
+    else:
+        start_date = None
+
+    if start_date:
+        qs = qs.filter(order_lines__order__created__gte=start_date)
+
+    qs = qs.annotate(quantity_ordered=Sum('order_lines__quantity'))
+    qs = qs.filter(quantity_ordered__isnull=False)
+    return qs.order_by('-quantity_ordered')
