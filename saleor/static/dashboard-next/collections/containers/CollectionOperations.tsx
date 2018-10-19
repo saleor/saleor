0a940c9c92aad6c6e25b18d732c5833476e951de
dominik-zeglen <flesz3@o2.pl>
2018-10-19 18:32:39 +0200

    Add collection removing

--- saleor/static/dashboard-next/collections/containers/CollectionOperations.tsx
+++ saleor/static/dashboard-next/collections/containers/CollectionOperations.tsx
@@ -17,7 +17,12 @@ import {
   CollectionUpdate,
   CollectionUpdateVariables
 } from "../types/CollectionUpdate";
+import {
+  RemoveCollection,
+  RemoveCollectionVariables
+} from "../types/RemoveCollection";
 import CollectionAssignProductProvider from "./CollectionAssignProductProvider";
+import CollectionRemoveProvider from "./CollectionRemove";
 import CollectionUpdateProvider from "./CollectionUpdateProvider";
 import HomepageCollectionAssignProvider from "./HomepageCollectionAssignProvider";
 
@@ -35,45 +40,67 @@ interface CollectionUpdateOperationsProps extends MutationProviderProps {
       CollectionUpdate,
       CollectionUpdateVariables
     >;
+    removeCollection: PartialMutationProviderOutput<
+      RemoveCollection,
+      RemoveCollectionVariables
+    >;
   }>;
   onHomepageCollectionAssign: (data: AssignHomepageCollection) => void;
   onUpdate: (data: CollectionUpdate) => void;
   onProductAssign: (data: CollectionAssignProduct) => void;
+  onRemove: (data: RemoveCollection) => void;
 }
 
 const CollectionOperations: React.StatelessComponent<
   CollectionUpdateOperationsProps
-> = ({ children, onHomepageCollectionAssign, onUpdate, onProductAssign }) => (
+> = ({
+  children,
+  onHomepageCollectionAssign,
+  onUpdate,
+  onProductAssign,
+  onRemove
+}) => (
   <CollectionUpdateProvider onSuccess={onUpdate}>
     {updateCollection => (
-      <CollectionAssignProductProvider onSuccess={onProductAssign}>
-        {assignProduct => (
-          <HomepageCollectionAssignProvider
-            onSuccess={onHomepageCollectionAssign}
-          >
-            {assignHomepageCollection =>
-              children({
-                assignHomepageCollection: {
-                  data: assignHomepageCollection.data,
-                  loading: assignHomepageCollection.loading,
-                  mutate: variables =>
-                    assignHomepageCollection.mutate({ variables })
-                },
-                assignProduct: {
-                  data: assignProduct.data,
-                  loading: assignProduct.loading,
-                  mutate: variables => assignProduct.mutate({ variables })
-                },
-                updateCollection: {
-                  data: updateCollection.data,
-                  loading: updateCollection.loading,
-                  mutate: variables => updateCollection.mutate({ variables })
+      <CollectionRemoveProvider onSuccess={onRemove}>
+        {removeCollection => (
+          <CollectionAssignProductProvider onSuccess={onProductAssign}>
+            {assignProduct => (
+              <HomepageCollectionAssignProvider
+                onSuccess={onHomepageCollectionAssign}
+              >
+                {assignHomepageCollection =>
+                  children({
+                    assignHomepageCollection: {
+                      data: assignHomepageCollection.data,
+                      loading: assignHomepageCollection.loading,
+                      mutate: variables =>
+                        assignHomepageCollection.mutate({ variables })
+                    },
+                    assignProduct: {
+                      data: assignProduct.data,
+                      loading: assignProduct.loading,
+                      mutate: variables => assignProduct.mutate({ variables })
+                    },
+                    removeCollection: {
+                      data: removeCollection.data,
+                      loading: removeCollection.loading,
+                      mutate: variables =>
+                        removeCollection.mutate({ variables })
+                    },
+                    updateCollection: {
+                      data: updateCollection.data,
+                      loading: updateCollection.loading,
+                      mutate: variables =>
+                        updateCollection.mutate({ variables })
+                    }
+                  })
                 }
-              })
-            }
-          </HomepageCollectionAssignProvider>
+              </HomepageCollectionAssignProvider>
+            )}
+          </CollectionAssignProductProvider>
         )}
-      </CollectionAssignProductProvider>
+      </CollectionRemoveProvider>
     )}
   </CollectionUpdateProvider>
 );
