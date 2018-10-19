8be5771bf3cdc8cb4153912f86ed256e1f3b849d
dominik-zeglen <flesz3@o2.pl>
2018-10-19 13:17:44 +0200

    Fix product assign url

--- saleor/static/dashboard-next/collections/urls.ts
+++ saleor/static/dashboard-next/collections/urls.ts
@@ -5,3 +5,5 @@ export const collectionRemoveUrl = (id: string) =>
   collectionUrl(id) + "remove/";
 export const collectionImageRemoveUrl = (id: string) =>
   collectionUrl(id) + "removeImage/";
+export const collectionAddProductUrl = (id: string) =>
+  collectionUrl(id) + "add/";
