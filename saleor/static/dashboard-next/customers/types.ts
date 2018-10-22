5f7f37c9d1f26416e275e8ec3d5f99f9d9d07520
dominik-zeglen <flesz3@o2.pl>
2018-10-22 18:07:05 +0200

    Move types

---
+++ saleor/static/dashboard-next/customers/types.ts
@@ -0,0 +1,30 @@
+export interface AddressTypeInput {
+  city: string;
+  cityArea?: string;
+  companyName?: string;
+  country: string;
+  countryArea?: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  postalCode: string;
+  streetAddress1: string;
+  streetAddress2?: string;
+}
+export interface AddressType {
+  id: string;
+  city: string;
+  cityArea?: string;
+  companyName?: string;
+  country: {
+    code: string;
+    country: string;
+  };
+  countryArea?: string;
+  firstName: string;
+  lastName: string;
+  phone: string;
+  postalCode: string;
+  streetAddress1: string;
+  streetAddress2?: string;
+}
