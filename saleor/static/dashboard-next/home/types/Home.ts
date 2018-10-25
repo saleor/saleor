879c52d90cc4f13b172a22d6c389ff6d851d4228
dominik-zeglen <flesz3@o2.pl>
2018-10-25 19:42:49 +0200

    Connect homepage to API

---
+++ saleor/static/dashboard-next/home/types/Home.ts
@@ -0,0 +1,169 @@
+/* tslint:disable */
+// This file was automatically generated and should not be edited.
+
+import { OrderEventsEmails, OrderEvents } from "./../../types/globalTypes";
+
+// ====================================================
+// GraphQL query operation: Home
+// ====================================================
+
+export interface Home_salesToday_gross {
+  __typename: "Money";
+  amount: number;
+  currency: string;
+}
+
+export interface Home_salesToday {
+  __typename: "TaxedMoney";
+  gross: Home_salesToday_gross;
+}
+
+export interface Home_ordersToday {
+  __typename: "OrderCountableConnection";
+  totalCount: number | null;
+}
+
+export interface Home_ordersToFulfill_edges_node {
+  __typename: "Order";
+  id: string;
+  number: string | null;
+}
+
+export interface Home_ordersToFulfill_edges {
+  __typename: "OrderCountableEdge";
+  node: Home_ordersToFulfill_edges_node;
+}
+
+export interface Home_ordersToFulfill {
+  __typename: "OrderCountableConnection";
+  edges: Home_ordersToFulfill_edges[];
+}
+
+export interface Home_ordersToCapture_edges_node {
+  __typename: "Order";
+  id: string;
+  number: string | null;
+}
+
+export interface Home_ordersToCapture_edges {
+  __typename: "OrderCountableEdge";
+  node: Home_ordersToCapture_edges_node;
+}
+
+export interface Home_ordersToCapture {
+  __typename: "OrderCountableConnection";
+  edges: Home_ordersToCapture_edges[];
+}
+
+export interface Home_productsOutOfStock_edges_node {
+  __typename: "Product";
+  id: string;
+  name: string;
+}
+
+export interface Home_productsOutOfStock_edges {
+  __typename: "ProductCountableEdge";
+  node: Home_productsOutOfStock_edges_node;
+}
+
+export interface Home_productsOutOfStock {
+  __typename: "ProductCountableConnection";
+  edges: Home_productsOutOfStock_edges[];
+}
+
+export interface Home_productTopToday_edges_node_revenue_gross {
+  __typename: "Money";
+  amount: number;
+  currency: string;
+}
+
+export interface Home_productTopToday_edges_node_revenue {
+  __typename: "TaxedMoney";
+  gross: Home_productTopToday_edges_node_revenue_gross;
+}
+
+export interface Home_productTopToday_edges_node_attributes_value {
+  __typename: "AttributeValue";
+  id: string;
+  name: string | null;
+  sortOrder: number;
+}
+
+export interface Home_productTopToday_edges_node_attributes {
+  __typename: "SelectedAttribute";
+  value: Home_productTopToday_edges_node_attributes_value | null;
+}
+
+export interface Home_productTopToday_edges_node_product_price {
+  __typename: "Money";
+  amount: number;
+  currency: string;
+}
+
+export interface Home_productTopToday_edges_node_product {
+  __typename: "Product";
+  id: string;
+  name: string;
+  thumbnailUrl: string | null;
+  price: Home_productTopToday_edges_node_product_price | null;
+}
+
+export interface Home_productTopToday_edges_node {
+  __typename: "ProductVariant";
+  id: string;
+  revenue: Home_productTopToday_edges_node_revenue | null;
+  attributes: (Home_productTopToday_edges_node_attributes | null)[] | null;
+  product: Home_productTopToday_edges_node_product;
+  quantityOrdered: number | null;
+}
+
+export interface Home_productTopToday_edges {
+  __typename: "ProductVariantCountableEdge";
+  node: Home_productTopToday_edges_node;
+}
+
+export interface Home_productTopToday {
+  __typename: "ProductVariantCountableConnection";
+  edges: Home_productTopToday_edges[];
+}
+
+export interface Home_activities_edges_node_user {
+  __typename: "User";
+  id: string;
+  email: string;
+}
+
+export interface Home_activities_edges_node {
+  __typename: "OrderEvent";
+  amount: number | null;
+  composedId: string | null;
+  date: any | null;
+  email: string | null;
+  emailType: OrderEventsEmails | null;
+  id: string;
+  message: string | null;
+  oversoldItems: (string | null)[] | null;
+  quantity: number | null;
+  type: OrderEvents | null;
+  user: Home_activities_edges_node_user | null;
+}
+
+export interface Home_activities_edges {
+  __typename: "OrderEventCountableEdge";
+  node: Home_activities_edges_node;
+}
+
+export interface Home_activities {
+  __typename: "OrderEventCountableConnection";
+  edges: Home_activities_edges[];
+}
+
+export interface Home {
+  salesToday: Home_salesToday | null;
+  ordersToday: Home_ordersToday | null;
+  ordersToFulfill: Home_ordersToFulfill | null;
+  ordersToCapture: Home_ordersToCapture | null;
+  productsOutOfStock: Home_productsOutOfStock | null;
+  productTopToday: Home_productTopToday | null;
+  activities: Home_activities | null;
+}
