1801060c27a9ea7869b87842f19b52ac435d74fe
dominik-zeglen <flesz3@o2.pl>
2018-10-29 13:17:02 +0100

    Add customer removal

--- saleor/static/dashboard-next/customers/urls.ts
+++ saleor/static/dashboard-next/customers/urls.ts
@@ -1,4 +1,6 @@
 export const customerSection = "/customers/";
 export const customerListUrl = customerSection;
 export const customerUrl = (id: string) => customerSection + id + "/";
+
 export const customerAddUrl = customerSection + "add/";
+export const customerRemoveUrl = (id: string) => customerUrl(id) + "remove/";
--- saleor/static/dashboard-next/customers/views/CustomerDetails.tsx
+++ saleor/static/dashboard-next/customers/views/CustomerDetails.tsx
@@ -1,15 +1,22 @@
+import DialogContentText from "@material-ui/core/DialogContentText";
 import * as React from "react";
+import { Route } from "react-router-dom";
 
+import ActionDialog from "../../components/ActionDialog";
 import Messages from "../../components/messages";
 import Navigator from "../../components/Navigator";
 import i18n from "../../i18n";
 import { maybe } from "../../misc";
 import { orderUrl } from "../../orders";
 import CustomerDetailsPage from "../components/CustomerDetailsPage/CustomerDetailsPage";
-import { TypedUpdateCustomerMutation } from "../mutations";
+import {
+  TypedRemoveCustomerMutation,
+  TypedUpdateCustomerMutation
+} from "../mutations";
 import { TypedCustomerDetailsQuery } from "../queries";
+import { RemoveCustomer } from "../types/RemoveCustomer";
 import { UpdateCustomer } from "../types/UpdateCustomer";
-import { customerListUrl } from "../urls";
+import { customerListUrl, customerRemoveUrl, customerUrl } from "../urls";
 
 interface CustomerDetailsViewProps {
   id: string;
@@ -34,44 +41,96 @@ export const CustomerDetailsView: React.StatelessComponent<
               });
             }
           };
+          const handleCustomerRemoveSuccess = (data: RemoveCustomer) => {
+            if (
+              data.customerDelete.errors === null ||
+              data.customerDelete.errors.length === 0
+            ) {
+              pushMessage({
+                text: i18n.t("Customer removed", {
+                  context: "notification"
+                })
+              });
+              navigate(customerListUrl);
+            }
+          };
           return (
-            <TypedUpdateCustomerMutation
-              onCompleted={handleCustomerUpdateSuccess}
+            <TypedRemoveCustomerMutation
+              variables={{ id }}
+              onCompleted={handleCustomerRemoveSuccess}
             >
-              {(updateCustomer, updateCustomerOpts) => (
-                <TypedCustomerDetailsQuery variables={{ id }}>
-                  {customerDetails => (
-                    <CustomerDetailsPage
-                      customer={customerDetails.data.user}
-                      disabled={
-                        customerDetails.loading || updateCustomerOpts.loading
-                      }
-                      errors={maybe(
-                        () => updateCustomerOpts.data.customerUpdate.errors
-                      )}
-                      onAddressManageClick={() => undefined} // TODO: add address management #3173
-                      onBack={() => navigate(customerListUrl)}
-                      onRowClick={id =>
-                        navigate(orderUrl(encodeURIComponent(id)))
-                      }
-                      onSubmit={formData =>
-                        updateCustomer({
-                          variables: {
-                            id,
-                            input: {
-                              email: formData.email,
-                              note: formData.note // TODO: add isActive #3174
+              {(removeCustomer, removeCustomerOpts) => (
+                <TypedUpdateCustomerMutation
+                  onCompleted={handleCustomerUpdateSuccess}
+                >
+                  {(updateCustomer, updateCustomerOpts) => (
+                    <TypedCustomerDetailsQuery variables={{ id }}>
+                      {customerDetails => (
+                        <>
+                          <CustomerDetailsPage
+                            customer={customerDetails.data.user}
+                            disabled={
+                              customerDetails.loading ||
+                              updateCustomerOpts.loading ||
+                              removeCustomerOpts.loading
                             }
-                          }
-                        })
-                      }
-                      onDelete={() => undefined} // add removal modal #3175
-                      onViewAllOrdersClick={() => undefined} // TODO: add filters to order #3172
-                    />
+                            errors={maybe(
+                              () =>
+                                updateCustomerOpts.data.customerUpdate.errors
+                            )}
+                            onAddressManageClick={() => undefined} // TODO: add address management #3173
+                            onBack={() => navigate(customerListUrl)}
+                            onRowClick={id =>
+                              navigate(orderUrl(encodeURIComponent(id)))
+                            }
+                            onSubmit={formData =>
+                              updateCustomer({
+                                variables: {
+                                  id,
+                                  input: {
+                                    email: formData.email,
+                                    note: formData.note // TODO: add isActive #3174
+                                  }
+                                }
+                              })
+                            }
+                            onDelete={() => navigate(customerRemoveUrl(id))}
+                            onViewAllOrdersClick={() => undefined} // TODO: add filters to order #3172
+                          />
+                          <Route exact path={customerRemoveUrl(id)}>
+                            {({ match }) => (
+                              <ActionDialog
+                                onClose={() => navigate(customerUrl(id))}
+                                onConfirm={() => removeCustomer()}
+                                title={i18n.t("Remove customer", {
+                                  context: "modal title"
+                                })}
+                                variant="delete"
+                                open={!!match}
+                              >
+                                <DialogContentText
+                                  dangerouslySetInnerHTML={{
+                                    __html: i18n.t(
+                                      "Are you sure you want to remove <strong>{{ email }}</strong>?",
+                                      {
+                                        context: "modal content",
+                                        email: maybe(
+                                          () => customerDetails.data.user.email
+                                        )
+                                      }
+                                    )
+                                  }}
+                                />
+                              </ActionDialog>
+                            )}
+                          </Route>
+                        </>
+                      )}
+                    </TypedCustomerDetailsQuery>
                   )}
-                </TypedCustomerDetailsQuery>
+                </TypedUpdateCustomerMutation>
               )}
-            </TypedUpdateCustomerMutation>
+            </TypedRemoveCustomerMutation>
           );
         }}
       </Messages>
