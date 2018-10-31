4d1cd5f777ede5a973aef1785c3b62c0bc11a317
Lukasz Szewczyk <szewczyk134@gmail.com>
2018-10-31 16:45:43 +0100

    Add product list filters

--- saleor/static/dashboard-next/storybook/stories/orders/OrderListPage.tsx
+++ saleor/static/dashboard-next/storybook/stories/orders/OrderListPage.tsx
@@ -4,16 +4,55 @@ import * as React from "react";
 import { pageListProps } from "../../../fixtures";
 import OrderListPage from "../../../orders/components/OrderListPage";
 import { orders } from "../../../orders/fixtures";
+import { Filter } from "../../../products/components/ProductListCard";
 import Decorator from "../../Decorator";
+
+const filtersList: Filter[] = [
+  {
+    label: "Gardner-Schultz",
+    onClick: () => undefined
+  },
+  {
+    label: "Davis, Brown and Ray",
+    onClick: () => undefined
+  },
+  {
+    label: "Franklin Inc",
+    onClick: () => undefined
+  }
+];
 
 storiesOf("Views / Orders / Order list", module)
   .addDecorator(Decorator)
   .add("default", () => (
-    <OrderListPage orders={orders} {...pageListProps.default} />
+    <OrderListPage
+      orders={orders}
+      {...pageListProps.default}
+      filtersList={[]}
+      currentTab={0}
+    />
+  ))
+  .add("with custom filters", () => (
+    <OrderListPage
+      orders={orders}
+      {...pageListProps.loading}
+      filtersList={filtersList}
+      currentTab={0}
+    />
   ))
   .add("loading", () => (
-    <OrderListPage orders={undefined} {...pageListProps.loading} />
+    <OrderListPage
+      orders={undefined}
+      {...pageListProps.loading}
+      filtersList={undefined}
+      currentTab={undefined}
+    />
   ))
   .add("when no data", () => (
-    <OrderListPage orders={[]} {...pageListProps.default} />
+    <OrderListPage
+      orders={[]}
+      {...pageListProps.default}
+      filtersList={[]}
+      currentTab={0}
+    />
   ));
