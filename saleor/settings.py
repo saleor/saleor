16a8ab42048dda93dda46648720cf09f1cdf8e9f
Pacu2 <filip.m.kucharczyk@gmail.com>
2018-10-31 10:29:30 +0100

    Remove unused import

--- saleor/settings.py
+++ saleor/settings.py
@@ -5,7 +5,6 @@ import dj_database_url
 import dj_email_url
 import django_cache_url
 from django.contrib.messages import constants as messages
-from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import gettext_lazy as _, pgettext_lazy
 from django_prices.templatetags.prices_i18n import get_currency_fraction
 
