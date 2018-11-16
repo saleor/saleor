68e54f955a568ec910504aaf47a70909960475bc
dominik-zeglen <flesz3@o2.pl>
2018-11-16 17:24:58 +0100

    Extract staff view modals outside pages

--- saleor/static/dashboard-next/staff/urls.ts
+++ saleor/static/dashboard-next/staff/urls.ts
@@ -2,4 +2,7 @@ import * as urlJoin from "url-join";
 
 const staffSection = "/staff/";
 export const staffListUrl = staffSection;
+export const staffMemberAddUrl = urlJoin(staffSection, "add");
 export const staffMemberDetailsUrl = (id: string) => urlJoin(staffSection, id);
+export const staffMemberRemoveUrl = (id: string) =>
+  urlJoin(staffMemberDetailsUrl(id), "remove");
