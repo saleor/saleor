f5a0d6f5f160c82e309b280e39c9e404098fad41
dominik-zeglen <flesz3@o2.pl>
2018-11-16 17:44:29 +0100

    Remove unused import

--- saleor/static/dashboard-next/staff/views/StaffList.tsx
+++ saleor/static/dashboard-next/staff/views/StaffList.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Route } from "react-router-dom";
-import ActionDialog from "../../components/ActionDialog";
 
 import Messages from "../../components/messages";
 import Navigator from "../../components/Navigator";
