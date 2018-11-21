f9fd672e3179b74ba1a7a6381bfc77ee3a2959dc
Marcin Gębala <maarcin.gebala@gmail.com>
2018-11-21 16:35:00 +0100

    Add more tests for GraphQL view

--- tests/api/test_view.py
+++ tests/api/test_view.py
@@ -1,5 +1,7 @@
 import graphene
+import pytest
 
+from .conftest import API_PATH
 from .utils import get_graphql_content
 
 
@@ -35,3 +37,21 @@ def test_batch_queries(category, product, staff_api_client):
         for field, value in content['data'].items()}
     assert data['product']['name'] == product.name
     assert data['category']['name'] == category.name
+
+
+def test_graphql_view_get(client):
+    response = client.get(API_PATH)
+    assert response.status_code == 200
+    assert response.templates[0].name == 'graphql/playground.html'
+
+
+def test_graphql_view_options(client):
+    response = client.options(API_PATH)
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize('method', ('put', 'patch', 'delete'))
+def test_graphql_view_not_allowed(method, client):
+    func = getattr(client, method)
+    response = func(API_PATH)
+    assert response.status_code == 405
