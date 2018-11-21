9d2a9d318596e23179bf68815b779a026694eb7e
dominik-zeglen <flesz3@o2.pl>
2018-11-21 14:43:06 +0100

    Update stories

--- saleor/static/dashboard-next/storybook/stories/orders/OrderMarkAsPaidDialog.tsx
+++ saleor/static/dashboard-next/storybook/stories/orders/OrderMarkAsPaidDialog.tsx
@@ -1,14 +1,17 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
-import OrderMarkAsPaidDialog, { OrderMarkAsPaidDialogProps } from "../../../orders/components/OrderMarkAsPaidDialog";
+import OrderMarkAsPaidDialog, {
+  OrderMarkAsPaidDialogProps
+} from "../../../orders/components/OrderMarkAsPaidDialog";
 import Decorator from "../../Decorator";
 
-const props:OrderMarkAsPaidDialogProps = {
-
-}
+const props: OrderMarkAsPaidDialogProps = {
+  onClose: () => undefined,
+  onConfirm: () => undefined,
+  open: true
+};
 
 storiesOf("Orders / OrderMarkAsPaidDialog", module)
   .addDecorator(Decorator)
-  .add("default", () => <OrderMarkAsPaidDialog {...OrderMarkAsPaidDialogProps} />)
-  .add("other", () => <OrderMarkAsPaidDialog {...OrderMarkAsPaidDialogProps} />);
+  .add("default", () => <OrderMarkAsPaidDialog {...props} />);
