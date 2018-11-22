6b05288cb8f99178c0ff98a6514fa7d327400107
dominik-zeglen <flesz3@o2.pl>
2018-11-22 16:35:01 +0100

    Add confirm button

--- saleor/static/dashboard-next/components/SaveButtonBar/SaveButtonBar.tsx
+++ saleor/static/dashboard-next/components/SaveButtonBar/SaveButtonBar.tsx
@@ -1,22 +1,17 @@
 import Button from "@material-ui/core/Button";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import gray from "@material-ui/core/colors/grey";
 import { withStyles } from "@material-ui/core/styles";
-import CheckIcon from "@material-ui/icons/Check";
-import classNames from "classnames";
 import * as React from "react";
 
 import i18n from "../../i18n";
+import { maybe } from "../../misc";
+import ConfirmButton, {
+  ConfirmButtonTransitionState
+} from "../ConfirmButton/ConfirmButton";
 
-export type SaveButtonBarState =
-  | "loading"
-  | "success"
-  | "error"
-  | "default"
-  | string;
 interface SaveButtonBarProps {
-  disabled?: boolean;
-  state?: SaveButtonBarState;
+  disabled: boolean;
+  state: ConfirmButtonTransitionState;
   labels?: {
     cancel?: string;
     delete?: string;
@@ -31,15 +26,6 @@ const decorate = withStyles(theme => ({
   button: {
     marginRight: theme.spacing.unit
   },
-  buttonProgress: {
-    "& svg": {
-      color: theme.palette.common.white,
-      margin: 0
-    },
-    opacity: 0,
-    position: "absolute" as "absolute",
-    transition: theme.transitions.duration.standard + "ms"
-  },
   cancelButton: {
     marginRight: theme.spacing.unit * 2
   },
@@ -49,25 +35,6 @@ const decorate = withStyles(theme => ({
     },
     backgroundColor: theme.palette.error.main,
     color: theme.palette.error.contrastText
-  },
-  error: {
-    "&:hover": {
-      backgroundColor: theme.palette.error.main
-    },
-    backgroundColor: theme.palette.error.main,
-    color: theme.palette.error.contrastText
-  },
-  icon: {
-    marginLeft: "0 !important",
-    opacity: 0,
-    position: "absolute" as "absolute",
-    transition: theme.transitions.duration.standard + "ms"
-  },
-  label: {
-    transition: theme.transitions.duration.standard + "ms"
-  },
-  labelInvisible: {
-    opacity: 0
   },
   root: {
     borderTop: `1px ${gray[300]} solid`,
@@ -81,16 +48,6 @@ const decorate = withStyles(theme => ({
   },
   spacer: {
     flex: "1"
-  },
-  success: {
-    "&:hover": {
-      backgroundColor: theme.palette.primary.main
-    },
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText
-  },
-  visible: {
-    opacity: 1
   }
 }));
 export const SaveButtonBar = decorate<SaveButtonBarProps>(
@@ -104,26 +61,6 @@ export const SaveButtonBar = decorate<SaveButtonBarProps>(
     onSave,
     ...props
   }) => {
-    let saveButtonClassName;
-    let buttonLabel;
-    switch (state) {
-      case "success":
-        saveButtonClassName = classes.success;
-        buttonLabel = i18n.t("Save");
-        break;
-      case "error":
-        saveButtonClassName = classes.error;
-        buttonLabel = i18n.t("Error");
-        break;
-      case "loading":
-        saveButtonClassName = "";
-        buttonLabel = i18n.t("Save");
-        break;
-      default:
-        saveButtonClassName = "";
-        buttonLabel = labels && labels.save ? labels.save : i18n.t("Save");
-        break;
-    }
     return (
       <div className={classes.root} {...props}>
         {!!onDelete && (
@@ -143,37 +80,18 @@ export const SaveButtonBar = decorate<SaveButtonBarProps>(
         >
           {labels && labels.cancel ? labels.cancel : i18n.t("Cancel")}
         </Button>
-        <Button
-          variant="contained"
-          onClick={state === "loading" ? undefined : onSave}
-          color="secondary"
+        <ConfirmButton
           disabled={disabled}
-          className={saveButtonClassName}
+          onClick={onSave}
+          transitionState={state}
         >
-          <CircularProgress
-            size={24}
-            color="inherit"
-            className={classNames({
-              [classes.buttonProgress]: true,
-              [classes.visible]: state === "loading"
-            })}
-          />
-          <CheckIcon
-            className={classNames({
-              [classes.icon]: true,
-              [classes.visible]: state === "success"
-            })}
-          />
-          <span
-            className={classNames({
-              [classes.label]: true,
-              [classes.labelInvisible]:
-                state === "loading" || state === "success"
-            })}
-          >
-            {buttonLabel}
-          </span>
-        </Button>
+          {maybe(
+            () => labels.save,
+            i18n.t("Save", {
+              context: "button"
+            })
+          )}
+        </ConfirmButton>
       </div>
     );
   }
