fb21badf98793ade8cf0992f76e70a6bc695984d
dominik-zeglen <flesz3@o2.pl>
2018-11-28 12:20:27 +0100

    Split paths and urls in staff views

--- saleor/static/dashboard-next/staff/urls.ts
+++ saleor/static/dashboard-next/staff/urls.ts
@@ -1,8 +1,18 @@
 import * as urlJoin from "url-join";
 
 const staffSection = "/staff/";
-export const staffListUrl = staffSection;
-export const staffMemberAddUrl = urlJoin(staffSection, "add");
-export const staffMemberDetailsUrl = (id: string) => urlJoin(staffSection, id);
+
+export const staffListPath = staffSection;
+export const staffListUrl = staffListPath;
+
+export const staffMemberAddPath = urlJoin(staffSection, "add");
+export const staffMemberAddUrl = staffMemberAddPath;
+
+export const staffMemberDetailsPath = (id: string) => urlJoin(staffSection, id);
+export const staffMemberDetailsUrl = (id: string) =>
+  staffMemberDetailsPath(encodeURIComponent(id));
+
+export const staffMemberRemovePath = (id: string) =>
+  urlJoin(staffMemberDetailsPath(id), "remove");
 export const staffMemberRemoveUrl = (id: string) =>
-  urlJoin(staffMemberDetailsUrl(id), "remove");
+  staffMemberRemovePath(encodeURIComponent(id));
