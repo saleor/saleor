7e23291ec6b99cebddf2340b88ef5aadb951b772
dominik-zeglen <flesz3@o2.pl>
2018-11-29 14:31:20 +0100

    Add transition states to collection views

--- saleor/static/dashboard-next/collections/components/CollectionAssignProductDialog/CollectionAssignProductDialog.tsx
+++ saleor/static/dashboard-next/collections/components/CollectionAssignProductDialog/CollectionAssignProductDialog.tsx
@@ -6,6 +6,9 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import { withStyles } from "@material-ui/core/styles";
 import * as React from "react";
 
+import ConfirmButton, {
+  ConfirmButtonTransitionState
+} from "../../../components/ConfirmButton/ConfirmButton";
 import Form from "../../../components/Form";
 import { SingleAutocompleteSelectField } from "../../../components/SingleAutocompleteSelectField";
 import i18n from "../../../i18n";
@@ -18,6 +21,7 @@ export interface FormData {
 }
 
 interface CollectionAssignProductDialogProps {
+  confirmButtonState: ConfirmButtonTransitionState;
   open: boolean;
   products: Array<{
     id: string;
@@ -45,48 +49,64 @@ const initialForm: FormData = {
 };
 const CollectionAssignProductDialog = decorate<
   CollectionAssignProductDialogProps
->(({ classes, open, loading, products, fetch, onClose, onSubmit }) => (
-  <Dialog
-    open={open}
-    classes={{ paper: classes.overflow }}
-    fullWidth
-    maxWidth="sm"
-  >
-    <Form initial={initialForm} onSubmit={onSubmit}>
-      {({ data, change }) => {
-        const choices =
-          !loading && products
-            ? products.map(product => ({
-                label: product.name,
-                value: product.id
-              }))
-            : [];
-        return (
-          <>
-            <DialogTitle>{i18n.t("Add product")}</DialogTitle>
-            <DialogContent className={classes.overflow}>
-              <SingleAutocompleteSelectField
-                name="product"
-                value={data.product}
-                choices={choices}
-                onChange={change}
-                fetchChoices={fetch}
-                loading={loading}
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={onClose}>
-                {i18n.t("Cancel", { context: "button" })}
-              </Button>
-              <Button color="primary" variant="contained" type="submit">
-                {i18n.t("Confirm", { context: "button" })}
-              </Button>
-            </DialogActions>
-          </>
-        );
-      }}
-    </Form>
-  </Dialog>
-));
+>(
+  ({
+    classes,
+    confirmButtonState,
+    open,
+    loading,
+    products,
+    fetch,
+    onClose,
+    onSubmit
+  }) => (
+    <Dialog
+      open={open}
+      classes={{ paper: classes.overflow }}
+      fullWidth
+      maxWidth="sm"
+    >
+      <Form initial={initialForm} onSubmit={onSubmit}>
+        {({ data, change }) => {
+          const choices =
+            !loading && products
+              ? products.map(product => ({
+                  label: product.name,
+                  value: product.id
+                }))
+              : [];
+          return (
+            <>
+              <DialogTitle>{i18n.t("Add product")}</DialogTitle>
+              <DialogContent className={classes.overflow}>
+                <SingleAutocompleteSelectField
+                  name="product"
+                  value={data.product}
+                  choices={choices}
+                  onChange={change}
+                  fetchChoices={fetch}
+                  loading={loading}
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={onClose}>
+                  {i18n.t("Cancel", { context: "button" })}
+                </Button>
+                <ConfirmButton
+                  transitionState={confirmButtonState}
+                  color="primary"
+                  variant="contained"
+                  type="submit"
+                >
+                  {i18n.t("Confirm", { context: "button" })}
+                </ConfirmButton>
+              </DialogActions>
+            </>
+          );
+        }}
+      </Form>
+    </Dialog>
+  )
+);
 CollectionAssignProductDialog.displayName = "CollectionAssignProductDialog";
 export default CollectionAssignProductDialog;
