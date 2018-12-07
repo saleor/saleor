a68cbbf1e4685911b7194ef0fc611fddecf97d8a
Unknown <jxltom@gmail.com>
2018-12-07 20:30:20 +0800

    Fix a bug where logic of refund/logic is not handled correctly

--- saleor/payment/gateways/stripe/errors.py
+++ saleor/payment/gateways/stripe/errors.py
@@ -1,4 +1,4 @@
 from django.utils.translation import pgettext_lazy
 
 ORDER_NOT_CHARGED = pgettext_lazy(
-    'Razorpay payment error', 'Order was not charged.')
+    'Stripe payment error', 'Order was not charged.')
