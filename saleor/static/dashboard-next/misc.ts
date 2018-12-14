7a74b385824fdea648b1e9cc24dd7de24bdec8b8
dominik-zeglen <flesz3@o2.pl>
2018-12-14 16:19:07 +0100

    Use full email instead of trimmed

--- saleor/static/dashboard-next/misc.ts
+++ saleor/static/dashboard-next/misc.ts
@@ -177,10 +177,12 @@ interface User {
   lastName?: string;
 }
 
-export function getUserName(user?: User) {
+export function getUserName(user?: User, returnEmail?: boolean) {
   return user && (user.email || (user.firstName && user.lastName))
     ? user.firstName && user.lastName
       ? [user.firstName, user.lastName].join(" ")
+      : returnEmail
+      ? user.email
       : user.email.split("@")[0]
     : null;
 }
