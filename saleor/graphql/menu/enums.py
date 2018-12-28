b8a02420dcbf9ed94804a2e6da359c6cc65c8908
Marcin Gębala <maarcin.gebala@gmail.com>
2018-12-28 15:58:49 +0100

    Extract enums for menus

---
+++ saleor/graphql/menu/enums.py
@@ -0,0 +1,11 @@
+import graphene
+
+class NavigationType(graphene.Enum):
+    MAIN = 'main'
+    SECONDARY = 'secondary'
+
+    @property
+    def description(self):
+        if self == NavigationType.MAIN:
+            return 'Main storefront\'s navigation.'
+        return 'Secondary storefront\'s navigation.'
