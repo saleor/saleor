410301a1bdf1cd54f2ab661396ceb6417f81c080
dominik-zeglen <flesz3@o2.pl>
2019-01-18 12:01:41 +0100

    Remove unused import

--- saleor/static/dashboard-next/taxes/components/CountryList/CountryList.tsx
+++ saleor/static/dashboard-next/taxes/components/CountryList/CountryList.tsx
@@ -3,7 +3,6 @@ import { createStyles, withStyles, WithStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableFooter from "@material-ui/core/TableFooter";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import * as classNames from "classnames";
