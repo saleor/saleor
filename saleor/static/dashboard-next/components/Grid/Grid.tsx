556fd16d1f8d36b6bb0461be86b0f39084722d61
dominik-zeglen <flesz3@o2.pl>
2019-01-18 15:54:34 +0100

    Add grid variants

--- saleor/static/dashboard-next/components/Grid/Grid.tsx
+++ saleor/static/dashboard-next/components/Grid/Grid.tsx
@@ -4,30 +4,54 @@ import {
   withStyles,
   WithStyles
 } from "@material-ui/core/styles";
+import classNames from "classnames";
 import * as React from "react";
 
-interface GridProps extends WithStyles<typeof styles> {
+export type GridVariant = "default" | "inverted" | "uniform";
+export interface GridProps extends WithStyles<typeof styles> {
   children: React.ReactNodeArray | React.ReactNode;
+  variant?: GridVariant;
 }
 
 const styles = (theme: Theme) =>
   createStyles({
+    default: {
+      gridTemplateColumns: "9fr 4fr"
+    },
+    inverted: {
+      gridTemplateColumns: "4fr 9fr"
+    },
     root: {
       display: "grid",
       gridColumnGap: theme.spacing.unit * 2 + "px",
-      gridTemplateColumns: "9fr 4fr",
       [theme.breakpoints.down("sm")]: {
         "& > div": {
           marginBottom: theme.spacing.unit
         },
         gridTemplateColumns: "1fr"
       }
+    },
+    uniform: {
+      gridTemplateColumns: "1fr 1fr"
     }
   });
 
 export const Grid = withStyles(styles, { name: "Grid" })(
-  ({ children, classes }: GridProps) => (
-    <div className={classes.root}>{children}</div>
+  ({ children, classes, variant }: GridProps) => (
+    <div
+      className={classNames({
+        [classes.root]: true,
+        [classes.default]: variant === "default",
+        [classes.inverted]: variant === "inverted",
+        [classes.uniform]: variant === "uniform"
+      })}
+    >
+      {children}
+    </div>
   )
 );
+Grid.displayName = "Grid";
+Grid.defaultProps = {
+  variant: "default"
+};
 export default Grid;
