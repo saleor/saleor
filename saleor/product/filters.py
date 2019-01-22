d4d71586cb31a54f1ca4291f0ca6e7775dde6afa
Unknown <jxltom@gmail.com>
2019-01-22 13:57:01 +0800

    Add custom attribute multiple choice filter

--- saleor/product/filters.py
+++ saleor/product/filters.py
@@ -15,6 +15,48 @@ SORT_BY_FIELDS = OrderedDict([
         'Product list sorting option', 'last updated'))])
 
 
+class AttributeMultipleChoiceFilter(MultipleChoiceFilter):
+    def __init__(self, merged_attributes, *args, **kwargs):
+        self._merged_attributes = merged_attributes
+        super().__init__(*args, **kwargs)
+
+    def filter(self, qs, value):
+        """Copied from django-filter directly but only modified
+        get_filter_predicate which supports returning query directly."""
+        if not value:
+            # Even though not a noop, no point filtering if empty.
+            return qs
+
+        if self.is_noop(qs, value):
+            return qs
+
+        if not self.conjoined:
+            q = Q()
+        for v in set(value):
+            if v == self.null_value:
+                v = None
+            predicate = self.get_filter_predicate(v)
+            if self.conjoined:
+                qs = self.get_method(qs)(predicate)
+            else:
+                q |= Q(predicate)
+
+        if not self.conjoined:
+            qs = self.get_method(qs)(q)
+
+        return qs.distinct() if self.distinct else qs
+
+    def get_filter_predicate(self, v):
+        # Parse attribute scalar
+        attribute = parse_attribute(v)
+        if attribute is None:
+            raise ValueError(
+                'Unknown attribute scalar: %r' % (attribute,))
+
+        attr_slug, value_slug = attribute
+        return self._merged_attributes.get_query(attr_slug, value_slug)
+
+
 class ProductFilter(SortedFilterSet):
     sort_by = OrderingFilter(
         label=pgettext_lazy('Product list sorting form', 'Sort by'),
@@ -29,26 +71,21 @@ class ProductFilter(SortedFilterSet):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.product_attributes, self.variant_attributes = (
-            self._get_attributes())
-        self.filters.update(self._get_product_attributes_filters())
-        self.filters.update(self._get_product_variants_attributes_filters())
+        self._merged_attributes = self._get_merged_attributes()
+        self.filters.update(self._get_attributes_filters())
         self.filters = OrderedDict(sorted(self.filters.items()))
 
-    def _get_attributes(self):
+    def _get_merged_attributes(self):
         q_product_attributes = self._get_product_attributes_lookup()
         q_variant_attributes = self._get_variant_attributes_lookup()
-        product_attributes = (
+        attributes = (
             Attribute.objects.all()
             .prefetch_related('translations', 'values__translations')
-            .filter(q_product_attributes)
+            .filter(q_product_attributes | q_variant_attributes)
             .distinct())
-        variant_attributes = (
-            Attribute.objects.all()
-            .prefetch_related('translations', 'values__translations')
-            .filter(q_variant_attributes)
-            .distinct())
-        return product_attributes, variant_attributes
+
+        merged_attributes = MergedAttributes(attributes)
+        return merged_attributes
 
     def _get_product_attributes_lookup(self):
         raise NotImplementedError()
@@ -67,11 +104,6 @@ class ProductFilter(SortedFilterSet):
                 widget=CheckboxSelectMultiple,
                 choices=self._merged_attributes.get_choices(attr_slug))
         return filters
-
-    def _get_attribute_choices(self, attribute):
-        return [
-            (choice.pk, choice.translated.name)
-            for choice in attribute.values.all()]
 
 
 class ProductCategoryFilter(ProductFilter):
