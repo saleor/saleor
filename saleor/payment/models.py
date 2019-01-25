e3f882802f3bc64fd8695e4757d6b9228d467c4b
Unknown <jxltom@gmail.com>
2019-01-25 16:57:27 +0800

    Migrate charge status of payment to maximum 20 characters

--- saleor/payment/models.py
+++ saleor/payment/models.py
@@ -35,7 +35,7 @@ class Payment(models.Model):
     #: Date and time of last modification
     modified = models.DateTimeField(auto_now=True)
     charge_status = models.CharField(
-        max_length=15,
+        max_length=20,
         choices=ChargeStatus.CHOICES,
         default=ChargeStatus.NOT_CHARGED)
 
