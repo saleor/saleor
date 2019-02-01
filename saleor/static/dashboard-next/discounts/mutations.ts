884f2979dbc08fb3f230d8f6d867060c02112d81
dominik-zeglen <flesz3@o2.pl>
2019-02-01 15:24:47 +0100

    Add voucher details view

--- saleor/static/dashboard-next/discounts/mutations.ts
+++ saleor/static/dashboard-next/discounts/mutations.ts
@@ -1,7 +1,12 @@
 import gql from "graphql-tag";
 
 import { TypedMutation } from "../mutations";
-import { saleDetailsFragment, saleFragment } from "./queries";
+import {
+  saleDetailsFragment,
+  saleFragment,
+  voucherDetailsFragment,
+  voucherFragment
+} from "./queries";
 import {
   SaleCataloguesAdd,
   SaleCataloguesAddVariables
@@ -13,6 +18,17 @@ import {
 import { SaleCreate, SaleCreateVariables } from "./types/SaleCreate";
 import { SaleDelete, SaleDeleteVariables } from "./types/SaleDelete";
 import { SaleUpdate, SaleUpdateVariables } from "./types/SaleUpdate";
+import {
+  VoucherCataloguesAdd,
+  VoucherCataloguesAddVariables
+} from "./types/VoucherCataloguesAdd";
+import {
+  VoucherCataloguesRemove,
+  VoucherCataloguesRemoveVariables
+} from "./types/VoucherCataloguesRemove";
+import { VoucherCreate, VoucherCreateVariables } from "./types/VoucherCreate";
+import { VoucherDelete, VoucherDeleteVariables } from "./types/VoucherDelete";
+import { VoucherUpdate, VoucherUpdateVariables } from "./types/VoucherUpdate";
 
 const saleUpdate = gql`
   ${saleFragment}
@@ -115,3 +131,108 @@ const saleDelete = gql`
 export const TypedSaleDelete = TypedMutation<SaleDelete, SaleDeleteVariables>(
   saleDelete
 );
+
+const voucherUpdate = gql`
+  ${voucherFragment}
+  mutation VoucherUpdate($input: VoucherInput!, $id: ID!) {
+    voucherUpdate(id: $id, input: $input) {
+      errors {
+        field
+        message
+      }
+      voucher {
+        ...VoucherFragment
+      }
+    }
+  }
+`;
+export const TypedVoucherUpdate = TypedMutation<
+  VoucherUpdate,
+  VoucherUpdateVariables
+>(voucherUpdate);
+
+const voucherCataloguesAdd = gql`
+  ${voucherDetailsFragment}
+  mutation VoucherCataloguesAdd(
+    $input: CatalogueInput!
+    $id: ID!
+    $after: String
+    $before: String
+    $first: Int
+    $last: Int
+  ) {
+    voucherCataloguesAdd(id: $id, input: $input) {
+      errors {
+        field
+        message
+      }
+      voucher {
+        ...VoucherDetailsFragment
+      }
+    }
+  }
+`;
+export const TypedVoucherCataloguesAdd = TypedMutation<
+  VoucherCataloguesAdd,
+  VoucherCataloguesAddVariables
+>(voucherCataloguesAdd);
+
+const voucherCataloguesRemove = gql`
+  ${voucherDetailsFragment}
+  mutation VoucherCataloguesRemove(
+    $input: CatalogueInput!
+    $id: ID!
+    $after: String
+    $before: String
+    $first: Int
+    $last: Int
+  ) {
+    voucherCataloguesRemove(id: $id, input: $input) {
+      errors {
+        field
+        message
+      }
+      voucher {
+        ...VoucherDetailsFragment
+      }
+    }
+  }
+`;
+export const TypedVoucherCataloguesRemove = TypedMutation<
+  VoucherCataloguesRemove,
+  VoucherCataloguesRemoveVariables
+>(voucherCataloguesRemove);
+
+const voucherCreate = gql`
+  ${voucherFragment}
+  mutation VoucherCreate($input: VoucherInput!) {
+    voucherCreate(input: $input) {
+      errors {
+        field
+        message
+      }
+      voucher {
+        ...VoucherFragment
+      }
+    }
+  }
+`;
+export const TypedVoucherCreate = TypedMutation<
+  VoucherCreate,
+  VoucherCreateVariables
+>(voucherCreate);
+
+const voucherDelete = gql`
+  mutation VoucherDelete($id: ID!) {
+    voucherDelete(id: $id) {
+      errors {
+        field
+        message
+      }
+    }
+  }
+`;
+export const TypedVoucherDelete = TypedMutation<
+  VoucherDelete,
+  VoucherDeleteVariables
+>(voucherDelete);
