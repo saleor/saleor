3fee3ade771b4d3692faf967b32d87098a6ef291
dominik-zeglen <flesz3@o2.pl>
2019-02-11 15:07:57 +0100

    Sort imports

--- saleor/static/dashboard-next/discounts/components/DiscountCountrySelectDialog/DiscountCountrySelectDialog.tsx
+++ saleor/static/dashboard-next/discounts/components/DiscountCountrySelectDialog/DiscountCountrySelectDialog.tsx
@@ -6,9 +6,9 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import {
   createStyles,
+  Theme,
   withStyles,
-  WithStyles,
-  Theme
+  WithStyles
 } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
