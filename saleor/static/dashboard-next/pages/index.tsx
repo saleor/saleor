40c7e505ede5e5cc060e99088f91ead6ec00a8e8
dominik-zeglen <flesz3@o2.pl>
2019-02-12 15:50:57 +0100

    Add page details view

--- saleor/static/dashboard-next/pages/index.tsx
+++ saleor/static/dashboard-next/pages/index.tsx
@@ -5,7 +5,7 @@ import { Route, RouteComponentProps, Switch } from "react-router-dom";
 import { WindowTitle } from "../components/WindowTitle";
 import i18n from "../i18n";
 import { pageListPath, pagePath } from "./urls";
-// import PageDetailsComponent from "./views/PageDetails";
+import PageDetailsComponent from "./views/PageDetails";
 import PageListComponent, { PageListQueryParams } from "./views/PageList";
 
 const PageList: React.StatelessComponent<RouteComponentProps<any>> = ({
@@ -19,18 +19,18 @@ const PageList: React.StatelessComponent<RouteComponentProps<any>> = ({
   return <PageListComponent params={params} />;
 };
 
-// const PageDetails: React.StatelessComponent<RouteComponentProps<any>> = ({
-//   match
-// }) => {
-//   return <PageDetailsComponent id={decodeURIComponent(match.params.id)} />;
-// };
+const PageDetails: React.StatelessComponent<RouteComponentProps<any>> = ({
+  match
+}) => {
+  return <PageDetailsComponent id={decodeURIComponent(match.params.id)} />;
+};
 
 const Component = () => (
   <>
     <WindowTitle title={i18n.t("Pages")} />
     <Switch>
       <Route exact path={pageListPath} component={PageList} />
-      {/* <Route path={pagePath(":id")} component={PageDetails} /> */}
+      <Route path={pagePath(":id")} component={PageDetails} />
     </Switch>
   </>
 );
