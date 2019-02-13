6198b6178aea181d1d6e07e1774beb328528ca72
dominik-zeglen <flesz3@o2.pl>
2019-02-13 17:32:26 +0100

    Fix date timezone bug

--- saleor/static/dashboard-next/storybook/stories/components/Date.tsx
+++ saleor/static/dashboard-next/storybook/stories/components/Date.tsx
@@ -6,5 +6,5 @@ import Decorator from "../../Decorator";
 
 storiesOf("Generics / Date", module)
   .addDecorator(Decorator)
-  .add("default", () => <Date date="2018-04-07T10:44:44+00:00" />)
-  .add("plain", () => <Date date="2018-04-07T10:44:44+00:00" plain={true} />);
+  .add("default", () => <Date date="2018-04-07" />)
+  .add("plain", () => <Date date="2018-04-07" plain={true} />);
