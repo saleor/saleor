acda2a27749c6d6228ad8096ef49b8adc6101776
dominik-zeglen <flesz3@o2.pl>
2019-02-13 12:00:23 +0100

    Add rich text editor component wip

---
+++ saleor/static/dashboard-next/components/RichTextEditor/RichTextEditor.tsx
@@ -0,0 +1,68 @@
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles
+} from "@material-ui/core/styles";
+import {
+  BLOCK_TYPE,
+  DraftailEditor,
+  ENTITY_TYPE,
+  INLINE_STYLE
+} from "draftail";
+import * as React from "react";
+
+export interface RichTextEditorProps {
+  disabled: boolean;
+  name: string;
+  initial: string;
+  onChange: (event: React.ChangeEvent<any>) => void;
+}
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      "& .Draftail-Toolbar": {
+        display: "flex"
+      }
+    }
+  });
+const RichTextEditor = withStyles(styles, { name: "RichTextEditor" })(
+  ({
+    classes,
+    initial,
+    name,
+    onChange
+  }: RichTextEditorProps & WithStyles<typeof styles>) => (
+    <div className={classes.root}>
+      <DraftailEditor
+        rawContentState={initial || null}
+        onSave={value =>
+          onChange({
+            target: {
+              name,
+              value
+            }
+          } as any)
+        }
+        blockTypes={[
+          { type: BLOCK_TYPE.HEADER_TWO },
+          { type: BLOCK_TYPE.HEADER_THREE },
+          { type: BLOCK_TYPE.BLOCKQUOTE },
+          { type: BLOCK_TYPE.UNORDERED_LIST_ITEM },
+          { type: BLOCK_TYPE.ORDERED_LIST_ITEM }
+        ]}
+        inlineStyles={[
+          { type: INLINE_STYLE.BOLD },
+          { type: INLINE_STYLE.ITALIC }
+        ]}
+        entityTypes={[
+          { type: ENTITY_TYPE.LINK },
+          { type: ENTITY_TYPE.HORIZONTAL_RULE }
+        ]}
+      />
+    </div>
+  )
+);
+RichTextEditor.displayName = "RichTextEditor";
+export default RichTextEditor;
---
+++ saleor/static/dashboard-next/storybook/stories/components/RichTextEditor.tsx
@@ -0,0 +1,25 @@
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import Form from "../../../components/Form";
+import RichTextEditor from "../../../components/RichTextEditor";
+import Decorator from "../../Decorator";
+
+storiesOf("Generics / Rich text editor", module)
+  .addDecorator(Decorator)
+  .add("default", () => (
+    <Form
+      initial={{
+        content: null
+      }}
+    >
+      {({ change, data }) => (
+        <RichTextEditor
+          disabled={false}
+          initial={data.content}
+          name="content"
+          onChange={change}
+        />
+      )}
+    </Form>
+  ));
