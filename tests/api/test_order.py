92b8955444cfe3b90afb6dc4eeb2ecd39ed0479f
Unknown <jxltom@gmail.com>
2019-02-14 09:42:21 +0800

    Fix more unused imports after merging from upstream

--- tests/api/test_order.py
+++ tests/api/test_order.py
@@ -9,8 +9,6 @@ from saleor.graphql.order.enums import OrderEventsEmailsEnum, OrderStatusFilter
 from saleor.graphql.order.mutations.orders import (
     clean_order_cancel, clean_order_capture, clean_refund_payment,
     clean_void_payment)
-from saleor.graphql.order.enums import (
-    OrderEventsEmailsEnum, OrderStatusFilter)
 from saleor.graphql.order.utils import can_finalize_draft_order
 from saleor.graphql.payment.types import PaymentChargeStatusEnum
 from saleor.order import OrderEvents, OrderEventsEmails, OrderStatus
