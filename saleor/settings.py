c02c4544ff40cafa01be5d2625deedeac65367e0
Kereat <artlicasio@gmail.com>
2019-02-16 17:55:26 +0300

    Added bayyes logo, changed 3 tmplates, added tabs

--- saleor/settings.py
+++ saleor/settings.py
@@ -378,7 +378,10 @@ bootstrap4 = {
 TEST_RUNNER = 'tests.runner.PytestTestRunner'
 
 ALLOWED_HOSTS = get_list(
-    os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1'))
+    os.environ.get('ALLOWED_HOSTS','localhost,127.0.0.1'))
+
+#ALLOWED_HOSTS = ["192.168.113.7"]
+
 ALLOWED_GRAPHQL_ORIGINS = os.environ.get('ALLOWED_GRAPHQL_ORIGINS', '*')
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
