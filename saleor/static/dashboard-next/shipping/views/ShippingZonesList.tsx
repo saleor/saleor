fba133ed870d790572e34ca7e522712c1e08f293
dominik-zeglen <flesz3@o2.pl>
2019-02-22 12:17:16 +0100

    Add shipping zone deletion dialog

--- saleor/static/dashboard-next/shipping/views/ShippingZonesList.tsx
+++ saleor/static/dashboard-next/shipping/views/ShippingZonesList.tsx
@@ -1,11 +1,14 @@
+import DialogContentText from "@material-ui/core/DialogContentText";
+import { stringify as stringifyQs } from "qs";
 import * as React from "react";
 
+import ActionDialog from "../../components/ActionDialog";
 import Messages from "../../components/messages";
 import Navigator from "../../components/Navigator";
 import { createPaginationState, Paginator } from "../../components/Paginator";
 import Shop from "../../components/Shop";
 import i18n from "../../i18n";
-import { maybe } from "../../misc";
+import { getMutationState, maybe } from "../../misc";
 import { Pagination } from "../../types";
 import ShippingZonesListPage from "../components/ShippingZonesListPage";
 import {
@@ -16,7 +19,10 @@ import { TypedShippingZones } from "../queries";
 import { UpdateDefaultWeightUnit } from "../types/UpdateDefaultWeightUnit";
 import { shippingZoneAddUrl, shippingZoneUrl } from "../urls";
 
-export type ShippingZonesListQueryParams = Pagination;
+export type ShippingZonesListQueryParams = Pagination &
+  Partial<{
+    delete: string;
+  }>;
 
 interface ShippingZonesListProps {
   params: ShippingZonesListQueryParams;
@@ -63,53 +69,120 @@ export const ShippingZonesList: React.StatelessComponent<
                           displayLoader
                           variables={paginationState}
                         >
-                          {({ data, loading }) => (
-                            <Paginator
-                              pageInfo={maybe(
-                                () => data.shippingZones.pageInfo
-                              )}
-                              paginationState={paginationState}
-                              queryString={params}
-                            >
-                              {({
-                                loadNextPage,
-                                loadPreviousPage,
-                                pageInfo
-                              }) => {
-                                return (
-                                  <ShippingZonesListPage
-                                    defaultWeightUnit={maybe(
-                                      () => shop.defaultWeightUnit
-                                    )}
-                                    disabled={
-                                      loading ||
-                                      deleteShippingZoneOpts.loading ||
-                                      updateDefaultWeightUnitOpts.loading
-                                    }
-                                    shippingZones={maybe(() =>
-                                      data.shippingZones.edges.map(
-                                        edge => edge.node
-                                      )
-                                    )}
-                                    pageInfo={pageInfo}
-                                    onAdd={() => navigate(shippingZoneAddUrl)}
-                                    onNextPage={loadNextPage}
-                                    onPreviousPage={loadPreviousPage}
-                                    onRemove={id =>
-                                      deleteShippingZone({ variables: { id } })
-                                    }
-                                    onRowClick={id => () =>
-                                      navigate(shippingZoneUrl(id))}
-                                    onSubmit={unit =>
-                                      updateDefaultWeightUnit({
-                                        variables: { unit }
-                                      })
-                                    }
-                                  />
-                                );
-                              }}
-                            </Paginator>
-                          )}
+                          {({ data, loading }) => {
+                            const deleteTransitionState = getMutationState(
+                              deleteShippingZoneOpts.called,
+                              deleteShippingZoneOpts.loading,
+                              maybe(
+                                () =>
+                                  deleteShippingZoneOpts.data.shippingZoneDelete
+                                    .errors
+                              )
+                            );
+
+                            return (
+                              <Paginator
+                                pageInfo={maybe(
+                                  () => data.shippingZones.pageInfo
+                                )}
+                                paginationState={paginationState}
+                                queryString={params}
+                              >
+                                {({
+                                  loadNextPage,
+                                  loadPreviousPage,
+                                  pageInfo
+                                }) => {
+                                  return (
+                                    <>
+                                      <ShippingZonesListPage
+                                        defaultWeightUnit={maybe(
+                                          () => shop.defaultWeightUnit
+                                        )}
+                                        disabled={
+                                          loading ||
+                                          deleteShippingZoneOpts.loading ||
+                                          updateDefaultWeightUnitOpts.loading
+                                        }
+                                        shippingZones={maybe(() =>
+                                          data.shippingZones.edges.map(
+                                            edge => edge.node
+                                          )
+                                        )}
+                                        pageInfo={pageInfo}
+                                        onAdd={() =>
+                                          navigate(shippingZoneAddUrl)
+                                        }
+                                        onNextPage={loadNextPage}
+                                        onPreviousPage={loadPreviousPage}
+                                        onRemove={id =>
+                                          navigate(
+                                            "?" +
+                                              stringifyQs({
+                                                ...params,
+                                                delete: id
+                                              })
+                                          )
+                                        }
+                                        onRowClick={id => () =>
+                                          navigate(shippingZoneUrl(id))}
+                                        onSubmit={unit =>
+                                          updateDefaultWeightUnit({
+                                            variables: { unit }
+                                          })
+                                        }
+                                      />
+                                      <ActionDialog
+                                        open={!!params.delete}
+                                        confirmButtonState={
+                                          deleteTransitionState
+                                        }
+                                        variant="delete"
+                                        title={i18n.t("Delete Shipping Zone", {
+                                          context: "modal title"
+                                        })}
+                                        onClose={() =>
+                                          navigate(
+                                            "?" +
+                                              stringifyQs({
+                                                ...params,
+                                                delete: undefined
+                                              })
+                                          )
+                                        }
+                                        onConfirm={() =>
+                                          deleteShippingZone({
+                                            variables: { id: params.delete }
+                                          })
+                                        }
+                                      >
+                                        <DialogContentText
+                                          dangerouslySetInnerHTML={{
+                                            __html: i18n.t(
+                                              "Are you sure you want to remove <strong>{{ name }}</strong>?",
+                                              {
+                                                context:
+                                                  "shipping zone removal",
+                                                name: maybe(
+                                                  () =>
+                                                    data.shippingZones.edges.find(
+                                                      edge =>
+                                                        edge.node.id ===
+                                                        params.delete
+                                                    ).node.name,
+                                                  "..."
+                                                )
+                                              }
+                                            )
+                                          }}
+                                        />
+                                      </ActionDialog>
+                                    </>
+                                  );
+                                }}
+                              </Paginator>
+                            );
+                          }}
                         </TypedShippingZones>
                       )}
                     </TypedUpdateDefaultWeightUnit>
