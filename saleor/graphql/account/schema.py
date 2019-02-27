2f39cdd26c146eb6af0f1db9fbdb202655cb50ab
Unknown <jxltom@gmail.com>
2019-02-27 11:06:09 +0800

    Only include cancelled order for staff

--- saleor/graphql/account/schema.py
+++ saleor/graphql/account/schema.py
@@ -38,7 +38,6 @@ class AccountQueries(graphene.ObjectType):
 
     @login_required
     def resolve_me(self, info):
-        info.context.kwargs = dict(ignore_cancelled_fulfillment=True)
         return info.context.user
 
     @permission_required('account.manage_staff')
