ff26826f00ec2eb4c6c848a65f6679e9fd49b000
Michał Jelonek <mjelonek92@gmail.com>
2019-03-06 11:19:08 +0100

    Add draft orders query

--- saleor/graphql/order/schema.py
+++ saleor/graphql/order/schema.py
@@ -20,7 +20,7 @@ from .mutations.orders import (
     OrderUpdate, OrderUpdateShipping, OrderVoid)
 from .resolvers import (
     resolve_homepage_events, resolve_order, resolve_order_by_token,
-    resolve_orders, resolve_orders_total)
+    resolve_orders, resolve_orders_total, resolve_draft_orders)
 from .types import Order, OrderEvent
 
 
@@ -40,6 +40,13 @@ class OrderQueries(graphene.ObjectType):
         status=graphene.Argument(
             OrderStatusFilter, description='Filter order by status'),
         description='List of the shop\'s orders.')
+    draft_orders = PrefetchingConnectionField(
+        Order,
+        query=graphene.String(description=DESCRIPTIONS['order']),
+        created=graphene.Argument(
+            ReportingPeriod,
+            description='Filter draft orders from a selected timespan.'),
+        description='List of the shop\'s draft orders.')
     orders_total = graphene.Field(
         TaxedMoney, description='Total sales.',
         period=graphene.Argument(
@@ -61,6 +68,10 @@ class OrderQueries(graphene.ObjectType):
     def resolve_orders(
             self, info, created=None, status=None, query=None, **kwargs):
         return resolve_orders(info, created, status, query)
+
+    @permission_required('order.manage_orders')
+    def resolve_draft_orders(self, info, created=None, query=None, **kwargs):
+        return resolve_draft_orders(info, created, query)
 
     @permission_required('order.manage_orders')
     def resolve_orders_total(self, info, period, **kwargs):
