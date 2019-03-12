2dc2696bdfcdc2e4cf0a2555638254da99201867
dominik-zeglen <flesz3@o2.pl>
2019-03-12 13:49:35 +0100

    Optimize scroll handler

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,32 @@
-import detectPassiveEvents from 'detect-passive-events';
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
-import { withRouter } from 'react-router';
-import ReactSVG from 'react-svg';
-import { withCookies } from 'react-cookie';
+import detectPassiveEvents from "detect-passive-events";
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import { withRouter } from "react-router";
+import ReactSVG from "react-svg";
+import { withCookies } from "react-cookie";
 
-import { GitHubLink } from '..';
-import { hotjar } from '../HotJar/HotJar'
+import { GitHubLink } from "..";
+import { hotjar } from "../HotJar/HotJar";
 
-import './header.css';
-import cookieImg from './../../images/cookie.png'
-import cookieAcceptImg from './../../images/cookie-accept.png'
+import "./header.css";
+import cookieImg from "./../../images/cookie.png";
+import cookieAcceptImg from "./../../images/cookie-accept.png";
 
 class Header extends Component {
-
   constructor(props) {
     super(props);
     const { cookies } = props;
-    const visiblePrivacyPolicyBar = cookies.get('privacypolicybar') ? false : true;
+    const visiblePrivacyPolicyBar = cookies.get("privacypolicybar")
+      ? false
+      : true;
     this.state = {
       mobileMenu: false,
       subMenu: false,
-      visibleNewsBar: cookies.get('newsbar') ? false : true,
+      visibleNewsBar: cookies.get("newsbar") ? false : true,
       visiblePrivacyPolicyBar,
       sticky: false,
-      scrollDirection: 'bottom'
-    }
+      scrollDirection: "bottom"
+    };
     this.lastScrollPos = null;
     if (!visiblePrivacyPolicyBar) {
       this.runHotJar();
@@ -33,94 +34,159 @@ class Header extends Component {
   }
 
   runHotJar = () => {
-    hotjar.initialize(716251, 6)
-  }
+    hotjar.initialize(716251, 6);
+  };
 
   toggleMenu = () => {
-    this.setState(({ mobileMenu }) => ({ mobileMenu: !mobileMenu }))
+    this.setState(({ mobileMenu }) => ({ mobileMenu: !mobileMenu }));
   };
 
   closeMenu = () => {
     this.setState({ mobileMenu: false });
-  }
+  };
 
-  toggleSubMenu = (e) => {
+  toggleSubMenu = e => {
     e.preventDefault();
-    this.setState(({ subMenu }) => ({ subMenu: !subMenu }))
-  }
+    this.setState(({ subMenu }) => ({ subMenu: !subMenu }));
+  };
 
   closeNewsBar = () => {
     const { cookies } = this.props;
     const maxAge = 14 * (24 * 3600);
-    cookies.set('newsbar', 1, { path: '/', maxAge: maxAge });
+    cookies.set("newsbar", 1, { path: "/", maxAge: maxAge });
     this.setState({ visibleNewsBar: false });
-  }
+  };
 
   closePrivacyPolicyBar = () => {
     const { cookies } = this.props;
     const maxAge = 30 * (24 * 3600);
-    cookies.set('privacypolicybar', 1, { path: '/', maxAge: maxAge });
+    cookies.set("privacypolicybar", 1, { path: "/", maxAge: maxAge });
     this.setState({ visiblePrivacyPolicyBar: false });
     this.runHotJar();
-  }
+  };
 
   componentDidMount() {
     this.lastScrollPos = window.scrollY;
     const opts = detectPassiveEvents.hasSupport ? { passive: true } : false;
-    window.addEventListener('scroll', this.handleScroll, opts);
+    window.addEventListener("scroll", this.handleScroll, opts);
   }
 
   componentWillUnmount() {
     const opts = detectPassiveEvents.hasSupport ? { passive: true } : false;
-    window.removeEventListener('scroll', this.handleScroll, opts);
+    window.removeEventListener("scroll", this.handleScroll, opts);
   }
 
-  handleScroll = (event) => {
+  handleScroll = () => {
     const scrollPosition = window.scrollY;
-    const { scrollDirection } = this.state;
-    if (this.lastScrollPos > scrollPosition && scrollDirection !== 'top') {
+    const { scrollDirection, sticky } = this.state;
+    if (this.lastScrollPos > scrollPosition && scrollDirection !== "top") {
       this.setState({
-        scrollDirection: 'top'
+        scrollDirection: "top"
       });
-    } else if (this.lastScrollPos < scrollPosition && scrollDirection !== 'bottom') {
+    } else if (
+      this.lastScrollPos < scrollPosition &&
+      scrollDirection !== "bottom"
+    ) {
       this.setState({
-        scrollDirection: 'bottom'
+        scrollDirection: "bottom"
       });
     }
-    if (scrollPosition > 120) {
+    if (scrollPosition > 120 && !sticky) {
       this.setState({ sticky: true });
-    } else {
+    } else if (scrollPosition <= 120 && sticky) {
       this.setState({ sticky: false });
     }
     this.lastScrollPos = scrollPosition;
-  }
+  };
 
   render() {
     return (
-      <header className={this.state.sticky ? ('sticky ' + this.state.scrollDirection) : null}>
-        {this.state.visibleNewsBar ?
+      <header
+        className={
+          this.state.sticky ? "sticky " + this.state.scrollDirection : null
+        }
+      >
+        {this.state.visibleNewsBar ? (
           <div className="news">
             <div className="content">
-              <a href="https://medium.com/p/1330f2151585" target="_blank">Saleor 2.0 release is out. <span className="text-underline">Check out what's new!</span></a>
-              <div className="close-icon" onClick={this.closeNewsBar}></div>
+              <a href="https://medium.com/p/1330f2151585" target="_blank">
+                Saleor 2.0 release is out.{" "}
+                <span className="text-underline">Check out what's new!</span>
+              </a>
+              <div className="close-icon" onClick={this.closeNewsBar} />
             </div>
-          </div> : null}
+          </div>
+        ) : null}
         <div className="container">
           <div className="grid">
-            <div className={this.state.mobileMenu ? 'logo open col-xs-3 col-ls-6 col-sm-6 col-md-3 col-lg-6 col-xlg-8' : 'logo col-xs-3 col-sm-6 col-md-3 col-lg-6 col-xlg-8'}>
-              <NavLink to="/" aria-label="Saleor Logo"><ReactSVG className="logo-svg" path="images/saleor-logo.svg" /></NavLink>
+            <div
+              className={
+                this.state.mobileMenu
+                  ? "logo open col-xs-3 col-ls-6 col-sm-6 col-md-3 col-lg-6 col-xlg-8"
+                  : "logo col-xs-3 col-sm-6 col-md-3 col-lg-6 col-xlg-8"
+              }
+            >
+              <NavLink to="/" aria-label="Saleor Logo">
+                <ReactSVG className="logo-svg" path="images/saleor-logo.svg" />
+              </NavLink>
             </div>
-            <nav className={this.state.visibleNewsBar ? 'menu newsbar col-xs-9 col-ls-6 col-sm-6 col-md-9 col-lg-6 col-xlg-4' : 'menu col-xs-9 col-sm-6 col-md-9 col-lg-6 col-xlg-4'}>
-              <ul className={this.state.mobileMenu ? 'menu-mobile hovered' : 'menu-desktop'}>
-                <li><span className="count">01. </span><NavLink exact to="/" onClick={this.closeMenu}>Home</NavLink></li>
-                <li className="underline"><span className="count">02. </span><NavLink to="/features" onClick={this.closeMenu}>Features</NavLink></li>
-                <li className="underline"><span className="count">03. </span><NavLink to="/roadmap" onClick={this.closeMenu}>Roadmap</NavLink></li>
-                <li className={this.state.subMenu ? 'background open' : 'background'}><span className="count">05. </span><a href="" onClick={e => this.toggleSubMenu (e)}>Developers <span className="trangleContainer"><span className="triangle"></span></span></a>
-                  <div className={this.state.subMenu ? 'submenu open' : 'submenu'}>
+            <nav
+              className={
+                this.state.visibleNewsBar
+                  ? "menu newsbar col-xs-9 col-ls-6 col-sm-6 col-md-9 col-lg-6 col-xlg-4"
+                  : "menu col-xs-9 col-sm-6 col-md-9 col-lg-6 col-xlg-4"
+              }
+            >
+              <ul
+                className={
+                  this.state.mobileMenu ? "menu-mobile hovered" : "menu-desktop"
+                }
+              >
+                <li>
+                  <span className="count">01. </span>
+                  <NavLink exact to="/" onClick={this.closeMenu}>
+                    Home
+                  </NavLink>
+                </li>
+                <li className="underline">
+                  <span className="count">02. </span>
+                  <NavLink to="/features" onClick={this.closeMenu}>
+                    Features
+                  </NavLink>
+                </li>
+                <li className="underline">
+                  <span className="count">03. </span>
+                  <NavLink to="/roadmap" onClick={this.closeMenu}>
+                    Roadmap
+                  </NavLink>
+                </li>
+                <li
+                  className={
+                    this.state.subMenu ? "background open" : "background"
+                  }
+                >
+                  <span className="count">05. </span>
+                  <a href="" onClick={e => this.toggleSubMenu(e)}>
+                    Developers{" "}
+                    <span className="trangleContainer">
+                      <span className="triangle" />
+                    </span>
+                  </a>
+                  <div
+                    className={this.state.subMenu ? "submenu open" : "submenu"}
+                  >
                     <div className="item">
-                      <a href="https://github.com/mirumee/saleor" target="_blank" rel="noopener">
+                      <a
+                        href="https://github.com/mirumee/saleor"
+                        target="_blank"
+                        rel="noopener"
+                      >
                         <div className="icon">
-                          <ReactSVG className="github-icon" svgStyle={{ width: 25, height: 25 }} path="images/github-icon.svg" />
+                          <ReactSVG
+                            className="github-icon"
+                            svgStyle={{ width: 25, height: 25 }}
+                            path="images/github-icon.svg"
+                          />
                         </div>
                         <div className="text">
                           <p>Github</p>
@@ -129,9 +195,16 @@ class Header extends Component {
                       </a>
                     </div>
                     <div className="item">
-                      <a href="https://gitter.im/mirumee/saleor" target="_blank" rel="noopener">
+                      <a
+                        href="https://gitter.im/mirumee/saleor"
+                        target="_blank"
+                        rel="noopener"
+                      >
                         <div className="icon">
-                          <ReactSVG className="gitter-icon" path="images/gitter-icon.svg" />
+                          <ReactSVG
+                            className="gitter-icon"
+                            path="images/gitter-icon.svg"
+                          />
                         </div>
                         <div className="text">
                           <p>Gitter</p>
@@ -140,9 +213,16 @@ class Header extends Component {
                       </a>
                     </div>
                     <div className="item">
-                      <a href="https://spectrum.chat/saleor" target="_blank" rel="noopener">
+                      <a
+                        href="https://spectrum.chat/saleor"
+                        target="_blank"
+                        rel="noopener"
+                      >
                         <div className="icon">
-                          <ReactSVG className="spectrum-icon" path="images/spectrum-icon.svg" />
+                          <ReactSVG
+                            className="spectrum-icon"
+                            path="images/spectrum-icon.svg"
+                          />
                         </div>
                         <div className="text">
                           <p>Spectrum</p>
@@ -151,9 +231,16 @@ class Header extends Component {
                       </a>
                     </div>
                     <div className="item">
-                      <a href="https://stackoverflow.com/questions/tagged/saleor" target="_blank" rel="noopener">
+                      <a
+                        href="https://stackoverflow.com/questions/tagged/saleor"
+                        target="_blank"
+                        rel="noopener"
+                      >
                         <div className="icon">
-                          <ReactSVG className="stackoverflow-icon" path="images/stackoverflow-icon.svg" />
+                          <ReactSVG
+                            className="stackoverflow-icon"
+                            path="images/stackoverflow-icon.svg"
+                          />
                         </div>
                         <div className="text">
                           <p>Stack Overflow</p>
@@ -162,9 +249,16 @@ class Header extends Component {
                       </a>
                     </div>
                     <div className="item">
-                      <a href="https://www.transifex.com/mirumee/saleor-1/" target="_blank" rel="noopener">
+                      <a
+                        href="https://www.transifex.com/mirumee/saleor-1/"
+                        target="_blank"
+                        rel="noopener"
+                      >
                         <div className="icon">
-                          <ReactSVG className="transifex-icon" path="images/transifex-icon.svg" />
+                          <ReactSVG
+                            className="transifex-icon"
+                            path="images/transifex-icon.svg"
+                          />
                         </div>
                         <div className="text">
                           <p>Transifex</p>
@@ -174,32 +268,89 @@ class Header extends Component {
                     </div>
                   </div>
                 </li>
-                <li className="underline"><span className="count">04. </span><a href="https://docs.getsaleor.com" target="_blank" rel="noopener">Docs</a></li>
-                <li className="underline"><span className="count">06. </span><a href="https://medium.com/saleor" target="_blank" rel="noopener">Blog</a></li>
-                <li className="github-link"><GitHubLink owner="mirumee" name="saleor" /></li>
-                <li><span className="count">07. </span><a className={this.state.mobileMenu ? null : 'contactBtn'} href="https://mirumee.typeform.com/to/Xwfril">Contact Us</a></li>
+                <li className="underline">
+                  <span className="count">04. </span>
+                  <a
+                    href="https://docs.getsaleor.com"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    Docs
+                  </a>
+                </li>
+                <li className="underline">
+                  <span className="count">06. </span>
+                  <a
+                    href="https://medium.com/saleor"
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    Blog
+                  </a>
+                </li>
+                <li className="github-link">
+                  <GitHubLink owner="mirumee" name="saleor" />
+                </li>
+                <li>
+                  <span className="count">07. </span>
+                  <a
+                    className={this.state.mobileMenu ? null : "contactBtn"}
+                    href="https://mirumee.typeform.com/to/Xwfril"
+                  >
+                    Contact Us
+                  </a>
+                </li>
               </ul>
               <ul className="mobile-btn">
-                <li className={this.state.mobileMenu ? 'github-link open' : 'github-link'} onClick={this.toggleMenu}><GitHubLink owner="mirumee" name="saleor" /></li>
-                <li className={this.state.mobileMenu ? 'menu-icon open' : 'menu-icon'} onClick={this.toggleMenu}>
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                  <span></span>
+                <li
+                  className={
+                    this.state.mobileMenu ? "github-link open" : "github-link"
+                  }
+                  onClick={this.toggleMenu}
+                >
+                  <GitHubLink owner="mirumee" name="saleor" />
+                </li>
+                <li
+                  className={
+                    this.state.mobileMenu ? "menu-icon open" : "menu-icon"
+                  }
+                  onClick={this.toggleMenu}
+                >
+                  <span />
+                  <span />
+                  <span />
+                  <span />
                 </li>
               </ul>
             </nav>
           </div>
         </div>
-        {this.state.visiblePrivacyPolicyBar ?
+        {this.state.visiblePrivacyPolicyBar ? (
           <div className="notification">
             <div className="content">
               <img className="cookieImg" src={cookieImg} />
-              <p className="privacyPolicyText">By accepting our usage of third-party software such as HotJar, you help us to deliver a better website experience to all our users. To see our full privacy policy, <a href="https://getsaleor.com/privacy-policy-terms-and-conditions/" target="_blank">click here.</a></p>
-              <h5 className="acceptButton" onClick={this.closePrivacyPolicyBar}>ACCEPT</h5>
-              <img className="cookieAcceptImg" src={cookieAcceptImg} onClick={this.closePrivacyPolicyBar} />
+              <p className="privacyPolicyText">
+                By accepting our usage of third-party software such as HotJar,
+                you help us to deliver a better website experience to all our
+                users. To see our full privacy policy,{" "}
+                <a
+                  href="https://getsaleor.com/privacy-policy-terms-and-conditions/"
+                  target="_blank"
+                >
+                  click here.
+                </a>
+              </p>
+              <h5 className="acceptButton" onClick={this.closePrivacyPolicyBar}>
+                ACCEPT
+              </h5>
+              <img
+                className="cookieAcceptImg"
+                src={cookieAcceptImg}
+                onClick={this.closePrivacyPolicyBar}
+              />
             </div>
-          </div> : null}
+          </div>
+        ) : null}
       </header>
     );
   }
