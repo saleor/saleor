6b3715c9aa593bab64dd03d8d57f2ce0f1283e37
Unknown <jxltom@gmail.com>
2019-03-13 09:52:10 +0800

    Update docstring to inline comments

--- saleor/graphql/account/types.py
+++ saleor/graphql/account/types.py
@@ -49,12 +49,10 @@ class Address(CountableDjangoObjectType):
             code=self.country.code, country=self.country.name)
 
     def resolve_is_default_shipping_address(self, info):
-        """
-        This field is added through annotation when using the
-        `resolve_addresses` resolver. It's invalid for
-        `resolve_default_shipping_address` and
-        `resolve_default_billing_address`
-        """
+        # This field is added through annotation when using the
+        # `resolve_addresses` resolver. It's invalid for
+        # `resolve_default_shipping_address` and
+        # `resolve_default_billing_address`
         user_default_shipping_address_pk = getattr(
             self, 'user_default_shipping_address_pk', None)
         if user_default_shipping_address_pk is None:
@@ -65,12 +63,10 @@ class Address(CountableDjangoObjectType):
         return False
 
     def resolve_is_default_billing_address(self, info):
-        """
-        This field is added through annotation when using the
-        `resolve_addresses` resolver. It's invalid for
-        `resolve_default_shipping_address` and
-        `resolve_default_billing_address`
-        """
+        # This field is added through annotation when using the
+        # `resolve_addresses` resolver. It's invalid for
+        # `resolve_default_shipping_address` and
+        # `resolve_default_billing_address`
         user_default_billing_address_pk = getattr(
             self, 'user_default_billing_address_pk', None)
         if user_default_billing_address_pk is None:
