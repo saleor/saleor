c259039c95d8fe15972aedb6adab4f1338deb48b
dominik-zeglen <flesz3@o2.pl>
2019-03-15 14:48:05 +0100

    Remove unused import

--- saleor/static/dashboard-next/customers/components/CustomerAddress/CustomerAddress.tsx
+++ saleor/static/dashboard-next/customers/components/CustomerAddress/CustomerAddress.tsx
@@ -2,12 +2,7 @@ import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import {
-  createStyles,
-  Theme,
-  withStyles,
-  WithStyles
-} from "@material-ui/core/styles";
+import { createStyles, withStyles, WithStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import * as React from "react";
 
