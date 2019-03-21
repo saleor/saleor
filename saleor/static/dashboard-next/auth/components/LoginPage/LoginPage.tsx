bc68fd8385f81b2544946523a8ed8aece131dcb1
dominik-zeglen <flesz3@o2.pl>
2019-03-21 11:35:29 +0100

    Restyle login page

--- saleor/static/dashboard-next/auth/components/LoginPage/LoginPage.tsx
+++ saleor/static/dashboard-next/auth/components/LoginPage/LoginPage.tsx
@@ -10,6 +10,7 @@ import Typography from "@material-ui/core/Typography";
 import * as React from "react";
 import SVG from "react-inlinesvg";
 
+import * as backgroundArt from "../../../../images/login-background.svg";
 import * as saleorLogo from "../../../../images/logo-document.svg";
 import { ControlledCheckbox } from "../../../components/ControlledCheckbox";
 import Form from "../../../components/Form";
@@ -44,6 +45,31 @@ const styles = (theme: Theme) =>
         marginBottom: theme.spacing.unit * 4
       }
     },
+    mainPanel: {
+      [theme.breakpoints.down("sm")]: {
+        padding: theme.spacing.unit * 2
+      },
+      background: theme.palette.background.paper,
+      display: "flex",
+      flexDirection: "column",
+      height: "100vh",
+      justifyContent: "center",
+      padding: theme.spacing.unit * 6,
+      width: "100%"
+    },
+    mainPanelContent: {
+      [theme.breakpoints.up("xs")]: {
+        width: "100%"
+      },
+      [theme.breakpoints.up("sm")]: {
+        width: 328
+      },
+      "@media (min-width: 1440px)": {
+        width: 464
+      },
+      margin: "auto",
+      width: "100%"
+    },
     panel: {
       "& span": {
         color: theme.palette.error.contrastText
@@ -54,17 +80,26 @@ const styles = (theme: Theme) =>
       padding: theme.spacing.unit * 1.5
     },
     root: {
+      [theme.breakpoints.up("lg")]: {
+        gridTemplateColumns: "376px 1fr"
+      },
+      "@media (min-width: 1440px)": {
+        gridTemplateColumns: "520px 1fr"
+      },
       display: "grid",
-      gridTemplateColumns: "424px 1fr"
+      gridTemplateColumns: "1fr",
+      overflow: "hidden"
     },
     sidebar: {
-      background: theme.palette.background.paper,
-      display: "flex",
-      flexDirection: "column",
-      height: "100vh",
-      justifyContent: "center",
-      padding: theme.spacing.unit * 6,
-      width: "100%"
+      [theme.breakpoints.up("lg")]: {
+        display: "block"
+      },
+      display: "none"
+    },
+    sidebarArt: {
+      "& svg": {
+        width: "100%"
+      }
     }
   });
 
@@ -85,65 +120,68 @@ const LoginCard = withStyles(styles, { name: "LoginCard" })(
         {({ change: handleChange, data, submit: handleSubmit }) => (
           <div className={classes.root}>
             <div className={classes.sidebar}>
-              <SVG className={classes.logo} src={saleorLogo} />
-              {error && (
-                <div className={classes.panel}>
-                  <Typography
-                    variant="caption"
-                    dangerouslySetInnerHTML={{
-                      __html: i18n.t(
-                        "Sorry, your username and/or password are incorrect. <br />Please try again."
-                      )
-                    }}
-                  />
-                </div>
-              )}
-              <TextField
-                autoFocus
-                fullWidth
-                autoComplete="username"
-                label={i18n.t("Email", { context: "form" })}
-                name="email"
-                onChange={handleChange}
-                value={data.email}
-              />
-              <FormSpacer />
-              <TextField
-                fullWidth
-                autoComplete="current-password"
-                label={i18n.t("Password")}
-                name="password"
-                onChange={handleChange}
-                type="password"
-                value={data.password}
-              />
-              <FormSpacer />
-              <div className={classes.buttonContainer}>
-                <ControlledCheckbox
-                  checked={data.rememberMe}
-                  label={i18n.t("Remember me")}
-                  name="rememberMe"
+              <SVG className={classes.sidebarArt} src={backgroundArt} />
+            </div>
+            <div className={classes.mainPanel}>
+              <div className={classes.mainPanelContent}>
+                <SVG className={classes.logo} src={saleorLogo} />
+                {error && (
+                  <div className={classes.panel}>
+                    <Typography
+                      variant="caption"
+                      dangerouslySetInnerHTML={{
+                        __html: i18n.t(
+                          "Sorry, your username and/or password are incorrect. <br />Please try again."
+                        )
+                      }}
+                    />
+                  </div>
+                )}
+                <TextField
+                  autoFocus
+                  fullWidth
+                  autoComplete="username"
+                  label={i18n.t("Email", { context: "form" })}
+                  name="email"
                   onChange={handleChange}
+                  value={data.email}
                 />
                 <FormSpacer />
-                <Button
-                  className={classes.loginButton}
-                  color="primary"
-                  disabled={disableLoginButton}
-                  variant="contained"
-                  onClick={handleSubmit}
-                  type="submit"
-                >
-                  {i18n.t("Login")}
-                </Button>
-              </div>
-              {/* <FormSpacer />
+                <TextField
+                  fullWidth
+                  autoComplete="current-password"
+                  label={i18n.t("Password")}
+                  name="password"
+                  onChange={handleChange}
+                  type="password"
+                  value={data.password}
+                />
+                <FormSpacer />
+                <div className={classes.buttonContainer}>
+                  <ControlledCheckbox
+                    checked={data.rememberMe}
+                    label={i18n.t("Remember me")}
+                    name="rememberMe"
+                    onChange={handleChange}
+                  />
+                  <FormSpacer />
+                  <Button
+                    className={classes.loginButton}
+                    color="primary"
+                    disabled={disableLoginButton}
+                    variant="contained"
+                    onClick={handleSubmit}
+                    type="submit"
+                  >
+                    {i18n.t("Login")}
+                  </Button>
+                </div>
+                {/* <FormSpacer />
                 <Typography className={classes.link}>
                   {i18n.t("Reset your password")}
                 </Typography> */}
+              </div>
             </div>
-            {/* TODO: Add parrot */}
-            <div>placeholder</div>
           </div>
         )}
       </Form>
