0de3abf0e0bd5a0b255e948e89f4f861b51dd83d
dominik-zeglen <flesz3@o2.pl>
2019-03-22 15:47:23 +0100

    Remove unused import

--- saleor/static/dashboard-next/components/Timeline/Timeline.tsx
+++ saleor/static/dashboard-next/components/Timeline/Timeline.tsx
@@ -12,7 +12,6 @@ import TextField from "@material-ui/core/TextField";
 import PersonIcon from "@material-ui/icons/Person";
 import * as React from "react";
 
-import * as avatar1 from "../../../images/avatars/avatar1.svg";
 import i18n from "../../i18n";
 
 const styles = (theme: Theme) =>
