a6c41da821e2ae8ee60cfe73f33f8fb801f01fd6
dominik-zeglen <flesz3@o2.pl>
2019-04-03 13:30:26 +0200

    Update language fixtures

--- saleor/static/dashboard-next/translations/fixtures.ts
+++ saleor/static/dashboard-next/translations/fixtures.ts
@@ -1,179 +1,25 @@
 import { ShopInfo_shop_languages } from "../components/Shop/types/ShopInfo";
+import { LanguageCodeEnum } from "../types/globalTypes";
 
 export const languages: ShopInfo_shop_languages[] = [
   {
     __typename: "LanguageDisplay",
-    code: "ar",
-    language: "arabski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "az",
-    language: "azerski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "bg",
-    language: "bułgarski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "bn",
-    language: "bengalski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "ca",
-    language: "kataloński"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "cs",
-    language: "Czeski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "da",
-    language: "duński"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "de",
+    code: LanguageCodeEnum.DE,
     language: "niemiecki"
   },
   {
     __typename: "LanguageDisplay",
-    code: "en",
+    code: LanguageCodeEnum.EN,
     language: "angielski"
   },
   {
     __typename: "LanguageDisplay",
-    code: "es",
+    code: LanguageCodeEnum.ES,
     language: "hiszpański"
   },
   {
     __typename: "LanguageDisplay",
-    code: "et",
-    language: "estoński"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "fa",
-    language: "perski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "fr",
-    language: "francuski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "hi",
-    language: "hindi"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "hu",
-    language: "węgierski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "id",
-    language: "indonezyjski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "it",
-    language: "włoski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "ja",
-    language: "japoński"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "ko",
-    language: "koreański"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "mn",
-    language: "mongolski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "nb",
-    language: "norweski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "nl",
-    language: "holenderski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "pl",
+    code: LanguageCodeEnum.PL,
     language: "polski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "pt-br",
-    language: "portugalski brazylijski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "ro",
-    language: "rumuński"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "ru",
-    language: "rosyjski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "sk",
-    language: "słowacki"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "sr",
-    language: "serbski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "sv",
-    language: "szwedzki"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "th",
-    language: "tajski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "tr",
-    language: "turecki"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "uk",
-    language: "ukraiński"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "vi",
-    language: "wietnamski"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "zh-hans",
-    language: "chiński uproszczony"
-  },
-  {
-    __typename: "LanguageDisplay",
-    code: "zh-hant",
-    language: "chiński tradycyjny"
   }
 ];
