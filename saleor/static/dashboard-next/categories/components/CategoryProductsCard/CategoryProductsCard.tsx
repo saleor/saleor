b9e01081f71304460b25559c07bd774fef45f607
dominik-zeglen <flesz3@o2.pl>
2019-04-08 17:57:30 +0200

    Add bulk product delete to category details

--- saleor/static/dashboard-next/categories/components/CategoryProductsCard/CategoryProductsCard.tsx
+++ saleor/static/dashboard-next/categories/components/CategoryProductsCard/CategoryProductsCard.tsx
@@ -1,14 +1,19 @@
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 import * as React from "react";
 
 import CardTitle from "../../../components/CardTitle";
 import ProductList from "../../../components/ProductList";
+import useBulkActions from "../../../hooks/useBulkActions";
 import i18n from "../../../i18n";
-import { PageListProps } from "../../../types";
+import { ListActionProps, PageListProps } from "../../../types";
 import { CategoryDetails_category_products_edges_node } from "../../types/CategoryDetails";
 
-interface CategoryProductsCardProps extends PageListProps {
+interface CategoryProductsCardProps
+  extends PageListProps,
+    ListActionProps<"onBulkDelete"> {
   products: CategoryDetails_category_products_edges_node[];
   categoryName: string;
 }
@@ -20,30 +25,50 @@ export const CategoryProductsCard: React.StatelessComponent<
   disabled,
   pageInfo,
   onAdd,
+  onBulkDelete,
   onNextPage,
   onPreviousPage,
   onRowClick,
   categoryName
-}) => (
-  <Card>
-    <CardTitle
-      title={i18n.t("Products in {{ categoryName }}", { categoryName })}
-      toolbar={
-        <Button color="primary" variant="text" onClick={onAdd}>
-          {i18n.t("Add product")}
-        </Button>
-      }
-    />
-    <ProductList
-      products={products}
-      disabled={disabled}
-      pageInfo={pageInfo}
-      onNextPage={onNextPage}
-      onPreviousPage={onPreviousPage}
-      onRowClick={onRowClick}
-    />
-  </Card>
-);
+}) => {
+  const { isMember: isChecked, listElements, toggle } = useBulkActions(
+    products
+  );
+
+  return (
+    <Card>
+      <CardTitle
+        title={i18n.t("Products in {{ categoryName }}", { categoryName })}
+        toolbar={
+          <Button color="primary" variant="text" onClick={onAdd}>
+            {i18n.t("Add product")}
+          </Button>
+        }
+      />
+      <ProductList
+        products={products}
+        disabled={disabled}
+        pageInfo={pageInfo}
+        onNextPage={onNextPage}
+        onPreviousPage={onPreviousPage}
+        onRowClick={onRowClick}
+        selected={listElements.length}
+        isChecked={isChecked}
+        toggle={toggle}
+        toolbar={
+          <>
+            <IconButton
+              color="primary"
+              onClick={() => onBulkDelete(listElements)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </>
+        }
+      />
+    </Card>
+  );
+};
 
 CategoryProductsCard.displayName = "CategoryProductsCard";
 export default CategoryProductsCard;
--- saleor/static/dashboard-next/products/components/ProductListCard/ProductListCard.tsx
+++ saleor/static/dashboard-next/products/components/ProductListCard/ProductListCard.tsx
@@ -3,7 +3,6 @@ import Card from "@material-ui/core/Card";
 import IconButton from "@material-ui/core/IconButton";
 import AddIcon from "@material-ui/icons/Add";
 import DeleteIcon from "@material-ui/icons/Delete";
-import * as CRC from "crc-32";
 import * as React from "react";
 
 import { CategoryDetails_category_products_edges_node } from "../../../categories/types/CategoryDetails";
@@ -12,9 +11,7 @@ import PageHeader from "../../../components/PageHeader";
 import ProductList from "../../../components/ProductList";
 import { Filter } from "../../../components/TableFilter";
 import useBulkActions from "../../../hooks/useBulkActions";
-import useListSelector from "../../../hooks/useListSelector";
 import i18n from "../../../i18n";
-import { maybe } from "../../../misc";
 import { ListActionProps, PageListProps } from "../../../types";
 import ProductListFilter, { ProductListFilterTabs } from "../ProductListFilter";
 
