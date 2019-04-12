bd40a5c220d95ca1c06157b68dc6607697756971
dominik-zeglen <flesz3@o2.pl>
2019-04-12 18:39:46 +0200

    Add sale catalogue unassigning actions

--- saleor/static/dashboard-next/discounts/components/DiscountProducts/DiscountProducts.tsx
+++ saleor/static/dashboard-next/discounts/components/DiscountProducts/DiscountProducts.tsx
@@ -1,5 +1,6 @@
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
+import Checkbox from "@material-ui/core/Checkbox";
 import IconButton from "@material-ui/core/IconButton";
 import {
   createStyles,
@@ -11,7 +12,6 @@ import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableFooter from "@material-ui/core/TableFooter";
-import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import DeleteIcon from "@material-ui/icons/Delete";
 import * as React from "react";
@@ -20,14 +20,15 @@ import CardTitle from "../../../components/CardTitle";
 import Skeleton from "../../../components/Skeleton";
 import StatusLabel from "../../../components/StatusLabel";
 import TableCellAvatar from "../../../components/TableCellAvatar";
+import TableHead from "../../../components/TableHead";
 import TablePagination from "../../../components/TablePagination";
 import i18n from "../../../i18n";
 import { maybe, renderCollection } from "../../../misc";
-import { ListProps } from "../../../types";
+import { ListActions, ListProps } from "../../../types";
 import { SaleDetails_sale } from "../../types/SaleDetails";
 import { VoucherDetails_voucher } from "../../types/VoucherDetails";
 
-export interface SaleProductsProps extends ListProps {
+export interface SaleProductsProps extends ListProps, ListActions {
   discount: SaleDetails_sale | VoucherDetails_voucher;
   onProductAssign: () => void;
   onProductUnassign: (id: string) => void;
@@ -63,7 +64,11 @@ const DiscountProducts = withStyles(styles, {
     onPreviousPage,
     onProductAssign,
     onProductUnassign,
-    onNextPage
+    onNextPage,
+    isChecked,
+    selected,
+    toggle,
+    toolbar
   }: SaleProductsProps & WithStyles<typeof styles>) => (
     <Card>
       <CardTitle
@@ -77,8 +82,9 @@ const DiscountProducts = withStyles(styles, {
         }
       />
       <Table>
-        <TableHead>
+        <TableHead selected={selected} toolbar={toolbar}>
           <TableRow>
+            <TableCell />
             <TableCell />
             <TableCell className={classes.wideColumn}>
               {i18n.t("Product name")}
@@ -95,7 +101,7 @@ const DiscountProducts = withStyles(styles, {
         <TableFooter>
           <TableRow>
             <TablePagination
-              colSpan={5}
+              colSpan={6}
               hasNextPage={pageInfo && !disabled ? pageInfo.hasNextPage : false}
               onNextPage={onNextPage}
               hasPreviousPage={
@@ -108,57 +114,72 @@ const DiscountProducts = withStyles(styles, {
         <TableBody>
           {renderCollection(
             maybe(() => sale.products.edges.map(edge => edge.node)),
-            product => (
-              <TableRow
-                hover={!!product}
-                key={product ? product.id : "skeleton"}
-                onClick={product && onRowClick(product.id)}
-                className={classes.tableRow}
-              >
-                <TableCellAvatar
-                  thumbnail={maybe(() => product.thumbnail.url)}
-                />
-                <TableCell>
-                  {maybe<React.ReactNode>(() => product.name, <Skeleton />)}
-                </TableCell>
-                <TableCell className={classes.textRight}>
-                  {maybe<React.ReactNode>(
-                    () => product.productType.name,
-                    <Skeleton />
-                  )}
-                </TableCell>
-                <TableCell className={classes.textRight}>
-                  {product && product.isPublished !== undefined ? (
-                    <StatusLabel
-                      label={
-                        product.isPublished
-                          ? i18n.t("Published", { context: "product status" })
-                          : i18n.t("Not published", {
-                              context: "product status"
-                            })
-                      }
-                      status={product.isPublished ? "success" : "error"}
+            product => {
+              const isSelected = product ? isChecked(product.id) : false;
+              return (
+                <TableRow
+                  hover={!!product}
+                  key={product ? product.id : "skeleton"}
+                  onClick={product && onRowClick(product.id)}
+                  className={classes.tableRow}
+                  selected={isSelected}
+                >
+                  <TableCell padding="checkbox">
+                    <Checkbox
+                      color="primary"
+                      checked={isSelected}
+                      disabled={disabled}
+                      onClick={event => {
+                        toggle(product.id);
+                        event.stopPropagation();
+                      }}
                     />
-                  ) : (
-                    <Skeleton />
-                  )}
-                </TableCell>
-                <TableCell className={classes.iconCell}>
-                  <IconButton
-                    disabled={!product || disabled}
-                    onClick={event => {
-                      event.stopPropagation();
-                      onProductUnassign(product.id);
-                    }}
-                  >
-                    <DeleteIcon color="primary" />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ),
+                  </TableCell>
+                  <TableCellAvatar
+                    thumbnail={maybe(() => product.thumbnail.url)}
+                  />
+                  <TableCell>
+                    {maybe<React.ReactNode>(() => product.name, <Skeleton />)}
+                  </TableCell>
+                  <TableCell className={classes.textRight}>
+                    {maybe<React.ReactNode>(
+                      () => product.productType.name,
+                      <Skeleton />
+                    )}
+                  </TableCell>
+                  <TableCell className={classes.textRight}>
+                    {product && product.isPublished !== undefined ? (
+                      <StatusLabel
+                        label={
+                          product.isPublished
+                            ? i18n.t("Published", { context: "product status" })
+                            : i18n.t("Not published", {
+                                context: "product status"
+                              })
+                        }
+                        status={product.isPublished ? "success" : "error"}
+                      />
+                    ) : (
+                      <Skeleton />
+                    )}
+                  </TableCell>
+                  <TableCell className={classes.iconCell}>
+                    <IconButton
+                      disabled={!product || disabled}
+                      onClick={event => {
+                        event.stopPropagation();
+                        onProductUnassign(product.id);
+                      }}
+                    >
+                      <DeleteIcon color="primary" />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              );
+            },
             () => (
               <TableRow>
-                <TableCell colSpan={5}>{i18n.t("No products found")}</TableCell>
+                <TableCell colSpan={6}>{i18n.t("No products found")}</TableCell>
               </TableRow>
             )
           )}
--- saleor/static/dashboard-next/discounts/views/SaleDetails.tsx
+++ saleor/static/dashboard-next/discounts/views/SaleDetails.tsx
@@ -1,3 +1,4 @@
+import Button from "@material-ui/core/Button";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import * as React from "react";
 
@@ -12,6 +13,7 @@ import { WindowTitle } from "../../components/WindowTitle";
 import { SearchCategoriesProvider } from "../../containers/SearchCategories";
 import { SearchCollectionsProvider } from "../../containers/SearchCollections";
 import { SearchProductsProvider } from "../../containers/SearchProducts";
+import useBulkActions from "../../hooks/useBulkActions";
 import useNavigator from "../../hooks/useNavigator";
 import useNotifier from "../../hooks/useNotifier";
 import usePaginator from "../../hooks/usePaginator";
@@ -31,6 +33,7 @@ import {
 } from "../mutations";
 import { TypedSaleDetails } from "../queries";
 import { SaleCataloguesAdd } from "../types/SaleCataloguesAdd";
+import { SaleCataloguesRemove } from "../types/SaleCataloguesRemove";
 import { SaleDelete } from "../types/SaleDelete";
 import { SaleUpdate } from "../types/SaleUpdate";
 import {
@@ -61,19 +64,16 @@ export const SaleDetails: React.StatelessComponent<SaleDetailsProps> = ({
   const paginate = usePaginator();
   const notify = useNotifier();
   const shop = useShop();
+  const { isSelected, listElements, reset, toggle } = useBulkActions();
 
   const paginationState = createPaginationState(PAGINATE_BY, params);
-  const changeTab = (tab: SaleDetailsPageTab) =>
+  const changeTab = (tab: SaleDetailsPageTab) => {
+    reset();
     navigate(
       saleUrl(id, {
         activeTab: tab
       })
     );
-
-  const handleCatalogueAdd = (data: SaleCataloguesAdd) => {
-    if (data.saleCataloguesAdd.errors.length === 0) {
-      closeModal();
-    }
   };
 
   const handleSaleDelete = (data: SaleDelete) => {
@@ -101,21 +101,36 @@ export const SaleDetails: React.StatelessComponent<SaleDetailsProps> = ({
     navigate(
       saleUrl(id, {
         ...params,
-        action: undefined
+        action: undefined,
+        ids: undefined
       }),
       true
     );
 
-  const openModal = (action: SaleUrlDialog) =>
+  const openModal = (action: SaleUrlDialog, ids?: string[]) =>
     navigate(
       saleUrl(id, {
         ...params,
-        action
+        action,
+        ids
       })
     );
 
+  const handleCatalogueAdd = (data: SaleCataloguesAdd) => {
+    if (data.saleCataloguesAdd.errors.length === 0) {
+      closeModal();
+    }
+  };
+
+  const handleCatalogueRemove = (data: SaleCataloguesRemove) => {
+    if (data.saleCataloguesRemove.errors.length === 0) {
+      closeModal();
+      reset();
+    }
+  };
+
   return (
-    <TypedSaleCataloguesRemove>
+    <TypedSaleCataloguesRemove onCompleted={handleCatalogueRemove}>
       {(saleCataloguesRemove, saleCataloguesRemoveOpts) => (
         <TypedSaleCataloguesAdd onCompleted={handleCatalogueAdd}>
           {(saleCataloguesAdd, saleCataloguesAddOpts) => (
@@ -149,11 +164,53 @@ export const SaleDetails: React.StatelessComponent<SaleDetailsProps> = ({
                                 .errors
                           )
                         );
+                        const unassignTransitionState = getMutationState(
+                          saleCataloguesRemoveOpts.called,
+                          saleCataloguesRemoveOpts.loading,
+                          maybe(
+                            () =>
+                              saleCataloguesRemoveOpts.data.saleCataloguesRemove
+                                .errors
+                          )
+                        );
                         const removeTransitionState = getMutationState(
                           saleDeleteOpts.called,
                           saleDeleteOpts.loading,
                           maybe(() => saleDeleteOpts.data.saleDelete.errors)
                         );
+
+                        const handleCategoriesUnassign = (ids: string[]) =>
+                          saleCataloguesRemove({
+                            variables: {
+                              ...paginationState,
+                              id,
+                              input: {
+                                categories: ids
+                              }
+                            }
+                          });
+
+                        const handleCollectionsUnassign = (ids: string[]) =>
+                          saleCataloguesRemove({
+                            variables: {
+                              ...paginationState,
+                              id,
+                              input: {
+                                collections: ids
+                              }
+                            }
+                          });
+
+                        const handleProductsUnassign = (ids: string[]) =>
+                          saleCataloguesRemove({
+                            variables: {
+                              ...paginationState,
+                              id,
+                              input: {
+                                products: ids
+                              }
+                            }
+                          });
 
                         const {
                           loadNextPage,
@@ -187,26 +244,10 @@ export const SaleDetails: React.StatelessComponent<SaleDetailsProps> = ({
                                 openModal("assign-collection")
                               }
                               onCollectionUnassign={collectionId =>
-                                saleCataloguesRemove({
-                                  variables: {
-                                    ...paginationState,
-                                    id,
-                                    input: {
-                                      collections: [collectionId]
-                                    }
-                                  }
-                                })
+                                handleCollectionsUnassign([collectionId])
                               }
                               onCategoryUnassign={categoryId =>
-                                saleCataloguesRemove({
-                                  variables: {
-                                    ...paginationState,
-                                    id,
-                                    input: {
-                                      categories: [categoryId]
-                                    }
-                                  }
-                                })
+                                handleCategoriesUnassign([categoryId])
                               }
                               onCollectionClick={id => () =>
                                 navigate(collectionUrl(id))}
@@ -214,15 +255,7 @@ export const SaleDetails: React.StatelessComponent<SaleDetailsProps> = ({
                                 openModal("assign-product")
                               }
                               onProductUnassign={productId =>
-                                saleCataloguesRemove({
-                                  variables: {
-                                    ...paginationState,
-                                    id,
-                                    input: {
-                                      products: [productId]
-                                    }
-                                  }
-                                })
+                                handleProductsUnassign([productId])
                               }
                               onProductClick={id => () =>
                                 navigate(productUrl(id))}
@@ -253,6 +286,42 @@ export const SaleDetails: React.StatelessComponent<SaleDetailsProps> = ({
                               }
                               onRemove={() => openModal("remove")}
                               saveButtonBarState={formTransitionState}
+                              categoryListToolbar={
+                                <Button
+                                  color="primary"
+                                  onClick={() =>
+                                    openModal("unassign-category", listElements)
+                                  }
+                                >
+                                  {i18n.t("Unassign")}
+                                </Button>
+                              }
+                              collectionListToolbar={
+                                <Button
+                                  color="primary"
+                                  onClick={() =>
+                                    openModal(
+                                      "unassign-collection",
+                                      listElements
+                                    )
+                                  }
+                                >
+                                  {i18n.t("Unassign")}
+                                </Button>
+                              }
+                              productListToolbar={
+                                <Button
+                                  color="primary"
+                                  onClick={() =>
+                                    openModal("unassign-product", listElements)
+                                  }
+                                >
+                                  {i18n.t("Unassign")}
+                                </Button>
+                              }
+                              isChecked={isSelected}
+                              selected={listElements.length}
+                              toggle={toggle}
                             />
                             <SearchProductsProvider>
                               {(searchProducts, searchProductsOpts) => (
@@ -349,6 +418,75 @@ export const SaleDetails: React.StatelessComponent<SaleDetailsProps> = ({
                                 />
                               )}
                             </SearchCollectionsProvider>
+                            <ActionDialog
+                              open={params.action === "unassign-category"}
+                              title={i18n.t("Unassign Categories From Sale")}
+                              confirmButtonState={unassignTransitionState}
+                              onClose={closeModal}
+                              onConfirm={() =>
+                                handleCategoriesUnassign(params.ids)
+                              }
+                            >
+                              <DialogContentText
+                                dangerouslySetInnerHTML={{
+                                  __html: i18n.t(
+                                    "Are you sure you want to unassign <strong>{{ saleName }}</strong> categories?",
+                                    {
+                                      saleName: maybe(
+                                        () => params.ids.length.toString(),
+                                        "..."
+                                      )
+                                    }
+                                  )
+                                }}
+                              />
+                            </ActionDialog>
+                            <ActionDialog
+                              open={params.action === "unassign-collection"}
+                              title={i18n.t("Unassign Collections From Sale")}
+                              confirmButtonState={unassignTransitionState}
+                              onClose={closeModal}
+                              onConfirm={() =>
+                                handleCollectionsUnassign(params.ids)
+                              }
+                            >
+                              <DialogContentText
+                                dangerouslySetInnerHTML={{
+                                  __html: i18n.t(
+                                    "Are you sure you want to unassign <strong>{{ saleName }}</strong> collections?",
+                                    {
+                                      saleName: maybe(
+                                        () => params.ids.length.toString(),
+                                        "..."
+                                      )
+                                    }
+                                  )
+                                }}
+                              />
+                            </ActionDialog>
+                            <ActionDialog
+                              open={params.action === "unassign-product"}
+                              title={i18n.t("Unassign Products From Sale")}
+                              confirmButtonState={unassignTransitionState}
+                              onClose={closeModal}
+                              onConfirm={() =>
+                                handleProductsUnassign(params.ids)
+                              }
+                            >
+                              <DialogContentText
+                                dangerouslySetInnerHTML={{
+                                  __html: i18n.t(
+                                    "Are you sure you want to unassign <strong>{{ saleName }}</strong> products?",
+                                    {
+                                      saleName: maybe(
+                                        () => params.ids.length.toString(),
+                                        "..."
+                                      )
+                                    }
+                                  )
+                                }}
+                              />
+                            </ActionDialog>
                             <ActionDialog
                               open={params.action === "remove"}
                               title={i18n.t("Remove Sale")}
