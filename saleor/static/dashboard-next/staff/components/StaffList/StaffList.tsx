1a4064ae0c693fb2885b21cde3033b61d29aacca
dominik-zeglen <flesz3@o2.pl>
2019-04-16 17:21:56 +0200

    Remove bulk actions from staff list

--- saleor/static/dashboard-next/staff/components/StaffList/StaffList.tsx
+++ saleor/static/dashboard-next/staff/components/StaffList/StaffList.tsx
@@ -1,5 +1,4 @@
 import Card from "@material-ui/core/Card";
-import Checkbox from "@material-ui/core/Checkbox";
 import {
   createStyles,
   Theme,
@@ -10,17 +9,17 @@ import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableFooter from "@material-ui/core/TableFooter";
+import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Typography from "@material-ui/core/Typography";
 import * as classNames from "classnames";
 import * as React from "react";
 
 import Skeleton from "../../../components/Skeleton";
-import TableHead from "../../../components/TableHead";
 import TablePagination from "../../../components/TablePagination";
 import i18n from "../../../i18n";
 import { getUserName, maybe, renderCollection } from "../../../misc";
-import { ListActions, ListProps } from "../../../types";
+import { ListProps } from "../../../types";
 import { StaffList_staffUsers_edges_node } from "../../types/StaffList";
 
 const styles = (theme: Theme) =>
@@ -50,10 +49,7 @@ const styles = (theme: Theme) =>
     }
   });
 
-interface StaffListProps
-  extends ListProps,
-    ListActions,
-    WithStyles<typeof styles> {
+interface StaffListProps extends ListProps, WithStyles<typeof styles> {
   staffMembers: StaffList_staffUsers_edges_node[];
 }
 
@@ -65,17 +61,12 @@ const StaffList = withStyles(styles, { name: "StaffList" })(
     onPreviousPage,
     onRowClick,
     pageInfo,
-    staffMembers,
-    isChecked,
-    selected,
-    toggle,
-    toolbar
+    staffMembers
   }: StaffListProps) => (
     <Card>
       <Table>
-        <TableHead selected={selected} toolbar={toolbar}>
+        <TableHead>
           <TableRow>
-            <TableCell />
             <TableCell className={classes.wideColumn}>
               {i18n.t("Name", { context: "object" })}
             </TableCell>
@@ -87,7 +78,7 @@ const StaffList = withStyles(styles, { name: "StaffList" })(
         <TableFooter>
           <TableRow>
             <TablePagination
-              colSpan={4}
+              colSpan={3}
               hasNextPage={
                 pageInfo && !disabled ? pageInfo.hasNextPage : undefined
               }
@@ -102,69 +93,49 @@ const StaffList = withStyles(styles, { name: "StaffList" })(
         <TableBody>
           {renderCollection(
             staffMembers,
-            staffMember => {
-              const isSelected = staffMember
-                ? isChecked(staffMember.id)
-                : false;
-
-              return (
-                <TableRow
-                  className={classNames({
-                    [classes.tableRow]: !!staffMember
-                  })}
-                  hover={!!staffMember}
-                  onClick={
-                    !!staffMember ? onRowClick(staffMember.id) : undefined
-                  }
-                  key={staffMember ? staffMember.id : "skeleton"}
-                  selected={isSelected}
-                >
-                  <TableCell padding="checkbox">
-                    <Checkbox
-                      color="primary"
-                      checked={isSelected}
-                      disabled={disabled}
-                      onClick={event => {
-                        toggle(staffMember.id);
-                        event.stopPropagation();
-                      }}
+            staffMember => (
+              <TableRow
+                className={classNames({
+                  [classes.tableRow]: !!staffMember
+                })}
+                hover={!!staffMember}
+                onClick={!!staffMember ? onRowClick(staffMember.id) : undefined}
+                key={staffMember ? staffMember.id : "skeleton"}
+              >
+                <TableCell>
+                  <div className={classes.avatar}>
+                    <img
+                      className={classes.avatarImage}
+                      src={maybe(() => staffMember.avatar.url)}
                     />
-                  </TableCell>
-                  <TableCell>
-                    <div className={classes.avatar}>
-                      <img
-                        className={classes.avatarImage}
-                        src={maybe(() => staffMember.avatar.url)}
-                      />
-                    </div>
-                    <Typography>
-                      {getUserName(staffMember) || <Skeleton />}
-                    </Typography>
-                    <Typography
-                      variant={"caption"}
-                      className={classes.statusText}
-                    >
-                      {maybe<React.ReactNode>(
-                        () =>
-                          staffMember.isActive
-                            ? i18n.t("Active", { context: "status" })
-                            : i18n.t("Inactive", { context: "status" }),
-                        <Skeleton />
-                      )}
-                    </Typography>
-                  </TableCell>
-                  <TableCell>
+                  </div>
+                  <Typography>
+                    {getUserName(staffMember) || <Skeleton />}
+                  </Typography>
+                  <Typography
+                    variant={"caption"}
+                    className={classes.statusText}
+                  >
                     {maybe<React.ReactNode>(
-                      () => staffMember.email,
+                      () =>
+                        staffMember.isActive
+                          ? i18n.t("Active", { context: "status" })
+                          : i18n.t("Inactive", { context: "status" }),
                       <Skeleton />
                     )}
-                  </TableCell>
-                </TableRow>
-              );
-            },
+                  </Typography>
+                </TableCell>
+                <TableCell>
+                  {maybe<React.ReactNode>(
+                    () => staffMember.email,
+                    <Skeleton />
+                  )}
+                </TableCell>
+              </TableRow>
+            ),
             () => (
               <TableRow>
-                <TableCell colSpan={4}>
+                <TableCell colSpan={3}>
                   {i18n.t("No staff members found")}
                 </TableCell>
               </TableRow>
--- saleor/static/dashboard-next/staff/views/StaffList.tsx
+++ saleor/static/dashboard-next/staff/views/StaffList.tsx
@@ -1,12 +1,7 @@
-import DialogContentText from "@material-ui/core/DialogContentText";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
 import * as React from "react";
 
-import ActionDialog from "../../components/ActionDialog";
 import { createPaginationState } from "../../components/Paginator";
 import { configurationMenuUrl } from "../../configuration";
-import useBulkActions from "../../hooks/useBulkActions";
 import useNavigator from "../../hooks/useNavigator";
 import useNotifier from "../../hooks/useNotifier";
 import usePaginator from "../../hooks/usePaginator";
@@ -16,13 +11,9 @@ import StaffAddMemberDialog, {
   FormData as AddStaffMemberForm
 } from "../components/StaffAddMemberDialog";
 import StaffListPage from "../components/StaffListPage";
-import {
-  TypedStaffMemberAddMutation,
-  TypedStaffMembersBulkDeleteMutation
-} from "../mutations";
+import { TypedStaffMemberAddMutation } from "../mutations";
 import { TypedStaffListQuery } from "../queries";
 import { StaffMemberAdd } from "../types/StaffMemberAdd";
-import { StaffMembersBulkDelete } from "../types/StaffMembersBulkDelete";
 import {
   staffListUrl,
   StaffListUrlQueryParams,
@@ -41,9 +32,6 @@ export const StaffList: React.StatelessComponent<StaffListProps> = ({
   const navigate = useNavigator();
   const notify = useNotifier();
   const paginate = usePaginator();
-  const { isSelected, listElements, reset, toggle } = useBulkActions(
-    params.ids
-  );
 
   const closeModal = () =>
     navigate(
@@ -58,7 +46,7 @@ export const StaffList: React.StatelessComponent<StaffListProps> = ({
   const paginationState = createPaginationState(PAGINATE_BY, params);
   return (
     <TypedStaffListQuery displayLoader variables={paginationState}>
-      {({ data, loading, refetch }) => {
+      {({ data, loading }) => {
         const handleStaffMemberAddSuccess = (data: StaffMemberAdd) => {
           if (data.staffCreate.errors.length === 0) {
             notify({
@@ -68,145 +56,70 @@ export const StaffList: React.StatelessComponent<StaffListProps> = ({
           }
         };
 
-        const handleStaffMembersDelete = (data: StaffMembersBulkDelete) => {
-          if (data.staffBulkDelete.errors.length === 0) {
-            notify({
-              text: i18n.t("Removed staff members")
-            });
-            closeModal();
-            reset();
-            refetch();
-          }
-        };
-
         return (
           <TypedStaffMemberAddMutation
             onCompleted={handleStaffMemberAddSuccess}
           >
-            {(addStaffMember, addStaffMemberData) => (
-              <TypedStaffMembersBulkDeleteMutation
-                onCompleted={handleStaffMembersDelete}
-              >
-                {(staffMembersBulkDelete, staffMembersBulkDeleteOpts) => {
-                  const handleStaffMemberAdd = (
-                    variables: AddStaffMemberForm
-                  ) =>
-                    addStaffMember({
-                      variables: {
-                        input: {
-                          email: variables.email,
-                          firstName: variables.firstName,
-                          lastName: variables.lastName,
-                          permissions: variables.fullAccess
-                            ? data.shop.permissions.map(perm => perm.code)
-                            : undefined,
-                          sendPasswordEmail: true
-                        }
-                      }
-                    });
-                  const addTransitionState = getMutationState(
-                    addStaffMemberData.called,
-                    addStaffMemberData.loading,
-                    maybe(() => addStaffMemberData.data.staffCreate.errors)
-                  );
+            {(addStaffMember, addStaffMemberData) => {
+              const handleStaffMemberAdd = (variables: AddStaffMemberForm) =>
+                addStaffMember({
+                  variables: {
+                    input: {
+                      email: variables.email,
+                      firstName: variables.firstName,
+                      lastName: variables.lastName,
+                      permissions: variables.fullAccess
+                        ? data.shop.permissions.map(perm => perm.code)
+                        : undefined,
+                      sendPasswordEmail: true
+                    }
+                  }
+                });
+              const addTransitionState = getMutationState(
+                addStaffMemberData.called,
+                addStaffMemberData.loading,
+                maybe(() => addStaffMemberData.data.staffCreate.errors)
+              );
 
-                  const { loadNextPage, loadPreviousPage, pageInfo } = paginate(
-                    maybe(() => data.staffUsers.pageInfo),
-                    paginationState,
-                    params
-                  );
+              const { loadNextPage, loadPreviousPage, pageInfo } = paginate(
+                maybe(() => data.staffUsers.pageInfo),
+                paginationState,
+                params
+              );
 
-                  const bulkRemoveTransitionState = getMutationState(
-                    staffMembersBulkDeleteOpts.called,
-                    staffMembersBulkDeleteOpts.loading,
-                    maybe(
-                      () =>
-                        staffMembersBulkDeleteOpts.data.staffBulkDelete.errors
-                    )
-                  );
-                  const onSaleBulkDelete = () =>
-                    staffMembersBulkDelete({
-                      variables: {
-                        ids: params.ids
-                      }
-                    });
-
-                  return (
-                    <>
-                      <StaffListPage
-                        disabled={loading || addStaffMemberData.loading}
-                        pageInfo={pageInfo}
-                        staffMembers={maybe(() =>
-                          data.staffUsers.edges.map(edge => edge.node)
-                        )}
-                        onAdd={() =>
-                          navigate(
-                            staffListUrl({
-                              action: "add"
-                            })
-                          )
-                        }
-                        onBack={() => navigate(configurationMenuUrl)}
-                        onNextPage={loadNextPage}
-                        onPreviousPage={loadPreviousPage}
-                        onRowClick={id => () =>
-                          navigate(staffMemberDetailsUrl(id))}
-                        isChecked={isSelected}
-                        selected={listElements.length}
-                        toggle={toggle}
-                        toolbar={
-                          <IconButton
-                            color="primary"
-                            onClick={() =>
-                              navigate(
-                                staffListUrl({
-                                  action: "remove",
-                                  ids: listElements
-                                })
-                              )
-                            }
-                          >
-                            <DeleteIcon />
-                          </IconButton>
-                        }
-                      />
-                      <StaffAddMemberDialog
-                        confirmButtonState={addTransitionState}
-                        errors={maybe(
-                          () => addStaffMemberData.data.staffCreate.errors,
-                          []
-                        )}
-                        open={params.action === "add"}
-                        onClose={closeModal}
-                        onConfirm={handleStaffMemberAdd}
-                      />
-                      <ActionDialog
-                        confirmButtonState={bulkRemoveTransitionState}
-                        onClose={closeModal}
-                        onConfirm={onSaleBulkDelete}
-                        open={params.action === "remove"}
-                        title={i18n.t("Remove Staff Members")}
-                        variant="delete"
-                      >
-                        <DialogContentText
-                          dangerouslySetInnerHTML={{
-                            __html: i18n.t(
-                              "Are you sure you want to remove <strong>{{ number }}</strong> staff members?",
-                              {
-                                number: maybe(
-                                  () => params.ids.length.toString(),
-                                  "..."
-                                )
-                              }
-                            )
-                          }}
-                        />
-                      </ActionDialog>
-                    </>
-                  );
-                }}
-              </TypedStaffMembersBulkDeleteMutation>
-            )}
+              return (
+                <>
+                  <StaffListPage
+                    disabled={loading || addStaffMemberData.loading}
+                    pageInfo={pageInfo}
+                    staffMembers={maybe(() =>
+                      data.staffUsers.edges.map(edge => edge.node)
+                    )}
+                    onAdd={() =>
+                      navigate(
+                        staffListUrl({
+                          action: "add"
+                        })
+                      )
+                    }
+                    onBack={() => navigate(configurationMenuUrl)}
+                    onNextPage={loadNextPage}
+                    onPreviousPage={loadPreviousPage}
+                    onRowClick={id => () => navigate(staffMemberDetailsUrl(id))}
+                  />
+                  <StaffAddMemberDialog
+                    confirmButtonState={addTransitionState}
+                    errors={maybe(
+                      () => addStaffMemberData.data.staffCreate.errors,
+                      []
+                    )}
+                    open={params.action === "add"}
+                    onClose={closeModal}
+                    onConfirm={handleStaffMemberAdd}
+                  />
+                </>
+              );
+            }}
           </TypedStaffMemberAddMutation>
         );
       }}
