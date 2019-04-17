06c6e0d1a62bbe43b55c1c3b255894a093b07190
Adam Janik <adam.janik@mirumee.com>
2019-04-17 10:57:06 +0200

    Add test for changing status of requesting user

--- tests/api/test_account.py
+++ tests/api/test_account.py
@@ -1528,6 +1528,7 @@ def test_user_avatar_update_mutation_permission(api_client):
 
     assert_no_permission(response)
 
+
 def test_user_avatar_update_mutation(
         monkeypatch, staff_api_client, media_root):
     query = USER_AVATAR_UPDATE_MUTATION
@@ -1689,3 +1690,23 @@ def test_change_active_status_for_superuser(
     assert data['errors'][0]['field'] == superuser_id
     assert data['errors'][0]['message'] == 'Cannot activate or deactivate ' \
                                            'superuser\'s account.'
+
+
+def test_change_active_status_for_himself(
+        staff_api_client, permission_manage_users):
+    users = [staff_api_client.user]
+    user_id = graphene.Node.to_global_id('User', staff_api_client.user.id)
+    active_status = False
+    variables = {
+        'ids': [
+            graphene.Node.to_global_id('User', user.id)
+            for user in users],
+        'is_active': active_status}
+    response = staff_api_client.post_graphql(
+        USER_CHANGE_ACTIVE_STATUS_MUTATION, variables,
+        permissions=[permission_manage_users])
+    content = get_graphql_content(response)
+    data = content['data']['userBulkSetActive']
+    assert data['errors'][0]['field'] == user_id
+    assert data['errors'][0]['message'] == 'Cannot activate or deactivate ' \
+                                           'your own account.'
