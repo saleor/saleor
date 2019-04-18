3528f872d51eddaac3192f0277007772f7bce7cc
dominik-zeglen <flesz3@o2.pl>
2019-04-18 18:39:43 +0200

    Drop Messages component

--- saleor/static/dashboard-next/storybook/stories/components/messages.tsx
+++ saleor/static/dashboard-next/storybook/stories/components/messages.tsx
@@ -2,36 +2,36 @@ import Button from "@material-ui/core/Button";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
-import Messages from "../../../components/messages";
+import useNotifier from "../../../hooks/useNotifier";
 import Decorator from "../../Decorator";
 
 storiesOf("Generics / Global messages", module)
   .addDecorator(Decorator)
-  .add("default", () => (
-    <Messages>
-      {pushMessage => (
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={() => pushMessage({ text: "This is message" })}
-        >
-          Push message
-        </Button>
-      )}
-    </Messages>
-  ))
-  .add("with undo action", () => (
-    <Messages>
-      {pushMessage => (
-        <Button
-          color="primary"
-          variant="contained"
-          onClick={() =>
-            pushMessage({ text: "This is message", onUndo: undefined })
-          }
-        >
-          Push message
-        </Button>
-      )}
-    </Messages>
-  ));
+  .add("default", () => {
+    const pushMessage = useNotifier();
+
+    return (
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() => pushMessage({ text: "This is message" })}
+      >
+        Push message
+      </Button>
+    );
+  })
+  .add("with undo action", () => {
+    const pushMessage = useNotifier();
+
+    return (
+      <Button
+        color="primary"
+        variant="contained"
+        onClick={() =>
+          pushMessage({ text: "This is message", onUndo: undefined })
+        }
+      >
+        Push message
+      </Button>
+    );
+  });
