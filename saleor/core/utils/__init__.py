85c285c9d6f4b2554d48724e486c03f179727379
Cezary Miącz <cezarymiacz@cezarys-mbp.lan>
2019-04-23 15:37:49 +0200

    Replace celery @shared_task with @app.task

--- saleor/core/utils/__init__.py
+++ saleor/core/utils/__init__.py
@@ -4,7 +4,6 @@ from json import JSONEncoder
 from urllib.parse import urljoin
 
 from babel.numbers import get_territory_currencies
-from celery import shared_task
 from django import forms
 from django.conf import settings
 from django.contrib.sites.models import Site
@@ -23,6 +22,7 @@ from versatileimagefield.image_warmer import VersatileImageFieldWarmer
 
 from ...account.models import User
 from ...account.utils import get_random_avatar
+from ...celeryconf import app
 from ...core.i18n import COUNTRY_CODE_CHOICES
 
 georeader = geolite2.reader()
@@ -117,7 +117,7 @@ def to_local_currency(price, currency):
     return None
 
 
-@shared_task
+@app.task
 def update_conversion_rates_from_openexchangerates():
     conversion_rates_queryset = update_conversion_rates()
     return serializers.serialize('json', conversion_rates_queryset)
