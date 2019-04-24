3a1fd88a43a6f9385abca3b1930b808dd59804eb
dominik-zeglen <flesz3@o2.pl>
2019-04-24 14:41:03 +0200

    Fix story

--- saleor/static/dashboard-next/storybook/stories/components/messages.tsx
+++ saleor/static/dashboard-next/storybook/stories/components/messages.tsx
@@ -5,33 +5,29 @@ import * as React from "react";
 import useNotifier from "../../../hooks/useNotifier";
 import Decorator from "../../Decorator";
 
+interface StoryProps {
+  undo: boolean;
+}
+const Story: React.FC<StoryProps> = ({ undo }) => {
+  const pushMessage = useNotifier();
+
+  return (
+    <Button
+      color="primary"
+      variant="contained"
+      onClick={() =>
+        pushMessage({
+          onUndo: undo ? () => undefined : undefined,
+          text: "This is message"
+        })
+      }
+    >
+      Push message
+    </Button>
+  );
+};
+
 storiesOf("Generics / Global messages", module)
   .addDecorator(Decorator)
-  .add("default", () => {
-    const pushMessage = useNotifier();
-
-    return (
-      <Button
-        color="primary"
-        variant="contained"
-        onClick={() => pushMessage({ text: "This is message" })}
-      >
-        Push message
-      </Button>
-    );
-  })
-  .add("with undo action", () => {
-    const pushMessage = useNotifier();
-
-    return (
-      <Button
-        color="primary"
-        variant="contained"
-        onClick={() =>
-          pushMessage({ text: "This is message", onUndo: undefined })
-        }
-      >
-        Push message
-      </Button>
-    );
-  });
+  .add("default", () => <Story undo={false} />)
+  .add("with undo action", () => <Story undo={true} />);
