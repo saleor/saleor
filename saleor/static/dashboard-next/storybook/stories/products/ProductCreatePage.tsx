fda898e14a4914579851c5988ee2f6ba53d1f630
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-04-25 14:49:40 +0200

    Add new product error view to storybook

--- saleor/static/dashboard-next/storybook/stories/products/ProductCreatePage.tsx
+++ saleor/static/dashboard-next/storybook/stories/products/ProductCreatePage.tsx
@@ -1,7 +1,12 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 
-import ProductCreatePage from "../../../products/components/ProductCreatePage";
+import ProductCreatePage, {
+  FormData
+} from "../../../products/components/ProductCreatePage";
+
+import { formError } from "../../misc";
+
 import { product as productFixture } from "../../../products/fixtures";
 import { productTypes } from "../../../productTypes/fixtures";
 import Decorator from "../../Decorator";
@@ -32,6 +37,25 @@ storiesOf("Views / Products / Create product", module)
       currency="USD"
       disabled={true}
       errors={[]}
+      header="Add product"
+      collections={product.collections}
+      fetchCategories={() => undefined}
+      fetchCollections={() => undefined}
+      productTypes={productTypes}
+      categories={[product.category]}
+      onAttributesEdit={undefined}
+      onBack={() => undefined}
+      onSubmit={() => undefined}
+      saveButtonBarState="default"
+    />
+  ))
+  .add("form errors", () => (
+    <ProductCreatePage
+      currency="USD"
+      disabled={false}
+      errors={(["name", "productType", "category", "sku"] as Array<
+        keyof FormData
+      >).map(field => formError(field))}
       header="Add product"
       collections={product.collections}
       fetchCategories={() => undefined}
