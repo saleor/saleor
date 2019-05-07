e398ddadb645a4803c3854c6858876f4e89ae444
Maciej Korycinski <maciej@mirumee.com>
2019-05-07 16:33:09 +0200

    Add list enum to some filters where list can be usefull

--- saleor/graphql/order/enums.py
+++ saleor/graphql/order/enums.py
@@ -7,9 +7,12 @@ OrderEventsEmailsEnum = graphene.Enum.from_enum(OrderEventsEmails)
 
 
 class OrderStatusFilter(graphene.Enum):
-    READY_TO_FULFILL = 'ready_to_fulfill'
-    READY_TO_CAPTURE = 'ready_to_capture'
     UNFULFILLED = 'unfulfilled'
     PARTIALLY_FULFILLED = 'partially fulfilled'
     FULFILLED = 'fulfilled'
     CANCELED = 'canceled'
+
+
+class CustomOrderStatusFilter(graphene.Enum):
+    READY_TO_FULFILL = 'ready_to_fulfill'
+    READY_TO_CAPTURE = 'ready_to_capture'
