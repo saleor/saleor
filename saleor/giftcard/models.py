8ddc02a4a8e326ee4c23bdeed2057d9f21ee28ae
fowczarek <filip.owczarek@defectprediction.net>
2019-05-07 14:32:43 +0200

    Add manage gift card permission

--- saleor/giftcard/models.py
+++ saleor/giftcard/models.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from django.conf import settings
 from django.db import models
+from django.utils.translation import pgettext_lazy
 from django_prices.models import MoneyField
 
 
@@ -23,3 +24,8 @@ class GiftCard(models.Model):
         currency=settings.DEFAULT_CURRENCY,
         max_digits=settings.DEFAULT_MAX_DIGITS,
         decimal_places=settings.DEFAULT_DECIMAL_PLACES)
+
+    class Meta:
+        permissions = ((
+            'manage_gift_card', pgettext_lazy(
+                'Permission description', 'Manage gift cards.')),)
