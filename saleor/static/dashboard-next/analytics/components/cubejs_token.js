3768e01e617f300368818c6001938e45e7e27dc1
sherwin wyco <sherwin.wyco20@gmail.com>
2019-05-08 04:13:25 -0400

    change link for now

--- saleor/static/dashboard-next/analytics/components/cubejs_token.js
+++ saleor/static/dashboard-next/analytics/components/cubejs_token.js
@@ -1,6 +1,5 @@
-require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const CUBEJS_API_SECRET=process.env.CUBEJS_API_SECRET;
+const CUBEJS_API_SECRET='d242612162ecbe8c2436d47d74b83ecc1640f690ef73aef1ccfd26cf6b887bc2';
 
 export function generate_token(){
   const cubejsToken = jwt.sign({}, CUBE_API_SECRET, { expiresIn: '1d' });
