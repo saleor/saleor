960ae13a6358ee2c2603a538a530d90e37c965fe
dominik-zeglen <flesz3@o2.pl>
2019-05-08 15:18:39 +0200

    Add menu item delete

--- saleor/static/dashboard-next/navigation/components/MenuDetailsPage/tree.test.ts
+++ saleor/static/dashboard-next/navigation/components/MenuDetailsPage/tree.test.ts
@@ -1,5 +1,6 @@
 import { menu } from "../../fixtures";
 import { MenuDetails_menu_items } from "../../types/MenuDetails";
+import { TreePermutation } from "../MenuItems";
 import { computeTree } from "./tree";
 
 // Readability FTW
@@ -22,55 +23,156 @@ function treeToString(tree: MenuDetails_menu_items[]): string {
 }
 
 describe("Properly computes trees", () => {
-  const testTable = [
+  const testTable: TreePermutation[][] = [
     [],
     [
-      { id: "1glasses", parentId: "0jewelry", sortOrder: 0 },
-      { id: "2accessories", parentId: "3groceries", sortOrder: 0 }
+      { id: "1glasses", parentId: "0jewelry", sortOrder: 0, operation: "move" },
+      {
+        id: "2accessories",
+        operation: "move",
+        parentId: "3groceries",
+        sortOrder: 0
+      }
     ],
     [
-      { id: "1glasses", parentId: "0jewelry", sortOrder: 0 },
-      { id: "2accessories", parentId: "3groceries", sortOrder: 0 },
-      { id: "3groceries", parentId: "4apparel", sortOrder: 0 }
+      { id: "1glasses", parentId: "0jewelry", sortOrder: 0, operation: "move" },
+      {
+        id: "2accessories",
+        operation: "move",
+        parentId: "3groceries",
+        sortOrder: 0
+      },
+      {
+        id: "3groceries",
+        operation: "move",
+        parentId: "4apparel",
+        sortOrder: 0
+      }
     ],
     [
-      { id: "0jewelry", sortOrder: 1 },
-      { id: "1glasses", sortOrder: 1 },
-      { id: "4apparel", parentId: "3groceries", sortOrder: 0 },
-      { id: "3groceries", parentId: "0jewelry", sortOrder: 0 },
-      { id: "0jewelry", parentId: "1glasses", sortOrder: 0 },
-      { id: "1glasses", parentId: "2accessories", sortOrder: 0 },
-      { id: "1glasses", sortOrder: 1 },
-      { id: "0jewelry", sortOrder: 2 }
+      { id: "0jewelry", sortOrder: 1, operation: "move" },
+      { id: "1glasses", sortOrder: 1, operation: "move" },
+      {
+        id: "4apparel",
+        operation: "move",
+        parentId: "3groceries",
+        sortOrder: 0
+      },
+      {
+        id: "3groceries",
+        operation: "move",
+        parentId: "0jewelry",
+        sortOrder: 0
+      },
+      { id: "0jewelry", parentId: "1glasses", sortOrder: 0, operation: "move" },
+      {
+        id: "1glasses",
+        operation: "move",
+        parentId: "2accessories",
+        sortOrder: 0
+      },
+      { id: "1glasses", sortOrder: 1, operation: "move" },
+      { id: "0jewelry", sortOrder: 2, operation: "move" }
     ],
     [
-      { id: "1glasses", sortOrder: 1 },
-      { id: "1glasses", parentId: "0jewelry", sortOrder: 0 },
-      { id: "0jewelry", sortOrder: 1 },
-      { id: "0jewelry", parentId: "2accessories", sortOrder: 0 },
-      { id: "3groceries", sortOrder: 0 },
-      { id: "2accessories", parentId: "3groceries", sortOrder: 0 },
-      { id: "2accessories", sortOrder: 1 },
-      { id: "0jewelry", sortOrder: 2 },
-      { id: "1glasses", sortOrder: 3 },
-      { id: "4apparel", sortOrder: 0 },
-      { id: "1glasses", sortOrder: 1 },
-      { id: "2accessories", sortOrder: 0 },
-      { id: "4apparel", parentId: "2accessories", sortOrder: 0 },
-      { id: "3groceries", parentId: "1glasses", sortOrder: 0 },
-      { id: "0jewelry", sortOrder: 0 },
-      { id: "0jewelry", parentId: "2accessories", sortOrder: 0 },
-      { id: "4apparel", parentId: "2accessories", sortOrder: 0 },
-      { id: "0jewelry", parentId: "2accessories", sortOrder: 0 },
-      { id: "1glasses", parentId: "2accessories", sortOrder: 2 },
-      { id: "0jewelry", parentId: "2accessories", sortOrder: 2 },
-      { id: "1glasses", parentId: "2accessories", sortOrder: 2 },
-      { id: "4apparel", parentId: "2accessories", sortOrder: 2 },
-      { id: "3groceries", parentId: "0jewelry", sortOrder: 0 },
-      { id: "4apparel", parentId: "1glasses", sortOrder: 0 },
-      { id: "1glasses", sortOrder: 1 },
-      { id: "0jewelry", sortOrder: 1 },
-      { id: "2accessories", parentId: "4apparel", sortOrder: 0 }
+      { id: "1glasses", sortOrder: 1, operation: "move" },
+      { id: "1glasses", parentId: "0jewelry", sortOrder: 0, operation: "move" },
+      { id: "0jewelry", sortOrder: 1, operation: "move" },
+      {
+        id: "0jewelry",
+        operation: "move",
+        parentId: "2accessories",
+        sortOrder: 0
+      },
+      { id: "3groceries", sortOrder: 0, operation: "move" },
+      {
+        id: "2accessories",
+        operation: "move",
+        parentId: "3groceries",
+        sortOrder: 0
+      },
+      { id: "2accessories", sortOrder: 1, operation: "move" },
+      { id: "0jewelry", sortOrder: 2, operation: "move" },
+      { id: "1glasses", sortOrder: 3, operation: "move" },
+      { id: "4apparel", sortOrder: 0, operation: "move" },
+      { id: "1glasses", sortOrder: 1, operation: "move" },
+      { id: "2accessories", sortOrder: 0, operation: "move" },
+      {
+        id: "4apparel",
+        operation: "move",
+        parentId: "2accessories",
+        sortOrder: 0
+      },
+      {
+        id: "3groceries",
+        operation: "move",
+        parentId: "1glasses",
+        sortOrder: 0
+      },
+      { id: "0jewelry", sortOrder: 0, operation: "move" },
+      {
+        id: "0jewelry",
+        operation: "move",
+        parentId: "2accessories",
+        sortOrder: 0
+      },
+      {
+        id: "4apparel",
+        operation: "move",
+        parentId: "2accessories",
+        sortOrder: 0
+      },
+      {
+        id: "0jewelry",
+        operation: "move",
+        parentId: "2accessories",
+        sortOrder: 0
+      },
+      {
+        id: "1glasses",
+        operation: "move",
+        parentId: "2accessories",
+        sortOrder: 2
+      },
+      {
+        id: "0jewelry",
+        operation: "move",
+        parentId: "2accessories",
+        sortOrder: 2
+      },
+      {
+        id: "1glasses",
+        operation: "move",
+        parentId: "2accessories",
+        sortOrder: 2
+      },
+      {
+        id: "4apparel",
+        operation: "move",
+        parentId: "2accessories",
+        sortOrder: 2
+      },
+      {
+        id: "3groceries",
+        operation: "move",
+        parentId: "0jewelry",
+        sortOrder: 0
+      },
+      { id: "4apparel", parentId: "1glasses", sortOrder: 0, operation: "move" },
+      { id: "1glasses", sortOrder: 1, operation: "move" },
+      { id: "0jewelry", sortOrder: 1, operation: "move" },
+      {
+        id: "2accessories",
+        operation: "move",
+        parentId: "4apparel",
+        sortOrder: 0
+      }
+    ],
+    [{ id: "2accessories", operation: "remove" }],
+    [
+      { id: "2accessories", operation: "remove" },
+      { id: "4apparel", operation: "move", sortOrder: 0 },
+      { id: "3groceries", operation: "remove" }
     ]
   ];
 
