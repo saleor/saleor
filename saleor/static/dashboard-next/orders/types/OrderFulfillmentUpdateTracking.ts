8c12e057c953a1e4b48fcbf50772eb9230ef908f
Unknown <jxltom@gmail.com>
2019-05-09 10:32:15 +0800

    Update typescript types

--- saleor/static/dashboard-next/orders/types/OrderFulfillmentUpdateTracking.ts
+++ saleor/static/dashboard-next/orders/types/OrderFulfillmentUpdateTracking.ts
@@ -2,7 +2,7 @@
 /* eslint-disable */
 // This file was automatically generated and should not be edited.
 
-import { FulfillmentUpdateTrackingInput, OrderEventsEmails, OrderEvents, FulfillmentStatus, PaymentChargeStatusEnum, OrderStatus, OrderAction } from "./../../types/globalTypes";
+import { FulfillmentUpdateTrackingInput, OrderEventsEmailsEnum, OrderEventsEnum, FulfillmentStatus, PaymentChargeStatusEnum, OrderStatus, OrderAction } from "./../../types/globalTypes";
 
 // ====================================================
 // GraphQL mutation operation: OrderFulfillmentUpdateTracking
@@ -48,10 +48,10 @@ export interface OrderFulfillmentUpdateTracking_orderFulfillmentUpdateTracking_o
   amount: number | null;
   date: any | null;
   email: string | null;
-  emailType: OrderEventsEmails | null;
+  emailType: OrderEventsEmailsEnum | null;
   message: string | null;
   quantity: number | null;
-  type: OrderEvents | null;
+  type: OrderEventsEnum | null;
   user: OrderFulfillmentUpdateTracking_orderFulfillmentUpdateTracking_order_events_user | null;
 }
 
