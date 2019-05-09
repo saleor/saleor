b6b9856779efb0c02c001e7a0c427041d03d7064
fowczarek <filip.owczarek@defectprediction.net>
2019-05-09 11:14:13 +0200

    Add test for create mutation

--- tests/api/test_gift_card.py
+++ tests/api/test_gift_card.py
@@ -3,6 +3,8 @@ from datetime import date
 import graphene
 from tests.api.utils import get_graphql_content
 
+from .utils import assert_no_permission
+
 
 def test_query_own_gift_card(user_api_client, gift_card):
     query = """
@@ -124,3 +126,97 @@ def test_query_own_gift_cards(
     data = content['data']['me']['giftCards']
     assert data['edges'][0]['node']['code'] == gift_card.code
     assert data['totalCount'] == 1
+
+
+CREATE_GIFT_CARD_MUTATION = """
+mutation giftCardCreate(
+    $code: String, $startDate: Date, $expirationDate: Date,
+    $initialBalance: Decimal) {
+        giftCardCreate(input: {
+                code: $code, startDate: $startDate,
+                expirationDate: $expirationDate,
+                initialBalance: $initialBalance}) {
+            errors {
+                field
+                message
+            }
+            giftCard {
+                code
+                creator {
+                    email
+                }
+                created
+                startDate
+                expirationDate
+                lastUsedOn
+                isActive
+                initialBalance {
+                    amount
+                }
+                currentBalance {
+                    amount
+                }
+            }
+        }
+    }
+"""
+
+
+def test_create_gift_card(staff_api_client, permission_manage_gift_card):
+    code = 'mirumee'
+    start_date = date(day=1, month=1, year=2018)
+    expiration_date = date(day=1, month=1, year=2019)
+    initial_balance = 100
+    variables = {
+        'code': code,
+        'startDate': start_date.isoformat(),
+        'expirationDate': expiration_date.isoformat(),
+        'initialBalance': initial_balance}
+    response = staff_api_client.post_graphql(
+        CREATE_GIFT_CARD_MUTATION,
+        variables,
+        permissions=[permission_manage_gift_card])
+    content = get_graphql_content(response)
+    data = content['data']['giftCardCreate']['giftCard']
+    assert data['code'] == code
+    assert data['creator']['email'] == staff_api_client.user.email
+    assert data['startDate'] == start_date.isoformat()
+    assert data['expirationDate'] == expiration_date.isoformat()
+    assert data['lastUsedOn'] == date.today().isoformat()
+    assert data['isActive']
+    assert data['initialBalance']['amount'] == initial_balance
+    assert data['currentBalance']['amount'] == initial_balance
+
+
+def test_create_gift_card_with_empty_code(
+        staff_api_client, permission_manage_gift_card):
+    start_date = date(day=1, month=1, year=2018)
+    expiration_date = date(day=1, month=1, year=2019)
+    initial_balance = 123
+    variables = {
+        'code': "",
+        'startDate': start_date.isoformat(),
+        'expirationDate': expiration_date.isoformat(),
+        'initialBalance': initial_balance}
+    response = staff_api_client.post_graphql(
+        CREATE_GIFT_CARD_MUTATION,
+        variables,
+        permissions=[permission_manage_gift_card])
+    content = get_graphql_content(response)
+    data = content['data']['giftCardCreate']['giftCard']
+    assert data['code'] != ''
+
+
+def test_create_gift_card_without_premissions(staff_api_client):
+    code = 'mirumee'
+    start_date = date(day=1, month=1, year=2018)
+    expiration_date = date(day=1, month=1, year=2019)
+    initial_balance = 100
+    variables = {
+        'code': code,
+        'startDate': start_date.isoformat(),
+        'expirationDate': expiration_date.isoformat(),
+        'initialBalance': initial_balance}
+    response = staff_api_client.post_graphql(
+        CREATE_GIFT_CARD_MUTATION, variables)
+    assert_no_permission(response)
