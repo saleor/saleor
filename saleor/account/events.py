da79acd677096c4ae2c9083954ecb13bad59b2c9
NyanKiyoshi <hello@vanille.bid>
2019-05-09 14:27:17 +0200

    Implement customer note added event

--- saleor/account/events.py
+++ saleor/account/events.py
@@ -20,3 +20,12 @@ def customer_placed_order_event(
         return None
 
     return _new_customer_event(user=user, order=order, type=CustomerEvents.PLACED_ORDER)
+
+
+def customer_added_to_note_order_event(*, user: UserType, order: Order, message: str):
+    return _new_customer_event(
+        user=user,
+        order=order,
+        type=CustomerEvents.NOTE_ADDED_TO_ORDER,
+        parameters={"message": message},
+    )
