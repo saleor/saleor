99b2a40ef5a3b97f9f8564502fe55e43650dc847
dominik-zeglen <flesz3@o2.pl>
2019-05-10 16:15:12 +0200

    Change size

--- saleor/static/dashboard-next/navigation/components/MenuCreateDialog/MenuCreateDialog.tsx
+++ saleor/static/dashboard-next/navigation/components/MenuCreateDialog/MenuCreateDialog.tsx
@@ -3,7 +3,6 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import { createStyles, withStyles, WithStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import * as React from "react";
 
@@ -28,63 +27,53 @@ export interface MenuCreateDialogProps {
 const initialForm: MenuCreateDialogFormData = {
   name: ""
 };
-const styles = createStyles({
-  root: {
-    maxWidth: "100%",
-    width: 400
-  }
-});
 
-const MenuCreateDialog = withStyles(styles, {
-  name: "MenuCreateDialog"
-})(
-  ({
-    classes,
-    confirmButtonState,
-    disabled,
-    onClose,
-    onConfirm,
-    open
-  }: MenuCreateDialogProps & WithStyles<typeof styles>) => (
-    <Dialog open={open}>
-      <DialogTitle>
-        {i18n.t("Add Menu", {
-          context: "create menu modal window title"
-        })}
-      </DialogTitle>
-      <Form initial={initialForm} onSubmit={onConfirm}>
-        {({ change, data, errors: formErrors, submit }) => (
-          <>
-            <DialogContent className={classes.root}>
-              <TextField
-                disabled={disabled}
-                error={!!formErrors.name}
-                fullWidth
-                helperText={formErrors.name}
-                label={i18n.t("Menu Title")}
-                name={"name" as keyof MenuCreateDialogFormData}
-                value={data.name}
-                onChange={change}
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={onClose}>
-                {i18n.t("Cancel", { context: "button" })}
-              </Button>
-              <ConfirmButton
-                transitionState={confirmButtonState}
-                color="primary"
-                variant="contained"
-                onClick={submit}
-              >
-                {i18n.t("Create")}
-              </ConfirmButton>
-            </DialogActions>
-          </>
-        )}
-      </Form>
-    </Dialog>
-  )
+const MenuCreateDialog: React.FC<MenuCreateDialogProps> = ({
+  confirmButtonState,
+  disabled,
+  onClose,
+  onConfirm,
+  open
+}) => (
+  <Dialog maxWidth="sm" fullWidth open={open}>
+    <DialogTitle>
+      {i18n.t("Add Menu", {
+        context: "create menu modal window title"
+      })}
+    </DialogTitle>
+    <Form initial={initialForm} onSubmit={onConfirm}>
+      {({ change, data, errors: formErrors, submit }) => (
+        <>
+          <DialogContent>
+            <TextField
+              disabled={disabled}
+              error={!!formErrors.name}
+              fullWidth
+              helperText={formErrors.name}
+              label={i18n.t("Menu Title")}
+              name={"name" as keyof MenuCreateDialogFormData}
+              value={data.name}
+              onChange={change}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={onClose}>
+              {i18n.t("Cancel", { context: "button" })}
+            </Button>
+            <ConfirmButton
+              transitionState={confirmButtonState}
+              color="primary"
+              variant="contained"
+              onClick={submit}
+            >
+              {i18n.t("Create")}
+            </ConfirmButton>
+          </DialogActions>
+        </>
+      )}
+    </Form>
+  </Dialog>
 );
+
 MenuCreateDialog.displayName = "MenuCreateDialog";
 export default MenuCreateDialog;
