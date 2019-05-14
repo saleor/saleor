d7c6b19f1a3983abc1d4bc8ee05a4b142e3215c8
fowczarek <filip.owczarek@defectprediction.net>
2019-05-14 09:46:11 +0200

    Add mutation for gift card deactivation

--- saleor/graphql/giftcard/schema.py
+++ saleor/graphql/giftcard/schema.py
@@ -3,7 +3,7 @@ from graphql_jwt.decorators import permission_required
 
 from ...giftcard import models
 from ..core.fields import PrefetchingConnectionField
-from .mutations import GiftCardCreate, GiftCardUpdate
+from .mutations import GiftCardCreate, GiftCardDeactivate, GiftCardUpdate
 from .resolvers import resolve_gift_card
 from .types import GiftCard
 
@@ -27,3 +27,4 @@ class GiftCardQueries(graphene.ObjectType):
 class GiftCardMutations(graphene.ObjectType):
     gift_card_create = GiftCardCreate.Field()
     gift_card_update = GiftCardUpdate.Field()
+    gift_card_deactivate = GiftCardDeactivate.Field()
