a78a12f460d47471ad44144f3fb80bc5f3bbfa4f
dominik-zeglen <flesz3@o2.pl>
2019-05-14 15:16:34 +0200

    Add checkbox component

--- saleor/static/dashboard-next/storybook/config.js
+++ saleor/static/dashboard-next/storybook/config.js
@@ -7,6 +7,7 @@ function loadStories() {
   require("./stories/components/AddressEdit");
   require("./stories/components/AddressFormatter");
   require("./stories/components/CardMenu");
+  require("./stories/components/Checkbox");
   require("./stories/components/Date");
   require("./stories/components/DateTime");
   require("./stories/components/EditableTableCell");
