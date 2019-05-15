875599d072bbb96c366c9747d9ae2e3c3feb3226
Marcin Gębala <maarcin.gebala@gmail.com>
2019-05-15 17:43:58 +0200

    Fix references to cart in Storefront 1.0

--- saleor/static/js/components/variantPicker/VariantPicker.js
+++ saleor/static/js/components/variantPicker/VariantPicker.js
@@ -164,7 +164,7 @@ export default observer(class VariantPicker extends Component {
               className={addToCheckoutBtnClasses}
               onClick={this.handleAddToCheckout}
               disabled={disableAddToCheckout}>
-              {pgettext('Product details primary action', 'Add to checkout')}
+              {pgettext('Product details primary action', 'Add to cart')}
             </button>
           </div>
         </div>
