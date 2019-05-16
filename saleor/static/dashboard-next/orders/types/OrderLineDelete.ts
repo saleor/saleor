a7d8296ea9684bdc6bea0489a9a75a91f4057db7
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-05-16 12:28:10 +0200

    Migrate OrderLine.thumbnailUrl field

--- saleor/static/dashboard-next/orders/types/OrderLineDelete.ts
+++ saleor/static/dashboard-next/orders/types/OrderLineDelete.ts
@@ -73,6 +73,11 @@ export interface OrderLineDelete_draftOrderLineDelete_order_fulfillments_lines_o
   net: OrderLineDelete_draftOrderLineDelete_order_fulfillments_lines_orderLine_unitPrice_net;
 }
 
+export interface OrderLineDelete_draftOrderLineDelete_order_fulfillments_lines_orderLine_thumbnail {
+  __typename: "Image";
+  url: string;
+}
+
 export interface OrderLineDelete_draftOrderLineDelete_order_fulfillments_lines_orderLine {
   __typename: "OrderLine";
   id: string;
@@ -82,7 +87,7 @@ export interface OrderLineDelete_draftOrderLineDelete_order_fulfillments_lines_o
   quantity: number;
   quantityFulfilled: number;
   unitPrice: OrderLineDelete_draftOrderLineDelete_order_fulfillments_lines_orderLine_unitPrice | null;
-  thumbnailUrl: string | null;
+  thumbnail: OrderLineDelete_draftOrderLineDelete_order_fulfillments_lines_orderLine_thumbnail | null;
 }
 
 export interface OrderLineDelete_draftOrderLineDelete_order_fulfillments_lines {
@@ -119,6 +124,11 @@ export interface OrderLineDelete_draftOrderLineDelete_order_lines_unitPrice {
   net: OrderLineDelete_draftOrderLineDelete_order_lines_unitPrice_net;
 }
 
+export interface OrderLineDelete_draftOrderLineDelete_order_lines_thumbnail {
+  __typename: "Image";
+  url: string;
+}
+
 export interface OrderLineDelete_draftOrderLineDelete_order_lines {
   __typename: "OrderLine";
   id: string;
@@ -128,7 +138,7 @@ export interface OrderLineDelete_draftOrderLineDelete_order_lines {
   quantity: number;
   quantityFulfilled: number;
   unitPrice: OrderLineDelete_draftOrderLineDelete_order_lines_unitPrice | null;
-  thumbnailUrl: string | null;
+  thumbnail: OrderLineDelete_draftOrderLineDelete_order_lines_thumbnail | null;
 }
 
 export interface OrderLineDelete_draftOrderLineDelete_order_shippingAddress_country {
