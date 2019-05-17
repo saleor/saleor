5cafbb9ffe532ff3ff3655aad3b92aa55b1531c0
fowczarek <filip.owczarek@defectprediction.net>
2019-05-17 12:17:12 +0200

    Add test for gift card activate

--- tests/api/test_gift_card.py
+++ tests/api/test_gift_card.py
@@ -359,6 +359,7 @@ mutation giftCardDeactivate($id: ID!) {
         }
         giftCard {
             isActive
+            lastUsedOn
         }
     }
 }
@@ -366,10 +367,7 @@ mutation giftCardDeactivate($id: ID!) {
 
 
 def test_deactivate_gift_card(staff_api_client, gift_card, permission_manage_gift_card):
-    gift_card.last_used_on = date(day=1, month=1, year=2018)
-    gift_card.save()
     assert gift_card.is_active
-    assert gift_card.last_used_on != date.today()
     variables = {"id": graphene.Node.to_global_id("GiftCard", gift_card.id)}
     response = staff_api_client.post_graphql(
         DEACTIVATE_GIFT_CARD_MUTATION,
@@ -382,10 +380,78 @@ def test_deactivate_gift_card(staff_api_client, gift_card, permission_manage_gif
 
 
 def test_deactivate_gift_card_without_premissions(staff_api_client, gift_card):
-    assert gift_card.is_active
     variables = {"id": graphene.Node.to_global_id("GiftCard", gift_card.id)}
     response = staff_api_client.post_graphql(DEACTIVATE_GIFT_CARD_MUTATION, variables)
     assert_no_permission(response)
+
+
+def test_deactivate_gift_card_inactive_gift_card(
+    staff_api_client, gift_card, permission_manage_gift_card
+):
+    gift_card.is_active = False
+    gift_card.save(update_fields=["is_active"])
+    assert not gift_card.is_active
+    variables = {"id": graphene.Node.to_global_id("GiftCard", gift_card.id)}
+    response = staff_api_client.post_graphql(
+        DEACTIVATE_GIFT_CARD_MUTATION,
+        variables,
+        permissions=[permission_manage_gift_card],
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["giftCardDeactivate"]["giftCard"]
+    assert not data["isActive"]
+
+
+ACTIVATE_GIFT_CARD_MUTATION = """
+mutation giftCardActivate($id: ID!) {
+    giftCardActivate(id: $id) {
+        errors {
+            field
+            message
+        }
+        giftCard {
+            isActive
+            lastUsedOn
+        }
+    }
+}
+"""
+
+
+def test_activate_gift_card(staff_api_client, gift_card, permission_manage_gift_card):
+    gift_card.is_active = False
+    gift_card.save(update_fields=["is_active"])
+    assert not gift_card.is_active
+    variables = {"id": graphene.Node.to_global_id("GiftCard", gift_card.id)}
+    response = staff_api_client.post_graphql(
+        ACTIVATE_GIFT_CARD_MUTATION,
+        variables,
+        permissions=[permission_manage_gift_card],
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["giftCardActivate"]["giftCard"]
+    assert data["isActive"]
+
+
+def test_activate_gift_card_without_premissions(staff_api_client, gift_card):
+    variables = {"id": graphene.Node.to_global_id("GiftCard", gift_card.id)}
+    response = staff_api_client.post_graphql(ACTIVATE_GIFT_CARD_MUTATION, variables)
+    assert_no_permission(response)
+
+
+def test_activate_gift_card_active_gift_card(
+    staff_api_client, gift_card, permission_manage_gift_card
+):
+    assert gift_card.is_active
+    variables = {"id": graphene.Node.to_global_id("GiftCard", gift_card.id)}
+    response = staff_api_client.post_graphql(
+        ACTIVATE_GIFT_CARD_MUTATION,
+        variables,
+        permissions=[permission_manage_gift_card],
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["giftCardActivate"]["giftCard"]
+    assert data["isActive"]
 
 
 VERIFY_CODE_GIFT_CARD_MUTATION = """
