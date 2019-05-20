f32fd9ddb877e3d41ebfe21790c04ec4dddb1958
dominik-zeglen <flesz3@o2.pl>
2019-05-20 11:47:50 +0200

    Refactor tree module

--- saleor/static/dashboard-next/utils/menu/menu.test.ts
+++ saleor/static/dashboard-next/utils/menu/menu.test.ts
@@ -1,17 +1,71 @@
-import { IMenu, validateMenuOptions } from "./menu";
+import {
+  fromFlat,
+  getMenuItemByPath,
+  getMenuItemByValue,
+  IMenu,
+  toFlat,
+  validateMenuOptions,
+  walkToMenuItem
+} from "./menu";
+
+const validMenu: IMenu = [
+  {
+    children: [],
+    data: null,
+    label: "1",
+    value: "1"
+  },
+  {
+    children: [],
+    data: null,
+    label: "2",
+    value: "2"
+  },
+  {
+    children: [],
+    data: null,
+    label: "3",
+    value: "3"
+  },
+  {
+    children: [
+      {
+        children: [],
+        data: null,
+        label: "4.1",
+        value: "4.1"
+      },
+      {
+        children: [],
+        data: null,
+        label: "4.2",
+        value: "4.2"
+      }
+    ],
+    data: null,
+    label: "4",
+    value: "4"
+  }
+];
 
 describe("Validate menu data structure", () => {
   it("Properly catches same level duplicated value", () => {
-    const menu: IMenu[] = [
+    const menu: IMenu = [
       {
+        children: [],
+        data: null,
         label: "1",
         value: "1"
       },
       {
+        children: [],
+        data: null,
         label: "2",
         value: "2"
       },
       {
+        children: [],
+        data: null,
         label: "1",
         value: "1"
       }
@@ -21,31 +75,43 @@ describe("Validate menu data structure", () => {
   });
 
   it("Properly catches multi level duplicated value", () => {
-    const menu: IMenu[] = [
+    const menu: IMenu = [
       {
+        children: [],
+        data: null,
         label: "1",
         value: "1"
       },
       {
+        children: [],
+        data: null,
         label: "2",
         value: "2"
       },
       {
+        children: [],
+        data: null,
         label: "3",
         value: "3"
       },
       {
         children: [
           {
+            children: [],
+            data: null,
             label: "4.1",
             value: "4.1"
           },
           {
+            children: [],
+            data: null,
             label: "1",
             value: "1"
           }
         ],
-        label: "4"
+        data: null,
+        label: "4",
+        value: "4"
       }
     ];
 
@@ -53,34 +119,46 @@ describe("Validate menu data structure", () => {
   });
 
   it("Properly passes valid structure", () => {
-    const menu: IMenu[] = [
-      {
-        label: "1",
-        value: "1"
-      },
-      {
-        label: "2",
-        value: "2"
-      },
-      {
-        label: "3",
-        value: "3"
-      },
-      {
-        children: [
-          {
-            label: "4.1",
-            value: "4.1"
-          },
-          {
-            label: "4.2",
-            value: "4.2"
-          }
-        ],
-        label: "4"
-      }
-    ];
+    expect(validateMenuOptions(validMenu)).toBeTruthy();
+  });
+});
 
-    expect(validateMenuOptions(menu)).toBeTruthy();
+describe("Converting to different representations", () => {
+  it("Properly converts to flat structure", () => {
+    expect(toFlat(validMenu)).toMatchSnapshot();
+  });
+
+  it("Properly converts to nested structure", () => {
+    expect(fromFlat(toFlat(validMenu))).toMatchObject(validMenu);
+  });
+});
+
+describe("Getting items", () => {
+  it("Properly returns an item from root by path", () => {
+    expect(getMenuItemByPath(validMenu, [1])).toMatchObject(validMenu[1]);
+  });
+
+  it("Properly returns an nested item by path", () => {
+    expect(getMenuItemByPath(validMenu, [3, 0])).toMatchObject(
+      validMenu[3].children[0]
+    );
+  });
+
+  it("Properly returns an item from root by value", () => {
+    expect(getMenuItemByValue(validMenu, validMenu[1].value)).toMatchObject(
+      validMenu[1]
+    );
+  });
+
+  it("Properly returns an nested item by value", () => {
+    expect(
+      getMenuItemByValue(validMenu, validMenu[3].children[0].value)
+    ).toMatchObject(validMenu[3].children[0]);
+  });
+});
+
+describe("Walk through menu", () => {
+  it("Properly returns all nodes on the path", () => {
+    expect(walkToMenuItem(validMenu, [3, 0])).toMatchSnapshot();
   });
 });
