16425c98514a99994c9d1ec9d4234b3aa7be947f
fowczarek <filip.owczarek@defectprediction.net>
2019-05-28 11:28:08 +0200

    Add filters for inactive giftcards

--- saleor/giftcard/utils.py
+++ saleor/giftcard/utils.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from django.core.exceptions import ValidationError
 
 from .models import GiftCard
@@ -5,7 +7,7 @@ from .models import GiftCard
 
 def add_gift_card_code_to_checkout(checkout, promo_code):
     try:
-        gift_card = GiftCard.objects.get(code=promo_code)
+        gift_card = GiftCard.objects.active(date=date.today()).get(code=promo_code)
     except GiftCard.DoesNotExist:
         raise ValidationError({"promo_code": "Gift card with given code is invalid."})
     add_gift_card_to_checkout(checkout, gift_card)
