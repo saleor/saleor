e6dc84c2ebd6bdb933e2f428c498c4e2313116a4
MahmoudRizk <mahmoudahmedrizk@gmail.com>
2019-05-28 23:56:52 +0200

    fixing missed files black style and flake8 issue

--- saleor/dashboard/discount/forms.py
+++ saleor/dashboard/discount/forms.py
@@ -169,7 +169,7 @@ class ProductVoucherForm(CommonVoucherForm):
 
     class Meta:
         model = Voucher
-        fields = ['products', 'apply_once_per_order', 'min_amount_spent']
+        fields = ["products", "apply_once_per_order", "min_amount_spent"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -180,8 +180,8 @@ class ProductVoucherForm(CommonVoucherForm):
 class CollectionVoucherForm(CommonVoucherForm):
     class Meta:
         model = Voucher
-        fields = ['collections', 'apply_once_per_order', 'min_amount_spent']
-        labels = {'collections': pgettext_lazy('Collections', 'Collections')}
+        fields = ["collections", "apply_once_per_order", "min_amount_spent"]
+        labels = {"collections": pgettext_lazy("Collections", "Collections")}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -197,4 +197,4 @@ class CategoryVoucherForm(CommonVoucherForm):
 
     class Meta:
         model = Voucher
-        fields = ['categories', 'apply_once_per_order', 'min_amount_spent']
+        fields = ["categories", "apply_once_per_order", "min_amount_spent"]
