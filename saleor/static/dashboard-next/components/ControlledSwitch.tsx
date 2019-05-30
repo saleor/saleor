7753a9660508380e96b53aec3b9561693621f85b
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-05-30 17:10:20 +0200

    Fix visibility card on product

--- saleor/static/dashboard-next/components/ControlledSwitch.tsx
+++ saleor/static/dashboard-next/components/ControlledSwitch.tsx
@@ -15,6 +15,7 @@ interface ControlledSwitchProps extends WithStyles<typeof styles> {
   disabled?: boolean;
   label: string | React.ReactNode;
   name: string;
+  secondLabel?: string;
   uncheckedLabel?: string | React.ReactNode;
   onChange?(event: React.ChangeEvent<any>);
 }
@@ -29,6 +30,7 @@ export const ControlledSwitch = withStyles(styles, {
     onChange,
     label,
     name,
+    secondLabel,
     uncheckedLabel
   }: ControlledSwitchProps) => (
     <FormControlLabel
@@ -45,6 +47,7 @@ export const ControlledSwitch = withStyles(styles, {
       label={
         <div className={classes.label}>
           {uncheckedLabel ? (checked ? label : uncheckedLabel) : label}
+          <div>{secondLabel ? secondLabel : null}</div>
         </div>
       }
       disabled={disabled}
