3f7eb4e9ecaac02b011313c31943d1b9adda7b30
fowczarek <filip.owczarek@defectprediction.net>
2019-06-05 10:22:25 +0200

    Make gift card utils simpler

--- saleor/giftcard/utils.py
+++ saleor/giftcard/utils.py
@@ -14,11 +14,6 @@ def add_gift_card_code_to_checkout(checkout: Checkout, promo_code: str):
         gift_card = GiftCard.objects.active(date=date.today()).get(code=promo_code)
     except GiftCard.DoesNotExist:
         raise ValidationError({"promo_code": "Gift card with given code is invalid."})
-    add_gift_card_to_checkout(checkout, gift_card)
-
-
-def add_gift_card_to_checkout(checkout: Checkout, gift_card: str):
-    """Add gift card data to checkout."""
     checkout.gift_cards.add(gift_card)
 
 
