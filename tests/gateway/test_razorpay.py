3ff5b1f6c92fd05ef7e9254e5ce61e33b153cb8e
Maciej Korycinski <maciej@mirumee.com>
2019-06-17 14:22:44 +0200

    Fix tests in gateway/test_razorpay

--- tests/gateway/test_razorpay.py
+++ tests/gateway/test_razorpay.py
@@ -81,7 +81,7 @@ def test_checkout_widget_render_without_prefill(razorpay_payment, gateway_config
         **gateway_config.connection_params,
     )
     assert widget.render() == (
-        '<script data-amount="8000" data-buttontext="Pay now with Razorpay" '
+        '<script data-amount="9840" data-buttontext="Pay now with Razorpay" '
         'data-currency="INR" data-custom="123" '
         'data-description="Total payment" '
         'data-image="image.png" data-key="public" data-name="Saleor" '
@@ -95,7 +95,7 @@ def test_checkout_widget_render_with_prefill(razorpay_payment, gateway_config):
         payment_information=payment_info, **gateway_config.connection_params
     )
     assert widget.render() == (
-        '<script data-amount="8000" data-buttontext="Pay now with Razorpay" '
+        '<script data-amount="9840" data-buttontext="Pay now with Razorpay" '
         'data-currency="INR" data-description="Total payment" '
         'data-image="image.png" data-key="public" data-name="Saleor" '
         'data-prefill.email="test@example.com" '
