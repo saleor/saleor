41e92cba571c01213f98e7844c4edd0987800fbc
fowczarek <filip.owczarek@defectprediction.net>
2019-06-17 14:44:18 +0200

    Add test for new type of start and end dates in sales

--- tests/test_discounts.py
+++ tests/test_discounts.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta
+from datetime import timedelta
 
 import pytest
 from django.utils import timezone
@@ -266,30 +266,33 @@ def test_get_voucher_discount_all_products(
     assert discount == Money(expected_value, "USD")
 
 
+date_time_now = timezone.now()
+
+
 @pytest.mark.parametrize(
     "current_date, start_date, end_date, is_active",
     (
-        (date.today(), date.today(), date.today() + timedelta(days=1), True),
+        (date_time_now, date_time_now, date_time_now + timedelta(days=1), True),
         (
-            date.today() + timedelta(days=1),
-            date.today(),
-            date.today() + timedelta(days=1),
+            date_time_now + timedelta(days=1),
+            date_time_now,
+            date_time_now + timedelta(days=1),
             True,
         ),
         (
-            date.today() + timedelta(days=2),
-            date.today(),
-            date.today() + timedelta(days=1),
+            date_time_now + timedelta(days=2),
+            date_time_now,
+            date_time_now + timedelta(days=1),
             False,
         ),
         (
-            date.today() - timedelta(days=2),
-            date.today(),
-            date.today() + timedelta(days=1),
+            date_time_now - timedelta(days=2),
+            date_time_now,
+            date_time_now + timedelta(days=1),
             False,
         ),
-        (date.today(), date.today(), None, True),
-        (date.today() + timedelta(weeks=10), date.today(), None, True),
+        (date_time_now, date_time_now, None, True),
+        (date_time_now + timedelta(weeks=10), date_time_now, None, True),
     ),
 )
 def test_sale_active(current_date, start_date, end_date, is_active):
@@ -297,4 +300,7 @@ def test_sale_active(current_date, start_date, end_date, is_active):
         type=DiscountValueType.FIXED, value=5, start_date=start_date, end_date=end_date
     )
     sale_is_active = Sale.objects.active(date=current_date).exists()
+    if not is_active == sale_is_active:
+        print(current_date.isoformat())
+        print(start_date.isoformat())
     assert is_active == sale_is_active
