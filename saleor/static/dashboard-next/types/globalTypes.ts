a53a3108e278405e5fe84bf06e01f0ad8f8427f1
dominik-zeglen <flesz3@o2.pl>
2019-06-18 16:55:22 +0200

    Add attribute creation

--- saleor/static/dashboard-next/types/globalTypes.ts
+++ saleor/static/dashboard-next/types/globalTypes.ts
@@ -244,6 +244,12 @@ export interface AddressInput {
   phone?: string | null;
 }
 
+export interface AttributeCreateInput {
+  name: string;
+  slug?: string | null;
+  values?: (AttributeValueCreateInput | null)[] | null;
+}
+
 export interface AttributeInput {
   slug: string;
   value: string;
