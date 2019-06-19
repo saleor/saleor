2286dc36e62406ddcab0184d7f27033dec96f065
dominik-zeglen <flesz3@o2.pl>
2019-06-19 11:35:39 +0200

    Add form errors

--- saleor/static/dashboard-next/attributes/components/AttributeDetails/AttributeDetails.tsx
+++ saleor/static/dashboard-next/attributes/components/AttributeDetails/AttributeDetails.tsx
@@ -3,20 +3,23 @@ import CardContent from "@material-ui/core/CardContent";
 import TextField from "@material-ui/core/TextField";
 import * as React from "react";
 
-import CardTitle from "../../../components/CardTitle";
-import FormSpacer from "../../../components/FormSpacer";
-import i18n from "../../../i18n";
+import CardTitle from "@saleor/components/CardTitle";
+import FormSpacer from "@saleor/components/FormSpacer";
+import i18n from "@saleor/i18n";
+import { FormErrors } from "@saleor/types";
 import { AttributePageFormData } from "../AttributePage";
 
 export interface AttributeDetailsProps {
   data: AttributePageFormData;
   disabled: boolean;
+  errors: FormErrors<"name" | "slug">;
   onChange: (event: React.ChangeEvent<any>) => void;
 }
 
 const AttributeDetails: React.FC<AttributeDetailsProps> = ({
   data,
   disabled,
+  errors,
   onChange
 }) => (
   <Card>
@@ -24,18 +27,22 @@ const AttributeDetails: React.FC<AttributeDetailsProps> = ({
     <CardContent>
       <TextField
         disabled={disabled}
+        error={!!errors.name}
         label={i18n.t("Default Label")}
         name={"name" as keyof AttributePageFormData}
         fullWidth
+        helperText={errors.name}
         value={data.name}
         onChange={onChange}
       />
       <FormSpacer />
       <TextField
         disabled={disabled}
+        error={!!errors.slug}
         label={i18n.t("Attribute Code")}
         name={"slug" as keyof AttributePageFormData}
         fullWidth
+        helperText={errors.slug}
         value={data.slug}
         onChange={onChange}
       />
