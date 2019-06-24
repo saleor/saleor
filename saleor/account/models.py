dafe710d78c9696d5eed8afc8101916c90512016
Jakub Draganek <jakub.draganek@gmail.com>
2019-06-24 10:06:09 +0200

    Store and retrieve private meta under label

--- saleor/account/models.py
+++ saleor/account/models.py
@@ -189,6 +189,16 @@ class User(PermissionsMixin, AbstractBaseUser):
             return "%s %s (%s)" % (address.first_name, address.last_name, self.email)
         return self.email
 
+    def get_private_meta(self, label):
+        if not label in self.private_meta:
+            return None
+        return self.private_meta[label]
+
+    def store_private_meta(self, label, key, value):
+        if not label in self.private_meta:
+            self.private_meta[label] = {}
+        self.private_meta[label][str(key)] = str(value)
+
 
 class CustomerNote(models.Model):
     user = models.ForeignKey(
