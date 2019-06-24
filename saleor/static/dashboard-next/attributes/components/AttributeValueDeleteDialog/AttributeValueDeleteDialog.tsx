64866ba857787b3d61270f8861646e04d1722619
dominik-zeglen <flesz3@o2.pl>
2019-06-24 13:10:43 +0200

    Fix copy

--- saleor/static/dashboard-next/attributes/components/AttributeValueDeleteDialog/AttributeValueDeleteDialog.tsx
+++ saleor/static/dashboard-next/attributes/components/AttributeValueDeleteDialog/AttributeValueDeleteDialog.tsx
@@ -6,16 +6,20 @@ import { ConfirmButtonTransitionState } from "@saleor/components/ConfirmButton";
 import i18n from "@saleor/i18n";
 
 export interface AttributeValueDeleteDialogProps {
+  attributeName: string;
   confirmButtonState: ConfirmButtonTransitionState;
   open: boolean;
+  name: string;
+  useName?: boolean;
   onConfirm: () => void;
   onClose: () => void;
-  name: string;
 }
 
 const AttributeValueDeleteDialog: React.FC<AttributeValueDeleteDialogProps> = ({
+  attributeName,
   name,
   confirmButtonState,
+  useName,
   onClose,
   onConfirm,
   open
@@ -28,16 +32,19 @@ const AttributeValueDeleteDialog: React.FC<AttributeValueDeleteDialogProps> = ({
     variant="delete"
     title={i18n.t("Remove attribute value")}
   >
-    <DialogContentText
-      dangerouslySetInnerHTML={{
-        __html: i18n.t(
-          "Are you sure you want to remove <strong>{{ name }}</strong>?",
-          {
+    <DialogContentText>
+      {useName
+        ? i18n.t(
+            'Are you sure you want to remove "{{ name }}" value? If you remove it you won’t be able to assign it to any of the products with "{{ attributeName }}" attribute.',
+            {
+              attributeName,
+              name
+            }
+          )
+        : i18n.t('Are you sure you want to remove "{{ name }}" value?', {
             name
-          }
-        )
-      }}
-    />
+          })}
+    </DialogContentText>
   </ActionDialog>
 );
 
