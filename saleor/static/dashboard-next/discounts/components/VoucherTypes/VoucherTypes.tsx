d929a7a3dc925dce6434afd44dd01cab530a53b4
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-06-26 13:01:36 +0200

    Add VoucherType component

---
+++ saleor/static/dashboard-next/discounts/components/VoucherTypes/VoucherTypes.tsx
@@ -0,0 +1,73 @@
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import {
+  createStyles,
+  Theme,
+  withStyles,
+  WithStyles
+} from "@material-ui/core/styles";
+import * as React from "react";
+
+import CardTitle from "@saleor/components/CardTitle";
+import RadioGroupField from "@saleor/components/RadioGroupField";
+import i18n from "../../../i18n";
+import { FormErrors } from "../../../types";
+import { VoucherType } from "../../../types/globalTypes";
+import { translateVoucherTypes } from "../../translations";
+import { FormData } from "../VoucherDetailsPage";
+
+interface VoucherTypesProps {
+  data: FormData;
+  errors: FormErrors<"name" | "code" | "type">;
+  disabled: boolean;
+  variant: "create" | "update";
+  onChange: (event: React.ChangeEvent<any>) => void;
+}
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      display: "grid",
+      gridColumnGap: theme.spacing.unit * 2 + "px",
+      gridTemplateColumns: "1fr"
+    }
+  });
+
+const VoucherTypes = withStyles(styles, {
+  name: "VoucherTypes"
+})(
+  ({
+    classes,
+    data,
+    disabled,
+    errors,
+    variant,
+    onChange
+  }: VoucherTypesProps & WithStyles<typeof styles>) => {
+    const translatedVoucherTypes = translateVoucherTypes();
+    const voucherTypeChoices = Object.values(VoucherType).map(type => ({
+      label: translatedVoucherTypes[type],
+      value: type
+    }));
+
+    return (
+      <Card>
+        <CardTitle title={i18n.t("Discount Type")} />
+        <CardContent>
+          <div className={classes.root}>
+            <RadioGroupField
+              choices={voucherTypeChoices}
+              disabled={disabled}
+              error={!!errors.type}
+              hint={errors.type}
+              name={"type" as keyof FormData}
+              value={data.type}
+              onChange={onChange}
+            />
+          </div>
+        </CardContent>
+      </Card>
+    );
+  }
+);
+export default VoucherTypes;
