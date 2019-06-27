35e24fdce9a059da0b17ea33317c869f5416873d
dominik-zeglen <flesz3@o2.pl>
2019-06-27 17:59:22 +0200

    Sort imports

--- saleor/static/dashboard-next/components/SingleAutocompleteSelectField/SingleAutocompleteSelectField.tsx
+++ saleor/static/dashboard-next/components/SingleAutocompleteSelectField/SingleAutocompleteSelectField.tsx
@@ -1,5 +1,5 @@
-import CircularProgress from "@material-ui/core/CircularProgress";
 import { Omit } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { InputProps } from "@material-ui/core/Input";
 import MenuItem from "@material-ui/core/MenuItem";
 import Paper from "@material-ui/core/Paper";
