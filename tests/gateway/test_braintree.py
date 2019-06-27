503e380bc424626d8470cf3c912961e715fd16f4
Jakub Draganek <jakub.draganek@gmail.com>
2019-06-27 14:04:14 +0200

    Test transaction void

--- tests/gateway/test_braintree.py
+++ tests/gateway/test_braintree.py
@@ -448,24 +448,15 @@ def test_capture_error_response(
 
 
 @pytest.mark.integration
-@patch("saleor.payment.gateways.braintree.get_braintree_gateway")
-def test_void(
-    mock_gateway, payment_txn_preauth, braintree_success_response, gateway_config
-):
+@pytest.mark.vcr(filter_headers=["authorization"])
+def test_void(payment_txn_preauth, sandbox_braintree_gateway_config):
     payment = payment_txn_preauth
-    mock_response = Mock(return_value=braintree_success_response)
-    mock_gateway.return_value = Mock(transaction=Mock(void=mock_response))
-
-    payment_info = create_payment_information(payment, "token")
-    response = void(payment_info, gateway_config)
+    payment_info = create_payment_information(payment, "narvpy2m")
+    response = void(payment_info, sandbox_braintree_gateway_config)
     assert not response.error
 
     assert response.kind == TransactionKind.VOID
-    assert response.amount == braintree_success_response.transaction.amount
-    assert response.currency == braintree_success_response.transaction.currency_iso_code
-    assert response.transaction_id == braintree_success_response.transaction.id
-    assert response.is_success == braintree_success_response.is_success
-    mock_response.assert_called_once_with(transaction_id=payment_info.token)
+    assert response.is_success is True
 
 
 @pytest.mark.integration
