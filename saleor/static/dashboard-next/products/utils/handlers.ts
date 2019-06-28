2b73daa0d6595b2a0f01111747087b661584d00a
dominik-zeglen <flesz3@o2.pl>
2019-06-28 16:57:27 +0200

    Make autocomplete multi select work

--- saleor/static/dashboard-next/products/utils/handlers.ts
+++ saleor/static/dashboard-next/products/utils/handlers.ts
@@ -1,42 +1,15 @@
 import { FormChange } from "@saleor/components/Form";
-import { SingleAutocompleteChoiceType } from "@saleor/components/SingleAutocompleteSelectField";
 import { SearchCategories_categories_edges_node } from "@saleor/containers/SearchCategories/types/SearchCategories";
 import { FormsetChange } from "@saleor/hooks/useFormset";
-import { ProductCreateData_productTypes_edges_node } from "@saleor/products/types/ProductCreateData";
-import { Collection, ProductType, ProductUpdatePageFormData } from "./data";
+import { ProductType } from "./data";
 
 export function createCollectionSelectHandler(
-  data: ProductUpdatePageFormData,
-  collections: SingleAutocompleteChoiceType[],
-  selectedCollections: Collection[],
-  setSelectedCollections: (collections: Collection[]) => void,
-  change: FormChange
+  change: FormChange,
+  triggerChange: () => void
 ): FormChange {
   return (event: React.ChangeEvent<any>) => {
-    const id = event.target.value;
-    const collectionIndex = data.collections.indexOf(id);
-    const collectionList =
-      collectionIndex === -1
-        ? [
-            ...selectedCollections,
-            {
-              id,
-              label: collections.find(collection => collection.value === id)
-                .label
-            }
-          ]
-        : [
-            ...selectedCollections.slice(0, collectionIndex),
-            ...selectedCollections.slice(collectionIndex + 1)
-          ];
-
-    setSelectedCollections(collectionList);
-    change({
-      target: {
-        name: "collections",
-        value: collectionList.map(collection => collection.id)
-      }
-    } as any);
+    change(event);
+    triggerChange();
   };
 }
 
