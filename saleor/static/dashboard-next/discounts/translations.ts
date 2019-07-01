a9d6b2744e1a6633d3dc01abc105468dc46bd1fe
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-07-01 02:56:50 +0200

    Hide category and collection in VoucherTypes

--- saleor/static/dashboard-next/discounts/translations.ts
+++ saleor/static/dashboard-next/discounts/translations.ts
@@ -2,9 +2,9 @@ import i18n from "../i18n";
 import { VoucherType } from "../types/globalTypes";
 
 export const translateVoucherTypes = () => ({
-  [VoucherType.CATEGORY]: i18n.t("Selected Categories"),
-  [VoucherType.COLLECTION]: i18n.t("Selected Collections"),
-  [VoucherType.PRODUCT]: i18n.t("Selected Products"),
+  // [VoucherType.CATEGORY]: i18n.t("Selected Categories"),
+  // [VoucherType.COLLECTION]: i18n.t("Selected Collections"),
+  [VoucherType.PRODUCT]: i18n.t("Specyfic Products"),
   [VoucherType.SHIPPING]: i18n.t("Shipment"),
   [VoucherType.ENTIRE_ORDER]: i18n.t("Entire order"),
   [VoucherType.SPECIFIC_PRODUCT]: i18n.t(
