eb652bf506c5cee028de6bc4d74b426268ba515d
dominik-zeglen <flesz3@o2.pl>
2019-07-02 15:35:49 +0200

    Drop asterisk import

--- saleor/static/dashboard-next/attributes/components/AttributeProperties/AttributeProperties.tsx
+++ saleor/static/dashboard-next/attributes/components/AttributeProperties/AttributeProperties.tsx
@@ -1,7 +1,7 @@
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import * as React from "react";
+import React from "react";
 
 import CardSpacer from "../../../components/CardSpacer";
 import CardTitle from "../../../components/CardTitle";
