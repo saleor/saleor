c0ed43359a0ffab40121f6bc4e86da91c70fde06
dominik-zeglen <flesz3@o2.pl>
2019-07-02 15:35:49 +0200

    Drop asterisk import

--- saleor/static/dashboard-next/attributes/components/AttributeListPage/AttributeListPage.tsx
+++ saleor/static/dashboard-next/attributes/components/AttributeListPage/AttributeListPage.tsx
@@ -1,7 +1,7 @@
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 import AddIcon from "@material-ui/icons/Add";
-import * as React from "react";
+import React from "react";
 
 import Container from "../../../components/Container";
 import PageHeader from "../../../components/PageHeader";
