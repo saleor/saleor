5e97f0f9cb41325208e4bc1dab9d97cae594bbf3
NyanKiyoshi <hello@vanille.bid>
2019-07-03 14:13:29 +0200

    Add sorting capabilities to attributes

--- saleor/graphql/product/enums.py
+++ saleor/graphql/product/enums.py
@@ -65,3 +65,41 @@ class ProductTypeConfigurable(graphene.Enum):
 class ProductTypeEnum(graphene.Enum):
     DIGITAL = "digital"
     SHIPPABLE = "shippable"
+
+
+class AttributeSortField(graphene.Enum):
+    NAME = "name"
+    SLUG = "slug"
+    VALUE_REQUIRED = "value_required"
+    IS_VARIANT_ONLY = "is_variant_only"
+    VISIBLE_IN_STOREFRONT = "visible_in_storefront"
+    FILTERABLE_IN_STOREFRONT = "filterable_in_storefront"
+    FILTERABLE_IN_DASHBOARD = "filterable_in_dashboard"
+
+    DASHBOARD_VARIANT_POSITION = "dashboard_variant_position"
+    DASHBOARD_PRODUCT_POSITION = "dashboard_product_position"
+    STOREFRONT_SEARCH_POSITION = "storefront_search_position"
+
+    @property
+    def description(self):
+        if self == AttributeSortField.NAME:
+            return "Sort attributes by name."
+        if self == AttributeSortField.SLUG:
+            return "Sort attributes by slug."
+        if self == AttributeSortField.VALUE_REQUIRED:
+            return "Sort attributes by the value required flag."
+        if self == AttributeSortField.IS_VARIANT_ONLY:
+            return "Sort attributes by the variant only flag."
+        if self == AttributeSortField.VISIBLE_IN_STOREFRONT:
+            return "Sort attributes by visibility in the storefront."
+        if self == AttributeSortField.FILTERABLE_IN_STOREFRONT:
+            return "Sort attributes by the filterable in storefront flag."
+        if self == AttributeSortField.FILTERABLE_IN_DASHBOARD:
+            return "Sort attributes by the filterable in dashboard flag."
+        if self == AttributeSortField.DASHBOARD_VARIANT_POSITION:
+            return "Sort variant attributes by their position in dashboard."
+        if self == AttributeSortField.DASHBOARD_PRODUCT_POSITION:
+            return "Sort product attributes by their position in dashboard."
+        if self == AttributeSortField.STOREFRONT_SEARCH_POSITION:
+            return "Sort attributes by their position in storefront."
+        raise ValueError("Unsupported enum value: %s" % self.value)
