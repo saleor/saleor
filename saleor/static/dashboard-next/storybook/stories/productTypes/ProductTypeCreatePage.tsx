ad464291fe6ef8766707cbf63eee579a94972b2b
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-07-12 12:33:11 +0200

    Add taxes to create page

--- saleor/static/dashboard-next/storybook/stories/productTypes/ProductTypeCreatePage.tsx
+++ saleor/static/dashboard-next/storybook/stories/productTypes/ProductTypeCreatePage.tsx
@@ -15,7 +15,8 @@ const props: Omit<ProductTypeCreatePageProps, "classes"> = {
   onBack: () => undefined,
   onSubmit: () => undefined,
   pageTitle: "Create product type",
-  saveButtonBarState: "default"
+  saveButtonBarState: "default",
+  taxTypes: []
 };
 
 storiesOf("Views / Product types / Create product type", module)
