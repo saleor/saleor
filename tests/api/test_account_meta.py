7d0be0040df548478d74fd5f4e4566adee56fa36
Jakub Draganek <jakub.draganek@gmail.com>
2019-07-12 15:07:55 +0200

    Resolve metadata

--- tests/api/test_account_meta.py
+++ tests/api/test_account_meta.py
@@ -1,4 +1,20 @@
+import graphene
+import pytest
 from tests.api.utils import get_graphql_content
+
+META_LABEL = "TEST_LABEL"
+META_CLIENT = "TEST_PLUGIN"
+
+KEY = "name"
+VALUE = "Bond"
+
+
+@pytest.fixture
+def customer_with_meta(customer_user):
+    customer_user.store_private_meta(META_LABEL, META_CLIENT, {KEY: VALUE})
+    customer_user.save()
+    return customer_user
+
 
 GET_PRIVATE_META_QUERY = """
     query UserMeta($id: ID!) {
@@ -19,8 +35,16 @@ GET_PRIVATE_META_QUERY = """
 """
 
 
-def test_get_private_meta(staff_api_client, permission_manage_users, customer_user):
-    variables = {"id": customer_user.id}
+def test_get_private_meta(
+    staff_api_client, permission_manage_users, customer_with_meta
+):
+    user_id = graphene.Node.to_global_id("User", customer_with_meta.id)
+    variables = {"id": user_id}
     staff_api_client.user.user_permissions.add(permission_manage_users)
     response = staff_api_client.post_graphql(GET_PRIVATE_META_QUERY, variables)
-    content = get_graphql_content(response)
+    meta = get_graphql_content(response)["data"]["user"]["privateMeta"][0]
+
+    assert meta["label"] == META_LABEL
+    assert meta["clients"] == [
+        {"metadata": [{"key": KEY, "value": VALUE}], "name": META_CLIENT}
+    ]
