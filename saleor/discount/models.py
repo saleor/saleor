dca8cca723d69d6f2209ab10e639b199bb72ca10
Mateusz Dereniowski <derenio@gmail.com>
2019-07-12 20:06:17 +0200

    Replace "datetime.date" with "timezone.now" in the Sale's code

--- saleor/discount/models.py
+++ saleor/discount/models.py
@@ -190,14 +190,14 @@ class VoucherCustomer(models.Model):
 class SaleQueryset(models.QuerySet):
     def active(self, date=None):
         if date is None:
-            date = timezone.now().date()
+            date = timezone.now()
         return self.filter(
             Q(end_date__isnull=True) | Q(end_date__gte=date), start_date__lte=date
         )
 
     def expired(self, date=None):
         if date is None:
-            date = timezone.now().date()
+            date = timezone.now()
         return self.filter(end_date__lt=date, start_date__lt=date)
 
 
