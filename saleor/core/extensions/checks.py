f687e9ed6bc8838fcd7b387212be4170b0c67e58
Maciej Korycinski <maciej@mirumee.com>
2019-07-12 09:25:13 +0200

    Add helper functions to manager checker. Add typing

--- saleor/core/extensions/checks.py
+++ saleor/core/extensions/checks.py
@@ -1,4 +1,5 @@
 import importlib
+from typing import List
 
 from django.conf import settings
 from django.core.checks import Error, register
@@ -8,6 +9,16 @@ from django.core.checks import Error, register
 def check_extensions(app_configs, **kwargs):
     """Confirm a correct import of plugins and manager."""
     errors = []
+    check_manager(errors)
+
+    plugins = settings.PLUGINS
+    if plugins:
+        check_plugins(errors, plugins)
+
+    return errors
+
+
+def check_manager(errors: List[Error]):
     if not settings.EXTENSION_MANAGER:
         return []
     manager_path = settings.EXTENSION_MANAGER
@@ -22,18 +33,16 @@ def check_extensions(app_configs, **kwargs):
             )
         )
 
-    plugins = settings.PLUGINS
 
-    if plugins:
-        for plugin_path in plugins:
-            plugin_path, _, plugin_name = plugin_path.rpartition(".")
-            plugin_module = importlib.import_module(plugin_path)
-            plugin_class = getattr(plugin_module, plugin_name, None)
-            if not plugin_class:
-                errors.append(
-                    Error(
-                        "Plugin %s doesn't exists in specific path %s"
-                        % (plugin_name, str(plugin_module))
-                    )
+def check_plugins(errors: List[Error], plugins: List[str]):
+    for plugin_path in plugins:
+        plugin_path, _, plugin_name = plugin_path.rpartition(".")
+        plugin_module = importlib.import_module(plugin_path)
+        plugin_class = getattr(plugin_module, plugin_name, None)
+        if not plugin_class:
+            errors.append(
+                Error(
+                    "Plugin %s doesn't exists in specific path %s"
+                    % (plugin_name, str(plugin_module))
                 )
-    return errors
+            )
