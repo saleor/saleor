542c130b536b0ac1a5a214e69fff3bd3fed72a02
dominik-zeglen <flesz3@o2.pl>
2019-07-15 17:21:12 +0200

    Fix form reset after modal actions

--- saleor/static/dashboard-next/hooks/useForm.ts
+++ saleor/static/dashboard-next/hooks/useForm.ts
@@ -35,13 +35,38 @@ function parseErrors(errors: UserError[]): Record<string, string> {
     : {};
 }
 
-function useForm<T extends Record<string, any | any[]>>(
+type FormData = Record<string, any | any[]>;
+
+function merge<T extends FormData>(prevData: T, prevState: T, data: T): T {
+  return Object.keys(prevState).reduce((acc, key) => {
+    if (!isEqual(data[key], prevData[key])) {
+      acc[key as keyof T] = data[key];
+    }
+
+    return acc;
+  }, prevState);
+}
+
+function handleRefresh<T extends FormData>(
+  data: T,
+  newData: T,
+  setChanged: (status: boolean) => void
+) {
+  if (isEqual(data, newData)) {
+    setChanged(false);
+  }
+}
+
+function useForm<T extends FormData>(
   initial: T,
   errors: UserError[],
   onSubmit: (data: T) => void
 ): UseFormResult<T> {
   const [hasChanged, setChanged] = useState(false);
-  const [data, setData] = useStateFromProps(initial, () => setChanged(false));
+  const [data, setData] = useStateFromProps(initial, {
+    mergeFunc: merge,
+    onRefresh: newData => handleRefresh(data, newData, setChanged)
+  });
 
   function toggleValue(event: ChangeEvent<T>) {
     const { name, value } = event.target;
--- saleor/static/dashboard-next/hooks/useStateFromProps.ts
+++ saleor/static/dashboard-next/hooks/useStateFromProps.ts
@@ -1,18 +1,27 @@
 import isEqual from "lodash-es/isEqual";
 import { Dispatch, SetStateAction, useState } from "react";
 
+export interface UseStateFromPropsOpts<T> {
+  mergeFunc?: (prevData: T, state: T, newData: T) => T;
+  onRefresh?: (data: T) => void;
+}
+
 function useStateFromProps<T>(
   data: T,
-  onRefresh?: (data: T) => void
+  opts: UseStateFromPropsOpts<T>
 ): [T, Dispatch<SetStateAction<T>>] {
   const [state, setState] = useState(data);
-  const [prevProps, setPrevProps] = useState(data);
+  const [prevData, setPrevData] = useState(data);
 
-  if (!isEqual(prevProps, data)) {
-    setState(data);
-    setPrevProps(data);
-    if (typeof data === "function") {
-      onRefresh(data);
+  const { mergeFunc, onRefresh } = opts;
+
+  if (!isEqual(prevData, data)) {
+    const newData =
+      typeof mergeFunc === "function" ? mergeFunc(prevData, state, data) : data;
+    setState(newData);
+    setPrevData(data);
+    if (typeof onRefresh === "function") {
+      onRefresh(newData);
     }
   }
 
