c119669c25cc89bfee5c025a1c63588e5acc60a7
Jakub Draganek <jakub.draganek@gmail.com>
2019-07-15 11:02:14 +0200

    Make List fields non nullable

--- saleor/graphql/account/types_meta.py
+++ saleor/graphql/account/types_meta.py
@@ -8,7 +8,9 @@ class MetaItem(graphene.ObjectType):
 
 class MetaClientStore(graphene.ObjectType):
     name = graphene.String(required=True, description="Metadata clients name.")
-    metadata = graphene.List(MetaItem, description="Metadata stored for a client.")
+    metadata = graphene.List(
+        MetaItem, required=True, description="Metadata stored for a client."
+    )
 
     @staticmethod
     def resolve_metadata(root, _info):
@@ -18,7 +20,9 @@ class MetaClientStore(graphene.ObjectType):
 class MetaStore(graphene.ObjectType):
     label = graphene.String(required=True, description="Name of metadata client group.")
     clients = graphene.List(
-        MetaClientStore, description="List of clients that stored metadata in a group."
+        MetaClientStore,
+        required=True,
+        description="List of clients that stored metadata in a group.",
     )
 
     @staticmethod
