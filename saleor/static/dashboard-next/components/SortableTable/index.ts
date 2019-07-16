5eb4ed0cf93c5cc6cf4e6dde6109622b35ece5c5
dominik-zeglen <flesz3@o2.pl>
2019-07-16 16:25:55 +0200

    Make sortables generic

---
+++ saleor/static/dashboard-next/components/SortableTable/index.ts
@@ -0,0 +1,5 @@
+export * from "./SortableTableBody";
+export { default as SortableTableBody } from "./SortableTableBody";
+
+export * from "./SortableTableRow";
+export { default as SortableTableRow } from "./SortableTableRow";
