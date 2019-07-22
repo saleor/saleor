80dcac1874d36de99890caf3b07709f64af55e39
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-07-22 18:42:17 +0200

    Update list of Lists

--- saleor/static/dashboard-next/config.ts
+++ saleor/static/dashboard-next/config.ts
@@ -20,8 +20,44 @@ export const defaultListSettings = {
     name: "COLLECTION_LIST",
     rowNumber: PAGINATE_BY
   },
+  CUSTOMER_LIST: {
+    name: "CUSTOMER_LIST",
+    rowNumber: PAGINATE_BY
+  },
+  DRAFT_LIST: {
+    name: "DRAFT_LIST",
+    rowNumber: PAGINATE_BY
+  },
+  NAVIGATION_LIST: {
+    name: "NAVIGATION_LIST",
+    rowNumber: PAGINATE_BY
+  },
+  ORDER_LIST: {
+    name: "ORDER_LIST",
+    rowNumber: PAGINATE_BY
+  },
+  PAGES_LIST: {
+    name: "PAGES_LIST",
+    rowNumber: PAGINATE_BY
+  },
   PRODUCT_LIST: {
     name: "PRODUCT_LIST",
+    rowNumber: PAGINATE_BY
+  },
+  SALES_LIST: {
+    name: "SALES_LIST",
+    rowNumber: PAGINATE_BY
+  },
+  SHIPPING_METHODS_LIST: {
+    name: "SHIPPING_METHODS_LIST",
+    rowNumber: PAGINATE_BY
+  },
+  STAFF_MEMBERS_LIST: {
+    name: "COLLECTIOSTAFF_MEMBERS_LIST",
+    rowNumber: PAGINATE_BY
+  },
+  VOUCHER_LIST: {
+    name: "VOUCHER_LIST",
     rowNumber: PAGINATE_BY
   }
 };
