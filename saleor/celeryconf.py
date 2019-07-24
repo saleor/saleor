ce24d9685a4c0f675601bf02eec04fc7529e5f42
Maciej Korycinski <maciej@mirumee.com>
2019-07-24 10:34:42 +0200

    Move funtion to discover plugins to extensions module

--- saleor/celeryconf.py
+++ saleor/celeryconf.py
@@ -1,25 +1,9 @@
-import importlib
 import os
-from typing import List
 
 from celery import Celery
 from django.conf import settings
 
-
-def discover_plugins_modules(plugins: List[str]):
-    plugins_modules = []
-    for dotted_path in plugins:
-        try:
-            module_path, class_name = dotted_path.rsplit(".", 1)
-        except ValueError as err:
-            raise ImportError(
-                "%s doesn't look like a module path" % dotted_path
-            ) from err
-
-        module = importlib.import_module(module_path)
-        plugins_modules.append(module.__package__)
-    return plugins_modules
-
+from .core.extensions import discover_plugins_modules
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saleor.settings")
 
---
+++ saleor/core/extensions/__init__.py
@@ -0,0 +1,17 @@
+import importlib
+from typing import List
+
+
+def discover_plugins_modules(plugins: List[str]):
+    plugins_modules = []
+    for dotted_path in plugins:
+        try:
+            module_path, class_name = dotted_path.rsplit(".", 1)
+        except ValueError as err:
+            raise ImportError(
+                "%s doesn't look like a module path" % dotted_path
+            ) from err
+
+        module = importlib.import_module(module_path)
+        plugins_modules.append(module.__package__)
+    return plugins_modules
