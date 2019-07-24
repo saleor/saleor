14b37b01ba097268a901a250bd6b1e41a68c7fc6
NyanKiyoshi <hello@vanille.bid>
2019-07-24 10:26:43 +0200

    Added unique check constraint in M2M models

--- saleor/product/models.py
+++ saleor/product/models.py
@@ -461,6 +461,9 @@ class AttributeProduct(SortableModel):
         ProductType, related_name="attributeproduct", on_delete=models.CASCADE
     )
 
+    class Meta:
+        unique_together = (("attribute", "product_type"),)
+
     def get_ordering_queryset(self):
         return self.product_type.attributeproduct.all()
 
@@ -472,6 +475,9 @@ class AttributeVariant(SortableModel):
     product_type = models.ForeignKey(
         ProductType, related_name="attributevariant", on_delete=models.CASCADE
     )
+
+    class Meta:
+        unique_together = (("attribute", "product_type"),)
 
     def get_ordering_queryset(self):
         return self.product_type.attributevariant.all()
@@ -665,6 +671,9 @@ class CollectionProduct(SortableModel):
     product = models.ForeignKey(
         Product, related_name="collectionproduct", on_delete=models.CASCADE
     )
+
+    class Meta:
+        unique_together = (("collection", "product"),)
 
     def get_ordering_queryset(self):
         return self.product.collectionproduct.all()
