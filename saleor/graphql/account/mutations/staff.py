65858fd2fd3dc65b7965bade76b58c5c5cc1e1eb
fowczarek <filip.owczarek@defectprediction.net>
2019-07-26 13:26:15 +0200

    Remove unnecessary exclude from account mutations

--- saleor/graphql/account/mutations/staff.py
+++ saleor/graphql/account/mutations/staff.py
@@ -268,7 +268,6 @@ class AddressUpdate(BaseAddressUpdate):
     class Meta:
         description = "Updates an address"
         model = models.Address
-        exclude = ["user_addresses"]
         permissions = ("account.manage_users",)
 
 
