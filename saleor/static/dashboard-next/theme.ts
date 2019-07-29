93cc822ccf3fbde337c0dfcf547683ea2e2522ec
Krzysztof Bialoglowicz <krzysztof@mirumee.com>
2019-07-29 12:35:42 +0200

    Fix button border radius

--- saleor/static/dashboard-next/theme.ts
+++ saleor/static/dashboard-next/theme.ts
@@ -56,7 +56,7 @@ export default (colors: IThemeColors): Theme =>
           "& svg": {
             marginLeft: 8
           },
-          borderRadius: 8
+          borderRadius: 4
         }
       },
       MuiCard: {
