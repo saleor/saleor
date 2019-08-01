87ca5c9d6e4bc7b150c529bcfefd2b2be3f31b8f
Marcin Gębala <maarcin.gebala@gmail.com>
2019-08-01 14:22:03 +0200

    Fix filters order in storefront

--- tests/test_category.py
+++ tests/test_category.py
@@ -5,10 +5,10 @@ def test_product_category_filter_filters_from_child_category(
     product_type, categories_tree
 ):
     product_filter = ProductCategoryFilter(data={}, category=categories_tree)
-    (product_attributes, variant_attributes) = product_filter._get_attributes()
+    attributes = product_filter._get_attributes()
 
-    attribut = product_type.product_attributes.get()
-    variant = product_type.variant_attributes.get()
+    product_attr = product_type.product_attributes.get()
+    variant_attr = product_type.variant_attributes.get()
 
-    assert attribut in product_attributes
-    assert variant in variant_attributes
+    assert product_attr in attributes
+    assert variant_attr in attributes
