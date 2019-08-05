c59eabcebe94c3b22fbf6ec1fce232763d88524f
dominik-zeglen <flesz3@o2.pl>
2019-08-05 15:43:54 +0200

    Fix product type select

--- saleor/static/dashboard-next/components/SingleAutocompleteSelectField/SingleAutocompleteSelectField.tsx
+++ saleor/static/dashboard-next/components/SingleAutocompleteSelectField/SingleAutocompleteSelectField.tsx
@@ -175,7 +175,7 @@ const SingleAutocompleteSelectFieldComponent = withStyles(styles, {
                             </MenuItem>
                           )}
                           {choices.map((suggestion, index) => {
-                            const choiceIndex = emptyOption ? index + 1 : 0;
+                            const choiceIndex = index + (emptyOption ? 1 : 0);
 
                             return (
                               <MenuItem
