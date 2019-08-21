c3bf79998f2c42b438e5cd3de4e2179739d57329
Mateusz Dereniowski <derenio@gmail.com>
2019-08-21 11:48:31 +0200

    Fix code climate's errors (default kwargs == [])

--- saleor/product/utils/variant_prices.py
+++ saleor/product/utils/variant_prices.py
@@ -1,3 +1,6 @@
+import operator
+from functools import reduce
+
 from django.db.models.query_utils import Q
 
 from ...discount.utils import fetch_active_discounts
@@ -41,13 +44,28 @@ def update_products_minimal_variant_prices(products, discounts=None):
 
 
 def update_products_minimal_variant_prices_of_catalogues(
-    product_ids=[], category_ids=[], collection_ids=[]
+    product_ids=None, category_ids=None, collection_ids=None
 ):
-    products = Product.objects.filter(
-        Q(pk__in=product_ids)
-        | Q(category_id__in=category_ids)
-        | Q(collectionproduct__collection_id__in=collection_ids)
-    ).distinct()
+    # Building the matching products query
+    q_list = []
+    if product_ids:
+        q_list.append(Q(pk__in=product_ids))
+    if category_ids:
+        q_list.append(Q(category_id__in=category_ids))
+    if collection_ids:
+        q_list.append(Q(collectionproduct__collection_id__in=collection_ids))
+    # Asserting that the function was called with some ids
+    if not q_list:
+        raise ValueError(
+            "Provide at least one of the ID lists:\n"
+            "\tproduct_ids,\n"
+            "\tcategory_ids,\n"
+            "\tcollection_ids."
+        )
+    # Querying the products
+    q_or = reduce(operator.or_, q_list)
+    products = Product.objects.filter(q_or).distinct()
+
     update_products_minimal_variant_prices(products)
 
 
