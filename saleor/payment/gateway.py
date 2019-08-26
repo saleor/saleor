73d0488aab0d9111853bdcd1dba9cffc5b3be49d
Jakub Draganek <jakub.draganek@gmail.com>
2019-08-26 17:20:21 +0200

    Basic flow for process payment in PaymentGateway

--- saleor/payment/gateway.py
+++ saleor/payment/gateway.py
@@ -1,7 +1,7 @@
 from ..core.payments import PaymentInterface, Gateway
 from ..extensions.manager import get_extensions_manager
 from .models import Payment, Transaction
-from .utils import create_payment_information
+from .utils import create_payment_information, create_transaction
 
 
 class PaymentGateway:
@@ -12,4 +12,10 @@ class PaymentGateway:
         self, gateway: Gateway, payment: Payment, token: str
     ) -> Transaction:
         payment_data = create_payment_information(payment=payment, payment_token=token)
-        self.plugin_manager.process_payment(gateway, payment_data, token)
+        response = self.plugin_manager.process_payment(gateway, payment_data, token)
+        return create_transaction(
+            payment,
+            kind=response.kind,
+            payment_information=payment_data,
+            gateway_response=response,
+        )
