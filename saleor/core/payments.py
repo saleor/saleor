5cbaa4a15182e4ed71f01dc6c3430198517fc8d6
Jakub Draganek <jakub.draganek@gmail.com>
2019-08-26 12:33:22 +0200

    Use gateway enum instead of str

--- saleor/core/payments.py
+++ saleor/core/payments.py
@@ -1,44 +1,57 @@
 from abc import ABC, abstractmethod
+from enum import Enum
 from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
     from saleor.payment.interface import PaymentData, GatewayResponse, CustomerSource
 
 
+class Gateway(Enum):
+    """Possible gateway values.
+
+    TODO: Create this in runtime based on available plugins
+    """
+
+    DUMMY = "dummy"
+    BRAINTREE = "braintree"
+    RAZORPAY = "razorpay"
+    STRIPE = "stripe"
+
+
 class PaymentInterface(ABC):
     @abstractmethod
     def authorize_payment(
-        self, gateway: str, payment_information: "PaymentData"
+        self, gateway: Gateway, payment_information: "PaymentData"
     ) -> "GatewayResponse":
         pass
 
     @abstractmethod
     def capture_payment(
-        self, gateway: str, payment_information: "PaymentData"
+        self, gateway: Gateway, payment_information: "PaymentData"
     ) -> "GatewayResponse":
         pass
 
     @abstractmethod
     def refund_payment(
-        self, gateway: str, payment_information: "PaymentData"
+        self, gateway: Gateway, payment_information: "PaymentData"
     ) -> "GatewayResponse":
         pass
 
     @abstractmethod
     def void_payment(
-        self, gateway: str, payment_information: "PaymentData"
+        self, gateway: Gateway, payment_information: "PaymentData"
     ) -> "GatewayResponse":
         pass
 
     @abstractmethod
     def confirm_payment(
-        self, gateway: str, payment_information: "PaymentData"
+        self, gateway: Gateway, payment_information: "PaymentData"
     ) -> "GatewayResponse":
         pass
 
     @abstractmethod
     def process_payment(
-        self, gateway: str, payment_information: "PaymentData"
+        self, gateway: Gateway, payment_information: "PaymentData"
     ) -> "GatewayResponse":
         pass
 
