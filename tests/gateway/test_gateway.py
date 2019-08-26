3eb56eb243c1f1596e296b57c6261cc29868533b
Jakub Draganek <jakub.draganek@gmail.com>
2019-08-26 17:20:21 +0200

    Basic flow for process payment in PaymentGateway

--- tests/gateway/test_gateway.py
+++ tests/gateway/test_gateway.py
@@ -1,29 +1,29 @@
 from decimal import Decimal
-from unittest.mock import ANY
 
 import pytest
-from sympy.physics.units import amount
 
 from saleor.core.payments import Gateway, PaymentInterface
+from saleor.payment import TransactionKind
 from saleor.payment.gateway import PaymentGateway
-from saleor.payment.interface import GatewayResponse, PaymentData
+from saleor.payment.interface import GatewayResponse
 from saleor.payment.utils import create_payment_information
 
+RAW_RESPONSE = {"test": "abcdefgheijklmn"}
 PROCESS_PAYMENT_RESPONSE = GatewayResponse(
     is_success=True,
     action_required=False,
-    kind="process_payment",
+    kind=TransactionKind.AUTH,
     amount=Decimal(10.0),
     currency="usd",
     transaction_id="1234",
     error=None,
+    raw_response=RAW_RESPONSE,
 )
 
 
 @pytest.fixture
 def fake_manager(mocker):
-    mgr = mocker.Mock(spec=PaymentInterface)
-    return mgr
+    return mocker.Mock(spec=PaymentInterface)
 
 
 @pytest.fixture(autouse=True, scope="function")
@@ -39,16 +39,21 @@ def mock_get_manager(mocker, fake_manager):
 
 def test_process_payment(payment_txn_preauth):
     TOKEN = "token"
+    USED_GATEWAY = Gateway.STRIPE
     gateway = PaymentGateway()
     PAYMENT_DATA = create_payment_information(
         payment=payment_txn_preauth, payment_token=TOKEN
     )
-    gateway.plugin_manager.process_payment.result_value = PROCESS_PAYMENT_RESPONSE
+    gateway.plugin_manager.process_payment.return_value = PROCESS_PAYMENT_RESPONSE
 
-    gateway.process_payment(
-        payment=payment_txn_preauth, gateway=Gateway.STRIPE, token=TOKEN
+    transaction = gateway.process_payment(
+        payment=payment_txn_preauth, gateway=USED_GATEWAY, token=TOKEN
     )
 
     gateway.plugin_manager.process_payment.assert_called_once_with(
-        Gateway.STRIPE, PAYMENT_DATA, TOKEN
+        USED_GATEWAY, PAYMENT_DATA, TOKEN
     )
+    assert transaction.amount == PROCESS_PAYMENT_RESPONSE.amount
+    assert transaction.kind == TransactionKind.AUTH
+    assert transaction.currency == "usd"
+    assert transaction.gateway_response == RAW_RESPONSE
