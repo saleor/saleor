f3d3fdee3b8c3001d3733778ee7af0d7e1af25f3
Maciej Korycinski <maciej@mirumee.com>
2019-08-28 09:29:52 +0200

    Append description to fields returned from update

--- saleor/extensions/base_plugin.py
+++ saleor/extensions/base_plugin.py
@@ -157,6 +157,9 @@ class BasePlugin:
         if "active" in cleaned_data:
             plugin_configuration.active = cleaned_data["active"]
         plugin_configuration.save()
+        if plugin_configuration.configuration:
+            # Let's add a translated descriptions and labels
+            cls._append_config_structure(plugin_configuration.configuration)
         return plugin_configuration
 
     @classmethod
@@ -166,7 +169,7 @@ class BasePlugin:
 
     @classmethod
     def _append_config_structure(cls, configuration):
-        config_structure = getattr(cls, "CONFIG_STRUCTURE", {})
+        config_structure = getattr(cls, "CONFIG_STRUCTURE") or {}
         for coniguration_field in configuration:
 
             structure_to_add = config_structure.get(coniguration_field.get("name"))
