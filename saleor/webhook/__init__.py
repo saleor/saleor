400e14fe0a5a045c0c1abe76c3ad2857af338566
Maciej Korycinski <maciej@mirumee.com>
2019-09-16 13:45:57 +0200

    Add tests for triggering webhooks logic

--- saleor/webhook/__init__.py
+++ saleor/webhook/__init__.py
@@ -1,9 +1,4 @@
-from typing import TYPE_CHECKING, List
-
 from django.utils.translation import pgettext_lazy
-
-if TYPE_CHECKING:
-    from ..account.models import ServiceAccount
 
 
 class WebhookEventType:
@@ -11,14 +6,3 @@ class WebhookEventType:
 
     CHOICES = [(ORDER_CREATED, pgettext_lazy("Order has been placed", "Order created"))]
     PERMISSIONS = {ORDER_CREATED: "order.manage_orders"}
-
-
-def service_account_has_required_permissions_for_events(
-    service_account: "ServiceAccount", events: List[str]
-):
-    """Confirm that service account has all permissions required by events."""
-    for event in events:
-        required_permission = WebhookEventType.PERMISSIONS.get(event)
-        if not service_account.has_perm(required_permission):
-            return False
-    return True
