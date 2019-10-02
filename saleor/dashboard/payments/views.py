ace53dad41f07eff7ce9de40585264ed8151e852
Mira Szewczyk <szewczyk.mira@gmail.com>
2019-10-02 13:11:32 +0200

    Add table with all payment gateways

--- saleor/dashboard/payments/views.py
+++ saleor/dashboard/payments/views.py
@@ -3,14 +3,27 @@ from typing import TYPE_CHECKING
 from django.contrib.auth.decorators import permission_required
 from django.template.response import TemplateResponse
 
+from ...payment.gateways.braintree.plugin import BraintreeGatewayPlugin
+from ...payment.gateways.dummy.plugin import DummyGatewayPlugin
+from ...payment.gateways.razorpay.plugin import RazorpayGatewayPlugin
+from ...payment.gateways.stripe.plugin import StripeGatewayPlugin
 from ..views import staff_member_required
 
 if TYPE_CHECKING:
     from django.http import HttpRequest
 
+GATEWAYS_PLUGINS = [
+    BraintreeGatewayPlugin,
+    DummyGatewayPlugin,
+    RazorpayGatewayPlugin,
+    StripeGatewayPlugin,
+]
+
 
 @staff_member_required
 @permission_required("extensions.manage_plugins")
 def index(request: "HttpRequest") -> TemplateResponse:
-    ctx = {}
+    payment_gateways = [gateway.PLUGIN_NAME for gateway in GATEWAYS_PLUGINS]
+
+    ctx = {"payment_gateways": payment_gateways}
     return TemplateResponse(request, "dashboard/payments/index.html", ctx)
