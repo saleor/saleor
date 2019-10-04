b08975227d56ddd0238137b55351db6a37696b97
Santo Cariotti <dcariotti24@gmail.com>
2019-10-04 11:24:18 +0200

    Check payment method
    Check if payment method is Stripe. In this case, return its public key

--- saleor/order/views.py
+++ saleor/order/views.py
@@ -129,6 +129,8 @@ def start_payment(request, order, gateway):
         "client_token": client_token,
         "order": order,
     }
+    if gateway_config.gateway_name == 'stripe':
+        ctx["public_key"] = payment_gateway.get_public_key(config=gateway_config)
     template_path = payment_gateway.get_template_path(payment.gateway)
     return TemplateResponse(request, template_path, ctx)
 
