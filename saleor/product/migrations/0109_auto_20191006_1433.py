178988d30fa2b0aaf97a3ab3ca494c49629c3bff
Alexey Pyltsyn <lex61rus@gmail.com>
2019-10-07 13:09:30 +0300

    Fix code style

--- saleor/product/migrations/0109_auto_20191006_1433.py
+++ saleor/product/migrations/0109_auto_20191006_1433.py
@@ -5,14 +5,12 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('product', '0108_auto_20191003_0422'),
-    ]
+    dependencies = [("product", "0108_auto_20191003_0422")]
 
     operations = [
         migrations.AlterField(
-            model_name='productvariant',
-            name='sku',
+            model_name="productvariant",
+            name="sku",
             field=models.CharField(max_length=255, unique=True),
-        ),
+        )
     ]
