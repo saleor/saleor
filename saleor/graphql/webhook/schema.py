ea32e694ca2ef553a58f1d5c4840ca66fe298322
Maciej Korycinski <maciej@mirumee.com>
2019-10-09 13:38:42 +0200

    Move sample payload to separate query

--- saleor/graphql/webhook/schema.py
+++ saleor/graphql/webhook/schema.py
@@ -1,8 +1,9 @@
 import graphene
 
 from ..core.fields import PrefetchingConnectionField
+from .enums import WebhookEventTypeEnum
 from .mutations import WebhookCreate, WebhookDelete, WebhookUpdate
-from .resolvers import resolve_webhook, resolve_webhooks
+from .resolvers import resolve_sample_payload, resolve_webhook, resolve_webhooks
 from .types import Webhook
 
 
@@ -15,6 +16,17 @@ class WebhookQueries(graphene.ObjectType):
         description="Lookup a webhook by ID.",
     )
     webhooks = PrefetchingConnectionField(Webhook, description="List of webhooks")
+    webhook_sample_payload = graphene.Field(
+        graphene.JSONString,
+        event_type=graphene.Argument(
+            WebhookEventTypeEnum,
+            required=True,
+            description="Name of the reguested event type.",
+        ),
+    )
+
+    def resolve_webhook_sample_payload(self, info, **data):
+        return resolve_sample_payload(info, data["event_type"])
 
     def resolve_webhooks(self, info, **_kwargs):
         return resolve_webhooks(info)
