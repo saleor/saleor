d0aeffa2f3ba9c389432a8c4ed915cdd956ac3e6
nix <khiem3t@gmail.com>
2019-10-11 23:01:08 +0700

    Add API for edit customer note in Order Dashboard

--- saleor/order/__init__.py
+++ saleor/order/__init__.py
@@ -96,7 +96,7 @@ class OrderEvents:
     FULFILLMENT_FULFILLED_ITEMS = "fulfillment_fulfilled_items"
     TRACKING_UPDATED = "tracking_updated"
     NOTE_ADDED = "note_added"
-    CUSTOMER_NOTE_UPDATED = "customer_note_updated"
+    CUSTOMER_NOTE_ADDED = "customer_note_added"
 
     # Used mostly for importing legacy data from before Enum-based events
     OTHER = "other"
@@ -246,13 +246,6 @@ class OrderEvents:
             pgettext_lazy(
                 "Event from an user that added a note to an order",
                 "A note was added to the order",
-            ),
-        ),
-        (
-            CUSTOMER_NOTE_UPDATED,
-            pgettext_lazy(
-                "Event from a user that updated a customer's note to an order",
-                "A customer's note was updated on the order",
             ),
         ),
         (
