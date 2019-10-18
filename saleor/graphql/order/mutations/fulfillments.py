a472c91c122b43461f164cc41b8dc9561dfe59b9
Mira Szewczyk <szewczyk.mira@gmail.com>
2019-10-18 13:43:18 +0200

    Handle Fulfillment metadata through graphql

--- saleor/graphql/order/mutations/fulfillments.py
+++ saleor/graphql/order/mutations/fulfillments.py
@@ -10,7 +10,11 @@ from ....order.actions import (
     order_fulfilled,
 )
 from ....order.error_codes import OrderErrorCode
-from ...core.mutations import BaseMutation
+from ...core.mutations import (
+    BaseMutation,
+    ClearMetaBaseMutation,
+    UpdateMetaBaseMutation,
+)
 from ...core.types.common import OrderError
 from ...order.types import Fulfillment, Order
 from ..types import OrderLine
@@ -42,6 +46,38 @@ class FulfillmentUpdateTrackingInput(graphene.InputObjectType):
 
 class FulfillmentCancelInput(graphene.InputObjectType):
     restock = graphene.Boolean(description="Whether item lines are restocked.")
+
+
+class FulfillmentClearMeta(ClearMetaBaseMutation):
+    class Meta:
+        description = "Clears meta for fulfillment"
+        model = models.Fulfillment
+        permission = "orders.manage_orders"
+        public = True
+
+
+class FulfillmentUpdateMeta(UpdateMetaBaseMutation):
+    class Meta:
+        description = "Updates meta for fulfillment"
+        model = models.Fulfillment
+        permission = "orders.manage_orders"
+        public = True
+
+
+class FulfillmentClearPrivateMeta(ClearMetaBaseMutation):
+    class Meta:
+        description = "Clears private meta for fulfillment"
+        model = models.Fulfillment
+        permission = "orders.manage_orders"
+        public = False
+
+
+class FulfillmentUpdatePrivateMeta(UpdateMetaBaseMutation):
+    class Meta:
+        description = "Updates meta for fulfillment"
+        model = models.Fulfillment
+        permission = "orders.manage_orders"
+        public = False
 
 
 class FulfillmentCreate(BaseMutation):
