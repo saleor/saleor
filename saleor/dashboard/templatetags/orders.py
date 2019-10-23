38bab3eb52d6f61a2d58144c6f2c556c90ac8fdd
nix <khiem3t@gmail.com>
2019-10-23 11:46:02 +0700

    update changelog and text messages

--- saleor/dashboard/templatetags/orders.py
+++ saleor/dashboard/templatetags/orders.py
@@ -91,7 +91,7 @@ def display_order_event(order_event: OrderEvent):
     if event_type == events.OrderEvents.CUSTOMER_NOTE_UPDATED:
         return pgettext_lazy(
             "Dashboard message related to an order",
-            "%(user_name)s added customer's note: %(note)s"
+            "%(user_name)s updated customer's note: %(note)s"
             % {"note": params["message"], "user_name": order_event.user},
         )
     if event_type == events.OrderEvents.FULFILLMENT_CANCELED:
