9ea66c1ff5847e4af9d5726816268874a34d4f06
fowczarek <filip.owczarek@defectprediction.net>
2019-10-24 12:58:19 +0200

    Add option to remove company address

--- saleor/site/models.py
+++ saleor/site/models.py
@@ -59,7 +59,7 @@ class SiteSettings(models.Model):
     default_digital_max_downloads = models.IntegerField(blank=True, null=True)
     default_digital_url_valid_days = models.IntegerField(blank=True, null=True)
     company_address = models.ForeignKey(
-        "account.Address", blank=True, null=True, on_delete=models.CASCADE
+        "account.Address", blank=True, null=True, on_delete=models.SET_NULL
     )
     default_mail_sender_name = models.CharField(
         max_length=settings.DEFAULT_MAX_EMAIL_DISPLAY_NAME_LENGTH,
