11ca8f920d4779864fff64bf9ee88db4429b461c
Mikail KOCAK <mikail@mirumee.com>
2019-10-31 08:52:40 +0100

    Added possibility to filter out db queries tests assertions

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,26 @@
+import re
+
+from django.utils.functional import SimpleLazyObject
+from typing import List, Pattern, Union
+
 # pylint: disable=W0401, W0614
 # flake8: noqa
 from saleor.settings import *  # noqa
+
+
+def lazy_re_compile(regex, flags=0):
+    """Lazily compile a regex with flags."""
+
+    def _compile():
+        # Compile the regex if it was not passed pre-compiled.
+        if isinstance(regex, str):
+            return re.compile(regex, flags)
+        else:
+            assert not flags, "flags must be empty if regex is passed pre-compiled"
+            return regex
+
+    return SimpleLazyObject(_compile)
+
 
 CELERY_TASK_ALWAYS_EAGER = True
 
@@ -44,3 +64,7 @@ PASSWORD_HASHERS = ["tests.dummy_password_hasher.DummyHasher"]
 EXTENSIONS_MANAGER = "saleor.extensions.manager.ExtensionsManager"
 
 PLUGINS = []
+
+PATTERNS_IGNORED_IN_QUERY_CAPTURES: List[Union[Pattern, SimpleLazyObject]] = [
+    lazy_re_compile(r"^SET\s+")
+]
