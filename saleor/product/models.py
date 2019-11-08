6f44abf9abf1110eac5d0fbad550de3b62660539
IKarbowiak <iga.karbowiak@mirumee.com>
2019-11-08 11:04:22 +0100

    Allow to create product without category and set published field to 'False' as default

--- saleor/product/models.py
+++ saleor/product/models.py
@@ -253,7 +253,11 @@ class Product(SeoModel, ModelWithMetadata, PublishableModel):
         blank=True, default=dict, sanitizer=clean_draft_js
     )
     category = models.ForeignKey(
-        Category, related_name="products", on_delete=models.CASCADE
+        Category,
+        related_name="products",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
     )
 
     currency = models.CharField(
