aa56883a4f45371b712a8e27c942357bd0a0102a
Mira Szewczyk <szewczyk.mira@gmail.com>
2019-11-27 12:49:18 +0100

    Add stock queryset

--- saleor/stock/models.py
+++ saleor/stock/models.py
@@ -3,12 +3,19 @@ from django.utils.translation import pgettext_lazy
 
 from ..core.exceptions import InsufficientStock
 from ..product.models import ProductVariant
+from ..shipping.models import ShippingZone
 from ..warehouse.models import Warehouse
 
 
 class StockQuerySet(models.QuerySet):
-    def get_stock(self, product_variant: ProductVariant, warehouse=Warehouse):
-        return self.get(product_variant=product_variant, warehouse=warehouse)
+    def get_stock_for_country(self, product_variant: ProductVariant, country_code: str):
+        shipping_zone = ShippingZone.objects.prefetch_related("warehouse_set").get(
+            countries__icontains=country_code
+        )
+        return self.get(
+            warehouse=models.Subquery(shipping_zone.warehouse_set.get()),
+            product_variant=product_variant,
+        )
 
 
 class Stock(models.Model):
@@ -16,8 +23,8 @@ class Stock(models.Model):
     product_variant = models.ForeignKey(
         ProductVariant, null=False, on_delete=models.CASCADE
     )
-    quantity = models.PositiveIntegerField()
-    quantity_allocated = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=0)
+    quantity_allocated = models.PositiveIntegerField(default=0)
 
     objects = StockQuerySet.as_manager()
 
@@ -36,6 +43,10 @@ class Stock(models.Model):
     @property
     def quantity_available(self) -> int:
         return max(self.quantity - self.quantity_allocated, 0)
+
+    @property
+    def is_available(self):
+        return self.quantity > 0
 
     def check_quantity(self, quantity):
         if quantity > self.quantity_available:
