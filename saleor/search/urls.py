bc0f3e9fdd88f189eb77050b28a6cbb5f0b382e4
IKarbowiak <iga.karbowiak@mirumee.com>
2019-11-27 14:39:02 +0100

    Remove apps forms, views and urls

--- saleor/search/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [url(r"^$", views.search, name="search")]
--- saleor/urls.py
+++ saleor/urls.py
@@ -7,19 +7,10 @@ from django.contrib.staticfiles.views import serve
 from django.views.decorators.csrf import csrf_exempt
 from django.views.i18n import JavaScriptCatalog, set_language
 
-from .account.urls import urlpatterns as account_urls
-from .checkout.urls import checkout_urlpatterns as checkout_urls
 from .core.sitemaps import sitemaps
-from .core.urls import urlpatterns as core_urls
 from .data_feeds.urls import urlpatterns as feed_urls
 from .graphql.api import schema
 from .graphql.views import GraphQLView
-from .order.urls import urlpatterns as order_urls
-from .page.urls import urlpatterns as page_urls
-from .product.urls import urlpatterns as product_urls
-from .search.urls import urlpatterns as search_urls
-
-handler404 = "saleor.core.views.handle_404"
 
 non_translatable_urlpatterns = [
     url(r"^graphql/", csrf_exempt(GraphQLView.as_view(schema=schema)), name="api"),
@@ -34,15 +25,8 @@ non_translatable_urlpatterns = [
 ]
 
 translatable_urlpatterns = [
-    url(r"^", include(core_urls)),
-    url(r"^checkout/", include((checkout_urls, "checkout"), namespace="checkout")),
     url(r"^jsi18n/$", JavaScriptCatalog.as_view(), name="javascript-catalog"),
-    url(r"^order/", include((order_urls, "order"), namespace="order")),
-    url(r"^page/", include((page_urls, "page"), namespace="page")),
-    url(r"^products/", include((product_urls, "product"), namespace="product")),
-    url(r"^account/", include((account_urls, "account"), namespace="account")),
     url(r"^feeds/", include((feed_urls, "data_feeds"), namespace="data_feeds")),
-    url(r"^search/", include((search_urls, "search"), namespace="search")),
 ]
 
 urlpatterns = non_translatable_urlpatterns + i18n_patterns(*translatable_urlpatterns)
