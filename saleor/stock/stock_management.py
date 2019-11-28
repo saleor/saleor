f1494e506a4ed5fa29ee04f41315eaf96f2994f1
Mira Szewczyk <szewczyk.mira@gmail.com>
2019-11-28 09:49:32 +0100

    Add check stock quantity function

---
+++ saleor/stock/stock_management.py
@@ -0,0 +1,17 @@
+from typing import TYPE_CHECKING
+
+from ..core.exceptions import InsufficientStock
+from .models import Stock
+
+if TYPE_CHECKING:
+    from ..product.models import ProductVariant
+
+
+def check_stock_quantity(variant: "ProductVariant", country_code: str, quantity: int):
+    try:
+        stock = Stock.objects.for_country(country_code).get(product_variant=variant)
+    except Stock.DoesNotExist:
+        raise InsufficientStock
+
+    if variant.track_inventory and quantity > stock.quantity_available:
+        raise InsufficientStock(variant)
