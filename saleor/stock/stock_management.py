ca228ced6ad9074d1abe7b609c1afb3cc47cf8f8
Mira Szewczyk <szewczyk.mira@gmail.com>
2019-11-29 12:30:04 +0100

    Get stock quantity for variant in given country

--- saleor/stock/stock_management.py
+++ saleor/stock/stock_management.py
@@ -15,3 +15,11 @@ def check_stock_quantity(variant: "ProductVariant", country_code: str, quantity:
 
     if variant.track_inventory and quantity > stock.quantity_available:
         raise InsufficientStock(variant)
+
+
+def get_available_quantity(variant: "ProductVariant", country_code: str) -> int:
+    try:
+        stock = Stock.objects.for_country(country_code).get(product_variant=variant)
+    except Stock.DoesNotExist:
+        return 0
+    return stock.quantity_available
