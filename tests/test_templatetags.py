e15416a0432ca15e4cc30aa3faa07917b60bb82e
IKarbowiak <iga.karbowiak@mirumee.com>
2019-12-02 11:00:28 +0100

    Remove unused code and update base.html

--- tests/test_templatetags.py
+++ tests/test_templatetags.py
@@ -1,5 +1,4 @@
 from saleor.core.templatetags.shop import menu
-from saleor.core.templatetags.status import LABEL_SUCCESS, render_page_availability
 
 
 def test_menu(menu_with_items):
@@ -8,13 +7,3 @@ def test_menu(menu_with_items):
 
     result = menu(menu_with_items)
     assert result["menu_items"] == menu_with_items.json_content
-
-
-def test_render_page_availability(page):
-    page_ctx = render_page_availability(page)
-    assert page_ctx == {"page": page, "is_visible": True, "label_cls": LABEL_SUCCESS}
-
-    page.is_published = False
-    page.save()
-    page_ctx = render_page_availability(page)
-    assert page_ctx == {"page": page, "is_visible": False}
