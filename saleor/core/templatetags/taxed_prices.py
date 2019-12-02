720d3594363170a1f8f5910cc728bbe3d05aed5f
IKarbowiak <iga.karbowiak@mirumee.com>
2019-12-02 10:51:15 +0100

    Remove more templates and connected django code

--- saleor/core/templatetags/taxed_prices.py
+++ saleor/core/templatetags/taxed_prices.py
@@ -6,19 +6,6 @@ from ...core.taxes import get_display_price
 register = template.Library()
 
 
-@register.inclusion_tag("product/_price_range.html", takes_context=True)
-def price_range(context, price_range):
-    display_gross = context["site"].settings.display_gross_prices
-    return {"display_gross": display_gross, "price_range": price_range}
-
-
-@register.simple_tag
-def tax_rate(request, product):
-    """Return tax rate value for given tax rate type in current country."""
-    extensions = request.extensions
-    return extensions.get_tax_rate_percentage_value(product, request.country)
-
-
 @register.inclusion_tag("price.html", takes_context=True)
 def price(context, base, display_gross=None, html=True):
     if isinstance(base, (TaxedMoney, TaxedMoneyRange)):
