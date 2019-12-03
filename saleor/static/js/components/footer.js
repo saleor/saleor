777e40b5376376b4e6140de2989f5a752260b7f4
IKarbowiak <iga.karbowiak@mirumee.com>
2019-12-03 10:47:12 +0100

    Remove static files - js and scss

--- saleor/static/js/components/footer.js
+++
@@ -1,9 +0,0 @@
-export default $(document).ready(e => {
-  let navbarHeight = $(".navbar").outerHeight(true);
-  let footerHeight = $(".footer").outerHeight(true);
-  let windowHeight = $(window).height();
-  $(".maincontent").css(
-    "min-height",
-    windowHeight - navbarHeight - footerHeight
-  );
-});
