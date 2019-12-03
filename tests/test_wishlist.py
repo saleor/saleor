9e79fde0ed2449289057d8c8c0e6f88f5cc49924
Mateusz Dereniowski <derenio@gmail.com>
2019-12-03 14:22:08 +0100

    Fix moving duplicate variants between wishlists

--- tests/test_wishlist.py
+++ tests/test_wishlist.py
@@ -80,3 +80,31 @@ def test_remove_single_variant_from_wishlist_item(
     customer_wishlist_item_with_two_variants.refresh_from_db()
     assert customer_wishlist_item_with_two_variants.variants.count() == 1
     assert customer_wishlist_item_with_two_variants.variants.first() == variant_2
+
+
+def test_move_items_between_wishlists_with_duplicates(variant, customer_wishlist_item):
+    dst_wishlist_item = customer_wishlist_item
+    assert dst_wishlist_item.variants.count() == 1
+    dst_wishlist = dst_wishlist_item.wishlist
+    assert dst_wishlist.items.count() == 1
+    dst_variant = dst_wishlist_item.variants.first()
+    assert variant.pk != dst_variant.pk
+    # Create the source wishlist
+    src_wishlist = Wishlist.objects.create()
+    # Add the new variant
+    src_item_1 = src_wishlist.add_variant(variant)
+    # Add the destination variants to the source wishlist (the duplicate case)
+    src_item_2 = src_wishlist.add_variant(dst_variant)
+    # Move items from the source to the destination wishlist
+    WishlistItem.objects.move_items_between_wishlists(src_wishlist, dst_wishlist)
+    # Check the source wishlist doesn't have any items
+    assert src_wishlist.items.count() == 0
+    # Check the destination wishlist has two items
+    assert dst_wishlist.items.count() == 2
+    # Check that the source wishlist item with the new variant was moved to the
+    # destination wishlist
+    src_item_1.refresh_from_db()
+    assert src_item_1.wishlist == dst_wishlist
+    # Check that the source wishlist item with the duplicate variant was removed
+    with pytest.raises(WishlistItem.DoesNotExist):
+        src_item_2.refresh_from_db()
