85342c673489ec363e51d24d787a148e15d5a4bd
Mateusz Dereniowski <derenio@gmail.com>
2019-12-03 13:18:35 +0100

    Add "ResolveInfo" typing in the wishlist resolvers
    also add comments to the resolvers.

--- saleor/graphql/wishlist/resolvers.py
+++ saleor/graphql/wishlist/resolvers.py
@@ -1,10 +1,17 @@
 from typing import List
 
+from graphene.types import ResolveInfo
+
 from ...wishlist.models import Wishlist, WishlistItem
 from ...wishlist.session_helpers import WishlistSessionHelper
 
 
-def get_wishlist_from_info(info) -> Wishlist:
+def get_wishlist_from_info(info: ResolveInfo) -> Wishlist:
+    """Return wishlist depending on the user.
+
+    For anonymous users return wishlist stored in the session
+    and for the logged in its private one.
+    """
     user = info.context.user
     if user.is_anonymous:
         session = info.context.session
@@ -15,6 +22,7 @@ def get_wishlist_from_info(info) -> Wishlist:
     return wishlist
 
 
-def resolve_wishlist_items(info) -> List[WishlistItem]:
+def resolve_wishlist_items(info: ResolveInfo) -> List[WishlistItem]:
+    """Return all the items of a wishlist obtained from the "info" object."""
     wishlist = get_wishlist_from_info(info)
     return wishlist.items.all()
