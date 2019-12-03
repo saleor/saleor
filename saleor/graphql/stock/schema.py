ae5d7e84c279c1c906850b99d2cdfc77971d7f53
Mira Szewczyk <szewczyk.mira@gmail.com>
2019-12-03 10:44:24 +0100

    Update and test stock queries

--- saleor/graphql/stock/schema.py
+++ saleor/graphql/stock/schema.py
@@ -1,6 +1,10 @@
 import graphene
+import graphene_django_optimizer as gql_optimizer
 
+from ...stock import models
+from ..core.fields import FilterInputConnectionField
 from ..decorators import permission_required
+from .filters import StockFilterInput
 from .mutations import StockBulkDelete, StockCreate, StockDelete, StockUpdate
 from .types import Stock
 
@@ -11,12 +15,27 @@ class StockQueries(graphene.ObjectType):
         description="Look up a stok by ID",
         id=graphene.ID(required=True, description="ID of an warehouse"),
     )
-    # TODO: Add stocks query
+    stocks = FilterInputConnectionField(
+        Stock,
+        description="List of stocks.",
+        filter=StockFilterInput(),
+        query=graphene.String(),
+    )
 
     @permission_required("stock.manage_stocks")
-    def resolve_warehouse(self, info, **kwargs):
+    def resolve_stock(self, info, **kwargs):
         stock_pk = kwargs.get("id")
-        return graphene.Node.get_node_from_global_id(info, stock_pk, Stock)
+        stock = graphene.Node.get_node_from_global_id(info, stock_pk, Stock)
+        return stock
+
+    @permission_required("stock.manage_stocks")
+    def resolve_stocks(self, info, **data):
+        qs = (
+            models.Stock.objects.select_related("warehouse")
+            .prefetch_related("product_variant__product")
+            .all()
+        )
+        return gql_optimizer.query(qs, info)
 
 
 class StockMutations(graphene.ObjectType):
