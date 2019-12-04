1a606b87446d0b3ae2b271a8bc92cd321ce3acbb
Mira Szewczyk <szewczyk.mira@gmail.com>
2019-12-04 19:35:52 +0100

    Add stock and warehouse in populatedb

--- saleor/stock/utils/management.py
+++ saleor/stock/utils/management.py
@@ -6,21 +6,37 @@ if TYPE_CHECKING:
     from ..product.models import ProductVariant
 
 
-def allocate_stock(variant: "ProductVariant", country_code: str, quantity: int):
+def allocate_stock(
+    variant: "ProductVariant", country_code: str, quantity: int, commit: bool = True
+) -> Stock:
     stock = Stock.objects.for_country(country_code).get(product_variant=variant)
-    stock.allocate_stock(quantity)
+    stock.allocate_stock(quantity, commit=commit)
+    return stock
 
 
-def deallocate_stock(variant: "ProductVariant", country_code: str, quantity: int):
+def deallocate_stock(
+    variant: "ProductVariant", country_code: str, quantity: int, commit: bool = True
+) -> Stock:
     stock = Stock.objects.for_country(country_code).get(product_variant=variant)
-    stock.deallocate_stock(quantity)
+    stock.deallocate_stock(quantity, commit=commit)
+    return stock
 
 
-def increase_stock(variant: "ProductVariant", country_code: str, quantity: int):
+def increase_stock(
+    variant: "ProductVariant",
+    country_code: str,
+    quantity: int,
+    allocate: bool = False,
+    commit: bool = True,
+) -> Stock:
     stock = Stock.objects.for_country(country_code).get(product_variant=variant)
-    stock.increase_stock(quantity)
+    stock.increase_stock(quantity, allocate=allocate, commit=commit)
+    return stock
 
 
-def decrease_stock(variant: "ProductVariant", country_code: str, quantity: int):
+def decrease_stock(
+    variant: "ProductVariant", country_code: str, quantity: int, commit: bool = True
+) -> Stock:
     stock = Stock.objects.for_country(country_code).get(product_variant=variant)
-    stock.decrease_stock(quantity)
+    stock.decrease_stock(quantity, commit=commit)
+    return stock
