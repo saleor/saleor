0eeca25dea17df0b5653428ecd74bf54a1738b69
fowczarek <filip.owczarek@defectprediction.net>
2019-12-05 12:16:24 +0100

    Add sort by to menus query

--- saleor/graphql/menu/resolvers.py
+++ saleor/graphql/menu/resolvers.py
@@ -2,7 +2,8 @@ import graphene
 import graphene_django_optimizer as gql_optimizer
 
 from ...menu import models
-from ..utils import filter_by_query_param
+from ..utils import filter_by_query_param, sort_queryset
+from .sorters import MenuSortField
 from .types import Menu
 
 MENU_SEARCH_FIELDS = ("name",)
@@ -18,9 +19,10 @@ def resolve_menu(info, menu_id=None, name=None):
     return graphene.Node.get_node_from_global_id(info, menu_id, Menu)
 
 
-def resolve_menus(info, query):
+def resolve_menus(info, query, sort_by=None, **_kwargs):
     qs = models.Menu.objects.all()
     qs = filter_by_query_param(qs, query, MENU_SEARCH_FIELDS)
+    qs = sort_queryset(qs, sort_by, MenuSortField)
     return gql_optimizer.query(qs, info)
 
 
---
+++ saleor/graphql/menu/sorters.py
@@ -0,0 +1,31 @@
+import graphene
+from django.db.models import Count, QuerySet
+
+from ..core.types import SortInputObjectType
+
+
+class MenuSortField(graphene.Enum):
+    NAME = "name"
+    ITEMS_COUNT = "items_count"
+
+    @property
+    def description(self):
+        # pylint: disable=no-member
+        if self in [MenuSortField.NAME, MenuSortField.ITEMS_COUNT]:
+            sort_name = self.name.lower().replace("_", " ")
+            return f"Sort menus by {sort_name}."
+        raise ValueError("Unsupported enum value: %s" % self.value)
+
+    @staticmethod
+    def sort_by_items_count(
+        queryset: QuerySet, sort_by: SortInputObjectType
+    ) -> QuerySet:
+        return queryset.annotate(items_count=Count("items__id")).order_by(
+            f"{sort_by.direction}items_count", "name"
+        )
+
+
+class MenuSortingInput(SortInputObjectType):
+    class Meta:
+        sort_enum = MenuSortField
+        type_name = "menus"
