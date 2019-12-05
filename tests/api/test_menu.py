9ad7d1d6d21b71d47590ef78c9beb61fd8d9883e
fowczarek <filip.owczarek@defectprediction.net>
2019-12-05 13:53:55 +0100

    Add sort by to menu items

--- tests/api/test_menu.py
+++ tests/api/test_menu.py
@@ -225,6 +225,42 @@ def test_menu_items_query_with_filter(
     assert content["data"]["menuItems"]["totalCount"] == count
 
 
+QUERY_MENU_ITEMS_WITH_SORT = """
+    query ($sort_by: MenuItemSortingInput!) {
+        menuItems(first:5, sortBy: $sort_by) {
+            edges{
+                node{
+                    name
+                }
+            }
+        }
+    }
+"""
+
+
+@pytest.mark.parametrize(
+    "menu_item_sort, result_order",
+    [
+        ({"field": "NAME", "direction": "ASC"}, ["MenuItem1", "MenuItem2"]),
+        ({"field": "NAME", "direction": "DESC"}, ["MenuItem2", "MenuItem1"]),
+    ],
+)
+def test_query_menu_items_with_sort(
+    menu_item_sort, result_order, staff_api_client, permission_manage_menus
+):
+    menu = Menu.objects.create(name="Menu1")
+    MenuItem.objects.create(name="MenuItem1", menu=menu)
+    MenuItem.objects.create(name="MenuItem2", menu=menu)
+    variables = {"sort_by": menu_item_sort}
+    staff_api_client.user.user_permissions.add(permission_manage_menus)
+    response = staff_api_client.post_graphql(QUERY_MENU_ITEMS_WITH_SORT, variables)
+    content = get_graphql_content(response)
+    menu_items = content["data"]["menuItems"]["edges"]
+
+    for order, menu_item_name in enumerate(result_order):
+        assert menu_items[order]["node"]["name"] == menu_item_name
+
+
 def test_menu_item_query_static_url(user_api_client, menu_item):
     query = """
     query menuitem($id: ID!) {
