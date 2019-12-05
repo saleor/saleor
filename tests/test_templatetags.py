2211111cddc08b91bbd33adc0c4a512a5c499599
IKarbowiak <iga.karbowiak@mirumee.com>
2019-12-05 11:51:20 +0100

    Removed tests for non existing file

--- tests/test_templatetags.py
+++
@@ -1,9 +0,0 @@
-from saleor.core.templatetags.shop import menu
-
-
-def test_menu(menu_with_items):
-    result = menu()
-    assert result == {"horizontal": False, "menu_items": []}
-
-    result = menu(menu_with_items)
-    assert result["menu_items"] == menu_with_items.json_content
