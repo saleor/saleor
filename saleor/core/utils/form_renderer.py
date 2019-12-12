201b87717622df3ae220febb392bc7262382972c
IKarbowiak <iga.karbowiak@mirumee.com>
2019-12-12 12:02:38 +0100

    Remove bootstrap4 remaining form and settings

--- saleor/core/utils/form_renderer.py
+++
@@ -1,6 +0,0 @@
-from bootstrap4.renderers import FormRenderer as BaseFormRenderer
-
-
-class FormRenderer(BaseFormRenderer):
-    def render_errors(self, type="non_fields"):
-        return super().render_errors(type)
