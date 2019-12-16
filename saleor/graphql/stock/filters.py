5963cca67bb73208c415520b88608cb994bfe4fc
Mira Szewczyk <szewczyk.mira@gmail.com>
2019-12-16 11:11:01 +0100

    Prefetch data for filters

--- saleor/graphql/stock/filters.py
+++ saleor/graphql/stock/filters.py
@@ -5,8 +5,14 @@ from ...stock.models import Stock
 from ..core.types import FilterInputObjectType
 
 
+def qs_prefetched_for_filters(qs):
+    return qs.prefetch_related("product_variant__product").select_related(
+        "product_variant"
+    )
+
+
 def filter_product_variant(qs, _, value):
-    qs = qs.filter(
+    qs = qs_prefetched_for_filters(qs).filter(
         Q(product_variant__product__name__icontains=value)
         | Q(product_variant__name__icontains=value)
     )
@@ -14,7 +20,7 @@ def filter_product_variant(qs, _, value):
 
 
 def filter_warehouse(qs, _, value):
-    qs = qs.filter(
+    qs = qs_prefetched_for_filters(qs).filter(
         Q(warehouse__name__icontains=value)
         | Q(warehouse__company_name__icontains=value)
     )
