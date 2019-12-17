93349a380633080580da9b3700893492ed7b90c5
Mira Szewczyk <szewczyk.mira@gmail.com>
2019-12-17 10:28:01 +0100

    Fix typo and assign error code

--- saleor/warehouse/error_codes.py
+++ saleor/warehouse/error_codes.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class WarehouseErorrCode(str, Enum):
+class WarehouseErrorCode(str, Enum):
     ALREADY_EXISTS = "already_exists"
     GRAPHQL_ERROR = "graphql_error"
     INVALID = "invalid"
