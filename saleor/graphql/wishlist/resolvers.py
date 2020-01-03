fbba47d7774330f29da220813f623b7c4916a30e
IKarbowiak <iga.karbowiak@mirumee.com>
2020-01-03 11:06:05 +0100

    Fix mypy errors for graphql.wishlist

--- saleor/graphql/wishlist/resolvers.py
+++ saleor/graphql/wishlist/resolvers.py
@@ -1,17 +1,19 @@
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING
 
 from ...wishlist.models import Wishlist
 
 if TYPE_CHECKING:
     # pylint: disable=unused-import
+    from django.db.models.query import QuerySet
     from graphene.types import ResolveInfo
     from ...account.models import User
+    from ...wishlist.models import WishlistItem
 
 
 def resolve_wishlist_from_user(user: "User") -> Wishlist:
     """Return wishlist of the logged in user."""
-    wishlist = Wishlist.objects.get_or_create(user)
-    return wishlist
+    wishlist = Wishlist.objects.get_or_create(user=user)
+    return wishlist  # type: ignore
 
 
 def resolve_wishlist_from_info(info: "ResolveInfo") -> Wishlist:
@@ -20,7 +22,7 @@ def resolve_wishlist_from_info(info: "ResolveInfo") -> Wishlist:
     return resolve_wishlist_from_user(user)
 
 
-def resolve_wishlist_items_from_user(user: "User") -> List[Wishlist]:
+def resolve_wishlist_items_from_user(user: "User") -> "QuerySet[WishlistItem]":
     """Return wishlist items of the logged in user."""
     wishlist = resolve_wishlist_from_user(user)
     return wishlist.items.all()
