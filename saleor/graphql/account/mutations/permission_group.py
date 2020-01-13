a7870d08da5b425eb0fe1697444e0a6e90730d8a
IKarbowiak <iga.karbowiak@mirumee.com>
2020-01-13 12:10:53 +0100

    Update PermissionGroupCreate - move permissions to input

--- saleor/graphql/account/mutations/permission_group.py
+++ saleor/graphql/account/mutations/permission_group.py
@@ -10,7 +10,12 @@ from ..types import PermissionGroup
 
 
 class PermissionGroupInput(graphene.InputObjectType):
-    name = graphene.String(description="Group name.")
+    name = graphene.String(description="Group name.", required=True)
+    permissions = graphene.List(
+        graphene.NonNull(PermissionEnum),
+        description="List of permission code names to assign to this group.",
+        required=False,
+    )
 
 
 class PermissionGroupCreate(ModelMutation):
@@ -18,16 +23,11 @@ class PermissionGroupCreate(ModelMutation):
 
     class Arguments:
         input = PermissionGroupInput(
-            description="Fields required to create permission group.", required=True
-        )
-        permissions = graphene.List(
-            graphene.NonNull(PermissionEnum),
-            description="List of permission code names to assign to this group.",
-            required=False,
+            description="Input fields to create permission group.", required=True
         )
 
     class Meta:
-        description = "Create new permission group"
+        description = "Create new permission group."
         model = models.Group
         permissions = (AccountPermissions.MANAGE_STAFF,)
         error_type_class = AccountError
@@ -37,7 +37,8 @@ class PermissionGroupCreate(ModelMutation):
     @transaction.atomic
     def perform_mutation(cls, _root, info, **data):
         success_response = super().perform_mutation(_root, info, **data)
-        permission_names = data.get("permissions", [])
+        input_data = data["input"]
+        permission_names = input_data.get("permissions", [])
         permission_codes = [name.split(".")[1] for name in permission_names]
         permissions = models.Permission.objects.filter(codename__in=permission_codes)
         instance = success_response.group
