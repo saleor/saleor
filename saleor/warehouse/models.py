556e0eee9f74b0d4a4570b380e9c5a67d5b44a55
Mira Szewczyk <szewczyk.mira@gmail.com>
2020-01-21 11:31:06 +0100

    Add management methods for Warehouse

--- saleor/warehouse/models.py
+++ saleor/warehouse/models.py
@@ -1,20 +1,41 @@
 import itertools
 import uuid
-from typing import Set
+from typing import Optional, Set
 
-from django.db import models
-from django.db.models import F
+from django.contrib.sites.models import Site
+from django.db import models, transaction
+from django.db.models import F, QuerySet
 from django.utils.translation import pgettext_lazy
 
 from ..account.models import Address
 from ..core.exceptions import InsufficientStock
 from ..product.models import ProductVariant
 from ..shipping.models import ShippingZone
+from ..site.models import SiteSettings
 
 
 class WarehouseQueryset(models.QuerySet):
     def prefetch_data(self):
         return self.select_related("address").prefetch_related("shipping_zones")
+
+    def for_country(self, country: str):
+        return self.filter(shipping_zone__countries__contains=country)
+
+    @transaction.atomic
+    def create_for_shipping_zones(
+        self,
+        address: Optional[Address] = None,
+        shipping_zones: Optional[QuerySet] = None,
+    ):
+        if shipping_zones is None:
+            shipping_zones = ShippingZone.objects.all()
+        current_settings = SiteSettings.objects.get(site=Site.objects.get_current())
+        address = address or current_settings.company_address
+        if address is None:
+            address = Address.objects.create()
+        warehouse = self.create(address=address)
+        warehouse.shipping_zones.add(*shipping_zones.values_list("pk", flat=True))
+        return warehouse
 
 
 class Warehouse(models.Model):
