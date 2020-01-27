1708748231e7c4626db732477b6e3807e0d00f82
IKarbowiak <iga.karbowiak@mirumee.com>
2020-01-27 11:06:46 +0100

    Add ProductVariant to product csv export

--- saleor/csv/utils.py
+++ saleor/csv/utils.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Dict, List, Set, Tuple, Union
 
 import petl as etl
 
@@ -16,33 +16,66 @@ def export_products(queryset, delimiter=";"):
         "price_amount": "price",
     }
 
-    attributes_fields = set()
-    fields = ["id", "name", "description"] + headers_mapping.keys()
+    attributes_and_warehouse_fields = set()
+    product_fields = ["id", "name", "description"] + list(headers_mapping.keys())
+    variant_fields = ["sku"]
+
     products_with_variants_data = []
-    products_data = queryset.values(*fields)
+    products_data = queryset.values(*product_fields)
 
     for product, product_data in zip(queryset, products_data):
-        product_data["collections"] = ", ".join(
-            product.collections.values_list("slug", flat=True)
+        product_data, product_attributes_fields = update_product_data(
+            product, product_data
         )
-        product_data["images"] = get_image_file_names(product)
-        product_attributes_data = prepare_attributes_data(product)
-        attributes_fields.update(product_attributes_data.keys())
-        product_data.update(product_attributes_data)
+        attributes_and_warehouse_fields.update(product_attributes_fields)
         products_with_variants_data.append(product_data)
 
-    fields += list(attributes_fields)
-    table = etl.fromdicts(products_with_variants_data, header=fields, missing="-")
+        variants = product.variants.all()
+        for variant in variants:
+            variant_data, variant_specific_fields = create_variant_data(variant)
+            attributes_and_warehouse_fields.update(variant_specific_fields)
+            products_with_variants_data.append(variant_data)
+
+    headers = product_fields + variant_fields + list(attributes_and_warehouse_fields)
+    table = etl.fromdicts(products_with_variants_data, header=headers, missing="-")
     table = etl.rename(table, headers_mapping)
 
     etl.tocsv(table, file_name, delimiter=delimiter)
 
 
-def get_image_file_names(product: "Product"):
+def update_product_data(
+    product: "Product", product_data: Dict["str", Union["str", bool]],
+) -> Tuple[dict, set]:
+    product_data["collections"] = ", ".join(
+        product.collections.values_list("slug", flat=True)
+    )
+    product_data["images"] = get_image_file_names(product)
+    product_attributes_data = prepare_attributes_data(product)
+    product_data.update(product_attributes_data)
+
+    return product_data, product_attributes_data.keys()
+
+
+def create_variant_data(variant: "ProductVariant"):
+    variant_data = {"sku": variant.sku}
+    variant_data["images"] = get_image_file_names(variant)
+    variant_attribute_data = prepare_attributes_data(variant)
+    variant_data.update(variant_attribute_data)
+
+    warehouse_data = prepare_warehouse_data(variant)
+    variant_data.update(warehouse_data)
+
+    variant_fields = set(variant_attribute_data.keys())
+    variant_fields.update(set(warehouse_data.keys()))
+
+    return variant_data, variant_fields
+
+
+def get_image_file_names(instance: Union["Product", "ProductVariant"]):
     return ", ".join(
         [
             image.split("/")[1]
-            for image in product.images.values_list("image", flat=True)
+            for image in instance.images.values_list("image", flat=True)
         ]
     )
 
@@ -56,3 +89,15 @@ def prepare_attributes_data(instance: Union["Product", "ProductVariant"]):
             assigned_attribute.values.values_list("slug", flat=True)
         )
     return attribute_values
+
+
+def prepare_warehouse_data(variant: "ProductVariant"):
+    data = variant.stock.values("warehouse__id", "quantity", "quantity_allocated")
+    warehouse_data = {}
+    for stock_data in data:
+        warehouse_key = f"warehouse_{stock_data['warehouse__id']}"
+        warehouse_data[f"{warehouse_key}_qty"] = stock_data["quantity"]
+        warehouse_data[f"{warehouse_key}_qty_allocated"] = stock_data[
+            "quantity_allocated"
+        ]
+    return warehouse_data
