da4ff2cb5854f5b34ebc914e01dc40b0bf84f43a
fowczarek <filip.owczarek@defectprediction.net>
2020-01-31 14:24:15 +0100

    Create a new mutation for clear public meta with tests

--- saleor/graphql/meta/schema.py
+++ saleor/graphql/meta/schema.py
@@ -1,7 +1,8 @@
 import graphene
 
-from .mutations import UpdateMeta
+from .mutations import ClearMeta, UpdateMeta
 
 
 class MetaMutations(graphene.ObjectType):
+    clear_meta = ClearMeta.Field()
     update_meta = UpdateMeta.Field()
