70e5a4fdf248d6e745c9a363aa30ee411edbfe37
Tomasz Szymański <lime129@gmail.com>
2020-01-31 11:16:52 +0100

    Removed vendor specific code

--- saleor/settings.py
+++ saleor/settings.py
@@ -10,8 +10,6 @@ from django.core.exceptions import ImproperlyConfigured
 from django_prices.utils.formatting import get_currency_fraction
 from sentry_sdk.integrations.django import DjangoIntegration
 
-from .tracing import OpenTracingConfig
-
 
 def get_list(text):
     return [item.strip() for item in text.split(",")]
@@ -563,17 +561,3 @@ if (
 
 
 ENABLE_OPENTRACING = get_bool_from_env("ENABLE_OPENTRACING", False)
-# TRACER_TYPE can be either DATADOG or JAEGER
-# Default host / port for DataDog: localhost / 8126, Jaeger: localhost / 5775
-TRACER_TYPE = os.environ.get("TRACER_TYPE")
-TRACER_REPORTING_HOST = os.environ.get("TRACER_REPORTING_HOST")
-TRACER_REPORTING_PORT = os.environ.get("TRACER_REPORTING_PORT")
-
-
-if ENABLE_OPENTRACING:
-    config = OpenTracingConfig(
-        tracer_type=TRACER_TYPE,
-        reporting_host=TRACER_REPORTING_HOST,
-        reporting_port=TRACER_REPORTING_PORT,
-    )
-    config.create_global_tracer(service_name="saleor")
