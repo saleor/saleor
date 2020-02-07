996e3de9e58b4f7e7798848db425e7e4cf813ab4
IKarbowiak <iga.karbowiak@mirumee.com>
2020-02-07 11:56:56 +0100

    Update ExportProducts mutation - call celery task for export data

--- saleor/graphql/csv/mutations.py
+++ saleor/graphql/csv/mutations.py
@@ -2,11 +2,14 @@ import graphene
 from django.core.exceptions import ValidationError
 
 from ...core.permissions import ProductPermissions
+from ...csv import models as csv_models
+from ...csv.utils.export import export_products
 from ...product import models as product_models
 from ..core.enums import CsvErrorCode
 from ..core.mutations import BaseMutation
 from ..core.types.common import CsvError
 from ..product.filters import ProductFilterInput
+from ..utils import _resolve_nodes
 from .enums import ExportScope
 from .types import Job
 
@@ -43,15 +46,15 @@ class ExportProducts(BaseMutation):
 
     @classmethod
     def perform_mutation(cls, root, info, **data):
-        # TODO: add prefetch_related: attributes, productVariant,
-        # productType, collections, category etc.
-        cls.get_queryset(cls, data["input"])
-        return None
+        scope = cls.get_products_scope(info, data["input"])
+        job = csv_models.Job.objects.create()
+        export_products.delay(scope, job.pk)
+        job.refresh_from_db()
+        return cls(job=job)
 
     @classmethod
-    def get_queryset(cls, info, input):
+    def get_products_scope(cls, info, input):
         scope = input["scope"]
-        queryset = product_models.Product.objects.all()
         if scope == ExportScope.IDS.value:
             ids = input.get("ids", [])
             if not ids:
@@ -63,9 +66,8 @@ class ExportProducts(BaseMutation):
                         )
                     }
                 )
-            queryset = cls.get_nodes_or_error(
-                ids, "ids", only_type=product_models.Product
-            )
+            _, pks = _resolve_nodes(ids, graphene_type=product_models.Product)
+            return {"ids": pks}
         elif scope == ExportScope.FILTER.value:
             filter = input.get("filter")
             if not filter:
@@ -77,7 +79,5 @@ class ExportProducts(BaseMutation):
                         )
                     }
                 )
-            queryset = ProductFilterInput.filterset_class(
-                data=filter, queryset=queryset
-            ).qs
-        return queryset.order_by("pk")
+            return {"filter": filter}
+        return {"all": ""}
--- saleor/graphql/csv/types.py
+++ saleor/graphql/csv/types.py
@@ -9,13 +9,21 @@ from ..core.connection import CountableDjangoObjectType
 @key(fields="id")
 class Job(CountableDjangoObjectType):
     url = graphene.String(description="The URL of field to download.")
+    status = graphene.String(description="Job status.")
 
     class Meta:
         description = "Represents job data"
         interfaces = [relay.Node]
         model = models.Job
-        only_fields = ["id", "created_at", "status", "url"]
+        only_fields = ["id", "created_at", "ended_at", "status", "url"]
 
     @staticmethod
     def resolve_url(root: models.Job, _info):
-        return root.content_file.url if root.content_file else ""
+        content_file = root.content_file
+        if not content_file:
+            return ""
+        return _info.context.build_absolute_uri(content_file.url)
+
+    @staticmethod
+    def resolve_status(root: models.Job, _info):
+        return root.status.split(".")[1]
