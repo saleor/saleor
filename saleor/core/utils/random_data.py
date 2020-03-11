121190024ae830831c083211fd29e92bba0d1eca
fowczarek <filip.owczarek@defectprediction.net>
2020-03-11 10:10:00 +0100

    Fix invalid permission in populated permission group

--- saleor/core/utils/random_data.py
+++ saleor/core/utils/random_data.py
@@ -29,6 +29,7 @@ from ...core.permissions import (
     CheckoutPermissions,
     GiftcardPermissions,
     OrderPermissions,
+    get_permissions,
 )
 from ...core.utils import build_absolute_uri
 from ...core.weight import zero_weight
@@ -569,7 +570,7 @@ def create_permission_groups():
     super_users = User.objects.filter(is_superuser=True)
     if not super_users:
         super_users = create_staff_users(1, True)
-    group = create_group("Full Access", Permission.objects.all(), super_users)
+    group = create_group("Full Access", get_permissions(), super_users)
     yield f"Group: {group}"
 
     staff_users = create_staff_users()
