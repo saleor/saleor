ee246cf316a1fa3364fe443f779aa59065d36037
fowczrek <filip.owczarek@defectprediction.net>
2020-03-19 12:39:41 +0100

    Remove duplicated tests

--- tests/api/pagination/test_discount.py
+++ tests/api/pagination/test_discount.py
@@ -10,7 +10,7 @@ from tests.api.utils import get_graphql_content
 
 
 @pytest.fixture
-@freeze_time("2020-03-18 12:00:00", auto_tick_seconds=60)
+@freeze_time("2020-03-18 12:00:00")
 def sales_for_pagination(db):
     now = timezone.now()
     return Sale.objects.bulk_create(
@@ -71,119 +71,6 @@ QUERY_SALES_PAGINATION = """
         }
     }
 """
-
-
-@pytest.mark.parametrize("page_size", [1, 3, 6])
-def test_sales_pagination_forward(
-    page_size, staff_api_client, permission_manage_discounts, sales_for_pagination,
-):
-    end_cursor = None
-    has_next_page = True
-    sale_count = 0
-    while has_next_page:
-        variables = {"first": page_size, "after": end_cursor}
-        response = staff_api_client.post_graphql(
-            QUERY_SALES_PAGINATION,
-            variables,
-            permissions=[permission_manage_discounts],
-            check_no_permissions=False,
-        )
-        content = get_graphql_content(response)
-        page_info = content["data"]["sales"]["pageInfo"]
-        has_next_page = page_info["hasNextPage"]
-        end_cursor = page_info["endCursor"]
-        sale_count += len(content["data"]["sales"]["edges"])
-    assert sale_count == len(sales_for_pagination)
-
-
-@pytest.mark.parametrize("page_size", [1, 3, 6])
-def test_sales_pagination_backward(
-    page_size, staff_api_client, permission_manage_discounts, sales_for_pagination,
-):
-    start_cursor = None
-    has_previous_page = True
-    sale_count = 0
-    while has_previous_page:
-        variables = {"last": page_size, "before": start_cursor}
-        response = staff_api_client.post_graphql(
-            QUERY_SALES_PAGINATION,
-            variables,
-            permissions=[permission_manage_discounts],
-            check_no_permissions=False,
-        )
-        content = get_graphql_content(response)
-        page_info = content["data"]["sales"]["pageInfo"]
-        has_previous_page = page_info["hasPreviousPage"]
-        start_cursor = page_info["startCursor"]
-        sale_count += len(content["data"]["sales"]["edges"])
-    assert sale_count == len(sales_for_pagination)
-
-
-def test_sales_pagination_order(
-    staff_api_client, permission_manage_discounts, sales_for_pagination,
-):
-    page_size = len(sales_for_pagination)
-
-    variables = {"first": page_size, "after": None}
-    response = staff_api_client.post_graphql(
-        QUERY_SALES_PAGINATION,
-        variables,
-        permissions=[permission_manage_discounts],
-        check_no_permissions=False,
-    )
-    content = get_graphql_content(response)
-    edges_forward = content["data"]["sales"]["edges"]
-
-    variables = {"last": page_size, "before": None}
-    response = staff_api_client.post_graphql(
-        QUERY_SALES_PAGINATION,
-        variables,
-        permissions=[permission_manage_discounts],
-        check_no_permissions=False,
-    )
-    content = get_graphql_content(response)
-    edges_backward = content["data"]["sales"]["edges"]
-
-    assert edges_forward == edges_backward
-
-
-def test_sales_pagination_previous_page_using_last(
-    staff_api_client, permission_manage_discounts, sales_for_pagination
-):
-    page_size = 2
-
-    variables = {"first": page_size, "after": None}
-    response = staff_api_client.post_graphql(
-        QUERY_SALES_PAGINATION,
-        variables,
-        permissions=[permission_manage_discounts],
-        check_no_permissions=False,
-    )
-    content = get_graphql_content(response)
-    first_page_edges_forward = content["data"]["sales"]["edges"]
-    first_page_info = content["data"]["sales"]["pageInfo"]
-
-    variables = {"first": page_size, "after": first_page_info["endCursor"]}
-    response = staff_api_client.post_graphql(
-        QUERY_SALES_PAGINATION,
-        variables,
-        permissions=[permission_manage_discounts],
-        check_no_permissions=False,
-    )
-    content = get_graphql_content(response)
-    second_page_info = content["data"]["sales"]["pageInfo"]
-
-    variables = {"last": page_size, "before": second_page_info["startCursor"]}
-    response = staff_api_client.post_graphql(
-        QUERY_SALES_PAGINATION,
-        variables,
-        permissions=[permission_manage_discounts],
-        check_no_permissions=False,
-    )
-    content = get_graphql_content(response)
-    first_page_edges_backward = content["data"]["sales"]["edges"]
-
-    assert first_page_edges_forward == first_page_edges_backward
 
 
 @pytest.mark.parametrize(
