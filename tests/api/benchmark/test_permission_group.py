866bd215589f7ae44fe6657464943fc1f9eafdc0
IKarbowiak <iga.karbowiak@mirumee.com>
2020-04-06 09:59:59 +0200

    Add more tests for group mutations

--- tests/api/benchmark/test_permission_group.py
+++ tests/api/benchmark/test_permission_group.py
@@ -139,6 +139,81 @@ def test_permission_group_update(
 
 @pytest.mark.django_db
 @pytest.mark.count_queries(autouse=False)
+def test_permission_group_update_remove_users_with_manage_staff(
+    permission_group_manage_users,
+    staff_users,
+    permission_manage_staff,
+    staff_api_client,
+    permission_manage_service_accounts,
+    permission_manage_users,
+    permission_manage_orders,
+    count_queries,
+):
+    query = """
+    mutation PermissionGroupUpdate(
+        $id: ID!, $input: PermissionGroupUpdateInput!) {
+        permissionGroupUpdate(
+            id: $id, input: $input)
+        {
+            group{
+                id
+                name
+                permissions {
+                    name
+                    code
+                }
+                users {
+                    email
+                }
+            }
+            permissionGroupErrors{
+                field
+                code
+                permissions
+                users
+                message
+            }
+        }
+    }
+    """
+
+    staff_user, staff_user1, staff_user2 = staff_users
+
+    groups = Group.objects.bulk_create(
+        [Group(name="manage users"), Group(name="manage staff, order and users")]
+    )
+    group1, group2 = groups
+
+    group1.permissions.add(permission_manage_staff, permission_manage_users)
+    group2.permissions.add(
+        permission_manage_staff, permission_manage_orders, permission_manage_users
+    )
+
+    group1.user_set.add(staff_user1, staff_user2)
+    group2.user_set.add(staff_user2)
+
+    staff_user.user_permissions.add(permission_manage_users, permission_manage_orders)
+    variables = {
+        "id": graphene.Node.to_global_id("Group", group1.id),
+        "input": {
+            "removeUsers": [
+                graphene.Node.to_global_id("User", user.id)
+                for user in [staff_user1, staff_user2]
+            ],
+        },
+    }
+
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=(permission_manage_staff,)
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["permissionGroupUpdate"]
+
+    assert len(data["group"]["users"]) == 0
+
+
+@pytest.mark.django_db
+@pytest.mark.count_queries(autouse=False)
 def test_permission_group_delete(
     staff_users,
     permission_manage_staff,
