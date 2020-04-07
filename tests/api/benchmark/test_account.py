cb4bae167239a9c246308218ec07e1d033b46685
IKarbowiak <iga.karbowiak@mirumee.com>
2020-04-07 09:22:04 +0200

    Add benchamrk for update staff and update account tests

--- tests/api/benchmark/test_account.py
+++ tests/api/benchmark/test_account.py
@@ -2,8 +2,10 @@ from unittest.mock import MagicMock
 
 import graphene
 import pytest
+from django.contrib.auth.models import Group
 from django.core.files import File
 
+from saleor.graphql.core.enums import PermissionEnum
 from tests.api.utils import get_graphql_content
 
 
@@ -113,3 +115,99 @@ def test_query_staff_user(
     content = get_graphql_content(response)
     data = content["data"]["user"]
     assert data
+
+
+def test_staff_update_groups_and_permissions(
+    staff_api_client,
+    staff_users,
+    media_root,
+    permission_manage_staff,
+    permission_manage_users,
+    permission_manage_orders,
+    permission_manage_products,
+):
+    query = """
+    mutation UpdateStaff(
+                $id: ID!, $input: StaffUpdateInput!) {
+            staffUpdate(
+                    id: $id,
+                    input: $input) {
+                staffErrors {
+                    field
+                    code
+                    message
+                    permissions
+                    groups
+                }
+                user {
+                    userPermissions {
+                        code
+                    }
+                    permissions {
+                        code
+                    }
+                    permissionGroups {
+                        name
+                    }
+                    isActive
+                }
+            }
+        }
+    """
+    groups = Group.objects.bulk_create(
+        [
+            Group(name="manage users"),
+            Group(name="manage staff"),
+            Group(name="manage orders"),
+        ]
+    )
+    group1, group2, group3 = groups
+    group1.permissions.add(permission_manage_users)
+    group2.permissions.add(permission_manage_staff)
+    group3.permissions.add(permission_manage_orders)
+
+    staff_user, staff_user1, staff_user2 = staff_users
+    group1.user_set.add(staff_user1, staff_user2)
+    group2.user_set.add(staff_user2, staff_user1)
+    group3.user_set.add(staff_user2)
+
+    id = graphene.Node.to_global_id("User", staff_user1.id)
+    variables = {
+        "id": id,
+        "input": {
+            "addGroups": [
+                graphene.Node.to_global_id("Group", gr.pk) for gr in [group2, group3]
+            ],
+            "removeGroups": [graphene.Node.to_global_id("Group", group1.pk)],
+            "permissions": [PermissionEnum.MANAGE_PRODUCTS.name],
+        },
+    }
+
+    staff_api_client.user.user_permissions.add(
+        permission_manage_users, permission_manage_orders, permission_manage_products
+    )
+
+    response = staff_api_client.post_graphql(
+        query, variables, permissions=[permission_manage_staff]
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["staffUpdate"]
+    assert data["staffErrors"] == []
+    assert len(data["user"]["userPermissions"]) == 3
+    assert {perm["code"].lower() for perm in data["user"]["userPermissions"]} == {
+        permission_manage_orders.codename,
+        permission_manage_products.codename,
+        permission_manage_staff.codename,
+    }
+    assert len(data["user"]["permissionGroups"]) == 2
+    assert {group["name"] for group in data["user"]["permissionGroups"]} == {
+        group2.name,
+        group3.name,
+    }
+    # deprecated, to remove in #5389
+    assert len(data["user"]["permissions"]) == 3
+    assert {perm["code"].lower() for perm in data["user"]["permissions"]} == {
+        permission_manage_orders.codename,
+        permission_manage_products.codename,
+        permission_manage_staff.codename,
+    }
