b16d2b9730ea5e786d40999d00b874a0e694df3a
Gabriel L Martinez <g.lorenzo.martinez@gmail.com>
2020-04-09 15:04:09 -0400

    Update tests parse the message json as expected

--- tests/api/test_collection.py
+++ tests/api/test_collection.py
@@ -620,10 +620,11 @@ def test_collections_query_ids_not_exists(collection_filter, user_api_client, ca
     variables = {"filter": collection_filter}
     response = user_api_client.post_graphql(query, variables)
     content = get_graphql_content(response, ignore_errors=True)
-    message_error = "{'ids': [{'message': 'Invalid ID specified.', 'code': ''}]}"
 
     assert len(content["errors"]) == 1
-    assert content["errors"][0]["message"] == message_error
+
+    error = json.loads(content["errors"][0]["message"])
+    assert error["ids"][0]["message"] == "Invalid ID specified."
     assert content["data"]["collections"] is None
 
 
