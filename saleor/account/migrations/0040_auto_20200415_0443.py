158842a346a89f26430198d4f624b722cbccb275
Damian Wysocki <damian.wysocki@stxnext.pl>
2020-04-15 13:18:07 +0200

    fix getting permissions on empty database

--- saleor/account/migrations/0040_auto_20200415_0443.py
+++ saleor/account/migrations/0040_auto_20200415_0443.py
@@ -6,16 +6,21 @@ from django.db import migrations
 def change_extension_permission_to_plugin_permission(apps, schema_editor):
     permission = apps.get_model("auth", "Permission")
     users = apps.get_model("account", "User")
-    plugin_permission = permission.objects.get(
+
+    plugin_permission = permission.objects.filter(
         codename="manage_plugins", content_type__app_label="plugins"
-    )
-    extension_permission = permission.objects.get(
+    ).first()
+    extension_permission = permission.objects.filter(
         codename="manage_plugins", content_type__app_label="extensions"
-    )
+    ).first()
     users = users.objects.filter(
         user_permissions__content_type__app_label="extensions",
         user_permissions__codename="manage_plugins",
     )
+
+    if not plugin_permission or not extension_permission:
+        return
+
     for user in users:
         user.user_permissions.remove(extension_permission)
         user.user_permissions.add(plugin_permission)
@@ -25,6 +30,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("account", "0039_auto_20200221_0257"),
+        ("plugins", "0001_initial"),
     ]
 
     operations = [
