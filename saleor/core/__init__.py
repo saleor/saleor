5dcab49543098fcb88086809693108af7e6f608f
IKarbowiak <iga.karbowiak@mirumee.com>
2020-04-20 10:51:41 +0200

    Add job abstract model and interface

---
+++ saleor/core/__init__.py
@@ -0,0 +1,12 @@
+class JobStatus:
+    PENDING = "pending"
+    SUCCESS = "success"
+    FAILED = "failed"
+    DELETED = "deleted"
+
+    CHOICES = [
+        (PENDING, "Pending"),
+        (SUCCESS, "Success"),
+        (FAILED, "Failed"),
+        (DELETED, "Deleted"),
+    ]
