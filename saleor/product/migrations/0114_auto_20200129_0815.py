7e0164bd39d9317baf4e3351ed83906828ab2b40
Piotr Antosz <boquete37@gmail.com>
2020-05-06 21:53:11 +0200

    Black formatting

--- saleor/product/migrations/0114_auto_20200129_0815.py
+++ saleor/product/migrations/0114_auto_20200129_0815.py
@@ -17,9 +17,11 @@ def create_unique_slug_for_products(apps, schema_editor):
         first_char = product.name[0].lower()
         if first_char != previous_char:
             previous_char = first_char
-            slug_values = list(Product.objects.filter(
-                slug__istartswith=first_char
-            ).values_list("slug", flat=True))
+            slug_values = list(
+                Product.objects.filter(slug__istartswith=first_char).values_list(
+                    "slug", flat=True
+                )
+            )
 
         slug = generate_unique_slug(product, slug_values)
         product.slug = slug
