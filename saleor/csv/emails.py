8db6b5b9607ce65d0fcde9a9a807d6a1b87e04b2
IKarbowiak <iga.karbowiak@mirumee.com>
2020-05-07 11:56:50 +0200

    Add event to sending email with link to download csv file

--- saleor/csv/emails.py
+++ saleor/csv/emails.py
@@ -5,6 +5,7 @@ from templated_email import send_templated_mail
 from ..celeryconf import app
 from ..core.emails import get_email_context
 from ..core.utils import build_absolute_uri
+from . import events
 
 if TYPE_CHECKING:
     from .models import ExportFile
@@ -14,13 +15,14 @@ EXPORT_TEMPLATES = {"export_products": "csv/export_products_csv"}
 
 
 @app.task
-def send_email_with_link_to_download_csv(job: "ExportFile", template_name: str):
-    recipient_email = job.created_by.email
+def send_email_with_link_to_download_csv(export_file: "ExportFile", template_name: str):
+    recipient_email = export_file.created_by.email
     send_kwargs, ctx = get_email_context()
-    ctx["csv_link"] = build_absolute_uri(job.content_file.url)
+    ctx["csv_link"] = build_absolute_uri(export_file.content_file.url)
     send_templated_mail(
         template_name=EXPORT_TEMPLATES[template_name],
         recipient_list=[recipient_email],
         context=ctx,
         **send_kwargs,
     )
+    events.export_file_sent_event(export_file=export_file, user=export_file.created_by)
