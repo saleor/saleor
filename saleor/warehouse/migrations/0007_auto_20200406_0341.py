a2d5871826d57891255e4b810e4fadca88694650
IKarbowiak <iga.karbowiak@mirumee.com>
2020-05-08 09:25:35 +0200

    Handle warehouse without shipping zone in migrations

--- saleor/warehouse/migrations/0007_auto_20200406_0341.py
+++ saleor/warehouse/migrations/0007_auto_20200406_0341.py
@@ -18,7 +18,10 @@ def create_allocations(apps, schema_editor):
     OrderLine = apps.get_model("order", "OrderLine")
     Warehouse = apps.get_model("warehouse", "Warehouse")
     for warehouse in Warehouse.objects.iterator():
-        shipping_zone_pk = warehouse.shipping_zones.first().pk
+        shipping_zone = warehouse.shipping_zones.first()
+        if not shipping_zone:
+            continue
+        shipping_zone_pk = shipping_zone.pk
         for order_line in OrderLine.objects.filter(
             order__shipping_method__shipping_zone__pk=shipping_zone_pk,
         ).iterator():
