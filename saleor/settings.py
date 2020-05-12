1870d89f41b4975f6b200bf7de526a5257cf6a2a
Marcin Gębala <maarcin.gebala@gmail.com>
2020-05-12 15:37:52 +0200

    Revert cache settings

--- saleor/settings.py
+++ saleor/settings.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 
 import dj_database_url
 import dj_email_url
+import django_cache_url
 import jaeger_client
 import jaeger_client.config
 import sentry_sdk
@@ -546,3 +547,10 @@ if "JAEGER_AGENT_HOST" in os.environ:
         service_name="saleor",
         validate=True,
     ).initialize_tracer()
+
+
+# Some cloud providers (Heroku) export REDIS_URL variable instead of CACHE_URL
+REDIS_URL = os.environ.get("REDIS_URL")
+if REDIS_URL:
+    CACHE_URL = os.environ.setdefault("CACHE_URL", REDIS_URL)
+CACHES = {"default": django_cache_url.config()}
