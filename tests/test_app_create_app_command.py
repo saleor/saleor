2e37fd459e3b9a5d5608c05d41d59121b78c5883
Maciej Korycinski <maciej@mirumee.com>
2020-05-12 11:13:48 +0200

    Add tests for create_app command

--- tests/test_app_create_app_command.py
+++ tests/test_app_create_app_command.py
@@ -1,12 +1,15 @@
 from unittest.mock import ANY, Mock
 
+import pytest
 import requests
 from django.core.management import call_command
 
-from saleor.app.models import App
+from saleor.app.models import App, AppJob
+from saleor.core import JobStatus
 from saleor.core.permissions import get_permissions
 
 
+@pytest.mark.vcr
 def test_creates_app_object():
     name = "Single App"
     manifest_url = "http://localhost:3000/manifest"
@@ -19,13 +22,34 @@ def test_creates_app_object():
     app = apps[0]
     tokens = app.tokens.all()
     assert len(tokens) == 1
+    assert not app.is_active
 
 
+@pytest.mark.vcr
+def test_activate_app():
+    name = "Single App"
+    manifest_url = "http://localhost:3000/manifest"
+    permissions = ["account.manage_users", "order.manage_orders"]
+    call_command(
+        "create_app", name, manifest_url, permission=permissions, activate=True
+    )
+
+    apps = App.objects.filter(name=name)
+    assert len(apps) == 1
+
+    app = apps[0]
+    tokens = app.tokens.all()
+    assert len(tokens) == 1
+    assert app.is_active
+
+
+@pytest.mark.vcr
 def test_app_has_all_required_permissions():
     name = "SA name"
+    manifest_url = "http://localhost:3000/manifest"
     permission_list = ["account.manage_users", "order.manage_orders"]
     expected_permission = get_permissions(permission_list)
-    call_command("create_app", name, permission=permission_list)
+    call_command("create_app", name, manifest_url, permission=permission_list)
 
     apps = App.objects.filter(name=name)
     assert len(apps) == 1
@@ -33,26 +57,63 @@ def test_app_has_all_required_permissions():
     assert set(app.permissions.all()) == set(expected_permission)
 
 
-def test_sends_data_to_target_url(monkeypatch):
+@pytest.mark.vcr
+def test_create_app_sends_token(monkeypatch):
     mocked_response = Mock()
     mocked_response.status_code = 200
     mocked_post = Mock(return_value=mocked_response)
 
     monkeypatch.setattr(requests, "post", mocked_post)
-
     name = "Single App"
-    target_url = "https://ss.shop.com/register"
+    manifest_url = "http://localhost:3000/manifest"
     permissions = [
         "account.manage_users",
     ]
 
-    call_command("create_app", name, permission=permissions, target_url=target_url)
+    call_command("create_app", name, manifest_url, permission=permissions)
 
     app = App.objects.filter(name=name)[0]
     token = app.tokens.all()[0].auth_token
     mocked_post.assert_called_once_with(
-        target_url,
-        headers={"x-saleor-domain": "mirumee.com"},
+        "http://localhost:3000/register",
+        headers={"Content-Type": "application/json", "x-saleor-domain": "mirumee.com"},
         json={"auth_token": token},
         timeout=ANY,
     )
+
+
+def test_schedule_installation_as_a_celery_task(monkeypatch):
+    mocked_install_task = Mock()
+    mocked_install = Mock()
+    monkeypatch.setattr(
+        "saleor.app.management.commands.create_app.install_app_task.delay",
+        mocked_install_task,
+    )
+    monkeypatch.setattr(
+        "saleor.app.management.commands.create_app.install_app", mocked_install
+    )
+
+    name = "Single App"
+    manifest_url = "http://localhost:3000/manifest"
+    permissions = ["account.manage_users", "order.manage_orders"]
+
+    call_command(
+        "create_app", name, manifest_url, permission=permissions, use_celery=True
+    )
+
+    app_job = AppJob.objects.get()
+    assert not mocked_install.called
+    mocked_install_task.assert_called_once_with(app_job.pk, False)
+
+
+@pytest.mark.vcr
+def test_installation_failed():
+    name = "Single App"
+    manifest_url = "http://localhost:3000/manifest"
+    permissions = ["account.manage_users", "order.manage_orders"]
+
+    with pytest.raises(Exception):
+        call_command("create_app", name, manifest_url, permission=permissions)
+
+    app_job = AppJob.objects.get()
+    assert app_job.status == JobStatus.FAILED
