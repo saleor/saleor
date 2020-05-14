885b385880e7ebfad694b7a5bc4811b1f2f3cd91
Maciej Korycinski <maciej@mirumee.com>
2020-05-14 12:52:48 +0200

    Drop saleor logo from emails

--- saleor/core/emails.py
+++ saleor/core/emails.py
@@ -1,18 +1,13 @@
 from urllib.parse import urlsplit
 
 from django.contrib.sites.models import Site
-from django.templatetags.static import static
-
-from ..core.utils import build_absolute_uri
 
 
 def get_email_context():
     site: Site = Site.objects.get_current()
-    logo_url = build_absolute_uri(static("images/logo-light.svg"))
     send_email_kwargs = {"from_email": site.settings.default_from_email}
     email_template_context = {
         "domain": site.domain,
-        "logo_url": logo_url,
         "site_name": site.name,
     }
     return send_email_kwargs, email_template_context
