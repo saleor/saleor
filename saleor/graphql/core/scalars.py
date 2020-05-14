7e98f3736d5c1e24d1db8015f1a2eb575e75e1fb
Michał Sipa <michal.sipa@mirumee.com>
2020-05-14 12:07:36 +0200

    Add our implementation of UUID Scalar

--- saleor/graphql/core/scalars.py
+++ saleor/graphql/core/scalars.py
@@ -88,3 +88,23 @@ class WeightScalar(graphene.Scalar):
             if field.name.value == "unit":
                 unit = field.value.value
         return Weight(**{unit: value})
+
+
+class UUID(graphene.UUID):
+    @staticmethod
+    def serialize(uuid):
+        return super(UUID, UUID).serialize(uuid)
+
+    @staticmethod
+    def parse_literal(node):
+        try:
+            return super(UUID, UUID).parse_literal(node)
+        except ValueError:
+            raise GraphQLError(f"Unsuported value: {node.value}")
+
+    @staticmethod
+    def parse_value(value):
+        try:
+            return super(UUID, UUID).parse_value(value)
+        except ValueError:
+            raise GraphQLError(f"Unsuported value: {value}")
