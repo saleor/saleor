8f68eefaf673415b7527804c54e2d9e158da2595
Maciej Korycinski <maciej@mirumee.com>
2020-05-14 15:05:43 +0200

    separate command for installation of app from manifest and by arguments

---
+++ saleor/app/management/commands/utils.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from django.core.management import CommandError
+
+from saleor.core.permissions import get_permissions, get_permissions_enum_list
+
+
+def clean_permissions(required_permissions: List[str]):
+    all_permissions = {perm[0]: perm[1] for perm in get_permissions_enum_list()}
+    for perm in required_permissions:
+        if not all_permissions.get(perm):
+            raise CommandError(
+                f"Permisssion: {perm} doesn't exist in Saleor."
+                f" Avaiable permissions: {all_permissions}"
+            )
+    permissions = get_permissions(
+        [all_permissions[perm] for perm in required_permissions]
+    )
+    return permissions
