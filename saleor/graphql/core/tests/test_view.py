1c274cb00d448db843b00dfbb7ef9bcca8e9e071
fowczrek <filip.owczarek@defectprediction.net>
2020-05-28 13:18:36 +0200

    Move fixtures

--- saleor/graphql/core/tests/test_view.py
+++ saleor/graphql/core/tests/test_view.py
@@ -6,7 +6,7 @@ from django.test import override_settings
 
 from saleor.demo.views import EXAMPLE_QUERY
 from saleor.graphql.product.types import Product
-from tests.api.fixtures import API_PATH
+from saleor.graphql.tests.fixtures import API_PATH
 from tests.api.utils import _get_graphql_content_from_response, get_graphql_content
 
 
