99f1a2a0956926b45fc85299a66b3dae84881684
Maciej Korycinski <maciej@mirumee.com>
2020-06-03 12:49:42 +0200

    Move app_token_verify from product dir to app

--- saleor/graphql/app/tests/test_app_token_verify_mutation.py
+++ saleor/graphql/app/tests/test_app_token_verify_mutation.py
@@ -1,4 +1,4 @@
-from tests.api.utils import get_graphql_content
+from saleor.graphql.tests.utils import get_graphql_content
 
 APP_TOKEN_VERIFY_MUTATION = """
 mutation AppTokenVerify($token: String!){
