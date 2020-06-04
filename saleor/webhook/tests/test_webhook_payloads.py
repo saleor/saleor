9e175236cf14cfffd7b32ab42af3c2dd72a4652a
fowczrek <filip.owczarek@defectprediction.net>
2020-06-04 13:26:22 +0200

    Refactor import in webhook tests

--- saleor/webhook/tests/test_webhook_payloads.py
+++ saleor/webhook/tests/test_webhook_payloads.py
@@ -4,9 +4,9 @@ import json
 import graphene
 import pytest
 
-from saleor.order import OrderStatus
-from saleor.webhook.event_types import WebhookEventType
-from saleor.webhook.payloads import (
+from ...order import OrderStatus
+from ..event_types import WebhookEventType
+from ..payloads import (
     generate_checkout_payload,
     generate_fulfillment_payload,
     generate_order_payload,
