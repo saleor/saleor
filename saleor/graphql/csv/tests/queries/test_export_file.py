1ebc32eb6aea47593c0dbbac7bca40f7e1b68187
IKarbowiak <iga.karbowiak@mirumee.com>
2020-06-10 09:00:10 +0200

    Add test for query export file

--- saleor/graphql/csv/tests/queries/test_export_file.py
+++ saleor/graphql/csv/tests/queries/test_export_file.py
@@ -71,6 +71,48 @@ def test_query_export_file(
     assert event["app"] is None
 
 
+def test_query_export_file_by_app(
+    app_api_client,
+    user_export_file,
+    permission_manage_products,
+    permission_manage_apps,
+    permission_manage_users,
+    user_export_event,
+):
+    # given
+    query = EXPORT_FILE_QUERY
+    variables = {"id": graphene.Node.to_global_id("ExportFile", user_export_file.pk)}
+
+    # when
+    response = app_api_client.post_graphql(
+        query,
+        variables=variables,
+        permissions=[
+            permission_manage_products,
+            permission_manage_apps,
+            permission_manage_users,
+        ],
+    )
+
+    # then
+    content = get_graphql_content(response)
+    data = content["data"]["exportFile"]
+
+    assert data["status"] == JobStatus.PENDING.upper()
+    assert data["createdAt"]
+    assert data["updatedAt"]
+    assert data["app"] is None
+    assert not data["url"]
+    assert data["user"]["email"] == user_export_file.user.email
+    assert len(data["events"]) == 1
+    event = data["events"][0]
+    assert event["date"]
+    assert event["message"] == user_export_event.parameters.get("message")
+    assert event["type"] == ExportEvents.EXPORT_FAILED.upper()
+    assert event["user"]["email"] == user_export_event.user.email
+    assert event["app"] is None
+
+
 def test_query_export_file_export_file_with_app(
     app,
     staff_api_client,
