217531363f8cb78d64dc190d0b61d4c55609fbb3
IKarbowiak <iga.karbowiak@mirumee.com>
2020-06-16 15:16:55 +0200

    Drop print from get_supported_currencies

--- saleor/payment/gateways/utils.py
+++ saleor/payment/gateways/utils.py
@@ -9,7 +9,6 @@ if TYPE_CHECKING:
 
 def get_supported_currencies(config: "GatewayConfig", gateway_name: str) -> List[str]:
     currencies = config.supported_currencies
-    print(config)
     if not currencies:
         currencies = [settings.DEFAULT_CURRENCY]
         warnings.warn(f"Default currency for {gateway_name} is used.")
