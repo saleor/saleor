7eef4539cc5aaf57ca211b0f056681532f0b178d
fowczrek <filip.owczarek@defectprediction.net>
2020-06-19 12:40:01 +0200

    Add chennel queries

--- saleor/graphql/channel/tests/fixtures.py
+++ saleor/graphql/channel/tests/fixtures.py
@@ -6,3 +6,8 @@ from ....channel.models import Channel
 @pytest.fixture
 def channel(db):
     return Channel.objects.create(name="Main Channel", slug="main", currency_code="USD")
+
+
+@pytest.fixture
+def channel_PLN(db):
+    return Channel.objects.create(name="Channel PLN", slug="c-pln", currency_code="PLN")
