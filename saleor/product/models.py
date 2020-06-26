dbb23440d6c200f942096157691f59edbff709cb
Damian W <damian.wysocki@mirumee.com>
2020-06-26 13:25:25 +0200

    Show only product with variant for users

--- saleor/product/models.py
+++ saleor/product/models.py
@@ -123,15 +123,6 @@ class ProductsQueryset(PublishedQuerySet):
         )
         return qs
 
-    def published_with_variants(self):
-        published = self.published()
-        return published.filter(variants__isnull=False)
-
-    def visible_to_user(self, user):
-        if self.user_has_access_to_all(user):
-            return self.all()
-        return self.published_with_variants()
-
     def sort_by_attribute(
         self, attribute_pk: Union[int, str], descending: bool = False
     ):
