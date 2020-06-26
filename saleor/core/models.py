a1d8dfcc16848fbc5aa26885d439d958a5a88df0
Damian W <damian.wysocki@mirumee.com>
2020-06-26 13:25:25 +0200

    Show only product with variant for users

--- saleor/core/models.py
+++ saleor/core/models.py
@@ -48,6 +48,10 @@ class PublishedQuerySet(models.QuerySet):
             is_published=True,
         )
 
+    def published_with_variants(self):
+        published = self.published()
+        return published.filter(variants__isnull=False)
+
     @staticmethod
     def user_has_access_to_all(user):
         return user.is_active and user.has_perm(ProductPermissions.MANAGE_PRODUCTS)
@@ -55,7 +59,7 @@ class PublishedQuerySet(models.QuerySet):
     def visible_to_user(self, user):
         if self.user_has_access_to_all(user):
             return self.all()
-        return self.published()
+        return self.published_with_variants()
 
 
 class PublishableModel(models.Model):
