4ef6e0e45325432d563d92d0231516917650486a
Patryk Pazura <patryk.pazura@gmail.com>
2020-06-29 21:37:28 +0200

    refs #5081 implemented for users and products

--- saleor/graphql/metaquery/filters.py
+++ saleor/graphql/metaquery/filters.py
@@ -2,9 +2,8 @@ import django_filters
 
 from saleor.graphql.core.filters import ObjectTypeFilter
 from saleor.graphql.core.types import FilterInputObjectType
-from saleor.product.models import ProductType
+from saleor.product.models import ProductType, Product
 from ..meta.mutations import MetadataInput
-
 
 def filter_product_type_metadata(qs, key, value):
     if not key or not value:
@@ -42,3 +41,34 @@ class ProductTypeMetadataFilter(django_filters.FilterSet):
 class ProductTypeMetadataFilterInput(FilterInputObjectType):
     class Meta:
         filterset_class = ProductTypeMetadataFilter
+
+
+def filter_product_metadata(qs, key, value):
+    if not key or not value:
+        return qs
+
+    json_dict = {
+        value.key: value.value
+    }
+    if key == 'private_metadata':
+        qs = Product.objects.filter(private_metadata__contains=json_dict)
+    elif key == 'metadata':
+        qs = Product.objects.filter(metadata__contains=json_dict)
+
+    return qs
+
+class ProductMetadataFilter(django_filters.FilterSet):
+
+    private_metadata = ObjectTypeFilter(input_class=MetadataInput,
+                                        method=filter_product_metadata)
+    metadata = ObjectTypeFilter(input_class=MetadataInput,
+                                method=filter_product_metadata)
+
+    class Meta:
+        model = Product
+        fields = ["private_metadata", "metadata"]
+
+
+class ProductMetadataFilterInput(FilterInputObjectType):
+    class Meta:
+        filterset_class = ProductMetadataFilter
--- saleor/graphql/metaquery/resolvers.py
+++ saleor/graphql/metaquery/resolvers.py
@@ -1,4 +1,5 @@
-from saleor.product.models import ProductType
+from saleor.account.models import User
+from saleor.product.models import ProductType, Product
 
 
 def resolve_product_type_by_metadata(privateMetadataKey=None, metadataKey=None,
@@ -14,3 +15,33 @@ def resolve_product_type_by_metadata(privateMetadataKey=None, metadataKey=None,
         qs = ProductType.objects.none()
 
     return qs.first()
+
+
+def resolve_user_by_metadata(privateMetadataKey=None, metadataKey=None,
+                                     privateMetadataValue=None, metadataValue=None):
+    json_dict = {}
+    if privateMetadataKey and privateMetadataValue:
+        json_dict[privateMetadataKey] = privateMetadataValue
+        qs = User.objects.filter(private_metadata__contains=json_dict)
+    elif metadataKey and metadataValue:
+        json_dict[metadataKey] = metadataValue
+        qs = User.objects.filter(metadata__contains=json_dict)
+    else:
+        qs = User.objects.none()
+
+    return qs.first()
+
+def resolve_product_by_metadata(privateMetadataKey=None, metadataKey=None,
+                                     privateMetadataValue=None, metadataValue=None):
+    json_dict = {}
+    if privateMetadataKey and privateMetadataValue:
+        json_dict[privateMetadataKey] = privateMetadataValue
+        qs = Product.objects.filter(private_metadata__contains=json_dict)
+    elif metadataKey and metadataValue:
+        json_dict[metadataKey] = metadataValue
+        qs = Product.objects.filter(metadata__contains=json_dict)
+    else:
+        qs = Product.objects.none()
+
+    return qs.first()
+
