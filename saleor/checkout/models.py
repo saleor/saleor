b20b6fc9e2efa44c252415a4faaa6670d6f5afe8
IKarbowiak <iga.karbowiak@mirumee.com>
2020-07-03 07:53:39 +0200

    Revert changes from checkout models and utils

--- saleor/checkout/models.py
+++ saleor/checkout/models.py
@@ -1,4 +1,5 @@
 """Checkout-related ORM models."""
+from operator import attrgetter
 from typing import TYPE_CHECKING, Optional
 from uuid import uuid4
 
@@ -138,7 +139,8 @@ class Checkout(ModelWithMetadata):
         return next(matching_lines, None)
 
     def get_last_active_payment(self) -> Optional["Payment"]:
-        return self.payments.filter(is_active=True).last()
+        payments = [payment for payment in self.payments.all() if payment.is_active]
+        return max(payments, default=None, key=attrgetter("pk"))
 
     def set_country(
         self, country_code: str, commit: bool = False, replace: bool = True
