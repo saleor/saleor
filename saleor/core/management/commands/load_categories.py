270527ea22b0643dcb15ce24b8d2f1b99fc57f22
tgarbowski <tomasz.garbowski@gammasoft.pl>
2020-07-07 12:26:17 +0200

    load categories WIP

--- saleor/core/management/commands/load_categories.py
+++ saleor/core/management/commands/load_categories.py
@@ -8,15 +8,15 @@ from django.utils.text import slugify
 
 class Command(BaseCommand):
 
-    FILE_PATH = 'filepath'
-
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str, help='category file path')
 
     def handle(self, *args, **options):
 
-        self.load_categories()
+        self.load_categories(options['path'])
 
-    def load_categories(self):
-        with open(self.FILE_PATH, encoding="utf8", errors='ignore') as csvfile:
+    def load_categories(self, path):
+        with open(path, encoding="utf8", errors='ignore') as csvfile:
             reader = csv.reader(csvfile, delimiter=';')
             for rid, row in enumerate(reader):
                 for elId, el in enumerate(row):
@@ -31,22 +31,22 @@ class Command(BaseCommand):
                     if(elId == 1 and el != ''):
 
                         try:
-                            category = Category.objects.get(name=row[1])
+                            category = Category.objects.get(name=row[1], parent=Category.objects.get(name=row[0]))
                             print('Kategoria istnieje: ', category)
                         except Category.DoesNotExist:
-                            Category.objects.create(name=row[1], slug=slugify(row[1]), parent=Category.objects.get(name=row[0]))
+                            Category.objects.create(name=row[1], slug=slugify(row[0] + " " + row[1]), parent=Category.objects.get(name=row[0]))
 
                     if (elId == 2 and el != ''):
                         try:
-                            category = Category.objects.get(name=row[2])
+                            category = Category.objects.get(name=row[2], parent=Category.objects.get(name=row[1]))
                             print('Kategoria istnieje: ', category)
                         except Category.DoesNotExist:
-                            Category.objects.create(name=row[2], slug=slugify(row[2]), parent=Category.objects.get(name=row[1]))
+                            Category.objects.create(name=row[2], slug=slugify(row[1] + " " + row[2]), parent=Category.objects.get(name=row[1]))
 
                     if (elId == 3 and el != ''):
                         try:
-                            category = Category.objects.get(name=row[3])
+                            category = Category.objects.get(name=row[3], parent=Category.objects.get(name=row[2]))
                             print('Kategoria istnieje: ', category)
                         except Category.DoesNotExist:
-                            Category.objects.create(name=row[3], slug=slugify(row[3]), parent=Category.objects.get(name=row[2]))
+                            Category.objects.create(name=row[3], slug=slugify(row[2] + " " + row[3]), parent=Category.objects.get(name=row[2]))
 
--- saleor/core/management/commands/load_product_types.py
+++ saleor/core/management/commands/load_product_types.py
@@ -8,30 +8,26 @@ from django.utils.text import slugify
 
 class Command(BaseCommand):
 
-    FILE_PATH = 'filepath'
-
+    def add_arguments(self, parser):
+        parser.add_argument('path', type=str, help='category file path')
 
     def handle(self, *args, **options):
 
-        self.load_product_types()
+        self.load_product_types(options['path'])
 
-
-
-    def load_product_types(self):
-        with open(self.FILE_PATH, encoding="utf8", errors='ignore') as csvfile:
+    def load_product_types(self, path):
+        with open(path, encoding="utf8", errors='ignore') as csvfile:
             reader = csv.reader(csvfile, delimiter=';')
             for rid, row in enumerate(reader):
 
                 try:
-                    productType = ProductType.objects.get(name=row[4])
+                    categoryPath = '/' + slugify(row[0]) + '/' + slugify(
+                        row[1]) + '/' + slugify(row[2])
+                    metadata = {'categoryPath': categoryPath, 'description': row[5]}
+                    productType = ProductType.objects.get(
+                        name=row[4], metadata=metadata)
+
                 except ProductType.DoesNotExist:
 
-
-                    categoryPath = slugify(row[0]) + '/' + slugify(row[1]) + '/' + slugify(row[2])
-
-                    metadata = {'categoryPath': categoryPath, 'description': row[5]}
-
-
-
-                    ProductType.objects.create(name=row[4], slug=slugify(row[4]), metadata=metadata)
-
+                    ProductType.objects.create(name=row[4], slug=slugify(row[1] + " " + row[2] + " " + row[4]),
+                                               metadata=metadata)
