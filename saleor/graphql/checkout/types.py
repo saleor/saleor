be57032dc8a84ae3a2b043cd9e2dcf77abb26c44
Maciej Korycinski <maciej@mirumee.com>
2020-07-15 10:49:09 +0200

    Use generic scalara instead of string

--- saleor/graphql/checkout/types.py
+++ saleor/graphql/checkout/types.py
@@ -1,4 +1,5 @@
 import graphene
+from graphene.types.generic import GenericScalar
 from promise import Promise
 
 from ...checkout import calculations, models
@@ -21,7 +22,7 @@ from .dataloaders import CheckoutLinesByCheckoutTokenLoader
 
 class GatewayConfigLine(graphene.ObjectType):
     field = graphene.String(required=True, description="Gateway config key.")
-    value = graphene.String(description="Gateway config value for key.")
+    value = GenericScalar(description="Gateway config value for key.")
 
     class Meta:
         description = "Payment gateway client configuration key and value pair."
