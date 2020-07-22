bcc1ae962815b8e0488a4e0c87357c35703d0603
IKarbowiak <iga.karbowiak@mirumee.com>
2020-07-22 15:14:47 +0200

    Drop extraData and creditCard fields from Payment and gatewayResponse from Transaction

--- saleor/graphql/payment/tests/test_payment.py
+++ saleor/graphql/payment/tests/test_payment.py
@@ -667,13 +667,6 @@ def test_payments_query(
                             amount
                         }
                     }
-                    creditCard {
-                        expMonth
-                        expYear
-                        brand
-                        firstDigits
-                        lastDigits
-                    }
                 }
             }
         }
@@ -713,13 +706,6 @@ def test_payments_query(
     assert data["transactions"] == [
         {"amount": {"currency": pay.currency, "amount": float(str(txn.amount))}}
     ]
-    assert data["creditCard"] == {
-        "expMonth": pay.cc_exp_month,
-        "expYear": pay.cc_exp_year,
-        "brand": pay.cc_brand,
-        "firstDigits": pay.cc_first_digits,
-        "lastDigits": pay.cc_last_digits,
-    }
 
 
 def test_query_payment(payment_dummy, user_api_client, permission_manage_orders):
