0ad42314ce02b39460b278b2e9159c6ba29bf0e5
Damian W <damian.wysocki@mirumee.com>
2020-07-27 14:32:05 +0200

    Remove ununsed error code

--- saleor/checkout/error_codes.py
+++ saleor/checkout/error_codes.py
@@ -21,4 +21,3 @@ class CheckoutErrorCode(Enum):
     UNIQUE = "unique"
     VOUCHER_NOT_APPLICABLE = "voucher_not_applicable"
     ZERO_QUANTITY = "zero_quantity"
-    MISSING_CHANNEL_SLUG = "missing_channel_slug"
