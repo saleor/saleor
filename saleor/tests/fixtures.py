dc13c4b0a751deed9022f3bf752d90a14c96bc3c
IKarbowiak <iga.karbowiak@mirumee.com>
2020-07-31 15:43:23 +0200

    Add test for request_data_for_payment in adyen

--- saleor/tests/fixtures.py
+++ saleor/tests/fixtures.py
@@ -1572,7 +1572,7 @@ def dummy_gateway_config():
 @pytest.fixture
 def dummy_payment_data(payment_dummy):
     return PaymentData(
-        amount=10,
+        amount=Decimal(10),
         currency="USD",
         payment_id=graphene.Node.to_global_id("Payment", payment_dummy.pk),
         billing=None,
