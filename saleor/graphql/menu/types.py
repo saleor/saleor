67c9eefa587eb1bf820fa76498545e5675729b19
IKarbowiak <iga.karbowiak@mirumee.com>
2020-08-18 10:38:24 +0200

    Update sort order descriptions in reorder inputs

--- saleor/graphql/menu/types.py
+++ saleor/graphql/menu/types.py
@@ -96,5 +96,9 @@ class MenuItemMoveInput(graphene.InputObjectType):
         description="ID of the parent menu. If empty, menu will be top level menu."
     )
     sort_order = graphene.Int(
-        description="The new relative sorting position of the item (from -inf to +inf)."
+        description=(
+            "The new relative sorting position of the item (from -inf to +inf). "
+            "1 moves the item one position forward, -1 moves the item one position "
+            "backward, 0 leaves the item unchanged."
+        )
     )
