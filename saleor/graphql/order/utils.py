e4562e86f3458aa0debf3690a05d0542a02b5c1e
IKarbowiak <iga.karbowiak@mirumee.com>
2020-08-19 08:19:30 +0200

    Add available for purchase validation for validate_draft_order method

--- saleor/graphql/order/utils.py
+++ saleor/graphql/order/utils.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.core.exceptions import ValidationError
 
 from ...core.exceptions import InsufficientStock
@@ -74,6 +76,22 @@ def validate_product_is_published(order):
             )
 
 
+def validate_product_is_available_for_purchase(order):
+    for line in order:
+        if (
+            not line.variant.product.available_for_purchase
+            or datetime.date.today() < line.variant.product.available_for_purchase
+        ):
+            raise ValidationError(
+                {
+                    "lines": ValidationError(
+                        "Can't finalize draft with product unavailable for purchase.",
+                        code=OrderErrorCode.PRODUCT_UNAVAILABLE_FOR_PURCHASE,
+                    )
+                }
+            )
+
+
 def validate_draft_order(order, country):
     """Check if the given order contains the proper data.
 
@@ -90,3 +108,4 @@ def validate_draft_order(order, country):
     validate_total_quantity(order)
     validate_order_lines(order, country)
     validate_product_is_published(order)
+    validate_product_is_available_for_purchase(order)
