2a1a1e8717e86d19a971e4768bd35861c67458dd
IKarbowiak <iga.karbowiak@mirumee.com>
2020-08-20 10:36:44 +0200

    Allow to export available_for_purchase product field

--- saleor/csv/utils/products_data.py
+++ saleor/csv/utils/products_data.py
@@ -24,6 +24,7 @@ class ProductExportFields:
             "name": "name",
             "description": "description",
             "visible": "is_published",
+            "available for purchase": "available_for_purchase",
             "category": "category__slug",
             "product type": "product_type__name",
             "charge taxes": "charge_taxes",
