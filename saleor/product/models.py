b5544bc5101b6dd04a2c14bbaed43248442ba5bb
IKarbowiak <iga.karbowiak@mirumee.com>
2020-08-21 10:28:10 +0200

    Add visible_in_listings product field

--- saleor/product/models.py
+++ saleor/product/models.py
@@ -264,6 +264,7 @@ class Product(SeoModel, ModelWithMetadata, PublishableModel):
     weight = MeasurementField(
         measurement=Weight, unit_choices=WeightUnits.CHOICES, blank=True, null=True
     )
+    visible_in_listings = models.BooleanField(default=False)
     objects = ProductsQueryset.as_manager()
     translated = TranslationProxy()
 
