0c329a497ed12a76f5939162a2e9c672ad084138
IKarbowiak <iga.karbowiak@mirumee.com>
2020-10-15 14:47:48 +0200

    Add benchmark test for page type query

---
+++ saleor/graphql/page/tests/benchmark/test_page_type.py
@@ -0,0 +1,66 @@
+import graphene
+import pytest
+
+from ....tests.utils import get_graphql_content
+
+
+@pytest.mark.django_db
+@pytest.mark.count_queries(autouse=True)
+def test_query_page_type(
+    page_type,
+    staff_api_client,
+    author_page_attribute,
+    permission_manage_pages,
+    permission_manage_products,
+    count_queries,
+):
+    query = """
+        query PageType($id: ID!, $filters: AttributeFilterInput) {
+            pageType(id: $id) {
+                id
+                name
+                slug
+                attributes {
+                    slug
+                    name
+                    type
+                    inputType
+                    values {
+                        name
+                        slug
+                        type
+                    }
+                    valueRequired
+                    visibleInStorefront
+                    filterableInStorefront
+                }
+                availableAttributes(first: 10, filter: $filters) {
+                    edges {
+                        node {
+                            slug
+                            name
+                            type
+                            inputType
+                            values {
+                                name
+                                slug
+                                type
+                            }
+                            valueRequired
+                            visibleInStorefront
+                            filterableInStorefront
+                        }
+                    }
+                }
+            }
+        }
+    """
+    variables = {"id": graphene.Node.to_global_id("PageType", page_type.pk)}
+    response = staff_api_client.post_graphql(
+        query,
+        variables,
+        permissions=[permission_manage_products, permission_manage_pages],
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["pageType"]
+    assert data
