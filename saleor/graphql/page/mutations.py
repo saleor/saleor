korycins/p-saleor

Add PageTypeUpdate mutation
Not finished yet - tests are failing

saleor/graphql/page/mutations.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import graphene
 from django.core.exceptions import ValidationError
 
@@ -7,7 +9,11 @@ from ...page.error_codes import PageErrorCode
 from ...product import AttributeType
 from ..core.mutations import ModelDeleteMutation, ModelMutation
 from ..core.types.common import PageError, SeoInput
-from ..core.utils import clean_seo_fields, validate_slug_and_generate_if_needed
+from ..core.utils import (
+    clean_seo_fields,
+    get_duplicates_ids,
+    validate_slug_and_generate_if_needed,
+)
 
 
 class PageInput(graphene.InputObjectType):
@@ -80,7 +86,7 @@ class PageDelete(ModelDeleteMutation):
         error_type_field = "page_errors"
 
 
-class PageTypeInput(graphene.InputObjectType):
+class PageTypeCreateInput(graphene.InputObjectType):
     name = graphene.String(description="Name of the page type.")
     slug = graphene.String(description="Page type slug.")
     add_attributes = graphene.List(
@@ -89,9 +95,38 @@ class PageTypeInput(graphene.InputObjectType):
     )
 
 
-class PageTypeCreate(ModelMutation):
+class PageTypeUpdateInput(PageTypeCreateInput):
+    remove_attributes = graphene.List(
+        graphene.NonNull(graphene.ID),
+        description="List of attribute IDs to be assigned to the page type.",
+    )
+
+
+class PageTypeMixin:
+    @classmethod
+    def validate_attributes(cls, errors: dict, attributes, field):
+        """All attributes must be page type attribute.
+
+        Raise an error if any of the attributes are not page attribute.
+        """
+        if attributes:
+            not_valid_attributes = [
+                graphene.Node.to_global_id("Attribute", attr.pk)
+                for attr in attributes
+                if attr.type != AttributeType.PAGE_TYPE
+            ]
+            if not_valid_attributes:
+                error = ValidationError(
+                    "Only page type attributes allowed.",
+                    code=PageErrorCode.INVALID.value,
+                    params={"attributes": not_valid_attributes},
+                )
+                errors[field].append(error)
+
+
+class PageTypeCreate(PageTypeMixin, ModelMutation):
     class Arguments:
-        input = PageTypeInput(
+        input = PageTypeCreateInput(
             description="Fields required to create page type.", required=True
         )
 
@@ -103,36 +138,23 @@ class PageTypeCreate(ModelMutation):
         error_type_field = "page_errors"
 
     @classmethod
-    def validate_attributes(cls, attributes):
-        if attributes:
-            not_valid_attributes = [
-                graphene.Node.to_global_id("Attribute", attr.pk)
-                for attr in attributes
-                if attr.type != AttributeType.PAGE_TYPE
-            ]
-            if not_valid_attributes:
-                raise ValidationError(
-                    {
-                        "add_attributes": ValidationError(
-                            "Only page type attributes allowed.",
-                            code=PageErrorCode.INVALID,
-                        )
-                    }
-                )
-
-    @classmethod
     def clean_input(cls, info, instance, data):
         cleaned_input = super().clean_input(info, instance, data)
-
+        errors = defaultdict(list)
         try:
             cleaned_input = validate_slug_and_generate_if_needed(
                 instance, "name", cleaned_input
             )
         except ValidationError as error:
-            error.code = PageErrorCode.REQUIRED
-            raise ValidationError({"slug": error})
+            error.code = PageErrorCode.REQUIRED.value
+            errors["slug"].append(error)
 
-        cls.validate_attributes(cleaned_input.get("add_attributes"))
+        cls.validate_attributes(
+            errors, cleaned_input.get("add_attributes"), "add_attributes"
+        )
+
+        if errors:
+            raise ValidationError(errors)
 
         return cleaned_input
 
@@ -141,4 +163,76 @@ class PageTypeCreate(ModelMutation):
         super()._save_m2m(info, instance, cleaned_data)
         attributes = cleaned_data.get("add_attributes")
         if attributes is not None:
-            instance.page_attributes.set(attributes)
+            instance.page_attributes.add(*attributes)
+
+
+class PageTypeUpdate(PageTypeMixin, ModelMutation):
+    class Arguments:
+        id = graphene.ID(description="ID of the page type to update.")
+        input = PageTypeUpdateInput(
+            description="Fields required to update page type.", required=True
+        )
+
+    class Meta:
+        description = "Update page type."
+        model = models.PageType
+        permissions = (PageTypePermissions.MANAGE_PAGE_TYPES_AND_ATTRIBUTES,)
+        error_type_class = PageError
+        error_type_field = "page_errors"
+
+    @classmethod
+    def check_for_duplicates(cls, errors, add_attributes, remove_attributes):
+        """Check if any items are on both list for adding and removing.
+
+        Raise error if some of items are duplicated.
+        """
+        duplicated_ids = get_duplicates_ids(add_attributes, remove_attributes)
+        if duplicated_ids:
+            error_msg = (
+                "The same object cannot be in both list"
+                "for adding and removing items."
+            )
+            error = ValidationError(
+                error_msg,
+                code=PageErrorCode.DUPLICATED_INPUT_ITEM.value,
+                params={"attributes": duplicated_ids},
+            )
+            errors["attributes"].append(error)
+
+    @classmethod
+    def clean_input(cls, info, instance, data):
+        cleaned_input = super().clean_input(info, instance, data)
+        errors = defaultdict(list)
+        try:
+            cleaned_input = validate_slug_and_generate_if_needed(
+                instance, "name", cleaned_input
+            )
+        except ValidationError as error:
+            error.code = PageErrorCode.REQUIRED
+            errors["slug"].append(error)
+
+        add_attributes = cleaned_input.get("add_attributes")
+        cls.validate_attributes(errors, add_attributes, "add_attributes")
+
+        remove_attributes = cleaned_input.get("remove_attributes")
+        cls.validate_attributes(errors, remove_attributes, "remove_attributes")
+
+        # TODO: move it up to operate on ids not on attributes
+        cls.check_for_duplicates(
+            errors, cleaned_input.get("add_attributes"), remove_attributes
+        )
+
+        if errors:
+            raise ValidationError(errors)
+
+        return cleaned_input
+
+    @classmethod
+    def _save_m2m(cls, info, instance, cleaned_data):
+        super()._save_m2m(info, instance, cleaned_data)
+        attributes = cleaned_data.get("remove_attributes")
+        attributes = cleaned_data.get("add_attributes")
+        if attributes is not None:
+            instance.page_attributes.remove(*attributes)
+        if attributes is not None:
+            instance.page_attributes.add(*attributes)

saleor/saleor

saleor/graphql/page/mutations.py
@@ -1,4 +1,6 @@
+from collections import defaultdict
 from datetime import date
+from typing import Dict, List
 
 import graphene
 from django.core.exceptions import ValidationError
@@ -9,7 +11,11 @@ from ...page.error_codes import PageErrorCode
 from ...product import AttributeType
 from ..core.mutations import ModelDeleteMutation, ModelMutation
 from ..core.types.common import PageError, SeoInput
-from ..core.utils import clean_seo_fields, validate_slug_and_generate_if_needed
+from ..core.utils import (
+    clean_seo_fields,
+    get_duplicates_ids,
+    validate_slug_and_generate_if_needed,
+)
 
 
 class PageInput(graphene.InputObjectType):
@@ -86,7 +92,7 @@ class PageDelete(ModelDeleteMutation):
         error_type_field = "page_errors"
 
 
-class PageTypeInput(graphene.InputObjectType):
+class PageTypeCreateInput(graphene.InputObjectType):
     name = graphene.String(description="Name of the page type.")
     slug = graphene.String(description="Page type slug.")
     add_attributes = graphene.List(
@@ -95,9 +101,38 @@ class PageTypeInput(graphene.InputObjectType):
     )
 
 
-class PageTypeCreate(ModelMutation):
+class PageTypeUpdateInput(PageTypeCreateInput):
+    remove_attributes = graphene.List(
+        graphene.NonNull(graphene.ID),
+        description="List of attribute IDs to be assigned to the page type.",
+    )
+
+
+class PageTypeMixin:
+    @classmethod
+    def validate_attributes(cls, errors: dict, attributes, field):
+        """All attributes must be page type attribute.
+
+        Raise an error if any of the attributes are not page attribute.
+        """
+        if attributes:
+            not_valid_attributes = [
+                graphene.Node.to_global_id("Attribute", attr.pk)
+                for attr in attributes
+                if attr.type != AttributeType.PAGE_TYPE
+            ]
+            if not_valid_attributes:
+                error = ValidationError(
+                    "Only page type attributes allowed.",
+                    code=PageErrorCode.INVALID.value,
+                    params={"attributes": not_valid_attributes},
+                )
+                errors[field].append(error)
+
+
+class PageTypeCreate(PageTypeMixin, ModelMutation):
     class Arguments:
-        input = PageTypeInput(
+        input = PageTypeCreateInput(
             description="Fields required to create page type.", required=True
         )
 
@@ -109,36 +144,23 @@ class PageTypeCreate(ModelMutation):
         error_type_field = "page_errors"
 
     @classmethod
-    def validate_attributes(cls, attributes):
-        if attributes:
-            not_valid_attributes = [
-                graphene.Node.to_global_id("Attribute", attr.pk)
-                for attr in attributes
-                if attr.type != AttributeType.PAGE_TYPE
-            ]
-            if not_valid_attributes:
-                raise ValidationError(
-                    {
-                        "add_attributes": ValidationError(
-                            "Only page type attributes allowed.",
-                            code=PageErrorCode.INVALID,
-                        )
-                    }
-                )
-
-    @classmethod
     def clean_input(cls, info, instance, data):
         cleaned_input = super().clean_input(info, instance, data)
-
+        errors = defaultdict(list)
         try:
             cleaned_input = validate_slug_and_generate_if_needed(
                 instance, "name", cleaned_input
             )
         except ValidationError as error:
-            error.code = PageErrorCode.REQUIRED
-            raise ValidationError({"slug": error})
+            error.code = PageErrorCode.REQUIRED.value
+            errors["slug"].append(error)
 
-        cls.validate_attributes(cleaned_input.get("add_attributes"))
+        cls.validate_attributes(
+            errors, cleaned_input.get("add_attributes"), "add_attributes"
+        )
+
+        if errors:
+            raise ValidationError(errors)
 
         return cleaned_input
 
@@ -147,4 +169,76 @@ class PageTypeCreate(ModelMutation):
         super()._save_m2m(info, instance, cleaned_data)
         attributes = cleaned_data.get("add_attributes")
         if attributes is not None:
-            instance.page_attributes.set(attributes)
+            instance.page_attributes.add(*attributes)
+
+
+class PageTypeUpdate(PageTypeMixin, ModelMutation):
+    class Arguments:
+        id = graphene.ID(description="ID of the page type to update.")
+        input = PageTypeUpdateInput(
+            description="Fields required to update page type.", required=True
+        )
+
+    class Meta:
+        description = "Update page type."
+        model = models.PageType
+        permissions = (PageTypePermissions.MANAGE_PAGE_TYPES_AND_ATTRIBUTES,)
+        error_type_class = PageError
+        error_type_field = "page_errors"
+
+    @classmethod
+    def check_for_duplicates(cls, errors: Dict[str, List[ValidationError]], data: dict):
+        """Check if any items are on both list for adding and removing.
+
+        Raise error if some of items are duplicated.
+        """
+        add_attributes = data.get("add_attributes")
+        remove_attributes = data.get("remove_attributes")
+
+        duplicated_ids = get_duplicates_ids(add_attributes, remove_attributes)
+        if duplicated_ids:
+            error_msg = (
+                "The same object cannot be in both list"
+                "for adding and removing items."
+            )
+            error = ValidationError(
+                error_msg,
+                code=PageErrorCode.DUPLICATED_INPUT_ITEM.value,
+                params={"attributes": duplicated_ids},
+            )
+            errors["attributes"].append(error)
+
+    @classmethod
+    def clean_input(cls, info, instance, data):
+        errors = defaultdict(list)
+        cls.check_for_duplicates(errors, data)
+
+        cleaned_input = super().clean_input(info, instance, data)
+        try:
+            cleaned_input = validate_slug_and_generate_if_needed(
+                instance, "name", cleaned_input
+            )
+        except ValidationError as error:
+            error.code = PageErrorCode.REQUIRED
+            errors["slug"].append(error)
+
+        add_attributes = cleaned_input.get("add_attributes")
+        cls.validate_attributes(errors, add_attributes, "add_attributes")
+
+        remove_attributes = cleaned_input.get("remove_attributes")
+        cls.validate_attributes(errors, remove_attributes, "remove_attributes")
+
+        if errors:
+            raise ValidationError(errors)
+
+        return cleaned_input
+
+    @classmethod
+    def _save_m2m(cls, info, instance, cleaned_data):
+        super()._save_m2m(info, instance, cleaned_data)
+        remove_attributes = cleaned_data.get("remove_attributes")
+        add_attributes = cleaned_data.get("add_attributes")
+        if remove_attributes is not None:
+            instance.page_attributes.remove(*remove_attributes)
+        if add_attributes is not None:
+            instance.page_attributes.add(*add_attributes)