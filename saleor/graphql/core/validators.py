51a4e3b8db28f7cefc6030da7f0bac3f5c77b6b6
Tomasz Szymański <tomasz.szymanski@stxnext.pl>
2020-10-19 15:53:00 +0200

    Add slug field to menu (#6258)
    * Add slug field to menu
    
    * Fix tests
    
    * Tests improvements
    
    * Improve description for validate_one_of_args_is_in_query
    
    * Allow MenuFilter to filter with slugs
    
    * Add search by slug to menus query

--- saleor/graphql/core/validators.py
+++ saleor/graphql/core/validators.py
@@ -9,15 +9,22 @@ if TYPE_CHECKING:
     from decimal import Decimal
 
 
-def validate_one_of_args_is_in_query(arg1_name, arg1, arg2_name, arg2):
-    if arg1 and arg2:
+def validate_one_of_args_is_in_query(*args):
+    # split args into a list with 2-element tuples:
+    # [(arg1_name, arg1_value), (arg2_name, arg2_value), ...]
+    splitted_args = [args[i : i + 2] for i in range(0, len(args), 2)]  # noqa: E203
+    # filter trueish values from each tuple
+    filter_args = list(filter(lambda item: bool(item[1]) is True, splitted_args))
+
+    if len(filter_args) > 1:
+        rest_args = ", ".join([f"'{item[0]}'" for item in filter_args[1:]])
         raise GraphQLError(
-            f"Argument '{arg1_name}' cannot be combined with '{arg2_name}'"
+            f"Argument '{filter_args[0][0]}' cannot be combined with {rest_args}"
         )
-    if not arg1 and not arg2:
-        raise GraphQLError(
-            f"Either '{arg1_name}'  or '{arg2_name}' argument is required"
-        )
+
+    if not filter_args:
+        required_args = ", ".join([f"'{item[0]}'" for item in splitted_args])
+        raise GraphQLError(f"At least one of arguments is required: {required_args}.")
 
 
 def validate_price_precision(value: Optional["Decimal"], currency: str = None):
