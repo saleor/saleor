fcf0d33de42e95236b4a6dc8e770fce8e37f4b93
IKarbowiak <iga.karbowiak@mirumee.com>
2020-10-26 11:39:39 +0100

    Add pageTypeReorderAttributes mutation

--- saleor/graphql/page/mutations/attributes.py
+++ saleor/graphql/page/mutations/attributes.py
@@ -2,19 +2,24 @@ from collections import defaultdict
 from typing import TYPE_CHECKING, Dict, List
 
 import graphene
-from django.core.exceptions import ValidationError
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.db import transaction
 
 from ....core.permissions import PageTypePermissions
+from ....page import models as page_models
 from ....page.error_codes import PageErrorCode
 from ....product import AttributeType, models
 from ...core.mutations import BaseMutation
 from ...core.types.common import PageError
+from ...core.utils import from_global_id_strict_type
+from ...core.utils.reordering import perform_reordering
 from ...page.types import PageType
+from ...product.mutations.common import ReorderInput
 from ...product.types import Attribute
 from ...utils import resolve_global_ids_to_primary_keys
 
 if TYPE_CHECKING:
-    from ....page import models as page_models
+    from django.db.models import QuerySet
 
 
 class PageAttributeAssign(BaseMutation):
@@ -139,3 +144,91 @@ class PageAttributeUnassign(BaseMutation):
         page_type.page_attributes.remove(*attr_pks)
 
         return cls(page_type=page_type)
+
+
+class PageTypeReorderAttributes(BaseMutation):
+    page_type = graphene.Field(
+        PageType, description="Page type from which attributes are reordered."
+    )
+
+    class Arguments:
+        page_type_id = graphene.Argument(
+            graphene.ID, required=True, description="ID of a page type."
+        )
+        moves = graphene.List(
+            graphene.NonNull(ReorderInput),
+            required=True,
+            description="The list of attribute reordering operations.",
+        )
+
+    class Meta:
+        description = "Reorder the attributes of a page type."
+        permissions = (PageTypePermissions.MANAGE_PAGE_TYPES_AND_ATTRIBUTES,)
+        error_type_class = PageError
+        error_type_field = "page_errors"
+
+    @classmethod
+    def prepare_operations(cls, moves: ReorderInput, page_attributes: "QuerySet"):
+        """Prepare operations dict for reordering attributes.
+
+        Operation dict format:
+            key: page attribute pk,
+            value: sort_order value - relative sorting position of the attribute
+        """
+        operations = {}
+        invalid_attributes = []
+
+        # resolve attribute moves
+        for move_info in moves:
+            attribute_pk = from_global_id_strict_type(
+                move_info.id, only_type=Attribute, field="pk"
+            )
+
+            try:
+                page_attribute = page_attributes.get(attribute_id=int(attribute_pk))
+            except ObjectDoesNotExist:
+                invalid_attributes.append(move_info.id)
+            else:
+                operations[page_attribute.pk] = move_info.sort_order
+
+        if invalid_attributes:
+            raise ValidationError(
+                {
+                    "moves": ValidationError(
+                        "Couldn't resolve to an attribute.",
+                        code=PageErrorCode.NOT_FOUND.value,
+                        params={"attributes": invalid_attributes},
+                    )
+                }
+            )
+
+        return operations
+
+    @classmethod
+    def perform_mutation(cls, _root, info, **data):
+        page_type_id = data["page_type_id"]
+        pk = from_global_id_strict_type(page_type_id, only_type=PageType, field="pk")
+
+        try:
+            page_type = page_models.PageType.objects.prefetch_related(
+                "attributepage"
+            ).get(pk=pk)
+        except ObjectDoesNotExist:
+            raise ValidationError(
+                {
+                    "page_type_id": ValidationError(
+                        f"Couldn't resolve to a page type: {page_type_id}",
+                        code=PageErrorCode.NOT_FOUND.value,
+                    )
+                }
+            )
+
+        page_attributes = page_type.attributepage.all()
+        moves = data["moves"]
+
+        operations = cls.prepare_operations(moves, page_attributes)
+
+        with transaction.atomic():
+            perform_reordering(page_attributes, operations)
+
+        return PageTypeReorderAttributes(page_type=page_type)
