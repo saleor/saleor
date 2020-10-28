4d5b1f6e95e17187a9fb68ca51f9071f6f7b43bd
fowczrek <filip.owczarek@defectprediction.net>
2020-10-28 10:32:46 +0100

    Optimize revnenue resolver on reportProductSales query

--- saleor/graphql/product/tests/benchmark/test_homepage.py
+++ saleor/graphql/product/tests/benchmark/test_homepage.py
@@ -1,5 +1,6 @@
 import pytest
 
+from ....core.enums import ReportingPeriod
 from ....tests.utils import get_graphql_content
 
 
@@ -28,3 +29,37 @@ def test_retrieve_product_list(
         }
     """
     get_graphql_content(api_client.post_graphql(query))
+
+
+@pytest.mark.django_db
+@pytest.mark.count_queries(autouse=False)
+def test_report_product_sales(
+    staff_api_client,
+    order_with_lines,
+    order_with_lines_channel_PLN,
+    permission_manage_products,
+    permission_manage_orders,
+    channel_USD,
+    count_queries,
+):
+    query = """
+        query TopProducts($period: ReportingPeriod!, $channel: String) {
+          reportProductSales(period: $period, first: 20, channel: $channel) {
+            edges {
+              node {
+                revenue(period: $period) {
+                  gross {
+                    amount
+                  }
+                }
+                quantityOrdered
+                sku
+              }
+            }
+          }
+        }
+    """
+    variables = {"period": ReportingPeriod.TODAY.name, "channel": channel_USD.slug}
+    permissions = [permission_manage_orders, permission_manage_products]
+    response = staff_api_client.post_graphql(query, variables, permissions)
+    get_graphql_content(response)
