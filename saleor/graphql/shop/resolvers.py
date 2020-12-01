87470f8027ff9981ca17b8254f2d010fe5faedbf
IKarbowiak <iga.karbowiak@mirumee.com>
2020-12-01 08:16:17 +0100

    Refactor resolve_available_shipping_methods for shop type

--- saleor/graphql/shop/resolvers.py
+++ saleor/graphql/shop/resolvers.py
@@ -1,4 +1,4 @@
-from ...shipping.models import ShippingMethod
+from ...shipping.models import ShippingMethod, ShippingMethodChannelListing
 from ..channel import ChannelContext
 
 
@@ -15,13 +15,12 @@ def resolve_available_shipping_methods(info, channel_slug: str, address):
         return []
     manager = info.context.plugins
     display_gross = info.context.site.settings.display_gross_prices
+    shipping_mapping = get_shipping_method_to_shipping_price_mapping(
+        available, channel_slug
+    )
     for shipping_method in available:
-        shipping_channel_listing = shipping_method.channel_listings.get(
-            channel__slug=channel_slug
-        )
-        taxed_price = manager.apply_taxes_to_shipping(
-            shipping_channel_listing.price, address
-        )
+        shipping_price = shipping_mapping[shipping_method.pk]
+        taxed_price = manager.apply_taxes_to_shipping(shipping_price, address)
         if display_gross:
             shipping_method.price = taxed_price.gross
         else:
@@ -31,3 +30,15 @@ def resolve_available_shipping_methods(info, channel_slug: str, address):
         ChannelContext(node=shipping, channel_slug=channel_slug)
         for shipping in available
     ]
+
+
+def get_shipping_method_to_shipping_price_mapping(shipping_methods, channel_slug):
+    """Prepare mapping shipping method to price from channel listings."""
+    shipping_mapping = {}
+    shipping_listings = ShippingMethodChannelListing.objects.filter(
+        shipping_method__in=shipping_methods, channel__slug=channel_slug
+    )
+    for listing in shipping_listings:
+        shipping_mapping[listing.shipping_method.id] = listing.price
+
+    return shipping_mapping
