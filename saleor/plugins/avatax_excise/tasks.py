d80b401f55b616349dcf4cf432b980db4fd0f0b1
stefano <sfenu3@gatech.edu>
2020-12-07 08:34:55 -0500

    Initialize fork

---
+++ saleor/plugins/avatax_excise/tasks.py
@@ -0,0 +1,8 @@
+from ...celeryconf import app
+from . import AvataxExciseConfiguration, api_post_request
+
+
+@app.task
+def api_post_request_task(transaction_url, data, config):
+    config = AvataxExciseConfiguration(**config)
+    api_post_request(transaction_url, data, config)
