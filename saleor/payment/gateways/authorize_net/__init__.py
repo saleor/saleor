2263046b810e4ac9beb934b829c0c0bb498cd8f2
David Burke <david@burkesoftware.com>
2020-12-07 17:45:04 -0500

    Integrate example auth.net python example into saleor auth.net plugin

--- saleor/payment/gateways/authorize_net/__init__.py
+++ saleor/payment/gateways/authorize_net/__init__.py
@@ -1,1 +1,94 @@
-from ...interface import GatewayConfig
+from authorizenet import apicontractsv1
+from authorizenet.apicontrollers import constants, createTransactionController
+
+from ...interface import GatewayConfig, PaymentData, GatewayResponse
+
+
+def process_payment(payment_information: PaymentData, config: GatewayConfig) -> GatewayResponse:
+    merchantAuth = apicontractsv1.merchantAuthenticationType()
+    merchantAuth.name = config.connection_params.get('api_login_id')
+    merchantAuth.transactionKey = config.connection_params.get('transaction_key')
+    print(payment_information)
+    
+    # Set the transaction's refId
+    refId = str(payment_information.payment_id)
+
+    # Create the payment object for a payment nonce
+    opaqueData = apicontractsv1.opaqueDataType()
+    opaqueData.dataDescriptor = "COMMON.ACCEPT.INAPP.PAYMENT"
+    opaqueData.dataValue = payment_information.token
+
+    # Add the payment data to a paymentType object
+    paymentOne = apicontractsv1.paymentType()
+    paymentOne.opaqueData = opaqueData
+    
+    # Create order information
+    order = apicontractsv1.orderType()
+    order.invoiceNumber = payment_information.order_id
+    # order.description = ""
+    
+    # Set the customer's Bill To address
+    customerAddress = apicontractsv1.customerAddressType()
+    customerAddress.firstName = payment_information.billing.first_name
+    customerAddress.lastName = payment_information.billing.last_name
+    customerAddress.company = payment_information.billing.company_name
+    customerAddress.address = payment_information.billing.street_address_1
+    customerAddress.city = payment_information.billing.city
+    customerAddress.state = payment_information.billing.country_area
+    customerAddress.zip = payment_information.billing.postal_code
+    customerAddress.country = payment_information.billing.country
+    
+    # Set the customer's identifying information
+    customerData = apicontractsv1.customerDataType()
+    customerData.type = "individual"
+    customerData.id = payment_information.customer_id
+    customerData.email = payment_information.customer_email
+    
+    # Add values for transaction settings
+    duplicateWindowSetting = apicontractsv1.settingType()
+    duplicateWindowSetting.settingName = "duplicateWindow"
+    duplicateWindowSetting.settingValue = "600"
+    settings = apicontractsv1.ArrayOfSetting()
+    settings.setting.append(duplicateWindowSetting)
+
+    # Create a transactionRequestType object and add the previous objects to it
+    transactionrequest = apicontractsv1.transactionRequestType()
+    transactionrequest.transactionType = "authCaptureTransaction"
+    transactionrequest.amount = payment_information.amount
+    transactionrequest.order = order
+    transactionrequest.payment = paymentOne
+    transactionrequest.billTo = customerAddress
+    transactionrequest.customer = customerData
+    transactionrequest.transactionSettings = settings
+
+    # Assemble the complete transaction request
+    createtransactionrequest = apicontractsv1.createTransactionRequest()
+    createtransactionrequest.merchantAuthentication = merchantAuth
+    createtransactionrequest.refId = refId
+    createtransactionrequest.transactionRequest = transactionrequest
+    
+    # Create the controller and get response
+    createtransactioncontroller = createTransactionController(createtransactionrequest)
+    createtransactioncontroller.execute()
+
+    response = createtransactioncontroller.getresponse()
+    print(response.messages.message[0]["text"])
+    print(response.transactionResponse)
+
+    if response is not None:
+        if response.messages.resultCode == "Ok":
+            if hasattr(response.transactionResponse, 'messages') == True:
+                return GatewayResponse(
+                    is_success=True,
+                    action_required=False,
+                    transaction_id=response.transactionResponse.transId,
+                    amount=payment_information.amount,
+                    currency=currency,
+                    error=None,
+                    kind=kind,
+                    raw_response=raw_response or intent,
+                    customer_id=customer_id,
+                )
+                response.transactionResponse.transId
+
+    return result
--- saleor/payment/gateways/authorize_net/plugin.py
+++ saleor/payment/gateways/authorize_net/plugin.py
@@ -1,10 +1,22 @@
+from typing import TYPE_CHECKING, List
+
 from saleor.plugins.base_plugin import BasePlugin, ConfigurationTypeField
 
-from . import GatewayConfig
+from ..utils import get_supported_currencies
+from . import GatewayConfig, process_payment
 
 
 GATEWAY_NAME = "Authorize.Net"
 
+
+def require_active_plugin(fn):
+    def wrapped(self, *args, **kwargs):
+        previous = kwargs.get("previous_value", None)
+        if not self.active:
+            return previous
+        return fn(self, *args, **kwargs)
+
+    return wrapped
 
 class AuthorizeNetGatewayPlugin(BasePlugin):
     PLUGIN_NAME = GATEWAY_NAME
@@ -33,14 +45,69 @@ class AuthorizeNetGatewayPlugin(BasePlugin):
         configuration = {item["name"]: item["value"] for item in self.configuration}
         self.config = GatewayConfig(
             gateway_name=GATEWAY_NAME,
+            auto_capture=True,
+            supported_currencies="USD",
             # auto_capture=configuration["Automatic payment capture"],
             # supported_currencies=configuration["Supported currencies"],
-            # connection_params={
-            #     "public_key": configuration["Public API key"],
-            #     "private_key": configuration["Secret API key"],
-            # },
+            connection_params={
+                "api_login_id": configuration["API Login ID"],
+                "transaction_key": configuration["Transaction Key"],
+            },
             # store_customer=configuration["Store customers card"],
         )
 
     def _get_gateway_config(self) -> GatewayConfig:
         return self.config
+
+    # @require_active_plugin
+    # def authorize_payment(
+    #     self, payment_information: "PaymentData", previous_value
+    # ) -> "GatewayResponse":
+    #     pass
+        # return authorize(payment_information, self._get_gateway_config())
+
+    @require_active_plugin
+    def capture_payment(
+        self, payment_information: "PaymentData", previous_value
+    ) -> "GatewayResponse":
+        pass # return capture(payment_information, self._get_gateway_config())
+
+    @require_active_plugin
+    def refund_payment(
+        self, payment_information: "PaymentData", previous_value
+    ) -> "GatewayResponse":
+        pass # return refund(payment_information, self._get_gateway_config())
+
+    @require_active_plugin
+    def void_payment(
+        self, payment_information: "PaymentData", previous_value
+    ) -> "GatewayResponse":
+        pass # return void(payment_information, self._get_gateway_config())
+
+    @require_active_plugin
+    def process_payment(
+        self, payment_information: "PaymentData", previous_value
+    ) -> "GatewayResponse":
+        return process_payment(payment_information, self._get_gateway_config())
+
+    @require_active_plugin
+    def list_payment_sources(
+        self, customer_id: str, previous_value
+    ) -> List["CustomerSource"]:
+        pass
+        # sources = list_client_sources(self._get_gateway_config(), customer_id)
+        # previous_value.extend(sources)
+        # return previous_value
+
+    @require_active_plugin
+    def get_supported_currencies(self, previous_value):
+        config = self._get_gateway_config()
+        return get_supported_currencies(config, GATEWAY_NAME)
+
+    @require_active_plugin
+    def get_payment_config(self, previous_value):
+        config = self._get_gateway_config()
+        return [
+            {"field": "api_key", "value": config.connection_params["api_login_id"]},
+            {"field": "store_customer_card", "value": config.store_customer},
+        ]
