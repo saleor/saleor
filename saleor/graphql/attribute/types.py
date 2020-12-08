7dbd0f408408fd48fd566cf35ab0cd85eea55255
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2020-12-08 12:26:49 +0100

    Allow settings references in product and page mutations (#6596)
    * Extend AttributeInputType with reference
    
    * Add entity_type field to Attribute type
    
    * Allow defining reference attribute values for Product mutations
    
    * Add reference field to AttributeValue type
    
    * Add test for updating Product with exisiting reference AttributeValue
    
    * Add tests for defining reference attribute value in ProductVariant mutations
    
    * Add tests for defining reference attribute value in Page mutations
    
    * Refactor graphql/attribute/utils.py
    
    * Add docsting to AttributeEntityType
    
    * Update error message for invalid references and rename validate_not_file_attributes_input function
    
    * Use dataloader in resolve_reference for AttributeValue

--- saleor/graphql/attribute/types.py
+++ saleor/graphql/attribute/types.py
@@ -2,7 +2,7 @@ import re
 
 import graphene
 
-from ...attribute import models
+from ...attribute import AttributeInputType, models
 from ..core.connection import CountableDjangoObjectType
 from ..core.types import File
 from ..decorators import (
@@ -12,7 +12,7 @@ from ..decorators import (
 from ..meta.types import ObjectWithMetadata
 from ..translations.fields import TranslationField
 from ..translations.types import AttributeTranslation, AttributeValueTranslation
-from .dataloaders import AttributeValuesByAttributeIdLoader
+from .dataloaders import AttributesByAttributeId, AttributeValuesByAttributeIdLoader
 from .descriptions import AttributeDescriptions, AttributeValueDescriptions
 from .enums import (
     AttributeEntityTypeEnum,
@@ -49,6 +49,7 @@ class AttributeValue(CountableDjangoObjectType):
         AttributeValueTranslation, type_name="attribute value"
     )
     input_type = AttributeInputTypeEnum(description=AttributeDescriptions.INPUT_TYPE)
+    reference = graphene.ID(description="The ID of the attribute reference.")
     file = graphene.Field(
         File, description=AttributeValueDescriptions.FILE, required=False
     )
@@ -73,6 +74,23 @@ class AttributeValue(CountableDjangoObjectType):
         if not root.file_url:
             return
         return File(url=root.file_url, content_type=root.content_type)
+
+    @staticmethod
+    def resolve_reference(root: models.AttributeValue, info, **_kwargs):
+        def prepare_reference(attribute):
+            if not attribute.input_type == AttributeInputType.REFERENCE:
+                return
+            reference_pk = root.slug.split("_")[1]
+            reference_id = graphene.Node.to_global_id(
+                attribute.entity_type, reference_pk
+            )
+            return reference_id
+
+        return (
+            AttributesByAttributeId(info.context)
+            .load(root.attribute_id)
+            .then(prepare_reference)
+        )
 
 
 class Attribute(CountableDjangoObjectType):
