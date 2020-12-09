45b9ac9778479ceb94f08f2b3fc1208be4641c11
Filip Owczarek <43955230+fowczarek@users.noreply.github.com>
2020-12-09 12:49:28 +0100

    Fix remove channel without orders (#6600)
    * Fix remove channel without orders
    
    * Add `canRemoveWithoutOrderMigration` to Channel Type
    
    * Fix review remars

--- saleor/channel/error_codes.py
+++ saleor/channel/error_codes.py
@@ -10,3 +10,4 @@ class ChannelErrorCode(Enum):
     UNIQUE = "unique"
     CHANNEL_TARGET_ID_MUST_BE_DIFFERENT = "channel_target_id_must_be_different"
     CHANNELS_CURRENCY_MUST_BE_THE_SAME = "channels_currency_must_be_the_same"
+    CHANNEL_WITH_ORDERS = "channel_with_orders"
