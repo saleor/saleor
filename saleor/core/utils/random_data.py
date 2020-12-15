1494601e4e5e5ffc16a4efd663220fa6a2d22cce
IKarbowiak <iga.karbowiak@mirumee.com>
2020-12-15 15:33:59 +0100

    Update populatedb with page reference attibute

--- saleor/core/utils/random_data.py
+++ saleor/core/utils/random_data.py
@@ -402,8 +402,8 @@ def create_products_by_schema(placeholder_dir, create_images):
     create_categories(
         categories_data=types["product.category"], placeholder_dir=placeholder_dir
     )
-    create_attributes(attributes_data=types["product.attribute"])
-    create_attributes_values(values_data=types["product.attributevalue"])
+    create_attributes(attributes_data=types["attribute.attribute"])
+    create_attributes_values(values_data=types["attribute.attributevalue"])
     create_products(
         products_data=types["product.product"],
         placeholder_dir=placeholder_dir,
@@ -421,21 +421,21 @@ def create_products_by_schema(placeholder_dir, create_images):
         ],
     )
     assign_attributes_to_product_types(
-        AttributeProduct, attributes=types["product.attributeproduct"]
+        AttributeProduct, attributes=types["attribute.attributeproduct"]
     )
     assign_attributes_to_product_types(
-        AttributeVariant, attributes=types["product.attributevariant"]
+        AttributeVariant, attributes=types["attribute.attributevariant"]
     )
     assign_attributes_to_page_types(
-        AttributePage, attributes=types["product.attributepage"]
+        AttributePage, attributes=types["attribute.attributepage"]
     )
     assign_attributes_to_products(
-        product_attributes=types["product.assignedproductattribute"]
+        product_attributes=types["attribute.assignedproductattribute"]
     )
     assign_attributes_to_variants(
-        variant_attributes=types["product.assignedvariantattribute"]
+        variant_attributes=types["attribute.assignedvariantattribute"]
     )
-    assign_attributes_to_pages(page_attributes=types["product.assignedpageattribute"])
+    assign_attributes_to_pages(page_attributes=types["attribute.assignedpageattribute"])
     create_collections(
         data=types["product.collection"], placeholder_dir=placeholder_dir
     )
@@ -1253,6 +1253,15 @@ def create_page_type():
                 "slug": "mission",
             },
         },
+        {
+            "pk": 3,
+            "fields": {
+                "private_metadata": {},
+                "metadata": {},
+                "name": "Product details",
+                "slug": "product-details",
+            },
+        },
     ]
     for page_type_data in data:
         pk = page_type_data.pop("pk")
@@ -1262,68 +1271,105 @@ def create_page_type():
         yield "Page type %s created" % page_type.slug
 
 
-def create_page():
-    content = """
-    <h2>E-commerce for the PWA era</h2>
-    <h3>A modular, high performance e-commerce storefront built with GraphQL,
-        Django, and ReactJS.</h3>
-    <p>Saleor is a rapidly-growing open source e-commerce platform that has served
-       high-volume companies from branches like publishing and apparel since 2012.
-       Based on Python and Django, the latest major update introduces a modular
-       front end with a GraphQL API and storefront and dashboard written in React
-       to make Saleor a full-functionality open source e-commerce.</p>
-    <p><a href="https://github.com/mirumee/saleor">Get Saleor today!</a></p>
-    """
-    content_json = {
-        "blocks": [
-            {
-                "data": {"text": "E-commerce for the PWA era", "level": 2},
-                "type": "header",
+def create_pages():
+    data_pages = {
+        1: {
+            "title": "About",
+            "slug": "about",
+            "page_type_id": 1,
+            "content": """
+                <h2>E-commerce for the PWA era</h2>
+                <h3>A modular, high performance e-commerce storefront built
+                with GraphQL, Django, and ReactJS.</h3>
+                <p>Saleor is a rapidly-growing open source e-commerce platform that
+                has served high-volume companies from branches like publishing
+                and apparel since 2012. Based on Python and Django, the latest major
+                update introduces a modular front end with a GraphQL API and storefront
+                and dashboard written in React to make Saleor a full-functionality
+                open source e-commerce.</p>
+                <p><a href="https://github.com/mirumee/saleor">Get Saleor today!</a></p>
+                """,
+            "content_json": {
+                "blocks": [
+                    {
+                        "data": {"text": "E-commerce for the PWA era", "level": 2},
+                        "type": "header",
+                    },
+                    {
+                        "data": {
+                            "text": (
+                                "A modular, high performance e-commerce storefront "
+                                "built with GraphQL, Django, and ReactJS."
+                            ),
+                            "level": 2,
+                        },
+                        "type": "header",
+                    },
+                    {"data": {"text": ""}, "type": "paragraph"},
+                    {
+                        "data": {
+                            "text": (
+                                "Saleor is a rapidly-growing open source e-commerce "
+                                "platform that has served high-volume companies "
+                                "from branches like publishing and apparel since 2012. "
+                                "Based on Python and Django, the latest major update "
+                                "introduces a modular front end with a GraphQL API "
+                                "and storefront and dashboard written in React "
+                                "to make Saleor a full-functionality "
+                                "open source e-commerce."
+                            )
+                        },
+                        "type": "paragraph",
+                    },
+                    {"data": {"text": ""}, "type": "paragraph"},
+                    {
+                        "data": {
+                            "text": (
+                                '<a href="https://github.com/mirumee/saleor">'
+                                "Get Saleor today!</a>"
+                            )
+                        },
+                        "type": "paragraph",
+                    },
+                ],
             },
-            {
-                "data": {
-                    "text": (
-                        "A modular, high performance e-commerce storefront built with "
-                        "GraphQL, Django, and ReactJS."
-                    )
-                },
-                "type": "paragraph",
+        },
+        2: {
+            "title": "Apple juice details",
+            "slug": "apple-juice-details",
+            "page_type_id": 3,
+            "content": (
+                "\n<h2>Apple juice details</h2>\n"
+                "<p>This is example product details page.</p>\n"
+            ),
+            "content_json": {
+                "blocks": [
+                    {
+                        "data": {"text": "Apple juice details", "level": 2},
+                        "type": "header",
+                    },
+                    {
+                        "data": {"text": "This is example product details page."},
+                        "type": "paragraph",
+                    },
+                ]
             },
-            {"data": {"text": ""}, "type": "paragraph"},
-            {
-                "data": {
-                    "text": (
-                        "Saleor is a rapidly-growing open source e-commerce platform "
-                        "that has served high-volume companies from branches like "
-                        "publishing and apparel since 2012. Based on Python and Django,"
-                        " the latest major update introduces a modular front end with "
-                        "a GraphQL API and storefront and dashboard written in React "
-                        "to make Saleor a full-functionality open source e-commerce."
-                    )
-                },
-                "type": "paragraph",
-            },
-            {"data": {"text": ""}, "type": "paragraph"},
-            {
-                "data": {
-                    "text": (
-                        '<a href="https://github.com/mirumee/saleor">'
-                        "Get Saleor today!</a>"
-                    )
-                },
-                "type": "paragraph",
-            },
-        ],
+        },
     }
-    page_data = {
-        "content": content,
-        "content_json": content_json,
-        "title": "About",
-        "is_published": True,
-        "page_type_id": 1,
-    }
-    page, dummy = Page.objects.get_or_create(pk=1, slug="about", defaults=page_data)
-    yield "Page %s created" % page.slug
+
+    for pk in [1, 2]:
+        data = data_pages[pk]
+        page_data = {
+            "content": data["content"],
+            "content_json": data["content_json"],
+            "title": data["title"],
+            "is_published": True,
+            "page_type_id": data["page_type_id"],
+        }
+        page, _ = Page.objects.get_or_create(
+            pk=pk, slug=data["slug"], defaults=page_data
+        )
+        yield "Page %s created" % page.slug
 
 
 def generate_menu_items(menu: Menu, category: Category, parent_menu_item):
