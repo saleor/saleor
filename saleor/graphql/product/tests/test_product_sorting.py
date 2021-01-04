1ef0aad15d3314cf8c5963435a5c7058b9beb7a7
Damian Wysocki <damian.wysocki@mirumee.com>
2021-01-04 15:33:47 +0100

    Fix reordering products duplicates (#6666)

--- saleor/graphql/product/tests/test_product_sorting.py
+++ saleor/graphql/product/tests/test_product_sorting.py
@@ -115,7 +115,10 @@ def test_sort_products_within_collection(
 
     variables = {
         "collectionId": collection_id,
-        "moves": [{"productId": product, "sortOrder": 1}],
+        "moves": [
+            {"productId": product, "sortOrder": 1},
+            {"productId": second_product, "sortOrder": -1},
+        ],
     }
     content = get_graphql_content(
         staff_api_client.post_graphql(COLLECTION_RESORT_QUERY, variables)
@@ -123,8 +126,8 @@ def test_sort_products_within_collection(
 
     products = content["collection"]["products"]["edges"]
     assert products[0]["node"]["id"] == third_product
-    assert products[1]["node"]["id"] == product
-    assert products[2]["node"]["id"] == second_product
+    assert products[1]["node"]["id"] == second_product
+    assert products[2]["node"]["id"] == product
 
 
 GET_SORTED_PRODUCTS_QUERY = """
