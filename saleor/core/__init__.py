74840ab7b4c3d1b6947435598a69a0d0a2862b94
IKarbowiak <iga.karbowiak@mirumee.com>
2021-01-15 15:21:25 +0100

    Prepare units enum

--- saleor/core/__init__.py
+++ saleor/core/__init__.py
@@ -1,3 +1,9 @@
+from enum import Enum
+
+from django.contrib.gis.measure import Area, Distance
+from measurement.measures import Volume, Weight
+
+
 class JobStatus:
     PENDING = "pending"
     SUCCESS = "success"
@@ -10,3 +16,28 @@ class JobStatus:
         (FAILED, "Failed"),
         (DELETED, "Deleted"),
     ]
+
+
+DISTANCE_UNITS = Enum(  # type: ignore
+    "DistanceUnits", [(unit.upper(), unit) for unit in Distance.UNITS.keys()]
+)
+
+AREA_UNITS = Enum(  # type: ignore
+    "AreaUnits", [(unit.upper(), unit) for unit in Area.UNITS.keys()]
+)
+VOLUME_UNITS = Enum(  # type: ignore
+    "VolumeUnits", [(unit.upper(), unit) for unit in Volume.UNITS.keys()]
+)
+WEIGHT_UNITS = Enum(  # type: ignore
+    "WeightUnits", [(unit.upper(), unit) for unit in Weight.UNITS.keys()]
+)
+
+
+MEASUREMENT_UNITS = Enum(  # type: ignore
+    "MeasurmentUnits",
+    [
+        (unit.value, unit.value.replace("_", " "))
+        for enum in [DISTANCE_UNITS, AREA_UNITS, VOLUME_UNITS, WEIGHT_UNITS]
+        for unit in enum
+    ],
+)
