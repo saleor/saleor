5c06f7384d95662bb1402c06e64c1d8d4dba4411
Krzysztof Wolski <krzysztof.k.wolski@gmail.com>
2021-01-29 16:57:04 +0100

    App type enum descriptions

--- saleor/graphql/app/enums.py
+++ saleor/graphql/app/enums.py
@@ -1,4 +1,19 @@
 from ...app.types import AppType
 from ..core.enums import to_enum
 
-AppTypeEnum = to_enum(AppType)
+
+def description(enum):
+    if enum == AppTypeEnum.LOCAL:
+        return (
+            "Local Saleor App. The app is fully manageable from dashboard. ",
+            "You can change assigned permissions, add webhooks, or authentication token",
+        )
+    elif enum == AppTypeEnum.THIRDPARTY:
+        return (
+            "Third party external App. Installation is fully automated. ",
+            "Saleor uses a defined App manifest to gather all required information.",
+        )
+    return "Enum determining type of your App."
+
+
+AppTypeEnum = to_enum(AppType, description=description)
