c86a331dcc9f486351ec86a696c5c44d47b46801
Maciej Korycinski <maciej@mirumee.com>
2021-02-14 15:23:27 +0100

    Add tests

---
+++ saleor/plugins/sendgrid/tests/conftest.py
@@ -0,0 +1,102 @@
+import pytest
+
+from ...manager import get_plugins_manager
+from ..plugin import SendgridEmailPlugin
+
+
+@pytest.fixture
+def sendgrid_email_plugin(settings):
+    def fun(
+        active=True,
+        sender_name=None,
+        sender_address=None,
+        account_confirmation_template_id=None,
+        account_set_customer_password_template_id=None,
+        account_delete_template_id=None,
+        account_change_email_confirm_template_id=None,
+        account_change_email_request_template_id=None,
+        account_password_reset_template_id=None,
+        invoice_ready_template_id=None,
+        order_confirmation_template_id=None,
+        order_confirmed_template_id=None,
+        order_fulfillment_confirmation_template_id=None,
+        order_fulfillment_update_template_id=None,
+        order_payment_confirmation_template_id=None,
+        order_canceled_template_id=None,
+        order_refund_confirmation_template_id=None,
+        api_key=None,
+    ):
+
+        settings.PLUGINS = ["saleor.plugins.sendgrid.plugin.SendgridEmailPlugin"]
+        manager = get_plugins_manager()
+        manager.save_plugin_configuration(
+            SendgridEmailPlugin.PLUGIN_ID,
+            {
+                "active": active,
+                "configuration": [
+                    {"name": "sender_name", "value": sender_name},
+                    {"name": "sender_address", "value": sender_address},
+                    {
+                        "name": "account_confirmation_template_id",
+                        "value": account_confirmation_template_id,
+                    },
+                    {
+                        "name": "account_set_customer_password_template_id",
+                        "value": account_set_customer_password_template_id,
+                    },
+                    {
+                        "name": "account_delete_template_id",
+                        "value": account_delete_template_id,
+                    },
+                    {
+                        "name": "account_change_email_confirm_template_id",
+                        "value": account_change_email_confirm_template_id,
+                    },
+                    {
+                        "name": "account_change_email_request_template_id",
+                        "value": account_change_email_request_template_id,
+                    },
+                    {
+                        "name": "account_password_reset_template_id",
+                        "value": account_password_reset_template_id,
+                    },
+                    {
+                        "name": "invoice_ready_template_id",
+                        "value": invoice_ready_template_id,
+                    },
+                    {
+                        "name": "order_confirmation_template_id",
+                        "value": order_confirmation_template_id,
+                    },
+                    {
+                        "name": "order_confirmed_template_id",
+                        "value": order_confirmed_template_id,
+                    },
+                    {
+                        "name": "order_fulfillment_confirmation_template_id",
+                        "value": order_fulfillment_confirmation_template_id,
+                    },
+                    {
+                        "name": "order_fulfillment_update_template_id",
+                        "value": order_fulfillment_update_template_id,
+                    },
+                    {
+                        "name": "order_payment_confirmation_template_id",
+                        "value": order_payment_confirmation_template_id,
+                    },
+                    {
+                        "name": "order_canceled_template_id",
+                        "value": order_canceled_template_id,
+                    },
+                    {
+                        "name": "order_refund_confirmation_template_id",
+                        "value": order_refund_confirmation_template_id,
+                    },
+                    {"name": "api_key", "value": api_key},
+                ],
+            },
+        )
+        manager = get_plugins_manager()
+        return manager.plugins[0]
+
+    return fun
---
+++ saleor/plugins/sendgrid/tests/test_plugin.py
@@ -0,0 +1,112 @@
+from unittest.mock import MagicMock, patch
+
+from ....core.notify_events import AdminNotifyEvent, UserNotifyEvent
+from .. import tasks
+from ..plugin import get_event_to_task_map
+
+
+def test_get_event_to_task_map():
+    assert get_event_to_task_map() == {
+        UserNotifyEvent.ACCOUNT_CONFIRMATION: (
+            tasks.send_account_confirmation_email_task
+        ),
+        UserNotifyEvent.ACCOUNT_SET_CUSTOMER_PASSWORD: (
+            tasks.send_set_user_password_email_task
+        ),
+        UserNotifyEvent.ACCOUNT_DELETE: (
+            tasks.send_account_delete_confirmation_email_task
+        ),
+        UserNotifyEvent.ACCOUNT_CHANGE_EMAIL_CONFIRM: (
+            tasks.send_user_change_email_notification_task
+        ),
+        UserNotifyEvent.ACCOUNT_CHANGE_EMAIL_REQUEST: (
+            tasks.send_request_email_change_email_task
+        ),
+        UserNotifyEvent.ACCOUNT_PASSWORD_RESET: tasks.send_password_reset_email_task,
+        UserNotifyEvent.INVOICE_READY: tasks.send_invoice_email_task,
+        UserNotifyEvent.ORDER_CONFIRMATION: tasks.send_order_confirmation_email_task,
+        UserNotifyEvent.ORDER_CONFIRMED: tasks.send_order_confirmed_email_task,
+        UserNotifyEvent.ORDER_FULFILLMENT_CONFIRMATION: (
+            tasks.send_fulfillment_confirmation_email_task
+        ),
+        UserNotifyEvent.ORDER_FULFILLMENT_UPDATE: (
+            tasks.send_fulfillment_update_email_task
+        ),
+        UserNotifyEvent.ORDER_PAYMENT_CONFIRMATION: (
+            tasks.send_payment_confirmation_email_task
+        ),
+        UserNotifyEvent.ORDER_CANCELED: tasks.send_order_canceled_email_task,
+        UserNotifyEvent.ORDER_REFUND_CONFIRMATION: tasks.send_order_refund_email_task,
+    }
+
+
+@patch("saleor.plugins.sendgrid.plugin.get_event_to_task_map")
+def test_notify_when_plugin_disabled(
+    mocked_get_event_to_task_map, sendgrid_email_plugin
+):
+    plugin = sendgrid_email_plugin(active=False)
+
+    plugin.notify(UserNotifyEvent.ACCOUNT_PASSWORD_RESET, {}, None)
+
+    assert not mocked_get_event_to_task_map.called
+
+
+@patch("saleor.plugins.sendgrid.plugin.get_event_to_task_map")
+def test_notify_not_valid_event_type(
+    mocked_get_event_to_task_map, sendgrid_email_plugin
+):
+    plugin = sendgrid_email_plugin(active=True)
+
+    plugin.notify(AdminNotifyEvent.CSV_EXPORT_FAILED, {}, None)
+
+    assert not mocked_get_event_to_task_map.called
+
+
+@patch("saleor.plugins.sendgrid.plugin.get_event_to_task_map")
+def test_notify_missing_handler(mocked_get_event_to_task_map, sendgrid_email_plugin):
+    sample_payload = {"key_1": "value"}
+
+    event_map = MagicMock()
+    mocked_get_event_to_task_map.return_value = event_map
+
+    plugin = sendgrid_email_plugin(active=True)
+
+    plugin.notify(UserNotifyEvent.ACCOUNT_PASSWORD_RESET, sample_payload, None)
+
+    assert mocked_get_event_to_task_map.called
+    assert not event_map.__getitem__.called
+
+
+@patch("saleor.plugins.sendgrid.plugin.get_event_to_task_map")
+def test_notify_missing_template_id(
+    mocked_get_event_to_task_map, sendgrid_email_plugin
+):
+    sample_payload = {"key_1": "value"}
+
+    event_map = MagicMock()
+    event_map.__contains__.return_value = True
+    mocked_get_event_to_task_map.return_value = event_map
+
+    plugin = sendgrid_email_plugin(active=True, account_password_reset_template_id=None)
+
+    plugin.notify(UserNotifyEvent.ACCOUNT_PASSWORD_RESET, sample_payload, None)
+
+    assert mocked_get_event_to_task_map.called
+    assert not event_map.__getitem__.called
+
+
+@patch("saleor.plugins.sendgrid.plugin.get_event_to_task_map")
+def test_notify(mocked_get_event_to_task_map, sendgrid_email_plugin):
+    sample_payload = {"key_1": "value"}
+
+    event_map = MagicMock()
+    event_map.__contains__.return_value = True
+    mocked_get_event_to_task_map.return_value = event_map
+
+    plugin = sendgrid_email_plugin(
+        active=True, account_password_reset_template_id="123"
+    )
+
+    plugin.notify(UserNotifyEvent.ACCOUNT_PASSWORD_RESET, sample_payload, None)
+
+    event_map.__getitem__.assert_called_once_with("account_password_reset")
