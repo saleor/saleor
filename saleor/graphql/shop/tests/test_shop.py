3958fc794fd6efc31cae64e8c643d828ca8e5a62
Tomasz Szymanski <lime129@gmail.com>
2021-02-24 08:44:47 +0100

    Fix shop query postal codes filtering

--- saleor/graphql/shop/tests/test_shop.py
+++ saleor/graphql/shop/tests/test_shop.py
@@ -7,6 +7,7 @@ from django_countries import countries
 from ....account.models import Address
 from ....core.error_codes import ShopErrorCode
 from ....core.permissions import get_permissions_codename
+from ....shipping import PostalCodeRuleInclusionType
 from ....shipping.models import ShippingMethod
 from ....site.models import Site
 from ...account.enums import CountryCodeEnum
@@ -763,6 +764,54 @@ def test_query_available_shipping_methods_for_given_address_vatlayer_set(
     assert graphene.Node.to_global_id(
         "ShippingMethod", shipping_zone_without_countries.pk
     ) not in {ship_meth["id"] for ship_meth in data}
+
+
+def test_query_available_shipping_methods_for_excluded_postal_code(
+    staff_api_client, channel_USD, shipping_method
+):
+    # given
+    query = AVAILABLE_SHIPPING_METHODS_QUERY
+    variables = {
+        "channel": channel_USD.slug,
+        "address": {"country": CountryCodeEnum.PL.name, "postalCode": "53-601"},
+    }
+    shipping_method.postal_code_rules.create(
+        start="53-600", end="54-600", inclusion_type=PostalCodeRuleInclusionType.EXCLUDE
+    )
+
+    # when
+    response = staff_api_client.post_graphql(query, variables)
+
+    # then
+    content = get_graphql_content(response)
+    data = content["data"]["shop"]["availableShippingMethods"]
+    assert graphene.Node.to_global_id("ShippingMethod", shipping_method.pk) not in {
+        ship_meth["id"] for ship_meth in data
+    }
+
+
+def test_query_available_shipping_methods_for_included_postal_code(
+    staff_api_client, channel_USD, shipping_method
+):
+    # given
+    query = AVAILABLE_SHIPPING_METHODS_QUERY
+    variables = {
+        "channel": channel_USD.slug,
+        "address": {"country": CountryCodeEnum.PL.name, "postalCode": "53-601"},
+    }
+    shipping_method.postal_code_rules.create(
+        start="53-600", end="54-600", inclusion_type=PostalCodeRuleInclusionType.INCLUDE
+    )
+
+    # when
+    response = staff_api_client.post_graphql(query, variables)
+
+    # then
+    content = get_graphql_content(response)
+    data = content["data"]["shop"]["availableShippingMethods"]
+    assert graphene.Node.to_global_id("ShippingMethod", shipping_method.pk) in {
+        ship_meth["id"] for ship_meth in data
+    }
 
 
 MUTATION_SHOP_ADDRESS_UPDATE = """
