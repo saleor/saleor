b9f4ef148591113faea24e02d1b82156b786a9e1
Damian W <damian.wysocki@mirumee.com>
2021-03-02 13:01:22 +0100

    Add unit tests

--- saleor/core/tests/test_url.py
+++ saleor/core/tests/test_url.py
@@ -1,6 +1,9 @@
 from urllib.parse import urlencode
 
-from ..utils.url import prepare_url
+import pytest
+from django.core.exceptions import ValidationError
+
+from ..utils.url import prepare_url, validate_url
 
 
 def test_prepare_url():
@@ -8,3 +11,16 @@ def test_prepare_url():
     params = urlencode({"param1": "abc", "param2": "xyz"})
     result = prepare_url(params, redirect_url)
     assert result == f"{redirect_url}?param1=abc&param2=xyz"
+
+
+def test_validate_url():
+    url = "http://otherapp:3000"
+    result = validate_url(url)
+
+    assert result == "otherapp"
+
+
+def test_validate_invalid_url():
+    url = "otherapp:3000"
+    with pytest.raises(ValidationError):
+        validate_url(url)
