92ae514d6cd68123435ffd297134acd4b4499131
IKarbowiak <iga.karbowiak@mirumee.com>
2021-03-08 10:41:33 +0100

    Return variant quantity available only for available warehouses for given channel

--- saleor/graphql/product/types/products.py
+++ saleor/graphql/product/types/products.py
@@ -70,7 +70,7 @@ from ...utils import (
 )
 from ...utils.filters import reporting_period_to_date
 from ...warehouse.dataloaders import (
-    AvailableQuantityByProductVariantIdAndCountryCodeLoader,
+    AvailableQuantityByProductVariantIdCountryCodeAndChannelSlugLoader,
     StocksWithAvailableQuantityByProductVariantIdAndCountryCodeLoader,
 )
 from ...warehouse.types import Stock
@@ -296,9 +296,9 @@ class ProductVariant(ChannelContextTypeWithMetadata, CountableDjangoObjectType):
         if not root.node.track_inventory:
             return settings.MAX_CHECKOUT_LINE_QUANTITY
 
-        return AvailableQuantityByProductVariantIdAndCountryCodeLoader(
+        return AvailableQuantityByProductVariantIdCountryCodeAndChannelSlugLoader(
             info.context
-        ).load((root.node.id, country_code))
+        ).load((root.node.id, country_code, root.channel_slug))
 
     @staticmethod
     @permission_required(ProductPermissions.MANAGE_PRODUCTS)
