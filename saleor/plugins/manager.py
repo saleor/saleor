7e2570cdd277b7609b7963bacaeb8ca37b755a3a
fowczrek <filip.owczarek@defectprediction.net>
2021-03-11 13:06:18 +0100

    Remove redundat Opentracing spans

--- saleor/plugins/manager.py
+++ saleor/plugins/manager.py
@@ -95,19 +95,21 @@ class PluginsManager(PaymentInterface):
         will return previous_value.
         """
         plugin_id = getattr(plugin, "PLUGIN_ID", "")
+        plugin_method = getattr(plugin, method_name, NotImplemented)
+        if plugin_method == NotImplemented:
+            return previous_value
         with opentracing.global_tracer().start_active_span(
-            f"{plugin_id}.{method_name}"
-        ):
-            plugin_method = getattr(plugin, method_name, NotImplemented)
-            if plugin_method == NotImplemented:
-                return previous_value
-
+            f"{plugin_id}.{method_name}", finish_on_close=False
+        ) as scope:
             returned_value = plugin_method(
                 *args, **kwargs, previous_value=previous_value
             )
             if returned_value == NotImplemented:
                 return previous_value
-            return returned_value
+            # We should create spans for plugin_method with return data instead of
+            # `NotImplemented`
+            scope.span.finish()
+        return returned_value
 
     def change_user_address(
         self, address: "Address", address_type: Optional[str], user: Optional["User"]
