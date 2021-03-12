3f6ae70d2cec3eebb6696b91eff94a2144df636c
Maciej Korycinski <korycinski.maciej@gmail.com>
2021-03-12 08:16:36 +0100

    Add possibility to use bearer as an token prefix (#6996)
    * Add possibility to user bearer as an token prefix
    
    * Add info about bearer to changelog
    
    * Add tests to cover new auth prefix

--- saleor/core/jwt.py
+++ saleor/core/jwt.py
@@ -16,7 +16,7 @@ from .permissions import (
 
 JWT_ALGORITHM = "HS256"
 JWT_AUTH_HEADER = "HTTP_AUTHORIZATION"
-JWT_AUTH_HEADER_PREFIX = "JWT"
+AUTH_HEADER_PREFIXES = ["JWT", "BEARER"]
 JWT_ACCESS_TYPE = "access"
 JWT_REFRESH_TYPE = "refresh"
 JWT_THIRDPARTY_ACCESS_TYPE = "thirdparty"
@@ -114,9 +114,8 @@ def create_refresh_token(
 
 def get_token_from_request(request: WSGIRequest) -> Optional[str]:
     auth = request.META.get(JWT_AUTH_HEADER, "").split(maxsplit=1)
-    prefix = JWT_AUTH_HEADER_PREFIX
 
-    if len(auth) != 2 or auth[0].upper() != prefix:
+    if len(auth) != 2 or auth[0].upper() not in AUTH_HEADER_PREFIXES:
         return None
     return auth[1]
 
