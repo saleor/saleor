03ddac84c9fa4768a59ecaf324928658c5a9b910
Tomasz Szymanski <lime129@gmail.com>
2021-04-02 10:20:16 +0200

    Benchmarks for orders query - user and userEmail fields

--- saleor/graphql/order/tests/benchmark/fixtures.py
+++ saleor/graphql/order/tests/benchmark/fixtures.py
@@ -3,18 +3,35 @@ import uuid
 import pytest
 from prices import Money, TaxedMoney
 
+from .....account.models import User
 from .....order.models import Order
 
 
 @pytest.fixture
-def orders_for_benchmarks(channel_USD, address, customer_user):
+def users_for_benchmarks(address):
+    users = [
+        User(
+            email=f"john.doe.{i}@exmaple.com",
+            is_active=True,
+            default_billing_address=address.get_copy(),
+            default_shipping_address=address.get_copy(),
+            first_name="John",
+            last_name="Doe",
+        )
+        for i in range(10)
+    ]
+    return User.objects.bulk_create(users)
+
+
+@pytest.fixture
+def orders_for_benchmarks(channel_USD, address, users_for_benchmarks):
     orders = [
         Order(
             token=str(uuid.uuid4()),
             channel=channel_USD,
             billing_address=address.get_copy(),
             shipping_address=address.get_copy(),
-            user=customer_user,
+            user=users_for_benchmarks[i],
             total=TaxedMoney(net=Money(i, "USD"), gross=Money(i, "USD")),
         )
         for i in range(10)
