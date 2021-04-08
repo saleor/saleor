527f5c39a6b36a92b3f8baf368f7ccace62660fa
IKarbowiak <iga.karbowiak@mirumee.com>
2021-04-08 10:18:24 +0200

    Add signal for deleting DigitalContent

--- saleor/product/app.py
+++ saleor/product/app.py
@@ -6,8 +6,8 @@ class ProductAppConfig(AppConfig):
     name = "saleor.product"
 
     def ready(self):
-        from .models import Category, Collection
-        from .signals import delete_background_image
+        from .models import Category, Collection, DigitalContent
+        from .signals import delete_background_image, delete_digital_content_file
 
         post_delete.connect(
             delete_background_image,
@@ -18,4 +18,9 @@ class ProductAppConfig(AppConfig):
             delete_background_image,
             sender=Collection,
             dispatch_uid="delete_collection_background",
+        )
+        post_delete.connect(
+            delete_digital_content_file,
+            sender=DigitalContent,
+            dispatch_uid="delete_digital_content_file",
         )
--- saleor/product/signals.py
+++ saleor/product/signals.py
@@ -4,3 +4,8 @@ from ..core.tasks import delete_from_storage
 def delete_background_image(sender, instance, **kwargs):
     if img := instance.background_image:
         delete_from_storage.delay(img.name)
+
+
+def delete_digital_content_file(sender, instance, **kwargs):
+    if file := instance.content_file:
+        delete_from_storage(file.name)
