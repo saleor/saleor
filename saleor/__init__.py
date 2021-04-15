60f84565724290830fba2e2dfa139e8898b8e480
fowczrek <filip.owczarek@defectprediction.net>
2021-04-15 13:15:26 +0200

    Fix sdl queyr for federation

--- saleor/__init__.py
+++ saleor/__init__.py
@@ -1,4 +1,19 @@
+from graphql.utils import schema_printer
+
 from .celeryconf import app as celery_app
 
 __all__ = ["celery_app"]
 __version__ = "dev"
+
+
+def patched_print_object(type):
+    interfaces = type.interfaces
+    implemented_interfaces = (
+        " implements {}".format(" & ".join(i.name for i in interfaces))
+        if interfaces
+        else ""
+    )
+
+    return ("type {}{} {{\n" "{}\n" "}}").format(
+        type.name, implemented_interfaces, schema_printer._print_fields(type)
+    )
