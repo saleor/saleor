5c3459934ed56ea5882a02a456921f08b4198f1e
Tomasz Szymanski <lime129@gmail.com>
2021-04-19 13:33:32 +0200

    Added extra check to see if payment is active

--- saleor/payment/model_helpers.py
+++ saleor/payment/model_helpers.py
@@ -15,19 +15,22 @@ def get_last_payment(payments: List[Payment]):
 def get_total_captured(payments: List[Payment], fallback_currency: str):
     # FIXME adjust to multiple payments in the future
     if last_payment := get_last_payment(payments):
-        if last_payment.charge_status in (  # type: ignore
-            ChargeStatus.PARTIALLY_CHARGED,
-            ChargeStatus.FULLY_CHARGED,
-            ChargeStatus.PARTIALLY_REFUNDED,
-        ):
-            return Money(
-                last_payment.captured_amount, last_payment.currency  # type: ignore
+        if (
+            last_payment.charge_status
+            in (
+                ChargeStatus.PARTIALLY_CHARGED,
+                ChargeStatus.FULLY_CHARGED,
+                ChargeStatus.PARTIALLY_REFUNDED,
             )
+            and last_payment.is_active
+        ):
+            return Money(last_payment.captured_amount, last_payment.currency)
     return zero_money(fallback_currency)
 
 
 def get_total_authorized(payments: List[Payment], fallback_currency: str):
     # FIXME adjust to multiple payments in the future
     if last_payment := get_last_payment(payments):
-        return last_payment.get_authorized_amount()
+        if last_payment.is_active:
+            return last_payment.get_authorized_amount()
     return zero_money(fallback_currency)
