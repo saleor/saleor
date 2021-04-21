96bf7ec5bfd27bf558f4441c6881cfaed7fbcc94
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2021-04-21 10:10:36 +0200

    Fix export with empty assignment values (#7207)
    * Prevent export from failing when attribute assigment without values exists
    
    * Drop intermediary model when values are unassigned from instance
    
    * Update changelog
    
    * Add migration to drop assigned models without values
    
    Co-authored-by: Marcin Gębala <5421321+maarcingebala@users.noreply.github.com>

--- saleor/csv/tests/export/products_data/test_prepare_headers.py
+++ saleor/csv/tests/export/products_data/test_prepare_headers.py
@@ -43,10 +43,16 @@ def test_get_export_fields_and_headers_no_fields():
     assert file_headers == ["id"]
 
 
-def test_get_attributes_headers(product_with_multiple_values_attributes):
+def test_get_attributes_headers(
+    product_with_multiple_values_attributes, product_type_without_variant
+):
     # given
     attribute_ids = Attribute.objects.values_list("id", flat=True)
     export_info = {"attributes": attribute_ids}
+
+    product_type = product_with_multiple_values_attributes.product_type
+    product_attribute = product_type.product_attributes.first()
+    product_type_without_variant.product_attributes.add(product_attribute)
 
     # when
     attributes_headers = get_attributes_headers(export_info)
