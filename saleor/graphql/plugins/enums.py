6448b02f9de6fcb6e423d97c88afa1f119739524
Maciej Korycinski <maciej@mirumee.com>
2021-04-26 14:10:52 +0200

    Remove commented code

--- saleor/graphql/plugins/enums.py
+++ saleor/graphql/plugins/enums.py
@@ -9,11 +9,3 @@ ConfigurationTypeFieldEnum = to_enum(ConfigurationTypeField)
 class PluginConfigurationType(graphene.Enum):
     PER_CHANNEL = "per_channel"
     GLOBAL = "global"
-
-
-#
-# class UserSortField(graphene.Enum):
-#     FIRST_NAME = ["first_name", "last_name", "pk"]
-#     LAST_NAME = ["last_name", "first_name", "pk"]
-#     EMAIL = ["email"]
-#     ORDER_COUNT = ["order_count", "email"]
