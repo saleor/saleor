9d7854b519286f01a4536f64815c36b129af62c1
IKarbowiak <iga.karbowiak@mirumee.com>
2021-04-28 12:40:41 +0200

    Add order events for deleting order lines that are caused by deleting product or variant

--- saleor/graphql/product/utils.py
+++ saleor/graphql/product/utils.py
@@ -1,11 +1,13 @@
-from collections import defaultdict
-from typing import TYPE_CHECKING, Dict, List
+from collections import defaultdict, namedtuple
+from typing import TYPE_CHECKING, Dict, Iterable, List
 
 import graphene
 from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
 
 from ...core.tracing import traced_atomic_transaction
+from ...order import OrderStatus
+from ...order import models as order_models
 from ...warehouse.models import Stock
 
 if TYPE_CHECKING:
@@ -78,3 +80,27 @@ def create_stocks(
     except IntegrityError:
         msg = "Stock for one of warehouses already exists for this product variant."
         raise ValidationError(msg)
+
+
+DraftOrderLinesData = namedtuple(
+    "DraftOrderLinesData", ["order_to_lines_mapping", "line_pks", "order_pks"]
+)
+
+
+def get_draft_order_lines_data_for_variants(
+    variant_ids: Iterable[int],
+):
+    lines = order_models.OrderLine.objects.filter(
+        variant__id__in=variant_ids, order__status=OrderStatus.DRAFT
+    ).select_related("order")
+    order_to_lines_mapping: Dict[
+        order_models.Order, List[order_models.OrderLine]
+    ] = defaultdict(list)
+    line_pks = set()
+    order_pks = set()
+    for line in lines:
+        order_to_lines_mapping[line.order].append(line)
+        line_pks.add(line.pk)
+        order_pks.add(line.order_id)
+
+    return DraftOrderLinesData(order_to_lines_mapping, line_pks, order_pks)
