5f1fac5949758462ccd4f2af717f71ceae1e3111
Tran Ngoc Van <van.tranngoc@ncc.asia>
2021-05-10 14:41:05 +0700

    Migrate update

--- saleor/social/__init__.py
+++
@@ -1,8 +0,0 @@
-class ProductMediaTypes:
-    IMAGE = "IMAGE"
-    VIDEO = "VIDEO"
-
-    CHOICES = [
-        (IMAGE, "An uploaded image or an URL to an image"),
-        (VIDEO, "A URL to an external video"),
-    ]
