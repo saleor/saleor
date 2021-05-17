82afd6a02fa1a917b4835dfe7969950eea663fc4
Tomasz Szymanski <lime129@gmail.com>
2021-05-17 13:23:42 +0200

    Refcator transaction_with_commit_on_errors to use traced_atomic_transaction

--- saleor/core/transactions.py
+++ saleor/core/transactions.py
@@ -1,24 +1,20 @@
 from contextlib import contextmanager
 
-import opentracing
-from django.db import DatabaseError, transaction
+from django.db import DatabaseError
+
+from ..core.tracing import traced_atomic_transaction
 
 
 @contextmanager
 def transaction_with_commit_on_errors():
     """Perform transaction and raise an error in any occurred."""
     error = None
-    with transaction.atomic():
-        with opentracing.global_tracer().start_active_span(
-            "transaction_with_commit_on_errors"
-        ) as scope:
-            span = scope.span
-            span.set_tag(opentracing.tags.COMPONENT, "db")
-            try:
-                yield
-            except DatabaseError:
-                raise
-            except Exception as e:
-                error = e
+    with traced_atomic_transaction():
+        try:
+            yield
+        except DatabaseError:
+            raise
+        except Exception as e:
+            error = e
     if error:
         raise error
