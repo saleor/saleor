9565534dbcb9d989109b7ba2286a7fa2a4339a10
Tomasz Szymanski <lime129@gmail.com>
2021-05-20 14:13:33 +0200

    Replace straight replacing with slugify in invoice file name

--- saleor/plugins/invoicing/plugin.py
+++ saleor/plugins/invoicing/plugin.py
@@ -2,6 +2,7 @@ from typing import Any, Optional
 from uuid import uuid4
 
 from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 from ...core import JobStatus
 from ...invoice.models import Invoice
@@ -17,14 +18,6 @@ class InvoicingPlugin(BasePlugin):
     PLUGIN_DESCRIPTION = "Built-in saleor plugin that handles invoice creation."
     CONFIGURATION_PER_CHANNEL = False
 
-    @staticmethod
-    def sanitize_invoice_number(invoice_number):
-        """Invoice number contains slashes so it's unsafe as saved file would mimic a path.
-
-        Slashes should be replaced with hyphens.
-        """
-        return invoice_number.replace("/", "-")
-
     def invoice_request(
         self,
         order: "Order",
@@ -35,9 +28,9 @@ class InvoicingPlugin(BasePlugin):
         invoice.update_invoice(number=generate_invoice_number())
         file_content, creation_date = generate_invoice_pdf(invoice)
         invoice.created = creation_date
-        sanitized_invoice_number = self.sanitize_invoice_number(invoice.number)
+        slugified_invoice_number = slugify(invoice.number)
         invoice.invoice_file.save(
-            f"invoice-{sanitized_invoice_number}-order-{order.id}-{uuid4()}.pdf",
+            f"invoice-{slugified_invoice_number}-order-{order.id}-{uuid4()}.pdf",
             ContentFile(file_content),
         )
         invoice.status = JobStatus.SUCCESS
