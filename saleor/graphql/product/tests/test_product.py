edef494c59f2e9481e962874785bb0500f392931
fowczrek <filip.owczarek@defectprediction.net>
2021-05-25 11:53:37 +0200

    Return empty values list for attribute without choises

--- saleor/graphql/product/tests/test_product.py
+++ saleor/graphql/product/tests/test_product.py
@@ -6626,14 +6626,7 @@ def test_create_product_type_with_rich_text_attribute(
         },
         {
             "name": "Text",
-            "values": [
-                {
-                    "name": "Rich text attribute content.",
-                    "richText": json.dumps(
-                        rich_text_attribute.values.first().rich_text
-                    ),
-                }
-            ],
+            "values": [],
         },
     ]
     for attribute in data["productAttributes"]:
