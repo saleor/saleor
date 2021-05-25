4f8e25b8d7b49a8a0fad25ceaba801d435fa924c
IKarbowiak <iga.karbowiak@mirumee.com>
2021-05-25 08:34:22 +0200

    Allow filtering pages by ids

--- saleor/graphql/page/filters.py
+++ saleor/graphql/page/filters.py
@@ -1,16 +1,25 @@
 import django_filters
 from graphene_django.filter import GlobalIDMultipleChoiceFilter
 
-from ...page.models import Page
+from ...page import models
 from ..core.filters import MetadataFilterBase
 from ..core.types import FilterInputObjectType
+from ..utils import resolve_global_ids_to_primary_keys
 from ..utils.filters import filter_by_query_param
+from .types import Page
 
 
 def filter_page_search(qs, _, value):
     page_fields = ["content", "slug", "title"]
     qs = filter_by_query_param(qs, value, page_fields)
     return qs
+
+
+def filter_page_ids(qs, _, value):
+    if not value:
+        return qs
+    _, page_pks = resolve_global_ids_to_primary_keys(value, Page)
+    return qs.filter(id__in=page_pks)
 
 
 def filter_page_type_search(qs, _, value):
@@ -23,9 +32,10 @@ def filter_page_type_search(qs, _, value):
 class PageFilter(MetadataFilterBase):
     search = django_filters.CharFilter(method=filter_page_search)
     page_types = GlobalIDMultipleChoiceFilter(field_name="page_type")
+    ids = GlobalIDMultipleChoiceFilter(method=filter_page_ids)
 
     class Meta:
-        model = Page
+        model = models.Page
         fields = ["search"]
 
 
