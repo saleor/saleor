0631e20eeb12a679948e42758a60a225447d56c7
fowczrek <filip.owczarek@defectprediction.net>
2021-06-10 12:29:49 +0200

    Add tracing info to logs

--- saleor/core/logging.py
+++ saleor/core/logging.py
@@ -1,6 +1,7 @@
 import platform
 import time
 
+import opentracing
 from pythonjsonlogger.jsonlogger import JsonFormatter as BaseFormatter
 
 
@@ -10,3 +11,12 @@ class JsonFormatter(BaseFormatter):
     def add_fields(self, log_record, record, message_dict):
         super().add_fields(log_record, record, message_dict)
         log_record["hostname"] = platform.node()
+        # Add tracing info to logs for Datadog
+        active_span = opentracing.global_tracer().active_span
+        span_id = 0
+        trace_id = 0
+        if active_span:
+            span_id = active_span.context.span_id
+            trace_id = active_span.context.trace_id
+        log_record["dd"] = {"trace_id": trace_id, "span_id": span_id}
+
