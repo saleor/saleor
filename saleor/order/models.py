b456c99df4ccbd16816d1aeb06f6196614d20df7
fowczrek <filip.owczarek@defectprediction.net>
2021-06-14 12:23:00 +0200

    Fix duplicated customers

--- saleor/order/models.py
+++ saleor/order/models.py
@@ -16,7 +16,6 @@ from django_measurement.models import MeasurementField
 from django_prices.models import MoneyField, TaxedMoneyField
 from measurement.measures import Weight
 
-from ..account.models import Address
 from ..channel.models import Channel
 from ..core.models import ModelWithMetadata
 from ..core.permissions import OrderPermissions
@@ -100,10 +99,18 @@ class Order(ModelWithMetadata):
     )
     tracking_client_id = models.CharField(max_length=36, blank=True, editable=False)
     billing_address = models.ForeignKey(
-        Address, related_name="+", editable=False, null=True, on_delete=models.SET_NULL
+        "account.Address",
+        related_name="+",
+        editable=False,
+        null=True,
+        on_delete=models.SET_NULL,
     )
     shipping_address = models.ForeignKey(
-        Address, related_name="+", editable=False, null=True, on_delete=models.SET_NULL
+        "account.Address",
+        related_name="+",
+        editable=False,
+        null=True,
+        on_delete=models.SET_NULL,
     )
     user_email = models.EmailField(blank=True, default="")
     original = models.ForeignKey(
