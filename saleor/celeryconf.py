8f3c2e75dd229a75509501050b273405fdca68cd
Tomasz Szymanski <lime129@gmail.com>
2021-06-21 14:51:03 +0200

    Skip celery logging configuration

--- saleor/celeryconf.py
+++ saleor/celeryconf.py
@@ -1,9 +1,20 @@
 import os
 
 from celery import Celery
+from celery.signals import setup_logging
 from django.conf import settings
 
 from .plugins import discover_plugins_modules
+
+
+@setup_logging.connect
+def setup_celery_logging(*_args, **_kwargs):
+    """Skip default Celery logging configuration.
+
+    Will rely on Django to set up the base root logger.
+    """
+    pass
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "saleor.settings")
 
