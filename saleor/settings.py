e767b353b163e1370b1f5c3a8c239038e6acc83e
Nicolas Stalder <n+github@stalder.io>
2021-06-29 10:21:46 -0400

    Parse GS_EXPIRATION, making GS_QUERYSTRING_AUTH work (#7483)

--- saleor/settings.py
+++ saleor/settings.py
@@ -429,7 +429,7 @@ GS_AUTO_CREATE_BUCKET = get_bool_from_env("GS_AUTO_CREATE_BUCKET", False)
 GS_QUERYSTRING_AUTH = get_bool_from_env("GS_QUERYSTRING_AUTH", False)
 GS_DEFAULT_ACL = os.environ.get("GS_DEFAULT_ACL", None)
 GS_MEDIA_CUSTOM_ENDPOINT = os.environ.get("GS_MEDIA_CUSTOM_ENDPOINT", None)
-GS_EXPIRATION = os.environ.get("GS_EXPIRATION", None)
+GS_EXPIRATION = timedelta(seconds=parse(os.environ.get("GS_EXPIRATION", "1 day")))
 GS_FILE_OVERWRITE = get_bool_from_env("GS_FILE_OVERWRITE", True)
 
 # If GOOGLE_APPLICATION_CREDENTIALS is set there is no need to load OAuth token
