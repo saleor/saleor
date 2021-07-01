f6231735cc49b8517d621414038f152f8427ac2a
Cezary Miącz <czarek.miacz@gmail.com>
2021-07-01 12:54:05 +0200

    Add Authorization-Bearer to allowed CORS headers

--- saleor/graphql/views.py
+++ saleor/graphql/views.py
@@ -106,7 +106,7 @@ class GraphQLView(View):
                     response["Access-Control-Allow-Methods"] = "POST, OPTIONS"
                     response[
                         "Access-Control-Allow-Headers"
-                    ] = "Origin, Content-Type, Accept, Authorization"
+                    ] = "Origin, Content-Type, Accept, Authorization, Authorization-Bearer"
                     response["Access-Control-Allow-Credentials"] = "true"
                     break
         return response
