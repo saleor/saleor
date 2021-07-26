957c281114389465678412ec7324d8dd379b078a
IKarbowiak <iga.karbowiak@mirumee.com>
2021-07-26 14:48:52 +0200

    Fix flake8 and black issues

--- saleor/graphql/meta/tests/test_meta_queries.py
+++ saleor/graphql/meta/tests/test_meta_queries.py
@@ -1599,90 +1599,6 @@ def test_query_private_meta_for_me_as_staff_with_manage_staff(
     assert metadata["value"] == PRIVATE_VALUE
 
 
-QUERY_GIFT_CARD_PUBLIC_META = """
-    query giftCardMeta($id: ID!){
-        giftCard(id: $id){
-            metadata{
-                key
-                value
-            }
-        }
-    }
-"""
-
-
-def test_query_public_meta_for_gift_card_as_anonymous_user(api_client, gift_card):
-    # given
-    gift_card.store_value_in_metadata({PUBLIC_KEY: PUBLIC_VALUE})
-    gift_card.save(update_fields=["metadata"])
-    variables = {"id": graphene.Node.to_global_id("GiftCard", gift_card.pk)}
-
-    # when
-    response = api_client.post_graphql(QUERY_GIFT_CARD_PUBLIC_META, variables)
-
-    # then
-    assert_no_permission(response)
-
-
-def test_query_public_meta_for_gift_card_as_customer(user_api_client, gift_card):
-    # given
-    gift_card.store_value_in_metadata({PUBLIC_KEY: PUBLIC_VALUE})
-    gift_card.save(update_fields=["metadata"])
-    variables = {"id": graphene.Node.to_global_id("GiftCard", gift_card.pk)}
-
-    # when
-    response = user_api_client.post_graphql(QUERY_GIFT_CARD_PUBLIC_META, variables)
-
-    # then
-    assert_no_permission(response)
-
-
-def test_query_public_meta_for_gift_card_as_staff(
-    staff_api_client, gift_card, permission_manage_gift_card
-):
-    # given
-    gift_card.store_value_in_metadata({PUBLIC_KEY: PUBLIC_VALUE})
-    gift_card.save(update_fields=["metadata"])
-    variables = {"id": graphene.Node.to_global_id("GiftCard", gift_card.pk)}
-
-    # when
-    response = staff_api_client.post_graphql(
-        QUERY_GIFT_CARD_PUBLIC_META,
-        variables,
-        [permission_manage_gift_card],
-        check_no_permissions=False,
-    )
-    content = get_graphql_content(response)
-
-    # then
-    metadata = content["data"]["giftCard"]["metadata"][0]
-    assert metadata["key"] == PUBLIC_KEY
-    assert metadata["value"] == PUBLIC_VALUE
-
-
-def test_query_public_meta_for_gift_card_as_app(
-    app_api_client, gift_card, permission_manage_gift_card
-):
-    # given
-    gift_card.store_value_in_metadata({PUBLIC_KEY: PUBLIC_VALUE})
-    gift_card.save(update_fields=["metadata"])
-    variables = {"id": graphene.Node.to_global_id("GiftCard", gift_card.pk)}
-
-    # when
-    response = app_api_client.post_graphql(
-        QUERY_GIFT_CARD_PUBLIC_META,
-        variables,
-        [permission_manage_gift_card],
-        check_no_permissions=False,
-    )
-    content = get_graphql_content(response)
-
-    # then
-    metadata = content["data"]["giftCard"]["metadata"][0]
-    assert metadata["key"] == PUBLIC_KEY
-    assert metadata["value"] == PUBLIC_VALUE
-
-
 QUERY_USER_PRIVATE_META = """
     query userMeta($id: ID!){
         user(id: $id){
@@ -3458,6 +3374,7 @@ def test_query_private_meta_for_sale_as_anonymous_user(api_client, sale):
 
     # then
     assert_no_permission(response)
+
 
 def test_query_private_meta_for_sale_as_customer(user_api_client, sale):
     # given
