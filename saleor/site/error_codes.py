1f743e4efb98f29cf8a6c53f46033d37b8be6cc5
IKarbowiak <iga.karbowiak@mirumee.com>
2021-07-29 10:10:18 +0200

    Add GiftCardSettingsUpdate mutation

--- saleor/site/error_codes.py
+++ saleor/site/error_codes.py
@@ -4,3 +4,13 @@ from enum import Enum
 class SiteErrorCode(Enum):
     FORBIDDEN_CHARACTER = "forbidden_character"
     GRAPHQL_ERROR = "graphql_error"
+
+
+class OrderSettingsErrorCode(Enum):
+    INVALID = "invalid"
+
+
+class GiftCardSettingsErrorCode(Enum):
+    INVALID = "invalid"
+    REQUIRED = "required"
+    GRAPHQL_ERROR = "graphql_error"
