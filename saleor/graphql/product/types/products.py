9522285120bf0880199a151ee65beff03fec9168
IKarbowiak <iga.karbowiak@mirumee.com>
2021-07-29 12:49:38 +0200

    Make kind field on ProductType required

--- saleor/graphql/product/types/products.py
+++ saleor/graphql/product/types/products.py
@@ -927,7 +927,7 @@ class Product(ChannelContextTypeWithMetadata, CountableDjangoObjectType):
 
 @key(fields="id")
 class ProductType(CountableDjangoObjectType):
-    kind = ProductTypeKindEnum(description="The product type kind.")
+    kind = ProductTypeKindEnum(description="The product type kind.", required=True)
     products = ChannelContextFilterConnectionField(
         Product,
         channel=graphene.String(
