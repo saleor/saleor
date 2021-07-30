d9f74822f28afdc48bde9b493bb81fa1fbb74da0
Aleksander Spyra <aleksander.spyra@mirumee.com>
2021-07-30 09:48:32 +0200

    Minor refactor

--- saleor/graphql/order/mutations/draft_orders.py
+++ saleor/graphql/order/mutations/draft_orders.py
@@ -397,9 +397,10 @@ class DraftOrderComplete(BaseMutation):
                 line_data = OrderLineData(
                     line=line, quantity=line.quantity, variant=line.variant
                 )
+                channel_slug = order.channel.slug
                 try:
-                    allocate_stocks([line_data], country, order.channel.slug)
-                    allocate_preorders([line_data], order.channel.slug)
+                    allocate_stocks([line_data], country, channel_slug)
+                    allocate_preorders([line_data], channel_slug)
                 except InsufficientStock as exc:
                     errors = prepare_insufficient_stock_order_validation_errors(exc)
                     raise ValidationError({"lines": errors})
