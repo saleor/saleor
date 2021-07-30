5b7cf5e1fabe53b8dcfea424537496aacb491e2a
IKarbowiak <iga.karbowiak@mirumee.com>
2021-07-30 11:22:12 +0200

    Add giftCardBulkActivate mutation

--- saleor/graphql/giftcard/tests/benchmark/test_gift_card_mutations.py
+++ saleor/graphql/giftcard/tests/benchmark/test_gift_card_mutations.py
@@ -3,6 +3,7 @@ from datetime import date, timedelta
 import graphene
 import pytest
 
+from .....giftcard.models import GiftCard
 from ....tests.utils import get_graphql_content
 from ...enums import GiftCardExpiryTypeEnum
 
@@ -269,3 +270,47 @@ def test_update_gift_card(
     content = get_graphql_content(response)
     data = content["data"]["giftCardUpdate"]["giftCard"]
     assert data
+
+
+MUTATION_GIFT_CARD_BULK_ACTIVATE = """
+    mutation GiftCardBulkActivate($ids: [ID]!) {
+        giftCardBulkActivate(ids: $ids) {
+            count
+            errors {
+                code
+                field
+            }
+        }
+    }
+"""
+
+
+@pytest.mark.django_db
+@pytest.mark.count_queries(autouse=False)
+def test_gift_card_bulk_activate_by_staff(
+    staff_api_client,
+    gift_cards_for_benchmarks,
+    permission_manage_gift_card,
+    count_queries,
+):
+    # given
+    for card in gift_cards_for_benchmarks:
+        card.is_active = False
+    GiftCard.objects.bulk_update(gift_cards_for_benchmarks, ["is_active"])
+
+    ids = [
+        graphene.Node.to_global_id("GiftCard", card.pk)
+        for card in gift_cards_for_benchmarks
+    ]
+    variables = {"ids": ids}
+
+    # when
+    response = staff_api_client.post_graphql(
+        MUTATION_GIFT_CARD_BULK_ACTIVATE,
+        variables,
+        permissions=(permission_manage_gift_card,),
+    )
+
+    # then
+    content = get_graphql_content(response)
+    assert content["data"]["giftCardBulkActivate"]["count"] == len(ids)
