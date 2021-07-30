32c38d54e7f0a7b59314fb2dfedacb4c492d21c9
IKarbowiak <iga.karbowiak@mirumee.com>
2021-07-30 11:22:12 +0200

    Add giftCardBulkActivate mutation

--- saleor/graphql/giftcard/bulk_mutations.py
+++ saleor/graphql/giftcard/bulk_mutations.py
@@ -1,8 +1,9 @@
 import graphene
 
 from ...core.permissions import GiftcardPermissions
-from ...giftcard import models
-from ..core.mutations import ModelBulkDeleteMutation
+from ...core.tracing import traced_atomic_transaction
+from ...giftcard import events, models
+from ..core.mutations import BaseBulkMutation, ModelBulkDeleteMutation
 from ..core.types.common import GiftCardError
 
 
@@ -17,3 +18,26 @@ class GiftCardBulkDelete(ModelBulkDeleteMutation):
         model = models.GiftCard
         permissions = (GiftcardPermissions.MANAGE_GIFT_CARD,)
         error_type_class = GiftCardError
+
+
+class GiftCardBulkActivate(BaseBulkMutation):
+    class Arguments:
+        ids = graphene.List(
+            graphene.ID, required=True, description="List of gift card IDs to activate."
+        )
+
+    class Meta:
+        description = "Activate gift cards."
+        model = models.GiftCard
+        permissions = (GiftcardPermissions.MANAGE_GIFT_CARD,)
+        error_type_class = GiftCardError
+
+    @classmethod
+    @traced_atomic_transaction()
+    def bulk_action(cls, info, queryset):
+        queryset = queryset.filter(is_active=False)
+        gift_card_ids = [gift_card.id for gift_card in queryset]
+        queryset.update(is_active=True)
+        events.gift_cards_activated(
+            gift_card_ids, user=info.context.user, app=info.context.app
+        )
