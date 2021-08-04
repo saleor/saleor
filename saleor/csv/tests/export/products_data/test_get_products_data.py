f52a6b1ff1bc95ad9363784fc495252ce9385645
Aleksander Spyra <aleksander.spyra@mirumee.com>
2021-08-04 12:20:08 +0200

    SALEOR-3806 update product export

--- saleor/csv/tests/export/products_data/test_get_products_data.py
+++ saleor/csv/tests/export/products_data/test_get_products_data.py
@@ -108,6 +108,8 @@ def test_get_products_data(product, product_with_image, collection, image, chann
                 "variant_weight": (
                     "{} g".foramt(int(variant.weight.value)) if variant.weight else ""
                 ),
+                "variants__is_preorder": variant.is_preorder,
+                "variants__preorder_end_date": variant.preorder_end_date,
             }
             data.update(product_data)
 
