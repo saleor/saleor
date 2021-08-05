2e958fd9e28769c920179ad97f0803356e3a7e2d
Tomasz Szymanski <lime129@gmail.com>
2021-08-05 13:52:42 +0200

    New payload structure

--- saleor/giftcard/notifications.py
+++ saleor/giftcard/notifications.py
@@ -5,11 +5,14 @@ from ..core.notify_events import NotifyEventType
 def send_gift_card_notification(user, app, email, gift_card, manager):
     """Trigger sending a gift card notification for the given recipient."""
     payload = {
-        "user_id": user.id if user else None,
-        "app_id": app.id if app else None,
+        "requester": {
+            "user_id": user.id if user else None,
+            "email": user.email if user else None,
+            "app_id": app.id if app else None,
+            "app_name": app.name if app else None,
+        },
         "recipient_email": email,
-        "gift_card_id": gift_card.id,
-        "code": gift_card.code,
+        "gift_card": {"id": gift_card.id, "code": gift_card.code},
         **get_site_context(),
     }
     manager.notify(NotifyEventType.SEND_GIFT_CARD, payload=payload)
