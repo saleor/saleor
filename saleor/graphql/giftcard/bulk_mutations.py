3d94bd2ea902c21c4fe21bddc39c27daa0c7a625
Tomasz Szymanski <lime129@gmail.com>
2021-08-06 13:06:51 +0200

    Gift card bulk deactivate mutation

--- saleor/graphql/giftcard/bulk_mutations.py
+++ saleor/graphql/giftcard/bulk_mutations.py
@@ -41,3 +41,28 @@ class GiftCardBulkActivate(BaseBulkMutation):
         events.gift_cards_activated(
             gift_card_ids, user=info.context.user, app=info.context.app
         )
+
+
+class GiftCardBulkDeactivate(BaseBulkMutation):
+    class Arguments:
+        ids = graphene.List(
+            graphene.ID,
+            required=True,
+            description="List of gift card IDs to deactivate.",
+        )
+
+    class Meta:
+        description = "Deactivate gift cards."
+        model = models.GiftCard
+        permissions = (GiftcardPermissions.MANAGE_GIFT_CARD,)
+        error_type_class = GiftCardError
+
+    @classmethod
+    @traced_atomic_transaction()
+    def bulk_action(cls, info, queryset):
+        queryset = queryset.filter(is_active=True)
+        gift_card_ids = [gift_card.id for gift_card in queryset]
+        queryset.update(is_active=True)
+        events.gift_cards_deactivated(
+            gift_card_ids, user=info.context.user, app=info.context.app
+        )
