9bb5acefda2e070f2a58c4ed09f3d5e42a271549
IKarbowiak <iga.karbowiak@mirumee.com>
2021-08-10 09:19:46 +0200

    Add used by email gift card valdiation

--- saleor/checkout/utils.py
+++ saleor/checkout/utils.py
@@ -486,7 +486,10 @@ def add_promo_code_to_checkout(
         )
     elif promo_code_is_gift_card(promo_code):
         add_gift_card_code_to_checkout(
-            checkout_info.checkout, promo_code, checkout_info.channel.currency_code
+            checkout_info.checkout,
+            checkout_info.get_customer_email(),
+            promo_code,
+            checkout_info.channel.currency_code,
         )
     else:
         raise InvalidPromoCode()
