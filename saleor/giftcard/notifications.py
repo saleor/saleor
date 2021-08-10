b9f43683c4686a7b86f0100bfd963de073f1acb0
Tomasz Szymanski <lime129@gmail.com>
2021-08-10 09:51:46 +0200

    Email context improvements

--- saleor/giftcard/notifications.py
+++ saleor/giftcard/notifications.py
@@ -13,13 +13,15 @@ def send_gift_card_notification(user, app, email, gift_card, manager):
     """Trigger sending a gift card notification for the given recipient."""
     payload = {
         "requester": {
+            "user_id": user.id if user else None,
             "email": user.email if user else None,
+            "app_id": app.id if app else None,
             "app_name": app.name if app else None,
         },
         "recipient": {
-            "email": email,
             "name": _get_recipient_name(email),
         },
+        "recipient_email": email,
         "gift_card": {
             "id": gift_card.id,
             "code": gift_card.code,
