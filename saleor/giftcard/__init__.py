3d2d12b645aeea18516d5f65fd65df0bee53f396
IKarbowiak <iga.karbowiak@mirumee.com>
2021-08-10 15:48:38 +0200

    Extend gift card events

--- saleor/giftcard/__init__.py
+++ saleor/giftcard/__init__.py
@@ -22,9 +22,11 @@ class GiftCardEvents:
     DEACTIVATED = "deactivated"
     BALANCE_RESET = "balance_reset"
     EXPIRY_SETTINGS_UPDATED = "expiry_settings_updated"
+    EXPIRY_DATE_SET = "expiry_date_set"
     SENT_TO_CUSTOMER = "sent_to_customer"
     RESENT = "resent"
     NOTE_ADDED = "note_added"
+    USED_IN_ORDER = "used_in_order"
 
     CHOICES = [
         (ISSUED, "The gift card was created be staff user or app."),
@@ -34,7 +36,9 @@ class GiftCardEvents:
         (DEACTIVATED, "The gift card was deactivated."),
         (BALANCE_RESET, "The gift card balance was reset."),
         (EXPIRY_SETTINGS_UPDATED, "The gift card expiry settings was updated."),
+        (EXPIRY_DATE_SET, "The expiry date was set."),
         (SENT_TO_CUSTOMER, "The gift card was sent to the customer."),
         (RESENT, "The gift card was resent to the customer."),
         (NOTE_ADDED, "A note was added to the gift card."),
+        (USED_IN_ORDER, "The gift card was used in order."),
     ]
