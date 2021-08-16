74540c86abbb1ed6196b10feb75266627b06c51f
mmarkusik <magdalena.markusik@mirumee.com>
2021-08-16 15:19:49 +0200

    Gift cards CRUD (#7681)
    * Update gift card model
    
    * Add GiftCardEvent model
    
    * Remove start_date field from gift card model
    
    * Extend TimePeriodType with day
    
    * Update gift card type
    
    * Add tests for gift card meta queries and mutations
    
    * Add GiftCardEvent model
    
    * Add benchamrk tests for gift card and optimize gift card types
    
    * Add tracing to expiry and balance gift card event resolvers
    
    * Fix flake8 and black issues
    
    * Update GiftCardCreate mutation
    
    * Update GiftCard.events field and fix typos in gift card mutations
    
    * Add gift card filter
    
    * Update giftCardUpdate mutations
    
    * Add giftCardDelete mutation
    
    * Add channelCurrencies on shop type
    
    * Add tests to GiftCardActivate and GiftCardDeactivate mutations
    
    * Drop giftCard expiry_type default value
    
    * Update giftCardUpdate benchamrk test
    
    * Do not allow defining balance currency in GiftCardUpdate mutation
    
    * Update promo code format
    
    * Add default value to giftcard.expiry_type field
    
    * Fix gift card migration
    
    Co-authored-by: IKarbowiak <iga.karbowiak@mirumee.com>

---
+++ saleor/graphql/giftcard/enums.py
@@ -0,0 +1,5 @@
+from ...giftcard import GiftCardEvents, GiftCardExpiryType
+from ..core.enums import to_enum
+
+GiftCardExpiryTypeEnum = to_enum(GiftCardExpiryType)
+GiftCardEventsEnum = to_enum(GiftCardEvents)
