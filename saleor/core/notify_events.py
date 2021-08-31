fe41acbf94de8342fe75e57c6b4de9e1b81ca4b3
IKarbowiak <iga.karbowiak@mirumee.com>
2021-08-31 15:26:35 +0200

    Allow exporting gift cards

--- saleor/core/notify_events.py
+++ saleor/core/notify_events.py
@@ -37,13 +37,13 @@ class UserNotifyEvent:
 class AdminNotifyEvent:
     ACCOUNT_SET_STAFF_PASSWORD = "account_set_staff_password"
     ACCOUNT_STAFF_RESET_PASSWORD = "account_staff_reset_password"
-    CSV_PRODUCT_EXPORT_SUCCESS = "csv_export_products_success"
+    CSV_EXPORT_SUCCESS = "csv_export_success"
     CSV_EXPORT_FAILED = "csv_export_failed"
     STAFF_ORDER_CONFIRMATION = "staff_order_confirmation"
 
     CHOICES = [
         ACCOUNT_SET_STAFF_PASSWORD,
-        CSV_PRODUCT_EXPORT_SUCCESS,
+        CSV_EXPORT_SUCCESS,
         CSV_EXPORT_FAILED,
         STAFF_ORDER_CONFIRMATION,
         ACCOUNT_STAFF_RESET_PASSWORD,
