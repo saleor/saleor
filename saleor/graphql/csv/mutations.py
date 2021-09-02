23042aa042da1cd64568a4e46dc0f6f3b3403694
IKarbowiak <iga.karbowiak@mirumee.com>
2021-09-02 09:28:46 +0200

    Add exportGiftCards muttaion

--- saleor/graphql/csv/mutations.py
+++ saleor/graphql/csv/mutations.py
@@ -3,20 +3,73 @@ from typing import Dict, List, Mapping, Union
 import graphene
 from django.core.exceptions import ValidationError
 
-from ...core.permissions import ProductPermissions
+from ...core.permissions import GiftcardPermissions, ProductPermissions
 from ...csv import models as csv_models
 from ...csv.events import export_started_event
-from ...csv.tasks import export_products_task
+from ...csv.tasks import export_gift_cards_task, export_products_task
 from ..attribute.types import Attribute
 from ..channel.types import Channel
+from ..core.descriptions import ADDED_IN_31
 from ..core.enums import ExportErrorCode
 from ..core.mutations import BaseMutation
 from ..core.types.common import ExportError
+from ..giftcard.filters import GiftCardFilterInput
+from ..giftcard.types import GiftCard
 from ..product.filters import ProductFilterInput
 from ..product.types import Product
 from ..warehouse.types import Warehouse
 from .enums import ExportScope, FileTypeEnum, ProductFieldEnum
 from .types import ExportFile
+
+
+class BaseExportMutation(BaseMutation):
+    export_file = graphene.Field(
+        ExportFile,
+        description=(
+            "The newly created export file job which is responsible for export data."
+        ),
+    )
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def get_scope(cls, input, only_type) -> Mapping[str, Union[list, dict, str]]:
+        scope = input["scope"]
+        if scope == ExportScope.IDS.value:  # type: ignore
+            return cls.clean_ids(input, only_type)
+        elif scope == ExportScope.FILTER.value:  # type: ignore
+            return cls.clean_filter(input)
+        return {"all": ""}
+
+    @classmethod
+    def clean_ids(cls, input, only_type) -> Dict[str, List[str]]:
+        ids = input.get("ids", [])
+        if not ids:
+            raise ValidationError(
+                {
+                    "ids": ValidationError(
+                        "You must provide at least one id.",
+                        code=ExportErrorCode.REQUIRED.value,
+                    )
+                }
+            )
+        pks = cls.get_global_ids_or_error(ids, only_type=only_type, field="ids")
+        return {"ids": pks}
+
+    @staticmethod
+    def clean_filter(input) -> Dict[str, dict]:
+        filter = input.get("filter")
+        if not filter:
+            raise ValidationError(
+                {
+                    "filter": ValidationError(
+                        "You must provide filter input.",
+                        code=ExportErrorCode.REQUIRED.value,
+                    )
+                }
+            )
+        return {"filter": filter}
 
 
 class ExportInfoInput(graphene.InputObjectType):
@@ -47,7 +100,7 @@ class ExportProductsInput(graphene.InputObjectType):
     )
     ids = graphene.List(
         graphene.NonNull(graphene.ID),
-        description="List of products IDS to export.",
+        description="List of products IDs to export.",
         required=False,
     )
     export_info = ExportInfoInput(
@@ -57,17 +110,10 @@ class ExportProductsInput(graphene.InputObjectType):
     file_type = FileTypeEnum(description="Type of exported file.", required=True)
 
 
-class ExportProducts(BaseMutation):
-    export_file = graphene.Field(
-        ExportFile,
-        description=(
-            "The newly created export file job which is responsible for export data."
-        ),
-    )
-
+class ExportProducts(BaseExportMutation):
     class Arguments:
         input = ExportProductsInput(
-            required=True, description="Fields required to export product data"
+            required=True, description="Fields required to export product data."
         )
 
     class Meta:
@@ -79,7 +125,7 @@ class ExportProducts(BaseMutation):
     @classmethod
     def perform_mutation(cls, root, info, **data):
         input = data["input"]
-        scope = cls.get_products_scope(input)
+        scope = cls.get_scope(input, Product)
         export_info = cls.get_export_info(input["export_info"])
         file_type = input["file_type"]
 
@@ -92,44 +138,6 @@ class ExportProducts(BaseMutation):
 
         export_file.refresh_from_db()
         return cls(export_file=export_file)
-
-    @classmethod
-    def get_products_scope(cls, input) -> Mapping[str, Union[list, dict, str]]:
-        scope = input["scope"]
-        if scope == ExportScope.IDS.value:  # type: ignore
-            return cls.clean_ids(input)
-        elif scope == ExportScope.FILTER.value:  # type: ignore
-            return cls.clean_filter(input)
-        return {"all": ""}
-
-    @classmethod
-    def clean_ids(cls, input) -> Dict[str, List[str]]:
-        ids = input.get("ids", [])
-        if not ids:
-            raise ValidationError(
-                {
-                    "ids": ValidationError(
-                        "You must provide at least one product id.",
-                        code=ExportErrorCode.REQUIRED.value,
-                    )
-                }
-            )
-        pks = cls.get_global_ids_or_error(ids, only_type=Product, field="ids")
-        return {"ids": pks}
-
-    @staticmethod
-    def clean_filter(input) -> Dict[str, dict]:
-        filter = input.get("filter")
-        if not filter:
-            raise ValidationError(
-                {
-                    "filter": ValidationError(
-                        "You must provide filter input.",
-                        code=ExportErrorCode.REQUIRED.value,
-                    )
-                }
-            )
-        return {"filter": filter}
 
     @classmethod
     def get_export_info(cls, export_info_input):
@@ -156,3 +164,46 @@ class ExportProducts(BaseMutation):
             return
         pks = cls.get_global_ids_or_error(ids, only_type=graphene_type, field=field)
         return pks
+
+
+class ExportGiftCardsInput(graphene.InputObjectType):
+    scope = ExportScope(
+        description="Determine which gift cards should be exported.", required=True
+    )
+    filter = GiftCardFilterInput(
+        description="Filtering options for gift cards.", required=False
+    )
+    ids = graphene.List(
+        graphene.NonNull(graphene.ID),
+        description="List of gift cards IDs to export.",
+        required=False,
+    )
+    file_type = FileTypeEnum(description="Type of exported file.", required=True)
+
+
+class ExportGiftCards(BaseExportMutation):
+    class Arguments:
+        input = ExportGiftCardsInput(
+            required=True, description="Fields required to export gift cards data."
+        )
+
+    class Meta:
+        description = f"{ADDED_IN_31} Export gift cards to csv file."
+        permissions = (GiftcardPermissions.MANAGE_GIFT_CARD,)
+        error_type_class = ExportError
+
+    @classmethod
+    def perform_mutation(cls, root, info, **data):
+        input = data["input"]
+        scope = cls.get_scope(input, GiftCard)
+        file_type = input["file_type"]
+
+        app = info.context.app
+        kwargs = {"app": app} if app else {"user": info.context.user}
+
+        export_file = csv_models.ExportFile.objects.create(**kwargs)
+        export_started_event(export_file=export_file, **kwargs)
+        export_gift_cards_task.delay(export_file.pk, scope, file_type)
+
+        export_file.refresh_from_db()
+        return cls(export_file=export_file)
