cd427b5d3b7213a058d4e02aa442aa304d6a1402
Dawid Matuszczyk <d.matuszczyk98@gmail.com>
2021-09-06 12:38:03 +0200

    Fix checking existing lines quantity 3.1 (#8040)
    * WIP Fix checking existing lines quantity
    
    * Fix checkout bug with wrong available stock calculation
    
    * fix after review
    
    * Fix checkout bug with wrong available stock calculation
    
    Co-authored-by: Marcin Gębala <maarcin.gebala@gmail.com>

--- saleor/warehouse/availability.py
+++ saleor/warehouse/availability.py
@@ -47,6 +47,7 @@ def check_stock_quantity_bulk(
     quantities: Iterable[int],
     channel_slug: str,
     additional_filter_lookup: Optional[Dict[str, Any]] = None,
+    existing_lines: Iterable = None,
 ):
     """Validate if there is stock available for given variants in given country.
 
@@ -67,7 +68,13 @@ def check_stock_quantity_bulk(
         variant_stocks[stock.product_variant_id].append(stock)
 
     insufficient_stocks: List[InsufficientStockData] = []
+    variants_quantities = {
+        line.variant.pk: line.line.quantity for line in existing_lines or []
+    }
     for variant, quantity in zip(variants, quantities):
+
+        quantity += variants_quantities.get(variant.pk, 0)
+
         stocks = variant_stocks.get(variant.pk, [])
         available_quantity = sum(
             [stock.available_quantity for stock in stocks]  # type: ignore
