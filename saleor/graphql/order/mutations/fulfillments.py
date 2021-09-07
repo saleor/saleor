7019789cbfdbc976fc34fb70cf5e923dfbca991e
Tomasz Szymanski <lime129@gmail.com>
2021-09-07 16:39:58 +0200

    Secure waiting fulfillments amount

--- saleor/graphql/order/mutations/fulfillments.py
+++ saleor/graphql/order/mutations/fulfillments.py
@@ -93,7 +93,19 @@ class OrderFulfill(BaseMutation):
     @classmethod
     def clean_lines(cls, order_lines, quantities):
         for order_line, line_quantities in zip(order_lines, quantities):
-            line_quantity_unfulfilled = order_line.quantity_unfulfilled
+            quantity_awaiting = sum(
+                [
+                    f_line.quantity
+                    if f_line.fulfillment.status
+                    == FulfillmentStatus.WAITING_FOR_APPROVAL
+                    else 0
+                    for f_line in order_line.fulfillment_lines.all()
+                ]
+            )
+
+            line_quantity_unfulfilled = (
+                order_line.quantity_unfulfilled - quantity_awaiting
+            )
 
             if sum(line_quantities) > line_quantity_unfulfilled:
                 msg = (
@@ -189,7 +201,12 @@ class OrderFulfill(BaseMutation):
         lines_ids = [line["order_line_id"] for line in lines]
         cls.check_lines_for_duplicates(lines_ids)
         order_lines = cls.get_nodes_or_error(
-            lines_ids, field="lines", only_type=OrderLine
+            lines_ids,
+            field="lines",
+            only_type=OrderLine,
+            qs=order_models.OrderLine.objects.prefetch_related(
+                "fulfillment_lines__fulfillment"
+            ),
         )
 
         cls.clean_lines(order_lines, quantities_for_lines)
