e5791e6def8882289568506028139afa47b77532
Maciej Korycinski <maciej@mirumee.com>
2021-09-18 16:26:16 +0200

    Clean up code. Add missing functionalities. Add JWKS endpoint

--- saleor/core/app.py
+++ saleor/core/app.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.conf import settings
 from django.db.models import Field
+from django.utils.module_loading import import_string
 
 from .db.filters import PostgresILike
 
@@ -13,3 +14,19 @@ class CoreAppConfig(AppConfig):
 
         if settings.SENTRY_DSN:
             settings.SENTRY_INIT(settings.SENTRY_DSN, settings.SENTRY_OPTS)
+
+    def validate_jwt_manager(self):
+        jwt_manager_path = getattr(settings, "JWT_MANAGER_PATH", None)
+        if not jwt_manager_path:
+            raise ImportError(
+                "Missing settings value for path for JWT Manager - JWT_MANAGER_PATH"
+            )
+        try:
+            jwt_manager = import_string(jwt_manager_path)
+        except ImportError as e:
+            raise ImportError(f"Failed to import JWT manager: {e}.")
+
+        validate_method = getattr(jwt_manager, "validate_configuration", NotImplemented)
+        if validate_method == NotImplemented:
+            return
+        validate_method()
