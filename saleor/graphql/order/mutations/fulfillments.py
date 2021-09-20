cae67368e95302d80c7ffbdfd72e4ed83f859170
L3str4nge <mtszzwdzk@gmail.com>
2021-09-20 08:51:53 +0200

    Add missing dot in field description

--- saleor/graphql/order/mutations/fulfillments.py
+++ saleor/graphql/order/mutations/fulfillments.py
@@ -69,7 +69,7 @@ class OrderFulfillInput(graphene.InputObjectType):
     )
 
     allow_stock_to_be_exceeded = graphene.Boolean(
-        description="If true, then allow proceed fulfillment when stock is exceeded",
+        description="If true, then allow proceed fulfillment when stock is exceeded.",
         default_value=False,
     )
 
