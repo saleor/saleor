010e0b56a72b5a306598ad11656f6768103e92d5
Adam Bogdał <adam@bogdal.pl>
2021-10-01 03:34:50 +0200

    Get methods when checkout is available

--- saleor/plugins/webhook/plugin.py
+++ saleor/plugins/webhook/plugin.py
@@ -496,16 +496,17 @@ class WebhookPlugin(BasePlugin):
         apps = App.objects.for_event_type(
             WebhookEventType.SHIPPING_LIST_METHODS
         ).prefetch_related("webhooks")
-        payload = generate_checkout_payload(checkout)
-        for app in apps:
-            response_data = trigger_webhook_sync(
-                event_type=WebhookEventType.SHIPPING_LIST_METHODS,
-                data=payload,
-                app=app,
-            )
-            if response_data:
-                shipping_methods = parse_list_shipping_methods_response(
-                    response_data, app
+        if checkout:
+            payload = generate_checkout_payload(checkout)
+            for app in apps:
+                response_data = trigger_webhook_sync(
+                    event_type=WebhookEventType.SHIPPING_LIST_METHODS,
+                    data=payload,
+                    app=app,
                 )
-                methods.extend(shipping_methods)
+                if response_data:
+                    shipping_methods = parse_list_shipping_methods_response(
+                        response_data, app
+                    )
+                    methods.extend(shipping_methods)
         return methods
