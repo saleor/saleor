440e6171f55878a0e48c8de674407ae96f218498
Rafał Pitoń <rafal@mirumee.com>
2021-10-11 13:33:40 +0300

    Add missing utils for graphene-django

--- saleor/graphql/django/utils/__init__.py
+++ saleor/graphql/django/utils/__init__.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.db.models.manager import Manager
 from django.utils.encoding import force_str
 from graphene.utils.str_converters import to_camel_case
@@ -36,3 +37,38 @@ def maybe_queryset(value):
     if isinstance(value, Manager):
         value = value.get_queryset()
     return value
+
+
+def get_model_fields(model):
+    local_fields = [
+        (field.name, field)
+        for field in sorted(
+            list(model._meta.fields) + list(model._meta.local_many_to_many)
+        )
+    ]
+
+    # Make sure we don't duplicate local fields with "reverse" version
+    local_field_names = [field[0] for field in local_fields]
+    reverse_fields = get_reverse_fields(model, local_field_names)
+
+    all_fields = local_fields + list(reverse_fields)
+
+    return all_fields
+
+
+def get_reverse_fields(model, local_field_names):
+    for name, attr in model.__dict__.items():
+        # Don't duplicate any local fields
+        if name in local_field_names:
+            continue
+
+        # "rel" for FK and M2M relations and "related" for O2O Relations
+        related = getattr(attr, "rel", None) or getattr(attr, "related", None)
+        if isinstance(related, models.ManyToOneRel):
+            yield (name, related)
+        elif isinstance(related, models.ManyToManyRel) and not related.symmetrical:
+            yield (name, related)
+
+
+def is_valid_django_model(model):
+    return inspect.isclass(model) and issubclass(model, models.Model)
