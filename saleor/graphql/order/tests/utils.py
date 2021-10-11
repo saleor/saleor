ee651ea9f837377f6c3d62050276d2e3d53782a8
L3str4nge <mtszzwdzk@gmail.com>
2021-10-11 09:08:34 +0200

    Review fixes

--- saleor/graphql/order/tests/utils.py
+++ saleor/graphql/order/tests/utils.py
@@ -1,6 +1,6 @@
 import graphene
 
-from saleor.order import OrderStatus
+from ....order import OrderStatus
 
 
 def assert_order_and_payment_ids(content, payment):
@@ -16,19 +16,16 @@ def assert_order_fulfilled(order):
     assert order.status == OrderStatus.FULFILLED
 
     order_lines = order.lines.all()
-    assert order_lines[0].quantity_fulfilled == 3
-    assert order_lines[0].quantity_unfulfilled == 0
 
-    assert order_lines[1].quantity_fulfilled == 2
-    assert order_lines[1].quantity_unfulfilled == 0
+    for order_line in order_lines:
+        assert order_line.quantity_fulfilled == order_line.quantity
+        assert order_line.quantity_unfulfilled == 0
 
 
 def assert_order_not_fulfilled(order):
     assert not order.status == OrderStatus.FULFILLED
 
     order_lines = order.lines.all()
-    assert order_lines[0].quantity_fulfilled == 0
-    assert order_lines[0].quantity_unfulfilled == 3
-
-    assert order_lines[1].quantity_fulfilled == 0
-    assert order_lines[1].quantity_unfulfilled == 2
+    for order_line in order_lines:
+        assert order_line.quantity_fulfilled == 0
+        assert order_line.quantity_unfulfilled == order_line.quantity
