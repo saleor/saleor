026c53a73b3eb7400e8385ae3dade7081b6045e0
L3str4nge <mtszzwdzk@gmail.com>
2021-11-09 11:41:17 +0100

    Fix migrations after back porting changes (#8420)

--- saleor/discount/migrations/0030_discount_variants.py
+++ saleor/discount/migrations/0030_discount_variants.py
@@ -4,8 +4,8 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("product", "0147_auto_20210817_1015"),
-        ("discount", "0029_merge_0028_alter_voucher_code_0028_auto_20210817_1015"),
+        ("product", "0150_collection_collection_search_gin"),
+        ("discount", "0028_auto_20210817_1015"),
     ]
 
     operations = [
