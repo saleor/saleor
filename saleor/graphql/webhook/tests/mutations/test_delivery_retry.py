5e9e56feff8c742faa47c482fc9a90b298011fe7
jakubkuc <jakub.kuc.87@gmail.com>
2021-11-16 12:15:56 +0100

    add delivery retry mutation tests

---
+++ saleor/graphql/webhook/tests/mutations/test_delivery_retry.py
@@ -0,0 +1,75 @@
+from unittest.mock import patch
+
+import graphene
+
+from saleor.graphql.tests.utils import assert_no_permission, get_graphql_content
+
+WEBHOOK_DELIVERY_RETRY_MUTATION = """
+    mutation webhookDeliveryRetry($id: ID!){
+      webhookDeliveryRetry(id: $id){
+        errors{
+          field
+          message
+        }
+        delivery{
+          id
+        }
+      }
+    }
+"""
+
+
+@patch("saleor.plugins.webhook.tasks.send_webhook_request_async.delay")
+def test_delivery_retry_mutation(
+    mocked_send_request_async, app_api_client, permission_manage_orders, event_delivery
+):
+    # given
+    query = WEBHOOK_DELIVERY_RETRY_MUTATION
+    delivery_id = graphene.Node.to_global_id("EventDelivery", event_delivery.pk)
+    variables = {"id": delivery_id}
+
+    # when
+    response = app_api_client.post_graphql(
+        query,
+        variables=variables,
+        permissions=[permission_manage_orders],
+        check_no_permissions=False,
+    )
+    content = get_graphql_content(response)
+
+    # then
+    mocked_send_request_async.assert_called_once_with(event_delivery.pk)
+    errors = content["data"]["webhookDeliveryRetry"]["errors"]
+    assert len(errors) == 0
+
+
+def test_webhook_delivery_retry_without_permission(
+    staff_api_client, app, event_delivery
+):
+    query = WEBHOOK_DELIVERY_RETRY_MUTATION
+    delivery_id = graphene.Node.to_global_id("EventDelivery", event_delivery.pk)
+    variables = {"id": delivery_id}
+    response = staff_api_client.post_graphql(query, variables=variables)
+    assert_no_permission(response)
+
+
+def test_webhook_delivery_retry_wrong_type(
+    staff_api_client, app, event_attempt, permission_manage_orders
+):
+    query = WEBHOOK_DELIVERY_RETRY_MUTATION
+    delivery_wrong_id = graphene.Node.to_global_id(
+        "EventDeliveryAttempt", event_attempt.id
+    )
+    variables = {"id": delivery_wrong_id}
+    expected_message = "Must receive a EventDelivery id."
+    response = staff_api_client.post_graphql(
+        query,
+        variables=variables,
+        permissions=[permission_manage_orders],
+        check_no_permissions=False,
+    )
+    content = get_graphql_content(response)
+    errors = content["data"]["webhookDeliveryRetry"]["errors"]
+    assert len(errors) == 1
+    assert errors[0]["field"] == "id"
+    assert errors[0]["message"] == expected_message
