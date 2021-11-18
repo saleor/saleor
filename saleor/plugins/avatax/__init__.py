46336b8a4c1af0e12203fffab50f839dbb7974a1
Maciej Korycinski <maciej@mirumee.com>
2021-11-18 13:13:08 +0100

    Add to set shipping tax code for Avatax

--- saleor/plugins/avatax/__init__.py
+++ saleor/plugins/avatax/__init__.py
@@ -34,9 +34,6 @@ CACHE_KEY = "avatax_request_id_"
 TAX_CODES_CACHE_KEY = "avatax_tax_codes_cache_key"
 TIMEOUT = 10  # API HTTP Requests Timeout
 
-# Common carrier code used to identify the line as a shipping service
-COMMON_CARRIER_CODE = "FR020100"
-
 # Common discount code use to apply discount on order
 COMMON_DISCOUNT_VOUCHER_CODE = "OD010000"
 
@@ -57,6 +54,7 @@ class AvataxConfiguration:
     use_sandbox: bool = True
     company_name: str = "DEFAULT"
     autocommit: bool = False
+    shipping_tax_code: str = ""
 
 
 class TransactionType:
@@ -223,6 +221,7 @@ def append_line_to_data(
 def append_shipping_to_data(
     data: List[Dict],
     shipping_method_channel_listings: Optional["ShippingMethodChannelListing"],
+    shipping_tax_code: str,
 ):
     charge_taxes_on_shipping = (
         Site.objects.get_current().settings.charge_taxes_on_shipping
@@ -233,7 +232,7 @@ def append_shipping_to_data(
             data,
             quantity=1,
             amount=shipping_price.amount,
-            tax_code=COMMON_CARRIER_CODE,
+            tax_code=shipping_tax_code,
             item_code="Shipping",
         )
 
@@ -241,6 +240,7 @@ def append_shipping_to_data(
 def get_checkout_lines_data(
     checkout_info: "CheckoutInfo",
     lines_info: Iterable["CheckoutLineInfo"],
+    config: AvataxConfiguration,
     discounts=None,
 ) -> List[Dict[str, Union[str, int, bool, None]]]:
     data: List[Dict[str, Union[str, int, bool, None]]] = []
@@ -268,14 +268,13 @@ def get_checkout_lines_data(
         )
 
     append_shipping_to_data(
-        data,
-        checkout_info.shipping_method_channel_listings,
+        data, checkout_info.shipping_method_channel_listings, config.shipping_tax_code
     )
     return data
 
 
 def get_order_lines_data(
-    order: "Order",
+    order: "Order", config: AvataxConfiguration
 ) -> List[Dict[str, Union[str, int, bool, None]]]:
     data: List[Dict[str, Union[str, int, bool, None]]] = []
     lines = order.lines.prefetch_related(
@@ -323,7 +322,9 @@ def get_order_lines_data(
     shipping_method_channel_listing = ShippingMethodChannelListing.objects.filter(
         shipping_method=order.shipping_method_id, channel=order.channel_id
     ).first()
-    append_shipping_to_data(data, shipping_method_channel_listing)
+    append_shipping_to_data(
+        data, shipping_method_channel_listing, config.shipping_tax_code
+    )
     return data
 
 
@@ -379,7 +380,7 @@ def generate_request_data_from_checkout(
 ):
 
     address = checkout_info.shipping_address or checkout_info.billing_address
-    lines = get_checkout_lines_data(checkout_info, lines_info, discounts)
+    lines = get_checkout_lines_data(checkout_info, lines_info, config, discounts)
 
     currency = checkout_info.checkout.currency
     data = generate_request_data(
@@ -448,7 +449,7 @@ def get_checkout_tax_data(
 
 def get_order_request_data(order: "Order", config: AvataxConfiguration):
     address = order.shipping_address or order.billing_address
-    lines = get_order_lines_data(order)
+    lines = get_order_lines_data(order, config)
     transaction = (
         TransactionType.INVOICE
         if not (order.is_draft() or order.is_unconfirmed())
