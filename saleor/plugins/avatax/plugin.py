dd96df0a22da2825bc69b2ae8035a19d423f8a9d
Maciej Korycinski <maciej@mirumee.com>
2021-11-19 11:02:47 +0100

    Fix zero for subtotal with avatax

--- saleor/plugins/avatax/plugin.py
+++ saleor/plugins/avatax/plugin.py
@@ -389,7 +389,7 @@ class AvataxPlugin(BasePlugin):
             return previous_value
 
         if not _validate_order(order):
-            return zero_taxed_money(order.total.currency)
+            return previous_value
 
         taxes_data = self._get_order_tax_data(order, previous_value)
         return self._calculate_line_total_price(taxes_data, variant.sku, previous_value)
@@ -496,7 +496,7 @@ class AvataxPlugin(BasePlugin):
             return previous_value
 
         if not _validate_order(order):
-            return zero_taxed_money(order.total.currency)
+            return previous_value
         taxes_data = get_order_tax_data(order, self.config, False)
 
         tax_included = (
