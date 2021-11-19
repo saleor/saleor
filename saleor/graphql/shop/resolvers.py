bb2b40993242466a1b8d622fc183a572a29b5c6a
Krzysztof Kulig <kris@tango.agency>
2021-11-20 00:32:14 +0100

    Use the shipping dataclass in shop resolvers

--- saleor/graphql/shop/resolvers.py
+++ saleor/graphql/shop/resolvers.py
@@ -2,6 +2,7 @@ from ...account.models import Address
 from ...core.tracing import traced_resolver
 from ...shipping.models import ShippingMethod, ShippingMethodChannelListing
 from ...shipping.postal_codes import filter_shipping_methods_by_postal_code_rules
+from ...shipping.utils import convert_to_shipping_method_data
 from ..channel import ChannelContext
 
 
@@ -18,27 +19,25 @@ def resolve_available_shipping_methods(info, channel_slug: str, address):
         )
 
     if available is not None:
-        shipping_mapping = get_shipping_method_to_shipping_price_mapping(
-            available, channel_slug
-        )
-        for shipping_method in available:
-            shipping_price = shipping_mapping[shipping_method.pk]
-            shipping_method.price = shipping_price
+        mapping = get_shipping_method_to_listing_mapping(available, channel_slug)
         instances += [
-            ChannelContext(node=shipping, channel_slug=channel_slug)
-            for shipping in available
+            ChannelContext(
+                node=convert_to_shipping_method_data(method, mapping[method.id]),
+                channel_slug=channel_slug,
+            )
+            for method in available
         ]
 
     return instances
 
 
-def get_shipping_method_to_shipping_price_mapping(shipping_methods, channel_slug):
-    """Prepare mapping shipping method to price from channel listings."""
+def get_shipping_method_to_listing_mapping(shipping_methods, channel_slug):
+    """Prepare mapping shipping method to its channel listings."""
     shipping_mapping = {}
     shipping_listings = ShippingMethodChannelListing.objects.filter(
         shipping_method__in=shipping_methods, channel__slug=channel_slug
     )
     for listing in shipping_listings:
-        shipping_mapping[listing.shipping_method.id] = listing.price
+        shipping_mapping[listing.shipping_method_id] = listing
 
     return shipping_mapping
