f54720aa30f61b6c52cbedd53265a41450117cca
Jakub <jakub.kuc.87@gmail.com>
2021-11-22 11:10:27 +0100

    Add API for event deliveries and attempts (#8548)
    * Add API for event deliveries and attempts
    
    * add tests for delivery query
    
    * add attempts pagination and tests
    
    * add data loader for attempts
    
    * fixes after review
    
    * cosmetic fixes
    
    * add default sorting to delivery models metaclass

--- saleor/graphql/webhook/types.py
+++ saleor/graphql/webhook/types.py
@@ -1,9 +1,16 @@
 import graphene
 
+from ...core import models as core_models
 from ...webhook import models
 from ...webhook.event_types import WebhookEventType
 from ..core.connection import CountableDjangoObjectType
-from .enums import WebhookEventTypeEnum
+from ..core.fields import FilterInputConnectionField
+from ..webhook.enums import EventDeliveryStatusEnum, WebhookEventTypeEnum
+from ..webhook.filters import EventDeliveryFilterInput
+from ..webhook.sorters import (
+    EventDeliveryAttemptSortingInput,
+    EventDeliverySortingInput,
+)
 
 
 class WebhookEvent(CountableDjangoObjectType):
@@ -22,6 +29,57 @@ class WebhookEvent(CountableDjangoObjectType):
         return WebhookEventType.DISPLAY_LABELS.get(root.event_type) or root.event_type
 
 
+class EventDeliveryAttempt(CountableDjangoObjectType):
+    created_at = graphene.DateTime(
+        description="Event delivery creation date and time.", required=True
+    )
+    status = EventDeliveryStatusEnum(
+        description="Event delivery status.", required=True
+    )
+
+    class Meta:
+        description = "Event delivery attempts."
+        model = core_models.EventDeliveryAttempt
+        interfaces = [graphene.relay.Node]
+        only_fields = [
+            "id",
+            "created_at",
+            "task_id",
+            "duration",
+            "status",
+            "response",
+            "response_headers",
+            "request_headers",
+        ]
+
+
+class EventDelivery(CountableDjangoObjectType):
+    status = EventDeliveryStatusEnum(
+        description="Event delivery status.", required=True
+    )
+    attempts = FilterInputConnectionField(
+        EventDeliveryAttempt,
+        sort_by=EventDeliveryAttemptSortingInput(description="Event delivery sorter"),
+        description="Event delivery attempts.",
+    )
+    event_type = WebhookEventTypeEnum(description="Webhook event type.", required=True)
+
+    class Meta:
+        description = "Event delivery."
+        model = core_models.EventDelivery
+        interfaces = [graphene.relay.Node]
+        only_fields = [
+            "id",
+            "created_at",
+            "status",
+            "event_type",
+        ]
+
+    @staticmethod
+    def resolve_attempts(root: core_models.EventDelivery, *_args, **_kwargs):
+        return core_models.EventDeliveryAttempt.objects.filter(delivery=root)
+
+
 class Webhook(CountableDjangoObjectType):
     name = graphene.String(required=True)
     events = graphene.List(
@@ -30,6 +88,12 @@ class Webhook(CountableDjangoObjectType):
         required=True,
     )
     app = graphene.Field("saleor.graphql.app.types.App", required=True)
+    deliveries = FilterInputConnectionField(
+        EventDelivery,
+        sort_by=EventDeliverySortingInput(description="Event delivery sorter."),
+        filter=EventDeliveryFilterInput(description="Event delivery filter options."),
+        description="Event deliveries.",
+    )
 
     class Meta:
         description = "Webhook."
@@ -45,3 +109,7 @@ class Webhook(CountableDjangoObjectType):
     @staticmethod
     def resolve_events(root: models.Webhook, *_args, **_kwargs):
         return root.events.all()
+
+    @staticmethod
+    def resolve_deliveries(root: models.Webhook, *_args, **_kwargs):
+        return core_models.EventDelivery.objects.filter(webhook_id=root.pk)
