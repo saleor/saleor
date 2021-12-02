931a1644bc803258395ae5f42b36a0ebda9178fd
Islam Mesha <eng.islammesha@gmail.com>
2021-12-02 16:17:36 +0200

    Handel product image link nullable value

--- saleor/plugins/oto/utils.py
+++ saleor/plugins/oto/utils.py
@@ -34,7 +34,12 @@ def get_order_items_data(order):
             "productId": line.variant.product.id,
             "price": line.total_price_net_amount,
             "image": "%s%s"
-            % (site.domain, line.variant.product.get_first_image().image.url),
+            % (
+                site.domain,
+                line.variant.product.get_first_image().image.url
+                if line.variant.product.get_first_image()
+                else "",
+            ),
         }
         for line in order.lines.all()
     ]
