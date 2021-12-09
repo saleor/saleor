6e789ecb002bb623ef71404bb1dd67493dcbc1a8
essamfakher5@gmail.com <Rasmia3112572017>
2021-12-09 18:00:57 +0200

    types

---
+++ saleor/plugins/customer_group/graphql/types.py
@@ -0,0 +1,8 @@
+from graphene_django import DjangoObjectType
+from saleor.plugins.customer_group.models import CustomerGroup
+
+
+class CustomerGroupType(DjangoObjectType):
+    class Meta:
+        model = CustomerGroup
+        fields = "__all__"
