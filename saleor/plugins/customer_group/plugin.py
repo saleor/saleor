0bbd5048457835fe5ec0779983c3676e9cc26e3e
essamfakher5@gmail.com <Rasmia3112572017>
2021-12-09 18:06:53 +0200

    plugin

---
+++ saleor/plugins/customer_group/plugin.py
@@ -0,0 +1,66 @@
+from ..base_plugin import BasePlugin, ConfigurationTypeField
+from .graphql.schema import schema
+from ...graphql.views import GraphQLView
+from django.core.exceptions import ValidationError
+from saleor.plugins.models import PluginConfiguration
+
+
+class CustomerGroupPlugin(BasePlugin):
+    PLUGIN_ID = "customer.group"
+    PLUGIN_NAME = "Customer Group"
+    DEFAULT_ACTIVE = True
+    CONFIGURATION_PER_CHANNEL = False
+
+    DEFAULT_CONFIGURATION = [
+        {"name": "name", "value": ""},
+        {"name": "description", "value": ""},
+        {"name": "Active", "value": True},
+    ]
+
+    CONFIG_STRUCTURE = {
+        "name": {
+            "type": ConfigurationTypeField.STRING,
+            "help_text": "Determines name." " Please enter the name of customer group.",
+            "label": "customer group name",
+        },
+        "description": {
+            "type": ConfigurationTypeField.SECRET,
+            "help_text": "description of customer group",
+            "label": "description",
+        },
+        "Active": {
+            "type": ConfigurationTypeField.BOOLEAN,
+            "label": "Active",
+        },
+    }
+
+    def _get_gateway_config(self):
+        return self.configuration
+
+    @classmethod
+    def validate_plugin_configuration(cls, plugin_configuration: "PluginConfiguration"):
+        """Validate if provided configuration is correct."""
+
+        missing_fields = []
+        configuration = plugin_configuration.configuration
+        configuration = {item["name"]: item["value"] for item in configuration}
+        if not configuration["name"]:
+            missing_fields.append("name")
+
+        if plugin_configuration.active and missing_fields:
+            error_msg = (
+                "To enable a plugin, you need to provide values for the "
+                "following fields: "
+            )
+            raise ValidationError(
+                {
+                    missing_fields[0]: ValidationError(
+                        error_msg + ", ".join(missing_fields), code="invalid"
+                    )
+                }
+            )
+
+    def webhook(self, request, path, previous_value):
+        view = GraphQLView.as_view(schema=schema)
+        request.app = self
+        return view(request)
