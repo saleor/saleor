fdc1980f8f50889910e0123c4f156876bae40631
essamfakher5@gmail.com <Rasmia3112572017>
2021-12-12 05:56:42 +0200

    making node edges and global ids

--- saleor/plugins/customer_group/graphql/schema.py
+++ saleor/plugins/customer_group/graphql/schema.py
@@ -1,4 +1,6 @@
 import graphene
+from graphene_django.filter import DjangoFilterConnectionField
+from graphene_django.types import DjangoObjectType
 
 from saleor.graphql.core.utils import from_global_id_or_error
 from saleor.plugins.customer_group.graphql.types import CustomerGroupType
@@ -11,24 +13,25 @@ from .mutations import (
 )
 
 
-class CustomerGroupQueries(graphene.ObjectType):
-    customer_group = graphene.Field(
-        CustomerGroupType,
-        id=graphene.ID(description="ID of the CustomerGroup", required=True),
-        description="Look up a CustomerGroup by ID",
-    )
-    customer_groups = graphene.List(
-        graphene.NonNull(CustomerGroupType), description="List of all CustomerGroups"
-    )
+class CustomerGroupNode(DjangoObjectType):
+    class Meta:
+        model = CustomerGroup
+        filter_fields = ["id", "name", "description", "customers"]
+        interfaces = (graphene.relay.Node,)
 
     def resolve_customer_groups(root, info, **kwargs):
         # Querying a list
         return CustomerGroup.objects.all()
 
-    def resolve_customer_group(root, info, id):
+    def resolve_customer_group(self, info, **data):
         # Querying a single CustomerGroup
-        _, id = from_global_id_or_error(id, CustomerGroupType)
+        _, id = from_global_id_or_error(data.get("id"), CustomerGroupType)
         return CustomerGroup.objects.get(id=id)
+
+
+class CustomerGroupQueries(graphene.ObjectType):
+    customer_group = graphene.relay.Node.Field(CustomerGroupNode)
+    customer_groups = DjangoFilterConnectionField(CustomerGroupNode)
 
 
 class CustomerGroupMutations(graphene.ObjectType):
