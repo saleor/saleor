d7add63d7187fb7ab3ac44c59282bc0ba6443247
essamfakher5@gmail.com <Rasmia3112572017>
2021-12-12 05:56:56 +0200

    edit for flake8 lint

--- saleor/plugins/customer_group/setup.py
+++ saleor/plugins/customer_group/setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
+# making two gloable var for passing the flake8 lint
+plugin_name = "saleor.plugins.customer_group"
+plugin_name_path = "saleor.plugins.customer_group.plugin:CustomerGroupPlugin"
+
+saleor_plugin = f"{plugin_name} = {plugin_name_path}"
 setup(
     name="customer_group",
-    entry_points={
-        "saleor.plugins": [
-            "saleor.plugins.customer_group = saleor.plugins.customer_group.plugin:CustomerGroupPlugin"
-        ]
-    },
+    entry_points={"saleor.plugins": [saleor_plugin]},
 )
