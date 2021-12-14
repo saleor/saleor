cbdad56348c5601444ce90d7872d3c98e8686036
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2021-12-14 10:53:29 +0100

    Use sale price when adding variant to order (#8766)

--- saleor/graphql/order/mutations/orders.py
+++ saleor/graphql/order/mutations/orders.py
@@ -778,7 +778,7 @@ class OrderLinesCreate(EditableOrderValidationMixin, BaseMutation):
             raise ValidationError(error)
 
     @staticmethod
-    def add_lines_to_order(order, lines_to_add, user, app, manager):
+    def add_lines_to_order(order, lines_to_add, user, app, manager, discounts):
         try:
             return [
                 add_variant_to_order(
@@ -788,6 +788,7 @@ class OrderLinesCreate(EditableOrderValidationMixin, BaseMutation):
                     user,
                     app,
                     manager,
+                    discounts=discounts,
                     allocate_stock=order.is_unconfirmed(),
                 )
                 for quantity, variant in lines_to_add
@@ -813,6 +814,7 @@ class OrderLinesCreate(EditableOrderValidationMixin, BaseMutation):
             info.context.user,
             info.context.app,
             info.context.plugins,
+            info.context.discounts,
         )
 
         # Create the products added event
