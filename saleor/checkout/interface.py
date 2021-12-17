385c542d40331ff4b901cbdae5f676bf6bc7128d
IKarbowiak <iga.karbowiak@mirumee.com>
2021-12-17 11:40:28 +0100

    Attach sale discount value to line when adding variant to order

--- saleor/checkout/interface.py
+++ saleor/checkout/interface.py
@@ -4,10 +4,10 @@ from prices import Money, TaxedMoney
 
 
 @dataclass
-class TaxedPricesData:
-    """Store a prices data with applied taxes.
+class CheckoutTaxedPricesData:
+    """Store a checkout prices data with applied taxes.
 
-    'price' includes discount from sale if any valid exists.
+    'price_with_sale' includes discount from sale if any valid exists.
     'price_with_discounts' includes voucher discount and sale discount if any valid
     exists.
     'undiscounted_price' is a price without any sale and voucher.
@@ -19,10 +19,10 @@ class TaxedPricesData:
 
 
 @dataclass
-class PricesData:
-    """Store a prices data without applied taxes.
+class CheckoutPricesData:
+    """Store a checkout prices data without applied taxes.
 
-    'price' includes discount from sale if any valid exists.
+    'price_with_sale' includes discount from sale if any valid exists.
     'price_with_discounts' includes voucher discount and sale discount if any valid
     exists.
     'undiscounted_price' is a price without any sale and voucher.
--- saleor/plugins/base_plugin.py
+++ saleor/plugins/base_plugin.py
@@ -9,7 +9,7 @@ from django_countries.fields import Country
 from measurement.measures import Weight
 from prices import Money, TaxedMoney
 
-from ..checkout.interface import TaxedPricesData
+from ..checkout.interface import CheckoutTaxedPricesData
 from ..payment.interface import (
     CustomerSource,
     GatewayResponse,
@@ -154,7 +154,7 @@ class BasePlugin:
             Iterable["DiscountInfo"],
             TaxedMoney,
         ],
-        TaxedPricesData,
+        CheckoutTaxedPricesData,
     ]
 
     #  Calculate checkout line unit price.
@@ -167,7 +167,7 @@ class BasePlugin:
             Iterable["DiscountInfo"],
             Any,
         ],
-        TaxedPricesData,
+        CheckoutTaxedPricesData,
     ]
 
     #  Calculate the shipping costs for checkout.
