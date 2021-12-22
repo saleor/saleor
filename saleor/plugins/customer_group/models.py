901599569a0a0de9ffa8b36fc9556bc45befb94e
essamfakher5@gmail.com <Rasmia3112572017>
2021-12-22 23:48:40 +0200

    add new column created at , updated at and add indexing

--- saleor/plugins/customer_group/models.py
+++ saleor/plugins/customer_group/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 
 class CustomerGroup(models.Model):
-    name = models.CharField(max_length=256, blank=True)
-    customers = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=256, db_index=True)
+    customers = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    description = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
-    date = models.DateTimeField(db_index=True, auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
