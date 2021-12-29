feef17a868606245cc71c9f951a0ad21032c1137
essamfakher5@gmail.com <Rasmia3112572017>
2021-12-29 07:29:26 +0200

    models and mutations

---
+++ saleor/plugins/vendor/models.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.db import models
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
+from saleor.account.validators import validate_possible_number
+
+
+class PossiblePhoneNumberField(PhoneNumberField):
+    """Less strict field for phone numbers written to database"""
+
+    default_validators = [validate_possible_number]
+
+
+class Vendor(models.Model):
+    GENDER_CHOICES = (
+        ("M", "Male"),
+        ("F", "Female"),
+        (
+            "U",
+            "Unsure",
+        ),
+    )
+    name = models.CharField(max_length=256, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    description = models.TextField(blank=True, default="")
+    country = CountryField()
+    phone = PossiblePhoneNumberField(blank=True, default="", db_index=True)
+    national_id = models.CharField(max_length=256, blank=True)
+    is_active = models.BooleanField(default=True)
+    birth_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
