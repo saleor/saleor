a7e60a5990d462528c0263e85c82c632704289dc
essamfakher5@gmail.com <Rasmia3112572017>
2022-01-02 06:14:31 +0200

    edit on model and migration

--- saleor/plugins/customer_group/models.py
+++ saleor/plugins/customer_group/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class CustomerGroup(models.Model):
     name = models.CharField(max_length=256, db_index=True)
     customers = models.ManyToManyField(settings.AUTH_USER_MODEL)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, default="")
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    variants = models.ManyToManyField("product.ProductVariant")
