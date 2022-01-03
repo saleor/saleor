1221c19e0eb5c8784d18e6058f4c63c33a6d8d97
LeOndaz <ahmeddark369@gmail.com>
2022-01-03 15:35:14 +0200

    remove useless query

--- saleor/plugins/oauth2/graphql/schema.py
+++ saleor/plugins/oauth2/graphql/schema.py
@@ -3,14 +3,10 @@ import graphene
 from .mutations import SocialLogin, SocialLoginByAccessToken, SocialLoginConfirm
 
 
-class Queries(graphene.ObjectType):
-    author = graphene.String(default_value="wecre8")
-
-
 class Mutations(graphene.ObjectType):
     social_login = SocialLogin.Field()
     social_login_confirm_mobile = SocialLoginByAccessToken.Field()
     social_login_confirm_web = SocialLoginConfirm.Field()
 
 
-schema = graphene.Schema(query=Queries, mutation=Mutations)
+schema = graphene.Schema(mutation=Mutations)
