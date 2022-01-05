27e162dadd99c96213de5bf7b32a25116669dd60
essamfakher5@gmail.com <Rasmia3112572017>
2022-01-05 13:01:07 +0200

    types update

--- saleor/plugins/customer_group/graphql/types.py
+++ saleor/plugins/customer_group/graphql/types.py
@@ -1,9 +1,6 @@
 import graphene
 from graphene import relay
-from graphene.types.objecttype import ObjectType
-from graphene.types.scalars import String
 
-from saleor.graphql.account.types import User
 from saleor.graphql.core.connection import (
     CountableDjangoObjectType,
     create_connection_slice,
@@ -12,7 +9,6 @@ from saleor.graphql.core.fields import FilterConnectionField
 from saleor.graphql.product.filters import ProductVariantFilterInput
 from saleor.graphql.product.types import ProductVariantCountableConnection
 from saleor.plugins.customer_group import models
-from saleor.product.models import ProductVariant
 
 
 class CustomerGroup(CountableDjangoObjectType):
@@ -30,11 +26,11 @@ class CustomerGroup(CountableDjangoObjectType):
         description="List of product variants.",
     )
 
-    def resolve_variants(self, info, ids=None, channel=None, **kwargs):
+    def resolve_variants(root, info, ids=None, channel=None, **kwargs):
         from saleor.graphql.channel import ChannelQsContext
 
-        qs = ChannelQsContext(ProductVariant.objects.all(), channel)
-
+        groups = models.CustomerGroup.objects.get(pk=root.id)
+        qs = ChannelQsContext(groups.variants.all(), channel)
         return create_connection_slice(
             qs, info, kwargs, ProductVariantCountableConnection
         )
@@ -43,18 +39,6 @@ class CustomerGroup(CountableDjangoObjectType):
         model = models.CustomerGroup
         filter_fields = ["id", "name", "description"]
         interfaces = (graphene.relay.Node,)
-
-
-class Customer(ObjectType):
-    class Meta:
-        interfaces = [relay.Node]
-
-    name = String()
-
-
-class CustomerConnection(relay.Connection):
-    class Meta:
-        node = User
 
 
 class CustomerGroupConnection(relay.Connection):
