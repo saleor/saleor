f7725e18c5971481e513c497b7b480470192287e
LeOndaz <ahmeddark369@gmail.com>
2022-01-06 18:31:01 +0200

    done

--- saleor/plugins/oauth2/graphql/schema.py
+++ saleor/plugins/oauth2/graphql/schema.py
@@ -4,10 +4,10 @@ from ....graphql.core.federation import build_federated_schema
 from .mutations import SocialLogin, SocialLoginByAccessToken, SocialLoginConfirm
 
 
-class Mutations(graphene.ObjectType):
+class Mutation(graphene.ObjectType):
     social_login = SocialLogin.Field()
     social_login_confirm_mobile = SocialLoginByAccessToken.Field()
     social_login_confirm_web = SocialLoginConfirm.Field()
 
 
-schema = build_federated_schema(mutation=Mutations)
+schema = build_federated_schema(mutation=Mutation)
