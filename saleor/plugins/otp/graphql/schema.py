a15c9f4dcda763c80f199ad51dc170faacd16194
LeOndaz <ahmeddark369@gmail.com>
2022-01-06 18:33:04 +0200

    fix federation

--- saleor/plugins/otp/graphql/schema.py
+++ saleor/plugins/otp/graphql/schema.py
@@ -4,13 +4,13 @@ from ....graphql.core.federation import build_federated_schema
 from .mutations import RequestPasswordRecovery, SetPasswordByCode
 
 
-class Queries(graphene.ObjectType):
+class Query(graphene.ObjectType):
     author = graphene.String(default_value="WeCre8")
 
 
-class Mutations(graphene.ObjectType):
+class Mutation(graphene.ObjectType):
     request_password_recovery = RequestPasswordRecovery.Field()
     set_password_by_code = SetPasswordByCode.Field()
 
 
-schema = build_federated_schema(query=Queries, mutation=Mutations)
+schema = build_federated_schema(query=Query, mutation=Mutation)
