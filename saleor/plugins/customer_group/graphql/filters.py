6ac3014c4990e8ebeeb338cb34a459cc7adaf494
essamfakher5@gmail.com <Rasmia3112572017>
2022-01-11 12:49:14 +0200

    edit lint

--- saleor/plugins/customer_group/graphql/filters.py
+++ saleor/plugins/customer_group/graphql/filters.py
@@ -1,7 +1,7 @@
 import django_filters
+
 from saleor.graphql.core.filters import MetadataFilterBase
 from saleor.graphql.core.types.filter_input import FilterInputObjectType
-
 from saleor.graphql.utils.filters import filter_by_query_param
 
 from .. import models
