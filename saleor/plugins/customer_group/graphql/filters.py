ba22b8087c7debaf0cde3edf863f34cc334559f6
essamfakher5@gmail.com <Rasmia3112572017>
2022-01-11 12:19:19 +0200

    add filter

---
+++ saleor/plugins/customer_group/graphql/filters.py
@@ -0,0 +1,26 @@
+import django_filters
+from saleor.graphql.core.filters import MetadataFilterBase
+from saleor.graphql.core.types.filter_input import FilterInputObjectType
+
+from saleor.graphql.utils.filters import filter_by_query_param
+
+from .. import models
+
+
+def filter_group_search(qs, _, value):
+    group_fields = ["name"]
+    qs = filter_by_query_param(qs, value, group_fields)
+    return qs
+
+
+class GroupFilter(MetadataFilterBase):
+    search = django_filters.CharFilter(method=filter_group_search)
+
+    class Meta:
+        model = models.CustomerGroup
+        fields = ["search"]
+
+
+class GroupFilterInput(FilterInputObjectType):
+    class Meta:
+        filterset_class = GroupFilter
--- saleor/plugins/customer_group/graphql/schema.py
+++ saleor/plugins/customer_group/graphql/schema.py
@@ -1,11 +1,15 @@
 import graphene
 
-from saleor.graphql.core.connection import create_connection_slice
+from saleor.graphql.core.connection import (
+    create_connection_slice,
+    filter_connection_queryset,
+)
 from saleor.graphql.core.fields import FilterConnectionField
 from saleor.graphql.core.utils import from_global_id_or_error
 
 from ..models import CustomerGroup
 from . import types
+from .filters import GroupFilterInput
 from .mutations import CustomerGroupCreate, CustomerGroupDelete, CustomerGroupUpdate
 
 
@@ -18,11 +22,15 @@ class CustomerGroupQueries(graphene.ObjectType):
         ),
         description="Look up a customer group by ID",
     )
-    customer_groups = FilterConnectionField(types.CustomerGroupConnection)
+    customer_groups = FilterConnectionField(
+        types.CustomerGroupConnection,
+        filter=GroupFilterInput(description="Filtering options for group."),
+    )
 
     def resolve_customer_groups(root, info, **kwargs):
         # Querying a list
         qs = CustomerGroup.objects.all()
+        qs = filter_connection_queryset(qs, kwargs)
         return create_connection_slice(qs, info, kwargs, types.CustomerGroupConnection)
 
     def resolve_customer_group(self, info, id, **data):
