7c3486d2eff1420c840d93a42890f8b90a56f355
LeOndaz <ahmeddark369@gmail.com>
2022-01-12 15:00:34 +0200

    remove useless query

--- saleor/plugins/otp/graphql/schema.py
+++ saleor/plugins/otp/graphql/schema.py
@@ -4,13 +4,9 @@ from ....graphql.core.federation import build_federated_schema
 from .mutations import RequestPasswordRecovery, SetPasswordByCode
 
 
-class Query(graphene.ObjectType):
-    author = graphene.String(default_value="WeCre8")
-
-
 class Mutation(graphene.ObjectType):
     request_password_recovery = RequestPasswordRecovery.Field()
     set_password_by_code = SetPasswordByCode.Field()
 
 
-schema = build_federated_schema(query=Query, mutation=Mutation)
+schema = build_federated_schema(mutation=Mutation)
