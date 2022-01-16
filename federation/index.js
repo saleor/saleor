eba98e5b9c7a83e51cb0b31b3b42be0a9f0c5c74
Hazem Khaled <hazem.khaled@gmail.com>
2022-01-16 21:06:14 +0300

    Enable federations

--- federation/index.js
+++ federation/index.js
@@ -6,10 +6,10 @@ const serviceList = [
     name: "saleor",
     url: "https://w8-saleor-staging.herokuapp.com/graphql/",
   },
-  // {
-  //   name: "social-login",
-  //   url: `https://w8-saleor-pr-${process.env.PR_NUMBER}.herokuapp.com/plugins/social-login/graphql`,
-  // }
+  {
+    name: "countries",
+    url: `https://w8-saleor-pr-${process.env.PR_NUMBER}.herokuapp.com/plugins/countries/graphql`,
+  }
 ];
 
 const gateway = new ApolloGateway({ serviceList });
