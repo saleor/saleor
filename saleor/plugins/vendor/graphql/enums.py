3386a05b39fa00b2387dc3a9cda10decf6a18d0a
essamfakher5@gmail.com <Rasmia3112572017>
2022-01-18 10:38:32 +0200

    enums

--- saleor/plugins/vendor/graphql/enums.py
+++ saleor/plugins/vendor/graphql/enums.py
@@ -4,3 +4,8 @@ from enum import Enum
 class VendorErrorCode(Enum):
     GROUP_NOT_FOUND = "vendor_not_found"
     GROUP_ERROR = "vendor_error"
+
+
+class BillingErrorCode(Enum):
+    GROUP_NOT_FOUND = "Billing_not_found"
+    GROUP_ERROR = "Billing_error"
