f03fd61735a9e457c15b15fa8eda093503fe1620
essamfakher5@gmail.com <Rasmia3112572017>
2022-01-20 10:01:38 +0200

    enums

--- saleor/plugins/vendor/graphql/enums.py
+++ saleor/plugins/vendor/graphql/enums.py
@@ -1,11 +1,25 @@
 from enum import Enum
 
+import graphene
+
 
 class VendorErrorCode(Enum):
-    GROUP_NOT_FOUND = "vendor_not_found"
-    GROUP_ERROR = "vendor_error"
+    VENDOR_NOT_FOUND = "vendor_not_found"
+    VENDOR_ERROR = "vendor_error"
+    VENDOR_SLUG = "vendor_error_generate_slug"
 
 
 class BillingErrorCode(Enum):
     GROUP_NOT_FOUND = "Billing_not_found"
     GROUP_ERROR = "Billing_error"
+
+
+class CommercialInfo(graphene.Enum):
+    CR = "cr"
+    MAROOF = "maroof"
+
+
+class SellsGender(graphene.Enum):
+    MEN = "men"
+    WOMEN = "women"
+    UNISEX = "unisex"
