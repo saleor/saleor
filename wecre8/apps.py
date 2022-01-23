3d000c1592bc6e402780d84ca23777c2a0e4ec38
Islam Mesha <eng.islammesha@gmail.com>
2022-01-23 16:23:25 +0200

    Delete the WeCre8 django app

--- wecre8/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Wecre8Config(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "wecre8"
--- wecre8/models.py
+++
@@ -1,1 +0,0 @@
-# Create your models here.
