7309f80b39cf453cbe9d95c8eab5a7c79ad8d48a
Jakub <jakub.kuc.87@gmail.com>
2022-01-24 14:58:21 +0100

    Add in_shipping_zones filter to shop countries query (#8897) (#8994)
    * add in_shipping_zones argument to shop countries query
    
    * add docstring and description
    
    * change argument to filter, fixes after review
    
    * add desription
    
    * fixes after review
    
    * added test

--- saleor/graphql/shop/resolvers.py
+++ saleor/graphql/shop/resolvers.py
@@ -1,8 +1,14 @@
+from django.utils import translation
+from django_countries import countries
+from django_prices_vatlayer.models import VAT
+
 from ...account.models import Address
 from ...core.tracing import traced_resolver
 from ...shipping.models import ShippingMethod, ShippingMethodChannelListing
 from ...shipping.postal_codes import filter_shipping_methods_by_postal_code_rules
 from ...shipping.utils import convert_to_shipping_method_data
+from ..core.types import CountryDisplay
+from .utils import get_countries_codes_list
 
 
 @traced_resolver
@@ -25,6 +31,22 @@ def resolve_available_shipping_methods(info, channel_slug: str, address):
         ]
 
     return instances
+
+
+def resolve_countries(**kwargs):
+    countries_filter = kwargs.get("filter", {})
+    attached_to_shipping_zones = countries_filter.get("attached_to_shipping_zones")
+    language_code = kwargs.get("language_code")
+    taxes = {vat.country_code: vat for vat in VAT.objects.all()}
+    # DEPRECATED: translation.override will be dropped in Saleor 4.0
+    with translation.override(language_code):
+        return [
+            CountryDisplay(
+                code=country[0], country=country[1], vat=taxes.get(country[0])
+            )
+            for country in countries
+            if country[0] in get_countries_codes_list(attached_to_shipping_zones)
+        ]
 
 
 def get_shipping_method_to_listing_mapping(shipping_methods, channel_slug):
