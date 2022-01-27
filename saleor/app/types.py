9069e5173751c6049f331a3eeb2bc36e14e48085
Maciej Korycinski <maciej@mirumee.com>
2022-01-27 15:11:41 +0100

    Simplify extensions' enums by converting them into one

--- saleor/app/types.py
+++ saleor/app/types.py
@@ -5,35 +5,34 @@ class AppType:
     CHOICES = [(LOCAL, "local"), (THIRDPARTY, "thirdparty")]
 
 
-class AppExtensionView:
-    """All available places where app's iframe can be mounted.
+class AppExtensionMount:
+    """All places where app extension can be mounted."""
 
-    PRODUCT - app's extension will be mounted in product section
-    ALL - app's extension will be mounted in all views
-    """
+    PRODUCT_OVERVIEW_CREATE = "product_overview_create"
+    PRODUCT_OVERVIEW_MORE_ACTIONS = "product_overview_more_actions"
+    PRODUCT_DETAILS_MORE_ACTIONS = "product_details_more_actions"
 
-    PRODUCT = "product"
-    ALL = "all"
+    NAVIGATION_CATALOG = "navigation_catalog"
+    NAVIGATION_ORDERS = "navigation_orders"
+    NAVIGATION_CUSTOMERS = "navigation_customers"
+    NAVIGATION_DISCOUNTS = "navigation_discounts"
+    NAVIGATION_TRANSLATIONS = "navigation_translations"
+    NAVIGATION_PAGES = "navigation_pages"
 
-    CHOICES = [(PRODUCT, "product"), (ALL, "all")]
+    CHOICES = [
+        (PRODUCT_OVERVIEW_CREATE, "product_overview_create"),
+        (PRODUCT_OVERVIEW_MORE_ACTIONS, "product_overview_more_actions"),
+        (PRODUCT_DETAILS_MORE_ACTIONS, "product_details_more_actions"),
+        (NAVIGATION_CATALOG, "navigation_catalog"),
+        (NAVIGATION_ORDERS, "navigation_orders"),
+        (NAVIGATION_CUSTOMERS, "navigation_customers"),
+        (NAVIGATION_DISCOUNTS, "navigation_discounts"),
+        (NAVIGATION_TRANSLATIONS, "navigation_translations"),
+        (NAVIGATION_PAGES, "navigation_pages"),
+    ]
 
 
-class AppExtensionType:
-    """All available types where app's iframe can be mounted.
-
-    OVERVIEW - app's extension will be mounted on list view.
-    DETAILS - app's extension will be mounted on detail view
-    NAVIGATION - app's extension will be mounted on navigation
-    """
-
-    OVERVIEW = "overview"
-    DETAILS = "details"
-    NAVIGATION = "navigation"
-
-    CHOICES = [(OVERVIEW, "overview"), (DETAILS, "details"), (NAVIGATION, "navigation")]
-
-
-class AppExtensionOpenAs:
+class AppExtensionTarget:
     """All available ways of opening an app extension.
 
     POPUP - app's extension will be mounted as a popup window
@@ -44,37 +43,3 @@ class AppExtensionOpenAs:
     APP_PAGE = "app_page"
 
     CHOICES = [(POPUP, "popup"), (APP_PAGE, "app_page")]
-
-
-class AppExtensionTarget:
-    """All available places where app's iframe can be mounted.
-
-    MORE_ACTIONS - more actions button
-    CREATE - create button
-    CATALOG - catalog section on navigation bar
-    ORDERS - orders section on navigation bar
-    CUSTOMERS - customers section on navigation bar
-    DISCOUNTS - discounts section on navigation bar
-    TRANSLATION - translations section on navigation bar
-    PAGES - pages section on navigation bar
-    """
-
-    MORE_ACTIONS = "more_actions"
-    CREATE = "create"
-    CATALOG = "catalog"
-    ORDERS = "orders"
-    CUSTOMERS = "customers"
-    DISCOUNTS = "discounts"
-    TRANSLATIONS = "translations"
-    PAGES = "pages"
-
-    CHOICES = [
-        (MORE_ACTIONS, "more_actions"),
-        (CREATE, "create"),
-        (CATALOG, "catalog"),
-        (ORDERS, "orders"),
-        (CUSTOMERS, "customers"),
-        (DISCOUNTS, "discounts"),
-        (TRANSLATIONS, "translations"),
-        (PAGES, "pages"),
-    ]
--- saleor/graphql/app/resolvers.py
+++ saleor/graphql/app/resolvers.py
@@ -1,7 +1,7 @@
 from urllib.parse import urljoin, urlparse
 
 from ...app import models
-from ...app.types import AppExtensionOpenAs
+from ...app.types import AppExtensionTarget
 from ...core.jwt import (
     create_access_token_for_app,
     create_access_token_for_app_extension,
@@ -61,13 +61,13 @@ def resolve_app_extension_url(root):
 
     Apply url stitching when these 3 conditions are met:
         - url starts with /
-        - openAs == "POPUP"
+        - target == "POPUP"
         - appUrl is defined
     """
-    open_as = root.get("open_as", AppExtensionOpenAs.POPUP)
+    target = root.get("target", AppExtensionTarget.POPUP)
     app_url = root["app_url"]
     url = root["url"]
-    if url.startswith("/") and app_url and open_as == AppExtensionOpenAs.POPUP:
+    if url.startswith("/") and app_url and target == AppExtensionTarget.POPUP:
         parsed_url = urlparse(app_url)
         new_path = urljoin(parsed_url.path, url[1:])
         return parsed_url._replace(path=new_path).geturl()
