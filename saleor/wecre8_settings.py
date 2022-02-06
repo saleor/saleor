38c65af565f8a5f475b8a0aa0426477c31b048d0
essamfakher5@gmail.com <Rasmia3112572017>
2022-02-06 08:48:07 +0200

    requirements and settings

--- saleor/wecre8_settings.py
+++ saleor/wecre8_settings.py
@@ -1,6 +1,23 @@
 from saleor.settings import *  # noqa F405
 
-INSTALLED_APPS.append("django_celery_results")  # noqa F405
+CITIES_LIGHT_TRANSLATION_LANGUAGES = ["ar"]
+CITIES_LIGHT_INCLUDE_COUNTRIES = ["SA"]
+CITIES_LIGHT_INCLUDE_CITY_TYPES = [
+    "PPL",
+    "PPLA",
+    "PPLA2",
+    "PPLA3",
+    "PPLA4",
+    "PPLC",
+    "PPLF",
+    "PPLG",
+    "PPLL",
+    "PPLR",
+    "PPLS",
+    "STLMT",
+]
+CITIES_LIGHT_APP_NAME = "provinces"  # noqa F405
+INSTALLED_APPS = ["cities_light", "django_celery_results", *INSTALLED_APPS]  # noqa F405
 
 CELERY_BEAT_SCHEDULE.update(  # noqa F405
     {
