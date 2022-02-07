31ce8b63107a28972890c47c44461079aef00b36
Maciej Korycinski <maciej@mirumee.com>
2022-02-07 13:06:14 +0100

    JWT token signed with RS256 for 3.1 (#8745)
    * Poc for manager which will handle a jwt tokens
    
    * Clean up code. Add missing functionalities. Add JWKS endpoint
    
    * Clean up an error msg
    
    * Apply changes after review
    
    * Add warning logger about missing RSA key
    
    * Update requirements
    
    * Update changelog
    
    * Apply changes after review
    
    * Clean up poetry files after merge main
    
    * Fix missed conclits in .isort file. Remove an additional record from changelog

--- saleor/core/app.py
+++ saleor/core/app.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 from django.conf import settings
 from django.db.models import Field
+from django.utils.module_loading import import_string
 
 from .db.filters import PostgresILike
 
@@ -13,3 +14,20 @@ class CoreAppConfig(AppConfig):
 
         if settings.SENTRY_DSN:
             settings.SENTRY_INIT(settings.SENTRY_DSN, settings.SENTRY_OPTS)
+        self.validate_jwt_manager()
+
+    def validate_jwt_manager(self):
+        jwt_manager_path = getattr(settings, "JWT_MANAGER_PATH", None)
+        if not jwt_manager_path:
+            raise ImportError(
+                "Missing setting value for JWT Manager path - JWT_MANAGER_PATH"
+            )
+        try:
+            jwt_manager = import_string(jwt_manager_path)
+        except ImportError as e:
+            raise ImportError(f"Failed to import JWT manager: {e}.")
+
+        validate_method = getattr(jwt_manager, "validate_configuration", NotImplemented)
+        if validate_method is NotImplemented:
+            return
+        validate_method()
---
+++ saleor/core/tests/test_jwt_manager.py
@@ -0,0 +1,89 @@
+import jwt
+import pytest
+from cryptography.hazmat.primitives import serialization
+from django.core.exceptions import ImproperlyConfigured
+from django.test import override_settings
+
+from ..jwt_manager import JWTManager, get_jwt_manager
+
+
+def test_get_jwt_manager(settings):
+    # given
+    assert settings.JWT_MANAGER_PATH == "saleor.core.jwt_manager.JWTManager"
+
+    # when
+    jwt_manager = get_jwt_manager()
+
+    # then
+    assert jwt_manager == JWTManager
+
+
+@override_settings(RSA_PRIVATE_KEY=None)
+def test_jwt_manager_validate_missing_rsa_private_key(settings):
+    # given
+    jwt_manager = get_jwt_manager()
+
+    # when & then
+    with pytest.raises(ImproperlyConfigured):
+        jwt_manager.validate_configuration()
+
+
+@override_settings(RSA_PRIVATE_KEY="WRONG-FORMAT")
+def test_jwt_manager_validate_incorect_format_of_rsa_private_key(settings):
+    # given
+    jwt_manager = get_jwt_manager()
+
+    # when & then
+    with pytest.raises(ImproperlyConfigured):
+        jwt_manager.validate_configuration()
+
+
+def test_jwt_manager_encode(settings):
+    # given
+    payload = {"A": "1", "B": "2"}
+    jwt_manager = get_jwt_manager()
+    private_key = serialization.load_pem_private_key(
+        settings.RSA_PRIVATE_KEY.encode("utf-8"), password=settings.RSA_PRIVATE_PASSWORD
+    )
+
+    # when
+    token = jwt_manager.encode(payload)
+
+    # then
+    headers = jwt.get_unverified_header(token)
+    decoded_token = jwt.decode(
+        token,
+        private_key.public_key(),
+        algorithms=["RS256"],
+    )
+    assert headers.get("alg") == "RS256"
+    assert decoded_token == payload
+
+
+def test_jwt_manager_decode_token_signed_with_rs256(settings):
+    # given
+    payload = {"A": "1", "B": "2"}
+    private_key = serialization.load_pem_private_key(
+        settings.RSA_PRIVATE_KEY.encode("utf-8"), password=settings.RSA_PRIVATE_PASSWORD
+    )
+    token = jwt.encode(payload, private_key, algorithm="RS256")
+    jwt_manager = get_jwt_manager()
+
+    # when
+    decoded_token = jwt_manager.decode(token)
+
+    # then
+    assert decoded_token == payload
+
+
+def test_jwt_manager_decode_token_signed_with_hs256(settings):
+    # given
+    payload = {"A": "1", "B": "2"}
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm="HS256")
+    jwt_manager = get_jwt_manager()
+
+    # when
+    decoded_token = jwt_manager.decode(token)
+
+    # then
+    assert decoded_token == payload
