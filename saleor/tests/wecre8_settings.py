4c61463ba5dfb669cab316b19dcbfd6c1d6e198a
Islam Mesha <eng.islammesha@gmail.com>
2022-02-07 16:09:44 +0200

    Change PyTest settings file

---
+++ saleor/tests/wecre8_settings.py
@@ -0,0 +1,21 @@
+from tests.settings import *  # noqa
+
+CITIES_LIGHT_INCLUDE_CITY_TYPES = [
+    "PPL",
+    "PPLA",
+    "PPLA2",
+    "PPLA3",
+    "PPLA4",
+    "PPLC",
+    "PPLF",
+    "PPLG",
+    "PPLL",
+    "PPLR",
+    "PPLS",
+    "STLMT",
+]
+CITIES_LIGHT_INCLUDE_COUNTRIES = ["SA"]
+CITIES_LIGHT_TRANSLATION_LANGUAGES = ["ar"]
+CITIES_LIGHT_APP_NAME = "provinces"  # noqa F405
+
+INSTALLED_APPS += ["cities_light", "django_celery_results"]  # noqa F405
