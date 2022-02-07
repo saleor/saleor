f4e64f3a2d961113fa0889f2a30bfe5fbcea5b07
Maciej Korycinski <maciej@mirumee.com>
2022-02-07 16:47:37 +0100

    Refund/void inactive payments in Adyen/Stripe webhooks handling - 3.1 (#9075)
    * Refund/void non active payments in Adyen/Stripe webhooks handling
    
    * Fix typing
    
    * Fix tests
    
    * Refund non active payment for Adyen partial payment
    
    * Add try except for handling refund for inactive payment
    
    Co-authored-by: L3str4nge <mtszzwdzk@gmail.com>

--- saleor/payment/tests/test_payment.py
+++ saleor/payment/tests/test_payment.py
@@ -370,17 +370,6 @@ def test_create_transaction_no_gateway_response(transaction_data):
     assert txn.gateway_response == {}
 
 
-@pytest.mark.parametrize(
-    "func",
-    [gateway.authorize, gateway.capture, gateway.confirm, gateway.refund, gateway.void],
-)
-def test_payment_needs_to_be_active_for_any_action(func, payment_dummy):
-    payment_dummy.is_active = False
-    with pytest.raises(PaymentError) as exc:
-        func(payment_dummy, "token")
-    assert exc.value.message == NOT_ACTIVE_PAYMENT_ERROR
-
-
 @patch.object(PluginsManager, "capture_payment")
 @patch("saleor.order.actions.handle_fully_paid_order")
 def test_gateway_charge_failed(
@@ -560,7 +549,7 @@ def test_can_void(payment_txn_preauth: Payment):
     assert payment_txn_preauth.charge_status == ChargeStatus.NOT_CHARGED
 
     payment_txn_preauth.is_active = False
-    assert not payment_txn_preauth.can_void()
+    assert payment_txn_preauth.can_void()
 
     payment_txn_preauth.is_active = True
     assert payment_txn_preauth.can_void()
--- saleor/payment/utils.py
+++ saleor/payment/utils.py
@@ -19,6 +19,7 @@ from . import (
     PaymentError,
     StorePaymentMethod,
     TransactionKind,
+    gateway,
 )
 from .error_codes import PaymentErrorCode
 from .interface import (
@@ -466,6 +467,39 @@ def price_to_minor_unit(value: Decimal, currency: str):
     number_places = Decimal("10.0") ** precision
     value_without_comma = value * number_places
     return str(value_without_comma.quantize(Decimal("1")))
+
+
+def get_channel_slug_from_payment(payment: Payment) -> Optional[str]:
+    channel_slug = None
+
+    if payment.checkout:
+        channel_slug = payment.checkout.channel.slug
+    elif payment.order:
+        channel_slug = payment.order.channel.slug
+
+    return channel_slug
+
+
+def try_void_or_refund_inactive_payment(
+    payment: Payment, transaction: Transaction, manager: "PluginsManager"
+):
+    """Handle refund or void inactive payments.
+
+    In case when we have open multiple payments for single checkout but only one is
+    active. Some payment methods don't required confirmation so we can receive delayed
+    webhook when we have order already paid.
+    """
+    if transaction.is_success:
+        update_payment_charge_status(payment, transaction)
+        channel_slug = get_channel_slug_from_payment(payment)
+        try:
+            gateway.payment_refund_or_void(payment, manager, channel_slug=channel_slug)
+        except PaymentError:
+            logger.exception(
+                "Unable to void/refund an inactive payment %s, %s.",
+                payment.id,
+                payment.psp_reference,
+            )
 
 
 def payment_owned_by_user(payment_pk: int, user) -> bool:
