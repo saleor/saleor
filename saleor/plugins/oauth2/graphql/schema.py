1630b34dd6c16ec6e50acc2ee9c380997e19a2ee
Islam Mesha <eng.islammesha@gmail.com>
2022-02-10 10:17:29 +0200

    Revert "query test"
    This reverts commit 9569defcb7a506f8c3d0d7c7e0723072a9f9e840.

--- saleor/plugins/oauth2/graphql/schema.py
+++ saleor/plugins/oauth2/graphql/schema.py
@@ -1,24 +1,7 @@
 import graphene
 
-from saleor.account import models
-from saleor.graphql.account.types import UserCountableConnection
-from saleor.graphql.core.connection import create_connection_slice
-from saleor.graphql.core.fields import FilterConnectionField
-
 from ....graphql.core.federation import build_federated_schema
 from .mutations import SocialLogin, SocialLoginByAccessToken, SocialLoginConfirm
-
-
-# test any query
-class Query(graphene.ObjectType):
-    customers2 = FilterConnectionField(
-        UserCountableConnection,
-        description="List of the shop's customers.",
-    )
-
-    def resolve_customers2(self, info, **kwargs):
-        qs = models.User.objects.customers()
-        return create_connection_slice(qs, info, kwargs, UserCountableConnection)
 
 
 class Mutation(graphene.ObjectType):
@@ -27,4 +10,4 @@ class Mutation(graphene.ObjectType):
     social_login_confirm_web = SocialLoginConfirm.Field()
 
 
-schema = build_federated_schema(query=Query, mutation=Mutation)
+schema = build_federated_schema(mutation=Mutation)
