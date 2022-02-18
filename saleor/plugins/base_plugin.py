59097d776434521d6c7406cc8e857b38712e54dc
Mikail KOCAK <mikail.kocak@saleor.io>
2022-02-18 18:30:19 +0100

    Add possibility for plugins to execute code before each mutation
    This would allow plugins to execute custom logic whenever a mutation is invoked. The call would only happen once the permissions are checked.

--- saleor/plugins/base_plugin.py
+++ saleor/plugins/base_plugin.py
@@ -7,6 +7,9 @@ from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.utils.functional import SimpleLazyObject
 from django_countries.fields import Country
+from graphene import Mutation
+from graphql import GraphQLError, ResolveInfo
+from graphql.execution import ExecutionResult
 from prices import Money, TaxedMoney
 from promise.promise import Promise
 
@@ -535,6 +538,26 @@ class BasePlugin:
 
     # Triggers retry mechanism for event delivery
     event_delivery_retry: Callable[["EventDelivery", Any], EventDelivery]
+
+    # Invoked before each mutation is executed
+    #
+    # This allows to trigger specific logic before the mutation is executed
+    # but only once the permissions are checked.
+    #
+    # Returns one of:
+    #     - null if the execution shall continue
+    #     - an execution result
+    #     - graphql.GraphQLError
+    perform_mutation: Callable[
+        [
+            Optional[Union[ExecutionResult, GraphQLError]],  # previous value
+            Mutation,  # mutation class
+            Any,  # mutation root
+            ResolveInfo,  # resolve info
+            dict,  # mutation data
+        ],
+        Optional[Union[ExecutionResult, GraphQLError]],
+    ]
 
     def token_is_required_as_payment_input(self, previous_value):
         return previous_value
