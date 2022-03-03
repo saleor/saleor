e30156eb37d28202ab820dd9f7e54fdaa4052973
Ahmed Ibrahim <ahmeddark369@gmail.com>
2022-03-03 09:01:56 +0200

    change logo and header to image instead of strings

--- saleor/plugins/vendor/graphql/types.py
+++ saleor/plugins/vendor/graphql/types.py
@@ -1,14 +1,21 @@
 import graphene
 from graphene import relay
 
+from ....graphql.account.enums import CountryCodeEnum
 from ....graphql.core.connection import CountableDjangoObjectType
+from ....graphql.core.types.common import Image
 from .. import models
 
 
 class Vendor(CountableDjangoObjectType):
     users = graphene.List(graphene.ID, description="List of user IDs.")
-
     variants = graphene.List(graphene.ID, description="List of variant IDs.")
+    logo = graphene.Field(Image, size=graphene.Int(description="Size of the image."))
+    header_image = graphene.Field(
+        Image, size=graphene.Int(description="Size of the image.")
+    )
+    description = graphene.JSONString(description="Editorjs formatted description")
+    country = CountryCodeEnum(description="Country.")
 
     class Meta:
         model = models.Vendor
@@ -20,6 +27,26 @@ class Vendor(CountableDjangoObjectType):
 
     def resolve_variants(root, info):
         return root.variants.values_list("id")
+
+    def resolve_logo(root, info, size=None):
+        if root.logo:
+            return Image.get_adjusted(
+                image=root.background_image,
+                alt=f"{root.name}'s logo",
+                size=size,
+                rendition_key_set="logo",
+                info=info,
+            )
+
+    def resolve_header_image(root, info, size=None):
+        if root.header_image:
+            return Image.get_adjusted(
+                image=root.header_image,
+                alt=f"{root.name}'s header image",
+                size=size,
+                rendition_key_set="background_images",
+                info=info,
+            )
 
 
 class VendorConnection(relay.Connection):
