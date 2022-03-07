d18abe7309503280a109f7b1d032a90d4fca9fbe
essamfakher5@gmail.com <Rasmia3112572017>
2022-03-07 02:57:45 +0200

    model and migrations

--- saleor/plugins/celebrity/models.py
+++ saleor/plugins/celebrity/models.py
@@ -3,6 +3,7 @@ from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
 
 from ...account.validators import validate_possible_number
+from ...product.models import ProductVariant
 
 
 class PossiblePhoneNumberField(PhoneNumberField):
@@ -13,17 +14,18 @@ class Celebrity(models.Model):
 
     first_name = models.CharField(max_length=256, db_index=True)
     last_name = models.CharField(max_length=256, db_index=True)
-    phone_number = PossiblePhoneNumberField(db_index=True)
+    phone_number = PossiblePhoneNumberField(db_index=True, unique=True)
     email = models.EmailField(unique=True)
     country = CountryField()
     city = models.CharField(max_length=256, blank=True, null=True)
-    website = models.TextField(blank=True, null=True)
-    instagram_link = models.TextField(blank=True, null=True)
-    twitter_link = models.TextField(blank=True, null=True)
+    website_url = models.URLField(blank=True, null=True)
+    instagram_url = models.URLField(blank=True, null=True)
+    twitter_url = models.URLField(blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     about = models.TextField(blank=True, null=True)
     is_active = models.BooleanField()
     logo = models.ImageField(blank=True, null=True)
     header_image = models.ImageField(blank=True, null=True)
+    variants = models.ManyToManyField(ProductVariant)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
