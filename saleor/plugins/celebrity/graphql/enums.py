f2692cb8798ba1a592030dff60c818d00124415a
essamfakher5@gmail.com <Rasmia3112572017>
2022-03-07 03:02:48 +0200

    enums and errors

--- saleor/plugins/celebrity/graphql/enums.py
+++ saleor/plugins/celebrity/graphql/enums.py
@@ -4,3 +4,10 @@ from enum import Enum
 class CelebrityErrorCode(Enum):
     CELEBRITY_NOT_FOUND = "celebrity_not_found"
     CELEBRITY_ERROR = "celebrity_error"
+    UNKNOWN_ERROR = "unknown_error"
+    INVALID_FIRST_NAME = "invalid_first_name"
+    INVALID_LAST_NAME = "invalid_last_name"
+    INVALID_CELEBRITY = "invalid_celebrity"
+    INVALID_PHONE_NUMBER = "invalid_phone_number"
+    INVALID_EMAIL = "invalid_email"
+    INVALID_FIELD_VALUE = "invalid_field_value"
