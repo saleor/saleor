488f8e87e390b4a12093c2718480e451ce1b8e92
Islam Mesha <eng.islammesha@gmail.com>
2022-03-08 14:17:45 +0200

    Index product celebrities to Algolia

--- saleor/plugins/celebrity/models.py
+++ saleor/plugins/celebrity/models.py
@@ -29,3 +29,6 @@ class Celebrity(models.Model):
     products = models.ManyToManyField(Product)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
