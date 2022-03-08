f2052e6a9bcc8db47e440824508cd338eb63a629
Maciej Korycinski <maciej@mirumee.com>
2022-03-08 09:09:12 +0100

    Fix for sending empty emails 3.0 (#9316)
    * Check if template is not an empty string
    
    * Check if template is not an empty string in Admin email plugin

--- saleor/plugins/admin_email/notify_events.py
+++ saleor/plugins/admin_email/notify_events.py
@@ -24,6 +24,9 @@ def send_set_staff_password_email(
         constants.SET_STAFF_PASSWORD_DEFAULT_TEMPLATE,
         constants.DEFAULT_EMAIL_TEMPLATES_PATH,
     )
+    if not template:
+        # Empty template means that we don't want to trigger a given event.
+        return
     subject = get_email_subject(
         plugin.configuration,
         constants.SET_STAFF_PASSWORD_SUBJECT_FIELD,
@@ -45,6 +48,9 @@ def send_csv_product_export_success(
             constants.CSV_PRODUCT_EXPORT_SUCCESS_DEFAULT_TEMPLATE,
             constants.DEFAULT_EMAIL_TEMPLATES_PATH,
         )
+        if not template:
+            # Empty template means that we don't want to trigger a given event.
+            return
         subject = get_email_subject(
             plugin.configuration,
             constants.CSV_PRODUCT_EXPORT_SUCCESS_SUBJECT_FIELD,
@@ -65,6 +71,9 @@ def send_staff_order_confirmation(
         constants.STAFF_ORDER_CONFIRMATION_DEFAULT_TEMPLATE,
         constants.DEFAULT_EMAIL_TEMPLATES_PATH,
     )
+    if not template:
+        # Empty template means that we don't want to trigger a given event.
+        return
     subject = get_email_subject(
         plugin.configuration,
         constants.STAFF_ORDER_CONFIRMATION_SUBJECT_FIELD,
@@ -84,6 +93,9 @@ def send_csv_export_failed(payload: dict, config: dict, plugin: "AdminEmailPlugi
             constants.CSV_EXPORT_FAILED_TEMPLATE_DEFAULT_TEMPLATE,
             constants.DEFAULT_EMAIL_TEMPLATES_PATH,
         )
+        if not template:
+            # Empty template means that we don't want to trigger a given event.
+            return
         subject = get_email_subject(
             plugin.configuration,
             constants.CSV_EXPORT_FAILED_SUBJECT_FIELD,
@@ -103,6 +115,9 @@ def send_staff_reset_password(payload: dict, config: dict, plugin: "AdminEmailPl
             constants.STAFF_PASSWORD_RESET_DEFAULT_TEMPLATE,
             constants.DEFAULT_EMAIL_TEMPLATES_PATH,
         )
+        if not template:
+            # Empty template means that we don't want to trigger a given event.
+            return
         subject = get_email_subject(
             plugin.configuration,
             constants.STAFF_PASSWORD_RESET_SUBJECT_FIELD,
