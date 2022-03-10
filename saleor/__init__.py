7059896c86d9aaeba63fee53b2e283082255a886
Marcin Gębala <maarcin.gebala@gmail.com>
2022-03-10 11:04:33 +0100

    Release 3.1.5

--- saleor/__init__.py
+++ saleor/__init__.py
@@ -3,7 +3,7 @@ from graphql.utils import schema_printer
 from .celeryconf import app as celery_app
 
 __all__ = ["celery_app"]
-__version__ = "3.1.4"
+__version__ = "3.1.5"
 
 
 def patched_print_object(type):
