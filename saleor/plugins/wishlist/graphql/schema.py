f3211fca3e4ad22cc43a645791975cb8fbd9d8e8
essamfakher5@gmail.com <Rasmia3112572017>
2022-03-10 14:28:38 +0200

    get wishlist from user

--- saleor/plugins/wishlist/graphql/schema.py
+++ saleor/plugins/wishlist/graphql/schema.py
@@ -1,12 +1,30 @@
 import graphene
 from graphene_federation import build_schema
 
+from saleor.graphql.account.types import User as UserType
+from saleor.graphql.core.utils import from_global_id_or_error
+
+from .. import models
+from . import types
 from .mutations import (
     WishlistAddProductMutation,
     WishlistAddProductVariantMutation,
     WishlistRemoveProductMutation,
     WishlistRemoveProductVariantMutation,
 )
+
+
+class Query(graphene.ObjectType):
+
+    wishlist = graphene.Field(
+        types.Wishlist,
+        user_id=graphene.Argument(graphene.ID, description="User ID.", required=True),
+        description="Look up a vendor by ID",
+    )
+
+    def resolve_wishlist(self, info, user_id, **data):
+        _, id = from_global_id_or_error(user_id, UserType)
+        return models.Wishlist.objects.get(user_id=id)
 
 
 class Mutation(graphene.ObjectType):
@@ -16,6 +34,4 @@ class Mutation(graphene.ObjectType):
     wishlist_remove_variant = WishlistRemoveProductVariantMutation.Field()
 
 
-schema = build_schema(
-    mutation=Mutation,
-)
+schema = build_schema(query=Query, mutation=Mutation, types=[types.Wishlist])
