e2c505b8b94e0875767daaadb9752334bfca3021
essamfakher5@gmail.com <Rasmia3112572017>
2022-03-13 19:07:13 +0200

    update the types of wishlists

--- saleor/plugins/wishlist/graphql/types.py
+++ saleor/plugins/wishlist/graphql/types.py
@@ -1,43 +1,66 @@
 import graphene
+from graphene import relay
 
-from saleor.graphql.channel import ChannelQsContext
 from saleor.graphql.core.connection import (
     CountableDjangoObjectType,
     create_connection_slice,
 )
-from saleor.graphql.core.fields import FilterConnectionField
-from saleor.graphql.product.filters import ProductFilterInput, ProductVariantFilterInput
-from saleor.graphql.product.types import (
-    ProductCountableConnection,
-    ProductVariantCountableConnection,
-)
+from saleor.graphql.core.fields import ConnectionField
+from saleor.product.models import Product, ProductVariant
 
 from .. import models
 
 
+class ProductWishlistType(CountableDjangoObjectType):
+    class Meta:
+        interfaces = (graphene.relay.Node,)
+        model = Product
+        only_fields = [
+            "id",
+            "name",
+            "slug",
+            "description",
+            "description_plaintext",
+            "updated_at",
+            "charge_taxes",
+            "weight",
+            "rating",
+        ]
+
+
+class ProductWishlistTypeConnection(relay.Connection):
+    class Meta:
+        node = ProductWishlistType
+
+
+class VariantWishlistType(CountableDjangoObjectType):
+    class Meta:
+        interfaces = (graphene.relay.Node,)
+        model = ProductVariant
+        only_fields = [
+            "id",
+            "name",
+            "track_inventory",
+            "is_preorder",
+            "preorder_end_date",
+            "preorder_global_threshold",
+            "quantity_limit_per_customer",
+            "weight",
+        ]
+
+
+class VariantWishlistTypeConnection(relay.Connection):
+    class Meta:
+        node = VariantWishlistType
+
+
 class Wishlist(CountableDjangoObjectType):
-    products = FilterConnectionField(
-        ProductCountableConnection,
-        ids=graphene.List(
-            graphene.ID, description="Filter product variants by given IDs."
-        ),
-        channel=graphene.String(
-            description="Slug of a channel for which the data should be returned."
-        ),
-        filter=ProductFilterInput(description="Filtering options for product variant."),
+    products = ConnectionField(
+        ProductWishlistTypeConnection,
         description="List of product variants.",
     )
-    variants = FilterConnectionField(
-        ProductVariantCountableConnection,
-        ids=graphene.List(
-            graphene.ID, description="Filter product variants by given IDs."
-        ),
-        channel=graphene.String(
-            description="Slug of a channel for which the data should be returned."
-        ),
-        filter=ProductVariantFilterInput(
-            description="Filtering options for product variant."
-        ),
+    variants = ConnectionField(
+        VariantWishlistTypeConnection,
         description="List of product variants.",
     )
 
@@ -51,15 +74,12 @@ class Wishlist(CountableDjangoObjectType):
         exclude = ["user", "token"]
 
     def resolve_variants(root, info, ids=None, channel=None, **kwargs):
-
         groups = models.Wishlist.objects.get(pk=root.id)
-        qs = ChannelQsContext(groups.variants.all(), channel)
-        return create_connection_slice(
-            qs, info, kwargs, ProductVariantCountableConnection
-        )
+        qs = groups.variants.all()
+        return create_connection_slice(qs, info, kwargs, VariantWishlistTypeConnection)
 
     def resolve_products(root, info, ids=None, channel=None, **kwargs):
-
         groups = models.Wishlist.objects.get(pk=root.id)
-        qs = ChannelQsContext(groups.products.all(), channel)
-        return create_connection_slice(qs, info, kwargs, ProductCountableConnection)
+
+        qs = groups.products.all()
+        return create_connection_slice(qs, info, kwargs, ProductWishlistTypeConnection)
