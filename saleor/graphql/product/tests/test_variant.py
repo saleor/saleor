34a18d6bb242dd89bf2c6883653db0379f910ac0
l3str4nge <mateusz.zawadzki@saleor.io>
2022-03-18 14:40:16 +0100

    Fix str object has no attribute input type error (#9345)
    * Add checking if given attributes are variant attributes in ProductVariantCreate mutation
    
    * Fix compatibility between ProductVarianCreate and ProductVariantUpdate clean_input method
    
    * Simplify finding invalid attributes and fix dots
    
    * Make fixtures names and slugs more unique. Use set comprehension instead of casting to set.
    
    * Add checking if given attributes are variant attributes in ProductVantCreate mutation
    
    * Raise error when trying to assign attributes when has_variants=False
    
    * Update saleor/tests/fixtures.py
    
    Co-authored-by: Tomasz Szymański <lime129@gmail.com>
    
    * Update saleor/tests/fixtures.py
    
    Co-authored-by: Tomasz Szymański <lime129@gmail.com>
    
    * Update saleor/tests/fixtures.py
    
    Co-authored-by: Tomasz Szymański <lime129@gmail.com>
    
    * Make query more reusable in tests
    
    Co-authored-by: Tomasz Szymański <lime129@gmail.com>

--- saleor/graphql/product/tests/test_variant.py
+++ saleor/graphql/product/tests/test_variant.py
@@ -6044,3 +6044,80 @@ def test_product_variant_deactivate_preorder_as_app(
     )
 
     assert_no_permission(response)
+
+
+VARIANT_CREATE_MUTATION = """
+    mutation variantCreate($input: ProductVariantCreateInput!) {
+        productVariantCreate (input: $input)
+        {
+            productVariant {
+                id
+            }
+            errors {
+                field,
+                message,
+                code,
+                attributes
+            }
+        }
+    }
+"""
+
+
+def test_variant_create_product_without_variant_attributes(
+    product_with_product_attributes, staff_api_client, permission_manage_products
+):
+    product = product_with_product_attributes
+
+    prod_id = graphene.Node.to_global_id("Product", product.pk)
+    attr_id = graphene.Node.to_global_id(
+        "Attribute", product.product_type.product_attributes.first().pk
+    )
+    input = {
+        "sku": "my-sku",
+        "product": prod_id,
+        "attributes": [{"id": attr_id, "values": ["1"]}],
+    }
+    response = staff_api_client.post_graphql(
+        VARIANT_CREATE_MUTATION,
+        variables={"input": input},
+        permissions=[permission_manage_products],
+    )
+    content = get_graphql_content(response)
+
+    errors = content["data"]["productVariantCreate"]["errors"]
+    assert errors
+    assert errors[0]["code"] == ProductErrorCode.ATTRIBUTE_CANNOT_BE_ASSIGNED.name
+    assert len(errors[0]["attributes"]) == 1
+    assert errors[0]["attributes"][0] == attr_id
+
+
+def test_variant_create_product_with_variant_attributes_variant_flag_false(
+    product_with_variant_attributes, staff_api_client, permission_manage_products
+):
+    product = product_with_variant_attributes
+
+    product_type = product.product_type
+    product_type.has_variants = False
+    product_type.save()
+
+    prod_id = graphene.Node.to_global_id("Product", product.pk)
+    attr_id = graphene.Node.to_global_id(
+        "Attribute", product.product_type.variant_attributes.first().pk
+    )
+
+    input = {
+        "sku": "my-sku",
+        "product": prod_id,
+        "attributes": [{"id": attr_id, "values": ["1"]}],
+    }
+    response = staff_api_client.post_graphql(
+        VARIANT_CREATE_MUTATION,
+        variables={"input": input},
+        permissions=[permission_manage_products],
+    )
+    content = get_graphql_content(response)
+
+    errors = content["data"]["productVariantCreate"]["errors"]
+    assert errors
+    assert errors[0]["code"] == ProductErrorCode.INVALID.name
