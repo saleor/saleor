ed7bf15bed24d8eedd155cdff35741db6718a53a
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2022-03-30 13:08:20 +0200

    Make quantity optional in `CheckoutLinesUpdate` mutation (#9430)
    * Make quantity optional in CheckoutLinesUpdate mutation
    
    * Refactor add_variants_to_checkout checkout utils

--- saleor/graphql/checkout/tests/deprecated/test_checkout_lines_update.py
+++ saleor/graphql/checkout/tests/deprecated/test_checkout_lines_update.py
@@ -11,7 +11,7 @@ from ...mutations.utils import update_checkout_shipping_method_if_invalid
 
 MUTATION_CHECKOUT_LINES_UPDATE = """
     mutation checkoutLinesUpdate(
-            $checkoutId: ID, $token: UUID, $lines: [CheckoutLineInput!]!) {
+            $checkoutId: ID, $token: UUID, $lines: [CheckoutLineUpdateInput!]!) {
         checkoutLinesUpdate(checkoutId: $checkoutId, token: $token, lines: $lines) {
             checkout {
                 token
