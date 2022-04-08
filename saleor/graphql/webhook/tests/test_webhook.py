2f6174f2e1fbbea3ae35a26851a7fbe025963c6e
jakubkuc <jakub.kuc.87@gmail.com>
2022-04-08 12:50:58 +0200

    add subscription_query field to Webhook type and raise coverage for subscription tests

--- saleor/graphql/webhook/tests/test_webhook.py
+++ saleor/graphql/webhook/tests/test_webhook.py
@@ -349,6 +349,7 @@ QUERY_WEBHOOK = """
       webhook(id: $id) {
         id
         isActive
+        subscriptionQuery
         asyncEvents {
           eventType
         }
@@ -372,6 +373,27 @@ def test_query_webhook_by_staff(staff_api_client, webhook, permission_manage_app
     webhook_response = content["data"]["webhook"]
     assert webhook_response["id"] == webhook_id
     assert webhook_response["isActive"] == webhook.is_active
+    assert webhook_response["subscriptionQuery"] is None
+    events = webhook.events.all()
+    assert len(events) == 1
+    assert events[0].event_type == WebhookEventTypeAsyncEnum.ORDER_CREATED.value
+
+
+def test_query_webhook_with_subscription_query_by_staff(
+    staff_api_client, subscription_order_created_webhook, permission_manage_apps
+):
+    query = QUERY_WEBHOOK
+    webhook = subscription_order_created_webhook
+    webhook_id = graphene.Node.to_global_id("Webhook", webhook.pk)
+    variables = {"id": webhook_id}
+    staff_api_client.user.user_permissions.add(permission_manage_apps)
+    response = staff_api_client.post_graphql(query, variables=variables)
+
+    content = get_graphql_content(response)
+    webhook_response = content["data"]["webhook"]
+    assert webhook_response["id"] == webhook_id
+    assert webhook_response["isActive"] == webhook.is_active
+    assert webhook_response["subscriptionQuery"] == webhook.subscription_query
     events = webhook.events.all()
     assert len(events) == 1
     assert events[0].event_type == WebhookEventTypeAsyncEnum.ORDER_CREATED.value
