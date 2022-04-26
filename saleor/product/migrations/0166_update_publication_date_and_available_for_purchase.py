5bacd09722cd4f9b37926761eb9a8cb7e3afa77a
IKarbowiak <iga.karbowiak@mirumee.com>
2022-04-26 13:05:50 +0200

    Fix migration conflict in product app

--- saleor/product/migrations/0166_update_publication_date_and_available_for_purchase.py
+++ saleor/product/migrations/0166_update_publication_date_and_available_for_purchase.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("product", "0163_auto_20220414_1025"),
+        ("product", "0165_update_product_search_document"),
     ]
 
     operations = [
