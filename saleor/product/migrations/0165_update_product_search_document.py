6af4a2d660ce4474ca579f9d62ca1698a4e1dc5f
Marcin Gębala <5421321+maarcingebala@users.noreply.github.com>
2022-04-28 16:31:43 +0200

    Get or create permissions in migrations (#9662)
    * Get or create permissions in migrations
    
    * Use weak=False for connecting signal handlers

--- saleor/product/migrations/0165_update_product_search_document.py
+++ saleor/product/migrations/0165_update_product_search_document.py
@@ -8,7 +8,7 @@ def update_product_search_document_values(apps, _schema_editor):
     def on_migrations_complete(sender=None, **kwargs):
         set_product_search_document_values.delay()
 
-    post_migrate.connect(on_migrations_complete)
+    post_migrate.connect(on_migrations_complete, weak=False)
 
 
 class Migration(migrations.Migration):
