68a510971939bfa57dbb255e45f9a62dcc71c05d
Szymon Jędrasiak <sz.jedrasiak@gmail.com>
2022-05-05 12:04:40 +0200

    Schema update after rebase

--- saleor/tests/fixtures.py
+++ saleor/tests/fixtures.py
@@ -5130,7 +5130,7 @@ def webhook_app(
     permission_manage_shipping,
     permission_manage_gift_card,
     permission_manage_discounts,
-    permission_manage_menus
+    permission_manage_menus,
 ):
     app = App.objects.create(name="Sample app objects", is_active=True)
     app.permissions.add(permission_manage_shipping)
