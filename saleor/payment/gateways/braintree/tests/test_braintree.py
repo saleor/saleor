82b6dbdb4e2a0000641a190348b4025535dacf42
Anna Sirota <anna@blender.org>
2022-05-16 15:11:26 +0200

    Fix Braintree merchant accounts mismatch error
    In case Braintree is configured with multiple accounts
    for different currencies, unless a relevant merchant account ID
    is used to generate a client token before 3D Secure flow is completed,
    transaction will fail with the following error:
    
      code 91584: Merchant account must match the 3D Secure authorization merchant account
    
    This change ensures a correct Braintree merchant account
    is used to generate client tokens.

--- saleor/payment/gateways/braintree/tests/test_braintree.py
+++ saleor/payment/gateways/braintree/tests/test_braintree.py
@@ -190,7 +190,28 @@ def test_get_client_token(mock_gateway, gateway_config):
     mock_gateway.return_value = Mock(client_token=Mock(generate=mock_generate))
     token = get_client_token(gateway_config)
     mock_gateway.assert_called_once_with(**gateway_config.connection_params)
-    mock_generate.assert_called_once_with()
+    mock_generate.assert_called_once_with({})
+    assert token == expected_token
+
+
+@pytest.fixture
+def gateway_config_with_merchant_account_id(gateway_config):
+    gateway_config.connection_params["merchant_account_id"] = "foobar"
+    return gateway_config
+
+
+@patch("saleor.payment.gateways.braintree.get_braintree_gateway")
+def test_get_client_token_with_merchant_account_id(
+    mock_gateway, gateway_config_with_merchant_account_id
+):
+    expected_token = "client-token"
+    mock_generate = Mock(return_value=expected_token)
+    mock_gateway.return_value = Mock(client_token=Mock(generate=mock_generate))
+    token = get_client_token(gateway_config_with_merchant_account_id)
+    mock_gateway.assert_called_once_with(
+        **gateway_config_with_merchant_account_id.connection_params
+    )
+    mock_generate.assert_called_once_with({"merchant_account_id": "foobar"})
     assert token == expected_token
 
 
@@ -214,6 +235,37 @@ def test_get_client_token_with_customer_id(
         **gateway_config_with_store_enabled.connection_params
     )
     mock_generate.assert_called_once_with({"customer_id": "1234"})
+    assert token == expected_token
+
+
+@pytest.fixture
+def gateway_config_with_merchant_account_id_and_store_enabled(
+    gateway_config_with_merchant_account_id,
+):
+    gateway_config_with_merchant_account_id.store_customer = True
+    return gateway_config_with_merchant_account_id
+
+
+@patch("saleor.payment.gateways.braintree.get_braintree_gateway")
+def test_get_client_token_with_customer_id_and_merchant_account_id(
+    mock_gateway, gateway_config_with_merchant_account_id_and_store_enabled
+):
+    expected_token = "client-token"
+    mock_generate = Mock(return_value=expected_token)
+    mock_gateway.return_value = Mock(client_token=Mock(generate=mock_generate))
+    token = get_client_token(
+        gateway_config_with_merchant_account_id_and_store_enabled,
+        TokenConfig(customer_id="1234"),
+    )
+    mock_gateway.assert_called_once_with(
+        **gateway_config_with_merchant_account_id_and_store_enabled.connection_params
+    )
+    mock_generate.assert_called_once_with(
+        {
+            "customer_id": "1234",
+            "merchant_account_id": "foobar",
+        }
+    )
     assert token == expected_token
 
 
