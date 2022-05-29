7e19bfa3d08b497b8b3339fc091fdfedba4d9266
Przemysław Łada <przlada@gmail.com>
2022-05-29 20:13:14 +0200

    Refactor worker

--- saleor/webhook/observability/worker.py
+++ saleor/webhook/observability/worker.py
@@ -4,7 +4,7 @@ import os
 from queue import Empty, Full, Queue
 from threading import Lock, Thread
 from time import monotonic
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Dict, List, Optional
 
 from django.conf import settings
 
@@ -18,10 +18,12 @@ FLUSH_TIMEOUT = 2.0
 logger = logging.getLogger(__name__)
 
 
-def _shutdown_worker():
+def shutdown_worker(timeout=FLUSH_TIMEOUT):
+    global _worker
     logger.debug("[Observability] Atexit shutdown signal")
     if _worker:
-        _worker.close()
+        _worker.flush(timeout)
+        _worker = None
 
 
 def init():
@@ -31,7 +33,7 @@ def init():
             batch_size=settings.OBSERVABILITY_BUFFER_BATCH_SIZE,
             timeout=settings.OBSERVABILITY_REPORT_PERIOD.total_seconds(),
         )
-        atexit.register(_shutdown_worker)
+        atexit.register(shutdown_worker)
 
 
 def put_event(buffer_name: str, generate_payload: Callable[[], Any]) -> bool:
@@ -39,6 +41,10 @@ def put_event(buffer_name: str, generate_payload: Callable[[], Any]) -> bool:
         return _worker.submit_event(buffer_name, generate_payload)
     logger.warning("[Observability] Worker not initialized, event dropped")
     return False
+
+
+def buffer_put_multi_key_events(events: Dict[str, List]):
+    return get_buffer("").put_multi_key_events(events)
 
 
 class BackgroundWorker:
@@ -80,7 +86,7 @@ class BackgroundWorker:
                 if events_count:
                     with opentracing_trace("put_events", "buffer"):
                         try:
-                            get_buffer("").put_multi_key_events(events)
+                            buffer_put_multi_key_events(events)
                         except Exception:
                             logger.error(
                                 "[Observability] Buffer error, dropped %s events",
@@ -137,30 +143,21 @@ class BackgroundWorker:
     def _wait_flush(self, timeout: float):
         initial_timeout = min(0.1, timeout)
         if not self._timed_queue_join(initial_timeout):
-            pending = self._queue.qsize() + 1
+            pending = self._queue.qsize()
             logger.debug("[Observability] %d events pending on flush", pending)
             if not self._timed_queue_join(timeout - initial_timeout):
-                pending = self._queue.qsize() + 1
+                pending = self._queue.qsize()
                 logger.error(
                     "[Observability] Flush timed out, dropped %s events", pending
                 )
 
     def flush(self, timeout: float):
         with self._lock:
+            try:
+                self._queue.put_nowait(_TERMINATOR)
+            except Full:
+                logger.debug("[Observability] Worker queue full, flush failed")
             if self.is_alive and timeout > 0.0:
                 self._wait_flush(timeout)
-
-    def kill(self):
-        logger.debug("[Observability] Worker got kill request")
-        with self._lock:
-            if self._thread:
-                try:
-                    self._queue.put_nowait(_TERMINATOR)
-                except Full:
-                    logger.debug("[Observability] Worker queue full, kill failed")
-                self._thread = None
-                self._thread_for_pid = None
-
-    def close(self):
-        self.flush(FLUSH_TIMEOUT)
-        self.kill()
+            self._thread = None
+            self._thread_for_pid = None
