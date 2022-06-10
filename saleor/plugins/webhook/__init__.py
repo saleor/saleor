a7ea2b95289dae94f8155198b2aceb8290791118
Tomasz Magulski <tomasz.magulski@saleor.io>
2022-06-10 18:03:17 +0200

    [SALEOR-6404] Sign webhook paylaod using RS256 and private key used JWT infrastructure

--- saleor/plugins/webhook/__init__.py
+++ saleor/plugins/webhook/__init__.py
@@ -1,9 +1,15 @@
 import hashlib
 import hmac
 
+from jwt.algorithms import get_default_algorithms
+
+from ...core.jwt_manager import get_jwt_manager
+
 
 def signature_for_payload(body: bytes, secret_key):
-    if not secret_key:
-        return ""
+    if secret_key is None:
+        algorithm = get_default_algorithms()["RS256"]
+        private_key = get_jwt_manager().get_private_key()
+        return algorithm.sign(body, private_key).hex()
     hash = hmac.new(bytes(secret_key, "utf-8"), body, hashlib.sha256)
     return hash.hexdigest()
