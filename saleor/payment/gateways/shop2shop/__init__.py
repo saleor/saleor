7e8e2adfd865905dbfc58eff13f56d48848be269
Byron Rode <byronrode@gmail.com>
2022-06-10 13:31:55 +0200

    Added Shop2Shop Payment Gateway

---
+++ saleor/payment/gateways/shop2shop/__init__.py
@@ -0,0 +1,140 @@
+import uuid
+
+from ... import ChargeStatus, TransactionKind
+from ...interface import GatewayConfig, GatewayResponse, PaymentData, PaymentMethodInfo
+
+
+def dummy_success():
+    return True
+
+
+def get_client_token(**_):
+    return str(uuid.uuid4())
+
+
+def authorize(
+    payment_information: PaymentData, config: GatewayConfig
+) -> GatewayResponse:
+    success = dummy_success()
+    error = None
+    if not success:
+        error = "Unable to authorize transaction"
+    return GatewayResponse(
+        is_success=success,
+        action_required=False,
+        kind=TransactionKind.AUTH,
+        amount=payment_information.amount,
+        currency=payment_information.currency,
+        transaction_id=payment_information.token,
+        error=error,
+        payment_method_info=PaymentMethodInfo(
+            last_4="1234",
+            exp_year=2222,
+            exp_month=12,
+            brand="dummy_visa",
+            name="Holder name",
+            type="card",
+        ),
+    )
+
+
+def void(payment_information: PaymentData, config: GatewayConfig) -> GatewayResponse:
+    error = None
+    success = dummy_success()
+    if not success:
+        error = "Unable to void the transaction."
+    return GatewayResponse(
+        is_success=success,
+        action_required=False,
+        kind=TransactionKind.VOID,
+        amount=payment_information.amount,
+        currency=payment_information.currency,
+        transaction_id=payment_information.token,
+        error=error,
+    )
+
+
+def capture(payment_information: PaymentData, config: GatewayConfig) -> GatewayResponse:
+    """Perform capture transaction."""
+    error = None
+    success = dummy_success()
+    if not success:
+        error = "Unable to process capture"
+
+    return GatewayResponse(
+        is_success=success,
+        action_required=False,
+        kind=TransactionKind.CAPTURE,
+        amount=payment_information.amount,
+        currency=payment_information.currency,
+        transaction_id=payment_information.token,
+        error=error,
+        payment_method_info=PaymentMethodInfo(
+            last_4="1234",
+            exp_year=2222,
+            exp_month=12,
+            brand="dummy_visa",
+            name="Holder name",
+            type="card",
+        ),
+    )
+
+
+def confirm(payment_information: PaymentData, config: GatewayConfig) -> GatewayResponse:
+    """Perform confirm transaction."""
+    error = None
+    success = dummy_success()
+    if not success:
+        error = "Unable to process capture"
+
+    return GatewayResponse(
+        is_success=success,
+        action_required=False,
+        kind=TransactionKind.CAPTURE,
+        amount=payment_information.amount,
+        currency=payment_information.currency,
+        transaction_id=payment_information.token,
+        error=error,
+    )
+
+
+def refund(payment_information: PaymentData, config: GatewayConfig) -> GatewayResponse:
+    error = None
+    success = dummy_success()
+    if not success:
+        error = "Unable to process refund"
+    return GatewayResponse(
+        is_success=success,
+        action_required=False,
+        kind=TransactionKind.REFUND,
+        amount=payment_information.amount,
+        currency=payment_information.currency,
+        transaction_id=payment_information.token,
+        error=error,
+    )
+
+
+def process_payment(
+    payment_information: PaymentData, config: GatewayConfig
+) -> GatewayResponse:
+    """Process the payment."""
+    token = payment_information.token
+
+    # Process payment normally if payment token is valid
+    if token not in dict(ChargeStatus.CHOICES):
+        return capture(payment_information, config)
+
+    # Process payment by charge status which is selected in the payment form
+    # Note that is for testing by dummy gateway only
+    charge_status = token
+    authorize_response = authorize(payment_information, config)
+    if charge_status == ChargeStatus.NOT_CHARGED:
+        return authorize_response
+
+    if not config.auto_capture:
+        return authorize_response
+
+    capture_response = capture(payment_information, config)
+    if charge_status == ChargeStatus.FULLY_REFUNDED:
+        return refund(payment_information, config)
+    return capture_response
---
+++ saleor/payment/gateways/shop2shop/tests/test_shop2shop.py
@@ -0,0 +1,221 @@
+from decimal import Decimal
+
+import pytest
+
+from .... import ChargeStatus, PaymentError, TransactionKind, gateway
+
+
+@pytest.fixture(autouse=True)
+def setup_shop2shop_gateway(settings):
+    settings.PLUGINS = [
+        "saleor.payment.gateways.shop2shop.plugin.Shop2ShopGatewayPlugin"]
+    return settings
+
+
+def test_authorize_success(payment_shop2shop):
+    txn = gateway.authorize(payment=payment_shop2shop, token="Fake")
+    assert txn.is_success
+    assert txn.kind == TransactionKind.AUTH
+    assert txn.payment == payment_shop2shop
+    payment_shop2shop.refresh_from_db()
+    assert payment_shop2shop.is_active
+
+
+@pytest.mark.parametrize(
+    "is_active, charge_status",
+    [
+        (False, ChargeStatus.NOT_CHARGED),
+        (False, ChargeStatus.PARTIALLY_CHARGED),
+        (False, ChargeStatus.FULLY_CHARGED),
+        (False, ChargeStatus.PARTIALLY_REFUNDED),
+        (False, ChargeStatus.FULLY_REFUNDED),
+        (True, ChargeStatus.PARTIALLY_CHARGED),
+        (True, ChargeStatus.FULLY_CHARGED),
+        (True, ChargeStatus.PARTIALLY_REFUNDED),
+        (True, ChargeStatus.FULLY_REFUNDED),
+    ],
+)
+def test_authorize_failed(is_active, charge_status, payment_shop2shop):
+    payment = payment_shop2shop
+    payment.is_active = is_active
+    payment.charge_status = charge_status
+    payment.save()
+    with pytest.raises(PaymentError):
+        txn = gateway.authorize(payment=payment, token="Fake")
+        assert txn is None
+
+
+def test_authorize_gateway_error(payment_shop2shop, monkeypatch):
+    monkeypatch.setattr("saleor.payment.gateways.shop2shop.shop2shop_success",
+                        lambda: False)
+    with pytest.raises(PaymentError):
+        txn = gateway.authorize(payment=payment_shop2shop, token="Fake")
+        assert txn.kind == TransactionKind.AUTH
+        assert not txn.is_success
+        assert txn.payment == payment_shop2shop
+
+
+def test_void_success(payment_txn_preauth):
+    assert payment_txn_preauth.is_active
+    assert payment_txn_preauth.charge_status == ChargeStatus.NOT_CHARGED
+    txn = gateway.void(payment=payment_txn_preauth)
+    assert txn.is_success
+    assert txn.kind == TransactionKind.VOID
+    assert txn.payment == payment_txn_preauth
+    payment_txn_preauth.refresh_from_db()
+    assert not payment_txn_preauth.is_active
+    assert payment_txn_preauth.charge_status == ChargeStatus.NOT_CHARGED
+
+
+@pytest.mark.parametrize(
+    "is_active, charge_status",
+    [
+        (False, ChargeStatus.NOT_CHARGED),
+        (False, ChargeStatus.PARTIALLY_CHARGED),
+        (False, ChargeStatus.FULLY_CHARGED),
+        (False, ChargeStatus.PARTIALLY_REFUNDED),
+        (False, ChargeStatus.FULLY_REFUNDED),
+        (True, ChargeStatus.PARTIALLY_CHARGED),
+        (True, ChargeStatus.FULLY_CHARGED),
+        (True, ChargeStatus.PARTIALLY_REFUNDED),
+        (True, ChargeStatus.FULLY_REFUNDED),
+    ],
+)
+def test_void_failed(is_active, charge_status, payment_shop2shop):
+    payment = payment_shop2shop
+    payment.is_active = is_active
+    payment.charge_status = charge_status
+    payment.save()
+    with pytest.raises(PaymentError):
+        txn = gateway.void(payment=payment)
+        assert txn is None
+
+
+def test_void_gateway_error(payment_txn_preauth, monkeypatch):
+    monkeypatch.setattr("saleor.payment.gateways.shop2shop.shop2shop_success",
+                        lambda: False)
+    with pytest.raises(PaymentError):
+        txn = gateway.void(payment=payment_txn_preauth)
+        assert txn.kind == TransactionKind.VOID
+        assert not txn.is_success
+        assert txn.payment == payment_txn_preauth
+
+
+@pytest.mark.parametrize(
+    "amount, charge_status",
+    [("98.40", ChargeStatus.FULLY_CHARGED), (70, ChargeStatus.PARTIALLY_CHARGED)],
+)
+def test_capture_success(amount, charge_status, payment_txn_preauth):
+    txn = gateway.capture(payment=payment_txn_preauth, amount=Decimal(amount))
+    assert txn.is_success
+    assert txn.payment == payment_txn_preauth
+    payment_txn_preauth.refresh_from_db()
+    assert payment_txn_preauth.charge_status == charge_status
+    assert payment_txn_preauth.is_active
+
+
+@pytest.mark.parametrize(
+    "amount, captured_amount, charge_status, is_active",
+    [
+        (80, 0, ChargeStatus.NOT_CHARGED, False),
+        (120, 0, ChargeStatus.NOT_CHARGED, True),
+        (80, 20, ChargeStatus.PARTIALLY_CHARGED, True),
+        (80, 80, ChargeStatus.FULLY_CHARGED, True),
+        (80, 0, ChargeStatus.FULLY_REFUNDED, True),
+    ],
+)
+def test_capture_failed(
+    amount, captured_amount, charge_status, is_active, payment_shop2shop
+):
+    payment = payment_shop2shop
+    payment.is_active = is_active
+    payment.captured_amount = captured_amount
+    payment.charge_status = charge_status
+    payment.save()
+    with pytest.raises(PaymentError):
+        txn = gateway.capture(payment=payment, amount=amount)
+        assert txn is None
+
+
+def test_capture_gateway_error(payment_txn_preauth, monkeypatch):
+    monkeypatch.setattr("saleor.payment.gateways.shop2shop.shop2shop_success",
+                        lambda: False)
+    with pytest.raises(PaymentError):
+        txn = gateway.capture(payment=payment_txn_preauth, amount=80)
+        assert txn.kind == TransactionKind.CAPTURE
+        assert not txn.is_success
+        assert txn.payment == payment_txn_preauth
+
+
+@pytest.mark.parametrize(
+    (
+        "initial_captured_amount, refund_amount, final_captured_amount, "
+        "final_charge_status, active_after"
+    ),
+    [
+        (80, 80, 0, ChargeStatus.FULLY_REFUNDED, False),
+        (80, 10, 70, ChargeStatus.PARTIALLY_REFUNDED, True),
+    ],
+)
+def test_refund_success(
+    initial_captured_amount,
+    refund_amount,
+    final_captured_amount,
+    final_charge_status,
+    active_after,
+    payment_txn_captured,
+):
+    payment = payment_txn_captured
+    payment.charge_status = ChargeStatus.FULLY_CHARGED
+    payment.captured_amount = initial_captured_amount
+    payment.save()
+    txn = gateway.refund(payment=payment, amount=Decimal(refund_amount))
+
+    payment.refresh_from_db()
+    assert txn.kind == TransactionKind.REFUND
+    assert txn.is_success
+    assert txn.payment == payment
+    assert payment.charge_status == final_charge_status
+    assert payment.captured_amount == final_captured_amount
+    assert payment.is_active == active_after
+
+
+@pytest.mark.parametrize(
+    "initial_captured_amount, refund_amount, initial_charge_status",
+    [
+        (0, 10, ChargeStatus.NOT_CHARGED),
+        (10, 20, ChargeStatus.PARTIALLY_CHARGED),
+        (10, 20, ChargeStatus.FULLY_CHARGED),
+        (10, 20, ChargeStatus.PARTIALLY_REFUNDED),
+        (80, 0, ChargeStatus.FULLY_REFUNDED),
+    ],
+)
+def test_refund_failed(
+    initial_captured_amount, refund_amount, initial_charge_status, payment_shop2shop
+):
+    payment = payment_shop2shop
+    payment.charge_status = initial_charge_status
+    payment.captured_amount = Decimal(initial_captured_amount)
+    payment.save()
+    with pytest.raises(PaymentError):
+        txn = gateway.refund(payment=payment, amount=Decimal(refund_amount))
+        assert txn is None
+
+
+def test_refund_gateway_error(payment_txn_captured, monkeypatch):
+    monkeypatch.setattr("saleor.payment.gateways.shop2shop.shop2shop_success",
+                        lambda: False)
+    payment = payment_txn_captured
+    payment.charge_status = ChargeStatus.FULLY_CHARGED
+    payment.captured_amount = Decimal("80.00")
+    payment.save()
+    with pytest.raises(PaymentError):
+        gateway.refund(payment=payment, amount=Decimal("80.00"))
+
+    payment.refresh_from_db()
+    txn = payment.transactions.last()
+    assert txn.kind == TransactionKind.REFUND
+    assert not txn.is_success
+    assert txn.payment == payment
+    assert payment.charge_status == ChargeStatus.FULLY_CHARGED
+    assert payment.captured_amount == Decimal("80.00")
