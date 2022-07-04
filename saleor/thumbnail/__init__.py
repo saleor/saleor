dfa88de03622ccd5271027c815d0b07168cf8a0d
IKarbowiak <iga.karbowiak@mirumee.com>
2022-07-04 08:03:23 +0200

    Drop comments from ThumbnailFormat

--- saleor/thumbnail/__init__.py
+++ saleor/thumbnail/__init__.py
@@ -5,11 +5,9 @@ THUMBNAIL_SIZES = [32, 64, 128, 256, 512, 1024, 2048, 4096]
 
 
 class ThumbnailFormat:
-    # ORIGINAL = "original"
     WEBP = "webp"
 
     CHOICES = [
-        # (ORIGINAL, "original"),
         (WEBP, "WebP"),
     ]
 
