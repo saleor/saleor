3ac9714b506329eac78678dde33a2d7f022057e7
Tomasz Szymański <lime129@gmail.com>
2022-07-04 11:28:10 +0200

    Unquote secret access when calling sqs (#10076)
    * Unquote secret access when calling sqs
    
    * Handle empty secret case

--- saleor/plugins/webhook/tasks.py
+++ saleor/plugins/webhook/tasks.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 from enum import Enum
 from json import JSONDecodeError
 from typing import TYPE_CHECKING, Any, Callable, Dict, List, Optional, Tuple, Type
-from urllib.parse import urlparse, urlunparse
+from urllib.parse import unquote, urlparse, urlunparse
 
 import boto3
 import requests
@@ -228,7 +228,9 @@ def send_webhook_using_aws_sqs(target_url, message, domain, signature, event_typ
         "sqs",
         region_name=region,
         aws_access_key_id=parts.username,
-        aws_secret_access_key=parts.password,
+        aws_secret_access_key=(
+            unquote(parts.password) if parts.password else parts.password
+        ),
     )
     queue_url = urlunparse(
         ("https", parts.hostname, parts.path, parts.params, parts.query, parts.fragment)
