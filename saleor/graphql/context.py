8b58df5a147ac96e48f567f6688709bd2cefa08a
Dominik Kozaczko <dominik@kozaczko.info>
2022-07-20 17:01:23 +0200

    Start removing context.app

--- saleor/graphql/context.py
+++ saleor/graphql/context.py
@@ -1,9 +1,9 @@
+from collections import defaultdict
 from typing import Optional, Protocol, Union, cast
 
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.models import AnonymousUser
-from django.db.models import Exists, OuterRef
 from django.utils.functional import SimpleLazyObject
 from promise import Promise
 
@@ -12,7 +12,7 @@ from ..app.models import App, AppToken
 from ..core.auth import get_token_from_request
 from ..core.jwt import jwt_decode_with_exception_handler
 from .api import API_PATH
-from .core.dataloaders import SingleObjectLoader
+from .core.dataloaders import DataLoader
 
 
 def get_context_value(request):
@@ -33,21 +33,39 @@ class RequestWithUser(Protocol):
 
 def get_app(raw_auth_token) -> Optional[App]:
     tokens = AppToken.objects.filter(token_last_4=raw_auth_token[-4:]).values_list(
-        "id", "auth_token"
+        "app_id", "auth_token"
     )
-    token_ids = [
-        id for id, auth_token in tokens if check_password(raw_auth_token, auth_token)
+    app_ids = [
+        app_id
+        for app_id, auth_token in tokens
+        if check_password(raw_auth_token, auth_token)
     ]
-    return App.objects.filter(
-        Exists(tokens.filter(id__in=token_ids, app_id=OuterRef("pk"))), is_active=True
-    ).first()
+    return App.objects.filter(id__in=app_ids, is_active=True).first()
 
 
-class AppByTokenLoader(SingleObjectLoader):
+class AppByTokenLoader(DataLoader):
     context_key = "app_by_token"
 
     def batch_load(self, keys):
-        return [get_app(list(keys)[0])]
+        last_4s_map = defaultdict(list)
+        for key in keys:
+            last_4s_map[key[-4:]].append(key)
+
+        tokens = (
+            AppToken.objects.using(self.database_connection_name)
+            .filter(token_last_4__in=last_4s_map.keys())
+            .values_list("auth_token", "token_last_4", "app_id")
+        )
+        app_ids = set()
+        for auth_token, token_last_4, app_id in tokens:
+            for raw_token in last_4s_map[token_last_4]:
+                if check_password(raw_token, auth_token):
+                    app_ids.add(app_id)
+        apps = App.objects.using(self.database_connection_name).filter(
+            id__in=app_ids, is_active=True
+        )
+
+        return list(apps)
 
 
 def set_decoded_auth_token(request):
@@ -61,7 +79,7 @@ def set_app_on_context(request):
         request.app = None
         auth_token = get_token_from_request(request)
         if auth_token and len(auth_token) == 30:
-            request.app = AppByTokenLoader(request).load(auth_token)
+            request.app = SimpleLazyObject(lambda: get_app(auth_token))
 
 
 def get_user(request: RequestWithUser) -> Optional[UserType]:
