d10b015073fdbb193944cde6a9437e95cedf81a3
Krzysztof Kwasniak <mr.brzys@gmail.com>
2022-07-28 14:56:44 +0200

    Add log for AttributeValue.DoesNotExist case

--- saleor/attribute/tasks.py
+++ saleor/attribute/tasks.py
@@ -14,6 +14,13 @@ def update_associated_products_search_vector(attribute_value_pk: int):
     try:
         instance = AttributeValue.objects.get(pk=attribute_value_pk)
     except AttributeValue.DoesNotExist:
+        task_logger.warning(
+            (
+                "Could not perform search_vector update on associated products as "
+                'AttributeValue with pk "%s" does not exists.'
+            ),
+            attribute_value_pk,
+        )
         return
 
     variants = ProductVariant.objects.filter(
