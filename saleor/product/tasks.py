fd3a8758f5e0702d8258fd6d589ca05bb1aaa5ed
SzymJ <31098825+SzymJ@users.noreply.github.com>
2022-08-01 17:16:57 +0200

    Add expires time to task

--- saleor/product/tasks.py
+++ saleor/product/tasks.py
@@ -112,7 +112,7 @@ def _get_preorder_variants_to_clean():
     )
 
 
-@app.task(name=settings.UPDATE_SEARCH_VECTOR_INDEX_QUEUE_NAME)
+@app.task(queue=settings.UPDATE_SEARCH_VECTOR_INDEX_QUEUE_NAME, expires=20)
 def update_products_search_vector_task():
     products = Product.objects.filter(search_index_dirty=True)[:PRODUCTS_BATCH_SIZE]
     update_products_search_vector(products, use_batches=False)
