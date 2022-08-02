76f2bc810cd6f661a80f659d4629d793389083ca
Cezary Miącz <czarek.miacz@gmail.com>
2022-08-02 13:17:37 +0200

    Fix thumbnail deletion signal (#10296)
    * Fix thumbnail deletion signal
    
    * Fix signals in product and account apps
    
    * Fix test_digital_content.py

--- saleor/product/signals.py
+++ saleor/product/signals.py
@@ -3,17 +3,17 @@ from ..core.tasks import delete_from_storage_task, delete_product_media_task
 
 def delete_background_image(sender, instance, **kwargs):
     if img := instance.background_image:
-        delete_from_storage_task.delay(img.path)
+        delete_from_storage_task.delay(img.name)
 
 
 def delete_digital_content_file(sender, instance, **kwargs):
     if file := instance.content_file:
-        delete_from_storage_task.delay(file.path)
+        delete_from_storage_task.delay(file.name)
 
 
 def delete_product_media_image(sender, instance, **kwargs):
     if file := instance.image:
-        delete_from_storage_task.delay(file.path)
+        delete_from_storage_task.delay(file.name)
 
 
 def delete_product_all_media(sender, instance, **kwargs):
