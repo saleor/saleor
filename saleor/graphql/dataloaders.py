8b1d8e3a8bd2c2b8d45ba8a11b2a48d6e48089cb
Dominik Kozaczko <dominik@kozaczko.info>
2022-08-03 10:48:38 +0200

    Fix AppByTokenDataLoader to return properly prepared result.

--- saleor/graphql/dataloaders.py
+++ saleor/graphql/dataloaders.py
@@ -24,21 +24,22 @@ class AppByTokenLoader(DataLoader):
 
     def batch_load(self, keys):
         last_4s_map = defaultdict(list)
-        for key in keys:
-            last_4s_map[key[-4:]].append(key)
+        for raw_token in keys:
+            last_4s_map[raw_token[-4:]].append(raw_token)
 
         tokens = (
             AppToken.objects.using(self.database_connection_name)
             .filter(token_last_4__in=last_4s_map.keys())
             .values_list("auth_token", "token_last_4", "app_id")
         )
-        app_ids = set()
+        authed_apps = {}
         for auth_token, token_last_4, app_id in tokens:
             for raw_token in last_4s_map[token_last_4]:
                 if check_password(raw_token, auth_token):
-                    app_ids.add(app_id)
+                    authed_apps[raw_token] = app_id
+
         apps = App.objects.using(self.database_connection_name).filter(
-            id__in=app_ids, is_active=True
+            id__in=authed_apps.values(), is_active=True
         )
 
-        return list(apps)
+        return [apps.filter(id=authed_apps.get(key, None)).first() for key in keys]
