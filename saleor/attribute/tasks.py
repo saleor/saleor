f563b0c7118bf4978a0973170f0911a678d79069
SzymJ <31098825+SzymJ@users.noreply.github.com>
2022-08-03 10:03:22 +0200

    Move product search vector indexing to scheduled task (#10295)
    * Move product search vector indexing to scheduled task
    
    * Add operation on revers migration
    
    * update_associated_products_search_vector task update

--- saleor/attribute/tasks.py
+++ saleor/attribute/tasks.py
@@ -4,7 +4,6 @@ from django.db.models import Exists, OuterRef, Q
 from ..attribute.models import AttributeValue
 from ..celeryconf import app
 from ..product.models import Product, ProductVariant
-from ..product.search import update_products_search_vector
 
 task_logger = get_task_logger(__name__)
 
@@ -26,8 +25,7 @@ def update_associated_products_search_vector(attribute_value_pk: int):
     variants = ProductVariant.objects.filter(
         Exists(instance.variantassignments.filter(variant_id=OuterRef("id")))
     )
-    products = Product.objects.filter(
+    Product.objects.filter(
         Q(Exists(instance.productassignments.filter(product_id=OuterRef("id"))))
         | Q(Exists(variants.filter(product_id=OuterRef("id"))))
-    )
-    update_products_search_vector(products)
+    ).update(search_index_dirty=True)
