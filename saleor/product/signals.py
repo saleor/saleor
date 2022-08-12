012472eb848269f873e8dbf94f05e1d6652261f6
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2022-08-12 12:41:39 +0200

    Drop handling product media deletion in celery task (#10193)
    * Drop handling product media deletion in celery task
    
    * Do not remove to_remove field from ProductMedia
    
    * Ensure migration backwards compatibility in ProductMedia
    
    * Restore delete_product_media_task - clear the task body
    
    * Fix failing test

--- saleor/product/signals.py
+++ saleor/product/signals.py
@@ -1,4 +1,4 @@
-from ..core.tasks import delete_from_storage_task, delete_product_media_task
+from ..core.tasks import delete_from_storage_task
 
 
 def delete_background_image(sender, instance, **kwargs):
@@ -13,11 +13,4 @@ def delete_digital_content_file(sender, instance, **kwargs):
 
 def delete_product_media_image(sender, instance, **kwargs):
     if file := instance.image:
-        delete_from_storage_task.delay(file.name)
-
-
-def delete_product_all_media(sender, instance, **kwargs):
-    if all_media := instance.media.all():
-        for media in all_media:
-            media.set_to_remove()
-            delete_product_media_task.delay(media.id)
+        delete_from_storage_task.delay(file.path)
