ef8d4f2f98772b13a34ac5bbee04bb83782a3565
Filip Owczarek <filip.owczarek@saleor.io>
2022-09-07 16:21:10 +0200

    Fix base order total recalculation (#10577)

--- saleor/order/base_calculations.py
+++ saleor/order/base_calculations.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, Iterable
 from prices import Money, TaxedMoney
 
 from ..core.taxes import zero_money
+from ..discount import DiscountValueType, OrderDiscountType
 from ..discount.utils import apply_discount_to_value
 from .interface import OrderTaxedPricesData
 
@@ -21,29 +22,67 @@ def base_order_shipping(order: "Order") -> Money:
     return channel_listing.price
 
 
-def base_order_total(order: "Order", lines: Iterable["OrderLine"]) -> Money:
-    currency = order.currency
-    total = base_order_total_without_order_discount(order, lines)
-    order_discounts = order.discounts.all()
-    for order_discount in order_discounts:
-        total = apply_discount_to_value(
-            value=order_discount.value,
-            value_type=order_discount.value_type,
-            currency=currency,
-            price_to_discount=total,
-        )
-    return max(total, zero_money(currency))
-
-
-def base_order_total_without_order_discount(
-    order: "Order", lines: Iterable["OrderLine"]
-) -> Money:
+def _base_order_subtotal(order: "Order", lines: Iterable["OrderLine"]) -> Money:
     currency = order.currency
     subtotal = zero_money(currency)
     for line in lines:
         quantity = line.quantity
         price_with_discounts = line.base_unit_price * quantity
         subtotal += price_with_discounts
+    return subtotal
+
+
+def base_order_total(order: "Order", lines: Iterable["OrderLine"]) -> Money:
+    currency = order.currency
+    subtotal = _base_order_subtotal(order, lines)
+    shipping_price = base_order_shipping(order)
+    order_discounts = order.discounts.all()
+    for order_discount in order_discounts:
+        if order_discount.type == OrderDiscountType.VOUCHER:
+            subtotal = apply_discount_to_value(
+                value=order_discount.value,
+                value_type=order_discount.value_type,
+                currency=currency,
+                price_to_discount=subtotal,
+            )
+        else:
+            if order_discount.value_type == DiscountValueType.PERCENTAGE:
+                subtotal = apply_discount_to_value(
+                    value=order_discount.value,
+                    value_type=order_discount.value_type,
+                    currency=currency,
+                    price_to_discount=subtotal,
+                )
+                shipping_price = apply_discount_to_value(
+                    value=order_discount.value,
+                    value_type=order_discount.value_type,
+                    currency=currency,
+                    price_to_discount=shipping_price,
+                )
+            else:
+                temporary_undiscounted_total = subtotal + shipping_price
+                temporary_total = apply_discount_to_value(
+                    value=order_discount.value,
+                    value_type=order_discount.value_type,
+                    currency=currency,
+                    price_to_discount=temporary_undiscounted_total,
+                )
+                total_discount = temporary_undiscounted_total - temporary_total
+                subtotal_discount = (
+                    subtotal / temporary_undiscounted_total
+                ) * total_discount
+                shipping_discount = total_discount - subtotal_discount
+
+                subtotal -= subtotal_discount
+                shipping_price -= shipping_discount
+
+    return max(subtotal + shipping_price, zero_money(currency))
+
+
+def base_order_total_without_order_discount(
+    order: "Order", lines: Iterable["OrderLine"]
+) -> Money:
+    subtotal = _base_order_subtotal(order, lines)
     base_shipping_price = base_order_shipping(order)
     return subtotal + base_shipping_price
 
