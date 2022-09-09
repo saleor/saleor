3872ce6645404971a107d114de3bbd61d65664a2
Filip Owczarek <filip.owczarek@saleor.io>
2022-09-09 16:08:43 +0200

    Fix base order total calculation with voucher  (#10604)
    * Fix order total calculations with voucher
    
    * Fix base order total recalculation
    
    * Increase test coverage of order base calculations
    
    * Move order discount recalculation to base order total calculation

--- saleor/order/base_calculations.py
+++ saleor/order/base_calculations.py
@@ -3,7 +3,8 @@ from typing import TYPE_CHECKING, Iterable
 from prices import Money, TaxedMoney
 
 from ..core.taxes import zero_money
-from ..discount import OrderDiscountType
+from ..discount import DiscountValueType, OrderDiscountType
+from ..discount.models import OrderDiscount
 from ..discount.utils import apply_discount_to_value
 from .interface import OrderTaxedPricesData
 
@@ -22,31 +23,81 @@ def base_order_shipping(order: "Order") -> Money:
     return channel_listing.price
 
 
-def base_order_total(order: "Order", lines: Iterable["OrderLine"]) -> Money:
-    currency = order.currency
-    total = base_order_total_without_order_discount(order, lines)
-    order_discount = order.discounts.filter(type=OrderDiscountType.MANUAL).first()
-    if order_discount:
-        total = apply_discount_to_value(
-            value=order_discount.value,
-            value_type=order_discount.value_type,
-            currency=currency,
-            price_to_discount=total,
-        )
-    return max(total, zero_money(currency))
-
-
-def base_order_total_without_order_discount(
-    order: "Order", lines: Iterable["OrderLine"]
-) -> Money:
+def _base_order_subtotal(order: "Order", lines: Iterable["OrderLine"]) -> Money:
     currency = order.currency
     subtotal = zero_money(currency)
     for line in lines:
         quantity = line.quantity
         price_with_discounts = line.base_unit_price * quantity
         subtotal += price_with_discounts
-    base_shipping_price = base_order_shipping(order)
-    return subtotal + base_shipping_price
+    return subtotal
+
+
+def base_order_total(order: "Order", lines: Iterable["OrderLine"]) -> Money:
+    """Return order total, recalculate, and update order discounts.
+
+    This function returns the order total. All discounts are included in this price.
+    Shipping vouchers are included in the shipping price.
+    Specific product vouchers are included in line base prices.
+    Entire order vouchers are recalculated and updated in this function
+    (OrderDiscounts with type `order_discount.type == OrderDiscountType.VOUCHER`).
+    Staff order discounts are recalculated and updated in this function
+    (OrderDiscounts with type `order_discount.type == OrderDiscountType.MANUAL`).
+    """
+    currency = order.currency
+    subtotal = _base_order_subtotal(order, lines)
+    shipping_price = base_order_shipping(order)
+    order_discounts = order.discounts.all()
+    order_discounts_to_update = []
+    for order_discount in order_discounts:
+        subtotal_before_discount = subtotal
+        shipping_price_before_discount = shipping_price
+        if order_discount.type == OrderDiscountType.VOUCHER:
+            subtotal = apply_discount_to_value(
+                value=order_discount.value,
+                value_type=order_discount.value_type,
+                currency=currency,
+                price_to_discount=subtotal,
+            )
+        elif order_discount.value_type == DiscountValueType.PERCENTAGE:
+            subtotal = apply_discount_to_value(
+                value=order_discount.value,
+                value_type=order_discount.value_type,
+                currency=currency,
+                price_to_discount=subtotal,
+            )
+            shipping_price = apply_discount_to_value(
+                value=order_discount.value,
+                value_type=order_discount.value_type,
+                currency=currency,
+                price_to_discount=shipping_price,
+            )
+        else:
+            temporary_undiscounted_total = subtotal + shipping_price
+            if temporary_undiscounted_total.amount > 0:
+                temporary_total = apply_discount_to_value(
+                    value=order_discount.value,
+                    value_type=order_discount.value_type,
+                    currency=currency,
+                    price_to_discount=temporary_undiscounted_total,
+                )
+                total_discount = temporary_undiscounted_total - temporary_total
+                subtotal_discount = (
+                    subtotal / temporary_undiscounted_total
+                ) * total_discount
+                shipping_discount = total_discount - subtotal_discount
+
+                subtotal -= subtotal_discount
+                shipping_price -= shipping_discount
+        shipping_discount_amount = shipping_price_before_discount - shipping_price
+        subtotal_discount_amount = subtotal_before_discount - subtotal
+        total_discount_amount = shipping_discount_amount + subtotal_discount_amount
+        if order_discount.amount != total_discount_amount:
+            order_discount.amount = total_discount_amount
+            order_discounts_to_update.append(order_discount)
+    if order_discounts_to_update:
+        OrderDiscount.objects.bulk_update(order_discounts_to_update, ["amount_value"])
+    return max(subtotal + shipping_price, zero_money(currency))
 
 
 def base_order_line_total(order_line: "OrderLine") -> OrderTaxedPricesData:
