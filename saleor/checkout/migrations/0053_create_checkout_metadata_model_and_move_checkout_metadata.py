c545a0c98895d40ad3a0b4bc2ef09b6d34634b49
jakubkuc <jakub.kuc.87@gmail.com>
2022-09-26 18:28:28 +0200

    fix migration

--- saleor/checkout/migrations/0053_create_checkout_metadata_model_and_move_checkout_metadata.py
+++ saleor/checkout/migrations/0053_create_checkout_metadata_model_and_move_checkout_metadata.py
@@ -32,8 +32,12 @@ def queryset_in_batches(queryset):
 def move_all_checkout_metadata(apps, schema_editor):
     Checkout = apps.get_model("checkout", "Checkout")
     CheckoutMetadata = apps.get_model("checkout", "CheckoutMetadata")
-    checkouts_with_meta = Checkout.objects.filter(
-        Q(metadata__isnull=False) | Q(private_metadata__isnull=False)
+    checkouts_with_meta = (
+        Checkout.objects.filter(
+            Q(metadata__isnull=False) | Q(private_metadata__isnull=False)
+        )
+        .order_by("pk")
+        .distinct("token")
     )
     for batch_pks in queryset_in_batches(checkouts_with_meta):
         checkouts = Checkout.objects.filter(pk__in=batch_pks)
