115fa229a1a9b98a551730ec3a9b60ec2654bccd
IKarbowiak <iga.karbowiak@mirumee.com>
2022-10-04 16:05:24 +0200

    Attach saleor-logo to admin emails

--- saleor/core/notification/utils.py
+++ saleor/core/notification/utils.py
@@ -1,4 +1,7 @@
 from django.contrib.sites.models import Site
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+from ..utils import build_absolute_uri
 
 
 def get_site_context():
@@ -6,5 +9,8 @@ def get_site_context():
     site_context = {
         "domain": site.domain,
         "site_name": site.name,
+        "logo_url": build_absolute_uri(
+            staticfiles_storage.url("images/saleor-logo.png")
+        ),
     }
     return site_context
