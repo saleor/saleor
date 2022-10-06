b8bdeb0ce90608ea5a629ce684d15cf2271e9347
jakubkuc <jakub.kuc.87@gmail.com>
2022-10-06 16:28:51 +0200

    add proper fix for get or create and add test

--- saleor/graphql/attribute/utils.py
+++ saleor/graphql/attribute/utils.py
@@ -143,20 +143,23 @@ class AttributeAssignmentMixin:
     def _pre_save_values(
         cls, attribute: attribute_models.Attribute, attr_values: AttrValuesInput
     ):
-        """Lazy-update or create the database objects from the supplied raw values."""
-        update_or_create = attribute.values.update_or_create
-
+        """Lazy-retrieve or create the database objects from the supplied raw values."""
+        result = []
         if not attr_values.values:
             return tuple()
-
-        return tuple(
-            update_or_create(
-                attribute=attribute,
-                slug=slugify(value, allow_unicode=True),
-                defaults={"name": value},
-            )[0]
-            for value in attr_values.values
-        )
+        for value in attr_values.values:
+            value_obj = attribute.values.filter(name=value).first()
+            if value_obj:
+                result.append(value_obj)
+            else:
+                instance = attribute_models.AttributeValue(
+                    attribute=attribute, name=value
+                )
+                slug = generate_unique_slug(instance, value)  # type: ignore
+                instance.slug = slug
+                instance.save()
+                result.append(instance)
+        return tuple(result)
 
     @classmethod
     def _pre_save_numeric_values(
