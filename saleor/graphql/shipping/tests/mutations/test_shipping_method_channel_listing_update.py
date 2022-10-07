074b156fdea6351fd9c5654722d4dd42d02dd481
SzymJ <31098825+SzymJ@users.noreply.github.com>
2022-10-07 13:06:42 +0200

    Fix ShippingMethodChannelListingUpdate minimum_order_price resolver

--- saleor/graphql/shipping/tests/mutations/test_shipping_method_channel_listing_update.py
+++ saleor/graphql/shipping/tests/mutations/test_shipping_method_channel_listing_update.py
@@ -146,6 +146,7 @@ def test_shipping_method_channel_listing_update_allow_to_set_null_for_limit_fiel
     )
     content = get_graphql_content(response)
     channel_listing.refresh_from_db()
+
     # then
     data = content["data"]["shippingMethodChannelListingUpdate"]
     shipping_method_data = data["shippingMethod"]
@@ -154,9 +155,7 @@ def test_shipping_method_channel_listing_update_allow_to_set_null_for_limit_fiel
     assert channel_listing.maximum_order_price_amount is None
     assert channel_listing.minimum_order_price_amount is None
     assert shipping_method_data["channelListings"][0]["maximumOrderPrice"] is None
-    assert (
-        shipping_method_data["channelListings"][0]["minimumOrderPrice"]["amount"] == 0
-    )
+    assert shipping_method_data["channelListings"][0]["minimumOrderPrice"] is None
 
 
 @patch("saleor.plugins.webhook.plugin._get_webhooks_for_event")
