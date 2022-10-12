52f181037073bf690f98061d2a85a5fb15f9aa07
IKarbowiak <iga.karbowiak@mirumee.com>
2022-10-12 10:24:14 +0200

    Add method for getting default storage root URL

--- saleor/core/tests/test_url.py
+++ saleor/core/tests/test_url.py
@@ -1,6 +1,8 @@
 from urllib.parse import urlencode
 
-from ..utils.url import prepare_url
+from django.conf import settings
+
+from ..utils.url import get_default_storage_root_url, prepare_url
 
 
 def test_prepare_url():
@@ -8,3 +10,11 @@ def test_prepare_url():
     params = urlencode({"param1": "abc", "param2": "xyz"})
     result = prepare_url(params, redirect_url)
     assert result == f"{redirect_url}?param1=abc&param2=xyz"
+
+
+def test_get_default_storage_root_url(site_settings):
+    # when
+    root_url = get_default_storage_root_url()
+
+    # then
+    assert root_url == f"http://{site_settings.site.domain}{settings.MEDIA_URL}"
