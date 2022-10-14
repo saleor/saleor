2e0f06ddb5c3107c44d64ff201d7f11a248bf184
Byron Rode <byronrode@gmail.com>
2022-10-14 09:55:12 +0200

    Remove default_country from migration (3.0) to test build

--- saleor/channel/migrations/0001_initial.py
+++ saleor/channel/migrations/0001_initial.py
@@ -61,8 +61,7 @@ def create_default_channel(apps, schema_editor):
             name="Default channel",
             slug=settings.DEFAULT_CHANNEL_SLUG,
             currency_code=default_currency,
-            is_active=True,
-            default_country=default_country,
+            is_active=True
         )
 
 
