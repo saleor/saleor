0ef7a7baab261c845b745a6f846aab054bb82a85
Marcin Gębala <maarcin.gebala@gmail.com>
2022-10-25 16:01:01 +0200

    Improve tests

--- saleor/graphql/tax/tests/mutations/test_tax_class_update.py
+++ saleor/graphql/tax/tests/mutations/test_tax_class_update.py
@@ -1,7 +1,7 @@
 import graphene
 
 from .....tax.error_codes import TaxClassUpdateErrorCode
-from .....tax.models import TaxClass
+from .....tax.models import TaxClass, TaxClassCountryRate
 from ....tests.utils import assert_no_permission, get_graphql_content
 from ..fragments import TAX_CLASS_FRAGMENT
 
@@ -145,6 +145,9 @@ def test_remove_all_country_rates(staff_api_client, permission_manage_taxes):
 
 def test_remove_individual_country_rates(staff_api_client, permission_manage_taxes):
     # given
+    default_rate_de, _ = TaxClassCountryRate.objects.get_or_create(
+        country="DE", tax_class=None, rate=19
+    )
     tax_class = TaxClass.objects.create(name="Tax Class")
     tax_class.country_rates.create(country="PL", rate=21)
     tax_class.country_rates.create(country="DE", rate=19)
@@ -171,3 +174,44 @@ def test_remove_individual_country_rates(staff_api_client, permission_manage_tax
     assert not data["errors"]
     assert len(data["taxClass"]["countries"]) == 1
     assert data["taxClass"]["countries"][0]["country"]["code"] == "PL"
+    assert data["taxClass"]["countries"][0]["rate"] == new_rate_pl
+
+    default_rate_de.refresh_from_db()
+    assert default_rate_de.pk is not None
+
+
+def test_remove_individual_country_rates_non_existing_rate(
+    staff_api_client, permission_manage_taxes
+):
+    # given
+    default_rate_de, _ = TaxClassCountryRate.objects.get_or_create(
+        country="DE", tax_class=None, rate=19
+    )
+    tax_class = TaxClass.objects.create(name="Tax Class")
+    tax_class.country_rates.create(country="PL", rate=21)
+    id = graphene.Node.to_global_id("TaxClass", tax_class.pk)
+    new_rate_pl = 23
+    variables = {
+        "id": id,
+        "input": {
+            "updateCountryRates": [
+                {"countryCode": "PL", "rate": new_rate_pl},
+                {"countryCode": "DE"},
+            ],
+        },
+    }
+
+    # when
+    response = staff_api_client.post_graphql(
+        MUTATION, variables, permissions=[permission_manage_taxes]
+    )
+
+    # then
+    content = get_graphql_content(response)
+    data = content["data"]["taxClassUpdate"]
+    assert not data["errors"]
+    assert len(data["taxClass"]["countries"]) == 1
+    assert data["taxClass"]["countries"][0]["country"]["code"] == "PL"
+
+    default_rate_de.refresh_from_db()
+    assert default_rate_de.pk is not None
