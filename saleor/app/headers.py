be9bf33f5d85e41b1740d38ac36c8f2adcdbbb91
Przemysław Łada <przlada@gmail.com>
2022-11-15 09:28:54 +0100

    Add App headers consts

---
+++ saleor/app/headers.py
@@ -0,0 +1,10 @@
+class AppHeaders:
+    DOMAIN = "Saleor-Domain"
+    EVENT_TYPE = "Saleor-Event"
+    SIGNATURE = "Saleor-Signature"
+
+
+class DeprecatedAppHeaders:
+    DOMAIN = "X-Saleor-Domain"
+    EVENT_TYPE = "X-Saleor-Event"
+    SIGNATURE = "X-Saleor-Signature"
