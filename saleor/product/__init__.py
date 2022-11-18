d975eed3cc5d35d0711da63aec8d63656ff31f47
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2022-11-18 10:38:41 +0100

    Extend collection model with rules related fields (#11228)
    * Extend Collection model with type and should_refresh fields
    
    * Add CollectionRule and RuleValue models

--- saleor/product/__init__.py
+++ saleor/product/__init__.py
@@ -19,3 +19,26 @@ class ProductTypeKind:
         (NORMAL, "A standard product type."),
         (GIFT_CARD, "A gift card product type."),
     ]
+
+
+class CollectionType:
+    STATIC = "static"
+    DYNAMIC = "dynamic"
+
+    CHOICES = [
+        (STATIC, "Static"),
+        (DYNAMIC, "Dynamic"),
+    ]
+
+
+class RuleOperator:
+    IS = "is"
+    IS_NOT = "is_not"
+
+    CHOICES = [(IS, "Is"), (IS_NOT, "Is not")]
+
+
+class RuleQualifier:
+    ATTRIBUTE = "attribute"
+
+    CHOICES = [(ATTRIBUTE, "Attribute")]
