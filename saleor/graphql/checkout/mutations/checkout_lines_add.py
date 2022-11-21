efdfea99ad024e76d9c7f4163090d3426ccdf90f
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2022-11-21 12:34:19 +0100

    Reverse api changes for checkoutLinesAdd.lines and PageFilterInput.ids (#11254)

--- saleor/graphql/checkout/mutations/checkout_lines_add.py
+++ saleor/graphql/checkout/mutations/checkout_lines_add.py
@@ -11,7 +11,7 @@ from ....warehouse.reservations import get_reservation_length, is_reservation_en
 from ...core.descriptions import DEPRECATED_IN_3X_INPUT
 from ...core.mutations import BaseMutation
 from ...core.scalars import UUID
-from ...core.types import CheckoutError, NonNullList
+from ...core.types import CheckoutError
 from ...core.validators import (
     validate_one_of_args_is_in_mutation,
     validate_variants_available_in_channel,
@@ -41,7 +41,7 @@ class CheckoutLinesAdd(BaseMutation):
             required=False,
         )
         token = UUID(description="Checkout token.", required=False)
-        lines = NonNullList(
+        lines = graphene.List(
             CheckoutLineInput,
             required=True,
             description=(
