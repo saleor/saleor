a925f62e5065af4e677ec6ce8ee3fe72365ab623
Maciej Korycinski <korycinski.maciej@gmail.com>
2022-12-01 15:38:46 +0100

    Fix failing save on stripe plugin

--- saleor/plugins/base_plugin.py
+++ saleor/plugins/base_plugin.py
@@ -92,6 +92,7 @@ class BasePlugin:
     PLUGIN_ID = ""
     PLUGIN_DESCRIPTION = ""
     CONFIG_STRUCTURE = None
+
     CONFIGURATION_PER_CHANNEL = True
     DEFAULT_CONFIGURATION = []
     DEFAULT_ACTIVE = False
@@ -677,12 +678,12 @@ class BasePlugin:
 
         cls.validate_plugin_configuration(plugin_configuration)
         cls.pre_save_plugin_configuration(plugin_configuration)
+        plugin_configuration.save()
 
         if plugin_configuration.configuration:
             # Let's add a translated descriptions and labels
             cls._append_config_structure(plugin_configuration.configuration)
 
-        plugin_configuration.save()
         return plugin_configuration
 
     @classmethod
