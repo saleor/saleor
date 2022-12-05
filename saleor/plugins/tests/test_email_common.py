d375887d5246ca2daf370b8e0633ba5643940ed9
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2022-12-05 11:51:41 +0100

    Fix invalid get_product_image_thumbnail email common method (#11387)

--- saleor/plugins/tests/test_email_common.py
+++ saleor/plugins/tests/test_email_common.py
@@ -3,11 +3,14 @@ from unittest.mock import patch
 import pytest
 from django.core.exceptions import ValidationError
 
-from saleor.plugins.email_common import (
+from saleor.plugins.error_codes import PluginErrorCode
+
+from ...order.notifications import get_image_payload
+from ..email_common import (
     DEFAULT_EMAIL_CONFIGURATION,
+    get_product_image_thumbnail,
     validate_default_email_configuration,
 )
-from saleor.plugins.error_codes import PluginErrorCode
 
 
 @pytest.mark.parametrize(
@@ -59,3 +62,14 @@ def test_validate_default_email_configuration_backend_raises(
             " Make sure that you provided correct values."
             " [Errno 61] Connection refused"
         )
+
+
+def test_get_product_image_thumbnail(product_with_image):
+    # given
+    image_data = {"original": get_image_payload(product_with_image.media.first())}
+
+    # when
+    thumbnail = get_product_image_thumbnail(None, 100, image_data)
+
+    # then
+    assert thumbnail == image_data["original"][128]
