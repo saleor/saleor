a1393915e4d2ffee908ac3116a4f228462e9d4c7
Przemysław Łada <przlada@gmail.com>
2022-12-15 17:10:07 +0100

    Add order_id to Invoice object type

--- saleor/graphql/invoice/types.py
+++ saleor/graphql/invoice/types.py
@@ -12,8 +12,14 @@ class Invoice(ModelObjectType):
     updated_at = graphene.DateTime(required=True)
     message = graphene.String()
     url = graphene.String(description="URL to download an invoice.")
+    order_id = graphene.ID()
 
     class Meta:
         description = "Represents an Invoice."
         interfaces = [ObjectWithMetadata, Job, graphene.relay.Node]
         model = models.Invoice
+
+    @staticmethod
+    def resolve_order_id(root: models.Invoice, info):
+        order_id = root.order_id
+        return graphene.Node.to_global_id("Order", order_id) if order_id else None
