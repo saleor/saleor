42ac5076a4f9a8b47c09d4ca6a8f6928e0de0856
Cezary Miącz <cezary.miacz@saleor.io>
2022-12-16 18:29:59 +0100

    Use raw_delete

--- saleor/core/management/commands/clearorders.py
+++ saleor/core/management/commands/clearorders.py
@@ -6,18 +6,23 @@ configuration, such as: warehouses, shipping zones, staff accounts, plugin confi
 """
 
 from django.core.management.base import BaseCommand
-from django.db.models import Q
 
 from ....account.models import User
-from ....checkout.models import Checkout
-from ....order.models import Order
+from ....checkout.models import Checkout, CheckoutLine, CheckoutMetadata
+from ....giftcard.models import GiftCard, GiftCardEvent, GiftCardTag
+from ....order.models import Fulfillment, FulfillmentLine, Order, OrderEvent, OrderLine
 from ....payment.models import Payment, Transaction
-
+from ....warehouse.models import Allocation
 
 class Command(BaseCommand):
     help = "Removes transactions data preserving shop's catalog and configuration."
 
     def add_arguments(self, parser):
+        parser.add_argument(
+            "--delete-gift-cards",
+            action="store_true",
+            help="Delete cutomers gift cards.",
+        )
         parser.add_argument(
             "--delete-customers",
             action="store_true",
@@ -25,21 +30,75 @@ class Command(BaseCommand):
         )
 
     def handle(self, **options):
-        Checkout.objects.all().delete()
+        self.delete_checkouts()
+        self.delete_payments()
+
+        should_delete_gift_cards = options.get("delete_gift_cards")
+        should_delete_customers = options.get("delete_customers")
+
+        if should_delete_gift_cards:
+            self.delete_gift_cards()
+        else:
+            self.clear_gift_cards()
+
+        self.delete_orders()
+
+        if should_delete_customers:
+            self.delete_customers()
+
+
+    def delete_checkouts(self):
+        metadata = CheckoutMetadata.objects.all()
+        metadata._raw_delete(metadata.db)
+    
+        checkout_lines = CheckoutLine.objects.all()
+        checkout_lines._raw_delete(checkout_lines.db)
+
+        checkout = Checkout.objects.all()
+        checkout._raw_delete(checkout.db)
         self.stdout.write("Removed checkouts")
 
-        Transaction.objects.all().delete()
+    def delete_payments(self):
+        transaction = Transaction.objects.all()
+        transaction._raw_delete(transaction.db)
         self.stdout.write("Removed transactions")
 
-        Payment.objects.all().delete()
+        payments = Payment.objects.all()
+        payments._raw_delete(payments.db)
         self.stdout.write("Removed payments")
 
-        Order.objects.all().delete()
+    def delete_gift_cards(self):
+        GiftCard.objects.all().delete()
+        GiftCardTag.objects.all().delete()
+        self.stdout.write("Removed gift cards")
+
+    def clear_gift_cards(self):
+        GiftCard.objects.all().update(fulfillment_line=None)
+        GiftCardEvent.objects.all().update(order=None)
+        
+    def delete_orders(self):
+        fulfillment_lines = FulfillmentLine.objects.all()
+        fulfillment_lines._raw_delete(fulfillment_lines.db)
+
+        fulfillments = Fulfillment.objects.all()
+        fulfillments._raw_delete(fulfillments.db)
+
+        allocations = Allocation.objects.all()
+        allocations._raw_delete(allocations.db)
+
+        order_lines = OrderLine.objects.all()
+        order_lines._raw_delete(order_lines.db)
+
+        order_events = OrderEvent.objects.all()
+        order_events._raw_delete(order_events.db)
+
+        orders = Order.objects.all()
+        orders._raw_delete(orders.db)
         self.stdout.write("Removed orders")
 
-        should_delete_customers = options.get("delete_customers")
-        if should_delete_customers:
-            # Delete all users except for staff members.
-            staff = User.objects.filter(Q(is_staff=True) | Q(is_superuser=True))
-            User.objects.exclude(pk__in=staff).delete()
-            self.stdout.write("Removed customers")
+    def delete_customers(self):
+        customers = User.objects.filter(is_staff=False, is_superuser=False)
+        for user in customers:
+            user.addresses.all().delete()
+            user.delete()
+        self.stdout.write("Removed customers")
