454dda90782c32350014123f75647ac9519d2209
Marcin Gębala <maarcin.gebala@gmail.com>
2022-12-21 14:36:56 +0100

    Add migrations to improve initial DB state with default objects

--- saleor/warehouse/migrations/0031_create_default_warehouse.py
+++ saleor/warehouse/migrations/0031_create_default_warehouse.py
@@ -5,13 +5,18 @@ from django.conf import settings
 def create_default_warehouse(apps, schema_editor):
     if settings.POPULATE_DEFAULTS:
         address = apps.get_model("account", "Address").objects.create(country="US")
+        Channel = apps.get_model("channel", "Channel")
         Warehouse = apps.get_model("warehouse", "Warehouse")
         if not Warehouse.objects.all().exists():
-            Warehouse.objects.create(
+            warehouse = Warehouse.objects.create(
                 address=address,
                 name="Default Warehouse",
                 slug="default-warehouse",
             )
+
+            channel = Channel.objects.filter(slug="default-channel").first()
+            if channel:
+                warehouse.channels.add(channel)
 
 
 class Migration(migrations.Migration):
