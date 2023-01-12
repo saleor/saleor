48dc7d9df33077fa48ea149a5f7d0fcf442b4bd9
jakubkuc <jakub.kuc.87@gmail.com>
2023-01-12 11:38:08 +0100

    fix out od stock and back in stock events triggering, add tests

--- saleor/graphql/product/tests/mutations/test_product_variant_stocks_update.py
+++ saleor/graphql/product/tests/mutations/test_product_variant_stocks_update.py
@@ -8,7 +8,7 @@ from django.db.models import Sum
 from .....plugins.manager import get_plugins_manager
 from .....tests.utils import flush_post_commit_hooks
 from .....warehouse.error_codes import StockErrorCode
-from .....warehouse.models import Stock, Warehouse
+from .....warehouse.models import Stock, Warehouse, Allocation
 from ....tests.utils import get_graphql_content
 from ...bulk_mutations.products import ProductVariantStocksUpdate
 from ...utils import create_stocks
@@ -358,6 +358,74 @@ def test_update_or_create_variant_with_back_in_stock_webhooks_only_failed(
     product_variant_stock_out_of_stock_webhook.assert_called_once_with(
         Stock.objects.all()[1]
     )
+
+
+@patch("saleor.plugins.manager.PluginsManager.product_variant_back_in_stock")
+@patch("saleor.plugins.manager.PluginsManager.product_variant_out_of_stock")
+def test_update_or_create_variant_with_back_in_stock_webhooks_with_allocations(
+    product_variant_stock_out_of_stock_webhook,
+    product_variant_back_in_stock_webhook,
+    settings,
+    variant,
+    warehouse,
+):
+    stock_quantity = 4
+    stock = Stock.objects.create(
+        warehouse=warehouse,
+        product_variant=variant,
+        quantity=stock_quantity)
+
+    # given
+    settings.PLUGINS = ["saleor.plugins.webhook.plugin.WebhookPlugin"]
+    plugins = get_plugins_manager()
+    stock.quantity_allocated = stock_quantity
+    stock.save(update_fields=['quantity_allocated'])
+    stocks_data = [
+        {"quantity": stock.quantity_allocated + 1},
+    ]
+
+    # when
+    ProductVariantStocksUpdate.update_or_create_variant_stocks(
+        variant, stocks_data, [warehouse], plugins
+    )
+    # then
+    flush_post_commit_hooks()
+    product_variant_back_in_stock_webhook.assert_called_once_with(stock)
+    product_variant_stock_out_of_stock_webhook.assert_not_called()
+
+
+@patch("saleor.plugins.manager.PluginsManager.product_variant_back_in_stock")
+@patch("saleor.plugins.manager.PluginsManager.product_variant_out_of_stock")
+def test_update_or_create_variant_with_out_of_stock_webhooks_with_allocations(
+    product_variant_stock_out_of_stock_webhook,
+    product_variant_back_in_stock_webhook,
+    settings,
+    variant,
+    warehouse,
+):
+    stock_quantity = 4
+    stock = Stock.objects.create(
+        warehouse=warehouse,
+        product_variant=variant,
+        quantity=stock_quantity)
+
+    # given
+    settings.PLUGINS = ["saleor.plugins.webhook.plugin.WebhookPlugin"]
+    plugins = get_plugins_manager()
+    stock.quantity_allocated = stock_quantity - 1
+    stock.save(update_fields=['quantity_allocated'])
+    stocks_data = [
+        {"quantity": stock.quantity_allocated},
+    ]
+
+    # when
+    ProductVariantStocksUpdate.update_or_create_variant_stocks(
+        variant, stocks_data, [warehouse], plugins
+    )
+    # then
+    flush_post_commit_hooks()
+    product_variant_stock_out_of_stock_webhook.assert_called_once_with(stock)
+    product_variant_back_in_stock_webhook.assert_not_called()
 
 
 @patch("saleor.plugins.manager.PluginsManager.product_variant_back_in_stock")
