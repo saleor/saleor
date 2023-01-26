d5eafc5bedce9429c3865299329801bb6b1f7d36
Patryk Zawadzki <81205+patrys@users.noreply.github.com>
2023-01-26 10:42:45 +0100

    Fix OIDC plugin crashing when given a JWT without an exp claim (#11814)

--- saleor/plugins/openid_connect/tests/test_plugin.py
+++ saleor/plugins/openid_connect/tests/test_plugin.py
@@ -911,6 +911,7 @@ def test_authenticate_user_with_jwt_access_token(
     decoded_access_token["scope"] = ""
     decoded_token = MagicMock()
     decoded_token.__getitem__.side_effect = decoded_access_token.__getitem__
+    decoded_token.get.side_effect = decoded_access_token.get
 
     # mock get token from request
     monkeypatch.setattr(
@@ -1151,6 +1152,7 @@ def test_authenticate_user_with_jwt_access_token_unable_to_fetch_user_info(
 
     decoded_token = MagicMock()
     decoded_token.__getitem__.side_effect = decoded_access_token.__getitem__
+    decoded_token.get.side_effect = decoded_access_token.get
 
     # mock get token from request
     monkeypatch.setattr(
@@ -1193,6 +1195,7 @@ def test_authenticate_user_with_jwt_invalid_access_token(
 
     decoded_token = MagicMock()
     decoded_token.__getitem__.side_effect = decoded_access_token.__getitem__
+    decoded_token.get.side_effect = decoded_access_token.get
     decoded_token.validate.side_effect = JoseError()
 
     # mock get token from request
