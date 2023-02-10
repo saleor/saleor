95623d165d258531aca3e41fe0e22db73b7d37f5
Patryk Zawadzki <81205+patrys@users.noreply.github.com>
2023-02-10 16:56:45 +0100

    Add native gzip support to the core (#11833)
    The code is based on Starlette's middleware but does not use any
    external convenience wrappers.
    
    Starlette's middleware is incompatible with Django as Django uses
    case-sensitive headers in ASGI messages.

--- saleor/asgi/tests/conftest.py
+++ saleor/asgi/tests/conftest.py
@@ -19,10 +19,36 @@ def asgi_app() -> ASGI3Application:
                 type="http.response.start",
                 status=200,
                 headers=[(b"content-type", b"text/plain")],
+                trailers=False,
             )
         )
         await send(
             HTTPResponseBodyEvent(type="http.response.body", body=b"", more_body=False)
+        )
+
+    return fake_app
+
+
+@pytest.fixture
+def large_asgi_app() -> ASGI3Application:
+    async def fake_app(
+        scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
+    ) -> None:
+        await send(
+            HTTPResponseStartEvent(
+                type="http.response.start",
+                status=200,
+                headers=[
+                    (b"content-length", b"10000"),
+                    (b"content-type", b"text/plain"),
+                ],
+                trailers=False,
+            )
+        )
+        await send(
+            HTTPResponseBodyEvent(
+                type="http.response.body", body=10000 * b"x", more_body=False
+            )
         )
 
     return fake_app
