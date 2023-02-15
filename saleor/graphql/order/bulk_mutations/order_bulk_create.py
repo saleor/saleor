40e46c185a8ceafbc66f337f18010a6a91e2802f
zedzior <piotr.zabieglik@gmail.com>
2023-02-15 18:16:00 +0100

    Add all required objects.

--- saleor/graphql/order/bulk_mutations/order_bulk_create.py
+++ saleor/graphql/order/bulk_mutations/order_bulk_create.py
@@ -1,28 +1,179 @@
 import graphene
 
-from ....order.models import Order
 from ....permission.enums import OrderPermissions
+from ...account.types import AddressInput
 from ...core import ResolveInfo
-from ...core.descriptions import ADDED_IN_312, PREVIEW_FEATURE
-from ...core.enums import ErrorPolicyEnum
+from ...core.enums import ErrorPolicyEnum, LanguageCodeEnum
 from ...core.mutations import BaseMutation
+from ...core.scalars import WeightScalar
 from ...core.types import NonNullList
 from ...core.types.common import OrderBulkCreateError
-from ..mutations.draft_order_create import DraftOrderCreateInput
+from ...meta.mutations import MetadataInput
+from ...payment.mutations import MoneyInput
+from ..mutations.order_discount_common import OrderDiscountCommonInput
+from ..types import Order
+
+
+class TaxedMoneyInput(graphene.InputObjectType):
+    gross = graphene.Field(
+        MoneyInput, required=True, description="Gross value of an item."
+    )
+    net = graphene.Field(MoneyInput, required=True, description="Net value of an item.")
+
+
+class OrderBulkCreateUserInput(graphene.InputObjectType):
+    id = graphene.ID(description="Customer ID associated with the order.")
+    email = graphene.String(description="Customer email associated with the order.")
+    externalReference = graphene.String(
+        description="Customer external ID associated with the order."
+    )
+
+
+class OrderBulkCreateDeliveryMethodInput(graphene.InputObjectType):
+    warehouse_id = graphene.ID(description="The ID of the warehouse.")
+    shipping_method_id = graphene.ID(description="The ID of the shipping method.")
+    shipping_method_name = graphene.String(
+        description="The name of the shipping method."
+    )
+    collection_point_name = graphene.String(
+        description="The name of the collection point."
+    )
+    shipping_price = graphene.Field(
+        TaxedMoneyInput, description="The price of the shipping."
+    )
+    shipping_tax_rate = graphene.Float(
+        required=True, description="Tax rate of the shipping."
+    )
+    shipping_tax_class_id = graphene.ID(description="The ID of the tax class.")
+    shipping_tax_class_name = graphene.String(description="The name of the tax class.")
+    shipping_tax_class_metadata = NonNullList(
+        MetadataInput, description="Metadata of the tax class."
+    )
+    shipping_tax_class_private_metadata = NonNullList(
+        MetadataInput, description="Private metadata of the tax class."
+    )
+
+
+class OrderBulkCreateNoteInput(graphene.InputObjectType):
+    message = graphene.String(required=True, description="Note message.")
+    date = graphene.DateTime(description="The date associated with the message.")
+    user = graphene.ID(description="The user ID associated with the message.")
+    app = graphene.ID(description="The app ID associated with the message.")
+
+
+class OrderBulkCreateOrderLineInput(graphene.InputObjectType):
+    variant_id = graphene.ID(description="The ID of the product variant.")
+    variant_sku = graphene.String(description="The sku of the product variant.")
+    variant_external_reference = graphene.String(
+        description="The external ID of the product variant."
+    )
+    variant_name = graphene.String(description="The name of the product variant.")
+    product_name = graphene.String(description="The name of the product.")
+    translated_variant_name = graphene.String(
+        description="Translation of the product variant name."
+    )
+    translated_product_name = graphene.String(
+        description="Translation of the product name."
+    )
+    created_at = graphene.DateTime(
+        required=True, description="The date, when the order line was created."
+    )
+    is_shipping_required = graphene.Boolean(
+        required=True,
+        description="Determines whether shipping of the order line items is required.",
+    )
+    is_gift_card = graphene.Boolean(required=True, description="Gift card flag.")
+    quantity = graphene.Int(
+        required=True, description="Number of items in the order line"
+    )
+    quantity_fulfilled = graphene.Int(
+        required=True,
+        description="Number of items in the order line, "
+        "which have been already fulfilled.",
+    )
+    total_price = graphene.Field(
+        TaxedMoneyInput, required=True, description="Price of the order line."
+    )
+    undiscounted_total_price = graphene.Field(
+        TaxedMoneyInput,
+        required=True,
+        description="Price of the order line excluding applied discount.",
+    )
+    tax_rate = graphene.Float(required=True, description="Tax rate of the order line.")
+    tax_class_id = graphene.ID(description="The ID of the tax class.")
+    tax_class_name = graphene.String(description="The name of the tax class.")
+    tax_class_metadata = NonNullList(
+        MetadataInput, description="Metadata of the tax class."
+    )
+    tax_class_private_metadata = NonNullList(
+        MetadataInput, description="Private metadata of the tax class."
+    )
+
+
+class OrderBulkCreateInput(graphene.InputObjectType):
+    number = graphene.String(description="Unique identifier of the order.")
+    external_reference = graphene.String(description="External ID of the order.")
+    channel = graphene.ID(
+        required=True, description="ID of the channel associated with the order."
+    )
+    created_at = graphene.DateTime(
+        required=True,
+        description="The date, when the order was inserted to Saleor database.",
+    )
+    user = graphene.Field(
+        OrderBulkCreateUserInput,
+        required=True,
+        description="Customer accociated with the order.",
+    )
+    tracking_client_id = graphene.String(description="Tracking ID of the customer.")
+    billing_address = graphene.Field(
+        AddressInput, required=True, description="Billing address of the customer."
+    )
+    shipping_address = graphene.Field(
+        AddressInput, description="Shipping address of the customer."
+    )
+    metadata = NonNullList(MetadataInput, description="Metadata of the order.")
+    private_metadata = NonNullList(
+        MetadataInput, description="Private metadata of the order."
+    )
+    customer_note = graphene.String(description="Note about customer.")
+    notes = NonNullList(
+        OrderBulkCreateNoteInput,
+        description="Notes related to the order.",
+    )
+    language_code_enum = graphene.Field(
+        LanguageCodeEnum, required=True, description="Order language code."
+    )
+    display_gross_price = graphene.Boolean(
+        default=False,
+        description="Determines whether checkout prices should include taxes, "
+        "when displayed in a storefront.",
+    )
+    weight = WeightScalar(description="Weight of the order.")
+    redirect_url = graphene.String(
+        description="URL of a view, where users should be redirected "
+        "to see the order details.",
+    )
+    lines = NonNullList(
+        OrderBulkCreateOrderLineInput, required=True, description="List of order lines."
+    )
+    delivery_method = graphene.Field(
+        OrderBulkCreateDeliveryMethodInput,
+        required=True,
+        description="The delivery method selected for this order.",
+    )
+    promo_codes = NonNullList(graphene.String, description="List of promo codes.")
+    discounts = NonNullList(OrderDiscountCommonInput, description="List of discounts.")
+    # TODO invoices = [OrderBulkCreateInvoiceInput!]
+    # TODO transactions: [TransactionCreateInput!]!
+    # TODO fulfillments: [OrderBulkCreateFulfillmentInput!]
 
 
 class OrderBulkCreateResult(graphene.ObjectType):
-    order = graphene.Field(
-        Order,
-        required=False,
-        description="Order data." + ADDED_IN_312 + PREVIEW_FEATURE,
-    )
+    order = graphene.Field(Order, required=True, description="Order data.")
     errors = NonNullList(
         OrderBulkCreateError,
-        required=False,
-        description="List of errors occurred on create attempt."
-        + ADDED_IN_312
-        + PREVIEW_FEATURE,
+        description="List of errors occurred on create attempt.",
     )
 
 
@@ -30,24 +181,20 @@ class OrderBulkCreate(BaseMutation):
     count = graphene.Int(
         required=True,
         default_value=0,
-        description="Returns how many objects were created."
-        + ADDED_IN_312
-        + PREVIEW_FEATURE,
+        description="Returns how many objects were created.",
     )
     results = NonNullList(
         OrderBulkCreateResult,
         required=True,
         default_value=[],
-        description="List of the created orders." + ADDED_IN_312 + PREVIEW_FEATURE,
+        description="List of the created orders.",
     )
 
     class Arguments:
         orders = NonNullList(
-            DraftOrderCreateInput,
+            OrderBulkCreateInput,
             required=True,
-            description="Input list of orders to create."
-            + ADDED_IN_312
-            + PREVIEW_FEATURE,
+            description="Input list of orders to create.",
         )
         error_policy = ErrorPolicyEnum(
             required=False,
@@ -55,8 +202,6 @@ class OrderBulkCreate(BaseMutation):
             description=(
                 "Policies of error handling. DEFAULT: "
                 + ErrorPolicyEnum.REJECT_EVERYTHING.name
-                + ADDED_IN_312
-                + PREVIEW_FEATURE
             ),
         )
 
