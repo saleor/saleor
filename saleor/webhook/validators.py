027391bf14f3e16777222eb4ac053b13bd7708bb
zedzior <piotr.zabieglik@gmail.com>
2023-02-16 09:02:08 +0100

    Optimize validator.

--- saleor/webhook/validators.py
+++ saleor/webhook/validators.py
@@ -5,6 +5,14 @@ from django.core.exceptions import ValidationError
 
 HEADERS_NUMBER_LIMIT = 5
 HEADERS_LENGTH_LIMIT = 998
+KEY_CHARS_ALLOWED = (
+    "!\"#$%&'()*+,-./0123456789;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+)
+VALUE_CHARS_ALLOWED = (
+    "!\"#$%&'()*+,-./0123456789;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~ \t"
+)
 
 
 def custom_headers_validator(headers: Dict[str, str]) -> Dict[str, str]:
@@ -17,25 +25,22 @@ def custom_headers_validator(headers: Dict[str, str]) -> Dict[str, str]:
             f"Number of headers exceeds the limit: {HEADERS_NUMBER_LIMIT}."
         )
 
-    key_chars_allowed = "".join([chr(i) for i in range(33, 127) if i != ord(":")])
-    value_chars_allowed = key_chars_allowed + chr(32) + chr(9)
-
     for key, value in headers.items():
         try:
             header = ": ".join([key, value])
         except TypeError:
             raise ValidationError(f'Header with "{key}" can\'t be converted to string.')
 
-        if not set(key).issubset(set(key_chars_allowed)):
-            raise ValidationError(f'Key "{key}" contains invalid character.')
-
-        if not set(value).issubset(set(value_chars_allowed)):
-            raise ValidationError(f'Value "{value}" contains invalid character.')
-
         if len(header) > HEADERS_LENGTH_LIMIT:
             raise ValidationError(
                 f'"{header}" exceeds the limit of characters: {HEADERS_LENGTH_LIMIT}.'
             )
+
+        if not set(key).issubset(set(KEY_CHARS_ALLOWED)):
+            raise ValidationError(f'Key "{key}" contains invalid character.')
+
+        if not set(value).issubset(set(VALUE_CHARS_ALLOWED)):
+            raise ValidationError(f'Value "{value}" contains invalid character.')
 
         if not re.search(r"(^X-\S*)|(^Authorization\S*)", key):
             raise ValidationError(
