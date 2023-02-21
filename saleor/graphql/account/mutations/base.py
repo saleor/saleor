f6abfa0a424318b555f7c9534ec3a22b8d9e3fba
Tomasz Szymanski <lime129@gmail.com>
2023-02-21 11:03:56 +0100

    Throttle password reset mutation per user

--- saleor/graphql/account/mutations/base.py
+++ saleor/graphql/account/mutations/base.py
@@ -1,9 +1,11 @@
 from typing import cast
 
 import graphene
+from django.conf import settings
 from django.contrib.auth import password_validation
 from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.utils import timezone
 
 from ....account import events as account_events
 from ....account import models
@@ -160,7 +162,6 @@ class RequestPasswordReset(BaseMutation):
 
     @classmethod
     def clean_user(cls, email, redirect_url):
-
         try:
             validate_storefront_url(redirect_url)
         except ValidationError as error:
@@ -178,6 +179,7 @@ class RequestPasswordReset(BaseMutation):
                     )
                 }
             )
+
         if not user.is_active:
             raise ValidationError(
                 {
@@ -187,6 +189,19 @@ class RequestPasswordReset(BaseMutation):
                     )
                 }
             )
+
+        if password_reset_time := user.last_password_reset_request:
+            delta = timezone.now() - password_reset_time
+            if delta.total_seconds() < settings.RESET_PASSWORD_LOCK_TIME:
+                raise ValidationError(
+                    {
+                        "email": ValidationError(
+                            "Password reset already requested",
+                            code=AccountErrorCode.PASSWORD_RESET_ALREADY_REQUESTED,
+                        )
+                    }
+                )
+
         return user
 
     @classmethod
@@ -212,6 +227,8 @@ class RequestPasswordReset(BaseMutation):
             channel_slug=channel_slug,
             staff=user.is_staff,
         )
+        user.last_password_reset_request = timezone.now()
+        user.save(update_fields=["last_password_reset_request"])
         return RequestPasswordReset()
 
 
