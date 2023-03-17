8a5443c4b78ca219dff0fb1398dbadb414484fa5
Marcin Gębala <maarcin.gebala@gmail.com>
2023-03-17 10:16:41 +0100

    Add function docstring

--- saleor/graphql/product/mutations/utils.py
+++ saleor/graphql/product/mutations/utils.py
@@ -3,20 +3,17 @@ from django.db.models import Q
 from ....plugins.manager import PluginsManager
 from ....tax.models import TaxClass
 
-"""
-Clean deprecated `taxCode` field.
-
-This function provides backwards compatibility for the `taxCode` input field. If the
-`taxClass` is not provided but the `taxCode` is, try to find a tax class with given
-tax code and assign it to the product type. If no matching tax class is found, create
-one with the given tax code.
-"""
-
 
 def clean_tax_code(cleaned_input: dict, manager: PluginsManager):
+    """Clean deprecated `taxCode` field.
+
+    This function provides backwards compatibility for the `taxCode` input field. If the
+    `taxClass` is not provided but the `taxCode` is, try to find a tax class with given
+    tax code and assign it to the product type. If no matching tax class is found,
+    create one with the given tax code.
+    """
     tax_code = cleaned_input.get("tax_code")
     if tax_code and "tax_class" not in cleaned_input:
-        tax_code = cleaned_input["tax_code"]
         tax_class = TaxClass.objects.filter(
             Q(name=tax_code)
             | Q(metadata__contains={"avatax.code": tax_code})
