d7eb46d42b8fae64a070b9b2f9f51ac5efcac43e
zedzior <piotr.zabieglik@gmail.com>
2023-03-17 09:53:33 +0100

    Fix tests.

--- saleor/graphql/order/tests/mutations/test_utils.py
+++ saleor/graphql/order/tests/mutations/test_utils.py
@@ -3,6 +3,7 @@ import pytest
 from django.core.exceptions import ValidationError
 
 from .....account.models import User
+from .....order.error_codes import OrderBulkCreateErrorCode
 from ...bulk_mutations.utils import get_instance
 
 USER_KEY_MAP = {
@@ -16,7 +17,10 @@ def test_get_instance(customer_user):
         "user_id": graphene.Node.to_global_id("User", customer_user.id),
         "dummy_data": ["abc", 1],
     }
-    assert get_instance(input, User, USER_KEY_MAP, {}) == customer_user
+    assert (
+        get_instance(input, User, USER_KEY_MAP, {}, OrderBulkCreateErrorCode)
+        == customer_user
+    )
 
 
 def test_get_instance_fail_multiple_identifiers(customer_user):
@@ -25,7 +29,7 @@ def test_get_instance_fail_multiple_identifiers(customer_user):
         "user_email": customer_user.email,
     }
     with pytest.raises(ValidationError) as error:
-        get_instance(input, User, USER_KEY_MAP, {})
+        get_instance(input, User, USER_KEY_MAP, {}, OrderBulkCreateErrorCode)
 
     assert (
         error.value.message == "Only one of [user_id, user_email] arguments can"
@@ -36,7 +40,7 @@ def test_get_instance_fail_multiple_identifiers(customer_user):
 def test_get_instance_fail_no_identifier(customer_user):
     input = {"dummy_data": ["abc", 1]}
     with pytest.raises(ValidationError) as error:
-        get_instance(input, User, USER_KEY_MAP, {})
+        get_instance(input, User, USER_KEY_MAP, {}, OrderBulkCreateErrorCode)
 
     assert (
         error.value.message == "One of [user_id, user_email] arguments must"
@@ -47,7 +51,7 @@ def test_get_instance_fail_no_identifier(customer_user):
 def test_get_instance_fail_invalid_global_id_syntax(customer_user):
     input = {"user_id": "wrong_global_ID"}
     with pytest.raises(ValidationError) as error:
-        get_instance(input, User, USER_KEY_MAP, {})
+        get_instance(input, User, USER_KEY_MAP, {}, OrderBulkCreateErrorCode)
 
     assert error.value.message == "Couldn't resolve id: wrong_global_ID."
 
@@ -56,7 +60,7 @@ def test_get_instance_fail_invalid_global_id_model(customer_user, app):
     app_global_id = graphene.Node.to_global_id("App", app.id)
     input = {"user_id": app_global_id}
     with pytest.raises(ValidationError) as error:
-        get_instance(input, User, USER_KEY_MAP, {})
+        get_instance(input, User, USER_KEY_MAP, {}, OrderBulkCreateErrorCode)
 
     assert error.value.message == "Must receive a User id."
 
@@ -65,7 +69,7 @@ def test_get_instance_fail_non_existing_global_id(customer_user):
     non_existing_email = "non_existing@example.com"
     input = {"user_email": non_existing_email}
     with pytest.raises(ValidationError) as error:
-        get_instance(input, User, USER_KEY_MAP, {})
+        get_instance(input, User, USER_KEY_MAP, {}, OrderBulkCreateErrorCode)
 
     assert (
         error.value.message == "User instance with email=non_existing@example.com"
