74c3edcd395051a34079c7b6b836dfe2bb42b0fd
kadewu <krzysztof.waliczek@saleor.io>
2023-03-20 08:20:29 +0100

    Add celery task

---
+++ saleor/order/migrations/tasks/saleor3_13.py
@@ -0,0 +1,27 @@
+from ....celeryconf import app
+from ...models import OrderEvent
+
+# Batch size of size 10000 is about 1MB memory usage in task
+BATCH_SIZE = 10000
+
+
+@app.task
+def order_eventes_rename_transaction_void_events():
+    qs = OrderEvent.objects.filter(type="transaction_void_requested").order_by("pk")
+    ids = qs.values_list("pk", flat=True)[:BATCH_SIZE]
+    if ids:
+        OrderEvent.objects.filter(pk__in=ids).update(
+            type="transaction_cancel_requested"
+        )
+        order_eventes_rename_transaction_void_events.delay()
+
+
+@app.task
+def order_eventes_rename_transaction_capture_events():
+    qs = OrderEvent.objects.filter(type="transaction_capture_requested").order_by("pk")
+    ids = qs.values_list("pk", flat=True)[:BATCH_SIZE]
+    if ids:
+        OrderEvent.objects.filter(pk__in=ids).update(
+            type="transaction_charge_requested"
+        )
+        order_eventes_rename_transaction_capture_events.delay()
