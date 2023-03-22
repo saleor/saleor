a8098f52231b857b659be6e2f3ebac02130d9522
kadewu <krzysztof.waliczek@saleor.io>
2023-03-22 14:07:22 +0100

    Add support for downtime migrations

--- saleor/payment/migrations/0041_add_calculation_transaction_events.py
+++ saleor/payment/migrations/0041_add_calculation_transaction_events.py
@@ -7,18 +7,31 @@ from .tasks.saleor3_12 import (
     create_event_for_canceled,
     create_event_for_charged,
     create_event_for_refunded,
+    create_event_for_authorized_task,
+    create_event_for_canceled_task,
+    create_event_for_charged_task,
+    create_event_for_refunded_task,
 )
+from ... import __version__
 
 
 def create_transaction_events(apps, schema_editor):
     def on_migrations_complete(sender=None, **kwargs):
-        create_event_for_authorized.delay()
-        create_event_for_canceled.delay()
-        create_event_for_charged.delay()
-        create_event_for_refunded.delay()
+        create_event_for_authorized_task.delay()
+        create_event_for_canceled_task.delay()
+        create_event_for_charged_task.delay()
+        create_event_for_refunded_task.delay()
 
-    sender = registry.get_app_config("payment")
-    post_migrate.connect(on_migrations_complete, weak=False, sender=sender)
+    if __version__.startswith("3.12"):
+        sender = registry.get_app_config("payment")
+        post_migrate.connect(on_migrations_complete, weak=False, sender=sender)
+    else:
+        TransactionItem = apps.get_model("payment", "TransactionItem")
+        TransactionEvent = apps.get_model("payment", "TransactionEvent")
+        create_event_for_authorized(TransactionItem, TransactionEvent)
+        create_event_for_canceled(TransactionItem, TransactionEvent)
+        create_event_for_charged(TransactionItem, TransactionEvent)
+        create_event_for_refunded(TransactionItem, TransactionEvent)
 
 
 class Migration(migrations.Migration):
