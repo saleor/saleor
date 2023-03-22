27c8ac2f8721e0eca8500102757d7a0b31167644
kadewu <krzysztof.waliczek@saleor.io>
2023-03-22 14:07:22 +0100

    Add support for downtime migrations

--- saleor/payment/migrations/0040_migrate_renamed_fields.py
+++ saleor/payment/migrations/0040_migrate_renamed_fields.py
@@ -8,19 +8,34 @@ from .tasks.saleor3_12 import (
     transaction_item_migrate_reference_to_psp_reference,
     transaction_item_migrate_type_to_name,
     transaction_item_migrate_voided_to_canceled,
+    transaction_event_migrate_name_to_message_task,
+    transaction_event_migrate_reference_to_psp_reference_task,
+    transaction_item_migrate_reference_to_psp_reference_task,
+    transaction_item_migrate_type_to_name_task,
+    transaction_item_migrate_voided_to_canceled_task,
 )
+from ... import __version__
 
 
 def migrate_data_for_renamed_fields(apps, _schema_editor):
     def on_migrations_complete(sender=None, **kwargs):
-        transaction_event_migrate_name_to_message.delay()
-        transaction_event_migrate_reference_to_psp_reference.delay()
-        transaction_item_migrate_reference_to_psp_reference.delay()
-        transaction_item_migrate_type_to_name.delay()
-        transaction_item_migrate_voided_to_canceled.delay()
+        transaction_event_migrate_name_to_message_task.delay()
+        transaction_event_migrate_reference_to_psp_reference_task.delay()
+        transaction_item_migrate_reference_to_psp_reference_task.delay()
+        transaction_item_migrate_type_to_name_task.delay()
+        transaction_item_migrate_voided_to_canceled_task.delay()
 
-    sender = registry.get_app_config("payment")
-    post_migrate.connect(on_migrations_complete, weak=False, sender=sender)
+    if __version__.startswith("3.12"):
+        sender = registry.get_app_config("payment")
+        post_migrate.connect(on_migrations_complete, weak=False, sender=sender)
+    else:
+        TransactionItem = apps.get_model("payment", "TransactionItem")
+        TransactionEvent = apps.get_model("payment", "TransactionEvent")
+        transaction_event_migrate_name_to_message(TransactionEvent)
+        transaction_event_migrate_reference_to_psp_reference(TransactionEvent)
+        transaction_item_migrate_reference_to_psp_reference(TransactionItem)
+        transaction_item_migrate_type_to_name(TransactionItem)
+        transaction_item_migrate_voided_to_canceled(TransactionItem)
 
 
 class Migration(migrations.Migration):
