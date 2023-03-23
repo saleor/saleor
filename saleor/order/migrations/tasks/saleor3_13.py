63902184402b8731c988f62ee4474e51460255de
kadewu <krzysztof.waliczek@saleor.io>
2023-03-23 21:10:31 +0100

    Remove checking saleor version
    other changes:
    - changed the ordering to DESC by primary key in queries

--- saleor/order/migrations/tasks/saleor3_13.py
+++ saleor/order/migrations/tasks/saleor3_13.py
@@ -5,47 +5,31 @@ from ...models import OrderEvent
 BATCH_SIZE = 1000
 
 
-def _get_ids(qs, batch_size=None):
-    if batch_size:
-        return qs.values_list("pk", flat=True)[:batch_size]
-    return qs.values_list("pk", flat=True)
-
-
-def order_eventes_rename_transaction_void_events(order_event_class, batch_size=None):
-    qs = order_event_class.objects.filter(type="transaction_void_requested").order_by(
-        "pk"
-    )
-    ids = _get_ids(qs, batch_size)
-    if ids:
-        order_event_class.objects.filter(pk__in=ids).update(
-            type="transaction_cancel_requested"
-        )
-
-        return True
-    return False
+def update_type_to_transaction_cancel_requested(qs):
+    qs.update(type="transaction_cancel_requested")
 
 
 @app.task
-def order_eventes_rename_transaction_void_events_task():
-    if order_eventes_rename_transaction_void_events(OrderEvent, BATCH_SIZE):
-        order_eventes_rename_transaction_void_events_task.delay()
+def order_events_rename_transaction_void_events_task():
+    qs = OrderEvent.objects.filter(type="transaction_void_requested").order_by("-pk")
+    ids = qs.values_list("pk", flat=True)[:BATCH_SIZE]
+    qs = qs.filter(pk__in=ids)
 
-
-def order_eventes_rename_transaction_capture_events(order_event_class, batch_size=None):
-    qs = order_event_class.objects.filter(
-        type="transaction_capture_requested"
-    ).order_by("pk")
-    ids = _get_ids(qs, batch_size)
     if ids:
-        order_event_class.objects.filter(pk__in=ids).update(
-            type="transaction_charge_requested"
-        )
+        update_type_to_transaction_cancel_requested(qs)
+        order_events_rename_transaction_void_events_task.delay()
 
-        return True
-    return False
+
+def update_type_to_transaction_charge_requested(qs):
+    qs.update(type="transaction_charge_requested")
 
 
 @app.task
-def order_eventes_rename_transaction_capture_events_task():
-    if order_eventes_rename_transaction_capture_events(OrderEvent, BATCH_SIZE):
-        order_eventes_rename_transaction_capture_events_task.delay()
+def order_events_rename_transaction_capture_events_task():
+    qs = OrderEvent.objects.filter(type="transaction_capture_requested").order_by("-pk")
+    ids = qs.values_list("pk", flat=True)[:BATCH_SIZE]
+    qs = qs.filter(pk__in=ids)
+
+    if ids:
+        update_type_to_transaction_charge_requested(qs)
+        order_events_rename_transaction_capture_events_task.delay()
