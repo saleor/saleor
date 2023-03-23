6465804bcf4dbdc0b1f469509330ae81aa99009f
IKarbowiak <iga.karbowiak@saleor.io>
2023-03-23 09:25:47 +0100

    Check channel permissions in order invoice mutations

--- saleor/graphql/invoice/tests/test_invoice_update.py
+++ saleor/graphql/invoice/tests/test_invoice_update.py
@@ -1,7 +1,7 @@
 import graphene
 
 from ....core import JobStatus
-from ....graphql.tests.utils import get_graphql_content
+from ....graphql.tests.utils import assert_no_permission, get_graphql_content
 from ....invoice.error_codes import InvoiceErrorCode
 from ....invoice.models import Invoice
 
@@ -32,7 +32,8 @@ INVOICE_UPDATE_MUTATION = """
 """
 
 
-def test_invoice_update(staff_api_client, permission_manage_orders, order):
+def test_invoice_update(staff_api_client, permission_group_manage_orders, order):
+    permission_group_manage_orders.user_set.add(staff_api_client.user)
     test_key = "test_key"
     metadata = {test_key: "test_val"}
     invoice = Invoice.objects.create(order=order, metadata=metadata)
@@ -44,9 +45,7 @@ def test_invoice_update(staff_api_client, permission_manage_orders, order):
         "number": number,
         "url": url,
     }
-    response = staff_api_client.post_graphql(
-        INVOICE_UPDATE_MUTATION, variables, permissions=[permission_manage_orders]
-    )
+    response = staff_api_client.post_graphql(INVOICE_UPDATE_MUTATION, variables)
     content = get_graphql_content(response)
     invoice.refresh_from_db()
     assert invoice.status == JobStatus.SUCCESS
@@ -58,7 +57,69 @@ def test_invoice_update(staff_api_client, permission_manage_orders, order):
     assert content["data"]["invoiceUpdate"]["invoice"]["id"] == graphene_invoice_id
 
 
-def test_invoice_update_single_value(staff_api_client, permission_manage_orders, order):
+def test_invoice_update_by_user_no_channel_access(
+    staff_api_client, permission_group_all_perms_channel_USD_only, order, channel_PLN
+):
+    # given
+    permission_group_all_perms_channel_USD_only.user_set.add(staff_api_client.user)
+
+    order.channel = channel_PLN
+    order.save(update_fields=["channel"])
+
+    test_key = "test_key"
+    metadata = {test_key: "test_val"}
+    invoice = Invoice.objects.create(order=order, metadata=metadata)
+    number = "01/12/2020/TEST"
+    url = "http://www.example.com"
+    graphene_invoice_id = graphene.Node.to_global_id("Invoice", invoice.pk)
+    variables = {
+        "id": graphene_invoice_id,
+        "number": number,
+        "url": url,
+    }
+
+    # when
+    response = staff_api_client.post_graphql(INVOICE_UPDATE_MUTATION, variables)
+
+    # then
+    assert_no_permission(response)
+
+
+def test_invoice_update_by_app(app_api_client, permission_manage_orders, order):
+    # given
+    test_key = "test_key"
+    metadata = {test_key: "test_val"}
+    invoice = Invoice.objects.create(order=order, metadata=metadata)
+    number = "01/12/2020/TEST"
+    url = "http://www.example.com"
+    graphene_invoice_id = graphene.Node.to_global_id("Invoice", invoice.pk)
+    variables = {
+        "id": graphene_invoice_id,
+        "number": number,
+        "url": url,
+    }
+
+    # when
+    response = app_api_client.post_graphql(
+        INVOICE_UPDATE_MUTATION, variables, permissions=(permission_manage_orders,)
+    )
+
+    # then
+    content = get_graphql_content(response)
+    invoice.refresh_from_db()
+    assert invoice.status == JobStatus.SUCCESS
+    assert invoice.number == content["data"]["invoiceUpdate"]["invoice"]["number"]
+    response_metadata = content["data"]["invoiceUpdate"]["invoice"]["metadata"][0]
+    assert response_metadata["key"] == test_key
+    assert response_metadata["value"] == metadata[test_key]
+    assert invoice.url == content["data"]["invoiceUpdate"]["invoice"]["url"]
+    assert content["data"]["invoiceUpdate"]["invoice"]["id"] == graphene_invoice_id
+
+
+def test_invoice_update_single_value(
+    staff_api_client, permission_group_manage_orders, order
+):
+    permission_group_manage_orders.user_set.add(staff_api_client.user)
     number = "01/12/2020/TEST"
     invoice = Invoice.objects.create(order=order, number=number)
     url = "http://www.example.com"
@@ -66,9 +127,7 @@ def test_invoice_update_single_value(staff_api_client, permission_manage_orders,
         "id": graphene.Node.to_global_id("Invoice", invoice.pk),
         "url": url,
     }
-    response = staff_api_client.post_graphql(
-        INVOICE_UPDATE_MUTATION, variables, permissions=[permission_manage_orders]
-    )
+    response = staff_api_client.post_graphql(INVOICE_UPDATE_MUTATION, variables)
     content = get_graphql_content(response)
     invoice.refresh_from_db()
     assert invoice.status == JobStatus.SUCCESS
@@ -77,17 +136,16 @@ def test_invoice_update_single_value(staff_api_client, permission_manage_orders,
 
 
 def test_invoice_update_missing_number(
-    staff_api_client, permission_manage_orders, order
+    staff_api_client, permission_group_manage_orders, order
 ):
+    permission_group_manage_orders.user_set.add(staff_api_client.user)
     invoice = Invoice.objects.create(order=order)
     url = "http://www.example.com"
     variables = {
         "id": graphene.Node.to_global_id("Invoice", invoice.pk),
         "url": url,
     }
-    response = staff_api_client.post_graphql(
-        INVOICE_UPDATE_MUTATION, variables, permissions=[permission_manage_orders]
-    )
+    response = staff_api_client.post_graphql(INVOICE_UPDATE_MUTATION, variables)
     content = get_graphql_content(response)
     invoice.refresh_from_db()
     error = content["data"]["invoiceUpdate"]["errors"][0]
@@ -97,11 +155,10 @@ def test_invoice_update_missing_number(
     assert invoice.status == JobStatus.PENDING
 
 
-def test_invoice_update_invalid_id(staff_api_client, permission_manage_orders):
+def test_invoice_update_invalid_id(staff_api_client, permission_group_manage_orders):
+    permission_group_manage_orders.user_set.add(staff_api_client.user)
     variables = {"id": "SW52b2ljZToxMzM3", "number": "01/12/2020/TEST"}
-    response = staff_api_client.post_graphql(
-        INVOICE_UPDATE_MUTATION, variables, permissions=[permission_manage_orders]
-    )
+    response = staff_api_client.post_graphql(INVOICE_UPDATE_MUTATION, variables)
     content = get_graphql_content(response)
     error = content["data"]["invoiceUpdate"]["errors"][0]
     assert error["code"] == InvoiceErrorCode.NOT_FOUND.name
