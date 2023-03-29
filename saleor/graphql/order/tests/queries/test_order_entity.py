f8e40499dbdac5f456602f955f5dc13506813a9a
IKarbowiak <iga.karbowiak@saleor.io>
2023-03-29 13:40:50 +0200

    Fix failing test

--- saleor/graphql/order/tests/queries/test_order_entity.py
+++ saleor/graphql/order/tests/queries/test_order_entity.py
@@ -20,6 +20,7 @@ ORDER_FEDERATION_QUERY = """
 def test_staff_query_order_by_id_for_federation(
     staff_api_client, fulfilled_order, permission_group_manage_orders
 ):
+    # given
     permission_group_manage_orders.user_set.add(staff_api_client.user)
     order_id = graphene.Node.to_global_id("Order", fulfilled_order.id)
     variables = {
@@ -31,11 +32,14 @@ def test_staff_query_order_by_id_for_federation(
         ],
     }
 
+    # when
     response = staff_api_client.post_graphql(
         ORDER_FEDERATION_QUERY,
         variables,
         check_no_permissions=False,
     )
+
+    # then
     content = get_graphql_content(response)
     assert content["data"]["_entities"] == [
         {
