9cae386f512e7d10c53f8cdab95936ce76170ce6
Kirill Plaksin <kyrylo.plaksin@mirumee.com>
2023-03-29 12:14:34 +0200

    added tests.

--- saleor/graphql/core/tests/test_file_validation.py
+++ saleor/graphql/core/tests/test_file_validation.py
@@ -20,6 +20,21 @@ from ...core.utils import FILE_NAME_MAX_LENGTH
 def test_get_filename_from_url_unique():
     # given
     file_format = "jpg"
+    file_name = "lenna"
+    url = f"http://example.com/{file_name}.{file_format}"
+
+    # when
+    result = get_filename_from_url(url)
+
+    # then
+    assert result.startswith(file_name)
+    assert result.endswith(file_format)
+    assert result != f"{file_name}.{file_format}"
+
+
+def test_get_filename_from_url_with_long_name_is_trimmed():
+    # given
+    file_format = "jpg"
     file_name = "2Fvar2Ffolders2Fbj2F61gtb14j7rz474yd15tnkzjh0000gn2FT2Fa"
     assert len(file_name) > FILE_NAME_MAX_LENGTH
     url = f"http://example.com/{file_name}.{file_format}"
@@ -32,6 +47,23 @@ def test_get_filename_from_url_unique():
     assert result.endswith(file_format)
     assert result != f"{file_name}.{file_format}"
     assert len(result.split("_")[0]) == FILE_NAME_MAX_LENGTH
+
+
+def test_get_filename_from_url_with_short_name_is_not_trimmed():
+    # given
+    file_format = "jpg"
+    file_name = "short"
+    assert len(file_name) < FILE_NAME_MAX_LENGTH
+    url = f"http://example.com/{file_name}.{file_format}"
+
+    # when
+    result = get_filename_from_url(url)
+
+    # then
+    assert result.startswith(file_name)
+    assert result.endswith(file_format)
+    assert result != f"{file_name}.{file_format}"
+    assert len(result.split("_")[0]) < FILE_NAME_MAX_LENGTH
 
 
 def test_is_image_mimetype_valid_mimetype():
