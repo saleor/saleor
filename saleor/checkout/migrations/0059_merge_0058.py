fefb7542925deb69c063954cb433d76d808d9c05
Mikail Kocak <mikail.kocak@saleor.io>
2023-04-03 13:27:51 +0200

    Workaround typing bug and fix migration style

--- saleor/checkout/migrations/0059_merge_0058.py
+++ saleor/checkout/migrations/0059_merge_0058.py
@@ -4,11 +4,9 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('checkout', '0058_auto_20230313_1602'),
-        ('checkout', '0058_merge_0055_add_last_change_idx'),
+        ("checkout", "0058_auto_20230313_1602"),
+        ("checkout", "0058_merge_0055_add_last_change_idx"),
     ]
 
-    operations = [
-    ]
+    operations = []
