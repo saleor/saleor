e0ba958c9134ec5a1de3c4fcf9d019ee509faf25
Mikail Kocak <mikail.kocak@saleor.io>
2023-04-03 10:28:27 +0200

    Improve performance of Celery task deleting inactive  checkouts
    This improves the following issues of the Celery task `delete_expired_checkouts`:
    - It was scanning the whole table and putting it in memory, added `LIMIT`
    - It was doing unnecessary joins, replaced with subquery
    - Slight reordering of the conditions to improve performance
    - Removed unnecessary `ORDER BY`
    - Added a limit to how many checkouts can be deleted per task to limit how long the task may run for

--- saleor/checkout/models.py
+++ saleor/checkout/models.py
@@ -39,7 +39,7 @@ class Checkout(models.Model):
     """A shopping checkout."""
 
     created_at = models.DateTimeField(auto_now_add=True)
-    last_change = models.DateTimeField(auto_now=True)
+    last_change = models.DateTimeField(auto_now=True, db_index=True)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         blank=True,
