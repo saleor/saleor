34f61cd3a9c18ea6594569eaaa4560c47c76340c
Rafał Pitoń <rafal@mirumee.com>
2023-04-04 16:13:28 +0200

    Revert get_graphql_schema changes

--- saleor/graphql/management/commands/get_graphql_schema.py
+++ saleor/graphql/management/commands/get_graphql_schema.py
@@ -8,7 +8,4 @@ class Command(BaseCommand):
     help = "Writes SDL for GraphQL API schema to stdout"
 
     def handle(self, *args, **options):
-        # self.stdout.write(print_schema(schema))
-        with open("new-schema.graphql", "w") as fp:
-            s = print_schema(schema)
-            fp.write(s)
+        self.stdout.write(print_schema(schema))
