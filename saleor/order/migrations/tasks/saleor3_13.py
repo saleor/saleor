d82bb36ee20f2ba24ad1ba286481909bb67e4027
Krzysztof Waliczek <krzysztof.waliczek@saleor.io>
2023-04-07 15:07:53 +0200

    Fix deadlock in order events celery migrations task (#12503)
    * Fix deadlock in order events celery migrations task
    
    Additionaly increase the batch size to decrease overall migrations time
    
    * Improve query performance
    
    * Update memory usage in comment

--- saleor/order/migrations/tasks/saleor3_13.py
+++ saleor/order/migrations/tasks/saleor3_13.py
@@ -1,21 +1,27 @@
 from ....celeryconf import app
 from ...models import OrderEvent
 
+from django.db import transaction
 from django.db.models import QuerySet
 
-# Batch size of size 1000 is about 1MB memory usage in task
-BATCH_SIZE = 1000
+# Batch size of size 5000 is about 3MB memory usage in task
+BATCH_SIZE = 5000
 
 
 def update_type_to_transaction_cancel_requested(qs: QuerySet[OrderEvent]):
-    qs.update(type="transaction_cancel_requested")
+    with transaction.atomic():
+        # lock the batch of objects
+        _events = list(qs.select_for_update(of=(["self"])))
+        qs.update(type="transaction_cancel_requested")
 
 
 @app.task
 def order_events_rename_transaction_void_events_task():
-    qs = OrderEvent.objects.filter(type="transaction_void_requested").order_by("-pk")
-    ids = qs.values_list("pk", flat=True)[:BATCH_SIZE]
-    qs = qs.filter(pk__in=ids)
+    events = OrderEvent.objects.filter(type="transaction_void_requested").order_by(
+        "-pk"
+    )
+    ids = events.values_list("pk", flat=True)[:BATCH_SIZE]
+    qs = OrderEvent.objects.filter(pk__in=ids)
 
     if ids:
         update_type_to_transaction_cancel_requested(qs)
@@ -23,14 +29,19 @@ def order_events_rename_transaction_void_events_task():
 
 
 def update_type_to_transaction_charge_requested(qs: QuerySet[OrderEvent]):
-    qs.update(type="transaction_charge_requested")
+    with transaction.atomic():
+        # lock the batch of objects
+        _events = list(qs.select_for_update(of=(["self"])))
+        qs.update(type="transaction_charge_requested")
 
 
 @app.task
 def order_events_rename_transaction_capture_events_task():
-    qs = OrderEvent.objects.filter(type="transaction_capture_requested").order_by("-pk")
-    ids = qs.values_list("pk", flat=True)[:BATCH_SIZE]
-    qs = qs.filter(pk__in=ids)
+    events = OrderEvent.objects.filter(type="transaction_capture_requested").order_by(
+        "-pk"
+    )
+    ids = events.values_list("pk", flat=True)[:BATCH_SIZE]
+    qs = OrderEvent.objects.filter(pk__in=ids)
 
     if ids:
         update_type_to_transaction_charge_requested(qs)
