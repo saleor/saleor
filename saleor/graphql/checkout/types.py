2cff842228b4dab49fe4cf6354551e7f545ef78e
Mikail Kocak <mikail.kocak@saleor.io>
2023-04-18 14:23:16 +0200

    Remove leftover TODO comments

--- saleor/graphql/checkout/types.py
+++ saleor/graphql/checkout/types.py
@@ -486,7 +486,6 @@ class Checkout(ModelObjectType):
                 delivery_method, ShippingMethodData
             ):
                 return
-            assert isinstance(delivery_method, LazyObject) is False  # TODO: drop
             return delivery_method
 
         return (
