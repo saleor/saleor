b12c2ae9fada2d88ba9eab9b2d85fa899dbf91b6
Mikail Kocak <mikail.kocak@saleor.io>
2023-04-18 13:56:07 +0200

    Reformat file with isort

--- saleor/checkout/fetch.py
+++ saleor/checkout/fetch.py
@@ -14,6 +14,7 @@ from typing import (
 )
 
 from django.utils.encoding import smart_text
+
 from ..core.utils.lazyobjects import lazy_no_retry
 from ..discount import DiscountInfo, VoucherType
 from ..discount.interface import fetch_voucher_info
