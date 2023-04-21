4e3554fe18f4faeb87d9dce01cfd52f25b4031ad
IKarbowiak <iga.karbowiak@saleor.io>
2023-04-21 11:45:56 +0200

    Allow defining metadata in invoice mutations

--- saleor/graphql/invoice/tests/test_invoice_create.py
+++ saleor/graphql/invoice/tests/test_invoice_create.py
@@ -10,13 +10,10 @@ from ....invoice.models import Invoice, InvoiceEvent, InvoiceEvents
 from ....order import OrderEvents, OrderStatus
 
 INVOICE_CREATE_MUTATION = """
-    mutation InvoiceCreate($orderId: ID!, $number: String!, $url: String!) {
+    mutation InvoiceCreate($orderId: ID!, $input: InvoiceCreateInput!) {
         invoiceCreate(
             orderId: $orderId,
-            input: {
-                number: $number,
-                url: $url
-            }
+            input: $input
         ) {
             invoice {
                 status
@@ -24,6 +21,14 @@ INVOICE_CREATE_MUTATION = """
                 url
                 order {
                     id
+                }
+                metadata {
+                    key
+                    value
+                }
+                privateMetadata {
+                    key
+                    value
                 }
             }
             errors {
@@ -36,20 +41,36 @@ INVOICE_CREATE_MUTATION = """
 
 
 def test_create_invoice(staff_api_client, permission_manage_orders, order):
+    # given
     number = "01/12/2020/TEST"
     url = "http://www.example.com"
     order_id = graphene.Node.to_global_id("Order", order.pk)
+    metadata = [{"key": "test key", "value": "test value"}]
+    private_metadata = [{"key": "private test key", "value": "private test value"}]
     variables = {
         "orderId": order_id,
-        "number": number,
-        "url": url,
+        "input": {
+            "number": number,
+            "url": url,
+            "metadata": metadata,
+            "privateMetadata": private_metadata,
+        },
     }
+
+    # when
     response = staff_api_client.post_graphql(
         INVOICE_CREATE_MUTATION, variables, permissions=[permission_manage_orders]
     )
+
+    # then
     content = get_graphql_content(response)
     invoice = Invoice.objects.get(order=order, status=JobStatus.SUCCESS)
     assert order_id == content["data"]["invoiceCreate"]["invoice"]["order"]["id"]
+    assert content["data"]["invoiceCreate"]["invoice"]["metadata"] == metadata
+    assert (
+        content["data"]["invoiceCreate"]["invoice"]["privateMetadata"]
+        == private_metadata
+    )
     assert invoice.url == content["data"]["invoiceCreate"]["invoice"]["url"]
     assert invoice.number == content["data"]["invoiceCreate"]["invoice"]["number"]
     assert (
@@ -80,8 +101,10 @@ def test_create_invoice_no_billing_address(
     url = "http://www.example.com"
     variables = {
         "orderId": graphene.Node.to_global_id("Order", order.pk),
-        "number": number,
-        "url": url,
+        "input": {
+            "number": number,
+            "url": url,
+        },
     }
     response = staff_api_client.post_graphql(
         INVOICE_CREATE_MUTATION, variables, permissions=[permission_manage_orders]
@@ -106,8 +129,10 @@ def test_create_invoice_invalid_order_status(
     url = "http://www.example.com"
     variables = {
         "orderId": graphene.Node.to_global_id("Order", order.pk),
-        "number": number,
-        "url": url,
+        "input": {
+            "number": number,
+            "url": url,
+        },
     }
     response = staff_api_client.post_graphql(
         INVOICE_CREATE_MUTATION, variables, permissions=[permission_manage_orders]
@@ -123,8 +148,10 @@ def test_create_invoice_invalid_order_status(
 def test_create_invoice_invalid_id(staff_api_client, permission_manage_orders):
     variables = {
         "orderId": graphene.Node.to_global_id("Order", uuid.uuid4()),
-        "number": "01/12/2020/TEST",
-        "url": "http://www.example.com",
+        "input": {
+            "number": "01/12/2020/TEST",
+            "url": "http://www.example.com",
+        },
     }
     response = staff_api_client.post_graphql(
         INVOICE_CREATE_MUTATION, variables, permissions=[permission_manage_orders]
@@ -138,8 +165,10 @@ def test_create_invoice_invalid_id(staff_api_client, permission_manage_orders):
 def test_create_invoice_empty_params(staff_api_client, permission_manage_orders, order):
     variables = {
         "orderId": graphene.Node.to_global_id("Order", order.pk),
-        "number": "",
-        "url": "",
+        "input": {
+            "number": "",
+            "url": "",
+        },
     }
     response = staff_api_client.post_graphql(
         INVOICE_CREATE_MUTATION, variables, permissions=[permission_manage_orders]
