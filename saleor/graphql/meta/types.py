49fad39cc27a7e7443c0672d2f3925fa331a1578
Jakub <jakub.kuc@saleor.io>
2023-05-02 13:43:26 +0200

    fix checkout metadata fetches to not create checkout metadata objects (#12646)
    * fix checkout metadata fetches to not create checkout metadata objects
    
    * chenge get_or_create to get in more places
    
    * fix creating checkout instance
    
    * cosmetic fixes
    
    * Add more tests to ensure that the checkout metadat is properly fetched
    
    ---------
    
    Co-authored-by: IKarbowiak <iga.karbowiak@saleor.io>

--- saleor/graphql/meta/types.py
+++ saleor/graphql/meta/types.py
@@ -4,7 +4,7 @@ import graphene
 from graphene.types.generic import GenericScalar
 
 from ...checkout.models import Checkout
-from ...checkout.utils import get_or_create_checkout_metadata
+from ...checkout.utils import get_checkout_metadata
 from ...core.models import ModelWithMetadata
 from ..channel import ChannelContext
 from ..core import ResolveInfo
@@ -151,5 +151,5 @@ class ObjectWithMetadata(graphene.Interface):
 
 def get_valid_metadata_instance(instance):
     if isinstance(instance, Checkout):
-        instance = get_or_create_checkout_metadata(instance)
+        instance = get_checkout_metadata(instance)
     return instance
