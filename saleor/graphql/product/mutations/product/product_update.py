30f243cc30a9bffe3e53558d70b79fd09d7d7ba1
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-05-08 12:32:35 +0200

    Update product mutations to recalculated products discounted price (#12750)
    * Recalculate the product discounted price in ProductCreate, ProductUpdate and ProductChannelListingUpdate mutations
    
    * Recalculate the product discounted price in ProductBulkCreate mutation

--- saleor/graphql/product/mutations/product/product_update.py
+++ saleor/graphql/product/mutations/product/product_update.py
@@ -6,6 +6,7 @@ from .....attribute import models as attribute_models
 from .....permission.enums import ProductPermissions
 from .....product import models
 from .....product.search import update_product_search_vector
+from .....product.tasks import update_product_discounted_price_task
 from ....attribute.utils import AttributeAssignmentMixin, AttrValuesInput
 from ....core import ResolveInfo
 from ....core.descriptions import ADDED_IN_310
@@ -50,9 +51,11 @@ class ProductUpdate(ProductCreate, ModelWithExtRefMutation):
         return attributes
 
     @classmethod
-    def post_save_action(cls, info: ResolveInfo, instance, _cleaned_input):
+    def post_save_action(cls, info: ResolveInfo, instance, cleaned_input):
         product = models.Product.objects.prefetched_for_webhook().get(pk=instance.pk)
         update_product_search_vector(instance)
+        if "category" in cleaned_input or "collections" in cleaned_input:
+            update_product_discounted_price_task.delay(instance.id)
         manager = get_plugin_manager_promise(info.context).get()
         cls.call_event(manager.product_updated, product)
 
