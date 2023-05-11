26351af2ca3d7a05c0a498010460892d8343bb6e
Tomasz Szymański <lime129@gmail.com>
2023-05-11 17:06:09 +0200

    Optimize App type resolvers

--- saleor/graphql/app/tests/benchmarks/test_apps.py
+++ saleor/graphql/app/tests/benchmarks/test_apps.py
@@ -1,7 +1,8 @@
 import graphene
 import pytest
 
-from .....app.models import App
+from .....app.models import App, AppToken
+from .....webhook.models import Webhook
 from ....tests.utils import get_graphql_content
 
 
@@ -71,3 +72,56 @@ def test_apps_for_federation_query_count(
         )
         content = get_graphql_content(response)
         assert len(content["data"]["_entities"]) == 3
+
+
+@pytest.mark.django_db
+@pytest.mark.count_queries(autouse=False)
+def test_apps_with_tokens(
+    staff_api_client,
+    permission_manage_apps,
+    django_assert_num_queries,
+    count_queries,
+):
+    apps = App.objects.bulk_create(
+        [
+            App(name="app 1"),
+            App(name="app 2"),
+            App(name="app 3"),
+        ]
+    )
+
+    for app in apps:
+        for i in range(3):
+            Webhook.objects.create(
+                app=app, target_url=f"http://www.example.com/{i}", name=f"webhook{i}"
+            )
+            AppToken.objects.create(app=app, name=f"token{i}")
+
+    query = """
+        query apps {
+            apps(first:100) {
+                edges {
+                    node {
+                        id
+                        tokens {
+                            name
+                            authToken
+                        }
+                        webhooks {
+                            id
+                            targetUrl
+                        }
+                    }
+                }
+            }
+        }
+    """
+
+    response = staff_api_client.post_graphql(
+        query,
+        {},
+        permissions=[permission_manage_apps],
+        check_no_permissions=False,
+    )
+    content = get_graphql_content(response)
+    assert len(content["data"]["apps"]["edges"]) == 3
