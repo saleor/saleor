19785aca41f0dc87d59c666340ec4858f64f0031
fremahA <captainfremah66@gmail.com>
2023-05-12 17:43:09 +0000

    add test for inactive user

--- saleor/checkout/tests/test_associate_checkout_with_account.py
+++ saleor/checkout/tests/test_associate_checkout_with_account.py
@@ -81,3 +81,42 @@ def test_associate_guest_checkout_with_account_if_exists_with_guest_user(
 
     # assert that the order is associated with the correct user
     assert order.user is None
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "email, paid_strategy",
+    [
+        ("test@example.com", MarkAsPaidStrategy.TRANSACTION_FLOW),
+        ("test@example.com", MarkAsPaidStrategy.PAYMENT_FLOW),
+    ],
+)
+def test_associate_guest_checkout_with_account_if_exists_with_inactive_user(
+    email, paid_strategy, app, address, checkout, customer_user
+):
+    # set the checkout email
+    checkout.email = email
+    checkout.save()
+    # deactivate the customer user
+    customer_user.is_active = False
+    customer_user.save()
+    user = None
+    manager = get_plugins_manager()
+    lines, _ = fetch_checkout_lines(checkout)
+    checkout_info = fetch_checkout_info(checkout, lines, [], manager)
+    checkout_info.billing_address = address
+    checkout_info.channel.order_mark_as_paid_strategy == paid_strategy
+
+    # call the complete_checkout function with the checkout object
+    order, _, _ = complete_checkout(
+        checkout_info=checkout_info,
+        manager=manager,
+        lines=lines,
+        payment_data={},
+        store_source=False,
+        user=user,
+        app=app,
+    )
+
+    # assert that the order is associated with the correct user
+    assert order.user is None
