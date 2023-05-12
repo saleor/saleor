a519a115fc4d98678b383d23fc62909a942a0a80
IKarbowiak <iga.karbowiak@saleor.io>
2023-05-12 14:27:44 +0200

    Update task for product discounted price recalculation

--- saleor/product/migrations/tasks/saleor3_13.py
+++ saleor/product/migrations/tasks/saleor3_13.py
@@ -1,6 +1,4 @@
-from django.db.models import Exists, OuterRef
-
-from ...models import Product, ProductVariantChannelListing, ProductVariant
+from ...models import Product
 from ...utils.variant_prices import update_products_discounted_price
 from ....celeryconf import app
 
@@ -10,21 +8,13 @@ BATCH_SIZE = 500
 
 
 @app.task
-def update_discounted_prices_task():
-    listings = ProductVariantChannelListing.objects.filter(
-        discounted_price_amount__isnull=True
-    )
-    variants = ProductVariant.objects.filter(
-        Exists(listings.filter(variant_id=OuterRef("id")))
-    )
+def update_discounted_prices_task(start_pk=0):
     products = list(
-        Product.objects.filter(
-            Exists(variants.filter(product_id=OuterRef("id"))),
-        )
+        Product.objects.filter(pk__gt=start_pk)
         .prefetch_related("channel_listings", "collections")
-        .order_by("-pk")[:BATCH_SIZE]
+        .order_by("pk")[:BATCH_SIZE]
     )
 
     if products:
         update_products_discounted_price(products)
-        update_discounted_prices_task.delay()
+        update_discounted_prices_task.delay(products[-1].pk)
