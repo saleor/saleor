e0d2fcda1761f34f34014420bb4a1183f0c26bde
Krzysztof Waliczek <krzysztof.waliczek@saleor.io>
2023-05-16 11:41:35 +0200

    zero-downtime: cleanup migrations and db (#12806)

--- saleor/order/migrations/0163_order_events_rename_transaction_events.py
+++ saleor/order/migrations/0163_order_events_rename_transaction_events.py
@@ -1,20 +1,65 @@
-from django.db import migrations
-from django.apps import apps as registry
-from django.db.models.signals import post_migrate
+from django.db import migrations, transaction
+from django.db.models import QuerySet
+from ..models import OrderEvent
 
-from .tasks.saleor3_13 import (
-    order_events_rename_transaction_capture_events_task,
-    order_events_rename_transaction_void_events_task,
-)
+
+BATCH_SIZE = 5000
+
+
+def queryset_in_batches(queryset):
+    """Slice a queryset into batches.
+
+    Input queryset should be sorted be pk.
+    """
+    start_pk = 0
+
+    while True:
+        qs = queryset.filter(pk__gt=start_pk)[:BATCH_SIZE]
+        pks = list(qs.values_list("pk", flat=True))
+
+        if not pks:
+            break
+
+        yield pks
+
+        start_pk = pks[-1]
+
+
+def update_type_to_transaction_cancel_requested(qs: QuerySet[OrderEvent]):
+    with transaction.atomic():
+        # lock the batch of objects
+        _events = list(qs.select_for_update(of=(["self"])))
+        qs.update(type="transaction_cancel_requested")
+
+
+def order_events_rename_transaction_void_events_task():
+    events = OrderEvent.objects.filter(type="transaction_void_requested").order_by("pk")
+
+    for ids in queryset_in_batches(events):
+        qs = OrderEvent.objects.filter(pk__in=ids)
+        update_type_to_transaction_cancel_requested(qs)
+
+
+def update_type_to_transaction_charge_requested(qs: QuerySet[OrderEvent]):
+    with transaction.atomic():
+        # lock the batch of objects
+        _events = list(qs.select_for_update(of=(["self"])))
+        qs.update(type="transaction_charge_requested")
+
+
+def order_events_rename_transaction_capture_events_task():
+    events = OrderEvent.objects.filter(type="transaction_capture_requested").order_by(
+        "pk"
+    )
+
+    for ids in queryset_in_batches(events):
+        qs = OrderEvent.objects.filter(pk__in=ids)
+        update_type_to_transaction_charge_requested(qs)
 
 
 def rename_order_events(apps, _schema_editor):
-    def on_migrations_complete(sender=None, **kwargs):
-        order_events_rename_transaction_void_events_task.delay()
-        order_events_rename_transaction_capture_events_task.delay()
-
-    sender = registry.get_app_config("order")
-    post_migrate.connect(on_migrations_complete, weak=False, sender=sender)
+    order_events_rename_transaction_capture_events_task()
+    order_events_rename_transaction_void_events_task()
 
 
 class Migration(migrations.Migration):
