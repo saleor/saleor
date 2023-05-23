eb22bef04047a4b61f57c0909a85ce9e902746f4
Michal Widera <michal.widera@saleor.io>
2023-05-23 11:02:19 +0200

    Disallow redirects for external requests

--- saleor/plugins/webhook/tasks.py
+++ saleor/plugins/webhook/tasks.py
@@ -323,7 +323,11 @@ def send_webhook_using_http(
     }
     try:
         response = requests.post(
-            target_url, data=message, headers=headers, timeout=timeout
+            target_url,
+            data=message,
+            headers=headers,
+            timeout=timeout,
+            allow_redirects=False,
         )
     except (RequestException,) as e:
         response = WebhookResponse(
