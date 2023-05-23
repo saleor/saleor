10d76fc1bd7d4475428ac3c9fd08fdde8556d5b1
renatagajzlerowicz <renata.gajzlerowicz@gmail.com>
2023-05-23 17:54:26 +0200

    add vitest

--- api-tests/vitest.config.ts
+++ api-tests/vitest.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  test: {
-    include: [__dirname + '*/tests/**/*.test.ts'],
-  },
+    test: {
+        include: [__dirname + '*/tests/**/*.test.ts']
+    },
 })
