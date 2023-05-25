9844155b1cd8c720e53dc5da0988d93774f49c7a
renatagajzlerowicz <renata.gajzlerowicz@gmail.com>
2023-05-25 11:33:09 +0200

    eslint

--- api-tests/tests/v3.13/test.test.ts
+++ api-tests/tests/v3.13/test.test.ts
@@ -1,5 +1,15 @@
 import { describe, expect, it } from 'vitest'
+import { GraphQLClient } from 'graphql-request'
 
+export const makeClient = (token: string): GraphQLClient => {
+  const baseEndpoint = 'https://master.staging.saleor.cloud/dashboard'
+
+  return new GraphQLClient(baseEndpoint, {
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+  })
+}
 describe('1st test', () => {
   it('tests something', () => {
     expect(true).toBe(true)
