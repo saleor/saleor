cfd0941d4e88acd909e3ae19907e8795b95a44cf
renatagajzlerowicz <renata.gajzlerowicz@gmail.com>
2023-05-25 09:51:44 +0200

    dummy test

--- api-tests/tests/v3.13/test.test.ts
+++ api-tests/tests/v3.13/test.test.ts
@@ -1,32 +1,4 @@
 import { describe, expect, it } from 'vitest'
-import { GraphQLClient, gql } from 'graphql-request'
-
-export const registerUser = gql`
-  mutation {
-    accountRegister(
-      input: { email: "customer@example.com", password: "secret", channel: "default-channel" }
-    ) {
-      errors {
-        field
-        code
-      }
-      user {
-        email
-        isActive
-        userPermissions
-      }
-    }
-  }
-`
-export const makeClient = (token: string): GraphQLClient => {
-  const endpoint = 'https://api.github.com/graphql'
-
-  return new GraphQLClient(endpoint, {
-    headers: {
-      Authorization: `bearer ${token}`,
-    },
-  })
-}
 
 describe('1st test', () => {
   it('tests something', () => {
