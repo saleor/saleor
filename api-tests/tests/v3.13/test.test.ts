2ed3e0e4fc081c0bddfbfeb2f445ca7b98fb7ec7
renatagajzlerowicz <renata.gajzlerowicz@gmail.com>
2023-05-25 09:49:40 +0200

    add eslint and other config

--- api-tests/tests/v3.13/test.test.ts
+++ api-tests/tests/v3.13/test.test.ts
@@ -1,8 +1,35 @@
-import {describe, expect, it} from "vitest";
+import { describe, expect, it } from 'vitest'
+import { GraphQLClient, gql } from 'graphql-request'
 
+export const registerUser = gql`
+  mutation {
+    accountRegister(
+      input: { email: "customer@example.com", password: "secret", channel: "default-channel" }
+    ) {
+      errors {
+        field
+        code
+      }
+      user {
+        email
+        isActive
+        userPermissions
+      }
+    }
+  }
+`
+export const makeClient = (token: string): GraphQLClient => {
+  const endpoint = 'https://api.github.com/graphql'
 
-describe('1st test', () =>{
-    it('tests something', () => {
-        expect(true).toBe(true)
-    })
+  return new GraphQLClient(endpoint, {
+    headers: {
+      Authorization: `bearer ${token}`,
+    },
+  })
+}
+
+describe('1st test', () => {
+  it('tests something', () => {
+    expect(true).toBe(true)
+  })
 })
