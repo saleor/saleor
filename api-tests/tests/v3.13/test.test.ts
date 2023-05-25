f646266e1b0bebdc213cb96fa27aedb6f528f9d6
renatagajzlerowicz <renata.gajzlerowicz@gmail.com>
2023-05-25 11:33:09 +0200

    eslint

--- api-tests/tests/v3.13/test.test.ts
+++ api-tests/tests/v3.13/test.test.ts
@@ -1,33 +1,15 @@
 import { describe, expect, it } from 'vitest'
-import { GraphQLClient, gql } from 'graphql-request'
+import { GraphQLClient } from 'graphql-request'
 
-export const registerUser = gql`
-  mutation {
-    accountRegister(
-      input: { email: "customer@example.com", password: "secret", channel: "default-channel" }
-    ) {
-      errors {
-        field
-        code
-      }
-      user {
-        email
-        isActive
-        userPermissions
-      }
-    }
-  }
-`
 export const makeClient = (token: string): GraphQLClient => {
-  const endpoint = 'https://api.github.com/graphql'
+  const baseEndpoint = 'https://master.staging.saleor.cloud/dashboard'
 
-  return new GraphQLClient(endpoint, {
+  return new GraphQLClient(baseEndpoint, {
     headers: {
       Authorization: `bearer ${token}`,
     },
   })
 }
-
 describe('1st test', () => {
   it('tests something', () => {
     expect(true).toBe(true)
