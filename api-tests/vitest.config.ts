e9c96bebdb0081656a7ad44b6e8223d4ce9a03e4
renatagajzlerowicz <renata.gajzlerowicz@gmail.com>
2023-05-28 17:54:47 +0200

    Adding test for token creation

--- api-tests/vitest.config.ts
+++ api-tests/vitest.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: [__dirname + '*/tests/**/*.test.ts'],
-    reporters: ['default', 'html'],
-    outputFile: '*/tests/reports',
     env: {
       apiEndpoint: 'https://master.staging.saleor.cloud/graphql/',
     },
