7c3d50b21d3269312c0ad9dc8ffc4de54fe0cffb
renatagajzlerowicz <renata.gajzlerowicz@gmail.com>
2023-05-28 17:12:27 +0200

    Adding test for token creation

--- api-tests/tests/utils.ts
+++ api-tests/tests/utils.ts
@@ -1,18 +1,40 @@
 import { GraphQLClient, gql } from 'graphql-request'
 
-export const makeClient = (): GraphQLClient => {
-  // todo - przesunac do envow
-  const endpoint = 'https://master.staging.saleor.cloud/graphql/'
+// todo - przesunac do envow
+const endpoint = 'https://master.staging.saleor.cloud/graphql/'
 
-  return new GraphQLClient(endpoint)
+export const makeClient = (): GraphQLClient => new GraphQLClient(endpoint)
+
+export const makeAuthorizedClient = async (): Promise<GraphQLClient> => {
+  const client = makeClient()
+  const mutation = gql`
+    mutation TokenCreate($email: String!, $password: String!) {
+      tokenCreate(email: $email, password: $password) {
+        csrfToken
+        refreshToken
+        token
+        errors: accountErrors {
+          ...AccountError
+        }
+        user {
+          id
+          email
+        }
+      }
+    }
+    fragment AccountError on AccountError {
+      code
+      field
+      message
+    }
+  `
+  const result = await client.request(mutation, {
+    email: 'testers+dashboard@saleor.io',
+    password: 'test1234',
+  })
+  return new GraphQLClient(endpoint, {
+    headers: {
+      Authorization: `bearer ${result.tokenCreate.token}`,
+    },
+  })
 }
-
-// export const makeClient = (token: string): GraphQLClient => {
-//   const endpoint = 'https://master.staging.saleor.cloud/graphql'
-//
-//   return new GraphQLClient(endpoint, {
-//     headers: {
-//       Authorization: `bearer ${token}`,
-//     },
-//   })
-// }
