ab1318e79facff6eebf35eb441cfed88ebde2f8f
renatagajzlerowicz <renata.gajzlerowicz@gmail.com>
2023-05-29 13:48:58 +0200

    add api-tests

---
+++ api-tests/codegen.ts
@@ -0,0 +1,17 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  schema: 'https://master.staging.saleor.cloud/graphql/',
+  documents: ['src/**/*.ts'],
+  config: { addExplicitOverride: true },
+  generates: {
+    './generated/': {
+      preset: 'client',
+      presetConfig: {
+        gqlTagName: 'gql',
+      },
+    },
+  },
+}
+
+export default config
---
+++ api-tests/src/test.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest'
+
+describe('1st test', () => {
+  it('tests something', () => {
+    expect(true).toBe(true)
+  })
+})
