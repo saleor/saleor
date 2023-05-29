d05fdc9d08801a620db9fd464337ad26fc95e9f8
renatagajzlerowicz <renata.gajzlerowicz@gmail.com>
2023-05-29 11:29:04 +0200

    adding types

--- api-tests/vitest.config.ts
+++ api-tests/vitest.config.ts
@@ -3,7 +3,5 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: [__dirname + '*/tests/**/*.test.ts'],
-    reporters: ['default', 'html'],
-    outputFile: '*/tests/reports',
   },
 })
