2df391de3bdd62c5736b8f6ca9e457feb2140b1d
Przemysław Łada <przlada@gmail.com>
2023-05-30 19:06:18 +0100

    Change get_json_schema command

--- saleor/core/management/commands/get_json_schema.py
+++ saleor/core/management/commands/get_json_schema.py
@@ -1,25 +1,19 @@
+import json
+from typing import Type
+
 from django.core.management.base import BaseCommand
-from pydantic import schema_json_of
+from pydantic import BaseModel
+from pydantic.schema import schema
 
 from ....app.manifest_schema import Manifest
-from ....webhook.event_types import WebhookEventSyncType
 from ...taxes import TaxData
 
-SCHEMA_MAP = {
-    "manifest": Manifest,
-    WebhookEventSyncType.CHECKOUT_CALCULATE_TAXES: TaxData,
-    WebhookEventSyncType.ORDER_CALCULATE_TAXES: TaxData,
-}
+SCHEMAS: list[Type[BaseModel]] = [Manifest, TaxData]
 
 
 class Command(BaseCommand):
     help = "Writes selected JSON-schema to stdout"
 
-    def add_arguments(self, parser):
-        parser.add_argument("event_type", type=str)
-
-    def handle(self, *args, event_type: str, **kwargs):
-        if model := SCHEMA_MAP.get(event_type.lower()):
-            self.stdout.write(schema_json_of(model, title=event_type.upper(), indent=2))
-        else:
-            self.stderr.write(f"Error: Can't find schema for event: {event_type}")
+    def handle(self, *args, **kwargs):
+        top_level_schema = schema(SCHEMAS, title="Saleor JSON-schema")
+        self.stdout.write(json.dumps(top_level_schema, indent=2))
