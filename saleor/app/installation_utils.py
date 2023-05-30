b8a923db8c1f14a50792ffbd113d28c0fb982ef1
Przemysław Łada <przlada@gmail.com>
2023-05-30 16:33:59 +0100

    Fix setting webhook activity using `isActive` field in the App manifest (#12714) (#12970)
    * Add webhook isActive validation in app manifest
    
    * Add tests

--- saleor/app/installation_utils.py
+++ saleor/app/installation_utils.py
@@ -96,6 +96,7 @@ def install_app(app_installation: AppInstallation, activate: bool = False):
         Webhook(
             app=app,
             name=webhook["name"],
+            is_active=webhook["isActive"],
             target_url=webhook["targetUrl"],
             subscription_query=webhook["query"],
             custom_headers=webhook.get("customHeaders", None),
