c2ad44ebaefd5704d6109293c37e948090450f1a
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-05-30 15:25:03 +0200

    Add promotion types (#12948)
    * Add Promotion and PromotionRule models
    
    * Add ADDED_IN_315 label
    
    * Add promotion types
    
    * Add promotion resolvers and dataloaders
    
    * Update promotion query test
    
    * Update the PromotionRule type
    
    * Rename notification_sent_datetime and add mising labels to promotion query

---
+++ saleor/graphql/discount/types/promotions.py
@@ -0,0 +1,101 @@
+import graphene
+from graphene import relay
+
+from ....discount import models
+from ....permission.auth_filters import AuthorizationFilters
+from ...channel.types import Channel
+from ...core import ResolveInfo
+from ...core.descriptions import ADDED_IN_315, PREVIEW_FEATURE
+from ...core.doc_category import DOC_CATEGORY_DISCOUNTS
+from ...core.fields import PermissionsField
+from ...core.scalars import JSON, PositiveDecimal
+from ...core.types import ModelObjectType, NonNullList
+from ...meta.types import ObjectWithMetadata
+from ..dataloaders import (
+    ChannelsByPromotionRuleIdLoader,
+    PromotionByIdLoader,
+    PromotionRulesByPromotionIdLoader,
+)
+from ..enums import RewardValueTypeEnum
+
+
+class Promotion(ModelObjectType[models.Promotion]):
+    id = graphene.GlobalID(required=True)
+    name = graphene.String(required=True, description="Name of the promotion.")
+    description = JSON(description="Description of the promotion.")
+    start_date = graphene.DateTime(
+        required=True, description="Start date of the promotion."
+    )
+    end_date = graphene.DateTime(description="End date of the promotion.")
+    created_at = graphene.DateTime(
+        required=True, description="Date time of promotion creation."
+    )
+    updated_at = graphene.DateTime(
+        required=True, description="Date time of last update of promotion."
+    )
+    rules = NonNullList(
+        lambda: PromotionRule, description="The list of promotion rules."
+    )
+
+    class Meta:
+        description = (
+            "Represents the promotion that allow creating discounts based on given "
+            "conditions, and is visible to all the customers."
+            + ADDED_IN_315
+            + PREVIEW_FEATURE
+        )
+        interfaces = [relay.Node, ObjectWithMetadata]
+        model = models.Promotion
+        doc_category = DOC_CATEGORY_DISCOUNTS
+
+    @staticmethod
+    def resolve_rules(root: models.Promotion, info: ResolveInfo):
+        return PromotionRulesByPromotionIdLoader(info.context).load(root.id)
+
+
+class PromotionRule(ModelObjectType[models.PromotionRule]):
+    id = graphene.GlobalID(required=True)
+    name = graphene.String(required=True, description="Name of the promotion rule.")
+    description = JSON(description="Description of the promotion rule.")
+    promotion = graphene.Field(
+        Promotion, description="Promotion to which the rule belongs."
+    )
+    channels = PermissionsField(
+        NonNullList(Channel),
+        description="List of channels where the rule applies.",
+        permissions=[
+            AuthorizationFilters.AUTHENTICATED_APP,
+            AuthorizationFilters.AUTHENTICATED_STAFF_USER,
+        ],
+    )
+    reward_value_type = RewardValueTypeEnum(
+        description="The type of reward value of the promotion rule."
+    )
+    catalogue_predicate = JSON(
+        description=(
+            "The catalogue predicate that must be met to apply the rule reward."
+        ),
+    )
+    reward_value = PositiveDecimal(
+        description=(
+            "The reward value of the promotion rule. Defines the discount value "
+            "applied when the rule conditions are met."
+        )
+    )
+
+    class Meta:
+        description = (
+            "Represents the promotion rule that specifies the conditions that must "
+            "be met to apply the promotion discount." + ADDED_IN_315 + PREVIEW_FEATURE
+        )
+        interfaces = [relay.Node]
+        model = models.PromotionRule
+        doc_category = DOC_CATEGORY_DISCOUNTS
+
+    @staticmethod
+    def resolve_promotion(root: models.PromotionRule, info: ResolveInfo):
+        return PromotionByIdLoader(info.context).load(root.promotion_id)
+
+    @staticmethod
+    def resolve_channels(root: models.PromotionRule, info: ResolveInfo):
+        return ChannelsByPromotionRuleIdLoader(info.context).load(root.id)
