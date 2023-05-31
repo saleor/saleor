bc270ab002cb5336837676df31c7ff153f580d8a
renatagajzlerowicz <renata.gajzlerowicz@gmail.com>
2023-05-31 10:38:15 +0200

    fetching schema

--- api-tests/codegen.ts
+++ api-tests/codegen.ts
@@ -1,13 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
-const schemaUrl = process.env.SCHEMA_URL
-
-if (!schemaUrl) {
-  throw new Error('Missing SCHEMA_URL environment variable')
-}
-
 const config: CodegenConfig = {
-  schema: process.env.SCHEMA_URL,
+  schema: 'graphql/schema.graphql',
   documents: ['src/**/*.ts'],
   config: { addExplicitOverride: true },
   generates: {
