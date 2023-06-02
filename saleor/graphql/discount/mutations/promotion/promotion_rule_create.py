97b9a2a02daeec674b69511dcdc1fa077d9203c0
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-06-02 10:24:08 +0200

    Add promotion rule mutations (#12979)
    * Add PromotionRuleCreate mutation
    
    * Add promotionRuleUpdate
    
    * Add promotionRuleDelete mutation
    
    * Update promotionRuleUpdate - remove transaction tracing

---
+++ saleor/graphql/discount/mutations/promotion/promotion_rule_create.py
@@ -0,0 +1,80 @@
+from collections import defaultdict
+from typing import DefaultDict, List
+
+import graphene
+from django.core.exceptions import ValidationError
+
+from .....discount import models
+from .....permission.enums import DiscountPermissions
+from ....core import ResolveInfo
+from ....core.descriptions import ADDED_IN_315, PREVIEW_FEATURE
+from ....core.doc_category import DOC_CATEGORY_DISCOUNTS
+from ....core.mutations import ModelMutation
+from ....core.types import Error
+from ...enums import PromotionRuleCreateErrorCode
+from ...types import PromotionRule
+from ...utils import clean_predicate
+from .promotion_create import PromotionRuleInput
+
+
+class PromotionRuleCreateInput(PromotionRuleInput):
+    promotion = graphene.ID(
+        description="The ID of the promotion that rule belongs to.", required=True
+    )
+
+
+class PromotionRuleCreateError(Error):
+    code = PromotionRuleCreateErrorCode(description="The error code.", required=True)
+
+
+class PromotionRuleCreate(ModelMutation):
+    class Arguments:
+        input = PromotionRuleCreateInput(
+            description="Fields required to create a promotion rule.", required=True
+        )
+
+    class Meta:
+        description = "Creates a new promotion rule." + ADDED_IN_315 + PREVIEW_FEATURE
+        model = models.PromotionRule
+        object_type = PromotionRule
+        permissions = (DiscountPermissions.MANAGE_DISCOUNTS,)
+        error_type_class = PromotionRuleCreateError
+        doc_category = DOC_CATEGORY_DISCOUNTS
+
+    @classmethod
+    def clean_input(
+        cls, info: ResolveInfo, instance: models.PromotionRule, data: dict, **kwargs
+    ):
+        cleaned_input = super().clean_input(info, instance, data, **kwargs)
+        errors: DefaultDict[str, List[ValidationError]] = defaultdict(list)
+
+        if "catalogue_predicate" not in cleaned_input:
+            errors["catalogue_predicate"].append(
+                ValidationError(
+                    "The cataloguePredicate field is required.",
+                    code=PromotionRuleCreateErrorCode.REQUIRED.value,
+                )
+            )
+        else:
+            if "reward_value_type" not in cleaned_input:
+                errors["reward_value_type"].append(
+                    ValidationError(
+                        "The rewardValueType is required for when "
+                        "cataloguePredicate is provided.",
+                        code=PromotionRuleCreateErrorCode.REQUIRED.value,
+                    )
+                )
+            if "reward_value" not in cleaned_input:
+                errors["reward_value"].append(
+                    ValidationError(
+                        "The rewardValue is required for when cataloguePredicate "
+                        "is provided.",
+                        code=PromotionRuleCreateErrorCode.REQUIRED.value,
+                    )
+                )
+            cleaned_input["catalogue_predicate"] = clean_predicate(
+                cleaned_input.get("catalogue_predicate")
+            )
+        if errors:
+            raise ValidationError(errors)
+        return cleaned_input
