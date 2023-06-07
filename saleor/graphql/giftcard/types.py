397c89ef5e56e5962ceddf0f85acc887baf7e598
Tomasz Szymanski <lime129@gmail.com>
2023-06-07 13:29:02 +0200

    Code resolver logic changes

--- saleor/graphql/giftcard/types.py
+++ saleor/graphql/giftcard/types.py
@@ -347,15 +347,12 @@ class GiftCard(ModelObjectType[models.GiftCard]):
     @staticmethod
     def resolve_code(root: models.GiftCard, info):
         def _resolve_code(user):
-            requestor = get_user_or_app_from_context(info.context)
             # Gift card code can be fetched by the staff user and app
             # with manage gift card permission and by the card owner.
-            if requestor:
+            if requestor := get_user_or_app_from_context(info.context):
                 requestor_is_an_owner = user and requestor == user
-                card_already_used = bool(root.used_by_email)
-                if requestor_is_an_owner or (
-                    not card_already_used
-                    and requestor.has_perm(GiftcardPermissions.MANAGE_GIFT_CARD)
+                if requestor_is_an_owner or requestor.has_perm(
+                    GiftcardPermissions.MANAGE_GIFT_CARD
                 ):
                     return root.code
 
