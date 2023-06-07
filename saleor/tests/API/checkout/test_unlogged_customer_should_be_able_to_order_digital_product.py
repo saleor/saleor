94bfeb0b1d8ed67f7c0d7f11f756e64589b78b08
fowczrek <f.a.owczarek@gmail.com>
2023-06-07 14:45:43 +0200

    Util for createing warehouse

--- saleor/tests/API/checkout/test_unlogged_customer_should_be_able_to_order_digital_product.py
+++ saleor/tests/API/checkout/test_unlogged_customer_should_be_able_to_order_digital_product.py
@@ -1,15 +1,22 @@
 from ..channel.utils import create_channel
 from ..products.utils import create_digital_product_type
+from ..warehouse.utils import create_warehouse
 
 
 def test_process_checkout_with_digital_product(
     staff_api_client,
     permission_manage_product_types_and_attributes,
     permission_manage_channels,
+    permission_manage_products,
 ):
     channel_data = create_channel(staff_api_client, [permission_manage_channels])
     channel_id = channel_data["id"]
     assert channel_id is not None
+
+    warehouse_data = create_warehouse(staff_api_client, [permission_manage_products])
+    warehouse_id = warehouse_data["id"]
+    assert warehouse_id is not None
+
     product_type_data = create_digital_product_type(
         staff_api_client, [permission_manage_product_types_and_attributes]
     )
---
+++ saleor/tests/API/warehouse/utils.py
@@ -0,0 +1,46 @@
+from ....graphql.tests.utils import get_graphql_content
+
+WAREHOUSE_CREATE_MUTATION = """
+mutation createWarehouse($input: WarehouseCreateInput!) {
+  createWarehouse(input: $input) {
+    errors {
+      message
+      field
+      code
+    }
+    warehouse {
+      id
+      name
+      slug
+    }
+  }
+}
+"""
+
+
+def create_warehouse(staff_api_client, permissions):
+    name = "Test warehouse"
+    slug = "test-slug"
+    address = {
+        "firstName": "John Saleor",
+        "lastName": "Doe Mirumee",
+        "companyName": "Saleor Commerce",
+        "streetAddress1": "	14208 Hawthorne Blvd",
+        "streetAddress2": "",
+        "postalCode": "90250",
+        "country": "US",
+        "city": "Hawthorne",
+        "countryArea": "CA",
+        "phone": "+12025550163",
+    }
+
+    variables = {"input": {"name": name, "slug": slug, "address": address}}
+    response = staff_api_client.post_graphql(
+        WAREHOUSE_CREATE_MUTATION, variables, permissions=permissions
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["createWarehouse"]["warehouse"]
+    assert data["name"] == name
+    assert data["slug"] == slug
+
+    return data
