e200887554b718da5e7ebbc01bc591189105f248
IKarbowiak <iga.karbowiak@saleor.io>
2023-06-12 09:36:08 +0200

    Call undiscounted price recalculations in PromotionRuleDelete mutation

--- saleor/graphql/discount/mutations/promotion/promotion_rule_delete.py
+++ saleor/graphql/discount/mutations/promotion/promotion_rule_delete.py
@@ -1,11 +1,16 @@
 import graphene
+from django.db.models import Exists, OuterRef
 
 from .....discount import models
 from .....graphql.core.mutations import ModelDeleteMutation
 from .....permission.enums import DiscountPermissions
+from .....product import models as product_models
+from .....product.tasks import update_products_discounted_prices_for_promotion_task
+from ....core import ResolveInfo
 from ....core.descriptions import ADDED_IN_315, PREVIEW_FEATURE
 from ....core.doc_category import DOC_CATEGORY_DISCOUNTS
 from ....core.types import Error
+from ....discount.utils import get_variants_for_predicate
 from ...enums import PromotionRuleDeleteErrorCode
 from ...types import PromotionRule
 
@@ -27,3 +32,28 @@ class PromotionRuleDelete(ModelDeleteMutation):
         permissions = (DiscountPermissions.MANAGE_DISCOUNTS,)
         error_type_class = PromotionRuleDeleteError
         doc_category = DOC_CATEGORY_DISCOUNTS
+
+    @classmethod
+    def perform_mutation(  # type: ignore[override]
+        cls, _root, info: ResolveInfo, /, *, external_reference=None, id=None
+    ):
+        """Perform a mutation that deletes a model instance."""
+        instance = cls.get_instance(info, external_reference=external_reference, id=id)
+        cls.clean_instance(info, instance)
+
+        variants = get_variants_for_predicate(instance.catalogue_predicate)
+        products = product_models.Product.objects.filter(
+            Exists(variants.filter(product_id=OuterRef("id")))
+        )
+        product_ids = list(products.values_list("id", flat=True))
+
+        db_id = instance.id
+        instance.delete()
+
+        # After the instance is deleted, set its ID to the original database's
+        # ID so that the success response contains ID of the deleted object.
+        instance.id = db_id
+
+        update_products_discounted_prices_for_promotion_task.delay(product_ids)
+
+        return cls.success_response(instance)
