6e6b3c91b5e7bd390213896656138094040b253d
fowczrek <f.a.owczarek@gmail.com>
2023-06-16 12:08:14 +0200

    Rename API module to e2r

--- saleor/tests/__init__.py
+++ saleor/tests/__init__.py
@@ -1,4 +1,4 @@
 import pytest
 
 # We want to have PyTest assert introspection in the API tests
-pytest.register_assert_rewrite("saleor.tests.API")
+pytest.register_assert_rewrite("saleor.tests.e2e")
