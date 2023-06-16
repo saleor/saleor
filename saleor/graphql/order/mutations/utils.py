e98beab6bd8aad51cc91eba022e0428eaf8ba7d4
Jakub <jakub.kuc@saleor.io>
2023-06-16 13:36:51 +0200

    Fix draft order shipping method update (#13079)
    * Fix draft order shipping method update (#12926)
    
    * add draft version of modifying and refactorind draft order update for shipping methods
    
    * add tests, fix linters
    
    * fix linters
    
    * fixes for clearing shipping method, cosmetic changes after review
    
    * add taxrate to clear
    
    * fix linters
    
    * fix resolver, deactivate validation for drafts
    
    * adjust port to new version
    
    * remove redundant function

--- saleor/graphql/order/mutations/utils.py
+++ saleor/graphql/order/mutations/utils.py
@@ -2,13 +2,32 @@ from typing import Optional
 
 from django.core.exceptions import ValidationError
 
+from ....core.taxes import zero_money, zero_taxed_money
 from ....order import ORDER_EDITABLE_STATUS, OrderStatus, events
 from ....order.error_codes import OrderErrorCode
+from ....order.utils import invalidate_order_prices
 from ....payment import PaymentError
 from ....payment import models as payment_models
 from ....plugins.manager import PluginsManager
 from ....shipping.interface import ShippingMethodData
+from ....shipping.models import ShippingMethodChannelListing
 from ..utils import get_shipping_method_availability_error
+
+SHIPPING_METHOD_UPDATE_FIELDS = [
+    "currency",
+    "shipping_method",
+    "shipping_price_net_amount",
+    "shipping_price_gross_amount",
+    "base_shipping_price_amount",
+    "shipping_method_name",
+    "shipping_tax_class",
+    "shipping_tax_class_name",
+    "shipping_tax_class_private_metadata",
+    "shipping_tax_class_metadata",
+    "shipping_tax_rate",
+    "should_refresh_prices",
+    "updated_at",
+]
 
 
 class EditableOrderValidationMixin:
@@ -27,6 +46,72 @@ class EditableOrderValidationMixin:
                 }
             )
         return order
+
+
+class ShippingMethodUpdateMixin:
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def clear_shipping_method_from_order(cls, order):
+        order.shipping_method = None
+        order.base_shipping_price = zero_money(order.currency)
+        order.shipping_price = zero_taxed_money(order.currency)
+        order.shipping_method_name = None
+        order.shipping_tax_rate = None
+        order.shipping_tax_class = None
+        order.shipping_tax_class_name = None
+        order.shipping_tax_class_private_metadata = {}
+        order.shipping_tax_class_metadata = {}
+        invalidate_order_prices(order)
+
+    @classmethod
+    def update_shipping_method(cls, order, method, shipping_method_data):
+        order.shipping_method = method
+        order.shipping_method_name = method.name
+
+        tax_class = method.tax_class
+        if tax_class:
+            order.shipping_tax_class = tax_class
+            order.shipping_tax_class_name = tax_class.name
+            order.shipping_tax_class_private_metadata = tax_class.private_metadata
+            order.shipping_tax_class_metadata = tax_class.metadata
+        invalidate_order_prices(order)
+
+    @classmethod
+    def validate_shipping_channel_listing(cls, method, order):
+        shipping_channel_listing = ShippingMethodChannelListing.objects.filter(
+            shipping_method=method, channel=order.channel
+        ).first()
+        if not shipping_channel_listing:
+            raise ValidationError(
+                {
+                    "shipping_method": ValidationError(
+                        "Shipping method not available in the given channel.",
+                        code=OrderErrorCode.SHIPPING_METHOD_NOT_APPLICABLE.value,
+                    )
+                }
+            )
+        return shipping_channel_listing
+
+    @classmethod
+    def _update_shipping_price(
+        cls,
+        order,
+        shipping_channel_listing,
+    ):
+        if not shipping_channel_listing:
+            order.base_shipping_price = zero_money(order.currency)
+            return
+
+        if (
+            order.shipping_method
+            and order.shipping_address
+            and order.is_shipping_required()
+        ):
+            order.base_shipping_price = shipping_channel_listing.price
+        else:
+            order.base_shipping_price = zero_money(order.currency)
 
 
 def clean_order_update_shipping(
