e54086fcf209ed3b22d2d04d61880b7849162cd3
fowczrek <f.a.owczarek@gmail.com>
2023-06-26 11:04:36 +0200

    Create channel

--- saleor/tests/e2e/checkout/test_unlogged_customer_should_be_able_to_order_physical_product.py
+++ saleor/tests/e2e/checkout/test_unlogged_customer_should_be_able_to_order_physical_product.py
@@ -1,3 +1,4 @@
+from ..channel.utils import create_channel
 from ..utils import assign_permissions
 from ..warehouse.utils import create_warehouse
 
@@ -5,13 +6,21 @@ from ..warehouse.utils import create_warehouse
 def test_process_checkout_with_physical_product(
     e2e_staff_api_client,
     permission_manage_products,
+    permission_manage_channels,
 ):
     # given
     permissions = [
         permission_manage_products,
+        permission_manage_channels,
     ]
     assign_permissions(e2e_staff_api_client, permissions)
 
     warehouse_data = create_warehouse(e2e_staff_api_client)
     warehouse_id = warehouse_data["id"]
-    assert warehouse_id is not None
+
+    warehouse_ids = [warehouse_id]
+    channel_data = create_channel(e2e_staff_api_client, warehouse_ids=warehouse_ids)
+    channel_id = channel_data["id"]
+    channel_slug = channel_data["slug"]
+    assert channel_id is not None
+    assert channel_slug is not None
