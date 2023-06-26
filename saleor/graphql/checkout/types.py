ace87c0eab696e50d7f5c2bb90ead8e6ee4f2024
Krzysztof Kwaśniak <mr.brzys@gmail.com>
2023-06-26 08:32:37 +0200

    Add GirtCardsByCheckoutIdLoader (#13178)

--- saleor/graphql/checkout/types.py
+++ saleor/graphql/checkout/types.py
@@ -41,6 +41,7 @@ from ..core.types import ModelObjectType, Money, NonNullList, TaxedMoney
 from ..core.utils import str_to_enum
 from ..decorators import one_of_permissions_required
 from ..discount.dataloaders import DiscountsByDateTimeLoader
+from ..giftcard.dataloaders import GirtCardsByCheckoutIdLoader
 from ..giftcard.types import GiftCard
 from ..meta import resolvers as MetaResolvers
 from ..meta.types import ObjectWithMetadata, _filter_metadata
@@ -676,8 +677,8 @@ class Checkout(ModelObjectType[models.Checkout]):
         )
 
     @staticmethod
-    def resolve_gift_cards(root: models.Checkout, _info):
-        return root.gift_cards.all()
+    def resolve_gift_cards(root: models.Checkout, info):
+        return GirtCardsByCheckoutIdLoader(info.context).load(root.pk)
 
     @staticmethod
     def resolve_is_shipping_required(root: models.Checkout, info: ResolveInfo):
