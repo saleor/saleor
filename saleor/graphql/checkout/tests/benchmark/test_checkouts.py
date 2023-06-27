7e1d3b37819380bdf3ca8d3b7413fbd9f51f3a27
Tomasz Szymanski <lime129@gmail.com>
2023-06-27 09:30:39 +0200

    Tests reformat

--- saleor/graphql/checkout/tests/benchmark/test_checkouts.py
+++ saleor/graphql/checkout/tests/benchmark/test_checkouts.py
@@ -28,10 +28,15 @@ def test_staff_multiple_checkouts(
     checkouts_for_benchmarks,
     count_queries,
 ):
+    # given
     staff_api_client.user.user_permissions.set(
         [permission_manage_checkouts, permission_manage_users]
     )
+
+    # when
     content = get_graphql_content(
         staff_api_client.post_graphql(MULTIPLE_CHECKOUT_DETAILS_QUERY)
     )
+
+    # then
     assert len(content["data"]["checkouts"]["edges"]) == 10
