df4918c811804737c091daae9339656d1d876474
fowczrek <f.a.owczarek@gmail.com>
2023-06-28 13:16:34 +0200

    Create prepare product function

--- saleor/tests/e2e/checkout/test_logged_in_customer_should_be_able_to_order_physical_product.py
+++ saleor/tests/e2e/checkout/test_logged_in_customer_should_be_able_to_order_physical_product.py
@@ -22,15 +22,14 @@ from .utils import (
 )
 
 
-def test_process_checkout_with_physical_product(
+def prepare_product(
     e2e_staff_api_client,
-    e2e_logged_api_client,
     permission_manage_products,
     permission_manage_channels,
     permission_manage_shipping,
     permission_manage_product_types_and_attributes,
+    channel_slug,
 ):
-    # Before
     permissions = [
         permission_manage_products,
         permission_manage_channels,
@@ -43,9 +42,10 @@ def test_process_checkout_with_physical_product(
     warehouse_id = warehouse_data["id"]
 
     warehouse_ids = [warehouse_id]
-    channel_data = create_channel(e2e_staff_api_client, warehouse_ids=warehouse_ids)
+    channel_data = create_channel(
+        e2e_staff_api_client, slug=channel_slug, warehouse_ids=warehouse_ids
+    )
     channel_id = channel_data["id"]
-    channel_slug = channel_data["slug"]
 
     channel_ids = [channel_id]
     shipping_zone_data = create_shipping_zone(
@@ -94,6 +94,27 @@ def test_process_checkout_with_physical_product(
         e2e_staff_api_client,
         product_variant_id,
         channel_id,
+    )
+    return product_variant_id
+
+
+def test_process_checkout_with_physical_product(
+    e2e_staff_api_client,
+    e2e_logged_api_client,
+    permission_manage_products,
+    permission_manage_channels,
+    permission_manage_shipping,
+    permission_manage_product_types_and_attributes,
+):
+    # Before
+    channel_slug = "test-channel"
+    product_variant_id = prepare_product(
+        e2e_staff_api_client,
+        permission_manage_products,
+        permission_manage_channels,
+        permission_manage_shipping,
+        permission_manage_product_types_and_attributes,
+        channel_slug,
     )
 
     # Step 1
