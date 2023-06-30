151fed5ab0159e2e5387a47c935e8bc905d9e1c1
Smit-Parmar <smitraj333@gmail.com>
2023-06-30 16:27:03 +0530

    pre-commit fix

--- saleor/channel/migrations/0013_auto_20230630_1039.py
+++ saleor/channel/migrations/0013_auto_20230630_1039.py
@@ -6,28 +6,41 @@ import saleor.core.utils.json_serializer
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('channel', '0012_channel_delete_expired_orders_after'),
+        ("channel", "0012_channel_delete_expired_orders_after"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='channel',
-            name='metadata',
-            field=models.JSONField(blank=True, default=dict, encoder=saleor.core.utils.json_serializer.CustomJsonEncoder, null=True),
+            model_name="channel",
+            name="metadata",
+            field=models.JSONField(
+                blank=True,
+                default=dict,
+                encoder=saleor.core.utils.json_serializer.CustomJsonEncoder,
+                null=True,
+            ),
         ),
         migrations.AddField(
-            model_name='channel',
-            name='private_metadata',
-            field=models.JSONField(blank=True, default=dict, encoder=saleor.core.utils.json_serializer.CustomJsonEncoder, null=True),
+            model_name="channel",
+            name="private_metadata",
+            field=models.JSONField(
+                blank=True,
+                default=dict,
+                encoder=saleor.core.utils.json_serializer.CustomJsonEncoder,
+                null=True,
+            ),
         ),
         migrations.AddIndex(
-            model_name='channel',
-            index=django.contrib.postgres.indexes.GinIndex(fields=['private_metadata'], name='channel_p_meta_idx'),
+            model_name="channel",
+            index=django.contrib.postgres.indexes.GinIndex(
+                fields=["private_metadata"], name="channel_p_meta_idx"
+            ),
         ),
         migrations.AddIndex(
-            model_name='channel',
-            index=django.contrib.postgres.indexes.GinIndex(fields=['metadata'], name='channel_meta_idx'),
+            model_name="channel",
+            index=django.contrib.postgres.indexes.GinIndex(
+                fields=["metadata"], name="channel_meta_idx"
+            ),
         ),
     ]
