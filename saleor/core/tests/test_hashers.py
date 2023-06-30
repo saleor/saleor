efc07f5277961b440f488632d28ccbb448c5176f
Diego Rasero <diego.rasero@breitling.com>
2023-06-30 15:51:20 +0200

    updated hash seed

--- saleor/core/tests/test_hashers.py
+++ saleor/core/tests/test_hashers.py
@@ -1,27 +1,14 @@
 import pytest
 from ..hashers import SHA512Base64PBKDF2PasswordHasher
 
-
-def test_migration_from_sha512_base64_to_sha512_base64_pbkdf2():
-    original_token = "GYZwGbUT4VFqNgejNlAabTyI0UAyK1nWm7Bs5ME5K9lIn47s4D43SucLFGor3DRnolbnI9QD7zZerO7hhLyCpQ=="
-    migrated_token = SHA512Base64PBKDF2PasswordHasher().PBKDF2_round(
-        password=original_token,
-        salt=SHA512Base64PBKDF2PasswordHasher().salt()
-    )
-    
-    assert SHA512Base64PBKDF2PasswordHasher().verify(
-        "Breitling123!",
-        migrated_token
-    )
-
 def test_encode_and_verify_sha512_base64_pbkdf2():
     generated_token = SHA512Base64PBKDF2PasswordHasher().encode(
-        "Breitling123!", salt=SHA512Base64PBKDF2PasswordHasher().salt()
+        "Saleor123!", salt=SHA512Base64PBKDF2PasswordHasher().salt()
     )
     
     assert 90 == len(generated_token)
     assert SHA512Base64PBKDF2PasswordHasher().verify(
-        "Breitling123!",
+        "Saleor123!",
         generated_token
     )
     
