cadbdcfefffea54375ffdf8c188919b222ad0d38
Anna Szczech <anna.m.szczech@gmail.com>
2023-07-04 12:29:36 +0200

    create product type

--- saleor/tests/e2e/checkout/test_unlogged_customer_should_be_able_to_buy_products_and_pick_up_from_selected_store.py
+++ saleor/tests/e2e/checkout/test_unlogged_customer_should_be_able_to_buy_products_and_pick_up_from_selected_store.py
@@ -1,6 +1,7 @@
 import pytest
 
 from ..channel.utils import create_channel
+from ..product.utils import create_product_type
 from ..utils import assign_permissions
 from ..warehouse.utils import create_warehouse, update_warehouse
 
@@ -10,12 +11,14 @@ def test_unlogged_customer_buy_by_click_and_collect(
     e2e_staff_api_client,
     permission_manage_products,
     permission_manage_channels,
+    permission_manage_product_types_and_attributes,
 ):
     assign_permissions(
         e2e_staff_api_client,
         [
             permission_manage_products,
             permission_manage_channels,
+            permission_manage_product_types_and_attributes,
         ],
     )
     warehouse_data = create_warehouse(e2e_staff_api_client)
@@ -25,6 +28,10 @@ def test_unlogged_customer_buy_by_click_and_collect(
         is_private=False,
         click_and_collect_option="LOCAL",
     )
-    channel_data = create_channel(e2e_staff_api_client, warehouse_data["id"])
+    _channel_data = create_channel(e2e_staff_api_client, warehouse_data["id"])
 
-    assert channel_data is not None
+    product_type_data = create_product_type(
+        e2e_staff_api_client,
+    )
+
+    assert product_type_data is not None
