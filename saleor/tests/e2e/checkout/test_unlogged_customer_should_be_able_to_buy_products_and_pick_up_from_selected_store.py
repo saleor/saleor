3f70511e74a47b7d4a39a9fe57958710a87d7c60
Anna Szczech <anna.m.szczech@gmail.com>
2023-07-04 12:33:40 +0200

    create category

--- saleor/tests/e2e/checkout/test_unlogged_customer_should_be_able_to_buy_products_and_pick_up_from_selected_store.py
+++ saleor/tests/e2e/checkout/test_unlogged_customer_should_be_able_to_buy_products_and_pick_up_from_selected_store.py
@@ -1,7 +1,7 @@
 import pytest
 
 from ..channel.utils import create_channel
-from ..product.utils import create_product_type
+from ..product.utils import create_category, create_product_type
 from ..utils import assign_permissions
 from ..warehouse.utils import create_warehouse, update_warehouse
 
@@ -30,8 +30,12 @@ def test_unlogged_customer_buy_by_click_and_collect(
     )
     _channel_data = create_channel(e2e_staff_api_client, warehouse_data["id"])
 
-    product_type_data = create_product_type(
+    _product_type_data = create_product_type(
         e2e_staff_api_client,
     )
 
-    assert product_type_data is not None
+    category_data = create_category(
+        e2e_staff_api_client,
+    )
+
+    assert category_data is not None
