85d4ce68093e9858f3acdd856baca4b45ed4b372
Anna Szczech <anna.m.szczech@gmail.com>
2023-07-04 12:22:01 +0200

    create channel

--- saleor/tests/e2e/checkout/test_unlogged_customer_should_be_able_to_buy_products_and_pick_up_from_selected_store.py
+++ saleor/tests/e2e/checkout/test_unlogged_customer_should_be_able_to_buy_products_and_pick_up_from_selected_store.py
@@ -1,16 +1,22 @@
 import pytest
 
+from ..channel.utils import create_channel
 from ..utils import assign_permissions
 from ..warehouse.utils import create_warehouse, update_warehouse
 
 
 @pytest.mark.e2e
 def test_unlogged_customer_buy_by_click_and_collect(
-    e2e_staff_api_client, permission_manage_products
+    e2e_staff_api_client,
+    permission_manage_products,
+    permission_manage_channels,
 ):
     assign_permissions(
         e2e_staff_api_client,
-        [permission_manage_products],
+        [
+            permission_manage_products,
+            permission_manage_channels,
+        ],
     )
     warehouse_data = create_warehouse(e2e_staff_api_client)
     update_warehouse(
@@ -19,5 +25,6 @@ def test_unlogged_customer_buy_by_click_and_collect(
         is_private=False,
         click_and_collect_option="LOCAL",
     )
+    channel_data = create_channel(e2e_staff_api_client, warehouse_data["id"])
 
-    assert warehouse_data is not None
+    assert channel_data is not None
