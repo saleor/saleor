f00b8f27b53dd70d34fa819697126a7592077182
Piotr Zabieglik <55899043+zedzior@users.noreply.github.com>
2023-07-06 10:08:15 +0200

    Add `search` to giftcard query (#13173)
    * Update GiftCard model and add search.py file to giftcard app.
    
    * Add search to gift_cards query; add celery task updating search vector.
    
    * Code refactor; process batches in separate celery tasks.
    
    * Mark gift card search index as dirty when relevant fields updated.
    
    * Correct search_vectors preparing; add index to the model.
    
    * Add test.
    
    * Update changelog.
    
    * Correct test.
    
    * Correct migration, which adds an index.
    
    * Correct migration, batch size and descriptions.
    
    * Config celery task.
    
    * Set default value for 'search_index_dirty'; update description.
    
    * Update search vector when user data has changed.
    
    * Correct if condition.
    
    * Rafactor code.
    
    ---------
    
    Co-authored-by: Marcin Gębala <5421321+maarcingebala@users.noreply.github.com>

--- saleor/giftcard/tasks.py
+++ saleor/giftcard/tasks.py
@@ -1,11 +1,14 @@
 from celery.utils.log import get_task_logger
+from django.conf import settings
 from django.utils import timezone
 
 from ..celeryconf import app
 from .events import gift_cards_deactivated_event
 from .models import GiftCard
+from .search import update_gift_cards_search_vector
 
 task_logger = get_task_logger(__name__)
+GIFT_CARD_BATCH_SIZE = 300
 
 
 @app.task
@@ -18,3 +21,16 @@ def deactivate_expired_cards_task():
     count = gift_cards.update(is_active=False)
     gift_cards_deactivated_event(gift_card_ids, user=None, app=None)
     task_logger.debug("Deactivate %s gift cards", count)
+
+
+@app.task(
+    queue=settings.UPDATE_SEARCH_VECTOR_INDEX_QUEUE_NAME,
+    expires=settings.BEAT_UPDATE_SEARCH_EXPIRE_AFTER_SEC,
+)
+def update_gift_cards_search_vector_task():
+    gift_cards = GiftCard.objects.filter(search_index_dirty=True)
+    if not gift_cards:
+        return
+    gift_cards_batch = list(gift_cards[:GIFT_CARD_BATCH_SIZE])
+    update_gift_cards_search_vector(gift_cards_batch)
+    update_gift_cards_search_vector_task.delay()
