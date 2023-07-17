b0d5ec1a55f2ceeba6f62cf15f53faea0adf93f9
Anna Szczęch <30683248+szczecha@users.noreply.github.com>
2023-07-17 09:05:36 +0300

    [TEST] Should not be able to buy a product unavailable for purchase  (#13390)
    * add test for buying unavailable products
    
    * rename file
    
    * update function for product channel listing

--- saleor/tests/e2e/product/utils/__init__.py
+++ saleor/tests/e2e/product/utils/__init__.py
@@ -1,7 +1,10 @@
 from .category import create_category
 from .digital_content import create_digital_content
 from .product import create_product
-from .product_channel_listing import create_product_channel_listing
+from .product_channel_listing import (
+    create_product_channel_listing,
+    raw_create_product_channel_listing,
+)
 from .product_type import create_product_type
 from .product_variant import create_product_variant
 from .product_variant_channel_listing import create_product_variant_channel_listing
@@ -14,4 +17,5 @@ __all__ = [
     "create_product_variant_channel_listing",
     "create_product_variant",
     "create_product",
+    "raw_create_product_channel_listing",
 ]
