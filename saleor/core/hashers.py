2604e946a17512a2d7960d5d453279ba933fc1fd
Diego Rasero <diego.rasero@breitling.com>
2023-07-18 09:36:28 +0200

    Fixing style

--- saleor/core/hashers.py
+++ saleor/core/hashers.py
@@ -1,13 +1,14 @@
 import base64
 import hashlib
+
 from django.contrib.auth.hashers import PBKDF2PasswordHasher
 from django.utils.encoding import force_bytes
 
 
 class SHA512Base64PBKDF2PasswordHasher(PBKDF2PasswordHasher):
-    """
-        Symfony 5000 iterations of sha512 plus base64 encoder, with an extra PBKDF2 round.
-    """
+    """Symfony 5000 iterations of sha512 plus base64 encoder, with an extra
+    PBKDF2 round."""
+
     algorithm = "sha512b64pbkdf2"
     iterations = 690000
 
@@ -15,10 +16,14 @@ class SHA512Base64PBKDF2PasswordHasher(PBKDF2PasswordHasher):
         symfony_iterations = 5000
         digest = hashlib.sha512(force_bytes(password, encoding="ISO-8859-1")).digest()
         for i in range(symfony_iterations - 1):
-            digest = hashlib.sha512(digest + force_bytes(password, encoding="ISO-8859-1")).digest()
+            digest = hashlib.sha512(
+                digest + force_bytes(password, encoding="ISO-8859-1")
+            ).digest()
         iterations = iterations or self.iterations
-        return self.PBKDF2_round(base64.b64encode(digest).decode("ISO-8859-1"), salt, iterations)
+        return self.PBKDF2_round(
+            base64.b64encode(digest).decode("ISO-8859-1"), salt, iterations
+        )
 
     def PBKDF2_round(self, password, salt, iterations=None):
-        """ PBKDF2 round (salt + secure hash function added) """
+        """PBKDF2 round (salt + secure hash function added)."""
         return super().encode(password, salt, iterations)
