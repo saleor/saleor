cb1a470c5db16923a559128172b3e3176ba68c5a
Maciej Korycinski <korycinski.maciej@gmail.com>
2023-07-25 11:53:31 +0200

    Add one more test with app that doesnt return correct response

--- saleor/plugins/webhook/tests/test_list_payment_methods_webhook.py
+++ saleor/plugins/webhook/tests/test_list_payment_methods_webhook.py
@@ -213,3 +213,61 @@ def test_list_stored_payment_methods_uses_cache_if_available(
     assert response == get_list_stored_payment_methods_from_response(
         list_stored_payment_methods_app, webhook_list_stored_payment_methods_response
     )
+
+
+@mock.patch("saleor.plugins.webhook.tasks.cache.set")
+@mock.patch("saleor.plugins.webhook.tasks.cache.get")
+@mock.patch("saleor.plugins.webhook.tasks.send_webhook_request_sync")
+def test_list_stored_payment_methods_app_returns_incorrect_response(
+    mock_request,
+    mocked_cache_get,
+    mocked_cache_set,
+    channel_USD,
+    customer_user,
+    webhook_plugin,
+    list_stored_payment_methods_app,
+    webhook_list_stored_payment_methods_response,
+):
+    # given
+    mock_request.return_value = None
+    mocked_cache_get.return_value = None
+
+    webhook = list_stored_payment_methods_app.webhooks.first()
+    webhook.subscription_query = LIST_STORED_PAYMENT_METHODS
+    webhook.save()
+
+    plugin = webhook_plugin()
+    amount = Decimal(100)
+    currency = "USD"
+    data = ListStoredPaymentMethodsRequestData(
+        channel=channel_USD,
+        user=customer_user,
+        amount=Money(amount, currency),
+    )
+
+    expected_payload = {
+        "user_id": graphene.Node.to_global_id("User", customer_user.pk),
+        "channel_slug": channel_USD.slug,
+        "currency": currency,
+        "amount": str(amount),
+    }
+    expected_cache_key = generate_cache_key_for_webhook(
+        expected_payload,
+        webhook.target_url,
+        WebhookEventSyncType.LIST_STORED_PAYMENT_METHODS,
+        list_stored_payment_methods_app.id,
+    )
+
+    # when
+    response = plugin.list_stored_payment_methods(data, [])
+
+    # then
+    delivery = EventDelivery.objects.get()
+    mock_request.assert_called_once_with(
+        list_stored_payment_methods_app, delivery, timeout=WEBHOOK_SYNC_TIMEOUT
+    )
+
+    mocked_cache_get.assert_called_once_with(expected_cache_key)
+    assert not mocked_cache_set.called
+
+    assert response == []
