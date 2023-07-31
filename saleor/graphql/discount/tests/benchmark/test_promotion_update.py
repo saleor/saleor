c2e44aa23e44da013560d6a73986de777832bbc3
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-07-31 11:08:36 +0200

    Add benchmark for promotions queries and mutations (#13552)
    * Add benchmark for promotions query
    
    * Add benchmark for PromotionCreate
    
    * Add benchmarks for rest of promotion mutations

---
+++ saleor/graphql/discount/tests/benchmark/test_promotion_update.py
@@ -0,0 +1,45 @@
+from datetime import timedelta
+
+import graphene
+import pytest
+from django.utils import timezone
+
+from ....tests.utils import get_graphql_content
+from ..mutations.test_promotion_update import PROMOTION_UPDATE_MUTATION
+
+
+@pytest.mark.django_db
+@pytest.mark.count_queries(autouse=False)
+def test_promotion_update(
+    staff_api_client,
+    description_json,
+    permission_group_manage_discounts,
+    promotion,
+    count_queries,
+):
+    # given
+    staff_api_client.user.groups.add(permission_group_manage_discounts)
+    start_date = timezone.now() - timedelta(days=30)
+    end_date = timezone.now() + timedelta(days=30)
+
+    variables = {
+        "id": graphene.Node.to_global_id("Promotion", promotion.id),
+        "input": {
+            "name": "Promotion",
+            "description": description_json,
+            "startDate": start_date.isoformat(),
+            "endDate": end_date.isoformat(),
+        },
+    }
+
+    # when
+    content = get_graphql_content(
+        staff_api_client.post_graphql(
+            PROMOTION_UPDATE_MUTATION,
+            variables,
+        )
+    )
+
+    # then
+    data = content["data"]["promotionUpdate"]
+    assert data["promotion"]
