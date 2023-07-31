595c7ea211c8096ffa96ad4d6c3513a9a9f65dc7
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-07-31 13:06:34 +0200

    Handle promotion toggle (#13539)
    * Add handle_promotion_toggle discount task
    
    * Add promotion schedule
    
    * Add promotion schedule
    
    * Adjust handle_promotion_toggle task
    
    * Call sale_toggle webhooks as well in handle_promotion_toggle

--- saleor/discount/tests/test_tasks.py
+++ saleor/discount/tests/test_tasks.py
@@ -1,155 +1,179 @@
-from collections import defaultdict
 from datetime import timedelta
+from decimal import Decimal
 from unittest.mock import ANY, patch
 
 import graphene
 from django.utils import timezone
 from freezegun import freeze_time
 
-from ..models import Sale
-from ..tasks import fetch_catalogue_infos, handle_sale_toggle
+from .. import RewardValueType
+from ..models import Promotion, PromotionRule
+from ..tasks import fetch_promotion_variants_and_product_ids, handle_promotion_toggle
 
 
-def test_fetch_catalogue_infos(sale, new_sale):
+def test_fetch_promotion_variants_and_product_ids(
+    promotion_list, product, collection, product_list
+):
     # given
-    sales = Sale.objects.all()
+    promotions = Promotion.objects.all()
+
+    collection.products.set(product_list[:2])
+
+    product_variants = set(product.variants.values_list("id", flat=True))
+    collection_variants = {
+        id
+        for product in product_list[:2]
+        for id in product.variants.values_list("id", flat=True)
+    }
 
     # when
-    sale_id_to_catalogue_infos, catalogue_infos = fetch_catalogue_infos(sales)
+    promotion_id_to_variants, product_ids = fetch_promotion_variants_and_product_ids(
+        promotions
+    )
 
     # then
-    expected_catalogue_info = defaultdict(set)
-    for sale_instance in sales:
-        catalogue_info = sale_id_to_catalogue_infos[sale_instance.id]
-        category_ids = sale_instance.categories.all().values_list("id", flat=True)
-        collection_ids = sale_instance.collections.all().values_list("id", flat=True)
-        product_ids = sale_instance.products.all().values_list("id", flat=True)
-        variant_ids = sale_instance.variants.all().values_list("id", flat=True)
-        assert catalogue_info["categories"] == set(
-            graphene.Node.to_global_id("Category", id) for id in category_ids
-        )
-        assert catalogue_info["collections"] == set(
-            graphene.Node.to_global_id("Collection", id) for id in collection_ids
-        )
-        assert catalogue_info["products"] == set(
-            graphene.Node.to_global_id("Product", id) for id in product_ids
-        )
-        assert catalogue_info["variants"] == set(
-            graphene.Node.to_global_id("ProductVariant", id) for id in variant_ids
-        )
-        expected_catalogue_info["categories"].update(set(category_ids))
-        expected_catalogue_info["collections"].update(set(collection_ids))
-        expected_catalogue_info["products"].update(set(product_ids))
-        expected_catalogue_info["variants"].update(set(variant_ids))
+    expected_product_ids = {product.id, product_list[0].id, product_list[1].id}
+    assert set(product_ids) == expected_product_ids
 
-    assert catalogue_infos == expected_catalogue_info
+    variants_promo_1 = promotion_id_to_variants[promotion_list[0].id]
+    variants_promo_2 = promotion_id_to_variants[promotion_list[1].id]
+    variants_promo_3 = promotion_id_to_variants[promotion_list[2].id]
+
+    assert {variant.id for variant in variants_promo_1} == product_variants.union(
+        collection_variants
+    )
+    assert {variant.id for variant in variants_promo_2} == product_variants
+    assert {variant.id for variant in variants_promo_3} == collection_variants
 
 
 @freeze_time("2020-03-18 12:00:00")
 @patch(
-    "saleor.product.tasks.update_products_discounted_prices_of_catalogues_task.delay"
+    "saleor.product.tasks.update_products_discounted_prices_for_promotion_task.delay"
 )
 @patch("saleor.plugins.manager.PluginsManager.sale_toggle")
-def test_handle_sale_toggle(
+@patch("saleor.plugins.manager.PluginsManager.promotion_ended")
+@patch("saleor.plugins.manager.PluginsManager.promotion_started")
+def test_handle_promotion_toggle(
+    promotion_started_mock,
+    promotion_ended_mock,
     sale_toggle_mock,
-    mock_update_products_discounted_prices_of_catalogues,
-    collection_list,
+    mock_update_products_discounted_prices_for_promotion_task,
     product_list,
-    category,
 ):
     # given
     now = timezone.now()
-    sales = Sale.objects.bulk_create([Sale(name=f"Sale-{i}") for i in range(10)])
+    promotions = Promotion.objects.bulk_create(
+        [Promotion(name=f"Promotion-{i}") for i in range(10)]
+    )
 
-    variant_ids = []
-    for collection, product, sale in zip(collection_list, product_list, sales):
-        sale.collections.add(collection)
-        sale.products.add(product)
-        variant = product.variants.first()
-        sale.variants.add(variant)
-        variant_ids.append(variant.id)
+    rules = []
+    for promotion, product in zip(promotions, product_list):
+        rules.append(
+            PromotionRule(
+                promotion=promotion,
+                catalogue_predicate={
+                    "productPredicate": {
+                        "ids": [graphene.Node.to_global_id("Product", product.id)]
+                    }
+                },
+                reward_value_type=RewardValueType.FIXED,
+                reward_value=Decimal("5"),
+            )
+        )
 
-    sales[7].categories.add(category)
+    PromotionRule.objects.bulk_create(rules)
 
-    # sales with start date before current date
-    # without notification sent day - should be sent
-    sales[0].start_date = now - timedelta(days=1)
-    sales[0].notification_sent_datetime = None
-
-    # with notification sent day after the start date - shouldn't be sent
-    sales[1].start_date = now - timedelta(days=1)
-    sales[1].notification_sent_datetime = now - timedelta(minutes=2)
-
-    # with notification sent day before the start date - should be sent
-    sales[2].start_date = now - timedelta(minutes=2)
-    sales[2].notification_sent_datetime = now - timedelta(minutes=5)
-
-    # sales with start date after current date - shouldn't be sent
-    # without notification sent day
-    sales[3].start_date = now + timedelta(days=1)
-    sales[3].notification_sent_datetime = None
-
-    # with notification sent day before the start date
-    sales[4].start_date = now + timedelta(days=1)
-    sales[4].notification_sent_datetime = now - timedelta(minutes=5)
-
-    # sales with end date before current date
-    # without notification sent day - should be sent
-    sales[5].end_date = now - timedelta(days=1)
-    sales[5].notification_sent_datetime = None
+    # promotions with start date before current date
+    # without notification sent day - should be sent for started
+    promotions[0].start_date = now - timedelta(days=1)
+    promotions[0].last_notification_scheduled_at = None
 
     # with notification sent day after the start date - shouldn't be sent
-    sales[6].start_date = now - timedelta(days=2)
-    sales[6].end_date = now - timedelta(days=1)
-    sales[6].notification_sent_datetime = now - timedelta(minutes=2)
+    promotions[1].start_date = now - timedelta(days=1)
+    promotions[1].last_notification_scheduled_at = now - timedelta(minutes=2)
 
-    # with notification sent day before the start date - should be sent
-    sales[7].start_date = now - timedelta(days=2)
-    sales[7].end_date = now - timedelta(minutes=2)
-    sales[7].notification_sent_datetime = now - timedelta(minutes=5)
+    # with notification sent day before the start date - should be sent for started
+    promotions[2].start_date = now - timedelta(minutes=2)
+    promotions[2].last_notification_scheduled_at = now - timedelta(minutes=5)
 
-    # sales with end date after current date
     # without notification sent day
-    sales[8].start_date = now + timedelta(days=2)
-    sales[8].end_date = now + timedelta(days=1)
-    sales[8].notification_sent_datetime = None
+    # promotions with start date after current date - shouldn't be sent
+    promotions[3].start_date = now + timedelta(days=1)
+    promotions[3].last_notification_scheduled_at = None
 
     # with notification sent day before the start date
-    sales[9].start_date = now + timedelta(days=2)
-    sales[9].end_date = now + timedelta(days=1)
-    sales[9].notification_sent_datetime = now - timedelta(minutes=5)
+    promotions[4].start_date = now + timedelta(days=1)
+    promotions[4].last_notification_scheduled_at = now - timedelta(minutes=5)
 
-    Sale.objects.bulk_update(
-        sales,
+    # promotions with end date before current date
+    # without notification sent day - should be sent for ended
+    promotions[5].start_date = now - timedelta(days=2)
+    promotions[5].end_date = now - timedelta(days=1)
+    promotions[5].last_notification_scheduled_at = None
+
+    # with notification sent day after the start date - shouldn't be sent
+    promotions[6].start_date = now - timedelta(days=2)
+    promotions[6].end_date = now - timedelta(days=1)
+    promotions[6].last_notification_scheduled_at = now - timedelta(minutes=2)
+
+    # with notification sent day before the start date - should be sent for ended
+    promotions[7].start_date = now - timedelta(days=2)
+    promotions[7].end_date = now - timedelta(minutes=2)
+    promotions[7].last_notification_scheduled_at = now - timedelta(minutes=5)
+
+    # promotions with end date after current date
+    # without notification sent day
+    promotions[8].start_date = now + timedelta(days=2)
+    promotions[8].end_date = now + timedelta(days=1)
+    promotions[8].last_notification_scheduled_at = None
+
+    # with notification sent day before the start date
+    promotions[9].start_date = now + timedelta(days=2)
+    promotions[9].end_date = now + timedelta(days=1)
+    promotions[9].last_notification_scheduled_at = now - timedelta(minutes=5)
+
+    Promotion.objects.bulk_update(
+        promotions,
         [
             "start_date",
             "end_date",
-            "notification_sent_datetime",
+            "last_notification_scheduled_at",
         ],
     )
-    indexes_of_toggle_sales = [0, 2, 5, 7]
+    indexes_of_started_promotions = {0, 2, 5}
+    indexes_of_ended_promotions = {5, 7}
+    indexes_of_toggle_sales = indexes_of_started_promotions.union(
+        indexes_of_ended_promotions
+    )
 
     # when
-    handle_sale_toggle()
+    handle_promotion_toggle()
 
     # then
-    assert sale_toggle_mock.call_count == 4
+    assert promotion_started_mock.call_count == len(indexes_of_started_promotions)
+    assert promotion_ended_mock.call_count == len(indexes_of_ended_promotions)
+    assert sale_toggle_mock.call_count == len(indexes_of_toggle_sales)
 
-    started_args_list = [args.args for args in sale_toggle_mock.call_args_list]
+    started_args_list = [args.args for args in promotion_started_mock.call_args_list]
+    for index in indexes_of_started_promotions:
+        assert (promotions[index],) in started_args_list
+
+    ended_args_list = [args.args for args in promotion_ended_mock.call_args_list]
+    for index in indexes_of_ended_promotions:
+        assert (promotions[index],) in ended_args_list
+
+    toggle_args_list = [args.args for args in sale_toggle_mock.call_args_list]
     for index in indexes_of_toggle_sales:
-        assert (sales[index], ANY) in started_args_list
+        assert (promotions[index], ANY) in toggle_args_list
 
-    for index in indexes_of_toggle_sales:
-        sales[index].refresh_from_db()
-        assert sales[index].notification_sent_datetime == now
+    for index in indexes_of_started_promotions | indexes_of_ended_promotions:
+        promotions[index].refresh_from_db()
+        assert promotions[index].last_notification_scheduled_at == now
 
-    mock_update_products_discounted_prices_of_catalogues.assert_called_once()
-    args, kwargs = mock_update_products_discounted_prices_of_catalogues.call_args
-    # get ids of instances assigned to sales that toggle
-    assert set(kwargs["product_ids"]) == {product_list[0].id, product_list[2].id}
-    assert set(kwargs["category_ids"]) == {category.id}
-    assert set(kwargs["collection_ids"]) == {
-        collection_list[0].id,
-        collection_list[2].id,
+    mock_update_products_discounted_prices_for_promotion_task.assert_called_once()
+    args, kwargs = mock_update_products_discounted_prices_for_promotion_task.call_args
+    # get ids of instances assigned to promotions that toggle
+    assert {product_id for product_id in args[0]} == {
+        product_list[0].id,
+        product_list[2].id,
     }
-    assert set(kwargs["variant_ids"]) == {variant_ids[0], variant_ids[2]}
