4754404a4499e3da31651696ed6b26b25e1f415c
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-07-31 13:06:34 +0200

    Handle promotion toggle (#13539)
    * Add handle_promotion_toggle discount task
    
    * Add promotion schedule
    
    * Add promotion schedule
    
    * Adjust handle_promotion_toggle task
    
    * Call sale_toggle webhooks as well in handle_promotion_toggle

--- saleor/tests/fixtures.py
+++ saleor/tests/fixtures.py
@@ -5479,7 +5479,11 @@ def promotion_list(channel_USD, product, collection):
                 description=dummy_editorjs(
                     "Test description for promotion 1 percentage rule."
                 ),
-                catalogue_predicate={"productPredicate": {"ids": [product.id]}},
+                catalogue_predicate={
+                    "productPredicate": {
+                        "ids": [graphene.Node.to_global_id("Product", product.id)]
+                    }
+                },
                 reward_value_type=RewardValueType.PERCENTAGE,
                 reward_value=Decimal("10"),
             ),
@@ -5489,7 +5493,11 @@ def promotion_list(channel_USD, product, collection):
                 description=dummy_editorjs(
                     "Test description for promotion 1 fixed rule."
                 ),
-                catalogue_predicate={"collectionPredicate": {"ids": [collection.id]}},
+                catalogue_predicate={
+                    "collectionPredicate": {
+                        "ids": [graphene.Node.to_global_id("Collection", collection.id)]
+                    }
+                },
                 reward_value_type=RewardValueType.FIXED,
                 reward_value=Decimal("5"),
             ),
@@ -5499,7 +5507,11 @@ def promotion_list(channel_USD, product, collection):
                 description=dummy_editorjs(
                     "Test description for promotion 2 percentage rule."
                 ),
-                catalogue_predicate={"productPredicate": {"ids": [product.id]}},
+                catalogue_predicate={
+                    "productPredicate": {
+                        "ids": [graphene.Node.to_global_id("Product", product.id)]
+                    }
+                },
                 reward_value_type=RewardValueType.PERCENTAGE,
                 reward_value=Decimal("10"),
             ),
@@ -5509,7 +5521,11 @@ def promotion_list(channel_USD, product, collection):
                 description=dummy_editorjs(
                     "Test description for promotion 3 fixed rule."
                 ),
-                catalogue_predicate={"collectionPredicate": {"ids": [collection.id]}},
+                catalogue_predicate={
+                    "collectionPredicate": {
+                        "ids": [graphene.Node.to_global_id("Collection", collection.id)]
+                    }
+                },
                 reward_value_type=RewardValueType.FIXED,
                 reward_value=Decimal("5"),
             ),
