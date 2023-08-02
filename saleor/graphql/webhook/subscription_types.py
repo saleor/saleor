7598dca02ae7ad0173c5e88350fe3824c6e33fa4
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-08-02 09:06:59 +0200

    Depracate sale queries and mutations (#13580)
    * Deprecate sale mutatios
    
    * Deprecate sale queries
    
    * Update description for SaleChannelListingUpdate
    
    * Deprecate sales events and types

--- saleor/graphql/webhook/subscription_types.py
+++ saleor/graphql/webhook/subscription_types.py
@@ -42,6 +42,7 @@ from ..core.descriptions import (
     ADDED_IN_313,
     ADDED_IN_314,
     ADDED_IN_315,
+    DEPRECATED_IN_3X_EVENT,
     PREVIEW_FEATURE,
 )
 from ..core.doc_category import (
@@ -1037,7 +1038,12 @@ class SaleCreated(SubscriptionObjectType, SaleBase):
         root_type = "Sale"
         enable_dry_run = True
         interfaces = (Event,)
-        description = "Event sent when new sale is created." + ADDED_IN_32
+        description = (
+            "Event sent when new sale is created."
+            + ADDED_IN_32
+            + DEPRECATED_IN_3X_EVENT
+            + " Use `PromotionCreated` event instead."
+        )
 
 
 class SaleUpdated(SubscriptionObjectType, SaleBase):
@@ -1045,7 +1051,12 @@ class SaleUpdated(SubscriptionObjectType, SaleBase):
         root_type = "Sale"
         enable_dry_run = True
         interfaces = (Event,)
-        description = "Event sent when sale is updated." + ADDED_IN_32
+        description = (
+            "Event sent when sale is updated."
+            + ADDED_IN_32
+            + DEPRECATED_IN_3X_EVENT
+            + " Use `PromotionUpdated` event instead."
+        )
 
 
 class SaleDeleted(SubscriptionObjectType, SaleBase):
@@ -1053,7 +1064,12 @@ class SaleDeleted(SubscriptionObjectType, SaleBase):
         root_type = "Sale"
         enable_dry_run = True
         interfaces = (Event,)
-        description = "Event sent when sale is deleted." + ADDED_IN_32
+        description = (
+            "Event sent when sale is deleted."
+            + ADDED_IN_32
+            + DEPRECATED_IN_3X_EVENT
+            + " Use `PromotionDeleted` event instead."
+        )
 
 
 class SaleToggle(SubscriptionObjectType, SaleBase):
@@ -1069,7 +1085,10 @@ class SaleToggle(SubscriptionObjectType, SaleBase):
         root_type = "Sale"
         enable_dry_run = True
         description = (
-            "The event informs about the start or end of the sale." + ADDED_IN_35
+            "The event informs about the start or end of the sale."
+            + ADDED_IN_35
+            + DEPRECATED_IN_3X_EVENT
+            + " Use `PromotionStarted` and `PromotionEnded` events instead."
         )
         interfaces = (Event,)
 
