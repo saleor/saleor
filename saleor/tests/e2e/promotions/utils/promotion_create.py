18d63f6dee8671dfd590244484c53aeedee555be
Anna Szczęch <30683248+szczecha@users.noreply.github.com>
2023-08-10 09:20:15 +0300

    [TEST] Checkout products on fixed promotion (#13649)
    * add creating promotion without rules
    
    * move promotion create to utils
    
    * add create promotion rules utils
    
    * add firest step to test
    
    * add rest steps
    
    * move before to separate function
    
    * add test case id
    
    * change net to gross
    
    * add assertion for subtotal

---
+++ saleor/tests/e2e/promotions/utils/promotion_create.py
@@ -0,0 +1,38 @@
+from ...utils import get_graphql_content
+
+PROMOTION_CREATE_MUTATION = """
+mutation CreatePromotion($input: PromotionCreateInput!) {
+  promotionCreate(input: $input) {
+    errors {
+      message
+      field
+      code
+    }
+    promotion {
+      id
+      name
+      startDate
+      endDate
+    }
+  }
+}
+"""
+
+
+def create_promotion(
+    staff_api_client,
+    promotion_name,
+):
+    variables = {"input": {"name": promotion_name}}
+
+    response = staff_api_client.post_graphql(
+        PROMOTION_CREATE_MUTATION,
+        variables,
+    )
+    content = get_graphql_content(response)
+
+    assert content["data"]["promotionCreate"]["errors"] == []
+
+    data = content["data"]["promotionCreate"]["promotion"]
+    assert data["id"] is not None
+    return data
