9493f434ea9045a4921b270fa752031427fd64a5
Anna Szczech <anna.m.szczech@gmail.com>
2023-08-11 11:34:01 +0200

    add test for promotion rule for collection

--- saleor/tests/e2e/product/utils/__init__.py
+++ saleor/tests/e2e/product/utils/__init__.py
@@ -1,10 +1,13 @@
 from .category import create_category
+from .collection import create_collection
+from .collection_listing_update import create_collection_channel_listing
 from .digital_content import create_digital_content
 from .product import create_product
 from .product_channel_listing import (
     create_product_channel_listing,
     raw_create_product_channel_listing,
 )
+from .product_query import get_product
 from .product_type import create_product_type
 from .product_variant import create_product_variant, raw_create_product_variant
 from .product_variant_channel_listing import create_product_variant_channel_listing
@@ -20,4 +23,7 @@ __all__ = [
     "raw_create_product_variant",
     "create_product",
     "raw_create_product_channel_listing",
+    "create_collection",
+    "create_collection_channel_listing",
+    "get_product",
 ]
