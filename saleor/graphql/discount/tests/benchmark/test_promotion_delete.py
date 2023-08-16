7568edd45f0844418a4871d0da104ba93b9624e4
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-08-16 12:49:32 +0200

    Fix promotionDelete mutation (#13674)

--- saleor/graphql/discount/tests/benchmark/test_promotion_delete.py
+++ saleor/graphql/discount/tests/benchmark/test_promotion_delete.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 import graphene
 import pytest
 
@@ -9,13 +7,7 @@ from ..mutations.test_promotion_delete import PROMOTION_DELETE_MUTATION
 
 @pytest.mark.django_db
 @pytest.mark.count_queries(autouse=False)
-@patch(
-    "saleor.product.tasks.update_products_discounted_prices_for_promotion_task.delay"
-)
-@patch("saleor.plugins.manager.PluginsManager.promotion_deleted")
 def test_promotion_delete(
-    promotion_deleted_mock,
-    update_products_discounted_prices_for_promotion_task_mock,
     staff_api_client,
     permission_group_manage_discounts,
     promotion,
