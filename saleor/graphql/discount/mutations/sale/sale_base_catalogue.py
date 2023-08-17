d90a7775947aed3230df2b206ecc3e1697bce3f3
zedzior <piotr.zabieglik@gmail.com>
2023-08-17 10:39:18 +0200

    Fix price recaulculating tests.

--- saleor/graphql/discount/mutations/sale/sale_base_catalogue.py
+++ saleor/graphql/discount/mutations/sale/sale_base_catalogue.py
@@ -1,3 +1,5 @@
+import copy
+
 import graphene
 
 from .....product.tasks import update_products_discounted_prices_for_promotion_task
@@ -42,8 +44,10 @@ class SaleBaseCatalogueMutation(BaseDiscountCatalogueMutation):
 
         previous_predicate = convert_catalogue_info_into_predicate(previous_catalogue)
         new_predicate = convert_catalogue_info_into_predicate(new_catalogue)
-        previous_product_ids = get_product_ids_for_predicate(previous_predicate)
-        new_product_ids = get_product_ids_for_predicate(new_predicate)
+        previous_product_ids = get_product_ids_for_predicate(
+            copy.deepcopy(previous_predicate)
+        )
+        new_product_ids = get_product_ids_for_predicate(copy.deepcopy(new_predicate))
 
         if previous_product_ids != new_product_ids:
             product_ids = previous_product_ids | new_product_ids
