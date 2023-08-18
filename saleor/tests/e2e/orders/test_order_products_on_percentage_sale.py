983633e872239a9e908e72c07828bb52097f8201
Anna Szczech <anna.m.szczech@gmail.com>
2023-08-18 14:31:18 +0200

    add test for products on percentage promotion in order

---
+++ saleor/tests/e2e/orders/test_order_products_on_percentage_sale.py
@@ -0,0 +1,210 @@
+import pytest
+
+from .. import DEFAULT_ADDRESS
+from ..channel.utils import create_channel
+from ..product.utils import (
+    create_category,
+    create_product,
+    create_product_channel_listing,
+    create_product_type,
+    create_product_variant,
+    create_product_variant_channel_listing,
+)
+from ..sales.utils import create_sale, create_sale_channel_listing, sale_catalogues_add
+from ..shipping_zone.utils import (
+    create_shipping_method,
+    create_shipping_method_channel_listing,
+    create_shipping_zone,
+)
+from ..utils import assign_permissions
+from ..warehouse.utils import create_warehouse
+from .utils import (
+    draft_order_complete,
+    draft_order_create,
+    draft_order_update,
+    order_lines_create,
+)
+
+
+def prepare_product(
+    e2e_staff_api_client,
+    permission_manage_products,
+    permission_manage_channels,
+    permission_manage_shipping,
+    permission_manage_product_types_and_attributes,
+    permission_manage_discounts,
+    permission_manage_orders,
+    channel_slug,
+    variant_price,
+    sale_name,
+    discount_type,
+    discount_value,
+):
+    permissions = [
+        permission_manage_products,
+        permission_manage_channels,
+        permission_manage_shipping,
+        permission_manage_product_types_and_attributes,
+        permission_manage_discounts,
+        permission_manage_orders,
+    ]
+    assign_permissions(e2e_staff_api_client, permissions)
+
+    warehouse_data = create_warehouse(e2e_staff_api_client)
+    warehouse_id = warehouse_data["id"]
+    warehouse_ids = [warehouse_id]
+
+    channel_data = create_channel(
+        e2e_staff_api_client,
+        warehouse_ids,
+        slug=channel_slug,
+    )
+    channel_id = channel_data["id"]
+    channel_ids = [channel_id]
+
+    shipping_zone_data = create_shipping_zone(
+        e2e_staff_api_client,
+        warehouse_ids=warehouse_ids,
+        channel_ids=channel_ids,
+    )
+    shipping_zone_id = shipping_zone_data["id"]
+
+    shipping_method_data = create_shipping_method(
+        e2e_staff_api_client, shipping_zone_id
+    )
+    shipping_method_id = shipping_method_data["id"]
+
+    create_shipping_method_channel_listing(
+        e2e_staff_api_client, shipping_method_id, channel_id
+    )
+
+    product_type_data = create_product_type(
+        e2e_staff_api_client,
+    )
+    product_type_id = product_type_data["id"]
+
+    category_data = create_category(
+        e2e_staff_api_client,
+    )
+    category_id = category_data["id"]
+
+    product_data = create_product(
+        e2e_staff_api_client,
+        product_type_id,
+        category_id,
+    )
+    product_id = product_data["id"]
+    create_product_channel_listing(e2e_staff_api_client, product_id, channel_id)
+
+    stocks = [
+        {
+            "warehouse": warehouse_data["id"],
+            "quantity": 5,
+        }
+    ]
+    variant_data = create_product_variant(
+        e2e_staff_api_client, product_id, stocks=stocks
+    )
+    product_variant_id = variant_data["id"]
+
+    create_product_variant_channel_listing(
+        e2e_staff_api_client,
+        product_variant_id,
+        channel_id,
+        variant_price,
+    )
+
+    sale = create_sale(e2e_staff_api_client, sale_name, discount_type)
+    sale_id = sale["id"]
+    sale_listing_input = [
+        {
+            "channelId": channel_id,
+            "discountValue": discount_value,
+        }
+    ]
+    create_sale_channel_listing(
+        e2e_staff_api_client, sale_id, add_channels=sale_listing_input
+    )
+    sale_catalogues_add(e2e_staff_api_client, sale_id, variants=product_variant_id)
+
+    return channel_id, product_variant_id, sale_id, shipping_method_id
+
+
+@pytest.mark.e2e
+def test_order_products_on_percentage_sale_CORE_1003(
+    e2e_staff_api_client,
+    permission_manage_products,
+    permission_manage_channels,
+    permission_manage_shipping,
+    permission_manage_product_types_and_attributes,
+    permission_manage_discounts,
+    permission_manage_orders,
+):
+    # Before
+    channel_slug = "test-channel"
+    variant_price = "30"
+    sale_name = "Sale Fixed"
+    discount_type = "PERCENTAGE"
+    discount_value = 3
+
+    channel_id, product_variant_id, sale_id, shipping_method_id = prepare_product(
+        e2e_staff_api_client,
+        permission_manage_products,
+        permission_manage_channels,
+        permission_manage_shipping,
+        permission_manage_product_types_and_attributes,
+        permission_manage_discounts,
+        permission_manage_orders,
+        channel_slug,
+        variant_price,
+        sale_name,
+        discount_type,
+        discount_value,
+    )
+
+    # Step 1 - Create a draft order
+    input = {
+        "channelId": channel_id,
+        "billingAddress": DEFAULT_ADDRESS,
+        "shippingAddress": DEFAULT_ADDRESS,
+    }
+    data = draft_order_create(e2e_staff_api_client, input)
+    order_id = data["order"]["id"]
+    assert data["order"]["billingAddress"] is not None
+    assert data["order"]["shippingAddress"] is not None
+    assert order_id is not None
+
+    # Step 2 - Add product on sale to draft order
+    lines = [{"variantId": product_variant_id, "quantity": 1}]
+    order_lines = order_lines_create(e2e_staff_api_client, order_id, lines)
+
+    draft_line = order_lines["order"]["lines"][0]
+    assert draft_line["variant"]["id"] == product_variant_id
+    discount = round(float(variant_price) * discount_value / 100, 2)
+    unit_price = float(variant_price) - discount
+    undiscounted_price = draft_line["undiscountedUnitPrice"]["gross"]["amount"]
+    assert undiscounted_price == float(variant_price)
+    assert draft_line["unitPrice"]["gross"]["amount"] == unit_price
+
+    # Step 3 - Add a shipping method to the order
+    input = {"shippingMethod": shipping_method_id}
+    draft_update = draft_order_update(e2e_staff_api_client, order_id, input)
+
+    order_shipping_id = draft_update["order"]["deliveryMethod"]["id"]
+    shipping_price = draft_update["order"]["shippingPrice"]["gross"]["amount"]
+    assert order_shipping_id is not None
+
+    # Step 4 - Complete the draft order
+    order = draft_order_complete(e2e_staff_api_client, order_id)
+
+    order_line = order["order"]["lines"][0]
+    assert order_line["unitDiscount"]["amount"] == discount
+    assert order_line["unitDiscountValue"] == discount
+    assert order_line["unitDiscountType"] == "FIXED"
+    assert draft_line["unitDiscountReason"] == f"Sale: {sale_id}"
+    product_price = order_line["undiscountedUnitPrice"]["gross"]["amount"]
+    assert product_price == undiscounted_price
+    total = order["order"]["total"]["gross"]["amount"]
+    subtotal = order["order"]["subtotal"]["gross"]["amount"]
+    assert shipping_price + subtotal == total
+    assert order["order"]["status"] == "UNFULFILLED"
