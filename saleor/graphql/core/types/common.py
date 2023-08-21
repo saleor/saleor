9a7276ea4795b09f1e2951f5dc76050d5f34b971
IKarbowiak <iga.karbowiak@saleor.io>
2023-08-21 15:44:55 +0200

    Fix mypy

--- saleor/graphql/core/types/common.py
+++ saleor/graphql/core/types/common.py
@@ -176,7 +176,7 @@ class BulkError(BaseObjectType):
 
 class AccountError(Error):
     code = AccountErrorCode(description="The error code.", required=True)
-    address_type = AddressTypeEnum(
+    address_type = AddressTypeEnum(  # type: ignore[has-type]
         description="A type of address that causes the error.", required=False
     )
 
@@ -261,7 +261,7 @@ class CheckoutError(Error):
         description="List of line Ids which cause the error.",
         required=False,
     )
-    address_type = AddressTypeEnum(
+    address_type = AddressTypeEnum(  # type: ignore[has-type]
         description="A type of address that causes the error.", required=False
     )
 
@@ -349,7 +349,7 @@ class OrderError(Error):
         description="List of product variants that are associated with the error",
         required=False,
     )
-    address_type = AddressTypeEnum(
+    address_type = AddressTypeEnum(  # type: ignore[has-type]
         description="A type of address that causes the error.", required=False
     )
 
