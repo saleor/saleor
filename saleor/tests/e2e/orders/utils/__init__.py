3179a4edc217bb59ecb417e04c7cc635cd55f07b
Renata <renata.gajzlerowicz@gmail.com>
2023-08-22 13:49:25 +0200

    [TEST] adding test for order with percentage promotion (#13702)

--- saleor/tests/e2e/orders/utils/__init__.py
+++ saleor/tests/e2e/orders/utils/__init__.py
@@ -1,7 +1,7 @@
 from .draft_order_complete import draft_order_complete
 from .draft_order_create import draft_order_create
 from .draft_order_update import draft_order_update
-from .order_lines import order_lines_create
+from .order_lines_create import order_lines_create
 
 __all__ = [
     "draft_order_create",
