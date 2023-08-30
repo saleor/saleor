184bf85139a58febc3500078dc2fa1a98fc25689
Anna Szczęch <30683248+szczecha@users.noreply.github.com>
2023-08-30 11:30:38 +0300

    [TEST] Staff should be able to create promotion for collection (#13821)
    * add test for promotion rule for collection
    
    * update create promotion rule to use catalogue predicate as input
    
    * fix parameter
    
    * clean empty lines

--- saleor/tests/e2e/product/utils/__init__.py
+++ saleor/tests/e2e/product/utils/__init__.py
@@ -1,4 +1,6 @@
 from .category import create_category
+from .collection import create_collection
+from .collection_listing_update import create_collection_channel_listing
 from .digital_content import create_digital_content
 from .product import create_product
 from .product_attribute_assignment_update import (
@@ -27,5 +29,7 @@ __all__ = [
     "raw_create_product_channel_listing",
     "create_variants_in_bulk",
     "update_product_type_assignment_attribute",
+    "create_collection",
+    "create_collection_channel_listing",
     "get_product",
 ]
