4905a05616a9afd79ab0cd185c8d67c08d43aa8d
Renata <renata.gajzlerowicz@gmail.com>
2023-08-31 14:30:33 +0200

    refactoring promotion e2e tests

--- saleor/tests/e2e/orders/test_order_promotion_not_applied_when_not_within_time_range.py
+++ saleor/tests/e2e/orders/test_order_promotion_not_applied_when_not_within_time_range.py
@@ -4,118 +4,12 @@ import pytest
 from dateutil.tz import tzlocal
 
 from .. import DEFAULT_ADDRESS
-from ..channel.utils import create_channel
-from ..product.utils import (
-    create_category,
-    create_product,
-    create_product_channel_listing,
-    create_product_type,
-    create_product_variant,
-    create_product_variant_channel_listing,
-    get_product,
-)
+from ..product.utils import get_product
+from ..product.utils.preparing_product import prepare_product
 from ..promotions.utils import create_promotion, create_promotion_rule
-from ..shipping_zone.utils import (
-    create_shipping_method,
-    create_shipping_method_channel_listing,
-    create_shipping_zone,
-)
+from ..shop.utils.preparing_shop import prepare_shop
 from ..utils import assign_permissions
-from ..warehouse.utils import create_warehouse, update_warehouse
 from .utils import draft_order_create
-
-
-def prepare_product(
-    e2e_staff_api_client,
-    permission_manage_products,
-    permission_manage_channels,
-    permission_manage_shipping,
-    permission_manage_product_types_and_attributes,
-    permission_manage_discounts,
-    permission_manage_orders,
-    channel_slug,
-    variant_price,
-):
-    permissions = [
-        permission_manage_products,
-        permission_manage_channels,
-        permission_manage_shipping,
-        permission_manage_product_types_and_attributes,
-        permission_manage_discounts,
-        permission_manage_orders,
-    ]
-    assign_permissions(e2e_staff_api_client, permissions)
-
-    warehouse_data = create_warehouse(e2e_staff_api_client)
-    warehouse_id = warehouse_data["id"]
-    update_warehouse(
-        e2e_staff_api_client,
-        warehouse_data["id"],
-        is_private=False,
-    )
-    warehouse_ids = [warehouse_id]
-
-    channel_data = create_channel(
-        e2e_staff_api_client,
-        warehouse_ids,
-        slug=channel_slug,
-    )
-    channel_id = channel_data["id"]
-    channel_ids = [channel_id]
-
-    shipping_zone_data = create_shipping_zone(
-        e2e_staff_api_client,
-        warehouse_ids=warehouse_ids,
-        channel_ids=channel_ids,
-    )
-    shipping_zone_id = shipping_zone_data["id"]
-
-    shipping_method_data = create_shipping_method(
-        e2e_staff_api_client, shipping_zone_id
-    )
-    shipping_method_id = shipping_method_data["id"]
-
-    create_shipping_method_channel_listing(
-        e2e_staff_api_client, shipping_method_id, channel_id
-    )
-
-    product_type_data = create_product_type(
-        e2e_staff_api_client,
-    )
-    product_type_id = product_type_data["id"]
-
-    category_data = create_category(
-        e2e_staff_api_client,
-    )
-    category_id = category_data["id"]
-
-    product_data = create_product(
-        e2e_staff_api_client,
-        product_type_id,
-        category_id,
-    )
-    product_id = product_data["id"]
-    create_product_channel_listing(e2e_staff_api_client, product_id, channel_id)
-
-    stocks = [
-        {
-            "warehouse": warehouse_data["id"],
-            "quantity": 5,
-        }
-    ]
-    variant_data = create_product_variant(
-        e2e_staff_api_client, product_id, stocks=stocks
-    )
-    product_variant_id = variant_data["id"]
-
-    create_product_variant_channel_listing(
-        e2e_staff_api_client,
-        product_variant_id,
-        channel_id,
-        variant_price,
-    )
-
-    return channel_id, product_id, product_variant_id, shipping_method_id
 
 
 @pytest.mark.e2e
@@ -129,8 +23,16 @@ def test_order_promotion_not_applied_when_not_within_time_range_CORE_2110(
     permission_manage_orders,
 ):
     # Before
-    channel_slug = "test-channel"
-    variant_price = "20"
+    permissions = [
+        permission_manage_products,
+        permission_manage_channels,
+        permission_manage_shipping,
+        permission_manage_product_types_and_attributes,
+        permission_manage_discounts,
+        permission_manage_orders,
+    ]
+    assign_permissions(e2e_staff_api_client, permissions)
+
     promotion_name = "Promotion Fixed"
     discount_value = 5
     discount_type = "FIXED"
@@ -140,20 +42,18 @@ def test_order_promotion_not_applied_when_not_within_time_range_CORE_2110(
     month_after = (datetime.now(tzlocal()) + timedelta(days=30)).isoformat()
 
     (
+        warehouse_id,
         channel_id,
+        channel_slug,
+        shipping_method_id,
+    ) = prepare_shop(e2e_staff_api_client)
+
+    (
         product_id,
         product_variant_id,
-        shipping_method_id,
+        product_variant_price,
     ) = prepare_product(
-        e2e_staff_api_client,
-        permission_manage_products,
-        permission_manage_channels,
-        permission_manage_shipping,
-        permission_manage_product_types_and_attributes,
-        permission_manage_discounts,
-        permission_manage_orders,
-        channel_slug,
-        variant_price,
+        e2e_staff_api_client, warehouse_id, channel_id, variant_price=20
     )
 
     # Step 1 - Create promotion lasting for a specific time range
