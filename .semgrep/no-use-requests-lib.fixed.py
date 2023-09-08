cb768260720e35970ae0f2792f53a8af79fdf695
Mikail Kocak <mikail.kocak@saleor.io>
2023-09-08 12:13:45 +0200

    Fix pre-commit rules running against semgrep config files
    Semgrep test files should not be checked by pre-commit as they may break the tests, and those are not really "Python code"
    
    This also fixes black breaking the tests.

--- .semgrep/no-use-requests-lib.fixed.py
+++ .semgrep/no-use-requests-lib.fixed.py
@@ -2,86 +2,85 @@ import requests
 
 # Test multiline
 # ruleid: no-requests-lib
-HTTPClient.send_request(
-    "get",
+HTTPClient.send_request("get",
     "https://foo",
     allow_redirects=True,
 )
 
 # ruleid: no-requests-lib
-HTTPClient.send_request("get", "https://foo")
+HTTPClient.send_request("get","https://foo")
 
 # ruleid: no-requests-lib
-HTTPClient.send_request("get", "https://foo", allow_redirects=True)
+HTTPClient.send_request("get","https://foo", allow_redirects=True)
 
 params = {"allow_redirects": True}
 # ruleid: no-requests-lib
-HTTPClient.send_request("get", "https://foo", **params)
+HTTPClient.send_request("get","https://foo", **params)
 
 # ruleid: no-requests-lib
-HTTPClient.send_request("get", "https://foo", allow_redirects=False)
+HTTPClient.send_request("get","https://foo", allow_redirects=False)
 
 session = requests.Session()
 # ruleid: no-requests-lib
-HTTPClient.send_request("get", "https://foo")
+HTTPClient.send_request("get","https://foo")
 # ruleid: no-requests-lib
-HTTPClient.send_request("get", "https://foo", allow_redirects=True)
+HTTPClient.send_request("get","https://foo", allow_redirects=True)
 # ruleid: no-requests-lib
-HTTPClient.send_request("get", "https://foo", allow_redirects=False)
+HTTPClient.send_request("get","https://foo", allow_redirects=False)
 
 # ruleid: no-requests-lib
-HTTPClient.send_request("post", "https://foo")
+HTTPClient.send_request("post","https://foo")
 # ruleid: no-requests-lib
-HTTPClient.send_request("request", "GET", "https://foo")
+HTTPClient.send_request("request","GET", "https://foo")
 # ruleid: no-requests-lib
-HTTPClient.send_request("get", "https://foo")
+HTTPClient.send_request("get","https://foo")
 # ruleid: no-requests-lib
-HTTPClient.send_request("post", "https://foo")
+HTTPClient.send_request("post","https://foo")
 # ruleid: no-requests-lib
-HTTPClient.send_request("put", "https://foo")
+HTTPClient.send_request("put","https://foo")
 # ruleid: no-requests-lib
-HTTPClient.send_request("delete", "https://foo")
+HTTPClient.send_request("delete","https://foo")
 # ruleid: no-requests-lib
-HTTPClient.send_request("head", "https://foo")
+HTTPClient.send_request("head","https://foo")
 # ruleid: no-requests-lib
-HTTPClient.send_request("patch", "https://foo")
+HTTPClient.send_request("patch","https://foo")
 
 # Test indentations
 if True:
     # ruleid: no-requests-lib
-    HTTPClient.send_request("get", "https://foo")
+    HTTPClient.send_request("get","https://foo")
 
     # ruleid: no-requests-lib
-    HTTPClient.send_request("get", "https://foo", allow_redirects=True)
+    HTTPClient.send_request("get","https://foo", allow_redirects=True)
 
     params = {"allow_redirects": True}
     # ruleid: no-requests-lib
-    HTTPClient.send_request("get", "https://foo", **params)
+    HTTPClient.send_request("get","https://foo", **params)
 
     # ruleid: no-requests-lib
-    HTTPClient.send_request("get", "https://foo", allow_redirects=False)
+    HTTPClient.send_request("get","https://foo", allow_redirects=False)
 
     session = requests.Session()
     # ruleid: no-requests-lib
-    HTTPClient.send_request("get", "https://foo")
+    HTTPClient.send_request("get","https://foo")
     # ruleid: no-requests-lib
-    HTTPClient.send_request("get", "https://foo", allow_redirects=True)
+    HTTPClient.send_request("get","https://foo", allow_redirects=True)
     # ruleid: no-requests-lib
-    HTTPClient.send_request("get", "https://foo", allow_redirects=False)
+    HTTPClient.send_request("get","https://foo", allow_redirects=False)
 
     # ruleid: no-requests-lib
-    HTTPClient.send_request("post", "https://foo")
+    HTTPClient.send_request("post","https://foo")
     # ruleid: no-requests-lib
-    HTTPClient.send_request("request", "GET", "https://foo")
+    HTTPClient.send_request("request","GET", "https://foo")
     # ruleid: no-requests-lib
-    HTTPClient.send_request("get", "https://foo")
+    HTTPClient.send_request("get","https://foo")
     # ruleid: no-requests-lib
-    HTTPClient.send_request("post", "https://foo")
+    HTTPClient.send_request("post","https://foo")
     # ruleid: no-requests-lib
-    HTTPClient.send_request("put", "https://foo")
+    HTTPClient.send_request("put","https://foo")
     # ruleid: no-requests-lib
-    HTTPClient.send_request("delete", "https://foo")
+    HTTPClient.send_request("delete","https://foo")
     # ruleid: no-requests-lib
-    HTTPClient.send_request("head", "https://foo")
+    HTTPClient.send_request("head","https://foo")
     # ruleid: no-requests-lib
-    HTTPClient.send_request("patch", "https://foo")
+    HTTPClient.send_request("patch","https://foo")
