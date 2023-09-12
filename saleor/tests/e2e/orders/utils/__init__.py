8840a47bdc9562d6815f8bf72fe14e9a78c379ab
Renata <renata.gajzlerowicz@gmail.com>
2023-09-12 14:18:58 +0200

    test_order_mark_as_paid (#13950)

--- saleor/tests/e2e/orders/utils/__init__.py
+++ saleor/tests/e2e/orders/utils/__init__.py
@@ -1,5 +1,6 @@
 from .draft_order_complete import draft_order_complete, raw_draft_order_complete
 from .draft_order_create import draft_order_create
+from .draft_order_delete import draft_order_delete
 from .draft_order_update import draft_order_update
 from .order_lines_create import order_lines_create
 from .order_mark_as_paid import mark_order_paid
