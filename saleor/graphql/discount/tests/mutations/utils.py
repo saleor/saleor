7f02d3b4d74c1146c6b160c1e9d62a24576bc0ae
zedzior <piotr.zabieglik@gmail.com>
2023-09-15 17:22:33 +0200

    Revert "Simplify code."
    This reverts commit ba3d55df0131ecd446101804e64bc954add93b96.

--- saleor/graphql/discount/tests/mutations/utils.py
+++ saleor/graphql/discount/tests/mutations/utils.py
@@ -19,7 +19,7 @@ def convert_migrated_sale_predicate_to_catalogue_info(
 
     if catalogue_predicate.get("OR"):
         predicates = {
-            next(item.keys()): next(item.values())["ids"]
+            list(item.keys())[0]: list(item.values())[0]["ids"]
             for item in catalogue_predicate["OR"]
         }
         for predicate_name, field in PREDICATE_TO_CATALOGUE_INFO_MAP.items():
