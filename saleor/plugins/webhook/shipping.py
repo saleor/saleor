fb77aad5d9e024538791b32ae3184a8717178dc1
Marcin Gębala <maarcin.gebala@gmail.com>
2023-09-18 10:20:46 +0200

    Increase shipping sync webhooks timeout

--- saleor/plugins/webhook/shipping.py
+++ saleor/plugins/webhook/shipping.py
@@ -4,6 +4,7 @@ import logging
 from collections import defaultdict
 from typing import Any, Callable, Dict, List, Optional, Union
 
+from django.conf import settings
 from django.core.cache import cache
 from django.db.models import QuerySet
 from graphql import GraphQLError
@@ -18,7 +19,7 @@ from ...shipping.interface import ShippingMethodData
 from ...webhook.utils import get_webhooks_for_event
 from ..base_plugin import ExcludedShippingMethod
 from ..const import APP_ID_PREFIX
-from .const import CACHE_EXCLUDED_SHIPPING_TIME, EXCLUDED_SHIPPING_REQUEST_TIMEOUT
+from .const import CACHE_EXCLUDED_SHIPPING_TIME
 from .tasks import trigger_webhook_sync
 
 logger = logging.getLogger(__name__)
@@ -117,7 +118,7 @@ def get_excluded_shipping_methods_or_fetch(
             payload,
             webhook,
             subscribable_object=subscribable_object,
-            timeout=EXCLUDED_SHIPPING_REQUEST_TIMEOUT,
+            timeout=settings.WEBHOOK_SYNC_TIMEOUT,
         )
         if response_data:
             excluded_methods.extend(
