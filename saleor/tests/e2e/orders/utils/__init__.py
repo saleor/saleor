770eb233c786cd8f9977aca0e7c1cb4bc42f63f6
Renata <renata.gajzlerowicz@gmail.com>
2023-09-18 12:08:13 +0200

    adding test for order expiration and order created from checkout

--- saleor/tests/e2e/orders/utils/__init__.py
+++ saleor/tests/e2e/orders/utils/__init__.py
@@ -3,6 +3,7 @@ from .draft_order_create import draft_order_create
 from .draft_order_delete import draft_order_delete
 from .draft_order_update import draft_order_update
 from .order_cancel import order_cancel
+from .order_create_from_checkout import order_create_from_checkout
 from .order_lines_create import order_lines_create
 from .order_mark_as_paid import mark_order_paid
 from .order_query import order_query
@@ -17,4 +18,5 @@ __all__ = [
     "mark_order_paid",
     "order_cancel",
     "draft_order_delete",
+    "order_create_from_checkout",
 ]
