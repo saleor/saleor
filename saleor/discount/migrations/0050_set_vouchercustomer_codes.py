90497455352b7589acc0060314995097e321189a
IKarbowiak <iga.karbowiak@saleor.io>
2023-09-26 12:15:58 +0200

    Update migration for setting voucher_code on VoucherCustomer

--- saleor/discount/migrations/0050_set_vouchercustomer_codes.py
+++ saleor/discount/migrations/0050_set_vouchercustomer_codes.py
@@ -3,8 +3,8 @@
 from django.db import migrations
 from django.db.models import Exists, OuterRef
 
-
-BATCH_SIZE = 500
+# The batch of size 10000 took about 0.2s
+BATCH_SIZE = 1000
 
 
 def set_voucher_code_to_voucher_customer(apps, schema_editor):
@@ -29,12 +29,12 @@ def set_voucher_code(VoucherCustomer, Voucher, VoucherCode, voucher_customers):
     voucher_id_to_code_map = get_voucher_id_to_code_map(
         Voucher, VoucherCode, voucher_customers
     )
-    voucher_customers = []
+    voucher_customers_list = []
     for voucher_customer in voucher_customers:
         code = voucher_id_to_code_map[voucher_customer.voucher_id]
         voucher_customer.voucher_code = code
-        voucher_customers.append(voucher_customer)
-    VoucherCustomer.objects.bulk_update(voucher_customers, ["voucher_code"])
+        voucher_customers_list.append(voucher_customer)
+    VoucherCustomer.objects.bulk_update(voucher_customers_list, ["voucher_code"])
 
 
 def get_voucher_id_to_code_map(Voucher, VoucherCode, voucher_customers):
