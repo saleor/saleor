03a698fc7ed5660d630df78a6197aa9ee6469057
IKarbowiak <iga.karbowiak@saleor.io>
2023-09-29 12:36:59 +0200

    Update discount migrations

--- saleor/discount/migrations/0055_vouchercustomer_voucher_code_index.py
+++ saleor/discount/migrations/0055_vouchercustomer_voucher_code_index.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
     atomic = False
 
     dependencies = [
-        ("discount", "0049_vouchercustomer_vouchercode"),
+        ("discount", "0054_vouchercustomer_vouchercode"),
     ]
 
     operations = [
--- saleor/discount/migrations/0056_set_vouchercustomer_codes.py
+++ saleor/discount/migrations/0056_set_vouchercustomer_codes.py
@@ -53,7 +53,7 @@ def get_voucher_id_to_code_map(Voucher, VoucherCode, voucher_customers):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("discount", "0050_vouchercustomer_voucher_code_index"),
+        ("discount", "0055_vouchercustomer_voucher_code_index"),
     ]
 
     operations = [
