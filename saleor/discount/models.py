c356478f6d196c75ecc609d5ff0a0eca6fc2eee9
zedzior <piotr.zabieglik@gmail.com>
2023-10-03 12:06:54 +0200

    Change model of manage discounts permission.

--- saleor/discount/models.py
+++ saleor/discount/models.py
@@ -371,6 +371,13 @@ class Promotion(ModelWithMetadata):
 
     class Meta:
         ordering = ("name", "pk")
+        app_label = "discount"
+        permissions = (
+            (
+                DiscountPermissions.MANAGE_DISCOUNTS.codename,
+                "Manage promotions and vouchers.",
+            ),
+        )
 
     def is_active(self, date=None):
         if date is None:
