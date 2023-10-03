6d1a48cb6f7787b1dee621e31b02ec4fd8e3ef45
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2023-10-03 09:01:28 +0200

    Add `singleUse` voucher (#14147)
    * Add single_use field to Voucher model
    
    * Allow define singleUse in VoucherCreate
    
    * Add singleUse field to Voucher, allow defining singleUse in VoucherUpdate
    
    * Update add_promo_code_to_checkout to take is_active flag into consideration
    
    * Mark single use Voucher code as inactive when checkout completed
    
    * Add tests for voucher code activation and deactivation
    
    * Update VoucherInput.singleUse description

--- saleor/graphql/discount/mutations/voucher/voucher_create.py
+++ saleor/graphql/discount/mutations/voucher/voucher_create.py
@@ -99,7 +99,7 @@ class VoucherInput(BaseInputObjectType):
         )
     )
     usage_limit = graphene.Int(
-        description=("Limit number of times this voucher can be used in total.")
+        description="Limit number of times this voucher can be used in total."
     )
 
     class Meta:
