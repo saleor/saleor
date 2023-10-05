429b7b6933ce4a9438795e7eb2a7fced87d3e48a
SzymJ <31098825+SzymJ@users.noreply.github.com>
2023-10-05 09:13:52 +0200

    Fix tests

--- saleor/graphql/discount/tests/benchmark/test_voucher_code_bulk_delete.py
+++ saleor/graphql/discount/tests/benchmark/test_voucher_code_bulk_delete.py
@@ -23,30 +23,24 @@ def test_voucher_code_bulk_delete_queries(
 ):
     # given
     codes = voucher_with_many_codes.codes.all()
-
+    staff_api_client.user.user_permissions.add(permission_manage_discounts)
     ids = [graphene.Node.to_global_id("VoucherCode", code.id) for code in codes]
 
     variables = {"ids": ids[:1]}
 
     # when
-    with django_assert_num_queries(9):
+    with django_assert_num_queries(10):
         response = staff_api_client.post_graphql(
-            VOUCHER_CODE_BULK_DELETE_MUTATION,
-            variables,
-            permissions=[permission_manage_discounts],
-            check_no_permissions=False,
+            VOUCHER_CODE_BULK_DELETE_MUTATION, variables
         )
         content = get_graphql_content(response)
         assert content["data"]["voucherCodeBulkDelete"]["count"] == 1
 
     variables = {"ids": ids[1:]}
 
-    with django_assert_num_queries(9):
+    with django_assert_num_queries(10):
         response = staff_api_client.post_graphql(
-            VOUCHER_CODE_BULK_DELETE_MUTATION,
-            variables,
-            permissions=[permission_manage_discounts],
-            check_no_permissions=False,
+            VOUCHER_CODE_BULK_DELETE_MUTATION, variables
         )
         content = get_graphql_content(response)
         assert content["data"]["voucherCodeBulkDelete"]["count"] == 4
