63e0bd69dd8b17e7fb773c481474d095588afd57
IKarbowiak <iga.karbowiak@saleor.io>
2023-10-06 14:51:42 +0200

    Fix migration conflicts

--- saleor/discount/migrations/0054_auto_20230821_0730.py
+++ saleor/discount/migrations/0054_auto_20230821_0730.py
@@ -5,7 +5,7 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("discount", "0052_move_codes_to_new_model"),
+        ("discount", "0053_move_codes_to_new_model"),
     ]
 
     operations = [
