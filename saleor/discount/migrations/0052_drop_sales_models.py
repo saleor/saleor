0b5f0851d388cad4ac7e37ac2fd6570db263fc9b
zedzior <piotr.zabieglik@gmail.com>
2023-10-06 12:27:36 +0200

    Drop indexes.

--- saleor/discount/migrations/0052_drop_sales_models.py
+++ saleor/discount/migrations/0052_drop_sales_models.py
@@ -1,4 +1,4 @@
-from django.db import migrations, connection
+from django.db import migrations
 
 DROP_CONSTRAINTS_SQL = """
 do $$
@@ -37,43 +37,96 @@ $$
 """
 
 
-# DROP INDEX CONCURRENTLY cannot be executed from a function, so we have to generate
-# multiple statements
-def generate_drop_index_sql():
-    GET_INDEXES_SQL = """
-        select indexname
-        from pg_indexes
-        where tablename in (
-          'discount_sale_collections',
-          'discount_sale_categories',
-          'discount_sale_products',
-          'discount_sale_variants',
-          'discount_salechannellisting',
-          'discount_saletranslation',
-          'discount_sale'
-        )
-    """
-
-    with connection.cursor() as cursor:
-        cursor.execute(GET_INDEXES_SQL)
-        indexes = cursor.fetchall()
-    sqls = [f'DROP INDEX CONCURRENTLY "{index[0]}"' for index in indexes]
-    return ";".join(sqls)
-
-
 class Migration(migrations.Migration):
+    atomic = False
     dependencies = [
         ("discount", "0051_detach_sale_from_permission"),
     ]
+
     operations = [
         migrations.SeparateDatabaseAndState(
             database_operations=[
                 migrations.RunSQL(
                     DROP_CONSTRAINTS_SQL, reverse_sql=migrations.RunSQL.noop
                 ),
-                # migrations.RunSQL(
-                #     generate_drop_index_sql(), reverse_sql=migrations.RunSQL.noop
-                # ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_sale_collections_sale_id_a912da4a";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_sale_collections_collection_id_f66df9d7";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_saletranslation_sale_id_36a69b0a";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_sale_categories_sale_id_2aeee4a7";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_sale_categories_category_id_64e132af";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_sale_products_sale_id_10e3a20f";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_sale_products_product_id_d42c9636";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY " '"discount_sale_created_c17254d6";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY " '"discount_sale_updated_at_1fb1171b";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_salechannellisting_channel_id_3319ed70";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_salechannellisting_sale_id_13a35e18";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_sale_variants_sale_id_50fc4c3a";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_sale_variants_productvariant_id_91fa5f1b";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_checkoutlinediscount_sale_id_b0964e58";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_orderlinediscount_sale_id_d95994f8";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
+                migrations.RunSQL(
+                    "DROP INDEX CONCURRENTLY "
+                    '"discount_orderdiscount_sale_id_849ebbef";',
+                    reverse_sql=migrations.RunSQL.noop,
+                ),
             ],
             state_operations=[
                 migrations.RemoveField(
