338d19a6a982452040aa17da637efce1469f42bc
Paul Tindall <paul@wellstandhealth.com>
2023-10-09 15:24:40 -0500

    chore: modify references to remove dependency on django-graphne

--- saleor/graphql/pharmacy/types.py
+++ saleor/graphql/pharmacy/types.py
@@ -1,15 +1,13 @@
 import graphene
-from django.db.models import QuerySet
-from graphene_django.types import DjangoObjectType
 
-from ..core.types import ModelObjectType
+from ..core.types import BaseObjectType
 from ...pharmacy import models
 
 
-class SiteSettingsType(DjangoObjectType):
+class SiteSettingsType(BaseObjectType):
     class Meta:
         model = models.SiteSettings
 
 
-class SiteSettingsList(graphene.ObjectType):
+class SiteSettingsList(BaseObjectType):
     edge = graphene.List(SiteSettingsType)
