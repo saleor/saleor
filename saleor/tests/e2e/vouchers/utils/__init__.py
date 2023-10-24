bdacdb3dda49564f7971f2209155a1d764128792
Renata <renata.gajzlerowicz@gmail.com>
2023-10-24 10:44:27 +0200

    tests for deleted vpuchers and voucher codes

--- saleor/tests/e2e/vouchers/utils/__init__.py
+++ saleor/tests/e2e/vouchers/utils/__init__.py
@@ -1,11 +1,19 @@
 from .query_voucher import get_voucher
+from .query_vouchers import get_vouchers
+from .voucher_bulk_delete import voucher_bulk_delete
 from .voucher_catalogues_add import add_catalogue_to_voucher
 from .voucher_channel_listing import create_voucher_channel_listing
+from .voucher_code_bulk_delete import voucher_code_bulk_delete
 from .voucher_create import create_voucher
+from .voucher_delete import voucher_delete
 
 __all__ = [
     "create_voucher",
     "create_voucher_channel_listing",
     "add_catalogue_to_voucher",
     "get_voucher",
+    "voucher_delete",
+    "voucher_bulk_delete",
+    "voucher_code_bulk_delete",
+    "get_vouchers",
 ]
