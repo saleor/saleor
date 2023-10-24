81f43d6f7726d8fbc116dbf42cbacd72966df14a
Maciej Korycinski <korycinski.maciej@gmail.com>
2023-10-24 14:50:34 +0200

    Rebuild celery migration task

--- saleor/order/migrations/tasks/saleor3_13.py
+++ saleor/order/migrations/tasks/saleor3_13.py
@@ -1,9 +1,9 @@
 from decimal import Decimal
 
+from babel.numbers import get_currency_precision
 from django.db import transaction
-from django.db.models import DecimalField, QuerySet, Sum
-from django.db.models.expressions import Exists, F, OuterRef
-from django.db.models.functions import Coalesce
+from django.db.models import QuerySet
+from django.db.models.expressions import Exists, OuterRef
 
 from ....celeryconf import app
 from ... import OrderChargeStatus
@@ -53,61 +53,68 @@ def order_events_rename_transaction_capture_events_task():
         order_events_rename_transaction_capture_events_task.delay()
 
 
+def quantize_price(price, currency: str):
+    precision = get_currency_precision(currency)
+    number_places = Decimal(10) ** -precision
+    return price.quantize(number_places)
+
+
+def update_order_charge_status(order: Order, granted_refund_amount: Decimal):
+    """Update the current charge status for the order.
+
+    We treat the order as overcharged when the charged amount is bigger that
+    order.total - order granted refund
+    We treat the order as fully charged when the charged amount is equal to
+    order.total - order granted refund.
+    We treat the order as partially charged when the charged amount covers only part of
+    the order.total - order granted refund
+    We treat the order as not charged when the charged amount is 0.
+    """
+    total_charged = order.total_charged_amount or Decimal("0")
+    total_charged = quantize_price(total_charged, order.currency)
+
+    current_total_gross = order.total_gross_amount - granted_refund_amount
+    current_total_gross = max(current_total_gross, Decimal("0"))
+    current_total_gross = quantize_price(current_total_gross, order.currency)
+
+    if total_charged == current_total_gross:
+        order.charge_status = OrderChargeStatus.FULL
+    elif total_charged <= Decimal(0):
+        order.charge_status = OrderChargeStatus.NONE
+    elif total_charged < current_total_gross:
+        order.charge_status = OrderChargeStatus.PARTIAL
+    else:
+        order.charge_status = OrderChargeStatus.OVERCHARGED
+
+
 @app.task
-def update_orders_charge_statuses_task():
+def update_orders_charge_statuses_task(stop_granted_refund_pk=-1):
+    """Update the charge status for orders with granted refunds.
+
+    Task takes around 0.3 seconds for 1000 orders and around 5 MB of memory.
+    """
+    if stop_granted_refund_pk == -1:
+        stop_granted_refund_pk = OrderGrantedRefund.objects.count()
+    order_charge_status_batch_size = 1000
+    start_pk = stop_granted_refund_pk - order_charge_status_batch_size
+    stop_pk = stop_granted_refund_pk
     orders = Order.objects.filter(
-        Exists(OrderGrantedRefund.objects.filter(order_id=OuterRef("pk")))
-    )
-    orders = orders.annotate(
-        refunded_amount=Coalesce(
-            Sum("granted_refunds__amount_value"),
-            0,
-            output_field=DecimalField(),
-        ),
-        current_total_gross=F("total_gross_amount") - F("refunded_amount"),
-    )
-    trigger_task = False
-
-    overcharged_orders = orders.filter(
-        total_charged_amount__gt=F("current_total_gross")
-    ).exclude(charge_status=OrderChargeStatus.OVERCHARGED)[:BATCH_SIZE]
-    if overcharged_orders.exists():
-        order_pks = overcharged_orders.values_list("pk", flat=True)
-        Order.objects.filter(id__in=order_pks).update(
-            charge_status=OrderChargeStatus.OVERCHARGED
+        Exists(
+            OrderGrantedRefund.objects.filter(
+                order_id=OuterRef("pk"),
+                pk__gte=start_pk,
+                pk__lt=stop_pk,
+            )
         )
-        trigger_task = True
-
-    full_orders = orders.filter(total_charged_amount=F("current_total_gross")).exclude(
-        charge_status=OrderChargeStatus.FULL
-    )[:BATCH_SIZE]
-    if full_orders.exists():
-        order_pks = full_orders.values_list("pk", flat=True)
-        Order.objects.filter(id__in=order_pks).update(
-            charge_status=OrderChargeStatus.FULL
+    ).prefetch_related("granted_refunds")
+    orders_to_update = []
+    for o in orders:
+        granted_refund_amount = sum(
+            [refund.amount.amount for refund in o.granted_refunds.all()], Decimal(0)
         )
-        trigger_task = True
+        update_order_charge_status(o, granted_refund_amount)
+        orders_to_update.append(o)
 
-    partial_orders = orders.filter(
-        total_charged_amount__lt=F("current_total_gross"),
-        total_charged_amount__gt=Decimal(0),
-    ).exclude(charge_status=OrderChargeStatus.PARTIAL)[:BATCH_SIZE]
-    if partial_orders.exists():
-        order_pks = partial_orders.values_list("pk", flat=True)
-        Order.objects.filter(id__in=order_pks).update(
-            charge_status=OrderChargeStatus.PARTIAL
-        )
-        trigger_task = True
-
-    none_orders = orders.filter(
-        total_charged_amount__lte=Decimal(0), current_total_gross__gt=Decimal(0)
-    ).exclude(charge_status=OrderChargeStatus.NONE)[:BATCH_SIZE]
-    if none_orders.exists():
-        order_pks = none_orders.values_list("pk", flat=True)
-        Order.objects.filter(id__in=order_pks).update(
-            charge_status=OrderChargeStatus.NONE
-        )
-        trigger_task = True
-
-    if trigger_task:
-        update_orders_charge_statuses_task.delay()
+    if orders_to_update:
+        Order.objects.bulk_update(orders_to_update, ["charge_status"])
+        update_orders_charge_statuses_task.delay(start_pk)
