4af706185d05ee414b0fb460603cac9518f6cfc7
jakubkuc <jakub.kuc.87@gmail.com>
2023-10-27 10:44:49 +0200

    remove unwanted migration

--- saleor/celeryconf.py
+++ saleor/celeryconf.py
@@ -39,11 +39,5 @@ app.autodiscover_tasks(
     ],
     related_name="saleor3_17",
 )
-app.autodiscover_tasks(
-    packages=[
-        "saleor.order.migrations.tasks",
-    ],
-    related_name="saleor3_18",
-)
 app.autodiscover_tasks(lambda: discover_plugins_modules(settings.PLUGINS))  # type: ignore[misc] # circular import # noqa: E501
 app.autodiscover_tasks(related_name="search_tasks")
