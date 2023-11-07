b99a9ab22b574e060e0d49e2a050be242e0d84b9
Maciej Korycinski <korycinski.maciej@gmail.com>
2023-11-07 11:54:12 +0100

    Add fixes for StripePlugin

--- saleor/payment/gateways/stripe/webhooks.py
+++ saleor/payment/gateways/stripe/webhooks.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Optional
+from typing import Optional, cast
 
 from django.core.exceptions import ValidationError
 from django.core.handlers.wsgi import WSGIRequest
@@ -20,7 +20,7 @@ from ....plugins.manager import get_plugins_manager
 from ... import ChargeStatus, TransactionKind
 from ...gateway import payment_refund_or_void
 from ...interface import GatewayConfig, GatewayResponse
-from ...models import Payment
+from ...models import Payment, Transaction
 from ...utils import (
     create_transaction,
     gateway_postprocess,
@@ -119,16 +119,14 @@ def _channel_slug_is_different_from_payment_channel_slug(
         )  # pragma: no cover
 
 
-def _get_payment(payment_intent_id: str) -> Optional[Payment]:
-    return (
-        Payment.objects.prefetch_related(
-            Prefetch("checkout", queryset=Checkout.objects.select_related("channel")),
-            Prefetch("order", queryset=Order.objects.select_related("channel")),
-        )
-        .select_for_update(of=("self",))
-        .filter(transactions__token=payment_intent_id)
-        .first()
+def _get_payment(payment_intent_id: str, with_lock=True) -> Optional[Payment]:
+    qs = Payment.objects.prefetch_related(
+        Prefetch("checkout", queryset=Checkout.objects.select_related("channel")),
+        Prefetch("order", queryset=Order.objects.select_related("channel")),
     )
+    if with_lock:
+        qs = qs.select_for_update(of=("self",))
+    return qs.filter(transactions__token=payment_intent_id).first()
 
 
 def _get_checkout(payment_id: int) -> Optional[Checkout]:
@@ -160,24 +158,35 @@ def _finalize_checkout(
         psp_reference=payment_intent.id,
     )
 
-    transaction = create_transaction(
-        payment,
-        kind=kind,
-        payment_information=None,
+    transaction = Transaction.objects.filter(
+        payment_id=payment.id,
+        is_success=True,
         action_required=False,
-        gateway_response=gateway_response,
-    )
+        kind=kind,
+    ).first()
 
-    # To avoid zombie payments we have to update payment `charge_status` without
-    # changing `to_confirm` flag. In case when order cannot be created then
-    # payment will be refunded.
-    update_payment_charge_status(payment, transaction)
-    payment.refresh_from_db()
-    checkout.refresh_from_db()
+    # Ensure that the transaction does not exist before creating it. The transaction
+    # can be created by the `checkoutComplete` logic, which can be executed
+    # simultaneously with this function.
+    if not transaction:
+        transaction = create_transaction(
+            payment,
+            kind=kind,
+            payment_information=None,
+            action_required=False,
+            gateway_response=gateway_response,
+        )
+        # To avoid zombie payments we have to update payment `charge_status` without
+        # changing `to_confirm` flag. In case when order cannot be created then
+        # payment will be refunded.
+        update_payment_charge_status(payment, transaction)
+        payment.refresh_from_db()
+        checkout.refresh_from_db()
 
     manager = get_plugins_manager()
     lines, unavailable_variant_pks = fetch_checkout_lines(checkout)
     if unavailable_variant_pks:
+        payment_refund_or_void(payment, manager, checkout.channel.slug)
         raise ValidationError("Some of the checkout lines variants are unavailable.")
     checkout_info = fetch_checkout_info(checkout, lines, manager)
     checkout_total = calculate_checkout_total_with_gift_cards(
@@ -188,12 +197,14 @@ def _finalize_checkout(
     )
 
     try:
-        # when checkout total value is different than total amount from payments
-        # it means that some products has been removed during the payment was completed
-        if checkout_total.gross.amount != payment.total:
+        # when checkout total amount is less than total amount from payments
+        # it means that something changed in the checkout and we make a refund
+        # if the checkout is overpaid we allow to create the order and handle it
+        # by staff.
+        if checkout_total.gross.amount > payment.total:
             payment_refund_or_void(payment, manager, checkout_info.channel.slug)
             raise ValidationError(
-                "Cannot complete checkout - some products do not exist anymore."
+                "Cannot complete checkout - payment doesn't cover the checkout total."
             )
 
         order, _, _ = complete_checkout(
@@ -255,14 +266,12 @@ def _update_payment_with_new_transaction(
 def _process_payment_with_checkout(
     payment: Payment,
     payment_intent: StripeObject,
+    checkout: Checkout,
     kind: str,
     amount: str,
     currency: str,
 ):
-    checkout = _get_checkout(payment.id)
-
-    if checkout:
-        _finalize_checkout(checkout, payment, payment_intent, kind, amount, currency)
+    _finalize_checkout(checkout, payment, payment_intent, kind, amount, currency)
 
 
 def _update_payment_method_metadata(
@@ -290,7 +299,7 @@ def update_payment_method_details_from_intent(
 def handle_authorized_payment_intent(
     payment_intent: StripeObject, gateway_config: "GatewayConfig", channel_slug: str
 ):
-    payment = _get_payment(payment_intent.id)
+    payment = _get_payment(payment_intent.id, with_lock=False)
 
     if not payment:
         logger.warning(
@@ -298,6 +307,13 @@ def handle_authorized_payment_intent(
             extra={"payment_intent": payment_intent.id},
         )
         return
+
+    # We apply the lock in the same order as in the checkoutComplete logic. By
+    # reverting these calls we have a risk of deadlocks.
+    checkout = _get_checkout(payment.id)
+    payment = _get_payment(payment_intent.id, with_lock=True)
+    # payment was already fetch, we are sure that it exists
+    payment = cast(Payment, payment)
 
     if _channel_slug_is_different_from_payment_channel_slug(channel_slug, payment):
         return
@@ -329,10 +345,11 @@ def handle_authorized_payment_intent(
         # Order already created
         return
 
-    if payment.checkout_id:
+    if checkout:
         _process_payment_with_checkout(
             payment,
             payment_intent,
+            checkout=checkout,
             kind=TransactionKind.AUTH,
             amount=payment_intent.amount,
             currency=payment_intent.currency,
@@ -369,7 +386,7 @@ def handle_failed_payment_intent(
 def handle_processing_payment_intent(
     payment_intent: StripeObject, _gateway_config: "GatewayConfig", channel_slug: str
 ):
-    payment = _get_payment(payment_intent.id)
+    payment = _get_payment(payment_intent.id, with_lock=False)
 
     if not payment:
         logger.warning(
@@ -389,10 +406,18 @@ def handle_processing_payment_intent(
         # Order already created
         return
 
-    if payment.checkout_id:
+    # We apply the lock in the same order as in the checkoutComplete logic. By
+    # reverting these calls we have a risk of deadlocks.
+    checkout = _get_checkout(payment.id)
+    payment = _get_payment(payment_intent.id, with_lock=True)
+    # payment was already fetch, we are sure that it exists
+    payment = cast(Payment, payment)
+
+    if checkout:
         _process_payment_with_checkout(
             payment,
             payment_intent,
+            checkout,
             TransactionKind.PENDING,
             amount=payment_intent.amount,
             currency=payment_intent.currency,
@@ -402,7 +427,7 @@ def handle_processing_payment_intent(
 def handle_successful_payment_intent(
     payment_intent: StripeObject, gateway_config: "GatewayConfig", channel_slug: str
 ):
-    payment = _get_payment(payment_intent.id)
+    payment = _get_payment(payment_intent.id, with_lock=False)
 
     if not payment:
         logger.warning(
@@ -410,6 +435,13 @@ def handle_successful_payment_intent(
             extra={"payment_intent": payment_intent.id},
         )
         return
+
+    # We apply the lock in the same order as in the checkoutComplete logic. By
+    # reverting these calls we have a risk of deadlocks.
+    checkout = _get_checkout(payment.id)
+    payment = _get_payment(payment_intent.id, with_lock=True)
+    # payment was already fetch, we are sure that it exists
+    payment = cast(Payment, payment)
 
     if _channel_slug_is_different_from_payment_channel_slug(channel_slug, payment):
         return
@@ -448,11 +480,12 @@ def handle_successful_payment_intent(
             )
         return
 
-    if payment.checkout_id:
+    if checkout:
         _process_payment_with_checkout(
             payment,
             payment_intent,
-            TransactionKind.CAPTURE,
+            checkout=checkout,
+            kind=TransactionKind.CAPTURE,
             amount=payment_intent.amount_received,
             currency=payment_intent.currency,
         )
