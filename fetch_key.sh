2767919af17796633e04cd18766902a66299beaa
Paul Tindall <paul@wellstandhealth.com>
2023-12-01 16:25:40 -0600

    WSP-438 update key

--- fetch_key.sh
+++ fetch_key.sh
@@ -1,1 +1,1 @@
-gcloud storage cp gs://wellstand/non-production-381217-b20c06b17263.json google-build-key.json
+gcloud storage cp gs://wellstand/non-production-381217-6dd7eb93ce89.json google-build-key.json
