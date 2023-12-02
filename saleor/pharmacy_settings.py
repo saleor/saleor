84f7c6d01bdf17efaba0047d151f1c37748be557
Paul Tindall <paul@wellstandhealth.com>
2023-12-01 19:08:59 -0600

    WSP-438 fix CIDR settings

--- saleor/pharmacy_settings.py
+++ saleor/pharmacy_settings.py
@@ -476,7 +476,9 @@ TEST_RUNNER = "saleor.tests.runner.PytestTestRunner"
 PLAYGROUND_ENABLED = get_bool_from_env("PLAYGROUND_ENABLED", True)
 
 ALLOWED_HOSTS = get_list(os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1"))
-ALLOWED_GRAPHQL_ORIGINS: list[str] = get_list(
+ALLOWED_CIDR_NETS = get_list(os.environ.get("ALLOWED_CIDR_NETS", "127.0.0.1/32"))
+
+ALLOWED_GRAPHQL_ORIGINS: List[str] = get_list(
     os.environ.get("ALLOWED_GRAPHQL_ORIGINS", "*")
 )
 
