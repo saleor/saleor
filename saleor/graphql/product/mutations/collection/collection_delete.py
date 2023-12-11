7e1b160f057da775bb6f4cd3b84ba33c338aab3d
Tomasz Szymanski <lime129@gmail.com>
2023-12-11 10:50:19 +0100

    Optimize collections create and delete

--- saleor/graphql/product/mutations/collection/collection_delete.py
+++ saleor/graphql/product/mutations/collection/collection_delete.py
@@ -2,7 +2,10 @@ import graphene
 
 from .....permission.enums import ProductPermissions
 from .....product import models
-from .....product.tasks import update_products_discounted_prices_task
+from .....product.tasks import (
+    collection_product_updated_task,
+    update_products_discounted_prices_task,
+)
 from ....channel import ChannelContext
 from ....core import ResolveInfo
 from ....core.mutations import ModelDeleteMutation
@@ -23,28 +26,32 @@ class CollectionDelete(ModelDeleteMutation):
         error_type_class = CollectionError
         error_type_field = "collection_errors"
 
+    @staticmethod
+    def batch_product_ids(ids):
+        # Batch size of 25k ids, assuming their pks are at least 7 digits each
+        # after json serialization, weights 225kB of payload.
+        BATCH_SIZE = 25000
+        _length = len(ids)
+        for i in range(0, _length, BATCH_SIZE):
+            yield ids[i : min(i + BATCH_SIZE, _length)]
+
     @classmethod
     def perform_mutation(  # type: ignore[override]
         cls, _root, info: ResolveInfo, /, *, id: str
     ):
         instance = cls.get_node_or_error(info, id, only_type=Collection)
-        products = list(
-            instance.products.prefetched_for_webhook(  # type: ignore[attr-defined]
-                single_object=False
-            )
-        )
+        product_ids = list(instance.products.values_list("id", flat=True))
         collection_on_sale = instance.sale_set.exists()
 
         result = super().perform_mutation(_root, info, id=id)
         manager = get_plugin_manager_promise(info.context).get()
         cls.call_event(manager.collection_deleted, instance)
-        for product in products:
-            cls.call_event(manager.product_updated, product)
 
-        if collection_on_sale:
-            update_products_discounted_prices_task.delay(
-                [product.id for product in products]
-            )
+        for ids_batch in cls.batch_product_ids(product_ids):
+            collection_product_updated_task.delay(ids_batch)
+
+        if product_ids and collection_on_sale:
+            update_products_discounted_prices_task.delay(product_ids)
 
         return CollectionDelete(
             collection=ChannelContext(node=result.collection, channel_slug=None)
