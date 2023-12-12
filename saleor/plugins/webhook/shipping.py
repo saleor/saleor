e20d3a8ca6eb86863add20d42a74440d0214736d
Jakub <jakub.kuc@saleor.io>
2023-12-12 16:30:06 +0100

    remove optional field  maximum_delivery_days from validation (#14994)

--- saleor/plugins/webhook/shipping.py
+++ saleor/plugins/webhook/shipping.py
@@ -80,7 +80,7 @@ def parse_list_shipping_methods_response(
 def validate_shipping_method_data(shipping_method_data):
     if type(shipping_method_data) is not dict:
         return False
-    keys = ["id", "name", "amount", "currency", "maximum_delivery_days"]
+    keys = ["id", "name", "amount", "currency"]
     return all(key in shipping_method_data for key in keys)
 
 
