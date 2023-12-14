e15b97de1a20caf74f15617d067f960057e0be1f
Jakub <jakub.kuc@saleor.io>
2023-12-14 09:24:41 +0100

    Fix handling subscription with empty data (#14988)
    * Fix handling subscription with empty data (#14858)
    
    * remove error raising for empty subscription data and add tests
    
    * ix linters
    
    * add check for webhook response structure
    
    * add test and handle incorrect structure payloads
    
    * delete files added by mistake
    
    * change validation for shipping method list
    
    * cosmetic fixes
    
    * fix ruff
    
    * fix mock in test
    
    * remove optional field  maximum_delivery_days from validation

--- saleor/plugins/webhook/tests/test_shipping_webhook.py
+++ saleor/plugins/webhook/tests/test_shipping_webhook.py
@@ -15,6 +15,8 @@ from ....webhook.payloads import (
 )
 from ....webhook.transport.shipping import (
     get_excluded_shipping_methods_from_response,
+    get_excluded_shipping_methods_or_fetch,
+    parse_list_shipping_methods_response,
     to_shipping_app_id,
 )
 from ....webhook.transport.synchronous.transport import trigger_webhook_sync
@@ -886,3 +888,44 @@ def test_generate_excluded_shipping_methods_for_checkout_payload(
     )
     assert "checkout" in json_payload
     assert "channel" in json_payload["checkout"]
+
+
+@mock.patch("saleor.webhook.transport.shipping.parse_excluded_shipping_methods")
+@mock.patch("saleor.webhook.transport.shipping.trigger_webhook_sync")
+@mock.patch(
+    "saleor.webhook.transport.shipping.get_excluded_shipping_methods_from_response"
+)
+def test_get_excluded_shipping_methods_or_fetch_invalid_response_type(
+    mocked_get_excluded,
+    mocked_webhook_sync_trigger,
+    mocked_parse,
+    app,
+    checkout,
+):
+    # given
+    mocked_webhook_sync_trigger.return_value = ["incorrect_type"]
+    webhook = Webhook.objects.create(
+        name="Simple webhook", app=app, target_url="http://www.example.com/test"
+    )
+    event_type = WebhookEventSyncType.ORDER_FILTER_SHIPPING_METHODS
+    webhook.events.create(event_type=event_type)
+    webhooks = Webhook.objects.all()
+
+    # when
+    get_excluded_shipping_methods_or_fetch(
+        webhooks, event_type, '{"test":"payload"}', "test", checkout
+    )
+    # then
+    mocked_get_excluded.asssert_not_called()
+    mocked_parse.assert_called_once_with([])
+
+
+def test_parse_list_shipping_methods_response_response_incorrect_format(app):
+    # given
+    response_data_with_incorrect_format = [[1], 2, "3"]
+    # when
+    result = parse_list_shipping_methods_response(
+        response_data_with_incorrect_format, app
+    )
+    # then
+    assert result == []
