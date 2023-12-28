aaa67b475647b54a8f687eefd6eb1f49bc80c1ca
Maciej Korycinski <korycinski.maciej@gmail.com>
2023-12-28 11:02:34 +0100

    Improve test, to check if dataloader is used correctly

--- saleor/graphql/warehouse/tests/benchmark/test_stock_bulk_update.py
+++ saleor/graphql/warehouse/tests/benchmark/test_stock_bulk_update.py
@@ -15,6 +15,12 @@ STOCKS_BULK_UPDATE_MUTATION = """
                     code
                 }
                 stock{
+                    warehouse{
+                        id
+                    }
+                    productVariant{
+                        id
+                    }
                     id
                     quantity
                 }
@@ -71,7 +77,7 @@ def test_stocks_bulk_update_queries_count(
     ]
 
     # test number of queries when single object is updated
-    with django_assert_num_queries(10):
+    with django_assert_num_queries(12):
         staff_api_client.user.user_permissions.add(permission_manage_products)
         response = staff_api_client.post_graphql(
             STOCKS_BULK_UPDATE_MUTATION, {"stocks": stocks_input}
@@ -107,7 +113,7 @@ def test_stocks_bulk_update_queries_count(
     ]
 
     # Test number of queries when multiple objects are updated
-    with django_assert_num_queries(10):
+    with django_assert_num_queries(12):
         staff_api_client.user.user_permissions.add(permission_manage_products)
         response = staff_api_client.post_graphql(
             STOCKS_BULK_UPDATE_MUTATION, {"stocks": stocks_input}
