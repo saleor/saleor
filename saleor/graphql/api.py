ec81e34dd0c0ad1b6073855148f215241a000807
Patryk Zawadzki <81205+patrys@users.noreply.github.com>
2023-12-28 10:24:02 +0100

    Add an LRU cache for GraphQL document parser and validator (#14843)
    * Add an LRU cache for GraphQL document parser and validator
    
    Parsing and validating queries costs precious milliseconds. We could avoid doing it for popular queries.
    
    * Fix scalar coercion to not raise exceptions
    
    The protocol is to return None for invalid values and literals.
    
    * Fix JSONString and tests
    
    * Add a changelog entry

--- saleor/graphql/api.py
+++ saleor/graphql/api.py
@@ -1,8 +1,21 @@
+from functools import partial
+
 import graphql
 from django.urls import reverse
 from django.utils.functional import SimpleLazyObject
-from graphql import GraphQLScalarType
+from graphql import (
+    GraphQLCachedBackend,
+    GraphQLCoreBackend,
+    GraphQLScalarType,
+    GraphQLSchema,
+    execute,
+    parse,
+    validate,
+)
+from graphql.backend.base import GraphQLDocument
+from graphql.execution import ExecutionResult
 
+from ..core.utils.cache import CacheDict
 from ..graphql.notifications.schema import ExternalNotificationMutations
 from .account.schema import AccountMutations, AccountQueries
 from .app.schema import AppMutations, AppQueries
@@ -168,3 +181,35 @@ schema = build_federated_schema(
     directives=graphql.specified_directives
     + [GraphQLDocDirective, GraphQLWebhookEventsInfoDirective],
 )
+
+
+def _fail(errors, **_kwargs) -> ExecutionResult:
+    return ExecutionResult(errors=errors, invalid=True)
+
+
+class SaleorGraphQLBackend(GraphQLCoreBackend):
+    def document_from_string(
+        self,
+        schema: GraphQLSchema,
+        document_string: str,  # type: ignore[override]
+    ) -> GraphQLDocument:
+        # validate eagerly so we can cache the result
+        document_ast = parse(document_string)
+        validation_errors = validate(schema, document_ast)
+        if validation_errors:
+            return GraphQLDocument(
+                schema=schema,
+                document_string=document_string,
+                document_ast=document_ast,
+                execute=partial(_fail, validation_errors),
+            )
+
+        return GraphQLDocument(
+            schema=schema,
+            document_string=document_string,
+            document_ast=document_ast,
+            execute=partial(execute, schema, document_ast, **self.execute_params),
+        )
+
+
+backend = GraphQLCachedBackend(SaleorGraphQLBackend(), cache_map=CacheDict(1000))
