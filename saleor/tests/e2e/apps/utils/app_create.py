ed71d939a6ff11dd9ad6b1bc7902cc72248728c1
Anna Szczęch <30683248+szczecha@users.noreply.github.com>
2024-01-04 11:12:57 +0100

    [Test] Use external shipping methods in the checkout (#14820)
    * add test for listing external shipping methods
    
    * add vcr matcher
    
    * cr fixes
    
    * rebase

---
+++ saleor/tests/e2e/apps/utils/app_create.py
@@ -0,0 +1,35 @@
+from ...utils import get_graphql_content
+
+APP_CREATE_MUTATION = """
+mutation AppCreate($input: AppInput!) {
+  appCreate(input: $input) {
+    errors {
+      field
+      code
+      message
+    }
+    authToken
+    app {
+      id
+      isActive
+    }
+  }
+}
+
+"""
+
+
+def add_app(
+    staff_api_client,
+    input,
+):
+    variables = {"input": input}
+
+    response = staff_api_client.post_graphql(APP_CREATE_MUTATION, variables)
+    content = get_graphql_content(response)
+
+    assert content["data"]["appCreate"]["errors"] == []
+    data = content["data"]["appCreate"]
+    assert data["app"]["id"] is not None
+
+    return data
--- saleor/tests/e2e/utils.py
+++ saleor/tests/e2e/utils.py
@@ -1,3 +1,5 @@
+import json
+
 from ...account.models import Group
 from ...graphql.tests.utils import (
     get_graphql_content,  # noqa: F401
@@ -18,3 +20,23 @@ def assign_permissions(api_client, permissions):
         app = api_client.app
         if app:
             app.permissions.add(*permissions)
+
+
+def request_matcher(r1, r2):
+    body1 = json.loads(r1.body.decode("utf-8"))
+    body2 = json.loads(r2.body.decode("utf-8"))
+
+    # Check if all key-value pairs in body1 are present in body2
+    if set(body1.keys()) != set(body2.keys()):
+        return False
+
+    for key, value in body1.items():
+        if key not in body2 or body2[key] != value:
+            return False
+
+    # Check if there are any extra key-value pairs in body2
+    for key in body2:
+        if key not in body1:
+            return False
+
+    return True
