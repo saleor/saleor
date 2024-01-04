5e0ced7c68465256ec243986ed43ddb634fbc1a8
Filip Owczarek <filip.owczarek@saleor.io>
2024-01-04 15:23:56 +0100

    Adjust batch size of removed apps in task (#15170)

--- saleor/app/tasks.py
+++ saleor/app/tasks.py
@@ -82,7 +82,7 @@ def remove_apps_task():
         # Saleor uses batch size here to prevent timeouts on database.
         # Batch size determines how many deliveries will be removed,
         # each delivery contains attempts and payloads.
-        batch_size = 10000
+        batch_size = 1000
         last_id = 0
         while True:
             deliveries_ids = list(
