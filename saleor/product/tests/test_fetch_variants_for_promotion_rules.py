cdb69ec92371d75f11cf5f3df9de262deec89eb5
zedzior <piotr.zabieglik@gmail.com>
2024-01-05 14:30:52 +0100

    Add test for race condition.

--- saleor/product/tests/test_fetch_variants_for_promotion_rules.py
+++ saleor/product/tests/test_fetch_variants_for_promotion_rules.py
@@ -1,9 +1,11 @@
 from decimal import Decimal
 
+import before_after
 import graphene
 
 from ...discount import RewardValueType
 from ...discount.models import PromotionRule
+from ...discount.utils import update_rule_variant_relation
 from ..utils.variants import fetch_variants_for_promotion_rules
 
 
@@ -112,3 +114,43 @@ def test_fetch_variants_for_promotion_rules_relation_already_exist(
     # then
     rule.refresh_from_db()
     assert rule.variants.count() > 0
+
+
+def test_fetch_variants_for_promotion_rules_discount_race_condition(
+    promotion_without_rules,
+    channel_USD,
+    product_variant_list,
+):
+    # given
+    promotion = promotion_without_rules
+    existing_variant = product_variant_list[0]
+    new_variant = product_variant_list[1]
+
+    percentage_reward_value = Decimal("10")
+    rule = promotion.rules.create(
+        name="Percentage promotion rule",
+        catalogue_predicate={
+            "variantPredicate": {
+                "ids": [
+                    graphene.Node.to_global_id("ProductVariant", new_variant.id),
+                ]
+            }
+        },
+        reward_value_type=RewardValueType.PERCENTAGE,
+        reward_value=percentage_reward_value,
+    )
+    rule.channels.add(channel_USD)
+    rule.variants.add(existing_variant)
+
+    # when
+    with before_after.after(
+        "saleor.product.utils.variants.update_rule_variant_relation",
+        update_rule_variant_relation,
+    ):
+        fetch_variants_for_promotion_rules(PromotionRule.objects.all())
+
+    # then
+    rule.refresh_from_db()
+    rule_variants = rule.variants.all()
+    assert len(rule_variants) == 1
+    assert new_variant in rule_variants
