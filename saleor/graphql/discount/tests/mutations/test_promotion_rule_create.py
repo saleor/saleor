cd5e927cae926099449b80496b305294ce1b2f0c
SzymJ <31098825+SzymJ@users.noreply.github.com>
2024-01-25 15:18:10 +0100

    Fix creating promotion rule with metadata (#15289)

--- saleor/graphql/discount/tests/mutations/test_promotion_rule_create.py
+++ saleor/graphql/discount/tests/mutations/test_promotion_rule_create.py
@@ -932,3 +932,53 @@ def test_promotion_rule_create_serializable_decimal_in_predicate(
     data = content["data"]["promotionRuleCreate"]
     assert not data["errors"]
     assert data["promotionRule"]["cataloguePredicate"] == catalogue_predicate
+
+
+def test_promotion_rule_create_with_metadata(
+    staff_api_client,
+    permission_group_manage_discounts,
+    description_json,
+    channel_USD,
+    channel_PLN,
+    variant,
+    product,
+    collection,
+    category,
+    promotion,
+):
+    # given
+    permission_group_manage_discounts.user_set.add(staff_api_client.user)
+
+    channel_ids = [
+        graphene.Node.to_global_id("Channel", channel.pk)
+        for channel in [channel_USD, channel_PLN]
+    ]
+    catalogue_predicate = {
+        "OR": [{"variantPredicate": {"metadata": [{"key": "test", "value": "test"}]}}]
+    }
+    name = "test promotion rule"
+    reward_value = Decimal("10")
+    reward_value_type = RewardValueTypeEnum.PERCENTAGE.name
+    promotion_id = graphene.Node.to_global_id("Promotion", promotion.id)
+
+    variables = {
+        "input": {
+            "name": name,
+            "promotion": promotion_id,
+            "description": description_json,
+            "channels": channel_ids,
+            "rewardValueType": reward_value_type,
+            "rewardValue": reward_value,
+            "cataloguePredicate": catalogue_predicate,
+        }
+    }
+
+    # when
+    response = staff_api_client.post_graphql(PROMOTION_RULE_CREATE_MUTATION, variables)
+
+    # then
+    content = get_graphql_content(response)
+    data = content["data"]["promotionRuleCreate"]
+
+    assert not data["errors"]
+    assert data["promotionRule"]
