fdaa031d525a6302aa376afd2c706d726f4097cb
Krzysztof Waliczek <krzysztof.waliczek@saleor.io>
2024-02-13 09:00:45 +0100

    Fix setting invalid identifier (#15373)
    * Fix setting invalid identifier
    
    * Review fixes

--- saleor/app/management/commands/create_app.py
+++ saleor/app/management/commands/create_app.py
@@ -1,6 +1,7 @@
 import json
 from typing import Any, Dict, Optional
 
+import graphene
 import requests
 from django.conf import settings
 from django.contrib.sites.models import Site
@@ -20,6 +21,15 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument("name", type=str)
+        parser.add_argument(
+            "--identifier",
+            dest="identifier",
+            default="",
+            help=(
+                "Canonical app ID. If not provided, "
+                "the identifier will be generated based on app.id."
+            ),
+        )
         parser.add_argument(
             "--permission",
             action="append",
@@ -66,9 +76,13 @@ class Command(BaseCommand):
         name = options["name"]
         is_active = options["activate"]
         target_url = options["target_url"]
+        identifier = options["identifier"]
         permissions = list(set(options["permissions"]))
         permissions = clean_permissions(permissions)
-        app = App.objects.create(name=name, is_active=is_active)
+        app = App.objects.create(name=name, is_active=is_active, identifier=identifier)
+        if not identifier:
+            app.identifier = graphene.Node.to_global_id("App", app.pk)
+            app.save()
         app.permissions.set(permissions)
         _, auth_token = app.tokens.create()  # type: ignore[call-arg] # method of a related manager # noqa: E501
         data = {
