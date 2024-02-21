17abbfafc7d8f98f0376a27a896446e52a8d403d
Przemysław Łada <przlada@gmail.com>
2024-02-21 09:46:39 +0100

    Declare to use all databases in tests

--- conftest.py
+++ conftest.py
@@ -10,12 +10,8 @@ from django.test.testcases import TransactionTestCase
 
 from saleor.tests.utils import prepare_test_db_connections
 
-databases = {
-    settings.DATABASE_CONNECTION_DEFAULT_NAME,
-    settings.DATABASE_CONNECTION_REPLICA_NAME,
-}
-django.test.TransactionTestCase.databases = databases
-django.test.TestCase.databases = databases
+django.test.TransactionTestCase.databases = "__all__"
+django.test.TestCase.databases = "__all__"
 
 prepare_test_db_connections()
 
