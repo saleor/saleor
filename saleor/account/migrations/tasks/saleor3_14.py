8952be5d96d19f4d124e3382659a205719fc691d
IKarbowiak <iga.karbowiak@saleor.io>
2024-02-29 14:28:14 +0100

    Confirm active users

--- saleor/account/migrations/tasks/saleor3_14.py
+++ saleor/account/migrations/tasks/saleor3_14.py
@@ -16,14 +16,32 @@ def set_user_is_confirmed_to_false(qs: QuerySet["User"]):
 
 
 @app.task
-def set_user_is_confirmed_task(email_confirmation):
+def set_user_is_confirmed_task():
     users = User.objects.order_by("pk").filter(is_confirmed=True)
-    if email_confirmation:
-        users = users.filter(is_active=False, last_login__isnull=True)
+    users = users.filter(is_active=False, last_login__isnull=True)
     ids = users.values_list("pk", flat=True)[:BATCH_SIZE]
 
     qs = User.objects.filter(pk__in=ids)
-
     if ids:
         set_user_is_confirmed_to_false(qs)
-        set_user_is_confirmed_task.delay(email_confirmation)
+        set_user_is_confirmed_task.delay()
+
+
+def set_user_is_confirmed_to_true(qs: QuerySet["User"]):
+    with transaction.atomic():
+        # lock the batch of objects
+        _users = list(qs.select_for_update(of=(["self"])))
+        qs.update(is_confirmed=True)
+
+
+@app.task
+def confirm_active_users_task():
+    users = User.objects.order_by("pk").filter(
+        is_confirmed=False, is_active=True, last_login__isnull=False
+    )
+    ids = users.values_list("pk", flat=True)[:BATCH_SIZE]
+
+    qs = User.objects.filter(pk__in=ids)
+    if ids:
+        set_user_is_confirmed_to_true(qs)
+        confirm_active_users_task.delay()
