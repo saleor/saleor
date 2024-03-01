312de1ed2038c1adebedd3131aea20b7bfec83e2
Przemysław Łada <przlada@gmail.com>
2024-03-01 13:12:27 +0100

    Set DB_CONN_MAX_AGE to 0

--- saleor/settings.py
+++ saleor/settings.py
@@ -91,7 +91,7 @@ INTERNAL_IPS = get_list(os.environ.get("INTERNAL_IPS", "127.0.0.1"))
 # Maximum time in seconds Django can keep the database connections opened.
 # Set the value to 0 to disable connection persistence, database connections
 # will be closed after each request.
-DB_CONN_MAX_AGE = int(os.environ.get("DB_CONN_MAX_AGE", 600))
+DB_CONN_MAX_AGE = 0  # int(os.environ.get("DB_CONN_MAX_AGE", 600))
 
 DATABASE_CONNECTION_DEFAULT_NAME = "default"
 # TODO: For local envs will be activated in separate PR.
