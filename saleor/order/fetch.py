8681dae4ab75f49fc099e0530390aa6e45f6955a
zedzior <piotr.zabieglik@gmail.com>
2024-03-11 14:25:59 +0100

    Use prefecthed data to get channel listings.

--- saleor/order/fetch.py
+++ saleor/order/fetch.py
@@ -117,8 +117,8 @@ def fetch_draft_order_lines_info(
     channel = order.channel
     for line in lines:
         variant = cast(ProductVariant, line.variant)
-        variant_channel_listing = ProductVariantChannelListing.objects.filter(
-            channel=channel, variant=variant
+        variant_channel_listing = variant.channel_listings.filter(
+            channel=channel
         ).first()
         if not variant_channel_listing:
             continue
