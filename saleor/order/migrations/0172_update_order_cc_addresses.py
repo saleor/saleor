cdba3cc2ce7256a2eb2f9a4883475347ea775d23
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2024-03-26 11:02:41 +0100

    Fix relation between order and click and collect address (#15687)
    * Create a copy of collection point address during checkout completion
    
    * Add migration for creating order shipping addresses

---
+++ saleor/order/migrations/0172_update_order_cc_addresses.py
@@ -0,0 +1,22 @@
+from django.db import migrations
+from django.apps import apps as registry
+from django.db.models.signals import post_migrate
+from .tasks.saleor3_17 import update_order_addresses_task
+
+
+def update_order_addresses(apps, schema_editor):
+    def on_migrations_complete(sender=None, **kwargs):
+        update_order_addresses_task.delay()
+
+    sender = registry.get_app_config("order")
+    post_migrate.connect(on_migrations_complete, weak=False, sender=sender)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("order", "0171_order_order_user_email_user_id_idx"),
+    ]
+
+    operations = [
+        migrations.RunPython(update_order_addresses, migrations.RunPython.noop),
+    ]
--- saleor/order/migrations/tasks/saleor3_17.py
+++ saleor/order/migrations/tasks/saleor3_17.py
@@ -3,6 +3,9 @@ from ...models import Order
 from ....discount.models import Voucher
 from django.db.models import Exists, OuterRef
 from django.db import transaction
+from ....account.models import Address
+from ....warehouse.models import Warehouse
+from django.forms.models import model_to_dict
 
 
 # For batch size 1000 with 2000 per model Order/Voucher/VoucherCode objects
@@ -18,6 +21,9 @@ from django.db import transaction
 # Migration took 46.44 seconds.
 # Memory usage increased by 294.54 MiB.
 BATCH_SIZE = 1000
+
+# The batch of size 250 takes ~0.5 second and consumes ~20MB memory at peak
+ADDRESS_UPDATE_BATCH_SIZE = 250
 
 
 @app.task
@@ -49,3 +55,22 @@ def get_voucher_id_to_code_map(orders):
         voucher_id: code for voucher_id, code in vouchers.values_list("id", "code")
     }
     return voucher_id_to_code_map
+
+
+@app.task
+def update_order_addresses_task():
+    qs = Order.objects.filter(
+        Exists(Warehouse.objects.filter(address_id=OuterRef("shipping_address_id"))),
+    )
+    order_ids = qs.values_list("pk", flat=True)[:ADDRESS_UPDATE_BATCH_SIZE]
+    addresses = []
+    if order_ids:
+        orders = Order.objects.filter(id__in=order_ids)
+        for order in orders:
+            if cc_address := order.shipping_address:
+                order_address = Address(**model_to_dict(cc_address, exclude=["id"]))
+                order.shipping_address = order_address
+                addresses.append(order_address)
+        Address.objects.bulk_create(addresses, ignore_conflicts=True)
+        Order.objects.bulk_update(orders, ["shipping_address"])
+        update_order_addresses_task.delay()
