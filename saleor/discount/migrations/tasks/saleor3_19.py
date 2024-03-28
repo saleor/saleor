c4d7d1ff0e5a0be0721babc5659b8f21b31c4a1e
AjmalPonneth <ponnethajmal@gmail.com>
2024-03-28 21:01:28 +0530

    update constant name

--- saleor/discount/migrations/tasks/saleor3_19.py
+++ saleor/discount/migrations/tasks/saleor3_19.py
@@ -2,13 +2,13 @@ from ....celeryconf import app
 from ... import DiscountType
 from ...models import CheckoutLineDiscount, OrderLineDiscount
 
-ORDER_LINE_DISCOUNT_BATCH_SIZE = 250
+BATCH_SIZE = 250
 
 
 @app.task
 def update_discount_type_order_line_task():
     order_lines = OrderLineDiscount.objects.filter(type=DiscountType.PROMOTION)[
-        :ORDER_LINE_DISCOUNT_BATCH_SIZE
+        :BATCH_SIZE
     ]
     if not order_lines:
         return
@@ -19,7 +19,7 @@ def update_discount_type_order_line_task():
 @app.task
 def update_discount_type_checkout_line_task():
     checkout_lines = CheckoutLineDiscount.objects.filter(type=DiscountType.PROMOTION)[
-        :ORDER_LINE_DISCOUNT_BATCH_SIZE
+        :BATCH_SIZE
     ]
     if not checkout_lines:
         return
