73215e83362981c1c4cc36db220241042fec970c
Marcin Gębala <maarcin.gebala@gmail.com>
2024-04-03 17:19:26 +0200

    Update comment

--- saleor/graphql/executor.py
+++ saleor/graphql/executor.py
@@ -14,9 +14,10 @@ def _patched_complete_value_catching_error(*args, **kwargs):
 def patch_executor():
     """Patch `complete_value_catching_error` function to allow writer DB in mutations.
 
-    Patch the GraphQL executor's `complete_value_catching_error` function to wrap each
-    call with `allow_writer_in_context`. This allows to safely use writer DB in
-    mutations, while it will raise or log error when a resolver is run in a query.
+    The `complete_value_catching_error` function is called when resolving a field in
+    GraphQL. This patch wraps each call with `allow_writer_in_context` context manager.
+    This allows to use writer DB in resolvers, when they are called via mutation, while
+    they will still raise or log error when a resolver is run in a query.
     """
 
     executor.complete_value_catching_error = _patched_complete_value_catching_error
