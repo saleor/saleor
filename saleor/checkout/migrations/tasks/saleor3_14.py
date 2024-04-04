dd176a193cd4ad88673359cc29bcda18fbdb5479
Artur <31221055+Air-t@users.noreply.github.com>
2024-04-04 11:18:41 +0200

    fix checkout shipping addresses which had refference to warehowe addresses (#15744)

--- saleor/checkout/migrations/tasks/saleor3_14.py
+++ saleor/checkout/migrations/tasks/saleor3_14.py
@@ -1,13 +1,19 @@
 from django.db import transaction
 from django.db.models.expressions import Exists, OuterRef, Q
+from django.forms import model_to_dict
 
+from ....account.models import Address
 from ....celeryconf import app
 from ....payment.models import TransactionItem
 from ...models import Checkout
+from ....warehouse.models import Warehouse
 
 # It takes less that a second to process the batch.
 # The memory usage peak on celery worker was around 40MB.
 BATCH_SIZE = 2000
+
+# The batch of size 250 takes ~0.5 second and consumes ~20MB memory at peak
+ADDRESS_UPDATE_BATCH_SIZE = 250
 
 
 @app.task
@@ -61,3 +67,22 @@ def update_checkout_refundable():
             )
 
         update_checkout_refundable.delay()
+
+
+@app.task
+def update_checkout_addresses_task():
+    qs = Checkout.objects.filter(
+        Exists(Warehouse.objects.filter(address_id=OuterRef("shipping_address_id"))),
+    )
+    checkout_ids = qs.values_list("pk", flat=True)[:ADDRESS_UPDATE_BATCH_SIZE]
+    addresses = []
+    if checkout_ids:
+        checkouts = Checkout.objects.filter(pk__in=checkout_ids)
+        for checkout in checkouts:
+            if cc_address := checkout.shipping_address:
+                checkout_address = Address(**model_to_dict(cc_address, exclude=["id"]))
+                checkout.shipping_address = checkout_address
+                addresses.append(checkout_address)
+        Address.objects.bulk_create(addresses, ignore_conflicts=True)
+        Checkout.objects.bulk_update(checkouts, ["shipping_address"])
+        update_checkout_addresses_task.delay()
