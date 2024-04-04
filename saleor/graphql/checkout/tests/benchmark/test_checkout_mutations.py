f50df834455402a6c7e277ff2314705e601ca46b
zedzior <piotr.zabieglik@gmail.com>
2024-04-04 19:48:35 +0200

    Fix test.

--- saleor/graphql/checkout/tests/benchmark/test_checkout_mutations.py
+++ saleor/graphql/checkout/tests/benchmark/test_checkout_mutations.py
@@ -562,7 +562,7 @@ def test_create_checkout_with_order_promotion(
     }
 
     # when
-    with django_assert_num_queries(90):
+    with django_assert_num_queries(73):
         response = user_api_client.post_graphql(MUTATION_CHECKOUT_CREATE, variables)
 
     # then
