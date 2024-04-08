9ffcadfeabb5dc695bbccb356c1d2bda96df4151
fowczrek <f.a.owczarek@gmail.com>
2024-04-08 11:41:55 +0200

    Add price and channel index on ProductVariantChannelListing

--- saleor/product/models.py
+++ saleor/product/models.py
@@ -535,6 +535,9 @@ class ProductVariantChannelListing(models.Model):
     class Meta:
         unique_together = [["variant", "channel"]]
         ordering = ("pk",)
+        indexes = [
+            GinIndex(fields=["price_amount", "channel_id"]),
+        ]
 
 
 class VariantChannelListingPromotionRule(models.Model):
