89fe9900e985fd22a0e3048ce00d6b8887cc3c52
Marcin Gębala <maarcin.gebala@gmail.com>
2024-04-15 15:45:13 +0200

    Deprecate  query

--- saleor/graphql/core/schema.py
+++ saleor/graphql/core/schema.py
@@ -1,5 +1,6 @@
 import graphene
 
+from ..core.descriptions import DEPRECATED_IN_3X_FIELD
 from ..core.doc_category import DOC_CATEGORY_TAXES
 from ..core.fields import BaseField
 from ..plugins.dataloaders import get_plugin_manager_promise
@@ -13,6 +14,7 @@ class CoreQueries(graphene.ObjectType):
         NonNullList(TaxType),
         description="List of all tax rates available from tax gateway.",
         doc_category=DOC_CATEGORY_TAXES,
+        deprecation_reason=f"{DEPRECATED_IN_3X_FIELD} Use `taxClasses` field instead.",
     )
 
     def resolve_tax_types(self, info: ResolveInfo):
