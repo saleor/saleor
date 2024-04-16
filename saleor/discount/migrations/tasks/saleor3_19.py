4111db846e3643651a7a9045512a3b47cc2e1e89
AjmalPonneth <ponnethajmal@gmail.com>
2024-04-17 00:50:10 +0530

    add requested changes

--- saleor/discount/migrations/tasks/saleor3_19.py
+++ saleor/discount/migrations/tasks/saleor3_19.py
@@ -2,26 +2,30 @@ from ....celeryconf import app
 from ... import DiscountType
 from ...models import CheckoutLineDiscount, OrderLineDiscount
 
-BATCH_SIZE = 250
+BATCH_SIZE = 1000
 
 
 @app.task
 def update_discount_type_order_line_task():
-    order_lines = OrderLineDiscount.objects.filter(type=DiscountType.PROMOTION)[
-        :BATCH_SIZE
-    ]
+    order_lines = OrderLineDiscount.objects.filter(
+        type=DiscountType.PROMOTION
+    ).values_list("id", flat=True)[:BATCH_SIZE]
     if not order_lines:
         return
-    order_lines.update(type=DiscountType.CATALOGUE_PROMOTION)
+    OrderLineDiscount.objects.filter(id__in=order_lines).only("id").update(
+        type=DiscountType.CATALOGUE_PROMOTION
+    )
     update_discount_type_order_line_task.delay()
 
 
 @app.task
 def update_discount_type_checkout_line_task():
-    checkout_lines = CheckoutLineDiscount.objects.filter(type=DiscountType.PROMOTION)[
-        :BATCH_SIZE
-    ]
+    checkout_lines = CheckoutLineDiscount.objects.filter(
+        type=DiscountType.PROMOTION
+    ).values_list("id", flat=True)[:BATCH_SIZE]
     if not checkout_lines:
         return
-    checkout_lines.update(type=DiscountType.CATALOGUE_PROMOTION)
+    CheckoutLineDiscount.objects.filter(id__in=checkout_lines).only("id").update(
+        type=DiscountType.CATALOGUE_PROMOTION
+    )
     update_discount_type_checkout_line_task.delay()
