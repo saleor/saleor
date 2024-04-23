7b5bd05be1ea1f074c56fb78a0f788691d0f1f4a
Tomasz Szymański <lime129@gmail.com>
2024-04-23 13:25:00 +0200

    Add transaction items limit to TransactionSessionBase (#15793)
    * Add transaction items limit to TransactionSessionBase
    
    * Styling

--- saleor/settings.py
+++ saleor/settings.py
@@ -901,3 +901,8 @@ WEBHOOK_SYNC_TIMEOUT = (REQUESTS_CONN_EST_TIMEOUT, 18)
 ENABLE_LIMITING_WEBHOOKS_FOR_IDENTICAL_PAYLOADS = get_bool_from_env(
     "ENABLE_LIMITING_WEBHOOKS_FOR_IDENTICAL_PAYLOADS", False
 )
+
+
+# Transaction items limit for PaymentGatewayInitialize / TransactionInitialize.
+# That setting limits the allowed number of transaction items for single entity.
+TRANSACTION_ITEMS_LIMIT = 100
