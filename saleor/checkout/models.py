121c98d17ce0cc2ffea8d9303842cac3af179b43
IKarbowiak <iga.karbowiak@saleor.io>
2024-05-09 12:04:45 +0200

    Handle concurrency checkoutPaymentCreate and checkoutCompelte
    Extend Checkout model with completing_started_at
    
    Update completing_started_at checkout value in complete_checkout
    
    Add validation in CheckoutPaymentCreate
    
    Drop unused variable from _process_payment
    
    Adjust _release_checkout_voucher_usage

--- saleor/checkout/models.py
+++ saleor/checkout/models.py
@@ -37,6 +37,7 @@ class Checkout(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     last_change = models.DateTimeField(auto_now=True, db_index=True)
+    completing_started_at = models.DateTimeField(blank=True, null=True)
 
     # Denormalized modified_at for the latest modified transactionItem assigned to
     # checkout
@@ -217,6 +218,15 @@ class Checkout(models.Model):
     def is_shipping_required(self) -> bool:
         """Return `True` if any of the lines requires shipping."""
         return any(line.is_shipping_required() for line in self)
+
+    def is_checkout_locked(self) -> bool:
+        return bool(
+            self.completing_started_at
+            and (
+                (timezone.now() - self.completing_started_at).seconds
+                < settings.CHECKOUT_COMPLETION_LOCK_TIME
+            )
+        )
 
     def get_total_gift_cards_balance(
         self, database_connection_name: str = settings.DATABASE_CONNECTION_DEFAULT_NAME
