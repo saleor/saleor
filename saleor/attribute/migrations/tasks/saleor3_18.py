4c4aa37800eefebd8597b0e023b77d638ed872c7
kadewu <krzysztof.waliczek@saleor.io>
2024-05-21 09:52:38 +0200

    fix migrations

--- saleor/attribute/migrations/tasks/saleor3_18.py
+++ saleor/attribute/migrations/tasks/saleor3_18.py
@@ -1,33 +1,71 @@
-from ....celeryconf import app
-from ...models import AssignedPageAttributeValue, AssignedProductAttributeValue
+from django.db import connection
 
-# TODO: change to ORM
+from ....celeryconf import app
+
+BATCH = 1000
 
 
 @app.task()
 def copy_product_id():
-    AssignedProductAttributeValue.objects.raw(
-        """
-        SELECT MIN(id) AS id, product_id FROM ATTRIBUTE_ASSIGNEDPRODUCTATTRIBUTEVALUE
-        GROUP BY PRODUCT_ID, VALUE_ID
-        HAVING AVG(PRODUCT_UNIQ) IS NULL;
-        """
-    )
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            UPDATE ATTRIBUTE_ASSIGNEDPRODUCTATTRIBUTEVALUE aa
+            SET PRODUCT_UNIQ = t.PRODUCT_ID
+            FROM (
+                SELECT MIN(id) AS id, product_id
+                FROM ATTRIBUTE_ASSIGNEDPRODUCTATTRIBUTEVALUE
+                GROUP BY PRODUCT_ID, VALUE_ID
+                HAVING AVG(PRODUCT_UNIQ) IS NULL
+                LIMIT %s
+            ) t
+            WHERE aa.id = t.id;
+            """,
+            [BATCH],
+        )
+
+        cursor.execute(
+            """
+            SELECT EXISTS(
+                SELECT 1
+                FROM ATTRIBUTE_ASSIGNEDPRODUCTATTRIBUTEVALUE
+                GROUP BY PRODUCT_ID, VALUE_ID
+                HAVING AVG(PRODUCT_UNIQ) IS NULL
+            );
+            """
+        )
+        if cursor.fetchone() is True:
+            copy_product_id.delay()
 
 
 @app.task()
 def copy_page_id():
-    AssignedPageAttributeValue.objects.raw(
-        """
-        UPDATE ATTRIBUTE_ASSIGNEDPAGEATTRIBUTEVALUE aa
-        SET PAGE_UNIQ = t.PAGE_ID
-        FROM (
-            SELECT MIN(id) AS id, page_id, VALUE_ID
-            FROM ATTRIBUTE_ASSIGNEDPAGEATTRIBUTEVALUE
-            GROUP BY PAGE_ID, VALUE_ID
-            HAVING AVG(PAGE_UNIQ) IS NULL
-            LIMIT 100
-        ) t
-        WHERE aa.id = t.id;
-        """
-    )
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            UPDATE ATTRIBUTE_ASSIGNEDPAGEATTRIBUTEVALUE aa
+            SET PAGE_UNIQ = t.PAGE_ID
+            FROM (
+                SELECT MIN(id) AS id, page_id
+                FROM ATTRIBUTE_ASSIGNEDPAGEATTRIBUTEVALUE
+                GROUP BY PAGE_ID, VALUE_ID
+                HAVING AVG(PAGE_UNIQ) IS NULL
+                LIMIT %s
+            ) t
+            WHERE aa.id = t.id;
+            """,
+            [BATCH],
+        )
+
+        cursor.execute(
+            """
+            SELECT EXISTS(
+                SELECT 1
+                FROM ATTRIBUTE_ASSIGNEDPAGEATTRIBUTEVALUE
+                GROUP BY PAGE_ID, VALUE_ID
+                HAVING AVG(PAGE_UNIQ) IS NULL
+            );
+            """
+        )
+        if cursor.fetchone() is True:
+            copy_page_id.delay()
