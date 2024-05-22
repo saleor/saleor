d388ad478adc8d2bfed3b570d75a2f39d055647f
Przemysław Łada <przlada@gmail.com>
2024-05-22 11:58:32 +0200

    Add docstrings

--- saleor/tests/utils.py
+++ saleor/tests/utils.py
@@ -9,18 +9,30 @@ from ..core.db.connection import allow_writer
 
 
 class FakeDbReplicaConnection:
+    """A class used to create a fake replica DB connection for use in tests.
+
+    This class serves as a wrapper around the writer DB connection (alias "default")
+    that simulates a replica DB connection by identifying itself with a replica alias
+    and overriding cursor creation methods. It effectively results in one DB connection
+    identified by two separate aliases, but with shared transactions.
+    """
+
     def __init__(self, replica_conn):
         self.replica_conn = replica_conn
         self.writer_conn = connections[settings.DATABASE_CONNECTION_DEFAULT_NAME]
 
     def cursor(self, *args, **kwargs):
         with allow_writer():
+            # Cursor creation is wrapped in allow_writer as it is effectively created
+            # using the writer DB connection
             cursor = self.writer_conn.cursor(*args, **kwargs)
             cursor.db = self
         return cursor
 
     def chunked_cursor(self, *args, **kwargs):
         with allow_writer():
+            # Cursor creation is wrapped in allow_writer as it is effectively created
+            # using the writer DB connection
             cursor = self.writer_conn.chunked_cursor(*args, **kwargs)
             cursor.db = self
         return cursor
@@ -32,6 +44,13 @@ class FakeDbReplicaConnection:
 
 
 def prepare_test_db_connections():
+    """Override the replica DB connection with a fake one for testing purposes.
+
+    This function allows simulation of replica DB usage in tests while using Django's
+    TestCase, avoiding the need for the slower TransactionTestCase. For more details,
+    refer to the Django documentation:
+    https://docs.djangoproject.com/en/4.2/topics/testing/advanced/#testing-primary-replica-configurations
+    """
     replica = settings.DATABASE_CONNECTION_REPLICA_NAME
     connections[replica] = FakeDbReplicaConnection(connections[replica])  # type: ignore
 
