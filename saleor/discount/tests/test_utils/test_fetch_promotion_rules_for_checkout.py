6a82a8babae8f625c357d65e18b4276f00f0b98c
zedzior <piotr.zabieglik@gmail.com>
2024-05-27 15:34:21 +0200

    Fix the issue

--- saleor/discount/tests/test_utils/test_fetch_promotion_rules_for_checkout.py
+++ saleor/discount/tests/test_utils/test_fetch_promotion_rules_for_checkout.py
@@ -82,3 +82,37 @@ def test_fetch_promotion_rules_for_checkout_relevant_channel_only(
     # then
     assert len(rules_per_promotion_id) == 1
     assert rules_per_promotion_id == [rule_2]
+
+
+def test_fetch_promotion_rules_for_checkout_inner_or_operator(
+    checkout, checkout_for_cc, order_promotion_rule
+):
+    # given
+    rule = order_promotion_rule
+    rule.order_predicate = {
+        "discountedObjectPredicate": {
+            "OR": [
+                {"baseTotalPrice": {"range": {"gte": "10"}}},
+                {"baseSubtotalPrice": {"range": {"gte": "10"}}},
+            ]
+        }
+    }
+    rule.save(update_fields=["order_predicate"])
+
+    checkout.base_total_amount = 100
+    checkout.base_subtotal_amount = 100
+    checkout.total_gross_amount = 100
+    checkout.save(
+        update_fields=[
+            "total_gross_amount",
+            "base_total_amount",
+            "base_subtotal_amount",
+        ]
+    )
+
+    # when
+    rules_per_promotion_id = fetch_promotion_rules_for_checkout_or_order(checkout)
+
+    # then
+    assert len(rules_per_promotion_id) == 1
+    assert rules_per_promotion_id == [rule]
--- saleor/graphql/core/connection.py
+++ saleor/graphql/core/connection.py
@@ -489,10 +489,13 @@ def filter_qs(
     except (NoDefaultChannel, ChannelNotDefined, GraphQLError, KeyError):
         filter_channel = None
     filter_input["channel"] = (
-        args.get("channel")
+        args.pop("channel", None)
         or filter_channel
         or get_default_channel_slug_or_graphql_error(allow_replica)
     )
+
+    for arg_name, arg_value in args.items():
+        filter_input[arg_name] = arg_value
 
     if isinstance(iterable, ChannelQsContext):
         queryset = iterable.qs
