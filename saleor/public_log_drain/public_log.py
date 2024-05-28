5be7774930ff99db981c36417fdd21468a530d10
Artur Pietracha <artur.pietracha@gmail.com>
2024-05-28 16:19:11 +0200

    add LogDrainHTTPTransporter

--- saleor/public_log_drain/public_log.py
+++ saleor/public_log_drain/public_log.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 from .public_log_drain import LogDrainAttributes, PublicLogDrain
+from .transporters.public_log_drain_http_transporter import LogDrainHTTPTransporter
 from .transporters.public_log_drain_otel_transporter import LogDrainOtelTransporter
 
 
@@ -8,6 +9,8 @@ def emit_public_log(logger_name: str, trace_id: int, attributes: LogDrainAttribu
     transports = []
     if otl_endpoint := settings.OTEL_TRANSPORTED_ENDPOINT:
         transports.append(LogDrainOtelTransporter(endpoint=otl_endpoint))
+    if otl_endpoint := settings.HTTP_TRANSPORTED_ENDPOINT:
+        transports.append(LogDrainHTTPTransporter(endpoint=otl_endpoint))
     if not transports:
         return
     PublicLogDrain(transports).emit_log(logger_name, trace_id, attributes)
---
+++ saleor/public_log_drain/transporters/public_log_drain_http_transporter.py
@@ -0,0 +1,41 @@
+from django.urls import reverse
+from django.utils import timezone
+
+from ...app.headers import AppHeaders
+from ...core.http_client import HTTPClient
+from ...core.utils import build_absolute_uri
+from ..public_log_drain import LogDrainAttributes
+from . import LogDrainTransporter
+
+
+class LogDrainHTTPTransporter(LogDrainTransporter):
+    def __init__(self, endpoint: str):
+        if not endpoint:
+            raise ValueError("Endpoint is required")
+
+        self.headers = {
+            "Content-Type": "application/json",
+            AppHeaders.DOMAIN: build_absolute_uri(reverse("api")),
+        }
+
+        self.endpoint = endpoint
+
+    def get_endpoint(self):
+        return self.endpoint
+
+    def get_payload(self, trace_id: int, attributes: LogDrainAttributes):
+        data = attributes.__dict__
+        data.update(
+            timestamp=int(timezone.now().timestamp()),
+            trace_id=trace_id,
+        )
+        return data
+
+    def emit(self, logger_name: str, trace_id: int, attributes: LogDrainAttributes):
+        HTTPClient.send_request(
+            "POST",
+            self.get_endpoint(),
+            json=self.get_payload(trace_id, attributes),
+            headers=self.headers,
+            allow_redirects=False,
+        )
