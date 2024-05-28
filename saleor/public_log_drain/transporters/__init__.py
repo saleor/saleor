734e94191830e70886e08a1c9bdcde1fcb321ec8
IKarbowiak <iga.karbowiak@saleor.io>
2024-05-28 15:13:27 +0200

    Adjust LogDrainTransporter

--- saleor/public_log_drain/transporters/__init__.py
+++ saleor/public_log_drain/transporters/__init__.py
@@ -1,10 +1,11 @@
-class LogDrainTransporter:
-    abstract = True
+from abc import ABC, abstractmethod
 
+
+class LogDrainTransporter(ABC):
+    @abstractmethod
     def emit(self, logger_name: str, trace_id: int, attributes):
-        raise NotImplementedError("You must implement emit method for a transporter.")
+        pass
 
+    @abstractmethod
     def get_endpoint(self):
-        raise NotImplementedError(
-            "You must implement get_endpoint method for a transporter."
-        )
+        pass
