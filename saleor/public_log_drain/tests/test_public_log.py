9332f8b07fad3dc7d2e71e3c8f0b65469e266bc4
Artur Pietracha <artur.pietracha@gmail.com>
2024-05-29 10:36:22 +0200

    fix tests

--- saleor/public_log_drain/tests/test_public_log.py
+++ saleor/public_log_drain/tests/test_public_log.py
@@ -7,23 +7,22 @@ from ..tasks import emit_public_log
 
 
 @override_settings(OTEL_TRANSPORTED_ENDPOINT=None, HTTP_TRANSPORTED_ENDPOINT=None)
-@patch("saleor.public_log_drain.public_log.PublicLogDrain.emit_log")
+@patch("saleor.public_log_drain.tasks.PublicLogDrain.emit_log")
 def test_emit_public_log_no_transporters(mocked_emit_log):
     # given
     logger_name = "test_name"
     trace_id = 1
     span_id = 1
     attributes = LogDrainAttributes(
-        type=LogType.WEBHOOK_SENT,
-        level=LogLevel.INFO,
-        api_url="test-api-url",
+        type=LogType.WEBHOOK_SENT.name,
+        level=LogLevel.INFO.name,
         message="Test message",
-        version=1,
+        version="1",
+        checkout_id=None,
+        order_id=None,
     )
-
     # when
-    emit_public_log(logger_name, trace_id, span_id, attributes)
-
+    emit_public_log(logger_name, trace_id, span_id, attributes.__dict__)
     # then
     mocked_emit_log.assert_not_called()
 
@@ -31,47 +30,45 @@ def test_emit_public_log_no_transporters(mocked_emit_log):
 @override_settings(
     OTEL_TRANSPORTED_ENDPOINT="test-endpoint", HTTP_TRANSPORTED_ENDPOINT=None
 )
-@patch("saleor.public_log_drain.public_log.PublicLogDrain.emit_log")
+@patch("saleor.public_log_drain.tasks.PublicLogDrain.emit_log")
 def test_emit_public_log_otel(mocked_emit_log):
     # given
     logger_name = "test_name"
     trace_id = 1
     span_id = 1
     attributes = LogDrainAttributes(
-        type=LogType.WEBHOOK_SENT,
-        level=LogLevel.INFO,
-        api_url="test-api-url",
+        type=LogType.WEBHOOK_SENT.name,
+        level=LogLevel.INFO.name,
         message="Test message",
-        version=1,
+        version="1",
+        checkout_id=None,
+        order_id=None,
     )
-
     # when
-    emit_public_log(logger_name, trace_id, span_id, attributes)
-
+    emit_public_log(logger_name, trace_id, span_id, attributes.__dict__)
     # then
-    mocked_emit_log.assert_called_once_with(logger_name, trace_id, attributes)
+    mocked_emit_log.assert_called_once_with(logger_name, trace_id, span_id, attributes)
 
 
 @override_settings(
     HTTP_TRANSPORTED_ENDPOINT="test-endpoint", OTEL_TRANSPORTED_ENDPOINT=None
 )
-@patch("saleor.public_log_drain.public_log.PublicLogDrain.emit_log")
+@patch("saleor.public_log_drain.tasks.PublicLogDrain.emit_log")
 def test_emit_public_log_http(mocked_emit_log):
     # given
     logger_name = "test_name"
     trace_id = 1
     span_id = 1
     attributes = LogDrainAttributes(
-        type=LogType.WEBHOOK_SENT,
-        level=LogLevel.INFO,
-        api_url="test-api-url",
+        type=LogType.WEBHOOK_SENT.name,
+        level=LogLevel.INFO.name,
         message="Test message",
-        version=1,
+        version="1",
+        checkout_id=None,
+        order_id=None,
     )
-
     # when
-    emit_public_log(logger_name, trace_id, attributes)
-
+    emit_public_log(logger_name, trace_id, span_id, attributes.__dict__)
     # then
     mocked_emit_log.assert_called_once_with(logger_name, trace_id, span_id, attributes)
 
@@ -80,22 +77,21 @@ def test_emit_public_log_http(mocked_emit_log):
     HTTP_TRANSPORTED_ENDPOINT="test-endpoint-http",
     OTEL_TRANSPORTED_ENDPOINT="test-endpoint-otel",
 )
-@patch("saleor.public_log_drain.public_log.PublicLogDrain.emit_log")
+@patch("saleor.public_log_drain.tasks.PublicLogDrain.emit_log")
 def test_emit_public_log_otel_and_http(mocked_emit_log):
     # given
     logger_name = "test_name"
     trace_id = 1
     span_id = 1
     attributes = LogDrainAttributes(
-        type=LogType.WEBHOOK_SENT,
-        level=LogLevel.INFO,
-        api_url="test-api-url",
+        type=LogType.WEBHOOK_SENT.name,
+        level=LogLevel.INFO.name,
         message="Test message",
-        version=1,
+        version="1",
+        checkout_id=None,
+        order_id=None,
     )
-
     # when
-    emit_public_log(logger_name, trace_id, span_id, attributes)
-
+    emit_public_log(logger_name, trace_id, span_id, attributes.__dict__)
     # then
-    mocked_emit_log.assert_called_once_with(logger_name, trace_id, attributes)
+    mocked_emit_log.assert_called_once_with(logger_name, trace_id, span_id, attributes)
