4c95d6956a91a520847f3c08f075a6ae5ca4eb90
Jakub <jakub.kuc@saleor.io>
2024-06-04 09:55:00 +0200

    Add more fields to external shipping method payload parsing (#15935)
    * add more fields to externall shipping method payload parser
    
    * fix linters
    
    * add structure validation and tests
    
    * fix linters
    
    * remove wrong files
    
    * change none to dict to be typing consistent

--- saleor/plugins/webhook/tests/test_shipping_webhook.py
+++ saleor/plugins/webhook/tests/test_shipping_webhook.py
@@ -938,3 +938,24 @@ def test_parse_list_shipping_methods_response_response_incorrect_format(app):
     )
     # then
     assert result == []
+
+
+def test_parse_list_shipping_methods_with_metadata(app):
+    # given
+    response_data_with_meta = [
+        {
+            "id": 123,
+            "amount": 10,
+            "currency": "USD",
+            "name": "shipping",
+            "description": "Description",
+            "maximum_delivery_days": 10,
+            "minimum_delivery_days": 2,
+            "metadata": {"field": "value"},
+        }
+    ]
+    # when
+    response = parse_list_shipping_methods_response(response_data_with_meta, app)
+    # then
+    assert response[0].metadata == response_data_with_meta[0]["metadata"]
+    assert response[0].description == response_data_with_meta[0]["description"]
