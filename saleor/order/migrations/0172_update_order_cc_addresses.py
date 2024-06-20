bc9e300332ee89c28405401ec2e200f5ebe8bd1f
Przemysław Łada <przlada@gmail.com>
2024-06-20 12:57:03 +0200

    Fix update_order_cc_addresses migration

--- saleor/order/migrations/0172_update_order_cc_addresses.py
+++ saleor/order/migrations/0172_update_order_cc_addresses.py
@@ -38,7 +38,11 @@ def update_order_addresses(apps, schema_editor):
                 order_address = Address(**model_to_dict(cc_address, exclude=["id"]))
                 order.shipping_address = order_address
                 addresses.append(order_address)
-        Address.objects.bulk_create(addresses, ignore_conflicts=True)
+        Address.objects.bulk_create(addresses, ignore_conflicts=False)
+        # Due to a bug in Django, addresses need to be reassigned for bulk_update of Orders to work correctly.
+        # For more information about the bug, see: https://code.djangoproject.com/ticket/33322
+        for order in orders:
+            order.shipping_address = order.shipping_address
         Order.objects.bulk_update(orders, ["shipping_address"])
 
 
