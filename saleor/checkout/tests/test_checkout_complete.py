7c72428c781711c707b04c0f741b1d6ef4afc442
IKarbowiak <iga.karbowiak@saleor.io>
2024-07-02 15:11:05 +0200

    Add undiscounted_base_shipping_price to Order
    Use order.undiscounted_base_shipping_price
    
    Set undiscounted_base_shipping_price during checkout completion
    
    Add migration for setting undiscounted base shipping price
    
    Make undiscounted_base_shipping_price_amount nullable
    
    Update migration
    
    Update batch sizes
    
    Ensure that undiscounted_base_shipping_price is set when order is created
    
    Update migration task
    
    Add undiscountedShippingPrice to Order type
    
    Fix invalid undiscounted_total calculations in complete checkout
    
    Add comment to set_udniscounted_base_shipping_price_on_orders_task
    
    Clean after cherry pick

--- saleor/checkout/tests/test_checkout_complete.py
+++ saleor/checkout/tests/test_checkout_complete.py
@@ -1796,7 +1796,12 @@ def test_process_shipping_data_for_order_store_customer_shipping_address(
 
     # when
     _ = _process_shipping_data_for_order(
-        checkout_info, base_shipping_price, shipping_price, manager, lines
+        checkout_info,
+        base_shipping_price,
+        base_shipping_price,
+        shipping_price,
+        manager,
+        lines,
     )
 
     # then
@@ -1831,7 +1836,12 @@ def test_process_shipping_data_for_order_dont_store_customer_click_and_collect_a
 
     # when
     _ = _process_shipping_data_for_order(
-        checkout_info, base_shipping_price, shipping_price, manager, lines
+        checkout_info,
+        base_shipping_price,
+        base_shipping_price,
+        shipping_price,
+        manager,
+        lines,
     )
 
     # then
@@ -1917,6 +1927,7 @@ def test_create_order_store_shipping_prices(
     )
 
     # then
+    assert order.undiscounted_base_shipping_price == expected_base_shipping_price
     assert order.base_shipping_price == expected_base_shipping_price
     assert order.shipping_price == expected_shipping_price
     manager.calculate_checkout_shipping.assert_called_once_with(
@@ -1941,6 +1952,9 @@ def test_create_order_store_shipping_prices_with_free_shipping_voucher(
     checkout = checkout_with_voucher_free_shipping
     manager = get_plugins_manager(allow_replica=False)
 
+    expected_undiscounted_shipping_price = shipping_method.channel_listings.get(
+        channel=checkout.channel
+    ).price
     expected_base_shipping_price = zero_money(checkout.currency)
     expected_shipping_price = zero_taxed_money(checkout.currency)
     expected_shipping_tax_rate = Decimal("0.0")
@@ -1971,8 +1985,23 @@ def test_create_order_store_shipping_prices_with_free_shipping_voucher(
     )
 
     # then
+    assert (
+        order.undiscounted_base_shipping_price == expected_undiscounted_shipping_price
+    )
     assert order.base_shipping_price == expected_base_shipping_price
     assert order.shipping_price == expected_shipping_price
+    undiscounted_subtotal = sum(
+        [line.undiscounted_total_price for line in order.lines.all()],
+        zero_taxed_money(order.currency),
+    )
+    assert (
+        order.undiscounted_total
+        == TaxedMoney(
+            net=expected_undiscounted_shipping_price,
+            gross=expected_undiscounted_shipping_price,
+        )
+        + undiscounted_subtotal
+    )
     manager.calculate_checkout_shipping.assert_called_once_with(
         checkout_info, lines, checkout.shipping_address, plugin_ids=None
     )
