41325bef143312dc9dc5062ef5f11bb4e94ff6be
fowczrek <f.a.owczarek@gmail.com>
2024-07-11 11:37:35 +0200

    Handle pregenerated paylods in checkout lines total price resolver

--- saleor/graphql/webhook/tests/pregenerated_subscription/test_checkout_resolvers.py
+++ saleor/graphql/webhook/tests/pregenerated_subscription/test_checkout_resolvers.py
@@ -317,3 +317,54 @@ def test_checkout_line_unit_price_use_pregenerated_payload(
     assert content["data"]["checkout"]["id"] == checkout_global_id
     mock_generate_payload.assert_not_called()
     mock_request.assert_called_once()
+
+
+@override_settings(PLUGINS=["saleor.plugins.webhook.plugin.WebhookPlugin"])
+@mock.patch("saleor.webhook.transport.synchronous.transport.send_webhook_request_sync")
+@mock.patch(
+    "saleor.webhook.transport.synchronous.transport.generate_payload_from_subscription"
+)
+def test_checkout_line_total_price_use_pregenerated_payload(
+    mock_generate_payload,
+    mock_request,
+    checkout_with_item,
+    api_client,
+    tax_data_response,
+    tax_app,
+):
+    # given
+    checkout_with_item.price_expiration = timezone.now() - timedelta(days=1)
+    checkout_with_item.save()
+    checkout_global_id = to_global_id_or_none(checkout_with_item)
+    checkout_total_price_query = """
+        query getCheckout($id: ID!) {
+            checkout(id: $id) {
+                id
+                lines {
+                    totalPrice {
+                        net {
+                            amount
+                        }
+                        gross {
+                            amount
+                        }
+                        tax {
+                            amount
+                        }
+                    }
+                }
+            }
+        }
+    """
+    variables = {"id": checkout_global_id}
+
+    mock_request.return_value = tax_data_response
+
+    # when
+    response = api_client.post_graphql(checkout_total_price_query, variables)
+    content = get_graphql_content(response)
+
+    # then
+    assert content["data"]["checkout"]["id"] == checkout_global_id
+    mock_generate_payload.assert_not_called()
+    mock_request.assert_called_once()
