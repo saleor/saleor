282ba316a0d3c97f9c80abe5f0b81a9ae175933d
pitkes22 <padampasam@gmail.com>
2024-07-14 18:30:06 +0200

    Add CheckoutNoteUpdate mutation

---
+++ saleor/graphql/checkout/mutations/checkout_note_update.py
@@ -0,0 +1,67 @@
+import graphene
+
+from ....webhook.event_types import WebhookEventAsyncType
+from ...core import ResolveInfo
+from ...core.descriptions import ADDED_IN_34, DEPRECATED_IN_3X_INPUT
+from ...core.doc_category import DOC_CATEGORY_CHECKOUT
+from ...core.mutations import BaseMutation
+from ...core.scalars import UUID
+from ...core.types import CheckoutError
+from ...core.utils import WebhookEventInfo
+from ...plugins.dataloaders import get_plugin_manager_promise
+from ..types import Checkout
+from .utils import get_checkout
+
+
+class CheckoutNoteUpdate(BaseMutation):
+    checkout = graphene.Field(Checkout, description="An updated checkout.")
+
+    class Arguments:
+        id = graphene.ID(
+            description="The checkout's ID." + ADDED_IN_34,
+            required=False,
+        )
+        token = UUID(
+            description=f"Checkout token.{DEPRECATED_IN_3X_INPUT} Use `id` instead.",
+            required=False,
+        )
+        checkout_id = graphene.ID(
+            required=False,
+            description=(
+                f"The ID of the checkout. {DEPRECATED_IN_3X_INPUT} Use `id` instead."
+            ),
+        )
+        note = graphene.String(required=True, description="New note content.")
+
+    class Meta:
+        description = "Updates note in the existing checkout object."
+        doc_category = DOC_CATEGORY_CHECKOUT
+        error_type_class = CheckoutError
+        error_type_field = "checkout_errors"
+        webhook_events_info = [
+            WebhookEventInfo(
+                type=WebhookEventAsyncType.CHECKOUT_UPDATED,
+                description="A checkout was updated.",
+            )
+        ]
+
+    @classmethod
+    def perform_mutation(  # type: ignore[override]
+        cls,
+        _root,
+        info: ResolveInfo,
+        /,
+        *,
+        checkout_id=None,
+        note,
+        id=None,
+        token=None,
+    ):
+        checkout = get_checkout(cls, info, checkout_id=checkout_id, token=token, id=id)
+
+        checkout.note = note
+        cls.clean_instance(info, checkout)
+        checkout.save(update_fields=["note", "last_change"])
+        manager = get_plugin_manager_promise(info.context).get()
+        cls.call_event(manager.checkout_updated, checkout)
+        return CheckoutNoteUpdate(checkout=checkout)
---
+++ saleor/graphql/checkout/tests/mutations/test_checkout_note_update.py
@@ -0,0 +1,62 @@
+from ....core.utils import to_global_id_or_none
+from ....tests.utils import get_graphql_content
+
+CHECKOUT_NOTE_UPDATE_MUTATION = """
+    mutation checkoutNoteUpdate($id: ID, $note: String!) {
+        checkoutNoteUpdate(id: $id, note: $note) {
+            checkout {
+                id,
+                note
+            },
+            errors {
+                field,
+                message
+            }
+            errors {
+                field,
+                message
+                code
+            }
+        }
+    }
+"""
+
+
+def test_checkout_note_update(user_api_client, checkout_with_item):
+    checkout = checkout_with_item
+    checkout.note = ""
+    checkout.save(update_fields=["note"])
+    previous_last_change = checkout.last_change
+
+    note = "New note value"
+    variables = {"id": to_global_id_or_none(checkout), "note": note}
+
+    response = user_api_client.post_graphql(CHECKOUT_NOTE_UPDATE_MUTATION, variables)
+    content = get_graphql_content(response)
+    data = content["data"]["checkoutNoteUpdate"]
+    assert not data["errors"]
+    checkout.refresh_from_db()
+    assert checkout.note == note
+    assert checkout.last_change != previous_last_change
+
+
+def test_with_active_problems_flow(api_client, checkout_with_problems):
+    # given
+    channel = checkout_with_problems.channel
+    channel.use_legacy_error_flow_for_checkout = False
+    channel.save(update_fields=["use_legacy_error_flow_for_checkout"])
+
+    variables = {
+        "id": to_global_id_or_none(checkout_with_problems),
+        "note": "New note value",
+    }
+
+    # when
+    response = api_client.post_graphql(
+        CHECKOUT_NOTE_UPDATE_MUTATION,
+        variables,
+    )
+    content = get_graphql_content(response)
+
+    # then
+    assert not content["data"]["checkoutNoteUpdate"]["errors"]
