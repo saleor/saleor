30088c99c6a563d5db0f1ce9d7475da6edd02139
Mikail Kocak <mikail.kocak@saleor.io>
2024-07-18 12:30:55 +0200

    Release v3.19.61

--- saleor/__init__.py
+++ saleor/__init__.py
@@ -3,7 +3,7 @@ import pillow_avif  # noqa: F401 # imported for side effects
 from .celeryconf import app as celery_app
 
 __all__ = ["celery_app"]
-__version__ = "3.19.60"
+__version__ = "3.19.61"
 
 
 class PatchedSubscriberExecutionContext:
