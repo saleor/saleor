d7400be7e767141048745f52dc0c3393b6eb67b2
Cezary Miącz <cezary.miacz@saleor.io>
2024-08-13 10:06:29 +0200

    Revert "Temopararily allow to configure webhook cache timeouts"
    This reverts commit f92f615a059c0bcb1afdac453024b40c83faac65.

--- saleor/webhook/const.py
+++ saleor/webhook/const.py
@@ -1,10 +1,3 @@
-import os
-
-# TODO: restore hardcoded values or move to settings
-CACHE_EXCLUDED_SHIPPING_TIME = int(
-    os.environ.get("WEBHOOK_CACHE_EXCLUDED_SHIPPING_TIME", 3 * 60)
-)
-WEBHOOK_CACHE_DEFAULT_TIMEOUT = int(
-    os.environ.get("WEBHOOK_CACHE_DEFAULT_TIMEOUT", 5 * 60)
-)
+CACHE_EXCLUDED_SHIPPING_TIME = 60 * 3
+WEBHOOK_CACHE_DEFAULT_TIMEOUT: int = 5 * 60  # 5 minutes
 APP_ID_PREFIX = "app"
