54de95fe49ce4f308713b4ede982f3de4418c959
Marcin Wcislo <marcin.wcislo@saleor.io>
2024-08-19 09:25:34 +0200

    Add unit test

---
+++ saleor/tests/test_cache_key_warning.py
@@ -0,0 +1,15 @@
+from django.core.cache import cache, CacheKeyWarning
+import warnings
+
+
+def test_ignore_cache_key_warning():
+    key_over_250_characters = 300 * "x"
+
+    # Ensure all warnings are errors
+    warnings.simplefilter("error")
+
+    # Ignore CacheKeyWarning
+    warnings.filterwarnings("ignore", category=CacheKeyWarning)
+
+    # Call a function that throws warning
+    cache.set(key_over_250_characters, "bar")
