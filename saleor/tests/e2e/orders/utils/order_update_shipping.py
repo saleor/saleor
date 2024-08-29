4aba11f192bb4eb5c0489ce05346bb989b16c24f
Anna Szczech <anna.m.szczech@gmail.com>
2024-08-29 17:49:43 +0200

    add test for combine catalog promotion and voucher on draft order

---
+++ saleor/tests/e2e/orders/utils/order_update_shipping.py
@@ -0,0 +1,83 @@
+from saleor.graphql.tests.utils import get_graphql_content
+
+DRAFT_ORDER_UPDATE_MUTATION = """
+mutation OrderUpdateShipping($input: OrderUpdateShippingInput!, $id: ID!) {
+  orderUpdateShipping(input: $input, order: $id) {
+    errors {
+      message
+      field
+      code
+    }
+    order {
+      id
+      subtotal {
+        ...BaseTaxedMoney
+      }
+      total {
+        ...BaseTaxedMoney
+      }
+      undiscountedTotal {
+        ...BaseTaxedMoney
+      }
+      deliveryMethod {
+        __typename
+        ... on ShippingMethod {
+          id
+        }
+      }
+      shippingPrice {
+        ...BaseTaxedMoney
+      }
+      voucher {
+        id
+        code
+        discountValue
+      }
+      lines {
+        totalPrice {
+          ...BaseTaxedMoney
+        }
+        unitPrice {
+          ...BaseTaxedMoney
+        }
+        unitDiscountReason
+      }
+    }
+  }
+}
+
+fragment BaseTaxedMoney on TaxedMoney {
+  gross {
+    amount
+  }
+  net {
+    amount
+  }
+  tax {
+    amount
+  }
+  currency
+}
+"""
+
+
+def order_update_shipping(
+    api_client,
+    id,
+    input,
+):
+    variables = {"id": id, "input": input}
+
+    response = api_client.post_graphql(
+        DRAFT_ORDER_UPDATE_MUTATION,
+        variables=variables,
+    )
+    content = get_graphql_content(response)
+    data = content["data"]["orderUpdateShipping"]
+    order_id = data["order"]["id"]
+    errors = data["errors"]
+
+    assert errors == []
+    assert order_id == id
+
+    return data
