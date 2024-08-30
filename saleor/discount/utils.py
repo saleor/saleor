d1e263c2108bad035ba82ce0d8456b011909b785
zedzior <piotr.zabieglik@gmail.com>
2024-08-30 13:48:36 +0200

    Add TaxableObjectDiscountType to inform how to distribute discounts

--- saleor/discount/utils.py
+++ saleor/discount/utils.py
@@ -22,6 +22,7 @@ from ..checkout.base_calculations import (
 )
 from ..checkout.models import Checkout, CheckoutLine
 from ..core.exceptions import InsufficientStock
+from ..core.prices import quantize_price
 from ..core.taxes import zero_money
 from ..core.utils.promo_code import InvalidPromoCode
 from ..order.models import Order
@@ -2036,3 +2037,42 @@ def mark_catalogue_promotion_rules_as_dirty(promotion_pks: Iterable[UUID]):
         PromotionRule.objects.filter(id__in=rule_ids_to_update).update(
             variants_dirty=True
         )
+
+
+def split_manual_discount(
+    discount: OrderDiscount, subtotal: Money, shipping_price: Money
+) -> tuple[Money, Money]:
+    """Discounts sent to tax app must be split into subtotal and shipping portion."""
+    currency = subtotal.currency
+    subtotal_discount, shipping_discount = zero_money(currency), zero_money(currency)
+    if discount.value_type == DiscountValueType.PERCENTAGE:
+        discounted_subtotal = apply_discount_to_value(
+            value=discount.value,
+            value_type=discount.value_type,
+            currency=currency,
+            price_to_discount=subtotal,
+        )
+        subtotal_discount = subtotal - discounted_subtotal
+        discounted_shipping_price = apply_discount_to_value(
+            value=discount.value,
+            value_type=discount.value_type,
+            currency=currency,
+            price_to_discount=shipping_price,
+        )
+        shipping_discount = shipping_price - discounted_shipping_price
+    elif discount.value_type == DiscountValueType.FIXED:
+        total = subtotal + shipping_price
+        if total.amount > 0:
+            discounted_total = apply_discount_to_value(
+                value=discount.value,
+                value_type=discount.value_type,
+                currency=currency,
+                price_to_discount=total,
+            )
+            total_discount = total - discounted_total
+            subtotal_discount = subtotal / total * total_discount
+            shipping_discount = total_discount - subtotal_discount
+
+    return quantize_price(subtotal_discount, currency), quantize_price(
+        shipping_discount, currency
+    )
