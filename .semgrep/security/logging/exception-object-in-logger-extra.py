f2b84c6f040143bdae6d48c3d52bd68cd4a4c2ec
Mikail Kocak <mikail.kocak@saleor.io>
2024-09-05 17:24:32 +0200

    semgrep: handle trailing arguments

--- .semgrep/security/logging/exception-object-in-logger-extra.py
+++ .semgrep/security/logging/exception-object-in-logger-extra.py
@@ -14,6 +14,25 @@ def test_log_multiple_extra():
         # ruleid: exception-object-in-logger-extra
         logger.info("Failed", extra={"other1": 1, "error": exc, "other2": 1})
 
+# Should match when extra is before other arguments (case #1).
+def test_log_exception_trailing_arguments_case1():
+    try:
+        do_something()
+    except Exception as exc:
+        # w/ exc_info trailing & extra={} in the middle
+        # ruleid: exception-object-in-logger-extra
+        logger.info("Failed", extra={"error": exc}, exc_info=True)
+
+
+# Should match when extra is before other arguments (case #2).
+def test_log_exception_trailing_arguments_case2():
+    try:
+        do_something()
+    except Exception as exc:
+        # Test: extra={} is first parameter
+        # ruleid: exception-object-in-logger-extra
+        logger.info(extra={"error": exc}, msg="Failed", exc_info=True)
+
 # Should not match when converting an exception object to string.
 def test_not_logging_exception():
     try:
