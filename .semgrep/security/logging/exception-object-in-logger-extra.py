48c771b2252222261299c28b0478e5abf24e8308
Mikail Kocak <mikail.kocak@saleor.io>
2024-09-05 17:13:44 +0200

    semgrep: prevent logging exceptions
    semgrep: support all different types of try-except blocks (#16662)
    
    semgrep: handle trailing arguments
    
    Set semgrep logging rule severity to ERROR
    
    reliability: fix and prevent logging exceptions
    
    This fixes Saleor logging exceptions in `logger.<func>(..., extra={})` - it is known to lead to high memory usage, due to Sentry holding the references to the thread objects. This is especially problematic due to using `asgiref`, which can create thousands of threads over a short time.
    
    This also prevents the issue from being re-introduced by putting in place a Semgrep rule that checks for this pattern.
    
    Follow-up and port of https://github.com/saleor/saleor/pull/16660 (3.20 -> main).

---
+++ .semgrep/security/logging/exception-object-in-logger-extra.py
@@ -0,0 +1,121 @@
+# Should match when logging an exception.
+def test_log_info_exception():
+    try:
+        do_something()
+    except Exception as exc:
+        # ruleid: exception-object-in-logger-extra
+        logger.info("Failed", extra={"error": exc})
+
+# Should match when catching multiple exceptions.
+def test_log_catch_multiple_exceptions():
+    try:
+        do_something()
+    except (OSError, FileNotFoundError) as exc:
+        # ruleid: exception-object-in-logger-extra
+        logger.info("Failed", extra={"error": exc})
+
+# TODO: uncomment once 'except*' is supported by Semgrep
+#       https://github.com/semgrep/semgrep/issues/10511
+# # Should match when catching an exception group.
+# def test_log_catch_wildcard():
+#     try:
+#         do_something()
+#     except* Exception as exc:
+#         <ruleid>: exception-object-in-logger-extra
+#         logger.info("Failed", extra={"error": exc})
+
+# Should match when logging an exception in the middle
+# of a 'except' block.
+def test_log_middle_of_statement():
+    try:
+        do_something()
+    except Exception as exc:
+        something()
+        # ruleid: exception-object-in-logger-extra
+        logger.info("Failed", extra={"error": exc})
+        something()
+
+# Should match when catching multiple exceptions.
+def test_log_info_exception():
+    try:
+        do_something()
+    except (OSError, FileNotFoundError) as exc:
+        # ruleid: exception-object-in-logger-extra
+        logger.info("Failed", extra={"error": exc})
+
+# Should match when extra contains multiple keys & values.
+def test_log_multiple_extra():
+    try:
+        do_something()
+    except Exception as exc:
+        # ruleid: exception-object-in-logger-extra
+        logger.info("Failed", extra={"other1": 1, "error": exc, "other2": 1})
+
+# Should match when extra is before other arguments (case #1).
+def test_log_exception_trailing_arguments_case1():
+    try:
+        do_something()
+    except Exception as exc:
+        # w/ exc_info trailing & extra={} in the middle
+        # ruleid: exception-object-in-logger-extra
+        logger.info("Failed", extra={"error": exc}, exc_info=True)
+
+
+# Should match when extra is before other arguments (case #2).
+def test_log_exception_trailing_arguments_case2():
+    try:
+        do_something()
+    except Exception as exc:
+        # Test: extra={} is first parameter
+        # ruleid: exception-object-in-logger-extra
+        logger.info(extra={"error": exc}, msg="Failed", exc_info=True)
+
+# Should match when using a 'finally' block
+def test_log_exception_with_finally_block():
+    try:
+        do_something()
+    except Exception as exc:
+        # ruleid: exception-object-in-logger-extra
+        logger.info("Failed", extra={"error": exc})
+    finally:
+        do_something_else()
+
+# Should match when using a 'else' block
+def test_log_exception_with_else_block():
+    try:
+        do_something()
+    except Exception as exc:
+        # ruleid: exception-object-in-logger-extra
+        logger.info("Failed", extra={"error": exc})
+    else:
+        do_something_else()
+
+# Should match when using a 'else' and 'finally' block
+def test_log_exception_with_finally_else_block():
+    try:
+        do_something()
+    except Exception as exc:
+        # ruleid: exception-object-in-logger-extra
+        logger.info("Failed", extra={"error": exc})
+    else:
+        do_something_else()
+    finally:
+        do_something_else()
+
+# Should not match when converting an exception object to string.
+def test_not_logging_exception():
+    try:
+        do_something()
+    except Exception as exc:
+        # ok: exception-object-in-logger-extra
+        logger.info("Failed", extra={"exc": str(exc)})
+
+
+# Should not match when using an attribute from the exception object.
+def test_not_logging_exception():
+    try:
+        do_something()
+    except Exception as exc:
+        # ok: exception-object-in-logger-extra
+        logger.info("Failed", extra={"exc": exc.message})
+
