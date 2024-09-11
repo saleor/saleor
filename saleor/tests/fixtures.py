edc4120f657f4620030d5bd8327883714b9e812b
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2024-09-11 14:24:53 +0200

    Revert "Fix fetching available collection points (#16653)" (#16681)
    This reverts commit 2f52e0eeecb84b4a7ed788fa4a26fde44696827b.

--- saleor/tests/fixtures.py
+++ saleor/tests/fixtures.py
@@ -1441,7 +1441,6 @@ def order_generator(customer_user, channel_USD):
             private_metadata=private_metadata,
             checkout_token=checkout_token,
             status=status,
-            undiscounted_base_shipping_price_amount=Decimal("0.0"),
         )
         if search_vector_class:
             search_vector = search_vector_class(
@@ -4576,7 +4575,6 @@ def order_line_with_allocation_in_many_stocks(
         user=customer_user,
         channel=channel_USD,
         origin=OrderOrigin.CHECKOUT,
-        undiscounted_base_shipping_price_amount=Decimal("0.0"),
     )
 
     product = variant.product
