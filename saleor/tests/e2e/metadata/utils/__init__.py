94c79f63257c2f27166594f822f743d6dbb40462
Anna Szczęch <30683248+szczecha@users.noreply.github.com>
2024-09-24 13:00:17 +0200

    [TEST] Add test for update metadata on draft order (#16729)
    * add test for update metadata on draft order
    
    * restore availableShippingMethods key in qoery order
    
    * create variables for metadata

--- saleor/tests/e2e/metadata/utils/__init__.py
+++ saleor/tests/e2e/metadata/utils/__init__.py
@@ -1,7 +1,11 @@
+from .delete_metadata import delete_metadata
+from .delete_private_metadata import delete_private_metadata
 from .update_metadata import update_metadata
 from .update_private_metadata import update_private_metadata
 
 __all__ = [
+    "delete_metadata",
+    "delete_private_metadata",
     "update_metadata",
     "update_private_metadata",
 ]
