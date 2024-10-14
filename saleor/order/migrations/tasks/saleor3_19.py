53eeb684558bad0600730850a9aa41f4729fb7ac
Maciej Korycinski <maciej.korycinski@saleor.io>
2024-10-14 09:50:34 +0200

    Fix missing subtotal for orders created from orderBulkCreate (#16806)
    * Fix missing subtotal for orders created from orderBulkCreate
    
    * Only update charge&authorize statuses instead of amounts recalculations

--- saleor/order/migrations/tasks/saleor3_19.py
+++ saleor/order/migrations/tasks/saleor3_19.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 
 from django.db import transaction
-from django.db.models import Exists, F, OuterRef, Q
+from django.db.models import Exists, F, OuterRef, Q, Sum
 from django.forms.models import model_to_dict
 
 from ....account.models import Address
@@ -9,12 +9,16 @@ from ....celeryconf import app
 from ....discount.models import OrderDiscount, Voucher
 from ....warehouse.models import Warehouse
 from ...models import Order, OrderLine
+from ...utils import update_order_authorize_status, update_order_charge_status
 
 # The batch of size 250 takes ~0.5 second and consumes ~20MB memory at peak
 ADDRESS_UPDATE_BATCH_SIZE = 250
 
 # The batch of size 250 takes ~0.2 second and consumes ~20MB memory at peak
 ORDER_SET_SHIPPING_PRICE_BATCH_SIZE = 250
+
+# The batch of size 100 takes ~0.2 second and consumes 6.5MB memory
+ORDER_SET_SUBTOTAL_PRICE_FOR_ORDER_FROM_BULK = 100
 
 
 @app.task
@@ -150,6 +154,71 @@ def _set_undiscounted_base_shipping_price(orders):
         )
 
 
+def _set_subtotal_for_orders_created_from_bulk(order_ids: list[str]):
+    orders = (
+        Order.objects.prefetch_related(
+            "payments", "payment_transactions", "granted_refunds"
+        )
+        .filter(id__in=order_ids)
+        .annotate(
+            sum_line_total_price_net_amount=Sum("lines__total_price_net_amount"),
+            sum_line_total_price_gross_amount=Sum("lines__total_price_gross_amount"),
+            sum_line_undiscounted_total_price_net_amount=Sum(
+                "lines__undiscounted_total_price_net_amount"
+            ),
+            sum_line_undiscounted_total_price_gross_amount=Sum(
+                "lines__undiscounted_total_price_gross_amount"
+            ),
+            sum_granted_refunds=Sum("granted_refunds__amount_value"),
+        )
+    )
+
+    for order in orders:
+        order.subtotal_net_amount = order.sum_line_total_price_net_amount
+        order.subtotal_gross_amount = order.sum_line_total_price_gross_amount
+        order.total_net_amount = (
+            order.subtotal_net_amount + order.shipping_price_net_amount
+        )
+        order.total_gross_amount = (
+            order.subtotal_gross_amount + order.shipping_price_gross_amount
+        )
+
+        # using order.shipping_price as this value is used for storing undiscounted
+        # shipping price, received as mutation input or fetched from db.
+        order.undiscounted_total_net_amount = (
+            order.sum_line_undiscounted_total_price_net_amount
+            + order.shipping_price_net_amount
+        )
+        order.undiscounted_total_gross_amount = (
+            order.sum_line_undiscounted_total_price_gross_amount
+            + order.shipping_price_gross_amount
+        )
+        update_order_authorize_status(
+            order, granted_refund_amount=order.sum_granted_refunds or Decimal(0)
+        )
+        update_order_charge_status(
+            order, granted_refund_amount=order.sum_granted_refunds or Decimal(0)
+        )
+
+    with transaction.atomic():
+        _orders = list(
+            Order.objects.filter(id__in=order_ids).select_for_update(of=(["self"]))
+        )
+        Order.objects.bulk_update(
+            orders,
+            [
+                "subtotal_net_amount",
+                "subtotal_gross_amount",
+                "total_net_amount",
+                "total_gross_amount",
+                "undiscounted_total_gross_amount",
+                "undiscounted_total_net_amount",
+                "charge_status",
+                "authorize_status",
+            ],
+        )
+
+
 @app.task
 def set_udniscounted_base_shipping_price_on_draft_orders_task():
     qs = Order.objects.filter(
@@ -164,3 +233,21 @@ def set_udniscounted_base_shipping_price_on_draft_orders_task():
         orders = Order.objects.filter(id__in=order_ids)
         _set_undiscounted_base_shipping_price(orders)
         set_udniscounted_base_shipping_price_on_draft_orders_task.delay()
+
+
+@app.task
+def set_order_subtotal_for_orders_created_from_bulk():
+    order_lines_qs = OrderLine.objects.filter(total_price_gross_amount__gt=0).values(
+        "order_id"
+    )
+    qs = Order.objects.filter(
+        Exists(order_lines_qs.filter(order_id=OuterRef("id"))),
+        origin="bulk_create",
+        subtotal_gross_amount=Decimal(0),
+    )
+    order_ids = list(
+        qs.values_list("pk", flat=True)[:ORDER_SET_SUBTOTAL_PRICE_FOR_ORDER_FROM_BULK]
+    )
+    if order_ids:
+        _set_subtotal_for_orders_created_from_bulk(order_ids)
+        set_order_subtotal_for_orders_created_from_bulk.delay()
