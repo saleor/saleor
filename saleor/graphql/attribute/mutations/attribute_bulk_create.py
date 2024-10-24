14802d316586c7dd4aabe6efafc181452135b1ad
kadewu <krzysztof.waliczek@saleor.io>
2024-10-24 11:08:22 +0200

    AttributeBulkCreate: optimize webhooks sql count

--- saleor/graphql/attribute/mutations/attribute_bulk_create.py
+++ saleor/graphql/attribute/mutations/attribute_bulk_create.py
@@ -13,6 +13,7 @@ from ....core.tracing import traced_atomic_transaction
 from ....core.utils import prepare_unique_slug
 from ....permission.enums import PageTypePermissions, ProductTypePermissions
 from ....webhook.event_types import WebhookEventAsyncType
+from ....webhook.utils import get_webhooks_for_event
 from ...core import ResolveInfo
 from ...core.descriptions import ADDED_IN_315, PREVIEW_FEATURE
 from ...core.doc_category import DOC_CATEGORY_ATTRIBUTES
@@ -543,8 +544,9 @@ class AttributeBulkCreate(BaseMutation):
     @classmethod
     def post_save_actions(cls, info: ResolveInfo, attributes: list[models.Attribute]):
         manager = get_plugin_manager_promise(info.context).get()
+        webhooks = get_webhooks_for_event(WebhookEventAsyncType.ATTRIBUTE_CREATED)
         for attribute in attributes:
-            cls.call_event(manager.attribute_created, attribute)
+            cls.call_event(manager.attribute_created, attribute, webhooks=webhooks)
 
     @classmethod
     @traced_atomic_transaction()
