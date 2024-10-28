d9c59f6c1e1dd2ec7714e1fcab6a1cd350f07948
Maciej Korycinski <korycinski.maciej@gmail.com>
2024-10-28 14:44:02 +0100

    Apply changes required for zero-downtime process

--- saleor/celeryconf.py
+++ saleor/celeryconf.py
@@ -42,5 +42,11 @@ app.autodiscover_tasks(
     ],
     related_name="saleor3_14",
 )
+app.autodiscover_tasks(
+    packages=[
+        "saleor.checkout.migrations.tasks",
+    ],
+    related_name="saleor3_19",
+)
 app.autodiscover_tasks(lambda: discover_plugins_modules(settings.PLUGINS))  # type: ignore[misc] # circular import # noqa: E501
 app.autodiscover_tasks(related_name="search_tasks")
