1f9aa97511493e101a1d06065232843adecd2096
Maciej Korycinski <korycinski.maciej@gmail.com>
2024-11-05 14:22:01 +0100

    Apply changes after review

--- saleor/celeryconf.py
+++ saleor/celeryconf.py
@@ -30,11 +30,6 @@ app.autodiscover_tasks()
 app.autodiscover_tasks(
     packages=[
         "saleor.order.migrations.tasks",
-    ],
-    related_name="saleor3_20",
-)
-app.autodiscover_tasks(
-    packages=[
         "saleor.checkout.migrations.tasks",
     ],
     related_name="saleor3_20",
