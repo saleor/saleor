f8985c3ca53e774131fc66f08df1974731f74986
Mikail Kocak <mikail.kocak@saleor.io>
2024-12-03 11:07:07 +0100

    deps: upgrade requests-hardened to latest version
    This upgrades requests-hardened to latest version (1.0.0b4 -> 1.0.0b5) which fixes issues with request URLs (such as webhooks) being logged and traced using the IP addresses instead of their domain names (thus reducing observability and making troubleshooting harder).
    
    The requests-hardened library no longer rewrites URLs, and instead pins the IP address at the socket-level thus this commit also added an additional test to ensure the behavior is remains consistent when urllib3 and requests libraries are updated due to the logic happening much deeper inside the libraries thus making the IP filtering feature more sensitive to transient dependencies breaking changes.

--- saleor/core/tests/test_http_client.py
+++ saleor/core/tests/test_http_client.py
@@ -1,16 +1,10 @@
+import pytest
 import requests_hardened
-from django.conf import settings
 from requests import Request
+from requests_hardened.ip_filter import InvalidIPAddress
 
 from ... import user_agent_version
-
-HTTPConfig = requests_hardened.Config(
-    ip_filter_enable=settings.HTTP_IP_FILTER_ENABLED,
-    ip_filter_allow_loopback_ips=settings.HTTP_IP_FILTER_ALLOW_LOOPBACK_IPS,
-    never_redirect=True,
-    default_timeout=(2, 10),
-    user_agent_override=user_agent_version,
-)
+from ..http_client import HTTPClient, HTTPConfig
 
 
 def test_user_agent_override():
@@ -25,3 +19,26 @@ def test_user_agent_override():
 
     # then
     assert request.headers.get("User-Agent") == user_agent_version
+
+
+@pytest.mark.vcr(record_mode="once")
+@pytest.mark.parametrize("protocol", ["https", "http"])
+def test_http_client_disallows_private_ip_ranges(protocol):
+    """Ensure requests made to private IP address ranges leads to an error to be raised.
+
+    This test ensures that the requests-hardened library works correctly,
+    especially when using untested 'requests' and 'urllib3' versions
+    that may potentially be unsupported by this library.
+    """
+
+    # Enable IP filtering (disabled by default in tests)
+    http_manager = HTTPClient.clone()
+    http_manager.config.ip_filter_enable = True  # Block private ranges.
+
+    # Should reject the IP address (expect error to be raised)
+    with pytest.raises(InvalidIPAddress, match="10.0.0.1"):
+        http_manager.send_request("GET", f"{protocol}://10.0.0.1", timeout=0.1)
+
+    # Should not reject public IP ranges (sanity check).
+    response = http_manager.send_request("GET", f"{protocol}://example.com")
+    assert response.status_code == 200
