fcf752c0d903cf1690b480424a26a05f01d11cf1
Filip Owczarek <filip.owczarek@saleor.io>
2024-12-11 11:31:30 +0100

    Generate sync webhook payloads in the dataloaders for filtering checkout shipping methods. (#16586) (#17153)
    * Pass paylaod from dataclass to request
    
    * Pass pregeneated payload to CheckoutInfo object
    
    * Add base dataloader for pregeneate payload in checkout shipping method filtering
    
    * Add tests for pregenerated paylods in checkout resolvers
    
    * Fix tests
    
    * Add test for exclude shipping methods for app without subscription
    
    * Add test for tax app and exclued shipping app in one request
    
    * Create PayloadsRequestContextByEventTypeLoader
    
    * Add AppByEventTypeLoader
    
    * Add WebhookByEventTypeLoader
    
    * Add WebhooksEventsByEventTypeLoader:
    
    * Fix linters
    
    * Move WebhooksByEventTypeLoader inside dataloaders module
    
    * Reorganize dataloaders
    
    * Fix WebhooksByEventTypeLoader when app is not active

--- saleor/graphql/webhook/dataloaders/__init__.py
+++ saleor/graphql/webhook/dataloaders/__init__.py
@@ -2,14 +2,14 @@ from .models import (
     PayloadByIdLoader,
     WebhookEventsByWebhookIdLoader,
     WebhooksByAppIdLoader,
+    WebhooksByEventTypeLoader,
 )
-from .pregenerated_payload_for_checkout_tax import (
-    PregeneratedCheckoutTaxPayloadsByCheckoutTokenLoader,
-)
+from .request_context import PayloadsRequestContextByEventTypeLoader
 
 __all__ = [
     "PayloadByIdLoader",
-    "PregeneratedCheckoutTaxPayloadsByCheckoutTokenLoader",
+    "PayloadsRequestContextByEventTypeLoader",
     "WebhookEventsByWebhookIdLoader",
     "WebhooksByAppIdLoader",
+    "WebhooksByEventTypeLoader",
 ]
