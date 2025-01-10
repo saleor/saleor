75baa5634eafa7fe0db62a6296f177ee76721058
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2025-01-10 11:51:33 +0100

    Fix checkout funds releasing task (#17198)
    * Fix checkout funds releasing task
    
    * Fix changelog
    
    * Fix typo in CORE_2118 e2e
    
    * Apply code review suggestions
    
    * Improve logs

--- saleor/tests/e2e/promotions/test_promotions_query_with_different_parameters.py
+++ saleor/tests/e2e/promotions/test_promotions_query_with_different_parameters.py
@@ -371,13 +371,13 @@ def test_step_10_promotions_with_end_date_after_CORE_2118(
             )
             assert promotion["endDate"] == end_date
 
-    feature_end_date = timezone.now() + datetime.timedelta(days=10)
+    future_end_date = timezone.now() + datetime.timedelta(days=10)
     e2e_staff_api_client.regenerate_access_token()
     promotion_dnm = create_promotion(
         e2e_staff_api_client,
         "Promotion does not match",
         promotion_type,
-        end_date=feature_end_date,
+        end_date=future_end_date,
     )
 
     promotions_list = promotions_query(
