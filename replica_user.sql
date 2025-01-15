b0dc09bcb98dbff3593d375c7264b3f28bf20f1c
Rafi1115 <alenrafi1511@gmail.com>
2025-01-15 19:03:24 +0600

    Add environment and Docker setup files

---
+++ replica_user.sql
@@ -0,0 +1,5 @@
+CREATE USER saleor_read_only WITH PASSWORD 'saleor';
+GRANT CONNECT ON DATABASE saleor TO saleor_read_only;
+GRANT USAGE ON SCHEMA public TO saleor_read_only;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO saleor_read_only;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO saleor_read_only;
