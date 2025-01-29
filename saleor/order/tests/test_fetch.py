67367f01328bfc0d5a9c4d5f410deef396510610
Piotr Zabieglik <55899043+zedzior@users.noreply.github.com>
2025-01-29 08:54:28 +0000

    Use denormalized prices during order update for catalogue discounts (#17125)
    * Add tests
    
    * Do not reevaluate catalogue discounts when refreshing prices
    
    * Split prepare_line_discount_objects_for_catalogue_promotions for checkout and order
    
    * Keep only one function responsible for catalogue discount reason
    
    * Fix catalogue promotion handler in orderLinesCreate mutation
    
    * Split catalogue discount handler to udpate and create functions
    
    * Fix and move failing test
    
    * Adjust test in test_create_or_update_discount_objects_from_promotion_for_order.py
    
    * Adjust tests in test_fetch_order_prices.py and test_create_or_update_discount_objects_from_promotion_for_order.py
    
    * Do not retrieve redundant variant listings and catalogue rules info during fecthing order prices
    
    * Ignore conflicts instead of rising unique error when trying to create multiple catalogue disocunts for an order line
    
    * Do the catalogue discount amount update directly in mutations
    
    * Use line_info.variant_discounted_price to update base prices with catalogue discount; udpate tests
    
    * Add entry to changelog
    
    * Self review
    
    * Add e2e test
    
    * Do not udpate any catalogue discounts inside fetch_order_prices function; update tests
    
    * Add typing to orderLinesCreate and its utils
    
    * Rebase and fix lint
    
    * Apply review comments

--- saleor/order/tests/test_fetch.py
+++ saleor/order/tests/test_fetch.py
@@ -2,7 +2,6 @@ from decimal import Decimal
 
 import pytest
 
-from ...product.models import ProductVariantChannelListing
 from ..fetch import EditableOrderLineInfo, fetch_draft_order_lines_info
 
 
@@ -17,6 +16,7 @@ def test_fetch_draft_order_lines_info(
     lines = order.lines.all()
     line_1 = [line for line in lines if line.quantity == 3][0]
     line_2 = [line for line in lines if line.quantity == 2][0]
+    catalogue_discount = line_2.discounts.get()
 
     manual_discount = line_1.discounts.create(
         value=Decimal(1),
@@ -34,7 +34,7 @@ def test_fetch_draft_order_lines_info(
     )
 
     # when
-    with django_assert_num_queries(12):
+    with django_assert_num_queries(8):
         lines_info = fetch_draft_order_lines_info(order)
 
     # then
@@ -43,34 +43,15 @@ def test_fetch_draft_order_lines_info(
 
     variant_1 = line_1.variant
     assert line_info_1.variant == variant_1
-    assert (
-        line_info_1.channel_listing
-        == ProductVariantChannelListing.objects.filter(
-            channel=channel, variant=variant_1
-        ).first()
-    )
     assert line_info_1.discounts == [manual_discount]
     assert line_info_1.channel == channel
     assert line_info_1.voucher is None
-    assert line_info_1.rules_info == []
 
     variant_2 = line_2.variant
     assert line_info_2.variant == variant_2
-    assert (
-        line_info_2.channel_listing
-        == ProductVariantChannelListing.objects.filter(
-            channel=channel, variant=variant_2
-        ).first()
-    )
-    assert line_info_2.discounts == []
+    assert line_info_2.discounts == [catalogue_discount]
     assert line_info_2.channel == channel
     assert line_info_2.voucher is None
-    rule_info_2 = line_info_2.rules_info[0]
-    assert rule_info_2.rule == rule_catalogue
-    assert rule_info_2.variant_listing_promotion_rule
-    assert rule_info_2.promotion == rule_catalogue.promotion
-    assert rule_info_2.promotion_translation.name == promotion_translation
-    assert rule_info_2.rule_translation.name == rule_translation
 
 
 def test_editable_order_line_info_variant_discounted_price(
@@ -93,85 +74,10 @@ def test_editable_order_line_info_variant_discounted_price(
     order_line_info = EditableOrderLineInfo(
         line=order_line,
         variant=variant,
-        channel_listing=variant_channel_listing,
         product=product,
         product_type=product_type,
         collections=[],
         discounts=discounts,
-        rules_info=[],
-        channel=channel,
-        voucher=None,
-        voucher_code=None,
-    )
-    # then
-    assert order_line_info.variant_discounted_price == expected_discounted_variant_price
-
-
-def test_editable_order_line_info_variant_discounted_price_without_listing(
-    order_with_lines_and_catalogue_promotion,
-):
-    # given
-    order_line = order_with_lines_and_catalogue_promotion.lines.get(quantity=3)
-    order = order_with_lines_and_catalogue_promotion
-    channel = order.channel
-    variant = order_line.variant
-    variant_channel_listing = variant.channel_listings.get(channel_id=channel.id)
-    product = variant.product
-    product_type = product.product_type
-    discounts = order_line.discounts.all()
-
-    expected_discounted_variant_price = variant_channel_listing.discounted_price
-    assert variant_channel_listing.discounted_price != variant_channel_listing.price
-
-    variant.channel_listings.all().delete()
-
-    # when
-    order_line_info = EditableOrderLineInfo(
-        line=order_line,
-        variant=variant,
-        channel_listing=None,
-        product=product,
-        product_type=product_type,
-        collections=[],
-        discounts=discounts,
-        rules_info=[],
-        channel=channel,
-        voucher=None,
-        voucher_code=None,
-    )
-    # then
-    assert order_line_info.variant_discounted_price == expected_discounted_variant_price
-
-
-def test_editable_order_line_info_variant_discounted_price_when_listing_without_price(
-    order_with_lines_and_catalogue_promotion,
-):
-    # given
-    order_line = order_with_lines_and_catalogue_promotion.lines.get(quantity=3)
-    order = order_with_lines_and_catalogue_promotion
-    channel = order.channel
-    variant = order_line.variant
-    variant_channel_listing = variant.channel_listings.get(channel_id=channel.id)
-    product = variant.product
-    product_type = product.product_type
-    discounts = order_line.discounts.all()
-
-    expected_discounted_variant_price = variant_channel_listing.discounted_price
-    assert variant_channel_listing.discounted_price != variant_channel_listing.price
-
-    variant_channel_listing.discounted_price_amount = None
-    variant_channel_listing.save(update_fields=["discounted_price_amount"])
-
-    # when
-    order_line_info = EditableOrderLineInfo(
-        line=order_line,
-        variant=variant,
-        channel_listing=variant_channel_listing,
-        product=product,
-        product_type=product_type,
-        collections=[],
-        discounts=discounts,
-        rules_info=[],
         channel=channel,
         voucher=None,
         voucher_code=None,
