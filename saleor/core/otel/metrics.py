89926cfe94f9d36cfa35a2d46aff996318ba39ab
Przemysław Łada <przlada@gmail.com>
2025-02-02 23:59:17 +0100

    Refactor metrics

--- saleor/core/otel/metrics.py
+++ saleor/core/otel/metrics.py
@@ -1,24 +1,11 @@
 from typing import cast
 
 from opentelemetry import metrics
-from opentelemetry.metrics import Meter, Synchronous
+from opentelemetry.metrics import Meter
 from opentelemetry.util.types import Attributes
 
 from ... import __version__
 from .context import enrich_with_trace_attributes
-
-
-class _ContextAwareSyncInstrument:
-    def __init__(self, instrument: Synchronous):
-        self._instrument = instrument
-
-    def add(self, amount, attributes: Attributes = None, **kwargs):
-        attributes = enrich_with_trace_attributes(attributes)
-        return getattr(self._instrument, "add")(amount, attributes, **kwargs)
-
-    def record(self, amount, attributes: Attributes = None, **kwargs):
-        attributes = enrich_with_trace_attributes(attributes)
-        return getattr(self._instrument, "record")(amount, attributes, **kwargs)
 
 
 class _ContextAwareMeter(Meter):
@@ -27,18 +14,28 @@ class _ContextAwareMeter(Meter):
     def __init__(self, meter: Meter):
         self._meter = meter
 
+    @staticmethod
+    def _wrapp(original_method):
+        def wrapper(amount, attributes: Attributes = None, *args, **kwargs):
+            attributes = enrich_with_trace_attributes(attributes)
+            return original_method(amount, attributes, *args, **kwargs)
+
+        return wrapper
+
     def create_counter(self, *args, **kwargs):
-        return _ContextAwareSyncInstrument(self._meter.create_counter(*args, **kwargs))
+        counter = self._meter.create_counter(*args, **kwargs)
+        setattr(counter, "add", self._wrapp(counter.add))
+        return counter
 
     def create_up_down_counter(self, *args, **kwargs):
-        return _ContextAwareSyncInstrument(
-            self._meter.create_up_down_counter(*args, **kwargs)
-        )
+        counter = self._meter.create_up_down_counter(*args, **kwargs)
+        setattr(counter, "add", self._wrapp(counter.add))
+        return counter
 
     def create_histogram(self, *args, **kwargs):
-        return _ContextAwareSyncInstrument(
-            self._meter.create_histogram(*args, **kwargs)
-        )
+        histogram = self._meter.create_histogram(*args, **kwargs)
+        setattr(histogram, "record", self._wrapp(histogram.record))
+        return histogram
 
     def create_observable_counter(self, *args, **kwargs):
         raise NotImplementedError(self._NOT_IMPLEMENTED_MSG)
