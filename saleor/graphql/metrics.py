c6d4ec400469ded6095279ef0eea995839dee322
Przemysław Łada <przlada@gmail.com>
2025-03-04 14:15:24 +0100

    Return metric name from create_metric (#17442)

--- saleor/graphql/metrics.py
+++ saleor/graphql/metrics.py
@@ -3,14 +3,14 @@ from contextlib import AbstractContextManager
 from ..core.telemetry import AttributeValue, MetricType, Scope, Unit, meter
 
 # Initialize metrics
-meter.create_metric(
+METRIC_GRAPHQL_QUERIES = meter.create_metric(
     "saleor.graphql_queries",
     scope=Scope.SERVICE,
     type=MetricType.COUNTER,
     unit=Unit.REQUEST,
     description="Number of GraphQL queries.",
 )
-meter.create_metric(
+METRIC_GRAPHQL_QUERY_DURATION = meter.create_metric(
     "saleor.graphql_query_duration",
     scope=Scope.SERVICE,
     type=MetricType.HISTOGRAM,
@@ -21,10 +21,10 @@ meter.create_metric(
 
 # Helper functions
 def record_graphql_queries_count(amount: int = 1) -> None:
-    meter.record("saleor.graphql_queries", amount)
+    meter.record(METRIC_GRAPHQL_QUERIES, amount)
 
 
 def record_graphql_query_duration() -> AbstractContextManager[
     dict[str, AttributeValue]
 ]:
-    return meter.record_duration("saleor.graphql_query_duration")
+    return meter.record_duration(METRIC_GRAPHQL_QUERY_DURATION)
