5eafb481eb2b790f06e08d5ea4a362010b8fbfcb
kadewu <krzysztof.waliczek@saleor.io>
2025-03-07 14:53:19 +0100

    POC add more metrics for webhook performance

--- saleor/webhook/metrics.py
+++ saleor/webhook/metrics.py
@@ -12,6 +12,22 @@ METRIC_FIRST_EVENT_DELIVERY_ATTEMPT_DELAY = meter.create_metric(
     description="Delay for the first attempt of delivering async webhook event after creation of EventDeliver.",
 )
 
+METRIC_ASYNC_WEBHOOK_CALLS = meter.create_metric(
+    "saleor.webhooks.async.calls",
+    scope=Scope.CORE,
+    type=MetricType.COUNTER,
+    unit=Unit.REQUEST,
+    description="Number of async webhook calls.",
+)
+
+METRICS_ASYNC_WEBHOOK_ERROR = meter.create_metric(
+    "saleor.webhooks.async.errors",
+    scope=Scope.CORE,
+    type=MetricType.COUNTER,
+    unit=Unit.REQUEST,
+    description="Number of async webhook errors.",
+)
+
 
 def record_first_delivery_attempt_delay(event_delivery: EventDelivery) -> None:
     delay = (datetime.now(UTC) - event_delivery.created_at).total_seconds()
@@ -25,3 +41,19 @@ def record_first_delivery_attempt_delay(event_delivery: EventDelivery) -> None:
         unit=Unit.SECOND,
         attributes=attributes,
     )
+
+
+def record_async_webhooks_count(event_delivery: EventDelivery, amount: int = 1) -> None:
+    attributes = {
+        "app.name": event_delivery.webhook.app.name,
+    }
+    meter.record(METRICS_ASYNC_WEBHOOK_ERROR, amount, attributes=attributes)
+
+
+def record_async_webhooks_error_count(
+    event_delivery: EventDelivery, amount: int = 1
+) -> None:
+    attributes = {
+        "app.name": event_delivery.webhook.app.name,
+    }
+    meter.record(METRICS_ASYNC_WEBHOOK_ERROR, amount, attributes=attributes)
