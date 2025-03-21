a233a9d71ce2774df90a89377411645ab8735b6e
IKarbowiak <iga.karbowiak@saleor.io>
2025-03-21 11:17:08 +0100

    Add e2e test

--- saleor/tests/e2e/warehouse/utils/create_warehouse.py
+++ saleor/tests/e2e/warehouse/utils/create_warehouse.py
@@ -1,6 +1,6 @@
 import uuid
 
-from ... import DEFAULT_ADDRESS
+from ... import DEFAULT_WAREHOUSE_ADDRESS
 from ...utils import get_graphql_content
 
 WAREHOUSE_CREATE_MUTATION = """
@@ -37,7 +37,7 @@ def create_warehouse(
     staff_api_client,
     name="Test warehouse",
     slug=f"warehouse_slug_{uuid.uuid4()}",
-    address=DEFAULT_ADDRESS,
+    address=DEFAULT_WAREHOUSE_ADDRESS,
 ):
     variables = {
         "input": {
