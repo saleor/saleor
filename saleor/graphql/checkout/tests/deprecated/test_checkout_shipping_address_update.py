ee2d680d7c7b3fd296493e510ed9c297459566e3
IKarbowiak <iga.karbowiak@saleor.io>
2025-03-24 09:55:03 +0100

    Fix tests

--- saleor/graphql/checkout/tests/deprecated/test_checkout_shipping_address_update.py
+++ saleor/graphql/checkout/tests/deprecated/test_checkout_shipping_address_update.py
@@ -69,7 +69,9 @@ def test_checkout_shipping_address_update_by_id(
     manager = get_plugins_manager(allow_replica=False)
     lines, _ = fetch_checkout_lines(checkout)
     checkout_info = fetch_checkout_info(checkout, lines, manager)
-    mocked_update_shipping_method.assert_called_once_with(checkout_info, lines)
+    mocked_update_shipping_method.assert_called_once_with(
+        checkout_info, lines, save=False
+    )
 
 
 @mock.patch(
@@ -115,7 +117,9 @@ def test_checkout_shipping_address_update_by_token(
     manager = get_plugins_manager(allow_replica=False)
     lines, _ = fetch_checkout_lines(checkout)
     checkout_info = fetch_checkout_info(checkout, lines, manager)
-    mocked_update_shipping_method.assert_called_once_with(checkout_info, lines)
+    mocked_update_shipping_method.assert_called_once_with(
+        checkout_info, lines, save=False
+    )
 
 
 def test_checkout_shipping_address_update_neither_token_and_id_given(
