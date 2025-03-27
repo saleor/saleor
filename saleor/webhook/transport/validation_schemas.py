3d4e7b8a82b7fc9abfc25c5b1ef9eef795543a61
IKarbowiak <iga.karbowiak@saleor.io>
2025-03-27 10:49:13 +0100

    Adjust ShippingMethodSchema

--- saleor/webhook/transport/validation_schemas.py
+++ saleor/webhook/transport/validation_schemas.py
@@ -1,20 +1,12 @@
 from decimal import Decimal
+from typing import Any
 
 from prices import Money
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 from ...app.models import App
 from ...shipping.interface import ShippingMethodData
 from .shipping_common import to_shipping_app_id
-
-
-def method_metadata_is_valid(metadata) -> bool:
-    if not isinstance(metadata, dict):
-        return False
-    for key, value in metadata.items():
-        if not isinstance(key, str) or not isinstance(value, str) or not key.strip():
-            return False
-    return True
 
 
 class ShippingMethodSchema(BaseModel):
@@ -27,11 +19,36 @@ class ShippingMethodSchema(BaseModel):
     description: str | None = None
     metadata: dict[str, str] | None = None
 
+    @field_validator("metadata", mode="before")
+    @classmethod
+    def clean_metadata(cls, value: Any) -> dict[str, str]:
+        metadata = value
+        if metadata:
+            metadata = metadata if cls.method_metadata_is_valid(metadata) else {}
+        return metadata or {}
+
+    @classmethod
+    def method_metadata_is_valid(cls, metadata: Any) -> bool:
+        if not isinstance(metadata, dict):
+            return False
+        for key, value in metadata.items():
+            if (
+                not isinstance(key, str)
+                or not isinstance(value, str)
+                or not key.strip()
+            ):
+                return False
+        return True
+
     @property
     def price(self) -> Money:
         return Money(self.amount, self.currency)
 
     def get_shipping_method_data(self, app: "App"):
+        # metadata might be `None` in case it was not provided in the input
+        # in such case the `field_validator` is not called and the self.metadata
+        # is `None` instead of an empty dict
+        metadata = self.metadata or {}
         return ShippingMethodData(
             id=to_shipping_app_id(app, self.id),
             name=self.name,
@@ -39,11 +56,5 @@ class ShippingMethodSchema(BaseModel):
             maximum_delivery_days=self.maximum_delivery_days,
             minimum_delivery_days=self.minimum_delivery_days,
             description=self.description,
-            metadata=self.get_cleaned_metadata(),
+            metadata=metadata,
         )
-
-    def get_cleaned_metadata(self) -> dict[str, str]:
-        metadata = self.metadata
-        if metadata:
-            metadata = metadata if method_metadata_is_valid(metadata) else {}
-        return metadata or {}
