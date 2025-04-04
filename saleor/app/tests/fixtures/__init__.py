1593d1f2092a362a1e4d77f55395cdc0c694e32a
kadewu <krzysztof.waliczek@saleor.io>
2025-04-04 11:02:02 +0200

    Draft changes

--- saleor/app/tests/fixtures/__init__.py
+++ saleor/app/tests/fixtures/__init__.py
@@ -1,6 +1,7 @@
 from .app import *  # noqa: F403
 from .app_extension import *  # noqa: F403
 from .app_installation import *  # noqa: F403
+from .app_webhook_mutex import *  # noqa: F403
 from .webhooks.payment_app import *  # noqa: F403
 from .webhooks.shipping_app import *  # noqa: F403
 from .webhooks.tax_app import *  # noqa: F403
