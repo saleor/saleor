175d5c9e75421d0e6e0e49d4fe98d1f1e49cb2fc
Przemysław Łada <przlada@gmail.com>
2025-04-17 21:49:48 +0200

    Change attribute keys in GraphQL view

--- saleor/core/telemetry/attributes.py
+++ saleor/core/telemetry/attributes.py
@@ -1,3 +1,22 @@
 from typing import Final
 
-SALEOR_ENVIRONMENT_DOMAIN: Final = "saleor.environment_domain"
+SALEOR_ENVIRONMENT_DOMAIN: Final = "saleor.environment.domain"
+
+COMPONENT: Final = "component"
+SPAN_TYPE: Final = "span.type"
+
+# GraphQL
+SALEOR_GRAPHQL_DOCUMENT_FINGERPRINT: Final = "graphql.document_fingerprint"
+SALEOR_GRAPHQL_OPERATION_IDENTIFIER: Final = "graphql.operation.identifier"
+SALEOR_GRAPHQL_OPERATION_COST: Final = "graphql.operation.cost"
+
+# Http
+SALEOR_SOURCE_SERVICE_NAME: Final = "saleor.source.service.name"
+
+# Apps
+SALEOR_APP_ID: Final = "saleor.app.id"
+SALEOR_APP_NAME: Final = "saleor.app.name"
+
+# Webhooks
+SALEOR_WEBHOOK_EXECUTION_MODE: Final = "saleor.webhook.execution_mode"
+SALEOR_WEBHOOK_PAYLOAD_SIZE: Final = "saleor.webhook.payload.size"
