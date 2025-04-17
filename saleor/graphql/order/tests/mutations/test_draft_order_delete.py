30958bce64c66ad67ca6846d30419b2c7dfe4860
kadewu <krzysztof.waliczek@saleor.io>
2025-04-17 15:51:27 +0200

    change celery queue name env var

--- saleor/graphql/order/tests/mutations/test_draft_order_delete.py
+++ saleor/graphql/order/tests/mutations/test_draft_order_delete.py
@@ -342,7 +342,7 @@ def test_draft_order_delete_do_not_trigger_sync_webhooks(
             "app_id": app.id,
             "telemetry_context": ANY,
         },
-        queue=settings.WEBHOOK_FIFO_QUEUE_NAME,
+        queue=settings.WEBHOOK_BATCH_CELERY_QUEUE_NAME,
         MessageGroupId="core",
         MessageDeduplicationId=f"{app.id}-{app_webhook_mutex.uuid}",
         bind=True,
