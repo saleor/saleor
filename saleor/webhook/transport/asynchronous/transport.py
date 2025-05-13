107878c5cb521c0146b88d963172de1c55e4a6a5
kadewu <krzysztof.waliczek@saleor.io>
2025-05-13 15:20:14 +0200

    adjust webhook otel trace after rebasing with main branch

--- saleor/webhook/transport/asynchronous/transport.py
+++ saleor/webhook/transport/asynchronous/transport.py
@@ -714,7 +714,6 @@ def send_webhooks_async_for_app(
                 record_first_delivery_attempt_delay(delivery)
             with webhooks_otel_trace(
                 delivery.event_type,
-                domain,
                 payload_size,
                 app=webhook.app,
                 span_links=telemetry_context.links,
