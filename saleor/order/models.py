fc4c0bbba41603154f6a890f211a4cd38890dda4
IKarbowiak <iga.karbowiak@saleor.io>
2025-06-13 11:49:49 +0200

    Add index for product_type_id on order

--- saleor/order/models.py
+++ saleor/order/models.py
@@ -747,6 +747,11 @@ class OrderLine(ModelWithMetadata):
     class Meta(ModelWithMetadata.Meta):
         ordering = ("created_at", "id")
 
+        indexes = [
+            *ModelWithMetadata.Meta.indexes,
+            BTreeIndex(fields=["product_type_id"], name="product_type_id_btree_idx"),
+        ]
+
     def __str__(self):
         return (
             f"{self.product_name} ({self.variant_name})"
