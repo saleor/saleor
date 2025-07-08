98ae25194cc3e71c748c5543e1a9244624d45ed7
kadewu <krzysztof.waliczek@saleor.io>
2025-07-08 18:41:06 +0200

    fix migration task

--- saleor/channel/migrations/tasks/saleor3_21.py
+++ saleor/channel/migrations/tasks/saleor3_21.py
@@ -3,17 +3,21 @@ from django.db import transaction
 from django.db.models import Q
 
 from ....celeryconf import app
+from ....core.db.connection import allow_writer
 from ...models import Channel
 
 BATCH_SIZE = 5000
 
 
 @app.task
+@allow_writer()
 def migrate_env_variable_setting_to_channels():
+    turn_on = int(settings.TRANSACTION_BATCH_FOR_RELEASING_FUNDS) > 0
     channels = Channel.objects.order_by("pk").filter(
         ~Q(
             checkout_ttl_before_releasing_funds=settings.CHECKOUT_TTL_BEFORE_RELEASING_FUNDS
         )
+        | ~Q(release_funds_for_expired_checkouts=turn_on)
     )
     ids = channels.values_list("pk", flat=True)[:BATCH_SIZE]
 
@@ -22,7 +26,6 @@ def migrate_env_variable_setting_to_channels():
         with transaction.atomic():
             # lock the batch of objects
             _channels = list(qs.select_for_update(of=(["self"])))
-            turn_on = settings.TRANSACTION_BATCH_FOR_RELEASING_FUNDS > 0
             qs.update(
                 checkout_ttl_before_releasing_funds=settings.CHECKOUT_TTL_BEFORE_RELEASING_FUNDS,
                 release_funds_for_expired_checkouts=turn_on,
