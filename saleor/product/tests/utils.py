8e31131560ce6f1cd2b7b3a4e14bd51f3fa57ac3
fowczrek <f.a.owczarek@gmail.com>
2025-09-09 15:08:00 +0200

    Fix image validation in ProductBulkCreate mutation

--- saleor/product/tests/utils.py
+++ saleor/product/tests/utils.py
@@ -12,6 +12,16 @@ def create_image(image_name="product2"):
     return image, image_name
 
 
+def create_image_without_extension(image_name="product_img_without_extension"):
+    # Ensure the image_name does not have an extension
+    image_name = image_name.split(".")[0]
+    img_data = BytesIO()
+    image = Image.new("RGB", size=(1, 1))
+    image.save(img_data, format="JPEG")
+    img = SimpleUploadedFile(image_name, img_data.getvalue(), "image/jpeg")
+    return img, image_name
+
+
 def create_zip_file_with_image_ext():
     file_name = "product.jpg"
     file_data = SimpleUploadedFile(file_name, b"product_data", "application/zip")
