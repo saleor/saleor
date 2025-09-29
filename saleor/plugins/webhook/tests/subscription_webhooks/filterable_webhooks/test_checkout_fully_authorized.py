e67c0a19ebde69e045a873992a945bdfb6a0052b
Cezary Miącz <cezary.miacz@saleor.io>
2025-09-29 18:05:52 +0200

    Fix tests

--- saleor/plugins/webhook/tests/subscription_webhooks/filterable_webhooks/test_checkout_fully_authorized.py
+++ saleor/plugins/webhook/tests/subscription_webhooks/filterable_webhooks/test_checkout_fully_authorized.py
@@ -28,7 +28,7 @@ subscription {
 
 
 @patch(
-    "saleor.webhook.transport.asynchronous.transport.send_webhook_request_async.apply_async"
+    "saleor.webhook.transport.asynchronous.transport.send_webhooks_async_for_app.apply_async"
 )
 @override_settings(
     PLUGINS=["saleor.plugins.webhook.plugin.WebhookPlugin"],
@@ -91,7 +91,7 @@ def test_checkout_fully_authorized(
 
 
 @patch(
-    "saleor.webhook.transport.asynchronous.transport.send_webhook_request_async.apply_async"
+    "saleor.webhook.transport.asynchronous.transport.send_webhooks_async_for_app.apply_async"
 )
 @override_settings(
     PLUGINS=["saleor.plugins.webhook.plugin.WebhookPlugin"],
