cb94c82d5ab36952bc6686082dadbd6519858221
Cezary Miącz <cezary.miacz@saleor.io>
2025-10-10 10:53:01 +0200

    Add AppWebhookMutex uuid to allow next iterations within 5-minute deduplication interval

--- saleor/app/utils.py
+++ saleor/app/utils.py
@@ -1,4 +1,5 @@
 import logging
+import uuid
 from contextlib import contextmanager
 
 from django.db import DatabaseError, transaction
@@ -30,15 +31,19 @@ def get_active_tax_apps(identifiers: list[str] | None = None):
 def acquire_webhook_lock(app_id: int):
     try:
         with transaction.atomic():
-            AppWebhookMutex.objects.select_for_update(nowait=True, of=(["self"])).get(
-                app_id=app_id
-            )
-            yield True
+            mutex = AppWebhookMutex.objects.select_for_update(
+                nowait=True, of=(["self"])
+            ).get(app_id=app_id)
+            new_lock_uuid = uuid.uuid4()  # Generate a new lock UUID for next iteration
+            yield True, new_lock_uuid
+            mutex.lock_uuid = new_lock_uuid
+            mutex.save()
+
     except DatabaseError:
         logger.warning("Couldn't acquire the webhook lock. App ID: %s", app_id)
-        yield False
+        yield False, None
     except AppWebhookMutex.DoesNotExist:
         logger.warning("AppWebhookMutex entry does not exist. App ID: %s", app_id)
         AppWebhookMutex.objects.get_or_create(app_id=app_id)
-        with acquire_webhook_lock(app_id) as acquired:
-            yield acquired
+        with acquire_webhook_lock(app_id) as (acquired, lock_uuid):
+            yield acquired, lock_uuid
