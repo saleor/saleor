5c728c13cab6373088525a2a5462c01d0f0c4c66
Przemysław Łada <przlada@gmail.com>
2025-10-14 15:01:22 +0200

    Optimize GraphQL metrics (#18314)
    * Refactor metrics tests to use InMemoryMetricReader instead of mocking
    
    * Refactor tests to use get_metric_and_data_point
    
    * Drop setting operation_identifier for GraphQL metrics
    
    * Refactor record_graphql_query_duration
    
    * Add saleor.graphql.slow_operation.duration metric
    
    * TELEMETRY_SLOW_GRAPHQL_OPERATION_THRESHOLD env
    
    * Add metric scope verification to get_metric_data test utils
    
    * Fix test_meter_create_histogram_with_explicit_bucket_boundaries
    
    * Add comment with the description of TELEMETRY_SLOW_GRAPHQL_OPERATION_THRESHOLD settings

--- saleor/settings.py
+++ saleor/settings.py
@@ -1090,6 +1090,11 @@ TELEMETRY_METER_CLASS = "saleor.core.telemetry.metric.Meter"
 # Whether to raise or log exceptions for telemetry unit conversion errors
 # Disabled by default to prevent disruptions caused by unexpected unit conversion issues
 TELEMETRY_RAISE_UNIT_CONVERSION_ERRORS = False
+# The default threshold for slow operations is set to 1 second, based on production monitoring data.
+# Only a small percentage of queries are expected to exceed this threshold.
+TELEMETRY_SLOW_GRAPHQL_OPERATION_THRESHOLD = float(
+    os.environ.get("TELEMETRY_SLOW_GRAPHQL_OPERATION_THRESHOLD", 1.0)
+)
 
 # Additional hash suffix, allowing to invalidate cached schema. In production usually we want this to be empty.
 # For development envs, where schema may change often, it may be convenient to set it to e.g. commit hash value.
