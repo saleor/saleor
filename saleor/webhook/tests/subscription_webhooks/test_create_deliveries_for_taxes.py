374d2832a90b6408d98e17cac2ffd3f94f1e22e9
IKarbowiak <iga.karbowiak@saleor.io>
2025-10-24 11:46:20 +0200

    Allow fetching checkout and order user with HANDLE_TAXES perm

--- saleor/webhook/tests/subscription_webhooks/test_create_deliveries_for_taxes.py
+++ saleor/webhook/tests/subscription_webhooks/test_create_deliveries_for_taxes.py
@@ -91,6 +91,9 @@ subscription {
               key
               value
             }
+            user {
+              id
+            }
         }
           ... on Order {
             id
@@ -101,6 +104,9 @@ subscription {
             privateMetadata {
               key
               value
+            }
+            user {
+              id
             }
           }
         }
@@ -124,15 +130,14 @@ def subscription_order_calculate_taxes(subscription_webhook):
 @pytest.mark.parametrize("charge_taxes", [True, False])
 def test_checkout_calculate_taxes(
     checkout_ready_to_complete,
-    webhook_app,
-    permission_handle_taxes,
+    tax_app,
     charge_taxes,
+    customer_user,
 ):
     # given
-    webhook_app.permissions.add(permission_handle_taxes)
     webhook = Webhook.objects.create(
         name="Webhook",
-        app=webhook_app,
+        app=tax_app,
         target_url="http://www.example.com/any",
         subscription_query=TAXES_SUBSCRIPTION_QUERY,
     )
@@ -143,6 +148,9 @@ def test_checkout_calculate_taxes(
     tax_configuration.charge_taxes = charge_taxes
     tax_configuration.save(update_fields=["charge_taxes"])
     tax_configuration.country_exceptions.all().delete()
+
+    checkout_ready_to_complete.user = customer_user
+    checkout_ready_to_complete.save(update_fields=["user_id"])
 
     # when
     deliveries = create_delivery_for_subscription_sync_event(
@@ -190,6 +198,9 @@ def test_checkout_calculate_taxes(
                     {"key": key, "value": value}
                     for key, value in private_metadata.items()
                 ],
+                "user": {
+                    "id": to_global_id_or_none(customer_user),
+                },
             },
         },
     }
@@ -198,16 +209,14 @@ def test_checkout_calculate_taxes(
 @freeze_time("2020-03-18 12:00:00")
 def test_checkout_calculate_taxes_with_free_shipping_voucher(
     checkout_with_voucher_free_shipping,
-    webhook_app,
-    permission_handle_taxes,
+    tax_app,
     checkout_with_shipping_address,
 ):
     # given
     checkout = checkout_with_voucher_free_shipping
-    webhook_app.permissions.add(permission_handle_taxes)
     webhook = Webhook.objects.create(
         name="Webhook",
-        app=webhook_app,
+        app=tax_app,
         target_url="http://www.example.com/any",
         subscription_query=TAXES_SUBSCRIPTION_QUERY,
     )
@@ -242,6 +251,7 @@ def test_checkout_calculate_taxes_with_free_shipping_voucher(
                     {"key": key, "value": value}
                     for key, value in private_metadata.items()
                 ],
+                "user": None,
             },
         },
     }
@@ -250,15 +260,13 @@ def test_checkout_calculate_taxes_with_free_shipping_voucher(
 @freeze_time("2020-03-18 12:00:00")
 def test_checkout_calculate_taxes_with_pregenerated_payload(
     checkout_with_voucher_free_shipping,
-    webhook_app,
-    permission_handle_taxes,
+    tax_app,
 ):
     # given
     checkout = checkout_with_voucher_free_shipping
-    webhook_app.permissions.add(permission_handle_taxes)
     webhook = Webhook.objects.create(
         name="Webhook",
-        app=webhook_app,
+        app=tax_app,
         target_url="http://www.example.com/any",
         subscription_query=TAXES_SUBSCRIPTION_QUERY,
     )
@@ -281,8 +289,7 @@ def test_checkout_calculate_taxes_with_pregenerated_payload(
 @freeze_time("2020-03-18 12:00:00")
 def test_checkout_calculate_taxes_with_entire_order_voucher(
     checkout_with_voucher,
-    webhook_app,
-    permission_handle_taxes,
+    tax_app,
     address,
     shipping_method,
 ):
@@ -293,10 +300,9 @@ def test_checkout_calculate_taxes_with_entire_order_voucher(
     checkout.billing_address = address
     checkout.save()
 
-    webhook_app.permissions.add(permission_handle_taxes)
     webhook = Webhook.objects.create(
         name="Webhook",
-        app=webhook_app,
+        app=tax_app,
         target_url="http://www.example.com/any",
         subscription_query=TAXES_SUBSCRIPTION_QUERY,
     )
@@ -347,6 +353,7 @@ def test_checkout_calculate_taxes_with_entire_order_voucher(
                     {"key": key, "value": value}
                     for key, value in private_metadata.items()
                 ],
+                "user": None,
             },
         },
     }
@@ -356,14 +363,13 @@ def test_checkout_calculate_taxes_with_entire_order_voucher(
 def test_checkout_calculate_taxes_with_entire_order_voucher_once_per_order(
     voucher,
     checkout_with_voucher,
-    webhook_app,
+    tax_app,
     permission_handle_taxes,
 ):
     # given
-    webhook_app.permissions.add(permission_handle_taxes)
     webhook = Webhook.objects.create(
         name="Webhook",
-        app=webhook_app,
+        app=tax_app,
         target_url="http://www.example.com/any",
         subscription_query=TAXES_SUBSCRIPTION_QUERY,
     )
@@ -414,6 +420,7 @@ def test_checkout_calculate_taxes_with_entire_order_voucher_once_per_order(
                     {"key": key, "value": value}
                     for key, value in private_metadata.items()
                 ],
+                "user": None,
             },
         },
     }
@@ -423,8 +430,7 @@ def test_checkout_calculate_taxes_with_entire_order_voucher_once_per_order(
 def test_checkout_calculate_taxes_with_shipping_voucher(
     checkout_with_item,
     voucher_free_shipping,
-    webhook_app,
-    permission_handle_taxes,
+    tax_app,
     address,
     shipping_method,
 ):
@@ -435,10 +441,9 @@ def test_checkout_calculate_taxes_with_shipping_voucher(
     checkout.billing_address = address
     checkout.voucher_code = voucher_free_shipping.codes.first()
 
-    webhook_app.permissions.add(permission_handle_taxes)
     webhook = Webhook.objects.create(
         name="Webhook",
-        app=webhook_app,
+        app=tax_app,
         target_url="http://www.example.com/any",
         subscription_query=TAXES_SUBSCRIPTION_QUERY,
     )
@@ -487,6 +492,7 @@ def test_checkout_calculate_taxes_with_shipping_voucher(
                     {"key": key, "value": value}
                     for key, value in private_metadata.items()
                 ],
+                "user": None,
             },
         },
     }
@@ -495,8 +501,7 @@ def test_checkout_calculate_taxes_with_shipping_voucher(
 @freeze_time("2020-03-18 12:00:00")
 def test_checkout_calculate_taxes_with_order_promotion(
     checkout_with_item_and_order_discount,
-    webhook_app,
-    permission_handle_taxes,
+    tax_app,
 ):
     # given
     checkout = checkout_with_item_and_order_discount
@@ -505,10 +510,9 @@ def test_checkout_calculate_taxes_with_order_promotion(
         channel=checkout.channel
     ).price_amount
     channel_id = to_global_id_or_none(checkout.channel)
-    webhook_app.permissions.add(permission_handle_taxes)
     webhook = Webhook.objects.create(
         name="Webhook",
-        app=webhook_app,
+        app=tax_app,
         target_url="http://www.example.com/any",
         subscription_query=TAXES_SUBSCRIPTION_QUERY,
     )
@@ -563,6 +567,7 @@ def test_checkout_calculate_taxes_with_order_promotion(
                     {"key": key, "value": value}
                     for key, value in private_metadata.items()
                 ],
+                "user": None,
             },
         },
     }
@@ -571,14 +576,12 @@ def test_checkout_calculate_taxes_with_order_promotion(
 @freeze_time("2020-03-18 12:00:00")
 def test_checkout_calculate_taxes_empty_checkout(
     checkout,
-    webhook_app,
-    permission_handle_taxes,
+    tax_app,
 ):
     # given
-    webhook_app.permissions.add(permission_handle_taxes)
     webhook = Webhook.objects.create(
         name="Webhook",
-        app=webhook_app,
+        app=tax_app,
         target_url="http://www.example.com/any",
         subscription_query=TAXES_SUBSCRIPTION_QUERY,
     )
@@ -613,6 +616,7 @@ def test_checkout_calculate_taxes_empty_checkout(
                     {"key": key, "value": value}
                     for key, value in private_metadata.items()
                 ],
+                "user": None,
             },
         },
     }
@@ -620,9 +624,7 @@ def test_checkout_calculate_taxes_empty_checkout(
 
 @freeze_time("2020-03-18 12:00:00")
 @pytest.mark.parametrize("charge_taxes", [True, False])
-def test_order_calculate_taxes(
-    order_line, webhook_app, permission_handle_taxes, shipping_zone, charge_taxes
-):
+def test_order_calculate_taxes(order_line, tax_app, shipping_zone, charge_taxes):
     # given
     order = order_line.order
     expected_shipping_price = Money("2.00", order.currency)
@@ -639,10 +641,9 @@ def test_order_calculate_taxes(
     )
     shipping_method = shipping_zone.shipping_methods.first()
     order.shipping_method = shipping_method
-    webhook_app.permissions.add(permission_handle_taxes)
     webhook = Webhook.objects.create(
         name="Webhook",
-        app=webhook_app,
+        app=tax_app,
         target_url="http://www.example.com/any",
         subscription_query=TAXES_SUBSCRIPTION_QUERY,
     )
@@ -698,6 +699,7 @@ def test_order_calculate_taxes(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -795,6 +797,7 @@ def test_draft_order_calculate_taxes_line_discount(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -894,6 +897,7 @@ def test_draft_order_calculate_taxes_entire_order_voucher(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -989,6 +993,7 @@ def test_draft_order_calculate_taxes_apply_once_per_order_voucher(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -1094,6 +1099,7 @@ def test_order_calculate_taxes_specific_product_voucher(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -1143,6 +1149,7 @@ def test_draft_order_calculate_taxes_free_shipping_voucher(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -1252,6 +1259,7 @@ def test_order_calculate_taxes_with_manual_discount(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -1298,6 +1306,7 @@ def test_order_calculate_taxes_empty_order(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": None,
             },
         },
     }
@@ -1393,6 +1402,7 @@ def test_order_calculate_taxes_order_promotion(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -1504,6 +1514,7 @@ def test_order_calculate_taxes_order_voucher_and_manual_discount(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -1619,6 +1630,7 @@ def test_order_calculate_taxes_order_promotion_and_manual_discount(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -1725,6 +1737,7 @@ def test_order_calculate_taxes_free_shipping_voucher_and_manual_discount_fixed(
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
@@ -1837,6 +1850,7 @@ def test_order_calculate_taxes_free_shipping_voucher_and_manual_discount_percent
                     {"key": key, "value": value}
                     for key, value in order.private_metadata.items()
                 ],
+                "user": {"id": to_global_id_or_none(order.user)},
             },
         },
     }
