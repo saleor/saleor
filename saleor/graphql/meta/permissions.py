56a493cd55badca62fdd84cc2bf6c4a40df7dcb2
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2025-10-28 12:53:15 +0100

    Allow fetching `privateMetadata` and `user` from `Checkout` and `Order` with `HANDLE_TAXES` permission (#18372)
    * Allow fetching checkout and order privateMetadata with handle taxes permission
    
    * Allow fetching checkout and order user with HANDLE_TAXES perm
    
    * Fix workflows

--- saleor/graphql/meta/permissions.py
+++ saleor/graphql/meta/permissions.py
@@ -160,7 +160,7 @@ def product_type_permissions(
 
 
 def order_permissions(_info: ResolveInfo, _object_pk: Any) -> list[BasePermissionEnum]:
-    return [OrderPermissions.MANAGE_ORDERS]
+    return [OrderPermissions.MANAGE_ORDERS, CheckoutPermissions.HANDLE_TAXES]
 
 
 def invoice_permissions(
@@ -204,7 +204,7 @@ def channel_permissions(
 def checkout_permissions(
     _info: ResolveInfo, _object_pk: Any
 ) -> list[BasePermissionEnum]:
-    return [CheckoutPermissions.MANAGE_CHECKOUTS]
+    return [CheckoutPermissions.MANAGE_CHECKOUTS, CheckoutPermissions.HANDLE_TAXES]
 
 
 def page_permissions(_info: ResolveInfo, _object_pk: Any) -> list[BasePermissionEnum]:
