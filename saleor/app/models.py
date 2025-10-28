26f4d5fc6cd29257509b78c50d53f74ac0ee19ce
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-10-28 09:58:37 +0100

    Model

--- saleor/app/models.py
+++ saleor/app/models.py
@@ -173,6 +173,7 @@ class AppExtension(models.Model):
         null=True,
         choices=AppExtensionHttpMethod.CHOICES,
     )
+    settings = models.JSONField(blank=True, default=dict)
 
 
 class AppInstallation(Job):
