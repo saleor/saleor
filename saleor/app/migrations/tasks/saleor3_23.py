0246ac899262eab4635b6c0a792786073809ae7a
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2025-12-08 10:37:47 +0100

    recursive migration

--- saleor/app/migrations/tasks/saleor3_23.py
+++ saleor/app/migrations/tasks/saleor3_23.py
@@ -1,4 +1,6 @@
+from django.apps import apps as registry
 from django.db import transaction
+from django.db.models.signals import post_migrate
 
 from ....celeryconf import app
 from ....core.db.connection import allow_writer
@@ -7,56 +9,50 @@ from ...models import AppExtension
 BATCH_SIZE = 100
 
 
-def queryset_in_batches(queryset):
-    """Slice a queryset into batches.
-
-    Input queryset should be sorted be pk.
-    """
-    start_pk = 0
-
-    while True:
-        qs = queryset.order_by("pk").filter(pk__gt=start_pk)[:BATCH_SIZE]
-        qs_list = list(qs)
-
-        if not qs_list:
-            break
-
-        yield qs_list
-
-        start_pk = qs_list[-1].pk
-
-
 def fill_settings_json():
     # Preserve filled settings, only migrate if empty (fill them)
     qs = AppExtension.objects.filter(
         settings={}, target__in=["widget", "new_tab"]
-    ).only("target", "http_target_method", "settings")
+    ).only("target", "http_target_method", "settings")[:BATCH_SIZE]
 
-    for chunk in queryset_in_batches(qs):
-        app_extensions = chunk
+    affected_items = []
 
-        with transaction.atomic():
-            dirty_extensions: list[AppExtension] = []
+    with transaction.atomic():
+        app_extensions = list(qs)
+        dirty_extensions: list[AppExtension] = []
 
-            for extension in app_extensions:
-                if extension.target.upper() == "WIDGET":
-                    extension.settings = {
-                        "widgetTarget": {"method": extension.http_target_method}
-                    }
+        affected_items = app_extensions
 
-                    dirty_extensions.append(extension)
+        for extension in app_extensions:
+            if extension.target.upper() == "WIDGET":
+                extension.settings = {
+                    "widgetTarget": {"method": extension.http_target_method}
+                }
 
-                if extension.target.upper() == "NEW_TAB":
-                    extension.settings = {
-                        "newTabTarget": {"method": extension.http_target_method}
-                    }
+                dirty_extensions.append(extension)
 
-                    dirty_extensions.append(extension)
+            if extension.target.upper() == "NEW_TAB":
+                extension.settings = {
+                    "newTabTarget": {"method": extension.http_target_method}
+                }
 
-            AppExtension.objects.bulk_update(dirty_extensions, fields=["settings"])
+                dirty_extensions.append(extension)
+
+        AppExtension.objects.bulk_update(dirty_extensions, fields=["settings"])
+
+    if affected_items:
+        fill_app_extension_settings_task.delay()
 
 
 @app.task
 @allow_writer()
 def fill_app_extension_settings_task():
     fill_settings_json()
+
+
+def fill_app_extension_settings_migration(apps, _schema_editor):
+    def on_migrations_complete(sender=None, **kwargs):
+        fill_app_extension_settings_task.delay()
+
+    sender = registry.get_app_config("app")
+    post_migrate.connect(on_migrations_complete, weak=False, sender=sender)
