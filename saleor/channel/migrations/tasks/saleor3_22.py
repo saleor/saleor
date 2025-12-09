120ef10b984d7d74bed7a163d767f19c62d86733
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2025-12-09 15:53:36 +0100

    Fix automatic checkout completion for empty lines (#18588)
    * Fix automatic checkout completion for empty lines
    
    * Adjust validation
    
    * Adjust shipping verification
    
    * Do not trigger checkout completion for checkouts with 0 total
    
    * Adjust scheduler

--- saleor/channel/migrations/tasks/saleor3_22.py
+++ saleor/channel/migrations/tasks/saleor3_22.py
@@ -6,6 +6,7 @@ from ...models import Channel
 @app.task
 @allow_writer()
 def set_automatic_completion_delay_task():
-    Channel.objects.filter(automatically_complete_fully_paid_checkouts=True).update(
-        automatic_completion_delay=0
-    )
+    Channel.objects.filter(
+        automatically_complete_fully_paid_checkouts=True,
+        automatic_completion_delay__isnull=True,
+    ).update(automatic_completion_delay=0)
