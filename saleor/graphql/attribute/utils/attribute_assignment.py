d0a851aaaa357d62514bef2cb7ed119594a92b41
IKarbowiak <iga.karbowiak@saleor.io>
2025-12-15 13:16:51 +0100

    Improve attribute fetching in AttributeAssignmentMixin

--- saleor/graphql/attribute/utils/attribute_assignment.py
+++ saleor/graphql/attribute/utils/attribute_assignment.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING, cast
 
 import graphene
 from django.core.exceptions import ValidationError
-from django.db.models import Q
+from django.db.models import Prefetch, Q
 from django.db.models.expressions import Exists, OuterRef
 from graphql.error import GraphQLError
 
@@ -11,6 +11,7 @@ from ....attribute import AttributeInputType
 from ....attribute import models as attribute_models
 from ....attribute.models import AttributeValue
 from ....attribute.utils import associate_attribute_values_to_instance
+from ....page import models as page_models
 from ....page.error_codes import PageErrorCode
 from ....product import models as product_models
 from ....product.error_codes import ProductErrorCode
@@ -79,7 +80,16 @@ class AttributeAssignmentMixin:
             Q(pk__in=id_map.keys()) | Q(external_reference__in=ext_ref_set)
         )
         nodes = list(
-            nodes.prefetch_related("reference_product_types", "reference_page_types")
+            nodes.prefetch_related(
+                Prefetch(
+                    "reference_product_types",
+                    queryset=product_models.ProductType.objects.only("id"),
+                ),
+                Prefetch(
+                    "reference_page_types",
+                    queryset=page_models.PageType.objects.only("id"),
+                ),
+            )
         )
 
         resolved_pks = {node.pk for node in nodes}
@@ -97,6 +107,16 @@ class AttributeAssignmentMixin:
                 code=error_class.NOT_FOUND.value,
             )
         return nodes
+
+    @classmethod
+    def _clear_prefetch_cache(cls, attributes: list[attribute_models.Attribute]):
+        """Clear prefetched objects cache.
+
+        After validation is complete, the prefetched relations are no longer needed.
+        """
+        for attribute in attributes:
+            if hasattr(attribute, "_prefetched_objects_cache"):
+                attribute._prefetched_objects_cache.clear()
 
     @classmethod
     def clean_input(
@@ -128,6 +148,9 @@ class AttributeAssignmentMixin:
             error_class,
             creation,
         )
+
+        # Clear prefetched cache after validation to prevent memory leaks
+        cls._clear_prefetch_cache(attributes)
 
         return cleaned_input
 
