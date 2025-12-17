6a51b7beac5064cfd8d3892982489eae9270e432
Marcin Wcisło <115464873+wcislo-saleor@users.noreply.github.com>
2025-12-17 11:36:40 +0100

    Release 3.20.106 (#18629)

--- saleor/__init__.py
+++ saleor/__init__.py
@@ -3,7 +3,7 @@ import pillow_avif  # noqa: F401 # imported for side effects
 from .celeryconf import app as celery_app
 
 __all__ = ["celery_app"]
-__version__ = "3.20.105"
+__version__ = "3.20.106"
 
 
 class PatchedSubscriberExecutionContext:
