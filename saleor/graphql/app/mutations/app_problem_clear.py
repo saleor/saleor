19902bf15605d9ac82f75747a90f6cf9e1ec3adb
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-01-30 10:26:35 +0100

    add mutation for clearning app errors for staff

--- saleor/graphql/app/mutations/app_problem_clear.py
+++ saleor/graphql/app/mutations/app_problem_clear.py
@@ -3,13 +3,15 @@ from django.core.exceptions import ValidationError
 
 from ....app.error_codes import AppErrorCode
 from ....app.models import AppProblem, AppProblemType
-from ....core.exceptions import PermissionDenied
 from ....permission.auth_filters import AuthorizationFilters
+from ....permission.enums import AppPermission
+from ...account.utils import can_manage_app
 from ...core import ResolveInfo
 from ...core.descriptions import ADDED_IN_322
 from ...core.doc_category import DOC_CATEGORY_APPS
 from ...core.mutations import BaseMutation
 from ...core.types import AppError
+from ...utils import get_user_or_app_from_context, requestor_is_superuser
 from ..types import App
 
 
@@ -20,6 +22,13 @@ class AppProblemClear(BaseMutation):
     )
 
     class Arguments:
+        app = graphene.ID(
+            required=False,
+            description=(
+                "ID of the app to clear problems for. "
+                "Required for staff, disallowed for app callers."
+            ),
+        )
         aggregate = graphene.String(
             required=False,
             description=(
@@ -34,18 +43,56 @@ class AppProblemClear(BaseMutation):
     class Meta:
         description = "Clear problems from the calling app." + ADDED_IN_322
         doc_category = DOC_CATEGORY_APPS
-        permissions = (AuthorizationFilters.AUTHENTICATED_APP,)
+        permissions = (
+            AppPermission.MANAGE_APPS,
+            AuthorizationFilters.AUTHENTICATED_APP,
+        )
         error_type_class = AppError
         error_type_field = "app_errors"
 
     @classmethod
     def perform_mutation(cls, _root, info: ResolveInfo, /, **data):
-        app = info.context.app
-        if not app:
-            raise PermissionDenied(permissions=[AuthorizationFilters.AUTHENTICATED_APP])
-
+        app_id = data.get("app")
         aggregate = data.get("aggregate")
         key = data.get("key")
+
+        if info.context.app:
+            # App caller
+            if app_id is not None:
+                raise ValidationError(
+                    {
+                        "app": ValidationError(
+                            "App callers cannot specify the 'app' argument.",
+                            code=AppErrorCode.INVALID.value,
+                        )
+                    }
+                )
+            target_app = info.context.app
+        else:
+            # Staff caller
+            if app_id is None:
+                raise ValidationError(
+                    {
+                        "app": ValidationError(
+                            "The 'app' argument is required for staff users.",
+                            code=AppErrorCode.REQUIRED.value,
+                        )
+                    }
+                )
+            target_app = cls.get_node_or_error(info, app_id, field="app", only_type=App)
+            requestor = get_user_or_app_from_context(info.context)
+            if not requestor_is_superuser(requestor) and not can_manage_app(
+                requestor, target_app
+            ):
+                raise ValidationError(
+                    {
+                        "app": ValidationError(
+                            "You can't manage this app.",
+                            code=AppErrorCode.OUT_OF_SCOPE_APP.value,
+                        )
+                    }
+                )
+
         if aggregate is not None and key is not None:
             raise ValidationError(
                 {
@@ -56,10 +103,10 @@ class AppProblemClear(BaseMutation):
                 }
             )
 
-        qs = AppProblem.objects.filter(app=app, type=AppProblemType.OWN)
+        qs = AppProblem.objects.filter(app=target_app, type=AppProblemType.OWN)
         if aggregate is not None:
             qs = qs.filter(aggregate=aggregate)
         if key is not None:
             qs = qs.filter(key=key)
         qs.delete()
-        return AppProblemClear(app=app)
+        return AppProblemClear(app=target_app)
--- saleor/graphql/app/tests/mutations/test_app_problem_clear.py
+++ saleor/graphql/app/tests/mutations/test_app_problem_clear.py
@@ -1,9 +1,11 @@
+import graphene
+
 from .....app.models import AppProblem, AppProblemType
 from ....tests.utils import assert_no_permission, get_graphql_content
 
 APP_PROBLEM_CLEAR_MUTATION = """
-    mutation AppProblemClear($aggregate: String, $key: String) {
-        appProblemClear(aggregate: $aggregate, key: $key) {
+    mutation AppProblemClear($app: ID, $aggregate: String, $key: String) {
+        appProblemClear(app: $app, aggregate: $aggregate, key: $key) {
             app {
                 id
                 problems {
@@ -63,7 +65,37 @@ def test_app_problem_clear_by_aggregate(app_api_client, app):
     assert remaining.first().aggregate == "group-b"
 
 
-def test_app_problem_clear_by_staff_user_fails(
+def test_app_problem_clear_by_staff_user_with_app_arg(
+    staff_api_client, app, permission_manage_apps
+):
+    # given
+    staff_api_client.user.user_permissions.add(permission_manage_apps)
+    AppProblem.objects.create(app=app, type=AppProblemType.OWN, message="Problem 1")
+    AppProblem.objects.create(app=app, type=AppProblemType.OWN, message="Problem 2")
+    variables = {"app": graphene.Node.to_global_id("App", app.id)}
+
+    # when
+    response = staff_api_client.post_graphql(APP_PROBLEM_CLEAR_MUTATION, variables)
+    content = get_graphql_content(response)
+
+    # then
+    data = content["data"]["appProblemClear"]
+    assert not data["errors"]
+    assert AppProblem.objects.filter(app=app).count() == 0
+
+
+def test_app_problem_clear_by_staff_without_permission(staff_api_client, app):
+    # given
+    variables = {"app": graphene.Node.to_global_id("App", app.id)}
+
+    # when
+    response = staff_api_client.post_graphql(APP_PROBLEM_CLEAR_MUTATION, variables)
+
+    # then
+    assert_no_permission(response)
+
+
+def test_app_problem_clear_by_staff_without_app_arg(
     staff_api_client, permission_manage_apps
 ):
     # given
@@ -71,9 +103,109 @@ def test_app_problem_clear_by_staff_user_fails(
 
     # when
     response = staff_api_client.post_graphql(APP_PROBLEM_CLEAR_MUTATION)
+    content = get_graphql_content(response)
 
     # then
-    assert_no_permission(response)
+    data = content["data"]["appProblemClear"]
+    assert len(data["errors"]) == 1
+    assert data["errors"][0]["field"] == "app"
+    assert data["errors"][0]["code"] == "REQUIRED"
+
+
+def test_app_problem_clear_app_caller_with_app_arg(app_api_client, app):
+    # given
+    variables = {"app": graphene.Node.to_global_id("App", app.id)}
+
+    # when
+    response = app_api_client.post_graphql(APP_PROBLEM_CLEAR_MUTATION, variables)
+    content = get_graphql_content(response)
+
+    # then
+    data = content["data"]["appProblemClear"]
+    assert len(data["errors"]) == 1
+    assert data["errors"][0]["field"] == "app"
+    assert data["errors"][0]["code"] == "INVALID"
+
+
+def test_app_problem_clear_by_staff_with_aggregate_filter(
+    staff_api_client, app, permission_manage_apps
+):
+    # given
+    staff_api_client.user.user_permissions.add(permission_manage_apps)
+    AppProblem.objects.create(
+        app=app, type=AppProblemType.OWN, message="Problem 1", aggregate="group-a"
+    )
+    AppProblem.objects.create(
+        app=app, type=AppProblemType.OWN, message="Problem 2", aggregate="group-b"
+    )
+    variables = {
+        "app": graphene.Node.to_global_id("App", app.id),
+        "aggregate": "group-a",
+    }
+
+    # when
+    response = staff_api_client.post_graphql(APP_PROBLEM_CLEAR_MUTATION, variables)
+    content = get_graphql_content(response)
+
+    # then
+    data = content["data"]["appProblemClear"]
+    assert not data["errors"]
+    remaining = AppProblem.objects.filter(app=app)
+    assert remaining.count() == 1
+    assert remaining.first().aggregate == "group-b"
+
+
+def test_app_problem_clear_by_staff_with_key_filter(
+    staff_api_client, app, permission_manage_apps
+):
+    # given
+    staff_api_client.user.user_permissions.add(permission_manage_apps)
+    AppProblem.objects.create(
+        app=app, type=AppProblemType.OWN, message="Keyed", key="my-key"
+    )
+    AppProblem.objects.create(
+        app=app, type=AppProblemType.OWN, message="Other", key="other-key"
+    )
+    variables = {
+        "app": graphene.Node.to_global_id("App", app.id),
+        "key": "my-key",
+    }
+
+    # when
+    response = staff_api_client.post_graphql(APP_PROBLEM_CLEAR_MUTATION, variables)
+    content = get_graphql_content(response)
+
+    # then
+    data = content["data"]["appProblemClear"]
+    assert not data["errors"]
+    remaining = AppProblem.objects.filter(app=app)
+    assert remaining.count() == 1
+    assert remaining.first().key == "other-key"
+
+
+def test_app_problem_clear_by_staff_out_of_scope_app(
+    staff_api_client, app, permission_manage_apps
+):
+    # given
+    staff_api_client.user.user_permissions.add(permission_manage_apps)
+    # Give the target app a permission the staff user doesn't have, making it
+    # out of scope.
+    from .....permission.models import Permission
+
+    extra_perm = Permission.objects.get(codename="manage_orders")
+    app.permissions.add(extra_perm)
+
+    variables = {"app": graphene.Node.to_global_id("App", app.id)}
+
+    # when
+    response = staff_api_client.post_graphql(APP_PROBLEM_CLEAR_MUTATION, variables)
+    content = get_graphql_content(response)
+
+    # then
+    data = content["data"]["appProblemClear"]
+    assert len(data["errors"]) == 1
+    assert data["errors"][0]["field"] == "app"
+    assert data["errors"][0]["code"] == "OUT_OF_SCOPE_APP"
 
 
 def test_app_problem_clear_by_key(app_api_client, app):
