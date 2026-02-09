05b1665441e06992395bb541c6ac5a877309cd5b
Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>
2026-02-09 11:58:18 +0100

    Add `transactions` query and extend order transactions where filter with `pspReference` (#18759)
    * Add transactions query
    
    * Allow users with manage_orders to query transaction
    
    * Allow filtering order transactions by psp reference
    
    * Improve transactions tests
    
    * Add index for transaction psp reference
    
    * Adjust transactions query
    
    * Allow filtering transactions by app indentifier
    
    * Split transactions tests
    
    * Add index for transactionItem app_identifier

--- saleor/graphql/payment/resolvers.py
+++ saleor/graphql/payment/resolvers.py
@@ -1,6 +1,10 @@
+from django.db.models import Q
+
 from ...app import models as app_models
+from ...checkout import models as checkout_models
 from ...order import models as order_models
 from ...payment import models
+from ...permission.enums import OrderPermissions
 from ..account.utils import get_user_accessible_channels
 from ..core.context import get_database_connection_name
 from ..utils import get_user_or_app_from_context
@@ -37,4 +41,33 @@ def resolve_transaction(info, id):
         models.TransactionItem.objects.using(get_database_connection_name(info.context))
         .filter(**query_params)
         .first()
+    )
+
+
+def resolve_transactions(info):
+    connection_name = get_database_connection_name(info.context)
+    requestor = get_user_or_app_from_context(info.context)
+    transactions = models.TransactionItem.objects.using(connection_name).all()
+
+    if isinstance(requestor, app_models.App):
+        # App with MANAGE_ORDERS permission can see all transactions
+        if requestor.has_perm(OrderPermissions.MANAGE_ORDERS):
+            return transactions
+        # Otherwise, app can only see transactions it created
+        return transactions.filter(app_id=requestor.id)
+
+    # Filter by accessible channels for user
+    accessible_channels = get_user_accessible_channels(info, requestor)
+    channel_ids = [channel.id for channel in accessible_channels]
+    orders = order_models.Order.objects.using(connection_name).filter(
+        channel_id__in=channel_ids
+    )
+    checkouts = checkout_models.Checkout.objects.using(connection_name).filter(
+        channel_id__in=channel_ids
+    )
+
+    # Filter transactions by orders or checkouts in accessible channels
+    return transactions.filter(
+        Q(order_id__in=orders.values("id"))
+        | Q(checkout_id__in=checkouts.values("token"))
     )
