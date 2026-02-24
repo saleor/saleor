596bf6a3bd3392a21300d80b65c232a5a77c5c17
IKarbowiak <iga.karbowiak@saleor.io>
2026-02-24 13:29:23 +0100

    Clarify NoValidationSearchVector init normalization

--- saleor/core/postgres.py
+++ saleor/core/postgres.py
@@ -44,14 +44,15 @@ class NoValidationSearchVector(SearchVector, NoValidationSearchVectorCombinable)
     and/or over clause.
     """
 
-    def __init__(self, *expressions, **kwargs):
+    def __init__(self, *expressions, config=None, weight=None):
         processed = []
         for expr in expressions:
             if isinstance(expr, Value) and isinstance(expr.value, str):
+                # Normalize only string literals to make search accent-insensitive.
                 processed.append(Value(strip_accents(expr.value)))
             else:
                 processed.append(expr)
-        super().__init__(*processed, **kwargs)
+        super().__init__(*processed, config=config, weight=weight)
 
 
 class FlatConcat(Expression):
