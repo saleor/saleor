1c439e294776528bbfda19ad0fc36cd1e3b08bf6
Przemysław Łada <przlada@gmail.com>
2026-02-26 10:52:15 +0100

    Update saleor/graphql/product/bulk_mutations/product_bulk_create.py
    Co-authored-by: Iga Karbowiak <40886528+IKarbowiak@users.noreply.github.com>

--- saleor/graphql/product/bulk_mutations/product_bulk_create.py
+++ saleor/graphql/product/bulk_mutations/product_bulk_create.py
@@ -440,7 +440,8 @@ class ProductBulkCreate(BaseMutation):
         for index, media_input in enumerate(media_inputs):
             image = media_input.get("image")
             media_url = media_input.get("media_url")
-            alt = media_input["alt"] = media_input.get("alt") or ""
+            alt = media_input.get("alt") or ""
+            media_input["alt"] = alt
 
             if not image and not media_url:
                 index_error_map[product_index].append(
