c886ebbb89dde91ac879cd3a52d53e1676489bc2
Marcin Wcisło <115464873+wcislo-saleor@users.noreply.github.com>
2026-03-02 10:25:06 +0100

    Implement a task for fetching product media image (#18862)
    * Implement a task for fetching product media image
    
    * Do not keep session open for longer than it's necessary.
    Replace catch-them-all Exception with list of actually expected
    exceptions.
    
    * Fix backoff for retrying the task.
    
    * Move shared functions away from graphql/ directory
    
    * Raise an exception instead of emitting warning when task encounters
    ProductMedia in an invalid state.
    
    * Simplify the test.
    
    * Change the processing order (first validate MIME type by reading magic
    bytes and only then open file with PIL)
    
    * Add test ensuring svg is not accepted.
    
    * Rework the implementation for easier reading.
    Most of the cases the were initally treated as retry-able are now not
    (mostly cases where incorrect headers, incorrect file was provided).
    
    * Introduce HTTP status code checks to prevent reading garbage responses.
    
    * Clean up and move helper functions to utils
    
    * Treat 3xx status codes as errors as well.
    Improve type hints.
    Introduce UnhandledException in place of custom logic that re-raises an
    exception.
    
    * Remove code duplication in tests.
    
    * Do not retry on all DatabaseErrors, scope it down to OperationalErrors
    (such as database and connection issues).
    
    * Add an option to run the task in a separate queue.
    
    * Remove unnecessary code.
    
    * Simplify the code.
    Catch both requests.ConnectionError and Python ConnectionError (retry
    task for both).
    
    * If we were to fail with non-retryable error with the current
    implementation (e.g. in productMediaCreate) then we would not create
    ProductMedia at all, therefore if non-retryable error is encountered
    remove the object.
    
    * Remove custom exceptions.
    
    * Fix signature.
    In most cases use .apply() when testing a task (to also include
    on_failure hook logic as part of the logic being tested).
    
    * Fix import.
    Add comment explaining crucial part of the task logic.
    Fix the test to intentionally mock HTTP request.
    
    * Do not catch ValueError and let on_failure handler deal with it.
    Accept situations where exception classes sharing IOError and ValueError
    as parents (e.g. requests.InvalidSchema) could be cause pointless
    retries.

--- saleor/settings.py
+++ saleor/settings.py
@@ -1061,6 +1061,8 @@ DATA_MIGRATIONS_TASKS_QUEUE_NAME = os.environ.get(
     "DATA_MIGRATIONS_TASKS_QUEUE_NAME", None
 )
 
+FETCH_IMAGES_QUEUE_NAME = os.environ.get("FETCH_IMAGES_QUEUE_NAME", None)
+
 # Lock time for request password reset mutation per user (seconds)
 RESET_PASSWORD_LOCK_TIME = parse(
     os.environ.get("RESET_PASSWORD_LOCK_TIME", "15 minutes")
