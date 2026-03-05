82e2153ffef17bd249a2afd1ba53c9ce0c286ac0
IKarbowiak <iga.karbowiak@saleor.io>
2026-03-05 13:32:43 +0100

    Extend SiteSettings with allowed_staff_auth_methods

--- saleor/site/__init__.py
+++ saleor/site/__init__.py
@@ -1,3 +1,15 @@
+class PasswordLoginMode:
+    ENABLED = "enabled"
+    CUSTOMERS_ONLY = "customers_only"
+    DISABLED = "disabled"
+
+    CHOICES = [
+        (ENABLED, "Enabled"),
+        (CUSTOMERS_ONLY, "Customers only"),
+        (DISABLED, "Disabled"),
+    ]
+
+
 class GiftCardSettingsExpiryType:
     NEVER_EXPIRE = "never_expire"
     EXPIRY_PERIOD = "expiry_period"
