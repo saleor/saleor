4630b3287ad148185e2ae8462711f1f57b953f39
Jayesh Patel <jaypatel6963@gmail.com>
2026-04-17 16:26:58 -0400

    Add checkout delete mutation
    Add a staff/app-only checkoutDelete mutation guarded by MANAGE_CHECKOUTS. The mutation deletes the checkout by global ID and returns only mutation errors, matching the regression-testing use case from issue #19114.

---
+++ saleor/graphql/checkout/mutations/checkout_delete.py
@@ -0,0 +1,45 @@
+import graphene
+
+from ....checkout.error_codes import CheckoutErrorCode
+from ....checkout.models import Checkout as CheckoutModel
+from ....permission.enums import CheckoutPermissions
+from ...core import ResolveInfo
+from ...core.descriptions import ADDED_IN_324
+from ...core.doc_category import DOC_CATEGORY_CHECKOUT
+from ...core.enums import CheckoutErrorCode as CheckoutErrorCodeEnum
+from ...core.mutations import BaseMutation
+from ...core.types import Error
+from ..types import Checkout
+
+
+class CheckoutDeleteError(Error):
+    code = CheckoutErrorCodeEnum(description="The error code.", required=True)
+
+    class Meta:
+        doc_category = DOC_CATEGORY_CHECKOUT
+
+
+class CheckoutDelete(BaseMutation):
+    class Arguments:
+        id = graphene.ID(
+            description="The checkout's ID." + ADDED_IN_324,
+            required=True,
+        )
+
+    class Meta:
+        description = "Deletes a checkout." + ADDED_IN_324
+        doc_category = DOC_CATEGORY_CHECKOUT
+        permissions = (CheckoutPermissions.MANAGE_CHECKOUTS,)
+        error_type_class = CheckoutDeleteError
+
+    @classmethod
+    def perform_mutation(cls, _root, info: ResolveInfo, /, *, id: str):
+        checkout = cls.get_node_or_error(
+            info,
+            id,
+            only_type=Checkout,
+            qs=CheckoutModel.objects,
+            code=CheckoutErrorCode.NOT_FOUND.value,
+        )
+        checkout.delete()
+        return CheckoutDelete()
---
+++ saleor/graphql/checkout/tests/mutations/test_checkout_delete.py
@@ -0,0 +1,138 @@
+import graphene
+import pytest
+
+from .....checkout.error_codes import CheckoutErrorCode
+from .....checkout.models import Checkout
+from ....core.utils import to_global_id_or_none
+from ....tests.utils import assert_no_permission, get_graphql_content
+
+MUTATION_CHECKOUT_DELETE = """
+    mutation checkoutDelete($id: ID!) {
+        checkoutDelete(id: $id) {
+            errors {
+                message
+                code
+                field
+            }
+        }
+    }
+"""
+
+
+def test_checkout_delete_by_staff(
+    staff_api_client, checkout_with_items, permission_manage_checkouts
+):
+    # given
+    checkout = checkout_with_items
+    checkout_id = to_global_id_or_none(checkout)
+    variables = {"id": checkout_id}
+
+    # when
+    response = staff_api_client.post_graphql(
+        MUTATION_CHECKOUT_DELETE,
+        variables,
+        permissions=[permission_manage_checkouts],
+    )
+
+    # then
+    content = get_graphql_content(response)
+    data = content["data"]["checkoutDelete"]
+    assert data["errors"] == []
+    assert not Checkout.objects.filter(pk=checkout.pk).exists()
+
+
+def test_checkout_delete_by_app(
+    app_api_client, checkout_with_items, permission_manage_checkouts
+):
+    # given
+    checkout = checkout_with_items
+    checkout_id = to_global_id_or_none(checkout)
+    variables = {"id": checkout_id}
+
+    # when
+    response = app_api_client.post_graphql(
+        MUTATION_CHECKOUT_DELETE,
+        variables,
+        permissions=[permission_manage_checkouts],
+    )
+
+    # then
+    content = get_graphql_content(response)
+    data = content["data"]["checkoutDelete"]
+    assert data["errors"] == []
+    assert not Checkout.objects.filter(pk=checkout.pk).exists()
+
+
+@pytest.mark.parametrize("client", ["api_client", "user_api_client"])
+def test_checkout_delete_without_permission_is_denied(
+    client, request, checkout_with_items
+):
+    # given
+    api_client = request.getfixturevalue(client)
+    checkout = checkout_with_items
+    checkout_id = to_global_id_or_none(checkout)
+    variables = {"id": checkout_id}
+
+    # when
+    response = api_client.post_graphql(MUTATION_CHECKOUT_DELETE, variables)
+
+    # then
+    assert_no_permission(response)
+    assert Checkout.objects.filter(pk=checkout.pk).exists()
+
+
+def test_checkout_delete_by_staff_without_permission_is_denied(
+    staff_api_client, checkout_with_items
+):
+    # given
+    checkout = checkout_with_items
+    checkout_id = to_global_id_or_none(checkout)
+    variables = {"id": checkout_id}
+
+    # when
+    response = staff_api_client.post_graphql(MUTATION_CHECKOUT_DELETE, variables)
+
+    # then
+    assert_no_permission(response)
+    assert Checkout.objects.filter(pk=checkout.pk).exists()
+
+
+def test_checkout_delete_by_app_without_permission_is_denied(
+    app_api_client, checkout_with_items
+):
+    # given
+    checkout = checkout_with_items
+    checkout_id = to_global_id_or_none(checkout)
+    variables = {"id": checkout_id}
+
+    # when
+    response = app_api_client.post_graphql(MUTATION_CHECKOUT_DELETE, variables)
+
+    # then
+    assert_no_permission(response)
+    assert Checkout.objects.filter(pk=checkout.pk).exists()
+
+
+def test_checkout_delete_invalid_checkout_id(
+    staff_api_client, checkout_with_items, permission_manage_checkouts
+):
+    # given
+    checkout = checkout_with_items
+    checkout_id = graphene.Node.to_global_id("Checkout", checkout.pk)
+    checkout.delete()
+    variables = {"id": checkout_id}
+
+    # when
+    response = staff_api_client.post_graphql(
+        MUTATION_CHECKOUT_DELETE,
+        variables,
+        permissions=[permission_manage_checkouts],
+    )
+
+    # then
+    content = get_graphql_content(response)
+    errors = content["data"]["checkoutDelete"]["errors"]
+    assert len(errors) == 1
+    assert errors[0]["field"] == "id"
+    assert errors[0]["code"] == CheckoutErrorCode.NOT_FOUND.name
+    assert errors[0]["message"] == f"Couldn't resolve to a node: {checkout_id}"
