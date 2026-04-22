f976e1976b8bd679bcd0083e1f175d8df6bdf0e2
Jayesh Patel <jaypatel6963@gmail.com>
2026-04-22 12:45:01 -0400

    Change checkout delete mutation version to ADDED_IN_323
    Backport target is 3.23, so update the version annotation on the
    checkoutDelete mutation and its id argument. Regenerate schema.graphql
    to match.

--- saleor/graphql/checkout/mutations/checkout_delete.py
+++ saleor/graphql/checkout/mutations/checkout_delete.py
@@ -4,7 +4,7 @@ from ....checkout.error_codes import CheckoutErrorCode
 from ....checkout.models import Checkout as CheckoutModel
 from ....permission.enums import CheckoutPermissions
 from ...core import ResolveInfo
-from ...core.descriptions import ADDED_IN_324
+from ...core.descriptions import ADDED_IN_323
 from ...core.doc_category import DOC_CATEGORY_CHECKOUT
 from ...core.enums import CheckoutErrorCode as CheckoutErrorCodeEnum
 from ...core.mutations import BaseMutation
@@ -22,12 +22,12 @@ class CheckoutDeleteError(Error):
 class CheckoutDelete(BaseMutation):
     class Arguments:
         id = graphene.ID(
-            description="The checkout's ID." + ADDED_IN_324,
+            description="The checkout's ID." + ADDED_IN_323,
             required=True,
         )
 
     class Meta:
-        description = "Deletes a checkout." + ADDED_IN_324
+        description = "Deletes a checkout." + ADDED_IN_323
         doc_category = DOC_CATEGORY_CHECKOUT
         permissions = (CheckoutPermissions.MANAGE_CHECKOUTS,)
         error_type_class = CheckoutDeleteError
