c3225432adb55107c56e8a0257d85bb9d958e1e5
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-04-28 18:46:07 +0200

    fix lint

--- saleor/app/management/commands/app_delete_all.py
+++ saleor/app/management/commands/app_delete_all.py
@@ -24,8 +24,7 @@ class Command(BaseCommand):
 
     def handle(self, *args: Any, **options: Any) -> None:
         apps = App.objects.not_removed().order_by("pk")
-        if not apps.exists():   
-        if not apps:
+        if not apps.exists():
             self.stdout.write("No active apps to delete.")
             return
 
