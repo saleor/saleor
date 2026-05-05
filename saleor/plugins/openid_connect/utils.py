saleor/saleor

commit b4324e723b76f8b3ac3be0e5a9d3ed569fac6037

Enable pygrep-hooks lint rules in Ruff (#16896)
Introduces strict checks for `#noqa` and `#type: ignore` comments.

I took the liberty to fix some of the existing type errors by correcting type annotations instead of fixing the comments that disabled typechecking.

Rewrote weird magic in class `WhereFilter` to the usual `property` syntax.

saleor/plugins/openid_connect/utils.py
@@ -399,11 +399,12 @@ def get_or_create_user_from_payload(
     if not user_email:
         raise AuthenticationError("Missing user's email.")
 
-    sub = payload.get("sub")
+    sub: str | None = payload.get("sub")
     get_kwargs = {"private_metadata__contains": {oidc_metadata_key: sub}}
     if not sub:
         get_kwargs = {"email": user_email}
         logger.warning("Missing sub section in OIDC payload")
+        raise AuthenticationError("Missing subject identifier.")
 
     defaults_create = {
         "is_active": True,
@@ -414,7 +415,7 @@ def get_or_create_user_from_payload(
         "private_metadata": {oidc_metadata_key: sub},
         "password": make_password(None),
     }
-    cache_key = oidc_metadata_key + ":" + str(sub)
+    cache_key = oidc_metadata_key + ":" + sub
     user_id = cache.get(cache_key)
     if user_id:
         get_kwargs = {"id": user_id}
@@ -448,7 +449,7 @@ def get_or_create_user_from_payload(
         user_email=user_email,
         user_first_name=defaults_create["first_name"],
         user_last_name=defaults_create["last_name"],
-        sub=sub,  # type: ignore
+        sub=sub,
         last_login=last_login,
     )
 

saleor/attribute/utils.py
@@ -106,7 +106,7 @@ def _associate_attribute_to_instance(
             "attribute_id__in": list(attr_val_map.keys()),
             "product_type_id": prod_type_id,
         }
-        instance_attrs_ids = instance_attribute_model.objects.filter(  # type: ignore
+        instance_attrs_ids = instance_attribute_model.objects.filter(  # type: ignore[attr-defined]
             **attribute_filter
         ).values_list("pk", flat=True)
 

saleor/checkout/utils.py
@@ -67,6 +67,7 @@ if TYPE_CHECKING:
     from measurement.measures import Weight
 
     from ..account.models import Address
+    from ..core.models import ModelWithMetadata
     from ..core.pricing.interface import LineInfo
     from ..order.models import Order, OrderLine
     from .fetch import CheckoutInfo, CheckoutLineInfo
@@ -77,7 +78,7 @@ PRIVATE_META_APP_SHIPPING_ID = "external_app_shipping_id"
 
 def invalidate_checkout(
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     manager: "PluginsManager",
     *,
     recalculate_discount: bool = True,
@@ -93,7 +94,7 @@ def invalidate_checkout(
 
 def recalculate_checkout_discounts(
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     manager: "PluginsManager",
 ):
     """Recalculate checkout discounts.
@@ -230,7 +231,7 @@ def add_variant_to_checkout(
     return checkout
 
 
-def calculate_checkout_quantity(lines: Iterable["CheckoutLineInfo"]):
+def calculate_checkout_quantity(lines: list["CheckoutLineInfo"]):
     return sum([line_info.line.quantity for line_info in lines])
 
 
@@ -398,7 +399,7 @@ def change_billing_address_in_checkout(checkout, address) -> list[str]:
 def change_shipping_address_in_checkout(
     checkout_info: "CheckoutInfo",
     address: "Address",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     manager: "PluginsManager",
     shipping_channel_listings: Iterable["ShippingMethodChannelListing"],
 ):
@@ -432,7 +433,7 @@ def change_shipping_address_in_checkout(
 def _get_shipping_voucher_discount_for_checkout(
     voucher: Voucher,
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     address: Optional["Address"],
 ):
     """Calculate discount value for a voucher of shipping type."""
@@ -488,7 +489,7 @@ def get_voucher_discount_for_checkout(
     manager: PluginsManager,
     voucher: Voucher,
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     address: Optional["Address"],
 ) -> Money:
     """Calculate discount value depending on voucher and discount types.
@@ -520,7 +521,7 @@ def get_voucher_discount_for_checkout(
 
 def _get_products_voucher_discount(
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     voucher,
 ):
     """Calculate products discount value for a voucher, depending on its type."""
@@ -606,7 +607,7 @@ def check_voucher_for_checkout(
     voucher: Voucher,
     manager: PluginsManager,
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
 ):
     checkout = checkout_info.checkout
     address = checkout_info.shipping_address or checkout_info.billing_address
@@ -628,7 +629,7 @@ def check_voucher_for_checkout(
 def recalculate_checkout_discount(
     manager: PluginsManager,
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
 ):
     """Recalculate `checkout.discount` based on the voucher.
 
@@ -684,7 +685,7 @@ def recalculate_checkout_discount(
 def add_promo_code_to_checkout(
     manager: PluginsManager,
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     promo_code: str,
 ):
     """Add gift card or voucher data to checkout.
@@ -713,7 +714,7 @@ def add_promo_code_to_checkout(
 def add_voucher_code_to_checkout(
     manager: PluginsManager,
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     voucher_code: str,
 ):
     """Add voucher data to checkout by code.
@@ -739,7 +740,7 @@ def add_voucher_code_to_checkout(
 def add_voucher_to_checkout(
     manager: PluginsManager,
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     voucher: Voucher,
     voucher_code: VoucherCode,
 ):
@@ -839,7 +840,7 @@ def remove_voucher_from_checkout(checkout: Checkout):
 
 def get_valid_internal_shipping_methods_for_checkout(
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     subtotal: "Money",
     shipping_channel_listings: Iterable["ShippingMethodChannelListing"],
     country_code: Optional[str] = None,
@@ -876,7 +877,7 @@ def get_valid_internal_shipping_methods_for_checkout(
 
 
 def get_valid_collection_points_for_checkout(
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     channel_id: int,
     quantity_check: bool = True,
     database_connection_name: str = settings.DATABASE_CONNECTION_DEFAULT_NAME,
@@ -933,7 +934,7 @@ def clear_delivery_method(checkout_info: "CheckoutInfo"):
 def is_fully_paid(
     manager: PluginsManager,
     checkout_info: "CheckoutInfo",
-    lines: Iterable["CheckoutLineInfo"],
+    lines: list["CheckoutLineInfo"],
     database_connection_name: str = settings.DATABASE_CONNECTION_DEFAULT_NAME,
 ):
     """Check if provided payment methods cover the checkout's total amount.
@@ -968,12 +969,12 @@ def activate_payments(payment_ids: list[int]) -> None:
     Payment.objects.filter(id__in=payment_ids).update(is_active=True)
 
 
-def is_shipping_required(lines: Iterable["CheckoutLineInfo"]):
+def is_shipping_required(lines: list["CheckoutLineInfo"]):
     """Check if shipping is required for given checkout lines."""
     return any(line_info.product_type.is_shipping_required for line_info in lines)
 
 
-def validate_variants_in_checkout_lines(lines: Iterable["CheckoutLineInfo"]):
+def validate_variants_in_checkout_lines(lines: list["CheckoutLineInfo"]):
     variants_listings_map = {line.variant.id: line.channel_listing for line in lines}
 
     not_available_variants = [
@@ -1006,11 +1007,12 @@ def set_external_shipping_id(checkout: Checkout, app_shipping_id: str):
 
 
 def get_external_shipping_id(container: Union["Checkout", "Order"]):
-    if type(container) is Checkout:
-        container = get_checkout_metadata(container)
-    return container.get_value_from_private_metadata(  # type:ignore
-        PRIVATE_META_APP_SHIPPING_ID
-    )
+    metadata_object: ModelWithMetadata
+    if isinstance(container, Checkout):
+        metadata_object = get_checkout_metadata(container)
+    else:
+        metadata_object = container
+    return metadata_object.get_value_from_private_metadata(PRIVATE_META_APP_SHIPPING_ID)
 
 
 def delete_external_shipping_id(checkout: Checkout, save: bool = False):
@@ -1036,7 +1038,7 @@ def get_checkout_metadata(checkout: "Checkout"):
     return CheckoutMetadata(checkout=checkout)
 
 
-def calculate_checkout_weight(lines: Iterable["CheckoutLineInfo"]) -> "Weight":
+def calculate_checkout_weight(lines: list["CheckoutLineInfo"]) -> "Weight":
     weights = zero_weight()
     for checkout_line_info in lines:
         variant = checkout_line_info.variant
@@ -1075,7 +1077,7 @@ def get_address_for_checkout_taxes(
 
 def checkout_info_for_logs(
     checkout_info: "CheckoutInfo",
-    checkout_lines_info: Iterable["CheckoutLineInfo"],
+    checkout_lines_info: list["CheckoutLineInfo"],
 ):
     checkout = checkout_info.checkout
     checkout_id = graphene.Node.to_global_id("Checkout", checkout.pk)
@@ -1136,7 +1138,7 @@ def checkout_info_for_logs(
 def log_unknown_discount_reason(
     order_lines: Iterable["OrderLine"],
     checkout_info: "CheckoutInfo",
-    checkout_lines_info: Iterable["CheckoutLineInfo"],
+    checkout_lines_info: list["CheckoutLineInfo"],
     logger,
 ):
     prices_entered_with_tax = checkout_info.tax_configuration.prices_entered_with_tax

saleor/giftcard/utils.py
@@ -161,7 +161,7 @@ def fulfill_gift_card_lines(
 @traced_atomic_transaction()
 def gift_cards_create(
     order: "Order",
-    gift_card_lines_info: Iterable["GiftCardLineData"],
+    gift_card_lines_info: list["GiftCardLineData"],
     settings: "SiteSettings",
     requestor_user: Optional["User"],
     app: Optional["App"],
@@ -275,7 +275,7 @@ def assign_user_gift_cards(user):
 def is_gift_card_expired(gift_card: GiftCard):
     """Return True when gift card expiry date pass."""
     today = timezone.now().date()
-    return bool(gift_card.expiry_date) and gift_card.expiry_date < today  # type: ignore
+    return bool(gift_card.expiry_date) and gift_card.expiry_date < today  # type: ignore[operator]
 
 
 def get_user_gift_cards(user: "User") -> "QuerySet":

saleor/graphql/account/mutations/authentication/utils.py
@@ -3,7 +3,7 @@ import datetime
 import jwt
 from django.conf import settings
 from django.core.exceptions import ValidationError
-from django.middleware.csrf import (  # type: ignore
+from django.middleware.csrf import (  # type: ignore[attr-defined]
     _get_new_csrf_string,
     _mask_cipher_secret,
     _unmask_cipher_token,

saleor/graphql/attribute/utils.py
@@ -746,7 +746,7 @@ class AttributeAssignmentMixin:
         for ref in attr_values.references:
             name = getattr(ref, field_name)
             if attribute.entity_type == AttributeEntityType.PRODUCT_VARIANT:
-                name = f"{ref.product.name}: {name}"  # type: ignore
+                name = f"{ref.product.name}: {name}"  # type: ignore[union-attr]
 
             reference_list.append(
                 (
@@ -754,7 +754,7 @@ class AttributeAssignmentMixin:
                     {
                         "attribute": attribute,
                         "slug": slugify(
-                            unidecode(f"{instance.id}_{ref.id}")  # type: ignore
+                            unidecode(f"{instance.id}_{ref.id}")  # type: ignore[union-attr]
                         ),
                         "defaults": {"name": name},
                         attr_value_field: ref,

saleor/graphql/checkout/mutations/utils.py
@@ -71,7 +71,7 @@ class CheckoutLineData:
 
 def clean_delivery_method(
     checkout_info: "CheckoutInfo",
-    lines: Iterable[CheckoutLineInfo],
+    lines: list[CheckoutLineInfo],
     method: Optional[
         Union[
             shipping_interface.ShippingMethodData,
@@ -111,14 +111,14 @@ def _is_external_shipping_valid(checkout_info: "CheckoutInfo") -> bool:
 
 
 def update_checkout_external_shipping_method_if_invalid(
-    checkout_info: "CheckoutInfo", lines: Iterable[CheckoutLineInfo]
+    checkout_info: "CheckoutInfo", lines: list[CheckoutLineInfo]
 ):
     if not _is_external_shipping_valid(checkout_info):
         delete_external_shipping_id(checkout_info.checkout, save=True)
 
 
 def update_checkout_shipping_method_if_invalid(
-    checkout_info: "CheckoutInfo", lines: Iterable[CheckoutLineInfo]
+    checkout_info: "CheckoutInfo", lines: list[CheckoutLineInfo]
 ):
     quantity = calculate_checkout_quantity(lines)
 

saleor/order/utils.py
@@ -89,7 +89,7 @@ def order_line_needs_automatic_fulfillment(line_data: OrderLineInfo) -> bool:
     return False
 
 
-def order_needs_automatic_fulfillment(lines_data: Iterable["OrderLineInfo"]) -> bool:
+def order_needs_automatic_fulfillment(lines_data: list["OrderLineInfo"]) -> bool:
     """Check if order has digital products which should be automatically fulfilled."""
     for line_data in lines_data:
         if line_data.is_digital and order_line_needs_automatic_fulfillment(line_data):
@@ -796,7 +796,7 @@ def create_order_discount_for_order(
 ):
     """Add new order discount and update the prices."""
 
-    current_total = order.undiscounted_total
+    current_total: TaxedMoney = order.undiscounted_total
     currency = order.currency
 
     gross_total = apply_discount_to_value(
@@ -810,7 +810,7 @@ def create_order_discount_for_order(
         value_type=value_type,
         value=value,
         reason=reason,
-        amount=new_amount,  # type: ignore
+        amount=new_amount,  # type: ignore[misc]
         **kwargs,
     )
     return order_discount

commit 205f1c6b63a40fd1b9dc9b3a713381869659d39f

fix(oidc): remove deadcode
Removes deadcode caused by b4324e723b76f8b3ac3be0e5a9d3ed569fac6037:

```
diff --git a/saleor/plugins/openid_connect/utils.py b/saleor/plugins/openid_connect/utils.py
index 6446f653861..f3d368c1324 100644
--- a/saleor/plugins/openid_connect/utils.py
+++ b/saleor/plugins/openid_connect/utils.py
@@ -399,11 +399,12 @@ def get_or_create_user_from_payload(
     if not user_email:
         raise AuthenticationError("Missing user's email.")
 
-    sub = payload.get("sub")
+    sub: str | None = payload.get("sub")
     get_kwargs = {"private_metadata__contains": {oidc_metadata_key: sub}}
     if not sub:
         get_kwargs = {"email": user_email}
         logger.warning("Missing sub section in OIDC payload")
+        raise AuthenticationError("Missing subject identifier.")
 
     defaults_create = {
         "is_active": True,
@@ -414,7 +415,7 @@ def get_or_create_user_from_payload(
         "private_metadata": {oidc_metadata_key: sub},
         "password": make_password(None),
     }
-    cache_key = oidc_metadata_key + ":" + str(sub)
+    cache_key = oidc_metadata_key + ":" + sub
     user_id = cache.get(cache_key)
     if user_id:
         get_kwargs = {"id": user_id}
@@ -448,7 +449,7 @@ def get_or_create_user_from_payload(
         user_email=user_email,
         user_first_name=defaults_create["first_name"],
         user_last_name=defaults_create["last_name"],
-        sub=sub,  # type: ignore
+        sub=sub,
         last_login=last_login,
     )
```

This was making the code look like it was able to a user fetch without 'sub' field

saleor/plugins/openid_connect/utils.py
@@ -416,7 +416,6 @@ def get_or_create_user_from_payload(
     sub: str | None = payload.get("sub")
     get_kwargs = {"private_metadata__contains": {oidc_metadata_key: sub}}
     if not sub:
-        get_kwargs = {"email": user_email}
         logger.warning("Missing sub section in OIDC payload")
         raise AuthenticationError("Missing subject identifier.")
 