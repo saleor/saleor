c4780c4c242e70119e4622390275d85f4d9ab6c0
Marcin Wcislo <marcin.wcislo@saleor.io>
2026-07-29 09:55:33 +0200

    Add missing permission tests and tests for counting queries in the new dataloaders

--- saleor/graphql/account/tests/queries/test_customer_type_attributes.py
+++ saleor/graphql/account/tests/queries/test_customer_type_attributes.py
@@ -181,3 +181,58 @@ def test_available_attributes_where_slug(
     available = content["data"]["customerType"]["availableAttributes"]
     assert available["totalCount"] == 1
     assert available["edges"][0]["node"]["slug"] == loyalty_customer_attribute.slug
+
+
+def test_attributes_with_manage_users_permission_includes_hidden(
+    staff_api_client,
+    permission_manage_users,
+    customer_type,
+    loyalty_customer_attribute,
+    hidden_customer_attribute,
+):
+    # given
+    staff_api_client.user.user_permissions.add(permission_manage_users)
+    customer_type.customer_attributes.add(
+        loyalty_customer_attribute, hidden_customer_attribute
+    )
+    variables = {"id": graphene.Node.to_global_id("CustomerType", customer_type.pk)}
+
+    # when
+    response = staff_api_client.post_graphql(CUSTOMER_TYPE_ATTRIBUTES_QUERY, variables)
+
+    # then
+    content = get_graphql_content(response)
+    attributes = content["data"]["customerType"]["attributes"]
+    assert {attr["slug"] for attr in attributes} == {
+        loyalty_customer_attribute.slug,
+        hidden_customer_attribute.slug,
+    }
+
+
+def test_available_attributes_with_manage_users_permission(
+    staff_api_client,
+    permission_manage_users,
+    customer_type,
+    loyalty_customer_attribute,
+    segment_customer_attribute,
+    hidden_customer_attribute,
+    size_page_attribute,
+):
+    # given
+    staff_api_client.user.user_permissions.add(permission_manage_users)
+    customer_type.customer_attributes.add(loyalty_customer_attribute)
+    variables = {"id": graphene.Node.to_global_id("CustomerType", customer_type.pk)}
+
+    # when
+    response = staff_api_client.post_graphql(
+        CUSTOMER_TYPE_AVAILABLE_ATTRIBUTES_QUERY, variables
+    )
+
+    # then
+    content = get_graphql_content(response)
+    available = content["data"]["customerType"]["availableAttributes"]
+    assert available["totalCount"] == 2
+    assert {edge["node"]["slug"] for edge in available["edges"]} == {
+        segment_customer_attribute.slug,
+        hidden_customer_attribute.slug,
+    }
